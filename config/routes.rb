3435f1018ad2fab735548a258a198cf87cd3344d
zanst <me@zan.st>
2011-09-23 14:07:48 -0300

    removed fakes created by rinaldi.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Olook::Application.routes.draw do
-  resources :products
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
--- spec/models/product_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe Product do
-  it { should validate_presence_of(:name) }
-end
