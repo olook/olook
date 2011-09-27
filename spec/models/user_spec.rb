46a61c8dc8cdee34549c3f7757aafa99befae917
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-09-27 14:22:33 -0300

    Tweaking tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,15 @@ require 'spec_helper'
 
 describe User do
 
-  let(:user) { Factory(:user) }
-  let(:profile) { mock_model('Profile') }
+  before :each do
+    @user = Factory.create(:user) 
+    @profile = mock_model('Profile')
+  end 
 
-  it { should validate_uniqueness_of(:email) }
   it { should allow_value("a@b.com").for(:email) }
   it { should_not allow_value("@b.com").for(:email) }
   it { should_not allow_value("a@b.").for(:email) }
+  it { should validate_uniqueness_of(:email) }
 
   it { should allow_value("José").for(:first_name) }
   it { should allow_value("José Bar").for(:first_name) }
@@ -18,8 +20,8 @@ describe User do
   it { should_not allow_value("123").for(:first_name) }
 
   it "should counts and write points" do
-    hash = {profile.id => 2}
-    user.counts_and_write_points(hash)
+    hash = {@profile.id => 2}
+    @user.counts_and_write_points(hash)
     Point.should have(1).record
   end
 
