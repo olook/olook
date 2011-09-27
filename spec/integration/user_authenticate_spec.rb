1ee96190162b70a4c6bae4f305f3c02e3d5ba107
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-09-27 13:10:46 -0300

    Added user sign up integration test

--- spec/integration/user_authenticate_spec.rb
+++ spec/integration/user_authenticate_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 feature "User Authenticate", %q{
-  In order to see give a full access
+  In order to give a full access
   As a user
   I want to authenticate using my Facebook account or a normal register
 } do
@@ -10,6 +10,15 @@ feature "User Authenticate", %q{
     @user = Factory(:user)
     User.any_instance.stub(:counts_and_write_points)
     @casual = Factory(:casual_profile)
+  end
+
+  scenario "User Sign up" do
+    visit new_user_registration_path
+    fill_in "user_email", :with => "fake@mail.com"
+    fill_in "user_password", :with => "123456"
+    fill_in "user_password_confirmation", :with => "123456"
+    click_button "Sign up"
+    page.should have_content(I18n.t "devise.registrations.signed_up")
   end
 
   scenario "User Log in" do
