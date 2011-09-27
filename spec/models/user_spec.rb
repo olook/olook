e26832e5c45d7b502b7b5e0abb8aeec17a699355
zanst <me@zan.st>
2011-09-26 23:25:50 -0300

    counts_and_write_points test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
 describe User do
-  it "should create an user" do
-    User.create!(:first_name => "John",
+  before(:all) do
+    @user = User.create!(:first_name => "John",
                  :last_name => "Doe",
                  :email => "johndoe@foobar.com",
                  :password => "123456")
+  end
+
+  before do
+    @profile = mock_model('Profile')
+  end
+
+  it "should counts and write points" do
+    hash = {@profile.id => 2}
+    @user.counts_and_write_points(hash)
+    Point.should have(1).record
   end
 
 end
