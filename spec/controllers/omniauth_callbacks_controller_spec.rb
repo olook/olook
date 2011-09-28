f5dc2ab3afdecae499b495ce5e88cd123f777f2f
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-09-28 15:48:34 -0300

    Facebook Register: Redirecting the user if the survey is blank

---
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe OmniauthCallbacksController do
+
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
+  describe "GET facebook" do
+    it "should redirect if the user dont fill the Survey" do
+      session[:profile_points] = nil
+      get :facebook
+      response.should redirect_to(survey_index_path)
+    end
+
+    it "should not redirect when the user fill the Survey" do
+      session[:profile_points] = :some_data
+      User.stub(:find_for_facebook_oauth).and_return(user = mock_model(User))
+      user.stub(:persisted?).and_return(false)
+      get :facebook
+      response.should_not redirect_to(survey_index_path)
+    end
+  end
+   
+end
