31c787edba81e9521df4010454f670d6c4600f72
zanst <me@zan.st>
2011-09-28 17:04:29 -0300

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(:version => 20110927193945) do
     t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["unlock_token"], :name => "index_users_on_unlock_token", :unique => true
 
 end
