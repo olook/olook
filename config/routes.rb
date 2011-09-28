abf63f9de88f9aa0f80db5aeb87cb252451eef63
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-09-28 11:13:00 -0300

    Added PagesController with the Welcome action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Olook::Application.routes.draw do
-  root :to => "home#index"
 
+  root :to => "home#index" 
   resources :survey, :only => [:index, :create]
+  match "/welcome", :to => "pages#welcome", :as => "welcome" 
 
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :registrations => "registrations" } do
     get '/users/auth/:provider' => 'omniauth_callbacks#passthru'
