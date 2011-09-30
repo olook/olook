4abc7711dc4422c5f3d975bd1f7bdff8f8e19bf8
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-09-30 14:38:55 -0300

    Fixed minor bug when not using spork

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,7 +13,7 @@ Spork.prefork do
   
   # Since we're using devise, the spork guys recommend us to reload the routes on this step
   # https://github.com/timcharper/spork/wiki/Spork.trap_method-Jujutsu
-  Spork.trap_method(Rails::Application::RoutesReloader, :reload!)
+  Spork.trap_method(Rails::Application::RoutesReloader, :reload!) if defined?(Rails)
   
   require File.expand_path("../../config/environment", __FILE__)
   require 'rspec/rails'
