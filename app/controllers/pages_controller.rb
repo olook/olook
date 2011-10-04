96ce74c9cab5b09eff646120f5e2f138a092b852
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-04 16:34:39 -0300

    added authentication to welcome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,9 @@
 class PagesController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:welcome]
+
   def welcome
     @member = current_user
   end
+
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,10 +2,25 @@ require 'spec_helper'
 
 describe PagesController do
 
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
   describe "GET 'welcome'" do
-    it "should be successful" do
-      get 'welcome'
-      response.should be_success
+    context "with a logged user" do
+      it "should be successful" do
+        user = Factory :user
+        sign_in user
+        get 'welcome'
+        response.should be_success
+      end
+    end
+
+    context "without a logged user" do
+      it "should be redirected to login" do
+        get 'welcome'
+        response.should redirect_to new_user_session_path
+      end
     end
   end
 
