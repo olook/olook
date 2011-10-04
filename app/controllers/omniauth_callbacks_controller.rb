1f35b41f7d16d9facbf691d48265eed621df85ca
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-04 13:19:26 -0300

    Linking facebook account for a logged user

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,14 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def facebook
-    user = User.find_for_facebook_oauth(env["omniauth.auth"])
-    if user
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
-      sign_in user
-      redirect_to welcome_path
+    if current_user
+      id = env["omniauth.auth"]["extra"]["user_hash"]["id"]
+      token = env["omniauth.auth"]["credentials"]["token"]
+      current_user.update_attributes(:uid => id, :facebook_token => token)
+      redirect_to root_path
     else
-      session["devise.facebook_data"] = env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      if user = User.find_for_facebook_oauth(env["omniauth.auth"])
+        flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
+        sign_in user
+        redirect_to welcome_path
+      else
+        session["devise.facebook_data"] = env["omniauth.auth"]
+        redirect_to new_user_registration_url
+      end
     end
   end
 
