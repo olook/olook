aa3a99dc4d4433c10109a5ece8eaef7fa7934b4a
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-04 15:45:02 -0300

    popup style to facebook auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -198,7 +198,7 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
-  config.omniauth :facebook, FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["app_secret"], :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }
+  config.omniauth :facebook, FACEBOOK_CONFIG["app_id"], FACEBOOK_CONFIG["app_secret"], :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } }, :display => "popup"
 
 
   # ==> Warden configuration
