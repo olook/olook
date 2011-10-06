576258e1d7dcee1e637ea8621b19cc278a6fe85d
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-06 14:54:36 -0300

    Fixed dumb integration error, my bad

--- spec/integration/helpers.rb
+++ spec/integration/helpers.rb
@@ -7,7 +7,7 @@ def answer_survey
   answer2 = FactoryGirl.create(:answer_from_casual_profile, :question => question)
   answer3 = FactoryGirl.create(:answer_from_casual_profile, :question => question)
   visit root_path
-  click_link "Take the Style Quiz"
+  click_link "take_survey"
   choose "questions[question_#{question.id}]" 
   click_button "Enviar"
 end
