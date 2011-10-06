f4c534ed647ba099b82d2e718c31d6edd80f7ca8
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-06 14:54:20 -0300

    Added CPF validation

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,12 +2,16 @@ FactoryGirl.define do
   factory :user do
     password "123456"
     password_confirmation "123456"
-    email "user@mail.com"
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
     first_name "User First Name"
     last_name "User Last Name"
 
     factory :member do
-      email "member@mail.com"
+      sequence :email do |n|
+      "member#{n}@example.com"
+      end
       first_name "First Name"
       last_name "Last Name"
 
