6db082ddf3bc280475f5fc436fcd36c5f522073a
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-06 11:56:00 -0300

    Added ProfileBuilder class. AR should be just a data structure

--- spec/controllers/survey_controller_spec.rb
+++ spec/controllers/survey_controller_spec.rb
@@ -25,8 +25,8 @@ describe SurveyController do
 
   describe "POST 'create'" do
     it "should assign profile_points in the session" do
-      Profile.stub(:profiles_given_questions).and_return("foo")
-      profile_points = Profile.stub(:build_profiles_points).and_return("bar").call
+      ProfileBuilder.stub(:profiles_given_questions).and_return("foo")
+      profile_points = ProfileBuilder.stub(:build_profiles_points).and_return("bar").call
       post 'create', :questions => {:foo => :bar}
       session[:profile_points].should == profile_points
     end
