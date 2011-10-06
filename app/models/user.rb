857e429bcfb31ac7c34cd8fc80894a47558b21bf
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-06 15:15:00 -0300

    Removed duplicated validation. Devise already sets the email as unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
 
   validate :check_cpf
-  validates :email, :uniqueness => true
   validates_format_of :email, :with => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
   validates_format_of :first_name, :with => /^[A-ZÀ-ÿ\s-]+$/i
   validates_format_of :last_name, :with => /^[A-ZÀ-ÿ\s-]+$/i
