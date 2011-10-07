cbc59df9d38e8f55bc592de4bfa2f0abfb768d51
zanst <me@zan.st>
2011-10-07 20:16:20 -0300

    UPDATE: fixing facebook sign in and popup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   before_filter :authenticate_user!, :only => [:welcome]
 
   def welcome
+    @redirect_uri = welcome_path
     @member = current_user
   end
 
