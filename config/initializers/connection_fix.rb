a1140fe384273358e1036a70d47ffff755cb0ee3
zanst <me@zan.st>
2011-10-11 15:42:50 -0300

    fixed connection_fix

--- config/initializers/connection_fix.rb
+++ config/initializers/connection_fix.rb
@@ -9,20 +9,46 @@
 # From:
 #   http://coderrr.wordpress.com/2009/01/08/activerecord-threading-issues-and-resolutions/
 
-module ActiveRecord::ConnectionAdapters
-  class MysqlAdapter
-    alias_method :execute_without_retry, :execute
-
-    def execute(*args)
-      execute_without_retry(*args)
-    rescue ActiveRecord::StatementInvalid => e
-      if e.message =~ /server has gone away/i
-        warn "Server timed out, retrying"
-        reconnect!
-        retry
-      else
-        raise e
+#Prevents Rails from loading multiple times. I could use a global variable too, but still...
+if $LOADED_FEATURES.include? __FILE__
+  if ActiveRecord::ConnectionAdapters.const_defined?(:MysqlAdapter)
+    module ActiveRecord::ConnectionAdapters
+      class MysqlAdapter
+        alias_method :execute_without_retry, :execute
+    
+        def execute(*args)
+          execute_without_retry(*args)
+        rescue ActiveRecord::StatementInvalid => e
+          if e.message =~ /server has gone away/i
+            warn "Server timed out, retrying"
+            reconnect!
+            retry
+          else
+            raise e
+          end
+        end
+      end
+    end
+  elsif ActiveRecord::ConnectionAdapters.const_defined?(:Mysql2Adapter)
+    module ActiveRecord::ConnectionAdapters
+      class Mysql2Adapter
+        alias_method :execute_without_retry, :execute
+    
+        def execute(*args)
+          execute_without_retry(*args)
+        rescue ActiveRecord::StatementInvalid => e
+          if e.message =~ /server has gone away/i
+            warn "Server timed out, retrying"
+            reconnect!
+            retry
+          else
+            raise e
+          end
+        end
       end
     end
   end
 end
+
+#Prevents Rails from loading multiple times. I could use a global variable too, but still...
+$LOADED_FEATURES << __FILE__
