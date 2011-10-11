e0ae9786016e78f68063a8b286a0e9b23ea5871d
zanst <me@zan.st>
2011-10-11 15:49:06 -0300

    added full app auth to production environment customer requested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,17 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
+  before_filter :authenticate
+
   protect_from_forgery
+
+  protected
+
+  def authenticate
+    if Rails.env == "production"
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "olook" && password == "olook123abc"
+      end
+    end
+  end
+
 end
