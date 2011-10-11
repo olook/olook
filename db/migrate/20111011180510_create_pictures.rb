a0fd77d9251e21a29460d215815f43e47d63a994
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-11 16:33:21 -0300

    Added pictures for products

---
+++ db/migrate/20111011180510_create_pictures.rb
@@ -0,0 +1,12 @@
+class CreatePictures < ActiveRecord::Migration
+  def change
+    create_table :pictures do |t|
+      t.string :image
+      t.string :display_on
+      t.belongs_to :product
+
+      t.timestamps
+    end
+    add_index :pictures, :product_id
+  end
+end
