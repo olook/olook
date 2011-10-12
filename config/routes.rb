0b67a4cc7699ee9fe404c3ffc79aaf103d33d133
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-12 12:35:19 -0300

    Added Devise to Admin model

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Olook::Application.routes.draw do
   resources :survey, :only => [:index, :create]
   match "/welcome", :to => "pages#welcome", :as => "welcome"
 
+  devise_for :admins
   devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks", :registrations => "registrations", :sessions => "sessions" } do
     get '/users/auth/:provider' => 'omniauth_callbacks#passthru'
     post "after_sign_in_path_for", :to => "sessions#after_sign_in_path_for", :as => "after_sign_in_path_for_session"
