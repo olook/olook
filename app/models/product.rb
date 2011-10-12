ee22a6f6f5123a8d071b490821fd42eab385db4d
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-12 19:12:42 -0300

    Added product CRUD, replaced Categories with enumerate_it and removed empty helpers

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,21 +1,13 @@
 # -*- encoding : utf-8 -*-
 class Product < ActiveRecord::Base
-  Categories = {shoe: 1, bag: 2, jewel: 3}
+  has_enumeration_for :category, :with => Category, :required => true
   
   has_many :pictures
   
   validates :name, :presence => true
   validates :description, :presence => true
   
-  scope :shoes , where(:category => Categories[:shoe])
-  scope :bags  , where(:category => Categories[:bag])
-  scope :jewels, where(:category => Categories[:jewel])
-  
-  def category
-    Categories.key(read_attribute(:category)) || :none
-  end
-
-  def category=(value)
-    write_attribute(:category, (Categories[value] || 0))
-  end
+  scope :shoes , where(:category => Category::SHOE)
+  scope :bags  , where(:category => Category::BAG)
+  scope :jewels, where(:category => Category::JEWEL)
 end
--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,19 +3,19 @@ FactoryGirl.define do
     factory :basic_shoe do
       name "Chanelle"
       description "Elegant black high-heeled shoe for executives"
-      category :shoe
+      category Category::SHOE
     end
 
     factory :basic_bag do
       name "Bagelle"
       description "Elegant black bag for executives"
-      category :bag
+      category Category::BAG
     end
 
     factory :basic_jewel do
       name "Jewelle"
       description "Elegant jewel for executives"
-      category :jewel
+      category Category::JEWEL
     end
   end
 end
