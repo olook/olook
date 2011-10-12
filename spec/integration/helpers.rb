e0059ccba5770a64447a332ed6665e727181404d
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-12 19:02:43 -0300

    Adding authentication in member_controller

--- spec/integration/helpers.rb
+++ spec/integration/helpers.rb
@@ -1,4 +1,11 @@
 # -*- encoding : utf-8 -*-
+def do_login!(user)
+  visit new_user_session_path
+  fill_in "user_email", :with => user.email
+  fill_in "user_password", :with => user.password
+  click_button "Sign in"
+end
+
 def answer_survey
   begin
     question = FactoryGirl.create(:question)
@@ -9,6 +16,6 @@ def answer_survey
   answer3 = FactoryGirl.create(:answer_from_casual_profile, :question => question)
   visit root_path
   click_link "take_survey"
-  choose "questions[question_#{question.id}]" 
+  choose "questions[question_#{question.id}]"
   click_button "Enviar"
 end
