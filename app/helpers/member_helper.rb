102c872c2d45564acfdc604fd488352cc6e8be8d
zanst <me@zan.st>
2011-10-13 13:51:53 -0300

    added orkut share

--- app/helpers/member_helper.rb
+++ app/helpers/member_helper.rb
@@ -1,6 +1,14 @@
 # -*- encoding : utf-8 -*-
 module MemberHelper
   def link_to_invitation(member)
-    "\"Venha ver minha vitrine na olook! Visite olook.com/invite/#{member.invite_token}\""
+    "\"#{invite_message} #{invitation_link(member)}\""
+  end
+
+  def invitation_link(member)
+    link = "http://beta.olook.com.br/invite/#{member.invite_token}"
+  end
+
+  def invite_message
+    message = "Venha ver minha vitrine na olook! Entre no link."
   end
 end
