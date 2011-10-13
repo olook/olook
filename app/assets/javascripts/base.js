b881a35d41bdc38bdc4734c001921b5e3d7d7c23
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-13 14:21:34 -0300

    Added facebook link to post in the wall

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
   $("#facebook_invite_friends").click(function() {
     sendFacebookMessage();
   });
+
+ $("#facebook_post_wall").click(function() {
+    postToFacebookFeed();
+  });
 });
 
 
