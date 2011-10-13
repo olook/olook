971f5444d4cb3be1f254c0bb43c53f06c373ca14
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-13 16:12:31 -0300

    Added Cancan gem

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,14 @@
 class Admin < ActiveRecord::Base
+  ROLES = %w[admin stylist]
+
   devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
   attr_accessible :email, :password, :password_confirmation, :remember_me
+
+  def admin?
+    self.role == "admin"
+  end
+
+  def stylist?
+    self.role == "stylist"
+  end
 end
