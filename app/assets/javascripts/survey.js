4987a32ad6bc130b75a4def040cb3663b4dc359b
Bruno Chagas <brunochaagsweb@gmail.com>
2011-10-13 18:32:07 -0300

    Add jcarousel

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,23 +1,24 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
 
-  $(document).ready(function() {
+  function popupCenter(url, width, height, name) {
+    var left = (screen.width/2)-(width/2);
+    var top = (screen.height/2)-(height/2);
 
-    function popupCenter(url, width, height, name) {
-      var left = (screen.width/2)-(width/2);
-      var top = (screen.height/2)-(height/2);
-    
-      return window.open(url, name, "menubar=no,toolbar=no,status=no,width=" + width + ",height=" + height + ",toolbar=no,left=" + left + ",top=" + top);
-  
-    }
-  
-    $("a.fbpopup").click(function(e) {
-      e.stopPropagation();
-      e.preventDefault();
-  
-      popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
-  
-      return false;
-    });
+    return window.open(url, name, "menubar=no,toolbar=no,status=no,width=" + width + ",height=" + height + ",toolbar=no,left=" + left + ",top=" + top);
 
+  }
+
+  $("a.fbpopup").click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
+
+    return false;
   });
+
+  $('.question').jcarousel({
+    scroll: 1
+  });
+
+});
