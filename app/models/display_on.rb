d60152a26e7840b8aa6198279eda35392f2af779
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-13 13:32:44 -0300

    Added reference to DisplayOn

--- app/models/display_on.rb
+++ app/models/display_on.rb
@@ -3,6 +3,7 @@ class DisplayOn < EnumerateIt::Base
   associate_values(
     :gallery        => [1, 'Gallery'],
     :member_banner  => [2, 'Member banner'],
-    :visitor_banner => [3, 'Visitor banner']
+    :visitor_banner => [3, 'Visitor banner'],
+    :reference      => [4, 'Reference']
   )
 end
