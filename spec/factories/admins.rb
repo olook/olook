e5a6af2b82170df390623dc83c2b15bd67606f61
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-13 16:46:07 -0300

    Added  role methods to admin

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,10 +1,20 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :admin do
+    role "admin"
     password "123456"
     password_confirmation "123456"
     sequence :email do |n|
       "admin#{n}@example.com"
+    end
+
+    factory :stylist do
+      role "stylist"
+      password "123456"
+      password_confirmation "123456"
+      sequence :email do |n|
+        "admin#{n}@example.com"
+      end
     end
   end
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
 describe Admin do
+  context "checking roles" do
+    it "should be a admin" do
+      admin = FactoryGirl.create(:admin)
+      admin.admin?.should eq(true)
+    end
+
+    it "should be a stylist" do
+      stylist = FactoryGirl.create(:stylist)
+      stylist.stylist?.should eq(true)
+    end
+  end
 end
