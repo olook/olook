6286724e3d3238936f2c003db4b23318ede5699c
zanst <me@zan.st>
2011-10-17 13:12:02 -0200

    configured aws s3 as the default storage for uploaded files

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,15 +7,15 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    root_dir = Rails.env.test? ? 'uploads/test/' : 'uploads/'
-    "#{root_dir}#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   root_dir = Rails.env.test? ? 'uploads/test/' : 'uploads/'
+  #   "#{root_dir}#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,9 @@
 # -*- encoding : utf-8 -*-
 CarrierWave.configure do |config|
-  config.permissions 0600
+  config.fog_credentials = {
+    :provider => 'AWS',
+    :aws_access_key_id => 'AKIAJ2WH3XLYA24UTAJQ',
+    :aws_secret_access_key => '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',
+  }
+  config.fog_directory = 'olook'
 end
