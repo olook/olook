a6ecbab585eb0f6c3475dc0678120ccda2e9fc61
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-18 17:54:28 -0200

    Fix tests. We will need to change the build_survey helper and make it more procedural

--- spec/integration/helpers.rb
+++ spec/integration/helpers.rb
@@ -10,12 +10,12 @@ def answer_survey
   build_survey
   visit root_path
   click_link "take_survey"
-  choose "questions[question_#{@question.id}]"
+  choose "questions[question_#{Question.first.id}]"
   click_button "Enviar"
 end
 
 def build_survey
-  2.times do
+  3.times do
     @question = FactoryGirl.create(:question)
     @answer = FactoryGirl.create(:answer_from_casual_profile, :question => @question)
   end
