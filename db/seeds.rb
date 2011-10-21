8c86e8bf721c8444b6709c715601254073b85f47
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-21 18:32:47 -0200

    Revised seed data, one word is missing

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ survey_data[1] = {
 }
 
 survey_data[2] = {
-  :question_title => "Qual desses icones de estilo voce gostaria de  herdar o guarda-roupa?",
+  :question_title => "Qual desses ícones de estilo você gostaria de herdar o guarda-roupa?",
   :answers => ["Sarah Jessica Parker", "Carla Bruni", "Kate Moss"],
   :weights => [{"Sarah Jessica Parker" => {"5" => female, "3" => contemporary, "10" => trendy}},
                {"Carla Bruni" => {"5" => traditional, "10" => elegant, "3" => female}},
@@ -45,15 +45,15 @@ survey_data[3] = {
 }
 
 survey_data[4] = {
-  :question_title => "Quais desses estilos de cabelo mais te agradam?",
-  :answers => ["Chanel", "Curtissimo", "Longo tipo Gisele"],
-  :weights => [{"Chanel" => {"5" => traditional, "10" => elegant, "3" => female}},
-               {"Curtíssimo" => {"10" => contemporary, "3" => trendy, "5" => casual}},
-               {"Longo tipo Gisele" => {"5" => female, "10" => sexy, "3" => trendy}}]
+  :question_title => "Qual destes estilos de cabelo mais te agradam?",
+  :answers => ["Chanel Básico", "Curtíssimo repicado", "Longo esvoaçante"],
+  :weights => [{"Chanel Básico" => {"5" => traditional, "10" => elegant, "3" => female}},
+               {"Curtíssimo repicado" => {"10" => contemporary, "3" => trendy, "5" => casual}},
+               {"Longo esvoaçante" => {"5" => female, "10" => sexy, "3" => trendy}}]
 }
 
 survey_data[5] = {
-  :question_title => "Se pudesse voltar no tempo que epoca da moda voce escolheria?",
+  :question_title => "Se pudesse voltar no tempo que época da moda você escolheria?",
   :answers => ["Anos 1950", "Anos 1960", "Anos 1970"],
   :weights => [{"Anos 1950" => {"3" => traditional, "10" => elegant, "5" => female}},
                {"Anos 1960" => {"10" => contemporary, "3" => trendy, "5" => casual}},
@@ -62,22 +62,22 @@ survey_data[5] = {
 
 survey_data[6] = {
   :question_title => "Qual desses roteiros de viagem mais te agradam?",
-  :answers => ["Europa Cultutal", "China e Japão", "Ilhas Gregas"],
-  :weights => [{"Europa Cultutal" => {"10" => traditional, "5" => elegant, "3" => female}},
+  :answers => ["Europa Cultural", "China e Japão", "Ilhas Gregas"],
+  :weights => [{"Europa Cultural" => {"10" => traditional, "5" => elegant, "3" => female}},
                {"China e Japão" => {"5" => casual, "10" => contemporary, "3" => trendy}},
                {"Ilhas Gregas" => {"3" => casual, "10" => sexy, "10" => trendy}}]
 }
 
 survey_data[7] = {
-  :question_title => "Se voce fosse escolher um estilo de dança para aprender, qual seria?",
-  :answers => ["Dança de salão", "Tango", "Forro" ],
+  :question_title => "Se você fosse escolher um estilo de dança para aprender, qual seria?",
+  :answers => ["Dança de salão", "Tango", "Samba" ],
   :weights => [{"Dança de salão" => {"10" => traditional, "3" => elegant, "5" => female}},
                {"Tango" => {"10" => sexy, "5" => contemporary, "3" => trendy}},
-               {"Forro" => {"10" => casual, "3" => female, "5" => sexy}}]
+               {"Samba" => {"10" => casual, "3" => female, "5" => sexy}}]
 }
 
 survey_data[8] = {
-  :question_title => "Qual dessas revistas femininas voce tem o habito de ler?",
+  :question_title => "Qual destas revistas femininas você tem o hábito de ler?",
   :answers => ["Elle", "Marie Claire", "Claudia" ],
   :weights => [{"Elle" => {"5" => female, "3" => sexy, "10" => trendy}},
                {"Marie Claire" => {"3" => traditional, "5" => elegant, "10" => contemporary}},
@@ -85,103 +85,103 @@ survey_data[8] = {
 }
 
 survey_data[9] = {
-  :question_title => "Com qual desses trajes voce contuma dormir?",
-  :answers => ["Camisola de Seda Longa", "Pijama de malha", "Baby Doll Rendinha" ],
-  :weights => [{"Camisola de Seda Longa" => {"3" => traditional, "5" => elegant, "10" => sexy}},
-               {"Pijama de malha" => {"10" => casual, "3" => traditional, "5" => contemporary}},
-               {"Baby Doll Rendinha" => {"5" => casual, "10" => female, "3" => sexy}}]
+  :question_title => "Com qual destes trajes você costuma dormir?",
+  :answers => ["Camisola de Seda", "Pijama de algodão", "Camisola de rendinha" ],
+  :weights => [{"Camisola de Seda" => {"3" => traditional, "5" => elegant, "10" => sexy}},
+               {"Pijama de algodão" => {"10" => casual, "3" => traditional, "5" => contemporary}},
+               {"Camisola de rendinha" => {"5" => casual, "10" => female, "3" => sexy}}]
 }
 survey_data[10] = {
-  :question_title => "Qual desses modelos de oculos escuros mais combinam com voce?",
-  :answers => ["quadrado de acetato", "ray ban aviador", "redondo com armacao clara" ],
-  :weights => [{"quadrado de acetato" => {"10" => traditional, "5" => elegant, "3" => contemporary}},
-               {"ray ban aviador" => {"10" => casual, "5" => sexy, "3" => trendy}},
-               {"redondo com armacao clara" => {"3" => casual, "10" => contemporary, "5" => trendy}}]
+  :question_title => "Qual destes modelos de óculos escuros mais combina com você?",
+  :answers => ["Clássicos em acetato", "O bom e velho modelo aviador", "Tipo gatinho" ],
+  :weights => [{"Clássicos em acetato" => {"10" => traditional, "5" => elegant, "3" => contemporary}},
+               {"O bom e velho modelo aviador" => {"10" => casual, "5" => sexy, "3" => trendy}},
+               {"Tipo gatinho" => {"3" => casual, "10" => contemporary, "5" => trendy}}]
 }
 
 survey_data[11] = {
   :question_title => "Escolha o look que mais te agrada para o dia a dia.",
-  :answers => ["jeans boyfriend e mocassim", "calca alfaiataria e scarpin", "vestido longo malha e rasteira" ],
-  :weights => [{"jeans boyfriend e mocassim" => {"5" => casual, "10" => contemporary, "3" => trendy}},
-               {"calca alfaiataria e scarpin" => {"10" => traditional, "5" => elegant, "3" => contemporary}},
-               {"vestido longo malha e rasteira" => {"10" => casual, "3" => sexy, "5" => contemporary}}]
+  :answers => ["Tudo com jeans", "Tudo com peças de alfaiataria", "Um vestidão de malha resolve tudo" ],
+  :weights => [{"Tudo com jeans" => {"5" => casual, "10" => contemporary, "3" => trendy}},
+               {"Tudo com peças de alfaiataria" => {"10" => traditional, "5" => elegant, "3" => contemporary}},
+               {"Um vestidão de malha resolve tudo" => {"10" => casual, "3" => sexy, "5" => contemporary}}]
 }
 
 survey_data[12] = {
-  :question_title => "Escolha o look que mais te agrada para noite",
-  :answers => ["pretinho basico e sandalia salto fino", "jeans skinny e ankle", "vestido tec + gladiadora" ],
-  :weights => [{"pretinho basico e sandalia salto fino" => {"10" => traditional, "5" => elegant, "3" => sexy}},
-               {"jeans skinny e ankle" => {"10" => casual, "5" => sexy, "3" => trendy}},
-               {"vestido tec + gladiadora" => {"10" => casual, "5" => contemporary, "3" => trendy}}]
+  :question_title => "Escolha o look que mais te agrada para noite.",
+  :answers => ["Pretinho basico + Sandália", "Jeans skinny + Saltão", "Vestido longo + Sandália rasteira" ],
+  :weights => [{"Pretinho basico + Sandália" => {"10" => traditional, "5" => elegant, "3" => sexy}},
+               {"Jeans skinny + Saltão" => {"10" => casual, "5" => sexy, "3" => trendy}},
+               {"Vestido longo + Sandália rasteira" => {"10" => casual, "5" => contemporary, "3" => trendy}}]
 }
 
 survey_data[13] = {
-  :question_title => "Qual desses modelos  você usaria para trabalhar?",
-  :answers => ["sapatilha + bolsa alca longa", "peep toe + bolsa reta", "scarpin salto quadrado + bolsa hit da estação" ],
-  :weights => [{"sapatilha + bolsa alca longa" => {"10" => casual, "3" => elegant, "5" => female}},
-               {"peep toe + bolsa reta" => {"10" => traditional, "5" => elegant, "3" => female}},
-               {"scarpin salto quadrado + bolsa hit da estação" => {"3" => casual, "10" => contemporary, "5" => trendy}}]
+  :question_title => "Quais acessórios você usaria para trabalhar?",
+  :answers => ["Sapatilha + Bolsa prática", "Tipo chanel + Bolsa clássica", "Scarpin salto quadrado + Bolsa hit da estação" ],
+  :weights => [{"Sapatilha + Bolsa prática" => {"10" => casual, "3" => elegant, "5" => female}},
+               {"Tipo chanel + Bolsa clássica" => {"10" => traditional, "5" => elegant, "3" => female}},
+               {"Scarpin salto quadrado + Bolsa hit da estação" => {"3" => casual, "10" => contemporary, "5" => trendy}}]
 }
 
 survey_data[14] = {
-  :question_title => "Qual desses modelos você usaria para um domigo a tarde?",
-  :answers => ["gladiadora + bolsa molhinha", "sapatilha + maxibolsa", "sandalia anabela + bolsa box alça longa" ],
-  :weights => [{"gladiadora + bolsa molhinha" => {"3" => casual, "5" => contemporary, "10" => trendy}},
-               {"sapatilha + maxibolsa" => {"3" => traditional, "10" => elegant, "5" => female}},
-               {"sandalia anabela + bolsa box alça longa" => {"3" => elegant, "10" => female, "5" => sexy}}]
+  :question_title => "Quais acessórios você usaria para um domigo a tarde?",
+  :answers => ["Gladiadora + Bolsa molhinha", "Sapatilha + Maxibolsa", "Sandália anabela + Bolsa box alça longa" ],
+  :weights => [{"Gladiadora + Bolsa molhinha" => {"3" => casual, "5" => contemporary, "10" => trendy}},
+               {"Sapatilha + Maxibolsa" => {"3" => traditional, "10" => elegant, "5" => female}},
+               {"Sandália anabela + Bolsa box alça longa" => {"3" => elegant, "10" => female, "5" => sexy}}]
 }
 
 survey_data[15] = {
-  :question_title => "Qual desses modelos você usaria em uma festa?",
-  :answers => ["sandalia salto fino + clutch oncinha", "ankle boot salto + clutch pedrarias", "sandalia salto grosso+ bolsa lateral verniz" ],
-  :weights => [{"sandalia salto fino + clutch oncinha" => {"3" => elegant, "5" => female, "10" => sexy}},
-               {"ankle boot salto + clutch pedrarias" => {"3" => sexy, "5" => contemporary, "10" => trendy}},
-               {"sandalia salto grosso+ bolsa lateral verniz" => {"5" => casual, "10" => traditional, "3" => contemporary}}]
+  :question_title => "Quais acessórios você usaria em uma festa?",
+  :answers => ["Sandália salto fino + Clutch oncinha", "Ankle boot salto + Clutch pedrarias", "Scarpin verniz + Clutch neutra" ],
+  :weights => [{"Sandália salto fino + Clutch oncinha" => {"3" => elegant, "5" => female, "10" => sexy}},
+               {"Ankle boot salto + Clutch pedrarias" => {"3" => sexy, "5" => contemporary, "10" => trendy}},
+               {"Scarpin verniz + Clutch neutra" => {"5" => casual, "10" => traditional, "3" => contemporary}}]
 }
 
 survey_data[16] = {
-  :question_title => "Quais desses acessórios você escolheria para  este pretinho basico?",
-  :answers => ["sapatilha/bolsa carteiro/ argola", "scarpin/bolsa dura/colares dourados", "ankle/cluch/ brinco de franja" ],
-  :weights => [{"sapatilha/bolsa carteiro/ argola" => {"10" => casual, "5" => traditional, "3" => female}},
-               {"scarpin/bolsa dura/colares dourados" => {"3" => traditional, "10" => elegant, "5" => female}},
-               {"ankle/cluch/ brinco de franja" => {"5" => sexy, "5" => contemporary, "10" => trendy}}]
+  :question_title => "Quais acessórios você escolheria para um pretinho básico?",
+  :answers => ["Sapatilha + Bolsa alçinha + Brinco básico", "Scarpin + Bolsa estruturada + Colares importantes", "Ankle boot + Clutch + Brincão" ],
+  :weights => [{"Sapatilha + Bolsa alçinha + Brinco básico" => {"10" => casual, "5" => traditional, "3" => female}},
+               {"Scarpin + Bolsa estruturada + Colares importantes" => {"3" => traditional, "10" => elegant, "5" => female}},
+               {"Ankle boot + Clutch + Brincão" => {"5" => sexy, "5" => contemporary, "10" => trendy}}]
 }
 
 survey_data[17] = {
   :question_title => "Maquiagem para você é...",
-  :answers => ["Indispensavel. Uso tudo todos os dias", "Importante. Rimel e Batom resolvem a questão", "Eventual. Só uso em ocasiões especiais" ],
-  :weights => [{"Indispensavel. Uso tudo todos os dias" => {"5" => elegant, "10" => female, "3" => trendy}},
+  :answers => ["Indispensável. Uso tudo todos os dias", "Importante. Rimel e Batom resolvem a questão", "Eventual. Só uso em ocasiões especiais" ],
+  :weights => [{"Indispensável. Uso tudo todos os dias" => {"5" => elegant, "10" => female, "3" => trendy}},
                {"Importante. Rimel e Batom resolvem a questão" => {"3" => traditional, "5" => female, "10" => contemporary}},
                {"Eventual. Só uso em ocasiões especiais" => {"10" => casual, "5" => traditional, "3" => female}}]
 }
 
 survey_data[18] = {
-  :question_title => "Como voce costuma combinar as cores?",
-  :answers => ["neutros e mais neutros", "neutros com toque vibrante", "tudo pode combinar com tudo" ],
-  :weights => [{"neutros e mais neutros" => {"5" => casual, "10" => traditional, "3" => contemporary}},
-               {"neutros com toque vibrante" => {"5" => elegant, "10" => female, "3" => sexy}},
-               {"tudo pode combinar com tudo" => {"5" => female, "3" => sexy, "10" => trendy}}]
+  :question_title => "Como você costuma combinar as cores?",
+  :answers => ["Neutros e mais neutros", "Neutros com toque vibrante", "Tudo pode combinar com tudo" ],
+  :weights => [{"Neutros e mais neutros" => {"5" => casual, "10" => traditional, "3" => contemporary}},
+               {"Neutros com toque vibrante" => {"5" => elegant, "10" => female, "3" => sexy}},
+               {"Tudo pode combinar com tudo" => {"5" => female, "3" => sexy, "10" => trendy}}]
 }
 
 survey_data[19] = {
   :question_title => "Como você gosta do caimento das suas roupas?",
-  :answers => ["Caimento certo, linhas retas", "Justas, valorizando minha silhueta", "Estruturadas, sem revelar muito as formas" ],
-  :weights => [{"Caimento certo, linhas retas" => {"10" => traditional, "5" => elegant, "3" => trendy}},
-               {"Justas, valorizando minha silhueta" => {"5" => female, "10" => sexy, "3" => trendy}},
+  :answers => ["Justas, valorizando minha silhueta", "Caimento certo, linhas retas", "Estruturadas, sem revelar muito as formas" ],
+  :weights => [{"Justas, valorizando minha silhueta" => {"5" => female, "10" => sexy, "3" => trendy}},
+               {"Caimento certo, linhas retas" => {"10" => traditional, "5" => elegant, "3" => trendy}},
                {"Estruturadas, sem revelar muito as formas" => {"3" => casual, "5" => traditional, "10" => contemporary}}]
 }
 
 survey_data[20] = {
   :question_title => "Que tipo de acessório mais te agrada?",
-  :answers => ["Clássicas e atemporais", "Peças diferenciadas toque vintage ou etnico", "Peças grandes e com brilho" ],
-  :weights => [{"Clássicas e atemporais" => {"5" => traditional, "10" => elegant, "3" => contemporary}},
-               {"Peças diferenciadas toque vintage ou etnico" => {"5" => casual, "10" => trendy}},
+  :answers => ["Clássicos e atemporais", "Peças diferenciadas com toque vintage ou étnico", "Peças grandes e com brilho" ],
+  :weights => [{"Clássicos e atemporais" => {"5" => traditional, "10" => elegant, "3" => contemporary}},
+               {"Peças diferenciadas com toque vintage ou étnico" => {"5" => casual, "10" => trendy}},
                {"Peças grandes e com brilho" => {"3" => elegant, "5" => female, "10" => sexy}}]
 }
 
 survey_data[21] = {
   :question_title => "Quais palavras você mais gosta? (marque 3 opções)",
-  :answers => ["Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra","Palavra"],
+  :answers => ["Esportiva", "Frágil", "Atraente", "Romântica", "Prática", "Chic", "Criativa", "Sofisticada", "Glamourosa", "Moderna", "Conservadora"],
   :weights => []
 }
 
@@ -192,13 +192,13 @@ survey_data[22] = {
 }
 
 survey_data[23] = {
-  :question_title => "Qual o tamanho do seu sapatos?",
+  :question_title => "Qual o tamanho do seu sapato?",
   :answers => (33..41).to_a,
   :weights => []
 }
 
 survey_data[24] = {
-  :question_title => "Qual tamanho de vestido que você veste?",
+  :question_title => "Qual tamanho de vestido você usa?",
   :answers => %w(PP, P, M, G, GG),
   :weights => []
 }
