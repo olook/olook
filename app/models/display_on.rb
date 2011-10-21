987cb66331b5e0671d1613ce02ce68772b98b318
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-21 12:16:48 -0200

    Renamed DisplayOn to DisplayPictureOn

--- app/models/display_on.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-class DisplayOn < EnumerateIt::Base
-  associate_values(
-    :gallery        => [1, 'Gallery'],
-    :member_banner  => [2, 'Member banner'],
-    :visitor_banner => [3, 'Visitor banner'],
-    :reference      => [4, 'Reference']
-  )
-end
--- spec/factories/picture.rb
+++ spec/factories/picture.rb
@@ -3,7 +3,7 @@ FactoryGirl.define do
   factory :picture do
     factory :gallery_picture do
       image "aa"
-      display_on DisplayOn::GALLERY
+      display_on DisplayPictureOn::GALLERY_1
       association :product, :factory => :basic_shoe
     end
   end
