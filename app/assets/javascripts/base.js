15137f01836a5bbeb0c1ead3970e52a9107c039b
Bruno Chagas <brunochaagsweb@gmail.com>
2011-10-26 15:36:47 -0200

    add style select

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
  $("#facebook_post_wall").click(function() {
     postToFacebookFeed();
   });
+
+  var options = {
+    styleClass: 'olookSelect',
+    jScrollPane: 1
+  }
+
+  $('select').styleSelect(options);
+
 });
-
-
