f1fcfd86f1531de5980665897d978053500054ae
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-26 23:43:58 -0200

    Added presenter pattern in SurveyController

---
+++ app/presenters/survey_questions.rb
@@ -0,0 +1,31 @@
+class SurveyQuestions
+  attr_reader :questions
+
+  def initialize(questions)
+    @questions = questions
+  end
+
+  def common_questions
+    questions[0..questions.size - 6]
+  end
+
+  def shoe_question
+    questions[questions.size - 5]
+  end
+
+  def word_question
+    questions[questions.size - 4]
+  end
+
+  def shoe_size_question
+    questions[questions.size - 2]
+  end
+
+  def dress_size_question
+    questions.last
+  end
+
+  def id_first_question
+    questions.first.id if questions.size > 0
+  end
+end
