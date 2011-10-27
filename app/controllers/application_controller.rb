75e94ce07838b9481b1c9b23f984b2335af7ec19
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-27 14:06:20 -0200

    Handling exceptions for contacts importation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
   before_filter :authenticate
-
   protect_from_forgery
+
+  rescue_from Contacts::AuthenticationError, :with => :contact_authentication_failed
+  rescue_from GData::Client::CaptchaError, :with => :contact_authentication_failed
 
   protected
 
@@ -14,4 +16,8 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def contact_authentication_failed
+    flash[:notice] = "Falha de autenticação na importação de contatos"
+    redirect_to :back
+  end
 end
