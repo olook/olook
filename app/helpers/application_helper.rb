0283330c67c53ec8b37f314bb4e682825a332f54
Bruno Chagas <brunochaagsweb@gmail.com>
2011-10-28 19:27:26 -0200

    add is_current? helper for add select class on menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def stylesheet_application
     stylesheet_link_tag  "application"
   end
+  
+  def is_current?(controller_name, action_name='index')
+    'selected' if params[:controller] == controller_name and params[:action] == action_name
+  end
 end
