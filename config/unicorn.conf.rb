da1641ee2f4437b78fd1bd14413ab11c3d4b357d
zanst <me@zan.st>
2011-10-28 21:06:40 -0200

    pointing unicorn port to the right one

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Unicorn configuration file
 
-listen 5080
+listen 80
 worker_processes 2
 pid "/var/run/olook-unicorn.pid"
 stderr_path "/var/log/olook-unicorn-error.log"
