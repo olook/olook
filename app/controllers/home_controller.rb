8975a56c6b3d00f1b3762edb4395acce841c3a8b
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-10-28 23:00:12 -0200

    Minor fixes on control flow

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
+  before_filter :redirect_logged_user, :only => :index
+
   def index
   end
 
+private
+  def redirect_logged_user
+    redirect_to member_invite_path if user_signed_in?
+  end
 end
