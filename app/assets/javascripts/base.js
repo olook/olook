2cffd91699c249eb5657ede0b85930a8d2035140
Bruno Chagas <brunochaagsweb@gmail.com>
2011-10-28 22:19:12 -0200

    add modal js functions

--- app/assets/javascripts/base.js
+++ app/assets/javascripts/base.js
@@ -3,14 +3,34 @@ $(document).ready(function() {
     sendFacebookMessage();
   });
 
- $("#facebook_post_wall").click(function() {
+  $("#facebook_post_wall").click(function() {
     postToFacebookFeed();
   });
 
- $(document).bind('keydown', 'esc',function (evt) {
+  $(document).bind('keydown', 'esc',function (evt) {
     $('#sign-in-dropdown').hide();
     $('body').removeClass('dialog-opened');
     return false; 
   });
+
+
+  if($('.dialog').length == 1) {
+    width = $(document).width();
+    height = $(document).width();
+    viewWidth = $(window).width();
+    viewHeight = $(window).height();
+    imageW = $('.dialog img').width();
+    imageH = $('.dialog img').height();
+
+    $('body').prepend("<div class='overlay'></div>");
+    $(".overlay").width(width).height(height);
+    
+    $('body .dialog').css("left", (viewWidth - imageW) / 2);
+    $('body .dialog').css("top", (viewHeight - imageH) / 2);
+
+    $('.dialog img, .overlay').click(function(){
+      $('.dialog, .overlay').remove();
+    });
+  }
 
 });
