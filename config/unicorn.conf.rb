14608936c07854e83cd01e6b6d0050ac3bc379e1
zanst <me@zan.st>
2011-10-31 20:11:34 -0200

    increasing the number of unicorn workers

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -2,7 +2,7 @@
 # Unicorn configuration file
 
 listen "/tmp/sockets/unicorn.sock", :backlog => 64
-worker_processes 2
+worker_processes 4
 pid "/var/run/olook-unicorn.pid"
 stderr_path "/var/log/olook-unicorn-error.log"
 stdout_path "/var/log/olook-unicorn.log"
