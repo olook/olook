18a9169eabffc9eb1d502b6d5a081dcc7e1572fe
Bruno Chagas <brunochaagsweb@gmail.com>
2011-10-31 20:02:02 -0200

    Update import js functions

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-  $("#import-contacts form").hide();
+  $('.import-dropdown').hide();
   $("#import-contacts .gmail, #import-contacts .yahoo").click(function(event){
     event.preventDefault();
-    $("#import-contacts form").show();
+    $(".import-dropdown > span").removeClass();
+    $("#import-contacts .import-dropdown").show();
     var email_provider = $(this).attr('class');
     $("#email_provider").val(email_provider);
+    $(".import-dropdown > span").addClass(email_provider);
+  });
+
+  $('.import-dropdown a').live('click', function(event){
+    event.preventDefault();
+    $(this).parent().hide();
+  });
+
+  $(document).bind('keydown', 'esc',function () {
+    $('.import-dropdown').hide();
+    return false; 
   });
 });
 
