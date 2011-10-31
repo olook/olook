38b780ae32379b51843444873f77271ee868c773
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-10-31 14:28:26 -0200

    Fix presenters helpers

--- spec/presenters/survey_questions_spec.rb
+++ spec/presenters/survey_questions_spec.rb
@@ -3,31 +3,32 @@ require "spec_helper"
 
 describe SurveyQuestions do
   let(:question) { double }
-  let(:fake_questions) { [question, 2, 3, 4, 5, 6, 7] }
-  subject { SurveyQuestions.new(fake_questions) }
+  let(:questions) { (1..29).to_a }
+  subject { SurveyQuestions.new(questions) }
 
-  xit "should return common questions" do
-    subject.common_questions.should == [question, 2]
+  it "should return common questions" do
+    subject.common_questions.should == questions[0..20]
   end
 
-  xit "should return shoe question" do
-    subject.word_question.should == 4
+  it "should return word question" do
+    subject.word_question.should == questions[22]
   end
 
-  xit "should return word question" do
-    subject.shoe_question.should == 3
+  it "should return show question" do
+    subject.shoe_question.should == questions[21]
   end
 
-  xit "should return shoe size question" do
-    subject.shoe_size_question.should == 6
+  it "should return shoe size question" do
+    subject.shoe_size_question.should == questions[27]
   end
 
-  xit "should return dress size question" do
-    subject.dress_size_question.should == 7
+  it "should return dress size question" do
+    subject.dress_size_question.should == questions[28]
   end
 
-  xit "should return id of the first question" do
+  it "should return id of the first question" do
     question.stub(:id).and_return(1)
+    questions[0] = question
     subject.id_first_question.should == 1
   end
 end
