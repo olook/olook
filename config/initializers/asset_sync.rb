1ceb7d2bcc5394083238dd93bc1cf572da52c546
zanst <me@zan.st>
2011-10-31 16:45:04 -0200

    configured assets to be imported and sync to amazon s3.

---
+++ config/initializers/asset_sync.rb
@@ -0,0 +1,7 @@
+AssetSync.configure do |config|
+  config.aws_access_key = "AKIAJ2WH3XLYA24UTAJQ"
+  config.aws_access_secret = "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"
+  config.aws_bucket = "cdn-app.olook.com.br"
+  # config.aws_region = "eu-west-1"
+  config.existing_remote_files = "delete"
+end
