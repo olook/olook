f4f4035ccb2c6d2e3f43e82ffaa4e07691b09c99
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-02 22:26:47 -0200

    Added 3 status for invites.

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,11 +1,12 @@
 # -*- encoding : utf-8 -*-
 class Invite < ActiveRecord::Base
+  STATUS = {:yes => "Sim", :no => "Não", :accepted => "Sim, mas por outro convite! ):"}
   belongs_to :user
   belongs_to :invited_member, :class_name => "User"
-  
+
   validates_presence_of :user, :email
   validates_format_of :email, :with => User::EmailFormat
-  
+
   scope :unsent, where('sent_at IS NULL')
   scope :sent, where('sent_at IS NOT NULL')
   scope :accepted, where('invited_member_id IS NOT NULL')
@@ -18,6 +19,18 @@ class Invite < ActiveRecord::Base
       invite.invited_member = accepting_member
       invite.accepted_at = Time.now
       invite.save
+    end
+  end
+
+  def self.status_for_user_invite(user_id, invite)
+    t = Invite.arel_table
+    invites = Invite.where(t[:email].eq(invite.email).and(t[:user_id].not_eq(user_id)).and(t[:accepted_at].not_eq(nil)))
+    if invites.size > 0
+      STATUS[:accepted]
+    elsif invite.invited_member.nil?
+      STATUS[:no]
+    else
+      STATUS[:yes]
     end
   end
 end
