356c80b7f3beb455e88eda2a134b84dad1435e4b
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-02 22:26:47 -0200

    Added 3 status for invites.

--- spec/models/invite_spec.rb
+++ spec/models/invite_spec.rb
@@ -28,7 +28,7 @@ describe Invite do
       Invite.sent.should == [subject]
     end
   end
-  
+
   describe "getting invinting member data" do
     it "should be able to return the member name" do
       subject.member_name.should == subject.user.name
@@ -37,8 +37,35 @@ describe Invite do
       subject.member_invite_token.should == subject.user.invite_token
     end
   end
-  
+
   let(:accepting_member) { FactoryGirl.create(:member, :first_name => 'Accepting') }
+
+  describe "invite status" do
+    context "when the user accept the invite" do
+      it "should return yes" do
+        user = subject.user
+        subject.accept_invitation(accepting_member)
+        Invite.status_for_user_invite(user.id, subject).should eq(Invite::STATUS[:yes])
+      end
+    end
+
+    context "when the user didnt accept the invite" do
+      it "should return no" do
+        user = subject.user
+        Invite.status_for_user_invite(user.id, subject).should eq(Invite::STATUS[:no])
+      end
+    end
+
+    context "when the user already accepted the invite from another user" do
+      it "should return accepted" do
+        invite = FactoryGirl.create(:invite, :email => subject.email)
+        accepting_user = FactoryGirl.create(:user)
+        invite.accept_invitation(accepting_user)
+        subject.accept_invitation(accepting_member)
+        Invite.status_for_user_invite(subject.user.id, subject).should eq(Invite::STATUS[:accepted])
+      end
+    end
+  end
 
   describe "#accept_invitation" do
     let(:accepting_datetime)   { Time.parse('2011-10-14 18:30') }
@@ -52,7 +79,7 @@ describe Invite do
       subject.accepted_at.should == accepting_datetime
     end
   end
-  
+
   describe "accepted scope" do
     it "should not contain unaccepted invites" do
       Invite.accepted.should be_empty
