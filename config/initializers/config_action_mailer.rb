e5656be5b51f8fda1e43a49775b4939994b92ef9
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-05 00:36:55 -0200

    Added devise forgot password

--- config/initializers/config_action_mailer.rb
+++ config/initializers/config_action_mailer.rb
@@ -1,27 +1,8 @@
-# -*- encoding : utf-8 -*-
-config_file = File.join(Rails.root, 'config', 'email.yml')
-
-if File.exist?(config_file)
-  Rails.logger.info "Loading e-mail configurations from #{config_file}"
-
-  configurations = YAML::load(File.open(config_file))[Rails.env] || {}
-  configurations.symbolize_keys!
-
-  if configurations.empty?
-    Rails.logger.info "There are no e-mail configurations for #{Rails.env} environment"
-  else
-    Rails.logger.info "Loading e-mail configurations for #{Rails.env} environment"
-
-    Olook::Application.configure do
-      config.action_mailer.delivery_method = :smtp
-      config.action_mailer.smtp_settings = configurations.clone
-      config.action_mailer.raise_delivery_errors = true
-      config.action_mailer.perform_deliveries = true
-    end
-
-    configurations[:password] = "FILTERED"
-    Rails.logger.info configurations
-  end
-else
-  Rails.logger.info "No configuration file for e-mail"
-end
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'convite@olook.com.br',
+  :password             => 'olook123abc',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true
+}
