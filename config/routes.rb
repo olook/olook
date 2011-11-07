fa2a894a59613ac196ca1caa9b2c013308513b55
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-07 16:24:41 -0200

    How to page

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Olook::Application.routes.draw do
 
   post "membro/convidar_contatos" => "members#invite_imported_contacts", :as => 'member_invite_imported_contacts'
   get "membro/convidadas" => "members#invite_list", :as => 'member_invite_list'
+  get "membro/como-funciona", :to => "members#how_to", :as => "member_how_to"
 
   namespace :admin do
     match "/", :to => "index#dashboard"
