e971c5869111fa54dffb8639a89af00969ed4dfd
zanst <me@zan.st>
2011-11-10 19:42:35 -0200

    contacts adapter spec FTW

--- spec/adapters/contacts_adapter_spec.rb
+++ spec/adapters/contacts_adapter_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 
 describe ContactsAdapter do
   let(:credentials) { {login: 'foo', password: 'bar'} }
-  let(:adapter) { ContactsAdapter.new(credentials[:login], credentials[:password]) }
+  let(:oauth) { {token: 'lasjhfl', secret: 'lasjhfl', verifier: 'lasjhfl'} }
+  let(:adapter) { ContactsAdapter.new(credentials[:login], credentials[:password], oauth[:token], oauth[:secret], oauth[:verifier]) }
 
   it "should return contacts from Yahoo" do
     Contacts::Gmail.should_receive(:new).with(credentials[:login], credentials[:password]).and_return(gmail_contacts = mock)
@@ -12,8 +13,8 @@ describe ContactsAdapter do
   end
 
   it "should return contacts from Yahoo" do
-    Contacts::Yahoo.should_receive(:new).with(credentials[:login], credentials[:password]).and_return(yahoo_contacts = mock)
-    yahoo_contacts.should_receive(:contacts)
+    OauthImport::Yahoo.stub(:new).and_return(oauth_import_yahoo = mock)
+    oauth_import_yahoo.should_receive(:contacts).with(oauth[:token], oauth[:secret], oauth[:verifier])
     adapter.contacts(ContactsAdapter::TYPE[:yahoo])
   end
 end
