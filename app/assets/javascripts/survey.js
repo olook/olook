93111f55cb8229cd6c2d99fde916ea98e592c25d
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-10 18:44:14 -0200

    Custom select on birthday

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -60,6 +60,16 @@ $(document).ready(function() {
     }
   });
 
+  if (!$.browser.opera) {
+    $('select.custom_select').each(function(){
+      var title = $(this).attr('title');
+      if( $('option:selected', this).val() != ''  ) title = $('option:selected',this).text();
+      $(this).css({'z-index':10,'opacity':0,'-khtml-appearance':'none'}).after('<span class="select">' + title + '</span>').change(function(){
+        val = $('option:selected',this).text();
+        $(this).next().text(val);
+      })
+    });
+  };
 });
 
 init = {
