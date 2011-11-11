8fe0cfe3f740eb897d6d96a321ae0436b1f5f8b3
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-11 17:43:09 -0200

    Finished shipping company CRUD, including CSV import for freight prices

--- spec/models/shipping_company_spec.rb
+++ spec/models/shipping_company_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 
 describe ShippingCompany do
   describe 'validations' do
+    it { should have_many(:freight_prices) }
+
     it { should validate_presence_of(:name) }
   end
 end
