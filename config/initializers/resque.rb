3a32a08df64f348a4644429975dfc973b7ea5997
zanst <me@zan.st>
2011-11-14 16:46:31 -0200

    added failure notification capabilities to resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,52 @@
-# -*- encoding : utf-8 -*-
-rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
-rails_env = ENV['RAILS_ENV'] || 'development'
+require "resque"
+require "resque/failure/multiple"
+require "resque/failure/redis"
 
-resque_config = YAML.load_file(rails_root + '/config/resque.yml')
-Resque.redis = resque_config[rails_env]
+Resque.redis = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env]
+
+module Resque
+  module Failure
+    # Logs failure messages.
+    class Logger < Base
+      def save
+        Rails.logger.error detailed
+      end
+
+      def detailed
+        <<-EOF
+#{worker} failed processing #{queue}:
+Payload:
+#{payload.inspect.split("\n").map { |l| "  " + l }.join("\n")}
+Exception:
+  #{exception}
+#{exception.backtrace.map { |l| "  " + l }.join("\n")}
+        EOF
+      end
+    end
+
+    # Emails failure messages.
+    # Note: uses Mail (default in Rails 3.0) not TMail (Rails 2.x).
+    class Notifier < Logger
+      def save
+        text, subject = detailed, "[Error] #{queue}: #{exception}"
+        Mail.deliver do
+          from "no-reply@olook.com.br"
+          to "stephano.zanzin@codeminer42.com", "rinaldi.fonseca@codeminer42.com", "rafaelrosafu@gmail.com"
+          subject subject
+          text_part do
+            body text
+          end
+        end
+      rescue
+        puts $!
+      end
+    end
+  end
+end
+
+Resque::Failure::Multiple.configure do |multi|
+  # Always stores failure in Redis and writes to log
+  multi.classes = Resque::Failure::Redis, Resque::Failure::Logger
+  # Production/staging only: also email us a notification
+  multi.classes << Resque::Failure::Notifier if Rails.env.production? || Rails.env.staging?
+end
