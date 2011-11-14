63d47a6db0c7977abff6044a554d6b71a05823b7
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-14 14:19:18 -0200

    user has_many address

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   has_one :survey_answer, :dependent => :destroy
   has_many :invites, :dependent => :destroy
   has_many :events, :dependent => :destroy
+  has_many :address
 
   before_create :generate_invite_token
 
