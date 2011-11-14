3c876fe30d7e1a8304ad38b92e653c28348bd7e9
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-14 10:24:32 -0200

    added migration: crate_orders

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111109184732) do
+ActiveRecord::Schema.define(:version => 20111114121640) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                              :default => "", :null => false
