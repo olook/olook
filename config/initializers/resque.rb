09aa07c5dbe903276c06741219be50b714b27905
zanst <me@zan.st>
2011-11-16 17:15:33 -0200

    now we're working with a paid plan from airbrake. Bringing security back.

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -6,7 +6,8 @@ require "resque/failure/redis"
 Resque.redis = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env]
 
 Resque::Failure::Airbrake.configure do |config|
-  config.api_key = 'ea4592991b87980de4f0edfb2a5a78be'
+  config.api_key = '7d489c216b28c67d36d16be815ae48b1'
+  config.secure = true
 end
 
 Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
