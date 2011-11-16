1470cb6b6f12205904b29eb7a03f29c88b6e7a4f
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-16 17:08:06 -0200

    Product page only logged

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -2,14 +2,15 @@
 require 'spec_helper'
 
 describe ProductController do
-  render_views
-  let(:product) { FactoryGirl.create(:basic_shoe) }
+  with_a_logged_user do
+    render_views
+    let(:product) { FactoryGirl.create(:basic_shoe) }
 
-  describe "#index" do
-    it "should load and display the product" do
-      get :index, :id => product.id
-      assigns(:product).should == product
-      response.should render_template(:index)
+    describe "GET index" do
+      it "should load and display the product" do
+        get :index, :id => product.id
+        assigns(:product).should == product
+      end
     end
   end
 end
