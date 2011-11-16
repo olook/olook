54f2f407a7acce832d6c661382091eed515815fa
zanst <me@zan.st>
2011-11-16 12:18:19 -0200

    added airbrake monitoring to resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,52 +1,14 @@
 require "resque"
 require "resque/failure/multiple"
+require "resque/failure/airbrake"
 require "resque/failure/redis"
 
 Resque.redis = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env]
 
-module Resque
-  module Failure
-    # Logs failure messages.
-    class Logger < Base
-      def save
-        Rails.logger.error detailed
-      end
-
-      def detailed
-        <<-EOF
-#{worker} failed processing #{queue}:
-Payload:
-#{payload.inspect.split("\n").map { |l| "  " + l }.join("\n")}
-Exception:
-  #{exception}
-#{exception.backtrace.map { |l| "  " + l }.join("\n")}
-        EOF
-      end
-    end
-
-    # Emails failure messages.
-    # Note: uses Mail (default in Rails 3.0) not TMail (Rails 2.x).
-    class Notifier < Logger
-      def save
-        text, subject = detailed, "[Error] #{queue}: #{exception}"
-        Mail.deliver do
-          from "no-reply@olook.com.br"
-          to "stephano.zanzin@codeminer42.com", "rinaldi.fonseca@codeminer42.com", "rafaelrosafu@gmail.com"
-          subject subject
-          text_part do
-            body text
-          end
-        end
-      rescue
-        puts $!
-      end
-    end
-  end
+Resque::Failure::Airbrake.configure do |config|
+  config.api_key = 'ea4592991b87980de4f0edfb2a5a78be'
+  config.secure = true
 end
 
-Resque::Failure::Multiple.configure do |multi|
-  # Always stores failure in Redis and writes to log
-  multi.classes = Resque::Failure::Redis, Resque::Failure::Logger
-  # Production/staging only: also email us a notification
-  multi.classes << Resque::Failure::Notifier if Rails.env.production? || Rails.env.staging?
-end
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure.backend = Resque::Failure::Multiple
