ca48d8c58ac354bfb564a479d7f041452c0c51a1
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-16 14:31:00 -0200

    Added dimensions and weight to variants

--- spec/factories/variants.rb
+++ spec/factories/variants.rb
@@ -3,11 +3,17 @@ FactoryGirl.define do
   factory :variant do
     factory :basic_shoe_size_35 do
       association :product, :factory => :basic_shoe
+      is_master false
       number '35A'
       description 'size 35'
       display_reference 'size-35'
       price 123.45
       inventory 10
+      
+      width 1
+      height 1
+      length 1
+      weight 1.0
     end
 
     factory :basic_shoe_size_40 do
