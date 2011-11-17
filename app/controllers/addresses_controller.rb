901667a650a020c31432aa48780671c052815a7b
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-17 13:48:41 -0200

    Added address id in the session

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -16,6 +16,7 @@ class AddressesController < ApplicationController
   def create
     @address = @user.addresses.build(params[:address])
     if @address.save
+      session[:delivery_address_id] = @address.id
       redirect_to(new_payment_path, :notice => 'Endereço cadastrado com sucesso')
     else
       respond_with(@address)
