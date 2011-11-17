e789a88d2e600928a7ce4f997f88aa97b8049ec7
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-17 17:41:53 -0200

    Added initial funcionality to change product image

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
+  $("div#pics_suggestions ul#thumbs li a").live("click", function() {
+    thumb_url = $(this).find('img').attr("src").split("thumb_");
+    domain = thumb_url[0];
+    new_image = thumb_url[1];
+    full_image = $("div#pics_suggestions div#full_pic a img");
+    full_image.attr("src", domain+new_image);
+    return false;
+  });
+
   $("div#infos div.size ol li").live('click', function() {
     if($(this).hasClass("unavailable") == false) {
       lists = $(this).parents("ol").find("li");
