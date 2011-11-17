7954733d7830a42b4beb8a79fe2bd1f31c06d44c
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-17 19:47:43 -0200

    Added default values for freight services

--- app/models/freight_calculator.rb
+++ app/models/freight_calculator.rb
@@ -2,6 +2,10 @@
 module FreightCalculator
   VALID_ZIP_FORMAT = /\A(\d{8})\z/
   
+  DEFAULT_FREIGHT_PRICE   = 0.0
+  DEFAULT_FREIGHT_COST    = 0.0
+  DEFAULT_INVENTORY_TIME  = 1
+  
   def self.freight_for_zip(zip_code, weight, volume)
     clean_zip_code = clean_zip(zip_code)
     return {} unless valid_zip?(clean_zip_code)
@@ -13,9 +17,9 @@ module FreightCalculator
       break if freight_price
     end
       
-    { :price          => freight_price.try(:price)  || 0.0,
-      :cost           => freight_price.try(:cost)   || 0.0,
-      :delivery_time  => freight_price.try(:delivery_time) || 0 }
+    { :price          => freight_price.try(:price)  || DEFAULT_FREIGHT_PRICE,
+      :cost           => freight_price.try(:cost)   || DEFAULT_FREIGHT_COST,
+      :delivery_time  => (freight_price.try(:delivery_time) || 0) + DEFAULT_INVENTORY_TIME }
   end
   
   def self.valid_zip?(zip_code)
