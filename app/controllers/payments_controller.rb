4ef6c1a21c26ee79eaeeb1c08a296b2c45fbd90e
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-17 16:54:32 -0200

    Added authenticate_user in the payments_controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class PaymentsController < ApplicationController
   respond_to :html
+  before_filter :authenticate_user!
   before_filter :load_user
   before_filter :check_user_address
 
