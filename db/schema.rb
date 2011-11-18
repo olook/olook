b060fe878008b4711633d8c104ae096bce73b478
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-18 17:03:45 -0200

    schama.db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111118141853) do
+ActiveRecord::Schema.define(:version => 20111118170846) do
 
   create_table "addresses", :force => true do |t|
     t.integer "user_id"
