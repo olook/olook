cec67ec3af3e1335bd8c8eb5f6478f42f7e55d2f
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-18 17:57:00 -0200

    fix tests

--- spec/routing/product_spec.rb
+++ spec/routing/product_spec.rb
@@ -4,7 +4,7 @@ require "spec_helper"
 describe ProductController do
   describe "should include a named route to" do
     it "the product index" do
-      {get: product_path(1)}.should route_to("product#index", :id => '1')
+      {get: product_path(1)}.should route_to("product#show", :id => '1')
     end
   end
 end
