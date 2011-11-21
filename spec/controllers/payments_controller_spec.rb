fa42269baad5f3831a5beb4f3177c073201dd14c
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-21 16:06:15 -0200

    checking the order in the session

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -14,24 +14,39 @@ describe PaymentsController do
   end
 
   describe "GET new" do
-    it "should assigns @payment" do
-      get 'new'
-      assigns(:payment).should be_a_new(Payment)
+    context "with a valid order" do
+      before :each do
+        session[:order] = :fake_order
+      end
+      it "should assigns @payment" do
+        get 'new'
+        assigns(:payment).should be_a_new(Payment)
+      end
+
+      it "should assigns @delivery_address from the session" do
+        get 'new'
+        assigns(:delivery_address).should eq(address)
+      end
+
+      it "should redirect to new_payment_path if the delivery_address_id is nil" do
+        session[:delivery_address_id] = nil
+        get 'new'
+        response.should redirect_to(addresses_path)
+      end
     end
 
-    it "should assigns @delivery_address from the session" do
-      get 'new'
-      assigns(:delivery_address).should eq(address)
-    end
-
-    it "should redirect to new_payment_path if the delivery_address_id is nil" do
-      session[:delivery_address_id] = nil
-      get 'new'
-      response.should redirect_to(addresses_path)
+    context "with a invalid order" do
+      it "should redirect to root path" do
+        get 'new'
+        response.should redirect_to(root_path)
+      end
     end
   end
 
   describe "POST create" do
+    before :each do
+      session[:order] = :fake_order
+    end
     describe "with valid params" do
       it "should process the payment" do
         PaymentBuilder.should_receive(:new).and_return(payment_builder = mock)
