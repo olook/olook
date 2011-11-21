118b7efa322546c2544f8df03921f843a4ec9f6e
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-21 16:55:21 -0200

    Using a cart_controller

---
+++ spec/controllers/cart_controller_spec.rb
@@ -0,0 +1,42 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe CartController do
+  with_a_logged_user do
+    render_views
+    let(:variant) { FactoryGirl.create(:basic_shoe_size_35) }
+    let(:product) { variant.product }
+
+    describe "POST create" do
+      before :each do
+        @back = request.env['HTTP_REFERER'] = product_path(product)
+      end
+
+      context "with a valid @variant" do
+        it "should redirect back to product page" do
+          post :create, :variant => {:id => variant.id}
+          response.should redirect_to(product_path(product))
+        end
+
+        it "should add a variant in the order" do
+          Order.any_instance.should_receive(:add_variant).with(variant).and_return(true)
+          post :create, :variant => {:id => variant.id}
+        end
+      end
+
+      context "with a invalid @variant" do
+        it "should redirect back with a warning if the variant dont exists" do
+          post :create, :variant => {:id => ""}
+          response.should redirect_to(@back, :notice => "Selecione um produto")
+        end
+
+        it "should redirect back with a warning if the variant is not available" do
+          Variant.any_instance.stub(:available?).and_return(false)
+          post :create, :variant => {:id => ""}
+          response.should redirect_to(@back, :notice => "Produto esgotado")
+        end
+      end
+    end
+  end
+end
+
