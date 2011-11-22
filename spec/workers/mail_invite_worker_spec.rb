51ca5c50d4440d78f71b5680f7edf447d2a96bd9
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-22 14:36:51 -0200

    Refactored system to use SendGrid instead of mailee.me

--- spec/workers/mail_invite_worker_spec.rb
+++ spec/workers/mail_invite_worker_spec.rb
@@ -2,17 +2,12 @@
 require "spec_helper"
 
 describe MailInviteWorker do
+  let(:invite) { FactoryGirl.create(:invite) }
+  let(:mock_mail) { double :mail }
+
   it "should send the invite e-mail given an invite id" do
-    invite = FactoryGirl.create(:invite)
-    
-    template = double(:template)
-    template.stub(:html).and_return('__invite_link__')
-    Mailee::Template.stub(:find).and_return(template)
-    
-    described_class.should_receive(:accept_invitation_url).with(invite.member_invite_token).and_return('site/convite')
-    
-    described_class.stub(:configuration).and_return({mailee: 'config', subject: '__inviter_name__', template_id: 1234})
-    Mailee::Message.should_receive(:create).with({mailee: 'config', subject: invite.member_name, emails: invite.email, html: 'site/convite' })
+    mock_mail.should_receive(:deliver)
+    InviteMailer.should_receive(:invite_email).with(invite).and_return(mock_mail)
     
     invite.sent_at.should be_nil
     
