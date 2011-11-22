6a528fe97aea3b9832bb2097cc2c6a1daf79021e
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-22 14:36:51 -0200

    Refactored system to use SendGrid instead of mailee.me

--- spec/workers/signup_notification_worker_spec.rb
+++ spec/workers/signup_notification_worker_spec.rb
@@ -2,10 +2,25 @@
 require 'spec_helper'
 
 describe SignupNotificationWorker do
-  it "should send a notification mailer when performs" do
-    user_id = 1
-    NotificationsMailer.should_receive(:new).and_return(mailer = double)
-    mailer.should_receive(:signup).with(user_id)
-    SignupNotificationWorker.perform(user_id)
+  let(:member) { FactoryGirl.create(:member) }
+  let(:mock_mail) { double :mail }
+
+  it "should send the welcome e-mail given a member id" do
+    mock_mail.should_receive(:deliver)
+    MemberMailer.should_receive(:welcome_email).with(member).and_return(mock_mail)
+    
+    member.welcome_sent_at.should be_nil
+    
+    described_class.perform(member.id)
+    
+    member.reload
+    member.welcome_sent_at.should_not be_nil
+  end
+  
+  it 'should raise "The welcome message for member X was already sent" when receiving a member to whom we sent a member' do
+    User.stub(:find).with(123).and_return( mock_model(User, :id => 123, :welcome_sent_at => Time.now) )
+    expect {
+      described_class.perform(123)
+    }.to raise_error(RuntimeError, "The welcome message for member 123 was already sent")
   end
 end
