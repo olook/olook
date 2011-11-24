f1f9c98aeb5f15239d52eee74c5d6625107b7dd2
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-24 14:04:27 -0200

    organizing accessors

--- app/models/ecommerce/credit_card.rb
+++ app/models/ecommerce/credit_card.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class CreditCard < Payment
-  attr_accessor :user_name, :credit_card_number, :bank, :security_code, :expiration_date, :user_identification, :telephone, :user_birthday, :payments, :receipt
+  attr_accessor :user_name, :credit_card_number, :bank, :security_code, :expiration_date, :telephone, :user_birthday, :payments
 
   PhoneFormat = /^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/
   validates_with CreditCardValidator
--- app/models/ecommerce/debit.rb
+++ app/models/ecommerce/debit.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Debit < Payment
-  attr_accessor :bank, :receipt
+  attr_accessor :bank, :user_identification
   validates_presence_of :bank, :receipt
   def to_s
     "DebitoBancario"
