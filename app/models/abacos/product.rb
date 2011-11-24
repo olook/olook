ebe16584a09c9adeeb821eca634b773a5f1e38d8
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-24 17:58:25 -0200

    Changed detail and profile parsing from the product

--- app/models/abacos/product.rb
+++ app/models/abacos/product.rb
@@ -3,11 +3,13 @@ module Abacos
   class Product
     include ::Abacos::Helpers
 
-    attr_reader :name, :model_name, :category,
+    attr_reader :integration_protocol,
+                :name, :model_name, :category,
                 :width, :height, :length, :weight,
-                :color
+                :color, :details, :profiles
 
     def initialize(abacos_product)
+      @integration_protocol = abacos_product[:protocolo_produto]
       @name = abacos_product[:nome_produto]
       @model_name = abacos_product[:codigo_produto].to_s
       @category = parse_category(abacos_product[:descricao_classe])
@@ -16,6 +18,8 @@ module Abacos
       @length = abacos_product[:comprimento].to_f
       @weight = abacos_product[:peso].to_f
       @color = parse_color( abacos_product[:descritor_pre_definido] )
+      @details = parse_details( abacos_product[:caracteristicas_complementares] )
+      @profiles = parse_profiles( abacos_product[:caracteristicas_complementares] )
     end
 
   private
@@ -29,8 +33,33 @@ module Abacos
       end
     end
     
-    def parse_color(abacos_descritor_pre_definido)
-      find_in_descritor_pre_definido(abacos_descritor_pre_definido, 'COR')
+    def parse_color(data)
+      find_in_descritor_pre_definido(data, 'COR')
+    end
+    
+    def parse_details(data)
+      items = data[:rows][:dados_caracteristicas_complementares]
+      items = [items] unless items.is_a? Array
+      
+      {}.tap do |result|
+        items.each do |item|
+          next if item[:tipo_nome].strip == 'Perfil'
+          result[ item[:tipo_nome].strip ] = item[:texto].strip
+        end
+      end
+    end
+
+    def parse_profiles(data)
+      items = data[:rows][:dados_caracteristicas_complementares]
+      items = [items] unless items.is_a? Array
+
+      result = []
+      items.each do |item|
+        if item[:tipo_nome].strip == 'Perfil'
+          result += item[:texto].split(',')
+        end
+      end
+      result.map &:strip
     end
   end
 end
