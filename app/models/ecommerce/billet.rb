13101514b0caeb6cc31b4e74df88378eaa12db3e
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-24 16:28:30 -0200

    Added State Machine for Payments

--- app/models/ecommerce/billet.rb
+++ app/models/ecommerce/billet.rb
@@ -2,6 +2,28 @@
 class Billet < Payment
   validates_presence_of :receipt
 
+  state_machine :initial => :started do
+    event :billet_printed do
+      transition :started => :billet_printed
+    end
+
+    event :authorized do
+      transition :billet_printed => :authorized
+    end
+
+    event :completed do
+      transition :authorized => :completed, :under_review => :completed
+    end
+
+    event :under_review do
+      transition :authorized => :under_review
+    end
+
+    event :refunded do
+      transition :under_review => :refunded
+    end
+  end
+
   def to_s
     "BoletoBancario"
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111124170730) do
+ActiveRecord::Schema.define(:version => 20111124172321) do
 
   create_table "addresses", :force => true do |t|
     t.integer "user_id"
@@ -145,6 +145,7 @@ ActiveRecord::Schema.define(:version => 20111124170730) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "type"
+    t.string   "state"
   end
 
   create_table "pictures", :force => true do |t|
