64212027bb6f89e3a0458dccbd4e9a854354210d
zanst <me@zan.st>
2011-11-25 15:39:17 -0200

    (re)added the marked checkbox to entire page sign in

--- db/schema.rb
+++ db/schema.rb
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(:version => 20111124172321) do
 
   create_table "events", :force => true do |t|
     t.integer  "user_id"
-    t.integer  "event_type",  :null => false
+    t.integer  "event_type",  :limit => 255, :null => false
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -78,13 +78,13 @@ ActiveRecord::Schema.define(:version => 20111124172321) do
 
   create_table "freight_prices", :force => true do |t|
     t.integer  "shipping_service_id"
-    t.integer  "zip_start"
-    t.integer  "zip_end"
-    t.decimal  "weight_start",        :precision => 8, :scale => 3
-    t.decimal  "weight_end",          :precision => 8, :scale => 3
+    t.integer  "zip_start",           :limit => 255
+    t.integer  "zip_end",             :limit => 255
+    t.decimal  "weight_start"
+    t.decimal  "weight_end"
     t.integer  "delivery_time"
-    t.decimal  "price",               :precision => 8, :scale => 2
-    t.decimal  "cost",                :precision => 8, :scale => 2
+    t.decimal  "price"
+    t.decimal  "cost"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -150,7 +150,7 @@ ActiveRecord::Schema.define(:version => 20111124172321) do
 
   create_table "pictures", :force => true do |t|
     t.string   "image"
-    t.integer  "display_on"
+    t.integer  "display_on", :limit => 255
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -264,15 +264,15 @@ ActiveRecord::Schema.define(:version => 20111124172321) do
     t.string   "number"
     t.string   "description"
     t.string   "display_reference"
-    t.decimal  "price",             :precision => 10, :scale => 2
+    t.decimal  "price"
     t.integer  "inventory"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_master"
-    t.decimal  "width",             :precision => 8,  :scale => 2
-    t.decimal  "height",            :precision => 8,  :scale => 2
-    t.decimal  "length",            :precision => 8,  :scale => 2
-    t.decimal  "weight",            :precision => 8,  :scale => 2
+    t.decimal  "width"
+    t.decimal  "height"
+    t.decimal  "length"
+    t.decimal  "weight"
   end
 
   add_index "variants", ["product_id"], :name => "index_variants_on_product_id"
