6fd37425ccca190a613d1e5c8ca1e403b830f9f4
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-25 15:17:10 -0200

    addeds state machine to credit_card

--- app/models/ecommerce/credit_card.rb
+++ app/models/ecommerce/credit_card.rb
@@ -5,6 +5,32 @@ class CreditCard < Payment
   PhoneFormat = /^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/
   validates_with CreditCardValidator
 
+  state_machine :initial => :started do
+    event :canceled do
+      transition :started => :canceled, :under_analysis => :canceled
+    end
+
+    event :under_analysis do
+      transition :started => :under_analysis
+    end
+
+    event :authorized do
+      transition :started => :authorized, :under_analysis => :authorized
+    end
+
+    event :completed do
+      transition :authorized => :completed, :under_review => :completed
+    end
+
+    event :under_review do
+      transition :authorized => :under_review
+    end
+
+    event :reversed do
+      transition :under_review => :reversed
+    end
+  end
+
   def to_s
     "CartaoCredito"
   end
