03e817e9d8f0b99083af00e97e94a3d8b511761d
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-25 19:30:08 -0200

    state machine tests for credit_card

--- spec/models/ecommerce/credit_card_spec.rb
+++ spec/models/ecommerce/credit_card_spec.rb
@@ -2,11 +2,66 @@ require "spec_helper"
 
 describe CreditCard do
   subject { FactoryGirl.build(:credit_card) }
+  let(:canceled) { "5" }
+  let(:authorized) { "1" }
+  let(:completed) { "4" }
+  let(:under_review) { "8" }
+  let(:reversed) { "7" }
+  let(:under_analysis) { "6" }
 
   context "status" do
     it "should return nil with a invalid status" do
       invalid_status = '0'
       subject.set_state(invalid_status).should be(nil)
+    end
+  end
+
+  describe "state machine" do
+    it "should set canceled" do
+      subject.set_state(canceled)
+      subject.canceled?.should eq(true)
+    end
+
+    it "should set authorized" do
+      subject.set_state(authorized)
+      subject.authorized?.should eq(true)
+    end
+
+    it "should set authorized given under_analysis" do
+      subject.set_state(under_analysis)
+      subject.set_state(authorized)
+      subject.authorized?.should eq(true)
+    end
+
+    it "should set completed" do
+      subject.set_state(authorized)
+      subject.set_state(completed)
+      subject.completed?.should eq(true)
+    end
+
+    it "should set completed given under_review" do
+      subject.set_state(authorized)
+      subject.set_state(under_review)
+      subject.set_state(completed)
+      subject.completed?.should eq(true)
+    end
+
+    it "should set under_review" do
+      subject.set_state(authorized)
+      subject.set_state(under_review)
+      subject.under_review?.should eq(true)
+    end
+
+    it "should set under_analysis" do
+      subject.set_state(under_analysis)
+      subject.under_analysis?.should eq(true)
+    end
+
+    it "should set reversed" do
+      subject.set_state(authorized)
+      subject.set_state(under_review)
+      subject.set_state(reversed)
+      subject.reversed?.should eq(true)
     end
   end
 
