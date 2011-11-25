ca195bada74503c3bb773d97f84f9a2505dd405f
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-25 20:45:38 -0200

    Updated credit cart page

--- app/models/ecommerce/credit_card.rb
+++ app/models/ecommerce/credit_card.rb
@@ -2,7 +2,7 @@
 class CreditCard < Payment
   attr_accessor :user_name, :credit_card_number, :bank, :security_code, :expiration_date, :telephone, :user_birthday, :payments
 
-  BANKS_OPTIONS = ["Visa", "AmericanExpress", "Mastercard", "Diners", "Hipercard", "Aura"]
+  BANKS_OPTIONS = ["Visa", "Mastercard", "AmericanExpress", "Diners", "Hipercard", "Aura"]
 
   PhoneFormat = /^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/
   validates_with CreditCardValidator
