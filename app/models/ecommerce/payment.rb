f01871791e8e613c228b5f9c2e4d0431f8a079f0
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-25 12:12:18 -0200

    using status in payment

--- app/models/ecommerce/payment.rb
+++ app/models/ecommerce/payment.rb
@@ -2,6 +2,14 @@
 class Payment < ActiveRecord::Base
   REASON = 'Pagamento'
   RECEIPT = 'AVista'
+  STATUS = {
+    "1" => :authorized,
+    "2" => :started,
+    "3" => :billet_printed,
+    "4" => :completed,
+    "5" => :canceled,
+    "6" => :under_review
+  }
   attr_accessor :receipt, :user_identification
   belongs_to :order
   has_one :payment_response
