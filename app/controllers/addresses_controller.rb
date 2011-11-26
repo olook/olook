c4ca85f78abc4f9b944140f51e2fb859894a0cce
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-26 20:13:21 -0200

    assigning freight in the session

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,8 +1,10 @@
 # -*- encoding : utf-8 -*-
 class AddressesController < ApplicationController
+  include PaymentManager
   respond_to :html
   before_filter :authenticate_user!
   before_filter :load_user
+  before_filter :check_order, :except => [:index]
   before_filter :assign_default_country, :only => [:create]
 
   def index
@@ -16,21 +18,28 @@ class AddressesController < ApplicationController
   def create
     @address = @user.addresses.build(params[:address])
     if @address.save
-      session[:delivery_address_id] = @address.id
-      redirect_to(payments_path, :notice => 'Endereço cadastrado com sucesso')
+      assign_address_and_freight_in_the_session(@address)
+      redirect_to(payments_path)
     else
       respond_with(@address)
     end
   end
 
   def assign_address
-    session[:delivery_address_id] = params[:delivery_address_id]
-    redirect_to payments_path
+    address = @user.addresses.find_by_id(params[:delivery_address_id])
+    if address
+      assign_address_and_freight_in_the_session(address)
+      redirect_to(payments_path)
+    else
+      redirect_to addresses_path
+    end
   end
 
   private
-  def load_user
-    @user = current_user
+
+  def assign_address_and_freight_in_the_session(address)
+    session[:delivery_address_id] = address.id
+    session[:freight] = FreightCalculator.new_freight_for_zip(address.zip_code, @order.total)
   end
 
   def assign_default_country
--- app/models/freight_calculator.rb
+++ app/models/freight_calculator.rb
@@ -1,11 +1,15 @@
 # -*- encoding : utf-8 -*-
 module FreightCalculator
   VALID_ZIP_FORMAT = /\A(\d{8})\z/
-  
+
   DEFAULT_FREIGHT_PRICE   = 0.0
   DEFAULT_FREIGHT_COST    = 0.0
   DEFAULT_INVENTORY_TIME  = 1
-  
+
+  def self.new_freight_for_zip(zip_code, price)
+    {:price => 26.36, :cost => 2.34, :delivery_time => 3}
+  end
+
   def self.freight_for_zip(zip_code, weight, volume)
     clean_zip_code = clean_zip(zip_code)
     return {} unless valid_zip?(clean_zip_code)
@@ -16,17 +20,17 @@ module FreightCalculator
 
       break if freight_price
     end
-      
+
     { :price          => freight_price.try(:price)  || DEFAULT_FREIGHT_PRICE,
       :cost           => freight_price.try(:cost)   || DEFAULT_FREIGHT_COST,
       :delivery_time  => (freight_price.try(:delivery_time) || 0) + DEFAULT_INVENTORY_TIME }
   end
-  
+
   def self.valid_zip?(zip_code)
     zip_code.match(VALID_ZIP_FORMAT) ? true : false
   end
-  
+
   def self.clean_zip(dirty_zip)
-    return dirty_zip.gsub /\D/, '' 
+    return dirty_zip.gsub /\D/, ''
   end
 end
