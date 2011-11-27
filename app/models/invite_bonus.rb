b1aad36aa19a4b81398adfeb9c0b023ed9e68728
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-11-27 14:17:01 -0200

    using alread_used method in the InviteBonus#calculate

--- app/models/invite_bonus.rb
+++ app/models/invite_bonus.rb
@@ -1,11 +1,15 @@
 # -*- encoding : utf-8 -*-
 module InviteBonus
   def self.calculate(member)
-    bonus = for_accepted_invites(member) + for_being_invited(member)
+    bonus = for_accepted_invites(member) + for_being_invited(member) - already_used(member)
     bonus = 300.0 if bonus > 300.0
     bonus
   end
-  
+
+  def self.already_used(member)
+    member.orders.inject(0) {|result, item| result + (item.credits || 0)}
+  end
+
 protected
   module FirstPromotion
     END_DATE = DateTime.civil(2011, 11, 9, 10, 0, 0)
@@ -18,13 +22,13 @@ protected
 
   module SecondPromotion
     START_DATE = FirstPromotion::END_DATE
-    
+
     def self.for_accepted_invites(member)
       accepted_invites_count = member.invites.accepted.where('accepted_at > :start_date', :start_date => START_DATE).count
       (accepted_invites_count / 5) * 20.0
     end
   end
-  
+
   @promotion_rules = [FirstPromotion, SecondPromotion]
 
   def self.for_accepted_invites(member)
