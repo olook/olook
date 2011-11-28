24e20f71289f2492c18e83524f7ef5482d4dff80
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-28 02:31:15 -0200

    Carousel showroom

--- app/assets/javascripts/showroom.js
+++ app/assets/javascripts/showroom.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  ShowroomInit.updateListSize();
+
   $("#showroom div.products_list a.more").toggle(function() {
     el = $(this).attr('rel');
     box = $(this).parents('.type_list').find("."+el);
@@ -10,4 +12,33 @@ $(document).ready(function() {
       box.slideUp();
     }
   );
+
+  $("div#mask_carousel_showroom a.arrows").live("click", function() {
+    list = $(this).siblings("ul");
+    listSize = $(list).find("li").size();
+    minWidth = (listSize-1)*(-323);
+    atualPosition = parseInt($(list).css("left").split("px")[0]);
+    if($(this).hasClass("next") == true) {
+      if(atualPosition > minWidth) {
+        $(list).stop().animate({
+          left: atualPosition+(-323)+"px"
+        }, 'fast');
+      }
+    } else {
+      if(atualPosition < 0) {
+        $(list).stop().animate({
+          left: atualPosition+(323)+"px"
+        }, 'fast');
+      }
+    }
+    return false;
+  });
 });
+
+ShowroomInit = {
+  updateListSize : function() {
+    list = $("div#mask_carousel_showroom ul");
+    listSize = $(list).find("li").size()*323;
+    $(list).width(listSize);
+  }
+};
