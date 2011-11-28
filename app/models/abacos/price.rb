735e43e2f2c5a05f58b6ebfcfaa7312d4746c4dd
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-27 22:39:26 -0200

    Added integration method to Abacos::Price and more tests to API

--- app/models/abacos/price.rb
+++ app/models/abacos/price.rb
@@ -8,5 +8,15 @@ module Abacos
       @number = abacos_product[:codigo_produto]
       @price = abacos_product[:preco_tabela].to_f
     end
+
+    def integrate
+      variant = ::Variant.find_by_number(self.number)
+      raise RuntimeError.new "Price is related with Variant number #{self.number}, but it doesn't exist" if variant.nil?
+      
+      variant.price = self.price
+      variant.save!
+
+      Abacos::ProductAPI.confirm_price(self.integration_protocol)
+    end
   end
 end
--- spec/models/abacos/price_spec.rb
+++ spec/models/abacos/price_spec.rb
@@ -16,4 +16,24 @@ describe Abacos::Price do
       subject.price.should == 69.9
     end
   end
+
+  describe '#integrate' do
+    it "should raise and error if the related Variant doesn't exist" do
+      expect {
+        ::Variant.should_receive(:find_by_number).with(subject.number).and_return(nil)
+        subject.integrate
+      }.to raise_error "Price is related with Variant number #{subject.number}, but it doesn't exist"
+    end
+
+    it 'should update the variant price and integrate it' do
+      mock_variant = mock_model(::Variant)
+      mock_variant.should_receive(:'price=').with(subject.price)
+      mock_variant.should_receive(:'save!')
+      ::Variant.should_receive(:find_by_number).with(subject.number).and_return(mock_variant)
+
+      Abacos::ProductAPI.should_receive(:confirm_price).with(subject.integration_protocol)
+      
+      subject.integrate
+    end
+  end
 end
