10b300a8097325e8135a1453c5b4fdf607b46009
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-28 20:46:30 -0200

    Fixed bug on master_variant update

--- app/models/abacos/product.rb
+++ app/models/abacos/product.rb
@@ -29,21 +29,21 @@ module Abacos
 
       integrate_attributes(product)
       integrate_details(product)
-      product.save!
-      
+
       Abacos::ProductAPI.confirm_product(self.integration_protocol)
     end
     
     def integrate_attributes(product)
       product.update_attributes(self.attributes)
       product.description ||= self.name
+      product.save!
     end
 
     def integrate_details(product)
       self.details.each do |key, value|
-        product.details.build(:translation_token => key,
-                              :description => value,
-                              :display_on => DisplayDetailOn::DETAILS )
+        product.details.create( :translation_token => key,
+                                :description => value,
+                                :display_on => DisplayDetailOn::DETAILS )
       end
     end
 
