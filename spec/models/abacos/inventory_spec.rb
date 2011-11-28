61069f53556e17c99261758a492e03172e3ee5b8
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-27 22:28:24 -0200

    Added integration method to Abacos::Inventory

--- spec/models/abacos/inventory_spec.rb
+++ spec/models/abacos/inventory_spec.rb
@@ -16,4 +16,24 @@ describe Abacos::Inventory do
       subject.inventory.should == 7
     end
   end
+  
+  describe '#integrate' do
+    it "should raise and error if the Variant with number doesn't exist" do
+      expect {
+        ::Variant.should_receive(:find_by_number).with(subject.number).and_return(nil)
+        subject.integrate
+      }.to raise_error "Inventory is related with Variant number #{subject.number}, but it doesn't exist"
+    end
+
+    it 'should create, merge the imported attributes on the variant and integrate it' do
+      mock_variant = mock_model(::Variant)
+      mock_variant.should_receive(:'inventory=').with(subject.inventory)
+      mock_variant.should_receive(:'save!')
+      ::Variant.should_receive(:find_by_number).with(subject.number).and_return(mock_variant)
+
+      Abacos::ProductAPI.should_receive(:confirm_inventory).with(subject.integration_protocol)
+      
+      subject.integrate
+    end
+  end
 end
