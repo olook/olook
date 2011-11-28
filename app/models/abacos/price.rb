f4fd7c7a53a3ecc1366907b05a175cc242f151ab
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-11-28 02:26:04 -0200

    Finished integration with Abacos products, refactored queue interface and added some tests

--- app/models/abacos/price.rb
+++ app/models/abacos/price.rb
@@ -3,12 +3,12 @@ module Abacos
   class Price
     attr_reader :integration_protocol, :number, :price
 
-    def initialize(abacos_product)
-      @integration_protocol = abacos_product[:protocolo_preco]
-      @number = abacos_product[:codigo_produto]
-      @price = abacos_product[:preco_tabela].to_f
+    def initialize(parsed_data)
+      parsed_data.each do |key, value|
+        self.instance_variable_set "@#{key}", value
+      end
     end
-
+    
     def integrate
       variant = ::Variant.find_by_number(self.number)
       raise RuntimeError.new "Price is related with Variant number #{self.number}, but it doesn't exist" if variant.nil?
@@ -17,6 +17,12 @@ module Abacos
       variant.save!
 
       Abacos::ProductAPI.confirm_price(self.integration_protocol)
+    end
+
+    def self.parse_abacos_data(abacos_product)
+      { integration_protocol: abacos_product[:protocolo_preco],
+        number:               abacos_product[:codigo_produto],
+        price:                abacos_product[:preco_tabela].to_f }
     end
   end
 end
