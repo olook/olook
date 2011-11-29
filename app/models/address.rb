866fc8d646bbe11a2b11bd3d1c01b8fe9b8e7d5d
Raul Souza Lima <raulsouzalima@gmail.com>
2011-11-29 14:41:55 -0200

    address.identification = address.first_name + address.last_name

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,4 +10,8 @@ class Address < ActiveRecord::Base
   validates :zip_code, :format => {:with => ZipCodeFormat}
   validates :telephone, :format => {:with => PhoneFormat}
   validates :state, :format => {:with => StateFormat}
+
+  def identification
+    "#{first_name} #{last_name}"
+  end
 end
