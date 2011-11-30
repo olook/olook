735580220cc6e896444128765fd635a76d15ce75
zanst <me@zan.st>
2011-11-30 20:41:30 -0200

    finished User::AddressesController implementation

--- app/controllers/user/addresses_controller.rb
+++ app/controllers/user/addresses_controller.rb
@@ -3,6 +3,7 @@ class User::AddressesController < ApplicationController
   respond_to :html
   before_filter :authenticate_user!
   before_filter :load_user
+  before_filter :assign_default_country, :only => [:create]
 
   def index
     @addresses = @user.addresses
@@ -19,6 +20,24 @@ class User::AddressesController < ApplicationController
   def create
     @address = @user.addresses.build(params[:address])
     if @address.save
+      redirect_to(user_addresses_path)
+    else
+      respond_with(@address)
+    end
+  end
+
+  def update
+    @address = @user.addresses.find(params[:id])
+    if @address.update_attributes(params[:address])
+      redirect_to(user_addresses_path)
+    else
+      respond_with(@address)
+    end
+  end
+
+  def destroy
+    @address = @user.addresses.find(params[:id])
+    if @address.destroy
       redirect_to(user_addresses_path)
     else
       respond_with(@address)
