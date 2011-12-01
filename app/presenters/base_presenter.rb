b835b3e3eb65d00e460857eada24ececdd43a72f
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-01 21:54:20 -0200

    Added first iteration of showroom presenter

---
+++ app/presenters/base_presenter.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+class BasePresenter
+  def initialize(object, template)
+    @object = object
+    @template = template
+  end
+  
+  def self.presents(name)
+    define_method(name) do
+      @object
+    end
+  end
+  
+  def h
+    @template
+  end
+end
---
+++ app/presenters/showroom_presenter.rb
@@ -0,0 +1,39 @@
+# -*- encoding : utf-8 -*-
+class ShowroomPresenter < BasePresenter
+  presents :member
+
+  def render_identification
+    if member.has_facebook?
+      h.render :partial => "showroom_facebook_connected"
+    else
+      h.render :partial => "showroom_facebook_connect"
+    end
+  end
+  
+  def collection_name
+    I18n.t('date.month_names')[Date.today.month]
+  end
+  
+  def display_products(asked_range, products = Array.new(20))
+    range = parse_range(asked_range, products)
+
+    output = ''
+    (products[range] || []).each do |product|
+      output << h.render(:partial => "shared/showroom_product_item", :locals => {:product => product})
+    end
+    h.raw output
+  end
+  
+private
+  def parse_range(asked_range, array)
+    if asked_range.is_a? Range
+      start_range = asked_range.first
+      end_range = asked_range.last
+    else
+      start_range = asked_range
+      end_range = start_range + 100000
+    end
+    end_range = (array.length-1) if end_range > (array.length-1)
+    (start_range..end_range)
+  end
+end
