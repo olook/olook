3727c09d11af8922351ecf166bc0b8b9d9c66ef5
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-02 17:51:18 -0200

    assigning @order in the products#show

--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -6,6 +6,11 @@ describe ProductController do
     render_views
     let(:variant) { FactoryGirl.create(:basic_shoe_size_35) }
     let(:product) { variant.product }
+    let(:order) { FactoryGirl.create(:order, :user => user).id }
+
+    before :each do
+      session[:order] = order
+    end
 
     describe "GET index" do
       it "should load and display the product" do
@@ -16,6 +21,11 @@ describe ProductController do
       it "should assigns @variants" do
         get :show, :id => product.id
         assigns(:variants).should == product.variants
+      end
+
+      it "should assigns @order" do
+        get :show, :id => product.id
+        assigns(:order).should == Order.find(order)
       end
     end
    end
