f98433e04395455e2fa8e3ca245f73320d09c657
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-01 22:19:28 -0200

    Using layout my_account on user/credits controller

--- app/controllers/user/credits_controller.rb
+++ app/controllers/user/credits_controller.rb
@@ -1,10 +1,14 @@
 # -*- encoding : utf-8 -*-
 class User::CreditsController < ApplicationController
+  layout "my_account"
+
   respond_to :html
   before_filter :authenticate_user!
   before_filter :load_user
 
   def index
+    @member = current_user
+    @invites = @member.invites.page(params[:page]).per_page(10)
   end
 
   private
