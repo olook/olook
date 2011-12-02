4f24586e4762abf1fbf67ae6d386bae6d6aeedcb
zanst <me@zan.st>
2011-12-02 21:37:33 -0200

    implementing contact actions on PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 class PagesController < ApplicationController
-
   before_filter :authenticate_user!, :only => [:welcome]
+  before_filter :get_contact_infos, :only => [:contact, :send_contact]
 
   def welcome
     @redirect_uri = welcome_path
@@ -9,7 +9,23 @@ class PagesController < ApplicationController
   end
 
   def contact
-    
+    @contact_form = ContactForm.new
   end
 
+  def send_contact
+    @contact_form = ContactForm.new(params[:contact_form])
+    if @contact_form.save
+      flash[:notice] = "Sua mensagem foi enviada com sucesso."
+      redirect_to(contact_path)
+    else
+      render :contact
+    end
+  end
 end
+
+private
+
+def get_contact_infos
+  @contact_infos = ContactInformation.all
+end
+
