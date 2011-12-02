e47b97fe328df627e370dfcc41062bb60822379b
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-02 00:50:49 -0200

    removing line_items_spec.rb

--- spec/models/ecommerce/line_items_spec.rb
+++
@@ -1,7 +0,0 @@
-require "spec_helper"
-
-describe LineItem do
-  it {should validate_presence_of(:order_id) }
-  it {should validate_presence_of(:variant_id) }
-  it {should validate_presence_of(:quantity) }
-end
