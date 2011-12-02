5fd50cfc92c9993a34d4d7f680ecd603d25c447a
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-01 23:44:23 -0200

    generating a identification code fo debits

--- spec/controllers/debits_controller_spec.rb
+++ spec/controllers/debits_controller_spec.rb
@@ -106,12 +106,23 @@ describe DebitsController do
     end
 
     describe "with invalid params" do
-      it "should redirect to debit_path" do
-        @processed_payment = OpenStruct.new(:status => Payment::FAILURE_STATUS, :payment => mock_model(Debit))
-        PaymentBuilder.stub(:new).and_return(payment_builder = mock)
-        payment_builder.should_receive(:process!).and_return(debit = @processed_payment)
-        post :create, :debit => attributes
-        response.should render_template('new')
+      context "when a payment fail" do
+        before :each do
+          processed_payment = OpenStruct.new(:status => Payment::FAILURE_STATUS, :payment => mock_model(CreditCard))
+          payment_builder = mock
+          payment_builder.stub(:process!).and_return(processed_payment)
+          PaymentBuilder.stub(:new).and_return(payment_builder)
+        end
+
+        it "should render new template" do
+          post :create, :debit => attributes
+          response.should render_template('new')
+        end
+
+        it "should generate a identification code" do
+          Order.any_instance.should_receive(:generate_identification_code)
+          post :create, :debit => attributes
+        end
       end
 
       it "should not create a payment" do
