e6a962cef636199a21bdb6d62ea59663f6251ba6
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-01 22:19:45 -0200

    Using layout my_account on user/orders controller

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,13 +1,17 @@
 # -*- encoding : utf-8 -*-
 class User::OrdersController < ApplicationController
+  layout "my_account"
+
   respond_to :html
   before_filter :authenticate_user!
   before_filter :load_user
 
   def index
+    @orders = @user.orders.page(params[:page]).per_page(8)
   end
 
   def show
+    @order = Order.find(params[:id])
   end
 
   private
