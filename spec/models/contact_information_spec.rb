a4c6da09631bcb5aaa99fd80abd5d96f4b070284
zanst <me@zan.st>
2011-12-05 20:19:16 -0200

    spec for ContactInformation model

--- spec/models/contact_information_spec.rb
+++ spec/models/contact_information_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe ContactInformation do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should create a contact information" do
+    ContactInformation.create!(:title => "Lorem ipsum dolor sit amet.", :email => "foo@bar.com")
+  end
 end
