e13a1e6de36a1c6b8c6cb6092b9816e0a24acc93
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-05 19:28:15 -0200

    validations just on create

--- app/models/ecommerce/credit_card.rb
+++ app/models/ecommerce/credit_card.rb
@@ -10,6 +10,8 @@ class CreditCard < Payment
   BirthdayFormat = /^\d{2}\/\d{2}\/\d{4}$/
   ExpirationDateFormat = /^\d{2}\/\d{2}$/
 
+  validates :user_name, :credit_card_number, :security_code, :expiration_date, :user_identification, :telephone, :user_birthday, :presence => true, :on => :create
+
   validates_with CreditCardValidator
 
   state_machine :initial => :started do
