a6873a2e8c956d3251c845a5a3ee40ba5a155267
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-06 19:59:29 -0200

    Changed picture labels on multiple upload

--- app/models/ecommerce/display_picture_on.rb
+++ app/models/ecommerce/display_picture_on.rb
@@ -1,13 +1,13 @@
 # -*- encoding : utf-8 -*-
 class DisplayPictureOn < EnumerateIt::Base
   associate_values(
-    :gallery_1      => [01, 'Gallery 1'],
-    :gallery_2      => [02, 'Gallery 2'],
-    :gallery_3      => [03, 'Gallery 3'],
-    :gallery_4      => [04, 'Gallery 4'],
-    :gallery_5      => [05, 'Gallery 5'],
-    :gallery_6      => [06, 'Gallery 6'],
-    :gallery_7      => [07, 'Gallery 7'],
+    :gallery_1      => [01, '45o / Frente'],
+    :gallery_2      => [02, 'Por trás'],
+    :gallery_3      => [03, 'Sola / Lado'],
+    :gallery_4      => [04, 'Frente / De cima'],
+    :gallery_5      => [05, 'Lado 1'],
+    :gallery_6      => [06, 'Lado 2'],
+    :gallery_7      => [07, 'De cima'],
     :member_banner  => [20, 'Member banner'],
     :visitor_banner => [21, 'Visitor banner']
   )
