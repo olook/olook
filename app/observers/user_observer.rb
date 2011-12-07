a4ea7a29d9d48d8f43a3940a46d93dfc7d0d5349
Hugo Borges <agaelebe@gmail.com>
2011-12-07 20:17:45 -0200

    adding action to enqueue an email notification about the showroom one hour after the user is created

--- app/observers/user_observer.rb
+++ app/observers/user_observer.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class UserObserver < ActiveRecord::Observer
   def after_create(user)
+    Resque.enqueue_in(1.hour, ShowroomReadyNotificationWorker, user.id)
     user.add_event(EventType::SIGNUP)
   end
 end
