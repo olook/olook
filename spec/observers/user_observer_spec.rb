d410b01c547fc4c7558f5504e2184c8e4a42e3ce
Hugo Borges <agaelebe@gmail.com>
2011-12-07 20:19:06 -0200

    moving enqueue action from controller to user observer and created related unit test

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -2,10 +2,17 @@
 require 'spec_helper'
 
 describe UserObserver do
+
   it "should create a signup event when the user is created" do
     user = FactoryGirl.create(:member)
     user.events.where(:event_type => EventType::SIGNUP).any?.should be_true
   end
+
+  it "enqueues a SignupNotificationWorker in resque passing the user id" do
+    Resque.should_receive(:enqueue).with(SignupNotificationWorker, anything)
+    FactoryGirl.create(:member)
+  end
+
   it "enqueues a ShowRoomReadyNotificationWorker email in to be sent in one hour" do
     Resque.should_receive(:enqueue_in).with(1.hour, ShowroomReadyNotificationWorker, anything)
     FactoryGirl.create(:member)
