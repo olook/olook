06b77c5f51db0f0ee6067a96ba11fc81e3155f98
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-07 15:15:52 -0200

    loading order in all pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@
 class PagesController < ApplicationController
 
   before_filter :authenticate_user!, :only => [:welcome]
+  before_filter :load_order
 
   def welcome
     @redirect_uri = welcome_path
