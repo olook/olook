1eaa860ca1fe182e14cd787491b7f048f5f9fe7b
Hugo Borges <agaelebe@gmail.com>
2011-12-07 20:09:08 -0200

    adding showroom ready email to MemberMailer (MISSING: correct email template)

---
+++ spec/mailers/member_mailer_spec.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe MemberMailer do
+  let(:member) { FactoryGirl.create(:member) }
+  let(:mail) { MemberMailer.showroom_ready_email(member) }
+  describe "#showroom_ready_email" do
+
+    it "sets 'to' attribute to passed member's email" do
+      mail.to.should include(member.email)
+    end
+
+    it "sets 'title' attribute describing that the showroom is ready for the user" do
+      mail.subject.should == "#{member.name}, sua vitrine personalizada já está pronta!"
+    end
+
+  end
+end
