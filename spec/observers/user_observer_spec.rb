2565adeb007820dbcb557ecaef1374ba261df767
Hugo Borges <agaelebe@gmail.com>
2011-12-08 14:09:55 -0200

    better description for user observer test

--- spec/observers/user_observer_spec.rb
+++ spec/observers/user_observer_spec.rb
@@ -8,12 +8,12 @@ describe UserObserver do
     user.events.where(:event_type => EventType::SIGNUP).any?.should be_true
   end
 
-  it "enqueues a SignupNotificationWorker in resque passing the user id" do
+  it "enqueues a SignupNotificationWorker in resque" do
     Resque.should_receive(:enqueue).with(SignupNotificationWorker, anything)
     FactoryGirl.create(:member)
   end
 
-  it "enqueues a ShowRoomReadyNotificationWorker email in to be sent in one hour" do
+  it "enqueues a ShowRoomReadyNotificationWorker to be run in one hour with resque" do
     Resque.should_receive(:enqueue_in).with(1.hour, ShowroomReadyNotificationWorker, anything)
     FactoryGirl.create(:member)
   end
