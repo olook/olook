87996c91fbb767a14c074b3373c06547eac09151
zanst <me@zan.st>
2011-12-07 22:58:49 -0200

    changed signin and signout to pt-BR names

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
   add_index "collections", ["end_date"], :name => "index_collections_on_end_date"
   add_index "collections", ["start_date"], :name => "index_collections_on_start_date"
 
+  create_table "contact_informations", :force => true do |t|
+    t.string   "title"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "details", :force => true do |t|
     t.integer  "product_id"
     t.string   "translation_token"
@@ -77,7 +84,7 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
 
   create_table "events", :force => true do |t|
     t.integer  "user_id"
-    t.integer  "event_type",  :null => false
+    t.integer  "event_type",  :limit => 255, :null => false
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -89,13 +96,13 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
 
   create_table "freight_prices", :force => true do |t|
     t.integer  "shipping_service_id"
-    t.integer  "zip_start"
-    t.integer  "zip_end"
-    t.decimal  "order_value_start",   :precision => 8, :scale => 3
-    t.decimal  "order_value_end",     :precision => 8, :scale => 3
+    t.integer  "zip_start",           :limit => 255
+    t.integer  "zip_end",             :limit => 255
+    t.decimal  "order_value_start"
+    t.decimal  "order_value_end"
     t.integer  "delivery_time"
-    t.decimal  "price",               :precision => 8, :scale => 2
-    t.decimal  "cost",                :precision => 8, :scale => 2
+    t.decimal  "price"
+    t.decimal  "cost"
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -178,7 +185,7 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
 
   create_table "pictures", :force => true do |t|
     t.string   "image"
-    t.integer  "display_on"
+    t.integer  "display_on", :limit => 255
     t.integer  "product_id"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -267,6 +274,8 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
     t.datetime "updated_at"
   end
 
+  add_index "survey_answers", ["user_id"], :name => "index_survey_answers_on_user_id"
+
   create_table "users", :force => true do |t|
     t.string   "email"
     t.datetime "created_at"
@@ -304,15 +313,15 @@ ActiveRecord::Schema.define(:version => 20111202022524) do
     t.string   "number"
     t.string   "description"
     t.string   "display_reference"
-    t.decimal  "price",             :precision => 10, :scale => 2
+    t.decimal  "price"
     t.integer  "inventory"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_master"
-    t.decimal  "width",             :precision => 8,  :scale => 2
-    t.decimal  "height",            :precision => 8,  :scale => 2
-    t.decimal  "length",            :precision => 8,  :scale => 2
-    t.decimal  "weight",            :precision => 8,  :scale => 2
+    t.decimal  "width"
+    t.decimal  "height"
+    t.decimal  "length"
+    t.decimal  "weight"
   end
 
   add_index "variants", ["product_id"], :name => "index_variants_on_product_id"
