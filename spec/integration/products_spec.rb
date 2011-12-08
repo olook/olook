c428c5de3dc03f240623f017171b72ccf5169f72
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-08 12:18:07 -0200

    Fixed add to cart integration test, add specs for products without variant

--- spec/integration/products_spec.rb
+++ spec/integration/products_spec.rb
@@ -9,14 +9,13 @@ feature "Buying products", %q{
 } do
 
   let(:user) { FactoryGirl.create(:user) }
-  let(:variant) { FactoryGirl.create(:basic_shoe_size_35) }
-  let(:product) { variant.product }
+  let(:product) { FactoryGirl.create :basic_shoe }
+  let!(:variant_a) { FactoryGirl.create(:basic_shoe_size_35, :product => product) }
 
   context "buying products" do
-
-  background do
-   do_login!(user)
-  end
+    background do
+     do_login!(user)
+    end
 
     context "in the products page" do
       scenario "I want to see product" do
@@ -26,11 +25,27 @@ feature "Buying products", %q{
     end
 
     context "with the shopping cart" do
-      scenario "I want to add a product" do
-        visit product_path(product)
-        choose variant.number
-        click_button "add_product"
-        page.should have_content("Produto adicionado com sucesso")
+      context "and the product has more than one variant" do
+        let!(:variant_b) { FactoryGirl.create(:basic_shoe_size_40, :product => product) }
+        scenario "I need to choose the variant and then add it to the cart" do
+          visit product_path(product)
+          choose variant_a.number
+          click_button "add_product"
+          page.should have_content("Produto adicionado com sucesso")
+        end
+        scenario "If I don't choose a variant and try to add it to the cart, it should tell me I need to pick a size" do
+          visit product_path(product)
+          click_button "add_product"
+          page.should have_content("Selecione um tamanho")
+        end
+      end
+
+      context "and the product has only one variant" do
+        scenario "just need to click 'add to the cart'" do
+          visit product_path(product)
+          click_button "add_product"
+          page.should have_content("Produto adicionado com sucesso")
+        end
       end
     end
   end
