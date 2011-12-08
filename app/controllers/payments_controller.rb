010009d5f100a13d22d344403c9e0b4ecb6b0e8a
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-08 17:13:02 -0200

    loging errors and moip production

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -20,14 +20,16 @@ class PaymentsController < ApplicationController
   end
 
   def create
-    order = Order.find_by_identification_code(params[:id_transacao])
+    order = Order.find_by_identification_code(params["id_transacao"])
     if order
       if update_order(order)
         render :nothing => true, :status => 200
       else
+        logger.error("Ordem nao encontrada")
         render :nothing => true, :status => 500
       end
     else
+      logger.error("Ordem nao encontrada")
       render :nothing => true, :status => 500
     end
   end
@@ -35,6 +37,6 @@ class PaymentsController < ApplicationController
   private
 
   def update_order(order)
-    order.payment.set_state(params[:status_pagamento])
+    order.payment.set_state(params["status_pagamento"])
   end
 end
