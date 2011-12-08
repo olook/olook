c3fb59ce21b9c24a0c15b952b2e393daa61187ea
Hugo Borges <agaelebe@gmail.com>
2011-12-08 18:59:01 -0200

    changing action mailer default email to avisos@olook.com.br

--- config/initializers/config_action_mailer.rb
+++ config/initializers/config_action_mailer.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :user_name            => 'convite@olook.com.br',
+  :user_name            => 'avisos@olook.com.br',
   :password             => 'olook123abc',
   :authentication       => 'plain',
   :enable_starttls_auto => true
