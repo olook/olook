e9bfab5083c23c56eb398d3bd0cc193d9d33f8da
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-09 10:28:47 -0200

    Added price and inventory to product admin

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -274,21 +274,29 @@ describe Product do
       subject.easy_to_find_description.should == 'M123 - Fake product - Black - Bag'
     end
   end
-  
-  describe '#sold_out?' do
+
+  describe 'inventory related methods' do  
     subject { FactoryGirl.create :basic_shoe }
     let!(:basic_shoe_size_35) { FactoryGirl.create :basic_shoe_size_35, :product => subject }
     let!(:basic_shoe_size_40) { FactoryGirl.create :basic_shoe_size_40, :product => subject }
 
-    it "should return false if any of the variants is available" do
-      basic_shoe_size_35.update_attributes(:inventory => 0)
-      basic_shoe_size_40.update_attributes(:inventory => 1)
-      subject.should_not be_sold_out
+    describe '#inventory' do
+      it "should return the sum of the variants inventory" do
+        basic_shoe_size_35.update_attributes(:inventory => 3)
+        basic_shoe_size_40.update_attributes(:inventory => 1)
+        subject.inventory.should == 4
+      end
     end
-    it "should return true if none of the variants is available" do
-      basic_shoe_size_35.update_attributes(:inventory => 0)
-      basic_shoe_size_40.update_attributes(:inventory => 0)
-      subject.should be_sold_out
+    
+    describe '#sold_out?' do
+      it "should return false if any of the variants is available" do
+        subject.stub(:inventory).and_return(2)
+        subject.should_not be_sold_out
+      end
+      it "should return true if none of the variants is available" do
+        subject.stub(:inventory).and_return(0)
+        subject.should be_sold_out
+      end
     end
   end
 end
