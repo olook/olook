33176bea8cd60693a3ba0d9b6213ec745ec94c0b
zanst <me@zan.st>
2011-12-09 00:36:19 -0200

    added after_update method to invalidate the past image on cdn after reuploads

--- app/models/ecommerce/picture.rb
+++ app/models/ecommerce/picture.rb
@@ -1,10 +1,16 @@
 # -*- encoding : utf-8 -*-
 class Picture < ActiveRecord::Base
   belongs_to :product
-  
   validates :product, :presence => true
-
   has_enumeration_for :display_on, :with => DisplayPictureOn, :required => true
-  
   mount_uploader :image, PictureUploader
+  after_update :invalidate_cdn_image
+
+  private
+
+  def invalidate_cdn_image
+    path = self.image.url.slice(23..150)
+    CloudfrontInvalidator.new.invalidate(path)
+  end
 end
+
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -6,13 +6,20 @@ describe Picture do
     it { should validate_presence_of(:product) }
     it { should belong_to(:product) }
   end
+
+  before(:each) do
+    @picture = FactoryGirl.create(:main_picture)
+  end
   
   it "should destroy the associated picture when the product is destroyed" do
-    picture = FactoryGirl.create(:main_picture)
     Product.count.should == 1
     Picture.count.should == 1
-    picture.product.destroy
+    @picture.product.destroy
     Product.count.should be_zero
     Picture.count.should be_zero
+  end
+
+  it "should invalidate the image on Amazon Cloudfront" do
+    mock("CloudfrontInvalidator")
   end
 end
