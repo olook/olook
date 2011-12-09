c906f45fbc1072b4c68d905d1c9936bf70c0b39c
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-09 11:41:13 -0200

    delivering more then on notification

--- app/workers/order_status_worker.rb
+++ app/workers/order_status_worker.rb
@@ -5,7 +5,11 @@ class OrderStatusWorker
   def self.perform(order_id)
     order = Order.find(order_id)
 
-    mail = OrderStatusMailer.order_requested(order)
+    if order.waiting_payment?
+      mail = OrderStatusMailer.order_requested(order)
+    elsif order.completed?
+      mail = OrderStatusMailer.payment_confirmed(order)
+    end
     mail.deliver
   end
 end
--- spec/workers/order_status_worker_spec.rb
+++ spec/workers/order_status_worker_spec.rb
@@ -5,9 +5,16 @@ describe OrderStatusWorker do
   let(:order) { FactoryGirl.create(:clean_order) }
   let(:mock_mail) { double :mail }
 
-  it "should send the invite e-mail given an order id" do
+  it "should send order_requested" do
     mock_mail.should_receive(:deliver)
     OrderStatusMailer.should_receive(:order_requested).with(order).and_return(mock_mail)
+    described_class.perform(order.id)
+  end
+
+  it "should send payment_confirmed" do
+    order.completed
+    mock_mail.should_receive(:deliver)
+    OrderStatusMailer.should_receive(:payment_confirmed).with(order).and_return(mock_mail)
     described_class.perform(order.id)
   end
 end
