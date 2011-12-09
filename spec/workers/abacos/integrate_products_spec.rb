319d5057b130074a8d3b21664cc2ac6a1a1a5c34
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-09 11:26:41 -0200

    Separated Abacos::Price on ProductPrice and VariantPrice

--- spec/workers/abacos/integrate_products_spec.rb
+++ spec/workers/abacos/integrate_products_spec.rb
@@ -15,8 +15,8 @@ describe Abacos::IntegrateProducts do
   describe "#process_products" do
     it 'should download product changes, call parse_abacos_data for received data and enqueue them' do
       Abacos::ProductAPI.should_receive(:download_products).and_return([:product, :variant])
-      described_class.should_receive(:parse_class).with(:product).and_return(Abacos::Product)
-      described_class.should_receive(:parse_class).with(:variant).and_return(Abacos::Variant)
+      described_class.should_receive(:parse_product_class).with(:product).and_return(Abacos::Product)
+      described_class.should_receive(:parse_product_class).with(:variant).and_return(Abacos::Variant)
       
       Abacos::Product.should_receive(:parse_abacos_data).with(:product).and_return(:parsed_product)
       Abacos::Variant.should_receive(:parse_abacos_data).with(:variant).and_return(:parsed_variant)
@@ -39,22 +39,41 @@ describe Abacos::IntegrateProducts do
   
   describe "#process_prices" do
     it 'should download price changes, call parse_abacos_data for received data and enqueue them' do
-      Abacos::ProductAPI.should_receive(:download_prices).and_return([:price])
-      Abacos::Price.should_receive(:parse_abacos_data).with(:price).and_return(:parsed_price)
-      Resque.should_receive(:enqueue).with(Abacos::Integrate, "Abacos::Price", :parsed_price)
+      Abacos::ProductAPI.should_receive(:download_prices).and_return([:product_price, :variant_price])
+      described_class.should_receive(:parse_price_class).with(:product_price).and_return(Abacos::ProductPrice)
+      described_class.should_receive(:parse_price_class).with(:variant_price).and_return(Abacos::VariantPrice)
+      
+      Abacos::ProductPrice.should_receive(:parse_abacos_data).with(:product_price).and_return(:parsed_product_price)
+      Abacos::VariantPrice.should_receive(:parse_abacos_data).with(:variant_price).and_return(:parsed_variant_price)
+      
+      Resque.should_receive(:enqueue).with(Abacos::Integrate, "Abacos::ProductPrice", :parsed_product_price)
+      Resque.should_receive(:enqueue).with(Abacos::Integrate, "Abacos::VariantPrice", :parsed_variant_price)
+
       described_class.process_prices
     end
   end
   
-  describe "#parse_class" do
+  describe "#parse_product_class" do
     let(:downloaded_product) { load_abacos_fixture :product }
     let(:downloaded_variant) { load_abacos_fixture :variant }
 
     it "should return Abacos::Product if the product is a parent" do
-      described_class.send(:parse_class, downloaded_product).should be(Abacos::Product)
+      described_class.send(:parse_product_class, downloaded_product).should be(Abacos::Product)
     end
     it "should return Abacos::Variant if the product isn't a parent" do
-      described_class.send(:parse_class, downloaded_variant).should be(Abacos::Variant)
+      described_class.send(:parse_product_class, downloaded_variant).should be(Abacos::Variant)
+    end
+  end
+
+  describe "#parse_price_class" do
+    let(:downloaded_product_price) { load_abacos_fixture :product_price }
+    let(:downloaded_variant_price) { load_abacos_fixture :variant_price }
+
+    it "should return Abacos::ProductPrice if the product is a parent" do
+      described_class.send(:parse_price_class, downloaded_product_price).should be(Abacos::ProductPrice)
+    end
+    it "should return Abacos::VariantPrice if the product isn't a parent" do
+      described_class.send(:parse_price_class, downloaded_variant_price).should be(Abacos::VariantPrice)
     end
   end
 end
