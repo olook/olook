3da4998fc97189411d41e5d5c25ea01f321b89c3
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-09 13:12:55 -0200

    To access lookbooks, user should be logged

--- spec/controllers/lookbooks_controller_spec.rb
+++ spec/controllers/lookbooks_controller_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe LookbooksController do
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end
+
   describe "GET 'flores'" do
-    context "without a logged user" do
+    context "with a logged user" do
       it "should be successful" do
+        user = Factory :user
+        sign_in user
         get 'flores'
         response.should be_success
+      end
+    end
+
+    context "without a logged user" do
+      it "should be redirected to login" do
+        get 'flores'
+        response.should redirect_to new_user_session_path
       end
     end
   end
