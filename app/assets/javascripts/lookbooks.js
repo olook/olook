24d2cda3a2a19a506152d7b027bbb3dc97e615b6
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-09 12:37:58 -0200

    Lookbook carousel

--- app/assets/javascripts/lookbooks.js
+++ app/assets/javascripts/lookbooks.js
@@ -22,6 +22,26 @@ $(document).ready(function() {
     return false;
   });
 
+  $("div#carousel_lookbooks_product a.arrows").live("click", function () {
+    list = $(this).siblings("ul");
+    listSize = $(list).find("li").size();
+    minWidth = (listSize-1)*(-970);
+    atualPosition = parseInt($(list).css("left").split("px")[0]);
+    if($(this).hasClass("next") == true) {
+      if(atualPosition > minWidth) {
+        $(list).stop().animate({
+          left: atualPosition+(-970)+"px"
+        }, 'fast');
+      }
+    } else {
+      if(atualPosition < 0) {
+        $(list).stop().animate({
+          left: atualPosition+(970)+"px"
+        }, 'fast');
+      }
+    }
+    return false;
+  });
 });
 
 initLookbooks = {
@@ -29,5 +49,8 @@ initLookbooks = {
     listLookbooks = $("div#carousel_lookbooks ul");
     listLookbooksSize = $(listLookbooks).find("li").size()*326;
     $(listLookbooks).width(listLookbooksSize);
+    listLookbookPics = $("div#carousel_lookbooks_product ul");
+    listLookbookPicsSize = $(listLookbookPics).find("li").size()*970;
+    $(listLookbookPics).width(listLookbookPicsSize);
   }
 }
