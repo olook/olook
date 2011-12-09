f12c5fdcdc3805d7aea7bd5d316382a538358d10
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-09 11:58:28 -0200

    JS to lookbooks carousel

--- app/assets/javascripts/lookbooks.js
+++ app/assets/javascripts/lookbooks.js
@@ -1,2 +1,33 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).ready(function() {
+  initLookbooks.updateListSize();
+
+  $("div#carousel_lookbooks a.arrows").live("click", function () {
+    list = $(this).siblings("ul");
+    listSize = $(list).find("li").size();
+    minWidth = (listSize-3)*(-326);
+    atualPosition = parseInt($(list).css("left").split("px")[0]);
+    if($(this).hasClass("next") == true) {
+      if(atualPosition > minWidth) {
+        $(list).stop().animate({
+          left: atualPosition+(-326)+"px"
+        }, 'fast');
+      }
+    } else {
+      if(atualPosition < 0) {
+        $(list).stop().animate({
+          left: atualPosition+(326)+"px"
+        }, 'fast');
+      }
+    }
+    return false;
+  });
+
+});
+
+initLookbooks = {
+  updateListSize :function() {
+    listLookbooks = $("div#carousel_lookbooks ul");
+    listLookbooksSize = $(listLookbooks).find("li").size()*326;
+    $(listLookbooks).width(listLookbooksSize);
+  }
+}
