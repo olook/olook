94748047385104c7eb9168ae7d556e55168cdacd
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-09 22:30:19 -0200

    changing subject

--- app/mailers/order_status_mailer.rb
+++ app/mailers/order_status_mailer.rb
@@ -25,7 +25,20 @@ class OrderStatusMailer < ActionMailer::Base
 
   private
 
+  def build_subject(order)
+    if order.waiting_payment?
+      subject = "#{order.user.first_name}, recebemos seu pedido."
+    elsif order.completed?
+      subject = "Seu pedido #{order.number} foi confirmado!"
+    elsif order.canceled? || order.reversed?
+      subject = "Seu pedido #{order.number} foi cancelado."
+    elsif order.delivered?
+      subject = "Seu pedido #{order.number} foi entregue."
+    end
+  end
+
   def send_mail(order)
-    mail(:to => order.user.email, :subject => "Olook - Status do Pedido")
+    subject = build_subject(order)
+    mail(:to => order.user.email, :subject => subject)
   end
 end
