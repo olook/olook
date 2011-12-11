bb23abe3d31312d31ddb4723948704268a271f1a
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-11 19:06:18 -0200

    Added orders to admin area

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,18 @@
+# -*- encoding : utf-8 -*-
+class Admin::OrdersController < ApplicationController
+  before_filter :authenticate_admin!
+
+  layout "admin"
+  respond_to :html
+
+  def index
+    @orders = Order.with_payment.page(params[:page]).per_page(20)
+    respond_with :admin, @orders
+  end
+
+  def show
+    @order = Order.find(params[:id])
+    @address = @order.freight.address
+    respond_with :admin, @order, @address
+  end
+end
