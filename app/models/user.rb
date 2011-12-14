50cd4b7153d5caf0e65e81ab84c889c5ab2d8ff5
Rafael Rosa Fu <rafaelrosafu@gmail.com>
2011-12-14 11:52:14 -0200

    Sold out products are swaped by non-sold out variations

--- app/models/user.rb
+++ app/models/user.rb
@@ -132,12 +132,24 @@ class User < ActiveRecord::Base
   private
 
   def remove_color_variations(products)
-    already_displayed = []
     result = []
+    already_displayed = []
+    displayed_and_sold_out = {}
+
     products.each do |product|
+      # Only add to the list the products that aren't already shown
       unless already_displayed.include?(product.name)
         result << product
         already_displayed << product.name
+        displayed_and_sold_out[product.name] = result.length - 1 if product.sold_out?
+      else
+        # If a product of the same color was already displayed but was sold out
+        # and the algorithm find another color that isn't, replace the sold out one
+        # by the one that's not sold out
+        if displayed_and_sold_out[product.name] && !product.sold_out?
+          result[displayed_and_sold_out[product.name]] = product
+          displayed_and_sold_out.delete product.name
+        end
       end
     end
     result
