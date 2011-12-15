7af0fbc319718dca6a3ee64325af6a1f0a6466ae
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-15 17:48:21 -0200

    Lookbook with temporary products

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -1,4 +1,8 @@
 class LookbooksController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_order
+
+  def flores
+    @products = Product.find(493, 495, 497, 413, 415, 417, 435, 433, 1, 563, 401, 403, 569)
+  end
 end
