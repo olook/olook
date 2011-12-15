36110a1f627c737ede97c3386d37a899c224593c
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-15 14:00:00 -0200

    Refactor on lookbooks.js

--- app/assets/javascripts/lookbooks.js
+++ app/assets/javascripts/lookbooks.js
@@ -1,26 +1,5 @@
 $(document).ready(function() {
-  $("div#carousel_lookbooks a.arrows").live("click", function () {
-    list = $(this).siblings("ul");
-    listSize = $(list).find("li").size();
-    minWidth = (listSize-3)*(-326);
-    atualPosition = parseInt($(list).css("left").split("px")[0]);
-    if($(this).hasClass("next") == true) {
-      if(atualPosition > minWidth) {
-        $(list).stop().animate({
-          left: atualPosition+(-326)+"px"
-        }, 'fast');
-      }
-    } else {
-      if(atualPosition < 0) {
-        $(list).stop().animate({
-          left: atualPosition+(326)+"px"
-        }, 'fast');
-      }
-    }
-    return false;
-  });
-
-  $("div#carousel_lookbooks_product ul").jcarousel({
+  $("div#carousel_lookbooks ul, div#carousel_lookbooks_product ul").jcarousel({
     scroll: 1
   });
 });
