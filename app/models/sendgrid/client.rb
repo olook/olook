10d07c50d7ba6fcb531fbaaf5008df4c25519907
Hugo Borges <agaelebe@gmail.com>
2011-12-15 18:39:13 -0200

    adding services to sendgrid client to get different kind of bounced emails

---
+++ app/models/sendgrid/client.rb
@@ -0,0 +1,28 @@
+# -*- encoding : utf-8 -*-
+
+module Sendgrid
+  class Client
+
+    BASE_URL = "https://sendgrid.com/api"
+    API_USER = "olook"
+    API_KEY = "olook123abc"
+    SERVICES = {
+                 :invalid_emails => "invalidemails.get.json",
+                 :spam_reports => "spamreports.get.json",
+                 :unsubscribes => "unsubscribes.get.json",
+                 :blocks => "blocks.get.json"
+               }
+
+    def initialize(name)
+      raise ArgumentError, "Service #{name} is not supported" unless SERVICES.include?(name)
+
+      @request = HTTPI::Request.new
+      @request.url = "#{BASE_URL}/#{SERVICES[name]}?api_user=#{API_USER}&api_key=#{API_KEY}"
+      @response = HTTPI.get(@request)
+    end
+
+    def parsed_response
+      JSON::parse(@response.body)
+    end
+  end
+end
---
+++ spec/models/sendgrid/client_spec.rb
@@ -0,0 +1,74 @@
+# -*- encoding : utf-8 -*-
+
+require 'spec_helper'
+
+describe Sendgrid::Client do
+  subject { Sendgrid::Client.new(:invalid_emails) }
+
+  describe  "#initialize" do
+    let(:request){ double :request }
+
+    it "creates new HTTPI request" do
+      request = mock.as_null_object
+      HTTPI.stub(:get)
+
+      HTTPI::Request.should_receive(:new).and_return(request)
+      Sendgrid::Client.new(:invalid_emails)
+    end
+
+    it "sets request url using sendgrid domain, user_key, api_key and service name" do
+      HTTPI::Request.stub(:new).and_return(request)
+      HTTPI.stub(:get)
+
+      request.should_receive(:url=).with("https://sendgrid.com/api/invalidemails.get.json?api_user=olook&api_key=olook123abc")
+      Sendgrid::Client.new(:invalid_emails)
+    end
+
+    it "makes a get with the created request" do
+      HTTPI::Request.stub(:new).and_return(request)
+      request.stub(:url=)
+
+      HTTPI.should_receive(:get).with(request)
+      Sendgrid::Client.new(:invalid_emails)
+    end
+  end
+
+  describe "suported services" do
+    before { HTTPI.stub(:get) }
+
+    it "raises invalid error message if service is not supported" do
+      expect {
+        Sendgrid::Client.new(:unsupported_service)
+        }.to raise_error(ArgumentError, "Service unsupported_service is not supported")
+    end
+
+    it "supports invalid_emails as a service" do
+      expect { Sendgrid::Client.new(:invalid_emails) }.to_not raise_error
+    end
+
+    it "supports blocks service" do
+      expect { Sendgrid::Client.new(:blocks) }.to_not raise_error
+    end
+
+    it "supports spam reports service" do
+      expect { Sendgrid::Client.new(:spam_reports) }.to_not raise_error
+    end
+
+    it "supports unsubscribes service" do
+      expect { Sendgrid::Client.new(:unsubscribes) }.to_not raise_error
+    end
+  end
+
+  describe "#parsed_response" do
+    it "returns JSON response body as a ruby hash" do
+      json_response_body = "[{\"reason\": \"Known bad domain\", \"email\": \"niceivanice@homail.com\"},{\"reason\":" +
+                      "\"Known bad domain\", \"email\": \"william_fi_ude@hotmai.com\"}]"
+      parsed_response = [ {"reason"=>"Known bad domain", "email"=>"niceivanice@homail.com"},
+                               {"reason"=>"Known bad domain", "email"=>"william_fi_ude@hotmai.com"} ]
+      response = double(:response, :body => json_response_body)
+      HTTPI.stub(:get).and_return(response)
+
+      subject.parsed_response.should == parsed_response
+    end
+  end
+end
