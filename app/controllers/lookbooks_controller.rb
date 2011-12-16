bac043abed722278f345991105e53673f5f337ba
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-16 03:15:23 -0200

    Removed some products of flores lookbook

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -3,7 +3,7 @@ class LookbooksController < ApplicationController
   before_filter :load_order
 
   def flores
-    @products = Product.find(493, 495, 497, 413, 415, 417, 435, 433, 1, 563, 401, 403, 569)
+    @products = Product.find(493, 417, 435, 1, 563, 401, 403, 569)
   end
 
   def lets_party
