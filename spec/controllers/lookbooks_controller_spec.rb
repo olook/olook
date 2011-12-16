68463e278d8b0ad2a2d226bd745878d95bfe4c81
Hugo Borges <agaelebe@gmail.com>
2011-12-16 12:51:52 -0200

    fixing test stubing Product.find call

--- spec/controllers/lookbooks_controller_spec.rb
+++ spec/controllers/lookbooks_controller_spec.rb
@@ -5,11 +5,17 @@ describe LookbooksController do
     request.env['devise.mapping'] = Devise.mappings[:user]
   end
 
+  let(:products) { [:product_a, :product_b]}
+
   describe "GET 'flores'" do
     context "with a logged user" do
-      it "should be successful" do
+      before do
         user = Factory :user
         sign_in user
+      end
+
+      it "should be successful" do
+        Product.stub(:find).and_return(products)
         get 'flores'
         response.should be_success
       end
