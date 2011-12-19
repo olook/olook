c62a18489bc5ee66626635bf2a75e033b226d1d2
Hugo Borges <agaelebe@gmail.com>
2011-12-19 14:18:44 -0200

    Sendgrid client now using xml services instead of json

--- lib/email_marketing/sendgrid_client.rb
+++ lib/email_marketing/sendgrid_client.rb
@@ -7,22 +7,26 @@ module EmailMarketing
     API_USER = "olook"
     API_KEY = "olook123abc"
     SERVICES = {
-                 :invalid_emails => "invalidemails.get.json",
-                 :spam_reports => "spamreports.get.json",
-                 :unsubscribes => "unsubscribes.get.json",
-                 :blocks => "blocks.get.json"
+                 :invalid_emails => "invalidemails.get.xml",
+                 :spam_reports => "spamreports.get.xml",
+                 :unsubscribes => "unsubscribes.get.xml",
+                 :blocks => "blocks.get.xml"
                }
+
+    attr_reader :request, :response
 
     def initialize(name)
       raise ArgumentError, "Service #{name} is not supported" unless SERVICES.include?(name)
-
+      @service_name = name
       @request = HTTPI::Request.new
       @request.url = "#{BASE_URL}/#{SERVICES[name]}?api_user=#{API_USER}&api_key=#{API_KEY}"
       @response = HTTPI.get(@request)
     end
 
     def parsed_response
-      JSON::parse(@response.body)
+      response_hash = Hash.from_xml(@response.body)
+      name = @service_name.to_s.delete("_")
+      response_hash[name][name.chop] if response_hash
     end
   end
 end
