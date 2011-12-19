f70684a6d253891203fea21bdf112b2f3f570b70
Hugo Borges <agaelebe@gmail.com>
2011-12-19 14:18:13 -0200

    correcting test

--- spec/lib/email_marketing/csv_uploader_spec.rb
+++ spec/lib/email_marketing/csv_uploader_spec.rb
@@ -5,6 +5,10 @@ require 'spec_helper'
 describe EmailMarketing::CsvUploader do
 
   describe "#initialize" do
+    context "when type is invalid" do
+
+    end
+
     it "sets csv to empty string" do
       subject.csv.should == ""
     end
@@ -39,19 +43,34 @@ describe EmailMarketing::CsvUploader do
     let(:connection) { mock(:ftp_connection) }
 
     it "opens new ftp connection using ftp server config" do
-      Net::FTP.should_receive(:new).with(EmailMarketing::EmailListUploader::FTP_SERVER[:host],
-                                         EmailMarketing::EmailListUploader::FTP_SERVER[:username],
-                                         EmailMarketing::EmailListUploader::FTP_SERVER[:password]).and_return(mock.as_null_object)
+      Net::FTP.should_receive(:new).with(EmailMarketing::CsvUploader::FTP_SERVER[:host],
+                                         EmailMarketing::CsvUploader::FTP_SERVER[:username],
+                                         EmailMarketing::CsvUploader::FTP_SERVER[:password]).and_return(mock.as_null_object)
       subject.copy_to_ftp(anything)
     end
 
-    it "copies the file with the received filename" do
-      filename = "file.csv"
-      Net::FTP.stub(:new).and_return(connection)
-      connection.stub(:close)
+    context "setting file name" do
+      it "uses emails.csv as filename when no name is passed" do
+        filename = "file.csv"
+        Net::FTP.stub(:new).and_return(connection)
+        connection.stub(:close)
 
-      connection.should_receive(:puttextfile).with(anything, filename)
-      subject.copy_to_ftp(filename)
+        connection.should_receive(:puttextfile).with(anything, "emails.csv")
+        subject.copy_to_ftp
+      end
+
+      it "uses received name as filename" do
+        filename = "file.csv"
+        Net::FTP.stub(:new).and_return(connection)
+        connection.stub(:close)
+
+        connection.should_receive(:puttextfile).with(anything, filename)
+        subject.copy_to_ftp(filename)
+      end
+    end
+
+    it "copies the ftp " do
+
     end
 
     it "closes the ftp connection" do
