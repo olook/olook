d2abbbaf24991f4f891c374d1dd53100871797eb
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-20 11:26:47 -0200

    enqueueing just when exist a payment

--- app/workers/order_status_worker.rb
+++ app/workers/order_status_worker.rb
@@ -7,9 +7,9 @@ class OrderStatusWorker
     send_email(order)
     integrate_with_abacos(order)
   end
-  
+
   def self.send_email(order)
-    if order.waiting_payment? && order.payment
+    if order.waiting_payment?
       mail = OrderStatusMailer.order_requested(order)
     elsif order.authorized?
       mail = OrderStatusMailer.payment_confirmed(order)
@@ -20,12 +20,12 @@ class OrderStatusWorker
     end
     mail.deliver if mail
   end
-  
+
   def self.integrate_with_abacos(order)
-    if order.waiting_payment? && order.payment
+    if order.waiting_payment?
       Resque.enqueue(Abacos::InsertOrder, order.number)
     elsif order.authorized?
       Resque.enqueue_in(10.minutes, Abacos::ConfirmPayment, order.number)
     end
-  end  
+  end
 end
