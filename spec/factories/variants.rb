18956dc0ec2e5a82f7419db7fb2fc17207ef9bb1
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-21 11:23:20 -0200

    refactoring

--- spec/factories/variants.rb
+++ spec/factories/variants.rb
@@ -14,7 +14,7 @@ FactoryGirl.define do
 
     price 0.0
     inventory 0
-    
+
     factory :basic_shoe_size_35 do
       number '35'
       description 'size 35'
@@ -23,11 +23,19 @@ FactoryGirl.define do
       inventory 10
     end
 
+    factory :basic_shoe_size_37 do
+      number '37A'
+      description 'size 37'
+      display_reference 'size-37'
+      price 124.19
+      inventory 5
+    end
+
     factory :basic_shoe_size_40 do
       number '40A'
       description 'size 40'
       display_reference 'size-45'
-      price 123.45
+      price 125.45
       inventory 5
     end
   end
