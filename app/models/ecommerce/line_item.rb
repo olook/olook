1839ce202ed8d4f2246f5a474d109fdea66d8c11
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-21 10:31:27 -0200

    returning the line_item.price considering the gift

--- app/models/ecommerce/line_item.rb
+++ app/models/ecommerce/line_item.rb
@@ -1,11 +1,15 @@
 class LineItem < ActiveRecord::Base
+  PRICE_FOR_GIFT = 0.01
   belongs_to :variant
   belongs_to :order
   validates_presence_of :variant_id
   validates_presence_of :order_id
   validates_presence_of :quantity
-
   scope :ordered_by_price, order('line_items.price DESC')
+
+  def price
+    (self.gift?) ? PRICE_FOR_GIFT : read_attribute(:price)
+  end
 
   def total_price
     price * quantity
