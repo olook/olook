27a22936df6e9f6e8aeb950ec5881d6eec9b30b1
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-21 10:31:27 -0200

    returning the line_item.price considering the gift

--- spec/models/ecommerce/line_item_spec.rb
+++ spec/models/ecommerce/line_item_spec.rb
@@ -5,10 +5,23 @@ describe LineItem do
   it {should validate_presence_of(:variant_id) }
   it {should validate_presence_of(:quantity) }
 
+  before :each do
+    @quantity = 2
+    @variant = FactoryGirl.create(:basic_shoe_size_35)
+    @line_item = FactoryGirl.create(:line_item, :variant => @variant, :quantity => @quantity, :price => @variant.price)
+    @normal_price = @line_item.price
+  end
+
   it "should return the total_price" do
-    quantity = 2
-    variant = FactoryGirl.create(:basic_shoe_size_35)
-    line_item = FactoryGirl.create(:line_item, :variant => variant, :quantity => quantity, :price => variant.price)
-    line_item.total_price.should == variant.price * quantity
+    @line_item.total_price.should == @variant.price * @quantity
+  end
+
+  it "should return the normal price" do
+    @line_item.price.should == @normal_price
+  end
+
+  it "should return the gift price" do
+    @line_item.update_attributes(:gift => true)
+    @line_item.price.should == LineItem::PRICE_FOR_GIFT
   end
 end
