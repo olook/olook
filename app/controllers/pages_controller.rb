fedca9a2710de5a340f63b8d8e79612f86524688
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-22 17:05:16 -0200

    Added christmas promo page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class PagesController < ApplicationController
-  before_filter :authenticate_user!, :only => [:welcome]
+  before_filter :authenticate_user!, :only => [:welcome, :christmas]
   before_filter :load_order
   before_filter :get_contact_infos, :only => [:contact, :send_contact]
 
