e4fce2aa818ccb34891fc37d451cb06d6c2f5c9c
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2011-12-22 00:40:12 -0200

    filtering parameters

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,7 @@ module Olook
     config.encoding = "utf-8"
 
     # Configure sensitive parameters which will be filtered from the log file.
-    config.filter_parameters += [:password, :password_confirmation]
+    config.filter_parameters += [:password, :password_confirmation, :credit_card_number, :security_code, :expiration_date, :user_identification, :telephone]
 
     # Enable the asset pipeline
     config.assets.enabled = true
