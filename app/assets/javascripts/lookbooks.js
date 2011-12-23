71d31771ae1a2b7e508fe2f0f079eca857fde3b6
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-23 16:03:15 -0200

    Make carousel on lookbooks infinite and with keyboard functional

--- app/assets/javascripts/lookbooks.js
+++ app/assets/javascripts/lookbooks.js
@@ -1,5 +1,27 @@
 $(document).ready(function() {
-  $("div#carousel_lookbooks ul, div#carousel_lookbooks_product ul").jcarousel({
-    scroll: 1
+  $("div#carousel_lookbooks ul").carouFredSel({
+    auto: false,
+    width: 970,
+    items: 3,
+    prev : {
+      button : ".prev-lookbook",
+      items : 1
+    },
+    next : {
+      button : ".next-lookbook",
+      items : 1
+    }
+  });
+
+  $("div#carousel_lookbooks_product ul").carouFredSel({
+    auto: false,
+    prev : {
+      button : ".prev-pic",
+      key : "left"
+    },
+    next : {
+      button : ".next-pic",
+      key : "right"
+    }
   });
 });
