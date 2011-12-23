18587eaa4786f394fcd851912393090cfc645101
Hugo Borges <agaelebe@gmail.com>
2011-12-23 10:58:05 -0200

    making criteo controller simple, as the customization of the xml is now done on product model and not on controller

--- app/controllers/criteo_controller.rb
+++ app/controllers/criteo_controller.rb
@@ -1,19 +1,9 @@
 class CriteoController < ApplicationController
+  respond_to :xml
+
   def show
-    instock = Proc.new { |options, record| options[:builder].tag!('instock', !record.sold_out?) }
-    producturl = Proc.new { |options, record| options[:builder].tag!('producturl', product_url(record)) }
-    bigimage = Proc.new { |options, record| options[:builder].tag!('bigimage', record.suggestion_picture) }
-    smallimage = Proc.new { |options, record| options[:builder].tag!('smallimage', record.thumb_picture) }
-
-
-
-    @products = Product.only_visible.all
-    respond_to do |format|
-      format.xml do
-        render :xml => @products.to_xml(:skip_types => true, :methods => [:price],
-                                        :only => [:name, :description, :id],
-                                        :procs => [instock, producturl, bigimage, smallimage])
-      end
-    end
+    @products = Product.only_visible
+    respond_with(@products, :skip_types => true)
   end
+
 end
--- spec/controllers/criteo_controller_spec.rb
+++ spec/controllers/criteo_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe CriteoController do
+  describe "GET show" do
 
+    let!(:product) { FactoryGirl.create(:basic_shoe) }
+
+    it "gets only visible products" do
+      Product.should_receive(:only_visible)
+      get :show
+    end
+
+    it "assigns all visible products to @products" do
+      get :show
+      assigns(:products).should include(product)
+    end
+  end
 end
