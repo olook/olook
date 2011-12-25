19b7d9685a8b9b6f8a551ef83881a0b45c526f48
zanst <me@zan.st>
2011-12-25 17:32:54 -0200

    Created yaml file to MoIP setup; Moved setup from environments to initializer.

---
+++ config/initializers/moip.rb
@@ -0,0 +1,11 @@
+# -*- encoding: utf-8 -*-
+moip_config_path = File.dirname(__FILE__) + '/../../config/moip.yml'
+moip_config = YAML.load_file(moip_config_path)
+
+(Rails.env == 'production') ? @moip_config = moip_config['production'] : @moip_config = moip_config['development']
+
+MoIP.setup do |config|
+  config.uri   = @moip_config['uri'] if true
+  config.token = @moip_config['token']
+  config.token = @moip_config['key']
+end
