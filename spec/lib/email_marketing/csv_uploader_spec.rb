42a994586e9e268cc08a77d858b2e8560bfbf61b
Hugo Borges <agaelebe@gmail.com>
2011-12-28 11:38:20 -0200

    refactoring tests

--- spec/lib/email_marketing/csv_uploader_spec.rb
+++ spec/lib/email_marketing/csv_uploader_spec.rb
@@ -3,6 +3,18 @@
 require 'spec_helper'
 
 describe EmailMarketing::CsvUploader do
+  let(:user_a) { FactoryGirl.create :user }
+  let(:user_b) { FactoryGirl.create :user }
+  let(:user_c) { FactoryGirl.create :user }
+  let(:response) { double(:response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"teste@teste.com"} ]) }
+  let(:services) do
+    {
+      :spam_reports   => double(:s_response, :parsed_response => [ {"reason"=>"Any", "email"=> "a@b.com"} ]),
+      :unsubscribes   => double(:u_response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"g@h.com"} ]),
+      :blocks         => double(:b_response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"e@f.com"} ]),
+      :invalid_emails => double(:i_response, :parsed_response => [ {"reason"=>"500", "email"=>"c@d.com"} ]),
+    }
+  end
 
   describe "#initialize" do
     context "when no type is passed" do
@@ -20,96 +32,73 @@ describe EmailMarketing::CsvUploader do
     end
 
     it "builds a csv file with invalid emails" do
-      response_hash = [
-                        {"reason"=>"Known bad domain", "email"=>"niceivanice@homail.com"},
-                        {"reason"=>"Known bad domain", "email"=>"william_fi_ude@hotmai.com"}
-                      ]
+      EmailMarketing::SendgridClient.stub(:new).with(:invalid_emails).and_return(services[:invalid_emails])
 
-      response = double(:response, :parsed_response => response_hash)
-      EmailMarketing::SendgridClient.stub(:new).with(:invalid_emails).and_return(response)
-
-      EmailMarketing::CsvUploader.new(:invalid).csv.should == "niceivanice@homail.com\nwilliam_fi_ude@hotmai.com\n"
+      EmailMarketing::CsvUploader.new(:invalid).csv.should == "c@d.com\n"
     end
   end
 
   describe "when type is equal to optout" do
+    let(:optout_services) { [:spam_reports, :unsubscribes, :blocks] }
 
     it "calls sendgrid spam_reports, unsubscribes and blocks services" do
-      response = double(:response, :parsed_response => [{"email"=>"teste@teste.com"}])
-
-      EmailMarketing::SendgridClient.should_receive(:new).with(:spam_reports).and_return(response)
-      EmailMarketing::SendgridClient.should_receive(:new).with(:unsubscribes).and_return(response)
-      EmailMarketing::SendgridClient.should_receive(:new).with(:blocks).and_return(response)
+      optout_services.each { |name| EmailMarketing::SendgridClient.should_receive(:new).with(name).and_return(services[name]) }
 
       EmailMarketing::CsvUploader.new(:optout)
     end
 
     it "builds a csv file with emails from spam_reports, unsubscribes and blocks" do
-      spam = [ {"reason"=>"Known bad domain", "email"=>"niceivanice@homail.com"} ]
-      unsubscribes =  [ {"reason"=>"500", "email"=>"william_fi_ude@yahoo.com"} ]
-      blocks =  [ {"reason"=>"Unknown", "email"=>"rinaldi.fonseca@gmail.com"} ]
+      optout_services.each { |name| EmailMarketing::SendgridClient.stub(:new).with(name).and_return(services[name]) }
 
-      spam_response = double(:response, :parsed_response => spam)
-      unsubscribes_response = double(:response, :parsed_response => unsubscribes)
-      blocks_reponse = double(:response, :parsed_response => blocks)
-
-      EmailMarketing::SendgridClient.stub(:new).with(:spam_reports).and_return(spam_response)
-      EmailMarketing::SendgridClient.stub(:new).with(:unsubscribes).and_return(unsubscribes_response)
-      EmailMarketing::SendgridClient.stub(:new).with(:blocks).and_return(blocks_reponse)
-
-      EmailMarketing::CsvUploader.new(:optout).csv.should == "niceivanice@homail.com\nwilliam_fi_ude@yahoo.com\nrinaldi.fonseca@gmail.com\n"
+      EmailMarketing::CsvUploader.new(:optout).csv.should == "a@b.com\ng@h.com\ne@f.com\n"
     end
   end
 
   context "when type is equal to userbase" do
-    let(:user_a) { FactoryGirl.create :user }
-    let(:user_b) { FactoryGirl.create :user }
-    let(:user_c) { FactoryGirl.create(:user, :email => "a@b.com") }
-    let(:response) { double(:response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"teste@teste.com"} ]) }
+    let(:csv_header) do
+      "id,email,created_at,sign_in_count,current_sign_in_at,last_sign_in_at,invite_token,first_name,last_name,facebook_token,birthday\n"
+    end
 
     it "builds a csv file containing all user data" do
-      [:spam_reports, :unsubscribes, :blocks, :invalid_emails].each do |type|
-        EmailMarketing::SendgridClient.stub(:new).with(type).and_return(response)
-      end
+      services.each { |service, response| EmailMarketing::SendgridClient.stub(:new).with(service).and_return(response) }
 
-      csv_header = "id,email,created_at,sign_in_count,current_sign_in_at,last_sign_in_at,invite_token,first_name,last_name,facebook_token,birthday\n"
-      csv_data = [user_a, user_b, user_c].inject("") do |data,user|
+      csv_body = [user_a, user_b, user_c].inject("") do |data, user|
         data += "#{user.id},#{user.email},#{user.created_at},#{user.sign_in_count},#{user.current_sign_in_at},#{user.last_sign_in_at},#{user.invite_token},#{user.first_name},#{user.last_name},#{user.facebook_token},#{user.birthday}\n"
         data
       end
 
-      EmailMarketing::CsvUploader.new(:userbase).csv.should == csv_header + csv_data
+      EmailMarketing::CsvUploader.new(:userbase).csv.should == csv_header + csv_body
     end
 
     it "does not include a user which email was included in any bounced list (spam reports, unsubscribers, blocks or invalid)" do
-      invalids_response = double(:response, :parsed_response => [ {"reason"=>"500", "email"=>"c@d.com"} ])
-      spam_response = double(:response, :parsed_response => [ {"reason"=>"Known bad domain", "email"=> user_c.email} ])
-      unsubscribes_response = double(:response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"g@h.com"} ])
-      blocks_reponse = double(:response, :parsed_response => [ {"reason"=>"Unknown", "email"=>"e@f.com"} ])
 
-      EmailMarketing::SendgridClient.should_receive(:new).with(:spam_reports).and_return(invalids_response)
-      EmailMarketing::SendgridClient.should_receive(:new).with(:unsubscribes).and_return(spam_response)
-      EmailMarketing::SendgridClient.should_receive(:new).with(:blocks).and_return(unsubscribes_response)
-      EmailMarketing::SendgridClient.should_receive(:new).with(:invalid_emails).and_return(blocks_reponse)
+      services = {
+        :spam_reports   => double(:s_response, :parsed_response => [ {"reason" => "Any", "email" => user_a.email } ]),
+        :unsubscribes   => double(:u_response, :parsed_response => [ {"reason" => "Unknown", "email" => user_b.email } ]),
+        :blocks         => double(:b_response, :parsed_response => [ {"reason" => "Unknown", "email" => user_c.email } ]),
+        :invalid_emails => double(:i_response, :parsed_response => [ {"reason" => "500", "email" => user_c.email } ])
+      }
 
-      csv_header = "id,email,created_at,sign_in_count,current_sign_in_at,last_sign_in_at,invite_token,first_name,last_name,facebook_token,birthday\n"
-      csv_data = [user_a, user_b].inject("") do |data,user|
-        data += "#{user.id},#{user.email},#{user.created_at},#{user.sign_in_count},#{user.current_sign_in_at},#{user.last_sign_in_at},#{user.invite_token},#{user.first_name},#{user.last_name},#{user.facebook_token},#{user.birthday}\n"
-        data
-      end
-      EmailMarketing::CsvUploader.new(:userbase).csv.should == csv_header + csv_data
+      services.each { |service, response| EmailMarketing::SendgridClient.stub(:new).with(service).and_return(response) }
+
+      EmailMarketing::CsvUploader.new(:userbase).csv.should == csv_header
     end
-
   end
 
-
   describe "#copy_to_ftp" do
+    before do
+      EmailMarketing::CsvUploader.class_eval { remove_const :FTP_SERVER }
+      EmailMarketing::CsvUploader::FTP_SERVER = {
+        :host => "ftp.host.com",
+        :username => "username",
+        :password => "password"
+      }
+    end
+
     let(:connection) { mock(:ftp_connection) }
 
-    it "opens new ftp connection using ftp server config" do
-      Net::FTP.should_receive(:new).with(EmailMarketing::CsvUploader::FTP_SERVER[:host],
-                                         EmailMarketing::CsvUploader::FTP_SERVER[:username],
-                                         EmailMarketing::CsvUploader::FTP_SERVER[:password]).and_return(mock.as_null_object)
+    it "opens new ftp connection using configuration from EmailMarketing::CsvUploader::FTP_SERVER" do
+      Net::FTP.should_receive(:new).with("ftp.host.com","username", "password").and_return(mock.as_null_object)
       subject.copy_to_ftp(anything)
     end
 
@@ -133,13 +122,12 @@ describe EmailMarketing::CsvUploader do
     end
 
     it "sets connection to passive mode" do
-      filename = "file.csv"
       Net::FTP.stub(:new).and_return(connection)
       connection.stub(:close)
       connection.stub(:puttextfile)
 
       connection.should_receive(:passive=).with(true)
-      subject.copy_to_ftp(filename)
+      subject.copy_to_ftp
     end
 
     it "closes the ftp connection" do
@@ -148,7 +136,7 @@ describe EmailMarketing::CsvUploader do
       connection.stub(:passive=)
 
       connection.should_receive(:close)
-      subject.copy_to_ftp(anything)
+      subject.copy_to_ftp
     end
   end
 end
