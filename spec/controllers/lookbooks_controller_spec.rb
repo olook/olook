daf68fc36cd14b13928c92c384f118b91fb2f539
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-27 23:27:44 -0200

    Added palha lookbook

--- spec/controllers/lookbooks_controller_spec.rb
+++ spec/controllers/lookbooks_controller_spec.rb
@@ -3,6 +3,22 @@ require 'spec_helper'
 describe LookbooksController do
   let(:products) { [:product_a, :product_b]}
 
+  describe "GET 'palha'" do
+    context "without a logged user" do
+      it "assigns @products to found products" do
+        Product.should_receive(:find).and_return(products)
+        get 'palha'
+        assigns(:products).should == products
+      end
+
+      it "should be succesfull" do
+        Product.stub(:find).and_return(products)
+        get 'palha'
+        response.should be_success
+      end
+    end
+  end
+
   describe "GET 'glitter'" do
     context "without a logged user" do
       it "assigns @products to found products" do
