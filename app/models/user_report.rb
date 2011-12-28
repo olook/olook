6b42e77b4cefd654de3157c8413bab942c04d166
Arthur Zapparoli <arthurzap@gmail.com>
2011-12-28 19:34:21 -0200

    Implemented UserReport#generate_csv which generates a CSV file with all Users from database

--- app/models/user_report.rb
+++ app/models/user_report.rb
@@ -1,5 +1,21 @@
 # -*- encoding : utf-8 -*-
 module UserReport
+  def self.generate_csv
+    attributes = [:first_name, :last_name, :email, :cpf, :is_invited, :created_at]
+    file_name = "users-#{Time.now.strftime("%Y-%m-%d_%H-%M")}.csv"
+
+    CSV.open(Rails.root.join("public", "admin", file_name), "w") do |csv|
+      # CSV header
+      csv << attributes.collect { |attr| attr.to_s.titleize }
+
+      User.select(attributes).find_each do |user|
+        csv << attributes.collect { |attr| user.send(attr) }
+      end
+    end
+
+    file_name
+  end
+
   def self.statistics
     User.select("date(created_at) as creation_date, count(id) as daily_total").group("date(created_at)")
   end
--- spec/models/user_report_spec.rb
+++ spec/models/user_report_spec.rb
@@ -2,7 +2,39 @@
 require 'spec_helper'
 
 describe UserReport do
-  describe '#statistics' do
+  describe ".generate_csv" do
+    let(:first_user) { double(:user, FactoryGirl.attributes_for(:user, :cpf => "123456", :is_invited => false, :created_at => Date.today)) }
+    let(:second_user) { double(:user, FactoryGirl.attributes_for(:user, :cpf => "654321", :is_invited => true, :created_at => Date.yesterday)) }
+    let(:formatted_time) { "2011-31-12_00-00" }
+    let(:file_name) { "users-#{formatted_time}.csv" }
+    let(:file_path) { Rails.root.join("public", "admin", file_name) }
+
+    before do
+      User.stub_chain(:select, :find_each).and_yield(first_user).and_yield(second_user)
+      Time.stub_chain(:now, :strftime).and_return(formatted_time)
+    end
+
+    after(:all) do
+      File.delete file_path
+    end
+
+    it "should generate a CSV file in public/admin containing all Users in the database" do
+      csv_string = <<-CSV.strip_heredoc
+        First Name,Last Name,Email,Cpf,Is Invited,Created At
+        #{first_user.first_name},#{first_user.last_name},#{first_user.email},#{first_user.cpf},#{first_user.is_invited},#{first_user.created_at}
+        #{second_user.first_name},#{second_user.last_name},#{second_user.email},#{second_user.cpf},#{second_user.is_invited},#{second_user.created_at}
+      CSV
+
+      UserReport.generate_csv
+      File.read(file_path).should == csv_string
+    end
+
+    it "should return the file name" do
+      UserReport.generate_csv.should == file_name
+    end
+  end
+
+  describe '.statistics' do
     let(:result_a) { double(:result, creation_date: Date.civil(2011, 11, 15), daily_total: 10 ) }
     let(:result_b) { double(:result, creation_date: Date.civil(2011, 11, 16), daily_total: 13 ) }
     let(:group_result) { [ result_a, result_b ] }
