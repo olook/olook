248d00872c20c639a7b6f583390a1d2515c1de5d
Raul Souza Lima <raulsouzalima@gmail.com>
2011-12-29 15:12:35 -0200

    Created my friends showroom page

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,4 +1,9 @@
 class FriendsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_user
+
+  def showroom
+    @products = []
+    15.times { @products << Product.all.shuffle.first }
+  end
 end
