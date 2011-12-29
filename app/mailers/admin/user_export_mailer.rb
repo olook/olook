a540c75df9516a15124bdec8859bbd0db266638f
Arthur Zapparoli <arthurzap@gmail.com>
2011-12-29 12:26:45 -0200

    Added UserExportMailer which send's an e-mail file the CSV download link

---
+++ app/mailers/admin/user_export_mailer.rb
@@ -0,0 +1,9 @@
+# -*- encoding : utf-8 -*-
+class Admin::UserExportMailer < ActionMailer::Base
+  default :from => "Admin Olook <admin@olook.com.br>"
+
+  def csv_ready(email, file_name)
+    @file_url = "http://olook.com.br/admin/#{file_name}"
+    mail(:to => email, :subject => "Arquivo CSV com os usuários está pronto")
+  end
+end
