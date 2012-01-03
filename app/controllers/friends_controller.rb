503ab22f09b540b0f99cec4d6917dc4da3d38e70
Raul Souza Lima <raulsouzalima@gmail.com>
2012-01-03 16:51:48 -0200

    Update users on 'minhas amigas' page

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -3,7 +3,23 @@ class FriendsController < ApplicationController
   before_filter :load_user
 
   def showroom
+    assign_facebook_friends
     @products = []
     15.times { @products << Product.all.shuffle.first }
+  end
+
+  def home
+    assign_facebook_friends
+  end
+
+  def update_friends_list
+    assign_facebook_friends
+  end
+
+  private
+
+  def assign_facebook_friends
+    @friends = []
+    7.times { @friends << User.where("id < ?", 100).shuffle.first }
   end
 end
