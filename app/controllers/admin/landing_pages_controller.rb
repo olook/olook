4e7cde23469598595c42c88ef7671524abc22bf6
Hugo Borges <agaelebe@gmail.com>
2012-01-09 17:04:18 -0200

    adding authentication to landing_page controller

--- app/controllers/admin/landing_pages_controller.rb
+++ app/controllers/admin/landing_pages_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Admin::LandingPagesController < ApplicationController
-  #before_filter :authenticate_admin!
+  before_filter :authenticate_admin!
   layout "admin"
   respond_to :html, :text
 
