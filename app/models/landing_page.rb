3ce8d6828e3b088c2883fbab4162de840c861913
Hugo Borges <agaelebe@gmail.com>
2012-01-10 10:59:43 -0200

    adding validation to button url and removing cnd_invalidation (I need to understand how it works before using it)

--- app/models/landing_page.rb
+++ app/models/landing_page.rb
@@ -3,12 +3,7 @@
 class LandingPage < ActiveRecord::Base
   validates :page_url, :page_title, :presence => true, :uniqueness => true
   validates :page_url, :format => { :with => /^[\w-]+$/ }
+  validates :button_url, :format => { :with => /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix }
   mount_uploader :page_image, LandingImageUploader
-  after_update :invalidate_cdn_image
-
-  private
-
-  def invalidate_cdn_image
-    CloudfrontInvalidator.new.invalidate(self.page_image.url.slice(23..150)) unless self.page_image.url.nil?
-  end
+  mount_uploader :button_image, LandingImageUploader
 end
