3a7a8a0a362217cbbed0b70b76e4f3d36ad44cb7
Hugo Borges <agaelebe@gmail.com>
2012-01-11 12:11:07 -0200

    landing page should redirect to home unless it is enabled

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,6 +1,14 @@
 # -*- encoding : utf-8 -*-
 class LandingPagesController < ApplicationController
+
   def show
-    @landing_page = LandingPage.find_by_page_url(params[:page_url])
+    @landing_page = LandingPage.find_by_page_url!(params[:page_url])
+    redirect_to root_path unless @landing_page.enabled?
+  end
+
+  private
+
+  def not_found
+    render :file => "#{Rails.root}/public/404.html", :status => :not_found
   end
 end
