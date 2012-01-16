f219f1fc98870929acc78463e77599c4c0f28502
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-01-16 14:39:24 -0200

    added BoletoVencimento' in Abacos::Pagamento

--- app/models/abacos/pagamento.rb
+++ app/models/abacos/pagamento.rb
@@ -9,14 +9,16 @@ module Abacos
       @valor    = parse_price order.total_with_freight
       @forma    = order.payment.is_a?(Billet) ? 'BOLETO' : order.payment.bank.upcase
       @parcelas = order.payment.payments || 1
+      @boleto_vencimento = order.payment.payment_expiration_date if order.payment.is_a?(Billet)
     end
-    
+
     def parsed_data
       {
         'DadosPedidosFormaPgto' => {
           'Valor'                 => @valor,
           'CartaoQtdeParcelas'    => @parcelas,
-          'FormaPagamentoCodigo'  => @forma
+          'FormaPagamentoCodigo'  => @forma,
+          'BoletoVencimento' => @boleto_vencimento
         }
       }
     end
