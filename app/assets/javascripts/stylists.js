9687925737e3f2e3b98521ca14ee2ccdce0ecc8a
Raul Souza Lima <raulsouzalima@gmail.com>
2012-01-18 18:07:06 -0200

    Slide to product based on url

--- app/assets/javascripts/stylists.js
+++ app/assets/javascripts/stylists.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  StylistInit.slideProductAnchor();
+
   $("section#stylist div.profile a").live("click", function() {
     container_position = $("div#products").position().top;
     StylistInit.slideToProducts(container_position);
@@ -6,6 +8,17 @@ $(document).ready(function() {
 });
 
 StylistInit = {
+  slideProductAnchor : function() {
+    anchor = window.location.hash;
+    container = $(anchor+"_container");
+    if($(container).length > 0) {
+      container_position = $(container).position().top;
+      position = container_position - 10;
+      $("html, body").animate({
+        scrollTop: position
+      }, 'slow');
+    }
+  },
   slideToProducts : function(container_position) {
     position = container_position - 40;
     $("html, body").animate({
