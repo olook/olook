5d55fe488eb31a329bb40f812005c294cfd21419
zanst <me@zan.st>
2012-01-18 20:58:43 -0200

    added more specs to SynchronizationEvent

--- spec/models/ecommerce/synchronization_event_spec.rb
+++ spec/models/ecommerce/synchronization_event_spec.rb
@@ -1,16 +1,25 @@
 # -*- encoding: utf-8 -*-
 require 'spec_helper'
 
-#let(:unlocked_event) { FactoryGirl.create(:unlocked_sync_event) }
-
 describe SynchronizationEvent do
   context "validations" do
     it { should validate_presence_of(:name) }
+
+    it 'should raise an exception if a not allowed name is given' do
+      expect { FactoryGirl.create(:weird_name_synchronization_event) }.should raise_exception
+    end
   end
 
-  it 'should return true when a event was created less than 10 minutes ago' do
-    event = FactoryGirl.create(:lockedsyncevent)
-    SynchronizationEvent.locked?
+  context "#locked?" do
+    it 'should return true when an event was created less than 10 minutes ago' do
+      sync_event = FactoryGirl.create(:locked_synchronization_event)
+      SynchronizationEvent.locked?.should be_true
+    end
+  
+    it 'should return false when an event was created more than 10 minutes ago' do
+      sync_event = FactoryGirl.create(:unlocked_synchronization_event)
+      SynchronizationEvent.locked?.should be_false
+    end
   end
 end
 
