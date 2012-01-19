bf4b2f71882836e88ed1d1036b2579bfa1929754
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-01-19 18:37:36 -0200

    added delivered email notification

--- app/workers/order_status_worker.rb
+++ app/workers/order_status_worker.rb
@@ -15,6 +15,8 @@ class OrderStatusWorker
       mail = OrderStatusMailer.payment_confirmed(order)
     elsif order.delivering?
       mail = OrderStatusMailer.order_shipped(order)
+    elsif order.delivered?
+      mail = OrderStatusMailer.order_delivered(order)
     elsif order.canceled? || order.reversed?
       if order.payment.credit_card?
         mail = OrderStatusMailer.payment_refused(order)
