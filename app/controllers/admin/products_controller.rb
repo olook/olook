84cfcfb6ff2b1f363543d231a37794885dd056ac
zanst <me@zan.st>
2012-01-19 20:08:22 -0200

    added action sync_products to admin/products

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,9 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Admin::ProductsController < Admin::BaseController
+  before_filter :load_products, :only => [:index, :sync_products]
   respond_to :html
 
   def index
-    @products = Product.all
+    @sync_event = SynchronizationEvent.new
     respond_with :admin, @products
   end
 
@@ -49,6 +50,15 @@ class Admin::ProductsController < Admin::BaseController
     respond_with :admin, @product
   end
 
+  def sync_products
+    @sync_event = SynchronizationEvent.new(:name => 'products')
+    if @sync_event.save
+      redirect_to admin_products_path
+    else
+      render :index
+    end
+  end
+
   def add_related
     @product = Product.find(params[:id])
     product_to_relate = Product.find(params[:related_product][:id])
@@ -64,4 +74,11 @@ class Admin::ProductsController < Admin::BaseController
 
     redirect_to admin_product_path(@product)
   end
+
+  private
+
+  def load_products
+    @products = Product.all
+  end
 end
+
