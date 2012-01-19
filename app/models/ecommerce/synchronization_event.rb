c9dddea3ff05b8671483b166a26d10df0734fa97
zanst <me@zan.st>
2012-01-19 20:10:05 -0200

    improved SynchronizationEvent model

--- app/models/ecommerce/synchronization_event.rb
+++ app/models/ecommerce/synchronization_event.rb
@@ -1,27 +1,29 @@
 # -*- encoding: utf-8 -*-
 class SynchronizationEvent < ActiveRecord::Base
   validates_presence_of :name
-  before_save :validates_name
+  validates :name, :inclusion => { :in => %w(products inventory),
+    :message => "Synchronization event was not characterized as 'products' or 'inventory' or has an erroneous name." }
+  validate :check_if_its_locked
+
   after_create :enqueue
 
   LOCK_TIME = 10.minutes
-  ALLOWED_NAMES = ["products", "inventory"]
-
-  protected
-
-  def self.locked?
-    (self.last.created_at + LOCK_TIME > Time.now) ? true : false
-  end
 
   private
+
+  def check_if_its_locked
+    if locked?
+      errors.add(:name, 'You need to wait at least 10 minutes to create a new event.')
+    end
+  end
 
   def enqueue
     Resque.enqueue(Abacos::IntegrateProducts) if self.name == 'products'
   end
 
-  def validates_name
-    unless ALLOWED_NAMES.include?(self.name)
-      raise "This synchronization event is not characterized as 'products' or 'inventory' or has an erroneous name." 
+  def locked?
+    unless SynchronizationEvent.last.nil?
+      (SynchronizationEvent.last.created_at + LOCK_TIME > Time.now) ? true : false
     end
   end
 end
