a22cac94f2ae1e0f0492bf248eb41af512b1ed8a
Hugo Borges <agaelebe@gmail.com>
2012-01-20 08:26:23 -0200

    This is a workaround for invalid dates that will redirect the user to the start of the quiz (it is far from the best solution but it is better than displaying a 500 error)

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -26,6 +26,6 @@ class SurveyController < ApplicationController
   end
 
   def check_questions_params
-    redirect_to new_survey_path if params[:questions].nil?
+    redirect_to new_survey_path if (params[:questions].nil? || !Date.valid_date?(params[:year].to_i,params[:month].to_i,params[:day].to_i))
   end
 end
