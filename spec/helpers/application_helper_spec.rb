fa0d6f18dc3b57a90b1deeb34cf9d211b42dfe74
Hugo Borges <agaelebe@gmail.com>
2012-01-20 13:11:33 -0200

    adding test to member_type and changing test descriptions

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,23 +2,23 @@
 require 'spec_helper'
 
 describe ApplicationHelper do
-  describe '' do
-    it "should return markup for order total" do
+  describe "#order_total" do
+    it "returns markup for order total" do
      expected = "<span>(<div id=\"cart_items\">0</div>)</span>"
      helper.order_total(nil).should eq(expected)
     end
   end
 
-  describe '#stylesheet_application' do
-    it "should" do
+  describe "#stylesheet_application" do
+    it "returns link to application.css" do
       expected = "<link href=\"/assets/application.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />"
       helper.stylesheet_application.should eq(expected)
     end
   end
 
-  describe '#render_google_remessaging_scripts' do
-    context 'for a logged in member' do
-      it 'should render the member script' do
+  describe "#render_google_remessaging_scripts" do
+    context "for a logged in member" do
+      it "renders the member script" do
         helper.stub(:'user_signed_in?').and_return(true)
         helper.should_receive(:render).with('shared/metrics/google/google_remessaging_member')
 
@@ -27,7 +27,7 @@ describe ApplicationHelper do
     end
 
     context 'for a visitor' do
-      it 'should render the visitor script' do
+      it 'renders the visitor script' do
         helper.stub(:'user_signed_in?').and_return(false)
         helper.should_receive(:render).with('shared/metrics/google/google_remessaging_visitor')
 
@@ -44,6 +44,26 @@ describe ApplicationHelper do
     context "when no item is passed" do
       it "returns a track event with category and action only" do
         helper.track_event('category','action').should == "_gaq.push(['_trackEvent', 'category', 'action', '']);"
+      end
+    end
+  end
+
+  describe "#member_type" do
+    it "checks if user is signed in" do
+      helper.should_receive(:'user_signed_in?')
+      helper.member_type
+    end
+    context "when user is not logged in" do
+      it "returns visitor" do
+        helper.stub(:'user_signed_in?').and_return(false)
+        helper.member_type.should == "visitor"
+      end
+    end
+
+    context "when user is logged in" do
+      it "returns member" do
+        helper.stub(:'user_signed_in?').and_return(true)
+        helper.member_type.should == "member"
       end
     end
   end
