d58c2ceb198297a6b9e4afc205de0decb5ec99f9
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-01-21 17:29:54 -0200

    added moip callbacks

---
+++ db/migrate/20120121192041_create_moip_callbacks.rb
@@ -0,0 +1,13 @@
+class CreateMoipCallbacks < ActiveRecord::Migration
+  def change
+    create_table :moip_callbacks do |t|
+      t.string :id_transacao
+      t.string :cod_moip
+      t.string :tipo_pagamento
+      t.string :status_pagamento
+      t.timestamps
+    end
+
+    add_index :moip_callbacks, :id_transacao
+  end
+end
