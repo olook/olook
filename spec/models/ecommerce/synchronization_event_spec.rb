5a703cdb633d5d469da58e3825d844440a0a3ccd
zanst <me@zan.st>
2012-01-23 20:19:19 -0200

    removed tests of old implementation

--- spec/models/ecommerce/synchronization_event_spec.rb
+++ spec/models/ecommerce/synchronization_event_spec.rb
@@ -9,17 +9,5 @@ describe SynchronizationEvent do
       expect { FactoryGirl.create(:weird_name_synchronization_event) }.should raise_exception
     end
   end
-
-  context "#locked?" do
-    it 'should return true when an event was created less than 10 minutes ago' do
-      sync_event = FactoryGirl.create(:locked_synchronization_event)
-      SynchronizationEvent.locked?.should be_true
-    end
-  
-    it 'should return false when an event was created more than 10 minutes ago' do
-      sync_event = FactoryGirl.create(:unlocked_synchronization_event)
-      SynchronizationEvent.locked?.should be_false
-    end
-  end
 end
 
