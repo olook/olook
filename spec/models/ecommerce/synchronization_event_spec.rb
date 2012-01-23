716f7ae7934c3400ab0a3aceafab161e700d00e6
zanst <me@zan.st>
2012-01-23 20:49:02 -0200

    added spec to SynchronizationEvent

--- spec/models/ecommerce/synchronization_event_spec.rb
+++ spec/models/ecommerce/synchronization_event_spec.rb
@@ -8,6 +8,11 @@ describe SynchronizationEvent do
     it 'should raise an exception if a not allowed name is given' do
       expect { FactoryGirl.create(:weird_name_synchronization_event) }.should raise_exception
     end
+
+    it 'should raise an exception if an event was created less than 10 minutes before' do
+      FactoryGirl.create(:locked_synchronization_event)
+      expect { FactoryGirl.create(:locked_synchronization_event) }.should raise_error(ActiveRecord::RecordInvalid)
+    end
   end
 end
 
