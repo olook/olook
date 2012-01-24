5dcda941abdf6ce39d97ec699e0ba1c127f90c57
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-01-24 17:34:59 -0200

    added post_wall_message method

--- spec/adapters/facebook_adapter_spec.rb
+++ spec/adapters/facebook_adapter_spec.rb
@@ -8,6 +8,12 @@ describe FacebookAdapter do
     subject.adapter.should_receive(:get_connections).with("me", "friends")
     subject.friends
   end
+
+  it "should post a message in the wall" do
+    message, attachment, target, options = :message, {}, "me", {}
+    subject.adapter.should_receive(:put_wall_post).with(message, attachment, target, options)
+    subject.post_wall_message(message, :attachment => attachment, :friend => target, :options => options)
+  end
 end
 
 
