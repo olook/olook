7a730a9163baf0ba0beada9817412fc4ff998082
Joao Moura <joaomdmoura@gmail.com>
2012-01-26 13:29:16 -0200

    adding like logic in index action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,21 @@ class HomeController < ApplicationController
   before_filter :redirect_logged_user, :save_tracking_params, :only => :index
 
   def index
+    if params[:share]
+      @user = User.find(params[:uid])
+      @profile = @user.profile_scores.first.try(:profile).first_visit_banner
+      profiles = {
+                    "casual" => 'Prática, Despojada, Independente, e adoto o lema "menos é mais"',
+                    "conteporanea" => 'Antenada, Criativa, Confiante e AMO moda',
+                    "elegant" => 'Chic, Bem Sucedida, Elegante e Exigente',
+                    "feminine" => 'Vaidosa, Romântica, Alegre e Delicada',
+                    "sexy" => 'Sexy, Extravagante, Segura e Vivaz',
+                    "traditional" => 'Sofisticada, Conservadora, Discreta e Clássica',
+                    "trendy" => 'Segura, Ousada, Sexy e Moderna' 
+                  } 
+      @qualities = profiles["#{@profile}"]
+      @url = request.protocol + request.host 
+    end
   end
 
 private
