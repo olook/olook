81bd451e0e74db59bdbd41aa0f74e81446882a45
Hugo Borges <agaelebe@gmail.com>
2012-01-26 17:44:48 -0200

    Making home controller tests simpler and reimplementing home index to pass parameters when redirecting to vitrine

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,65 +4,55 @@ require 'spec_helper'
 describe HomeController do
 
   describe "GET 'index'" do
+    let(:standard_params) { {'controller' => 'home_controller', 'action' => 'index', 'tracking_xyz' => 'test'} }
+    let(:tracked_params) { {'tracking_xyz' => 'test'} }
+
     it "should be successful" do
       get 'index'
       response.should be_success
     end
-  end
-  
-  describe 'filters' do
-    describe '#redirect_logged_user' do
-      it 'should redirect to the member invite if the user is logged in' do
-        subject.stub(:'user_signed_in?').and_return(true)
-        subject.should_receive(:redirect_to)
-        subject.send(:redirect_logged_user)
+
+    context "tracking params" do
+
+      context "when some tracking params are passed" do
+        it "saves tracking params in the session" do
+          get 'index', standard_params
+          subject.session[:tracking_params].should == {'tracking_xyz' => 'test'}
+        end
       end
 
-      it 'should do nothing if the user is logged in' do
-        subject.stub(:'user_signed_in?').and_return(false)
-        subject.should_not_receive(:redirect_to)
-        subject.send(:redirect_logged_user)
-      end
-    end
-
-    describe '#save_tracking_params' do
-      let(:standard_params) { {'controller' => 'test_controller', 'action' => 'test_action'} }
-      it 'when the site receives extra parameters they should be saved to the session' do
-        standard_params[:tracking_xyz] = 'test_tracking'
-        subject.stub(:params).and_return(standard_params)
-        subject.send(:save_tracking_params)
-        subject.session[:tracking_params].should == {:tracking_xyz => 'test_tracking'}
-      end
-
-      context 'when the site do not receive extra parameters' do
-        before :each do
-          subject.stub(:params).and_return(standard_params)
+      context "when some tracking params were already set in session" do
+        let(:old_params) { {'tracking_abc' => 'testing'} }
+        before do
+          subject.session[:tracking_params] = old_params
         end
 
-        context 'and the tracking params on the session is empty' do
-          before :each do
-            subject.session[:tracking_params] = nil
-          end
-
-          it 'should remaining empty' do
-            subject.send(:save_tracking_params)
-            subject.session[:tracking_params].should be_nil
-          end
-        end
-
-        context 'and the tracking params on the session is not empty' do
-          let(:old_params) { {:old => 'tracking'} }
-          before :each do
-            subject.session[:tracking_params] = old_params
-          end
-
-          it 'should keep the old parameters' do
-            subject.send(:save_tracking_params)
-            subject.session[:tracking_params].should == old_params
-          end
+        it "does not subscribe the old params" do
+          get 'index', standard_params
+          subject.session[:tracking_params].should == old_params
         end
       end
     end
+
+    context "redirecting to members showroom" do
+      context "when user is signed in" do
+        it "redirects to member showroom" do
+          subject.stub(:'user_signed_in?').and_return(true)
+          subject.should_receive(:redirect_to).with(member_showroom_path)
+          get 'index', :params => standard_params
+        end
+      end
+
+      context "when user is not signed in" do
+        it "does not redirect to member showroom" do
+          subject.stub(:'user_signed_in?').and_return(false)
+          get 'index', :params => standard_params
+          subject.should_not_receive(:redirect_to)
+        end
+      end
+
+    end
+
   end
 
 end
