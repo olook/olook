0eea6af571a063d3b318653cea520a88933d9865
Joao Moura <joaomdmoura@gmail.com>
2012-01-27 12:23:44 -0200

    adding tests of fb-like sharing

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -12,6 +12,34 @@ describe HomeController do
       response.should be_success
     end
 
+
+    context "User facebook sharing" do
+      let(:point) { FactoryGirl.create(:point) }
+      let(:profile) { point.user.profile_scores.first.try(:profile).first_visit_banner }
+      let(:sharing_params) { {'share' => 'share', 'uid' => point.user.id.to_s } }
+
+      it "assigning @user of the current id" do
+        get 'index', sharing_params
+        assigns(:user).should eq(point.user)
+      end
+
+      it "assigning @profile with visit_banner of the current @user" do
+        get 'index', sharing_params
+        assigns(:profile).should eq(profile)
+      end
+
+      it "assigning @qualities of the current @profile" do
+        get 'index', sharing_params
+        assigns(:qualities).should eq(Profile::DESCRIPTION["#{profile}"])
+      end
+
+      it "assigning @url of the current environment" do
+        get 'index', sharing_params
+        assigns(:url).should eq( request.protocol + request.host )
+      end
+
+    end
+
     context "tracking params" do
 
       context "when some tracking params are passed" do
