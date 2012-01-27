9cd07e29fc410c7753f29b084baf40c4f5d8bb58
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-01-27 16:53:12 -0200

    basic list friend

--- spec/adapters/facebook_adapter_spec.rb
+++ spec/adapters/facebook_adapter_spec.rb
@@ -3,23 +3,23 @@ require 'spec_helper'
 
 describe FacebookAdapter do
   subject { FacebookAdapter.new(:fake_access_token) }
+  let(:friend_list) { [{"id" => "1", "name" => "User name 1"}, {"id" => "2", "name" => "User name 2"}] }
+  let(:formated_friend_list) { [OpenStruct.new(:uid => "1", :name => "User name 1"), OpenStruct.new(:uid => "2", :name => "User name 2")] }
 
   it "should get the friend list" do
-    friend_list = [{"name"=>"User name 1", "id"=>"1"}, {"name"=>"User name 2", "id"=>"2"}]
     subject.adapter.should_receive(:get_connections).at_least(2).times.with("me", "friends").and_return(friend_list)
     subject.facebook_friends.first.uid.should == "1"
     subject.facebook_friends.first.name.should == "User name 1"
   end
 
   it "should get the friends ids list" do
-    friend_list = [{"name"=>"User name 1", "id"=>"1"}, {"name"=>"User name 2", "id"=>"2"}]
-    subject.stub(:facebook_friends).and_return(friend_list)
+    subject.stub(:facebook_friends).and_return(formated_friend_list)
     subject.facebook_friends_ids.should == ["1", "2"]
   end
 
   it "should get the facebook friends registered at olook" do
     subject.stub(:facebook_friends_ids).and_return(friends_ids = ["1", "2"])
-    User.should_receive(:find_by_uid).with(friends_ids)
+    User.should_receive(:find_all_by_uid).with(friends_ids)
     subject.olook_facebook_friends
   end
 
