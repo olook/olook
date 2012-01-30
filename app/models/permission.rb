10fe1330b3d96403189991d6616ef817483fefb2
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-01-30 00:01:39 -0200

    Changed admin factory and added roles factories

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,13 +1,7 @@
 class Permission < ActiveRecord::Base
 
   has_and_belongs_to_many :roles
-  validate :model_action_uniqueness
 
-  def model_action_uniqueness
-    if Permission.find_by_model_name_and_action_name(self.model_name, self.action_name)
-      errors.add(:id, ": The combination model / action must be unique")
-    end
-  end
-
+  validates :action_name, :uniqueness => {:scope => :model_name, :message => "for given Model exists"}
 
 end
