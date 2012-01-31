09eaa5331e6069ea63f0a2daa53865a52f372fb6
Raul Souza Lima <raulsouzalima@gmail.com>
2012-01-31 15:53:46 -0200

    BG welcome page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
   $("section#painel a.bt_painel").live("click", function() {
-    $("section#painel img.img_painel").slideToggle("slow");
+    $("section#painel img.img_painel").slideToggle("slow", function() {
+      if($(this).is(":visible") == true) {
+        $("body").addClass("bg_changed");
+      } else {
+        $("body").removeClass("bg_changed");
+      }
+    });
   });
 });
