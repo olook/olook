7b953e51b5784e3ae561338d0a46553fec348e10
codextremist <felipe.japm@gmail.com>
2012-02-01 18:31:07 -0200

    Fixed admin and roles factories

--- spec/factories/admins.rb
+++ spec/factories/admins.rb
@@ -1,21 +1,21 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :admin do
+    first_name "Bloody"
+    last_name "Killer"
     password "123456"
     password_confirmation "123456"
     sequence :email do |n|
       "admin#{n}@example.com"
     end
-    factory :admin_superadministrator do
-      after_create do |admin|
-        admin.roles << FactoryGirl.create(:superadministrator)
-      end
-    end  
-
-    factory :admin_sac_operator do
-      after_create do |admin|
-        admin.roles << FactoryGirl.create(:sac_operator)
-      end 
-    end
+    association :role, :factory => :generic_model
   end
+
+   factory :admin_superadministrator, :parent => :admin do
+      association :role, :factory => :superadministrator
+   end  
+
+   factory :admin_sac_operator, :parent => :admin do
+     association :role, :factory => :sac_operator
+   end
 end
