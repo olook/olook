95e6e0ecf0c61f39e1cc09b48e3c299d405eb921
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-02-01 00:22:37 -0200

    Changed roles association with admin to has_many

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,17 +1,15 @@
 # -*- encoding : utf-8 -*-
 class Admin < ActiveRecord::Base
 
-   has_and_belongs_to_many :roles
+  belongs_to :role
+  validates :role_id, :presence => true
 
   devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
   attr_accessible :email, :password, :password_confirmation, :remember_me
 
   def has_role?(role_name)
-    self.roles_name.include?(role_name) ? true : false
+    self.role.name.to_sym == role_name ? true : false
   end
 
-  def roles_name
-    self.roles.collect {|role| role.name.to_sym}
-  end
 
 end
