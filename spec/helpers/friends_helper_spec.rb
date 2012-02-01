e1876b93225dbcd23bac2d44c3f340e90cc10093
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-02-01 18:28:21 -0200

    added friend title to quiz

--- spec/helpers/friends_helper_spec.rb
+++ spec/helpers/friends_helper_spec.rb
@@ -1,15 +1,10 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the FriendsHelper. For example:
-#
-# describe FriendsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe FriendsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should return the question title for a friend" do
+    user_name = "User name"
+    question_title = "Qual sapato combina mais com __USER_NAME__?"
+    expected = "Qual sapato combina mais com <span>#{user_name}</span>?"
+    helper.question_title_for_friend(question_title, user_name).should == expected
+  end
 end
