dc1cfccc51edc5c190732651a0ec7a2e86b41368
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-02-02 08:15:20 -0200

    Changed hard code superadmin, fixed controller tests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,8 @@ class Ability
   include CanCan::Ability
 
   def initialize(admin)
-    if admin.has_role? :superadministrator
-      can :manage, :all
-    else
-      admin.role.permissions.each do |permission|
-        can permission.action_name.to_sym, permission.model_name.constantize
-      end
+    admin.role.permissions.each do |permission|
+      can permission.action_name.to_sym, permission.model_name.constantize
     end
   end
 
