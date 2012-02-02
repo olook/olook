2cf1f10f4b72006ec0e08e9f64b51d4f32d3c0cb
codextremist <felipe.japm@gmail.com>
2012-02-02 14:06:58 -0200

    Fixed tests

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,12 @@
 class Ability
   include CanCan::Ability
-
   def initialize(admin)
-    admin.role.permissions.each do |permission|
-      can permission.action_name.to_sym, permission.model_name.constantize
-    end
+  	if admin.has_role? :superadministrator
+      can :manage, :all
+  	else	
+   	  admin.role.permissions.each do |permission|
+        can permission.action_name.to_sym, permission.model_name.constantize
+      end
+    end  
   end
-
 end
