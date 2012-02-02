87740a120365e8f8dd1f335621a99a333b3efe66
Raul Souza Lima <raulsouzalima@gmail.com>
2012-02-02 17:01:00 -0200

    Style updates on welcome page

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
   $("section#painel a.bt_painel").live("click", function() {
     $("section#painel img.img_painel").slideToggle("slow", function() {
       if($(this).is(":visible") == true) {
+        $("section#painel a.bt_painel").text("Fechar estilo");
         $("body").addClass("bg_changed");
       } else {
+        $("section#painel a.bt_painel").text("Abrir estilo");
         $("body").removeClass("bg_changed");
       }
     });
