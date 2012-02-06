ba719fc2f5a7d60f2228355d36f81daabaa66824
Joao Moura <joaomdmoura@gmail.com>
2012-02-06 18:36:40 -0200

    fixing link of cookbooks and order of loading

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -1,9 +1,9 @@
 class LookbooksController < ApplicationController
   def show
     if params[:name]
-      @lookbook = Lookbook.where("name = '#{params[:name]}' and active = 1")[0]
+      @lookbook = Lookbook.where("name = '#{params[:name]}' and active = 1").order("created_at DESC")[0]
     else
-      @lookbook = Lookbook.where("active = 1").order(:created_at).limit(1)[0]
+      @lookbook = Lookbook.where("active = 1").order("created_at DESC").limit(1)[0]
     end
     @products = @lookbook.products
     @products_id = @lookbook.lookbooks_products.map{|item| ( item.criteo ) ? item.product_id : nil }.compact
