3963f4ea3da4ac89d52ab974a445769baba1be5b
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-02-06 09:11:44 -0200

    Fixed authentication and tests

--- app/controllers/admin/index_controller.rb
+++ app/controllers/admin/index_controller.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
 class Admin::IndexController < Admin::BaseController
-#load_and_authorize_resource
 
   def dashboard
   end
--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 
 describe Role do
 
-  let(:superadministrator) { Factory(:superadministrator) }
+  let(:sac_operator) { Factory(:sac_operator) }
 
   it "should associate permissions with valid params" do
     pending
   end
 
   it "should check if it has permissions associated" do
-    pending
+    sac_operator.has_permission?(sac_operator.permissions[0].id).should eq(true)
   end
 
 
