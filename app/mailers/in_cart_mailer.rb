41f84374a85318a4c667c6404b5cd00aa011bdbb
Joao Moura <joaomdmoura@gmail.com>
2012-02-09 21:43:12 -0200

    adding mailer and first version of front

---
+++ app/mailers/in_cart_mailer.rb
@@ -0,0 +1,13 @@
+# -*- encoding : utf-8 -*-
+class InCartMailer < ActionMailer::Base
+  default_url_options[:host] = "www.olook.com.br"
+  default :from => "olook <avisos@olook.com.br>"
+
+  def send_in_cart_mail( order )
+  	@user = order.user
+  	@products = order.line_items
+  	subject = "#{@user.first_name}, os seus produtos ainda estão disponíveis."
+    mail(:to => @user.email , :subject => subject)
+  end
+
+end
