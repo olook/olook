ddc2dd1b3cccb147479098291ab969586971b151
Raul Souza Lima <raulsouzalima@gmail.com>
2012-02-09 16:17:25 -0200

    Added facebook comments on product page

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -8,6 +8,7 @@ class ProductController < ApplicationController
   before_filter :load_order
 
   def show
+    @facebook_app_id = FACEBOOK_CONFIG["app_id"]
     @url = request.protocol + request.host
     @product = Product.only_visible.find(params[:id])
     @variants = @product.variants
