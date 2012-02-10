eddc740c514f90753adae810965666db73e9bc99
Hugo Borges <agaelebe@gmail.com>
2012-02-10 09:44:21 -0200

    replaced tabs by spaces PS: Please configure your text editor properly next time, we use soft tabs with 2 spaces!"

--- app/models/lookbook.rb
+++ app/models/lookbook.rb
@@ -1,10 +1,10 @@
 class Lookbook < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true
-	has_many :images, :dependent => :destroy
-	has_many :lookbooks_products, :dependent => :destroy
+  has_many :images, :dependent => :destroy
+  has_many :lookbooks_products, :dependent => :destroy
   has_many :products, :through => :lookbooks_products
 
-	accepts_nested_attributes_for :images, :reject_if => lambda{|p| p[:image].blank?}
+  accepts_nested_attributes_for :images, :reject_if => lambda{|p| p[:image].blank?}
 
   mount_uploader :thumb_image, ImageUploader
 
@@ -12,12 +12,11 @@ class Lookbook < ActiveRecord::Base
   attr_accessor :product_criteo
   after_save :update_products
 
-  private 
+  private
 
   def update_products
-    
     products.delete_all
-		selected_products = product_list.nil? ? [] : product_list.keys.collect{|id| Product.find_by_id(id)}
+    selected_products = product_list.nil? ? [] : product_list.keys.collect{|id| Product.find_by_id(id)}
     selected_products.each {|os| self.products << os}
     lb = self.lookbooks_products
     lb.each do |lb_product|
