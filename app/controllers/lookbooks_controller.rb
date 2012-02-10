8742b91a4607c1b5831031dda26f7f56bfcdb371
Raul Souza Lima <raulsouzalima@gmail.com>
2012-02-10 20:55:15 -0200

    Fix like button on lookbook page

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -8,6 +8,8 @@ class LookbooksController < ApplicationController
     @products = @lookbook.products
     @products_id = @lookbook.lookbooks_products.map{|item| ( item.criteo ) ? item.product_id : nil }.compact
     @lookbooks = Lookbook.order("created_at DESC")
+    @url = request.protocol + request.host
+    @facebook_app_id = FACEBOOK_CONFIG["app_id"]
   end
 
   def flores
