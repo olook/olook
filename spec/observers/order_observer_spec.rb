1247bdafb9724c183133c3fd24b08cb80d938417
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-02-15 15:24:57 -0200

    enqueing a update inventory when a order is created

--- spec/observers/order_observer_spec.rb
+++ spec/observers/order_observer_spec.rb
@@ -2,17 +2,31 @@
 require "spec_helper"
 
 describe OrderObserver do
-  subject { FactoryGirl.create(:order) }
+  context "on update" do
+    subject { FactoryGirl.create(:order) }
+    it "should enqueue a OrderStatusWorker when the state is updated" do
+      Resque.should_receive(:enqueue).with(OrderStatusWorker, subject.id)
+      subject.waiting_payment
+    end
 
-  it "should enqueue a OrderStatusWorker when the state is updated" do
-    Resque.should_receive(:enqueue).with(OrderStatusWorker, subject.id)
-    subject.waiting_payment
+    it "should use the coupon when authorized" do
+      subject.should_receive(:use_coupon)
+      subject.waiting_payment
+      subject.authorized
+    end
+
+    it "should update the inventory when necessary" do
+      OrderInventory.stub(:new).and_return(order_inventory = mock)
+      order_inventory.stub(:should_rollback?).and_return(true)
+      order_inventory.should_receive(:rollback).at_least(1).times
+      subject.canceled
+    end
   end
 
-  it "should update the inventory when necessary" do
-    OrderInventory.stub(:new).and_return(order_inventory = mock)
-    order_inventory.stub(:available_for_rollback?).and_return(true)
-    order_inventory.should_receive(:rollback).at_least(1).times
-    subject.canceled
+  context "on create" do
+   it "should enqueue a job to update the inventory" do
+     Resque.should_receive(:enqueue).with(Abacos::UpdateInventory)
+     FactoryGirl.create(:order)
+   end
   end
 end
