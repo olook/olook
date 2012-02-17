244dd4ba7fbd8eda4d5c96c0175ad830427dc739
Raul Souza Lima <raulsouzalima@gmail.com>
2012-02-17 02:17:03 -0200

    Added link to facebook settings on showroom page

--- app/controllers/user/settings_controller.rb
+++ app/controllers/user/settings_controller.rb
@@ -1,3 +1,8 @@
 # -*- encoding : utf-8 -*-
 class User::SettingsController < ApplicationController
+  layout "my_account"
+
+  respond_to :html
+  before_filter :authenticate_user!
+  before_filter :load_user
 end
