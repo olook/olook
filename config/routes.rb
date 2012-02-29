88a99ecec921ad7d4371708aa0a22cb678e0554b
Hugo Borges <agaelebe@gmail.com>
2012-02-29 17:12:33 -0300

    removing commented line

--- config/routes.rb
+++ config/routes.rb
@@ -34,8 +34,6 @@ Olook::Application.routes.draw do
 
   match "/criteo", :to => "xml#criteo", :as => "criteo_url", :defaults => { :format => 'xml' }
 
-  #resource :xml, :only => [:show], :path => 'criteo', :controller => :xml, :defaults => { :format => 'xml' }
-
   resource :survey, :only => [:new, :create], :path => 'quiz', :controller => :survey
   resources :payments, :path => 'pagamento', :controller => :payments
   resources :credit_cards, :only => [:new, :create], :path => 'credito', :controller => :credit_cards
