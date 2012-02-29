a33a83c78fb41fcd727fe5ae77c8ad2a769eb9c6
Hugo Borges <agaelebe@gmail.com>
2012-02-29 16:55:40 -0300

    renaming and correcting controller test

--- spec/controllers/xml_controller_spec.rb
+++ spec/controllers/xml_controller_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe XmlController do
-  describe "GET show" do
+  describe "GET criteo" do
 
     let!(:variant) { FactoryGirl.create(:variant, :is_master => true, :price => 2.2) }
     let!(:product) { variant.product }
 
     it "gets only products for xml" do
       Product.should_receive(:for_xml)
-      get :show
+      get :criteo
     end
 
     it "assigns all produts for xml to @products" do
-      get :show
+      get :criteo
       assigns(:products).should include(product)
     end
   end
