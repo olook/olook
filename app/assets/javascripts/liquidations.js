4b765c2b8d19367eaa3fb4dec304d373fba08b7f
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-01 17:51:00 -0300

    basic infinity scroll

--- app/assets/javascripts/liquidations.js
+++ app/assets/javascripts/liquidations.js
@@ -1,2 +1,20 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+jQuery(function() {
+  $("form").bind("ajax:beforeSend", function(evt, xhr, settings) {
+    return $("#products").fadeOut("slow", function() {
+      $(this).fadeIn("slow");
+      return $(this).html("");
+    });
+  });
+  if ($('.pagination').length) {
+    $(window).scroll(function() {
+      var url;
+      url = $('.pagination .next_page').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 300) {
+        $('.pagination').text("Buscando produtos");
+        return $.getScript(url);
+      }
+    });
+    return $(window).scroll();
+  }
+});
+
--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -1,7 +1,10 @@
 class LiquidationsController < ApplicationController
   layout "liquidation"
+  respond_to :html, :js
 
   def show
+    @products = Product.paginate(:page => params[:page], :per_page => 30)
+    respond_with @products
   end
 
 end
