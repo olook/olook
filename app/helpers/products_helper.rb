24095e29a1c98aa28e6194f37fdcbbde6b6b7319
Joao Moura <joaomdmoura@gmail.com>
2012-02-29 22:25:25 -0300

    adding integration test, and improving helper logic

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -3,8 +3,8 @@ module ProductsHelper
     classes = []
     if !variant.available_for_quantity?
       classes << "unavailable"
-    elsif variant.description == current_user.user_info.shoes_size.to_s
-      classes << "selected"
+    elsif !current_user.user_info.nil?  
+      classes << "selected" unless variant.description != current_user.user_info.shoes_size.to_s
     end
     classes.join(" ")
   end
