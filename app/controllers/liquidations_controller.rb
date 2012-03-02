f2bc61b745b437c9b01d7a2d2650e8ea2663da30
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-02 16:55:07 -0300

    fix basic filter

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -4,20 +4,19 @@ class LiquidationsController < ApplicationController
 
   def show
     if params[:category]
-    @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 5)
+      @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 8)
     else
-    @products = Product.paginate(:page => params[:page], :per_page => 5)
+      @products = Product.paginate(:page => params[:page], :per_page => 8)
     end
     respond_with @products
   end
 
   def update
     if params[:category]
-    @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 5)
+      @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 8)
     else
-    @products = Product.paginate(:page => params[:page], :per_page => 5)
+      @products = Product.paginate(:page => params[:page], :per_page => 8)
     end
-
     respond_with @products
   end
 end
