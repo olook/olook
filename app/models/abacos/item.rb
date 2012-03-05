c74f5f39f3199b5ea7fc428c2f315f548d8ee45c
Joao Moura <joaomdmoura@gmail.com>
2012-03-05 19:20:09 -0300

    adding preço unitário brute to each item and tests

--- app/models/abacos/item.rb
+++ app/models/abacos/item.rb
@@ -4,16 +4,18 @@ module Abacos
     attr_reader :codigo, :quantidade, :preco_unitario
 
     def initialize(line_item)
-      @codigo         = line_item.variant.number
-      @quantidade     = line_item.quantity
-      @preco_unitario = "%.2f" % line_item.price
+      @codigo                 = line_item.variant.number
+      @quantidade             = line_item.quantity
+      @preco_unitario         = "%.2f" % line_item.price
+      @preco_unitario_bruto   = "%.2f" % line_item.price
     end
     
     def parsed_data
       {
-        'CodigoProduto'     => @codigo,
-        'QuantidadeProduto' => @quantidade,
-        'PrecoUnitario'     => @preco_unitario
+        'CodigoProduto'       => @codigo,
+        'QuantidadeProduto'   => @quantidade,
+        'PrecoUnitario'       => @preco_unitario,
+        'PrecoUnitarioBruto'  => @preco_unitario_bruto
       }
     end
   end
