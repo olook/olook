0ec317b945a956bfe1f29b52f0b48c7a4a710d30
Hugo Borges <agaelebe@gmail.com>
2012-03-05 14:37:54 -0300

    corrected integration test

--- spec/integration/xml_spec.rb
+++ spec/integration/xml_spec.rb
@@ -3,27 +3,31 @@ require 'spec_helper'
 require 'integration/helpers'
 
 feature "Show products on xml format" do
-  let!(:variant) { FactoryGirl.create :variant, :price => 30.55 }
+  let!(:bag) { FactoryGirl.create :basic_bag }
+  let!(:product) { FactoryGirl.create :basic_shoe }
+
+  background do
+    product.master_variant.update_attribute(:price, 1.0)
+  end
 
   context "in the product xml page" do
     scenario "I want to see products of criteo" do
       visit criteo_path
-      debugger
       page.source.should == <<-END.gsub(/^ {6}/, '')
       <?xml version="1.0" encoding="UTF-8" ?>
       <products>
-        <product id="#{variant.product.id}">
-        <name>#{variant.product.name}</name>
-        <smallimage></smallimage>
-        <bigimage></bigimage>
-        <producturl>http://www.olook.com.br/produto/#{variant.product.id}?utm_campaign=remessaging&amp;utm_content=#{variant.product.id}&amp;utm_medium=banner&amp;utm_source=criteo</producturl>
-        <description>#{variant.product.description}</description>
-        <price>#{variant.price}</price>
-        <retailprice>#{variant.price}</retailprice>
-        <recommendable>1</recommendable>
-        <instock>#{variant.product.instock}</instock>
-        <category>#{variant.product.category}</category>
-        </product>
+      <product id="#{product.id}">
+      <name>#{product.name}</name>
+      <smallimage></smallimage>
+      <bigimage></bigimage>
+      <producturl>http://www.olook.com.br/produto/#{product.id}?utm_campaign=remessaging&amp;utm_content=#{product.id}&amp;utm_medium=banner&amp;utm_source=criteo</producturl>
+      <description>#{product.description}</description>
+      <price>#{product.price}</price>
+      <retailprice>#{product.price}</retailprice>
+      <recommendable>1</recommendable>
+      <instock>#{product.instock}</instock>
+      <category>#{product.category}</category>
+      </product>
       </products>
       END
     end
