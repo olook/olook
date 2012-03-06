d9efa95d08ec7210fa24b1ab61e81a9ff504ca49
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-06 19:17:27 -0300

    basic join for liquidations

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -3,11 +3,17 @@ class LiquidationsController < ApplicationController
   respond_to :html, :js
 
   def show
-    if params[:category]
-      @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 8)
-    else
-      @products = Product.paginate(:page => params[:page], :per_page => 8)
-    end
+    subcategory_names = params[:subcategories] if params[:subcategories]
+    shoe_sizes        = params[:shoe_sizes] if params[:shoe_sizes]
+    heels             = params[:heels] if params[:heels]
+
+    liquidation_products = LiquidationProduct.arel_table
+
+    @products = Product.joins(:liquidation_products).
+                        where(liquidation_products["subcategory_name"].in(subcategory_names).
+                        or(liquidation_products["shoe_size"].in(shoe_sizes).
+                        or(liquidation_products["heel"].in(heels))))
+
     respond_with @products
   end
 
