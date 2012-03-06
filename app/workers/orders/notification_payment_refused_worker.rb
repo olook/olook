864fbc93feb49724bf5692cf372a48c8437391b3
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-06 16:49:24 -0300

    sending notification emails from state machine transitions

---
+++ app/workers/orders/notification_payment_refused_worker.rb
@@ -0,0 +1,19 @@
+# -*- encoding : utf-8 -*-
+module Orders
+  class NotificationPaymentRefusedWorker
+    @queue = :order_status
+
+    def self.perform(order_id)
+      order = Order.find(order_id)
+      send_email(order)
+    end
+
+    def self.send_email(order)
+      if order.canceled? || order.reversed?
+        if order.payment.credit_card?
+          mail = OrderStatusMailer.payment_refused(order)
+        end
+      end
+    end
+  end
+end
