804e83ebcda2568b6509b353bf703626fd637fc7
Hugo Borges <agaelebe@gmail.com>
2012-03-06 12:06:49 -0300

    adding tests and refactoring

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -5,37 +5,25 @@ class Tracking < ActiveRecord::Base
   scope :google, where('gclid IS NOT NULL')
   scope :google_campaigns, google.group(:placement)
   scope :campaigns, where('utm_content IS NOT NULL').group(:utm_source).group(:utm_campaign).group(:utm_medium).group(:utm_content)
+  scope :with_complete_payment, joins("INNER JOIN orders on orders.user_id = trackings.user_id")
+                                .joins("INNER JOIN payments on orders.id = payments.order_id")
+                                .where("payments.state IN ('authorized','completed')")
 
   def total_revenue(total = :total)
-    related_orders_with_complete_payment.inject(0) do |sum, campaign|
-      sum += campaign.user.total_revenue(total)
-    end
+    related_with_complete_payment.inject(0) { |sum, campaign| sum += campaign.user.total_revenue(total) }
   end
 
   def total_revenue_for_google(total = :total)
-    self.class.google
-        .where(:placement => self.placement)
-        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
-        .joins("INNER JOIN payments on orders.id = payments.order_id")
-        .inject(0) do |sum, campaign|
-      sum += campaign.user.total_revenue(total)
-    end
+    related_with_complete_payment_for_google.inject(0) { |sum, campaign| sum += campaign.user.total_revenue(total) }
   end
 
-  def total_orders_for_google
-    self.class.google
-        .where(:placement => self.placement)
-        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
-        .joins("INNER JOIN payments on orders.id = payments.order_id")
-        .where("payments.state IN ('authorized','completed')").count
+  def related_with_complete_payment_for_google
+    self.class.google.with_complete_payment.where(:placement => placement)
   end
 
-  def related_orders_with_complete_payment
-    self.class.where(:utm_source => self.utm_source, :utm_campaign => self.utm_campaign,
-                     :utm_medium => self.utm_medium, :utm_content => self.utm_content)
-        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
-        .joins("INNER JOIN payments on orders.id = payments.order_id")
-        .where("payments.state IN ('authorized','completed')")
+  def related_with_complete_payment
+    self.class.where(:utm_source => utm_source, :utm_campaign => utm_campaign,
+                     :utm_medium => utm_medium, :utm_content =>  utm_content).with_complete_payment
   end
 
 end
