c29bbd2fa7097a1e05c554af91cfe46ef97c5876
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-06 16:49:24 -0300

    sending notification emails from state machine transitions

---
+++ app/workers/orders/notification_order_shipped_worker.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+module Orders
+  class NotificationOrderShippedWorker
+    @queue = :order_status
+
+    def self.perform(order_id)
+      order = Order.find(order_id)
+      send_email(order)
+    end
+
+    def self.send_email(order)
+      if order.delivering?
+        mail = OrderStatusMailer.order_shipped(order)
+      end
+    end
+  end
+end
