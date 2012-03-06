36ce5452d8675420051c169045b2181246116ec7
Hugo Borges <agaelebe@gmail.com>
2012-03-06 08:53:54 -0300

    adding helper methods and factories for tracking model

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -1,4 +1,41 @@
 # -*- encoding : utf-8 -*-
 class Tracking < ActiveRecord::Base
   belongs_to :user
+
+  scope :google, where('gclid IS NOT NULL')
+  scope :google_campaigns, google.group(:placement)
+  scope :campaigns, where('utm_content IS NOT NULL').group(:utm_source).group(:utm_campaign).group(:utm_medium).group(:utm_content)
+
+  def total_revenue(total = :total)
+    related_orders_with_complete_payment.inject(0) do |sum, campaign|
+      sum += campaign.user.total_revenue(total)
+    end
+  end
+
+  def total_revenue_for_google(total = :total)
+    self.class.google
+        .where(:placement => self.placement)
+        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
+        .joins("INNER JOIN payments on orders.id = payments.order_id")
+        .inject(0) do |sum, campaign|
+      sum += campaign.user.total_revenue(total)
+    end
+  end
+
+  def total_orders_for_google
+    self.class.google
+        .where(:placement => self.placement)
+        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
+        .joins("INNER JOIN payments on orders.id = payments.order_id")
+        .where("payments.state IN ('authorized','completed')").count
+  end
+
+  def related_orders_with_complete_payment
+    self.class.where(:utm_source => self.utm_source, :utm_campaign => self.utm_campaign,
+                     :utm_medium => self.utm_medium, :utm_content => self.utm_content)
+        .joins("INNER JOIN orders on orders.user_id = trackings.user_id")
+        .joins("INNER JOIN payments on orders.id = payments.order_id")
+        .where("payments.state IN ('authorized','completed')")
+  end
+
 end
--- spec/models/tracking_spec.rb
+++ spec/models/tracking_spec.rb
@@ -1,7 +1,30 @@
 require "spec_helper"
 
 describe Tracking do
-  describe 'relationships' do
+  describe "relationshios" do
     it { should belong_to :user }
+  end
+
+  context "scopes" do
+    let!(:google_tracking) { FactoryGirl.create(:google_tracking) }
+    let!(:tracking) { FactoryGirl.create(:tracking) }
+
+    describe "#google" do
+      it "includes a tracking with gclid and placement" do
+        Tracking.google.should include(google_tracking)
+      end
+    end
+
+    describe "#campaigns" do
+      let!(:tracking_two) { FactoryGirl.create(:tracking) }
+
+      it "includes a tracking with utm_content" do
+        Tracking.campaigns.should include(tracking)
+      end
+
+      it "groups two similar trackings in one record" do
+        Tracking.campaigns.all.size.should == 1
+      end
+    end
   end
 end
