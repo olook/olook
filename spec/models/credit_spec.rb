0c483875717ab1e0a46964a867a7f11e3d99203e
Hugo Borges <agaelebe@gmail.com>
2012-03-07 18:31:38 -0300

    adding model to consolidate user invite bonus

---
+++ spec/models/credit_spec.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe Credit do
+  it { should belong_to(:user) }
+  it { should belong_to(:order) }
+  it { should validate_presence_of(:value) }
+endt
