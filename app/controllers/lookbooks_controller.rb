de3e96d9818472bc5e676acd212051ca8d52a0e0
Hugo Borges <agaelebe@gmail.com>
2012-03-07 11:09:50 -0300

    Removed string interpolation from SQL query (prone to SQL injection), added scopes to lookbooks to avoid duplication

--- app/controllers/lookbooks_controller.rb
+++ app/controllers/lookbooks_controller.rb
@@ -1,13 +1,9 @@
 class LookbooksController < ApplicationController
   def show
-    if params[:name]
-      @lookbook = Lookbook.where("slug = '#{params[:name]}' and active = 1").order("created_at DESC")[0]
-    else
-      @lookbook = Lookbook.where("active = 1").order("created_at DESC").limit(1)[0]
-    end
+    @lookbook = params[:name] ? Lookbook.where(:slug => params[:name]).active.first : Lookbook.active.first
     @products = @lookbook.products
     @products_id = @lookbook.lookbooks_products.map{|item| ( item.criteo ) ? item.product_id : nil }.compact
-    @lookbooks = Lookbook.where("active = 1").order("created_at DESC")
+    @lookbooks = Lookbook.active.all
     @url = request.protocol + request.host
     @facebook_app_id = FACEBOOK_CONFIG["app_id"]
   end
