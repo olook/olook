6d67745edc895105af81868ae422a66d61aa225b
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-08 19:17:22 -0300

    refactoring using presenter

--- app/presenters/liquidation_presenter.rb
+++ app/presenters/liquidation_presenter.rb
@@ -4,4 +4,12 @@ class LiquidationPresenter < BasePresenter
   def render_search_form
     h.render :partial => 'liquidations/search_form', :locals => {:liquidation_presenter => self}
   end
+
+  def display_shoe_filters
+    h.render :partial => 'liquidations/shoe_filters', :locals => {:liquidation_presenter => self}
+  end
+
+  def display_shoe_size_filters
+    h.render :partial => 'liquidations/shoe_size_filters', :locals => {:liquidation_presenter => self}
+  end
 end
