3797530b5c4b08bd474e9beefbb617526f1d2d15
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-08 00:10:37 -0300

    using before filter

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -1,24 +1,26 @@
 class LiquidationsController < ApplicationController
   layout "liquidation"
   respond_to :html, :js
+  before_filter :load_products
 
   def show
-    liquidation_products = LiquidationProduct.arel_table
-
-    @products = Product.joins(:liquidation_products).
-                        where(liquidation_products["subcategory_name"].in(params[:subcategories]).
-                        or(liquidation_products["shoe_size"].in(params[:shoe_sizes]).
-                        or(liquidation_products["heel"].in(params[:heels])))).order('category asc')
-
     respond_with @products
   end
 
   def update
-    if params[:category]
-      @products = Product.where(:category => params[:category]).paginate(:page => params[:page], :per_page => 8)
-    else
-      @products = Product.paginate(:page => params[:page], :per_page => 8)
-    end
     respond_with @products
   end
+
+  private
+
+    def load_products
+      liquidation_products = LiquidationProduct.arel_table
+
+      @products = Product.joins(:liquidation_products).
+                          where(liquidation_products["subcategory_name"].in(params[:subcategories]).
+                          or(liquidation_products["shoe_size"].in(params[:shoe_sizes]).
+                          or(liquidation_products["heel"].in(params[:heels])))).order('category asc').
+                          paginate(:page => params[:page], :per_page => 15)
+
+    end
 end
