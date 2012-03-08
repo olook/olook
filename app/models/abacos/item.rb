8248e02b098d4f82869cf005426ea49b5ff4d5f8
Joao Moura <joaomdmoura@gmail.com>
2012-03-08 11:28:54 -0300

    adding logic of retail price

--- app/models/abacos/item.rb
+++ app/models/abacos/item.rb
@@ -7,7 +7,7 @@ module Abacos
       @codigo                 = line_item.variant.number
       @quantidade             = line_item.quantity
       @preco_unitario         = "%.2f" % line_item.price
-      @preco_unitario_bruto   = "%.2f" % line_item.price
+      @preco_unitario_bruto   = "%.2f" % line_item.retail_price if line_item.liquidation?
     end
     
     def parsed_data
