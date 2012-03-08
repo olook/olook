7a47b0ee836b955dd03ba50f208869b3d2e0b884
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-08 11:56:22 -0300

    scoped by liquidation

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -4,7 +4,10 @@ class LiquidationsController < ApplicationController
 
   def show
     @liquidation = Liquidation.find(params[:id])
-    @products = Product.joins(:liquidation_products).paginate(:page => params[:page], :per_page => 15).order('category asc')
+    liquidation_products = LiquidationProduct.arel_table
+    @products = Product.joins(:liquidation_products).
+                        where(liquidation_products["liquidation_id"].eq(@liquidation.id)).
+                        paginate(:page => params[:page], :per_page => 15).order('category asc')
     respond_with @products
   end
 
@@ -13,9 +16,10 @@ class LiquidationsController < ApplicationController
     liquidation_products = LiquidationProduct.arel_table
 
     @products = Product.joins(:liquidation_products).
-                        where(liquidation_products["subcategory_name"].in(params[:subcategories]).
+                        where(liquidation_products["liquidation_id"].eq(@liquidation.id).
+                        and(liquidation_products["subcategory_name"].in(params[:subcategories]).
                         or(liquidation_products["shoe_size"].in(params[:shoe_sizes]).
-                        or(liquidation_products["heel"].in(params[:heels])))).order('category asc').
+                        or(liquidation_products["heel"].in(params[:heels]))))).order('category asc').
                         paginate(:page => params[:page], :per_page => 15)
 
     respond_with @products
