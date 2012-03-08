be8345058013e713aa7732b30182d8ef2eb3a4ee
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-08 00:59:10 -0300

    testing update action

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -1,26 +1,21 @@
 class LiquidationsController < ApplicationController
   layout "liquidation"
   respond_to :html, :js
-  before_filter :load_products
 
   def show
+    @products = Product.joins(:liquidation_products).paginate(:page => params[:page], :per_page => 15).order('category asc')
     respond_with @products
   end
 
   def update
+    liquidation_products = LiquidationProduct.arel_table
+
+    @products = Product.joins(:liquidation_products).
+                        where(liquidation_products["subcategory_name"].in(params[:subcategories]).
+                        or(liquidation_products["shoe_size"].in(params[:shoe_sizes]).
+                        or(liquidation_products["heel"].in(params[:heels])))).order('category asc').
+                        paginate(:page => params[:page], :per_page => 15)
+
     respond_with @products
   end
-
-  private
-
-    def load_products
-      liquidation_products = LiquidationProduct.arel_table
-
-      @products = Product.joins(:liquidation_products).
-                          where(liquidation_products["subcategory_name"].in(params[:subcategories]).
-                          or(liquidation_products["shoe_size"].in(params[:shoe_sizes]).
-                          or(liquidation_products["heel"].in(params[:heels])))).order('category asc').
-                          paginate(:page => params[:page], :per_page => 15)
-
-    end
 end
--- spec/controllers/liquidations_controller_spec.rb
+++ spec/controllers/liquidations_controller_spec.rb
@@ -3,7 +3,13 @@ require 'spec_helper'
 describe LiquidationsController do
 
   describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', :id => 1
+      response.should be_success
+    end
+  end
 
+  describe "GET 'update'" do
     let(:basic_shoe_size_35) { FactoryGirl.create(:basic_shoe_size_35) }
     let(:basic_shoe_size_37) { FactoryGirl.create(:basic_shoe_size_37) }
     let(:basic_shoe_size_40) { FactoryGirl.create(:basic_shoe_size_40) }
@@ -16,17 +22,12 @@ describe LiquidationsController do
 
     let(:liquidation) { FactoryGirl.create(:liquidation) }
 
-    it "returns http success" do
-      get 'show', :id => 1
-      response.should be_success
-    end
-
     context "isolated filters" do
       it "returns products given some subcategories" do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :subcategory_name => "melissa")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :subcategories => ["rasteirinha", "melissa"]
+        get :update, :format => :js,  :id => 2, :subcategories => ["rasteirinha", "melissa"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_40.product]
       end
 
@@ -34,7 +35,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :shoe_size => 45)
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :shoe_size => 35)
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :shoe_sizes => ["45", "35"]
+        get :update, :format => :js, :id => 2, :shoe_sizes => ["45", "35"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_40.product]
       end
 
@@ -42,7 +43,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :heel => 4.5)
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :heel => 6.5)
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :subcategory_name => "melissa")
-        get :show, :id => 2, :heels => ["6.5", "4.5"]
+        get :update, :format => :js, :id => 2, :heels => ["6.5", "4.5"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_40.product]
       end
     end
@@ -52,7 +53,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :heel => 4.5)
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_accessory_1.product.id, :subcategory_name => "pulseira")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_bag_1.product.id, :subcategory_name => "lisa")
-        get :show, :id => 2, :subcategories => ["pulseira", "lisa"], :heels => ["4.5"]
+        get :update, :format => :js, :id => 2, :subcategories => ["pulseira", "lisa"], :heels => ["4.5"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_bag_1.product, basic_accessory_1.product]
       end
     end
@@ -62,7 +63,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :shoe_size => "45")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :subcategories => ["rasteirinha"], :shoe_sizes => ["45"], :heels => ["5.6"]
+        get :update, :format => :js, :id => 2, :subcategories => ["rasteirinha"], :shoe_sizes => ["45"], :heels => ["5.6"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_40.product, basic_shoe_size_37.product]
       end
 
@@ -70,7 +71,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :shoe_size => "45")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :subcategories => ["rasteirinha"], :shoe_sizes => ["45"]
+        get :update, :format => :js, :id => 2, :subcategories => ["rasteirinha"], :shoe_sizes => ["45"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_40.product]
       end
 
@@ -78,7 +79,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :shoe_size => "45")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :subcategories => ["rasteirinha"], :heels => ["5.6"]
+        get :update, :format => :js, :id => 2, :subcategories => ["rasteirinha"], :heels => ["5.6"]
         assigns(:products).should == [basic_shoe_size_35.product, basic_shoe_size_37.product]
       end
 
@@ -86,7 +87,7 @@ describe LiquidationsController do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :shoe_size => "45")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_37.product.id, :heel => 5.6)
-        get :show, :id => 2, :shoe_sizes => ["45"], :heels => ["5.6"]
+        get :update, :format => :js, :id => 2, :shoe_sizes => ["45"], :heels => ["5.6"]
         assigns(:products).should == [basic_shoe_size_40.product, basic_shoe_size_37.product]
       end
     end
