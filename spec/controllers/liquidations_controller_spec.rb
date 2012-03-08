18dc334da2011460bebcdf5d634cd0fa6cfd3ad0
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-08 11:07:20 -0300

    assigning @liquidations

--- spec/controllers/liquidations_controller_spec.rb
+++ spec/controllers/liquidations_controller_spec.rb
@@ -2,27 +2,32 @@ require 'spec_helper'
 
 describe LiquidationsController do
 
+  let(:basic_shoe_size_35) { FactoryGirl.create(:basic_shoe_size_35) }
+  let(:basic_shoe_size_37) { FactoryGirl.create(:basic_shoe_size_37) }
+  let(:basic_shoe_size_40) { FactoryGirl.create(:basic_shoe_size_40) }
+
+  let(:basic_bag) { FactoryGirl.create(:basic_bag) }
+  let(:basic_accessory) { FactoryGirl.create(:basic_accessory) }
+
+  let(:basic_bag_1) { FactoryGirl.create(:basic_bag_simple, :product => basic_bag) }
+  let(:basic_accessory_1) { FactoryGirl.create(:basic_accessory_simple, :product => basic_accessory) }
+
+  let(:liquidation) { FactoryGirl.create(:liquidation) }
+
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show', :id => 1
+      get 'show', :id => liquidation
       response.should be_success
+    end
+
+    it "assigns @liquidation" do
+      get 'show', :id => liquidation.id
+      assigns(:liquidation).should eql(liquidation)
     end
   end
 
   describe "GET 'update'" do
-    let(:basic_shoe_size_35) { FactoryGirl.create(:basic_shoe_size_35) }
-    let(:basic_shoe_size_37) { FactoryGirl.create(:basic_shoe_size_37) }
-    let(:basic_shoe_size_40) { FactoryGirl.create(:basic_shoe_size_40) }
-
-    let(:basic_bag) { FactoryGirl.create(:basic_bag) }
-    let(:basic_accessory) { FactoryGirl.create(:basic_accessory) }
-
-    let(:basic_bag_1) { FactoryGirl.create(:basic_bag_simple, :product => basic_bag) }
-    let(:basic_accessory_1) { FactoryGirl.create(:basic_accessory_simple, :product => basic_accessory) }
-
-    let(:liquidation) { FactoryGirl.create(:liquidation) }
-
-    context "isolated filters" do
+   context "isolated filters" do
       it "returns products given some subcategories" do
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_35.product.id, :subcategory_name => "rasteirinha")
         LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_size_40.product.id, :subcategory_name => "melissa")
