7bb9ca5e05af74eb645c96695540d0554d46b927
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-09 16:55:50 -0300

    We cant use arel sintaxe because will_paginate have a bug

--- app/controllers/liquidations_controller.rb
+++ app/controllers/liquidations_controller.rb
@@ -4,28 +4,31 @@ class LiquidationsController < ApplicationController
 
   def show
     @liquidation = Liquidation.find(params[:id])
-    liquidation_products = LiquidationProduct.arel_table
-
     @liquidation_products = LiquidationProduct.joins(:product).
-                                              where(liquidation_products[:liquidation_id].eq(@liquidation.id)).
-                                              paginate(:page => params[:page], :per_page => 15).order('category asc').
-                                              group(liquidation_products[:product_id])
-
+                                               where("liquidation_products.liquidation_id = ?", @liquidation.id).
+                                               paginate(:page => params[:page], :per_page => 15).order('category asc').
+                                               group("liquidation_products.product_id")
     respond_with @liquidation_products
   end
 
   def update
     @liquidation = Liquidation.find(params[:id])
-    liquidation_products = LiquidationProduct.arel_table
 
-    @liquidation_products = LiquidationProduct.joins(:product).
-                                              where(liquidation_products[:liquidation_id].eq(@liquidation.id).
-                                              and(liquidation_products[:subcategory_name].in(params[:subcategories]).
-                                              or(liquidation_products[:shoe_size].in(params[:shoe_sizes]).
-                                              or(liquidation_products[:heel].in(params[:heels]))))).order('category asc').
-                                              group(liquidation_products[:product_id]).
-                                              paginate(:page => params[:page], :per_page => 15)
+    subcategories = params[:subcategories] if params[:subcategories]
+    shoe_sizes = params[:shoe_sizes] if params[:shoe_sizes]
+    heels = params[:heels] if params[:heels]
 
+
+    @liquidation_products = LiquidationProduct.joins(:product).where("liquidation_products.liquidation_id = ? AND
+                                                                     (liquidation_products.subcategory_name IN (?) OR
+                                                                      liquidation_products.shoe_size IN (?) OR
+                                                                      liquidation_products.heel IN (?))", params[:id],
+                                                                                                          subcategories,
+                                                                                                          shoe_sizes,
+                                                                                                          heels).
+                                                                      order('category asc').
+                                                                      group("liquidation_products.product_id").
+                                                                      paginate(:page => params[:page], :per_page => 12)
     respond_with @liquidation_products
   end
 end
