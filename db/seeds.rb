fca1ee57b80ff1a69761841f15e3216151147bb3
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-03-10 09:29:49 -0300

    Improved output messages for create permissions

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 [Admin, Profile, Question, Answer, Weight, SurveyAnswer].map(&:delete_all)
 
-Admin.create(:email => "admin@olook.com", :password =>"123456")
+Admin.create(:email => "admin@olook.com", :password =>"123456", :first_name => "administrator", :last_name => "olook")
+Role.create(:name => "superadministrator", :description => "Manages the whole system")
 
 survey = Survey.new(SURVEY_DATA).build
 
