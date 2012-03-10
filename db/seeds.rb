4ccdfae5619c66c7c55e60bb747e5f774e06ff3a
Felipe Jordao AP Mattosinho <felipe.japm@gmail.com>
2012-03-10 10:34:43 -0300

    Changed seeds file to include roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # -*- encoding : utf-8 -*-
 [Admin, Profile, Question, Answer, Weight, SurveyAnswer].map(&:delete_all)
 
-Admin.create(:email => "admin@olook.com", :password =>"123456", :first_name => "administrator", :last_name => "olook")
-Role.create(:name => "superadministrator", :description => "Manages the whole system")
+superadmin = Role.create(:name => "superadministrator", :description => "Manages the whole system")
+Admin.create(:email => "admin@olook.com", :password =>"123456", :first_name => "administrator", :last_name => "olook", :role_id => superadmin.id)
 
 survey = Survey.new(SURVEY_DATA).build
-
