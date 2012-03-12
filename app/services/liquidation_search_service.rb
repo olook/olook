13077df7643fb7d0125208c25475ecc81df0ef7d
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-12 17:00:10 -0300

    using LiquidationSearchService

---
+++ app/services/liquidation_search_service.rb
@@ -0,0 +1,28 @@
+class LiquidationSearchService
+  attr_reader :query_base, :params, :l_products
+
+  def initialize(params)
+    @l_products = LiquidationProduct.arel_table
+    @query_base = @l_products[:liquidation_id].eq(params[:id])
+    @params = params
+  end
+
+  def search_products
+    query_subcategories = params[:subcategories] ? l_products[:subcategory_name].in(params[:subcategories]) : nil
+    query_shoe_sizes = params[:shoe_sizes] ? build_sub_query((query_subcategories || query_base),l_products[:shoe_size].in(params[:shoe_sizes])) : nil
+    query_heels =  params[:heels] ? build_sub_query((query_shoe_sizes || query_subcategories || query_base), l_products[:heel].in(params[:heels])) : nil
+
+    queries = [query_heels,  query_shoe_sizes, query_subcategories]
+    query_result = queries.detect{|query| !query.nil?}
+    @query_base = query_base.and(query_result) if query_result
+
+    LiquidationProduct.joins(:product).where(query_base)
+                                      .group("product_id")
+                                      .order('category_id asc')
+                                      .paginate(:page => params[:page], :per_page => 12)
+  end
+
+  def build_sub_query(current_query, sub_query)
+    current_query == query_base ? sub_query : current_query.or(sub_query)
+  end
+end
