9f87ef73e2f69c9109b264961302a7820ec80cde
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-03-12 17:01:08 -0300

    deleting code

--- app/models/liquidation_product.rb
+++ app/models/liquidation_product.rb
@@ -7,34 +7,4 @@ class LiquidationProduct < ActiveRecord::Base
   delegate :color_name, :to => :product
   delegate :thumb_picture, :to => :product
   delegate :color_sample, :to => :product
-
-  def self.search_products(params)
-    liquidation_products = LiquidationProduct.arel_table
-
-    query_base = self.build_query_base(params)
-    query_subcategories = params[:subcategories] ? liquidation_products[:subcategory_name].in(params[:subcategories]) : nil
-    query_shoe_sizes    = params[:shoe_sizes] ? self.build_sub_query(query_base, (query_subcategories || query_base),liquidation_products[:shoe_size].in(params[:shoe_sizes])) : nil
-    query_heels =  params[:heels] ? self.build_sub_query(query_base, (query_shoe_sizes || query_subcategories || query_base), liquidation_products[:heel].in(params[:heels])) : nil
-    queries = [query_heels,  query_shoe_sizes, query_subcategories]
-    query_result = queries.detect{|query| !query.nil?}
-    query_base = query_base.and(query_result) if query_result
-
-    LiquidationProduct.joins(:product).where(query_base).group("product_id").order('category_id asc').paginate(:page => params[:page], :per_page => 12)
-
-
-
-  end
-
-  def self.build_query_base(params)
-    liquidation_products = LiquidationProduct.arel_table
-    liquidation_products[:liquidation_id].eq(params[:id])
-  end
-
-  def self.build_sub_query(base_query, current_query, sub_query)
-    if current_query == base_query
-      sub_query
-    else
-      current_query.or(sub_query)
-    end
-  end
 end
