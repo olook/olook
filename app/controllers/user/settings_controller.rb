df7e70b9524559ed96659b307273f67520cecd20
Joao Moura <joaomdmoura@gmail.com>
2012-03-16 18:31:06 -0300

    adding tests os products helpers, and fixing shoes size on vitrine

--- app/controllers/user/settings_controller.rb
+++ app/controllers/user/settings_controller.rb
@@ -6,6 +6,11 @@ class User::SettingsController < ApplicationController
   before_filter :authenticate_user!
   before_filter :load_user
 
+  def showroom
+    @questions = Question.includes(:answers)
+    @presenter = SurveyQuestions.new(@questions)
+  end
+
   def update_info
     current_user.user_info.update_attributes(params[:user_info])
     redirect_to "/conta/minha-vitrine"
---
+++ spec/helpers/products_helper.rb
@@ -0,0 +1,21 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe ProductsHelper do
+  with_a_logged_user do
+    let!(:user_info) { Factory.create(:user_info, user: user) }
+    let(:not_size_variant) { FactoryGirl.create(:basic_shoe_size_37) }
+    let(:sold_out_variant) { FactoryGirl.create(:basic_shoe_size_35, :inventory => 0) }
+    let(:normal_variant) { FactoryGirl.create(:basic_shoe_size_35 ) }
+    let(:product) { variant.product }
+    it "should return a string with class unavailable" do
+      helper.variant_classes( sold_out_variant ).should == "unavailable"
+    end
+    it "should return a string with selected class" do
+      helper.variant_classes( normal_variant ).should == "selected"
+    end
+    it "should return a empty string" do
+      helper.variant_classes( not_size_variant ).should == ""
+    end
+  end
+end
