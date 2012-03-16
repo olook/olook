c1b5bbcbed6ad92080055c3a04f84b283acf08fe
Hugo Borges <agaelebe@gmail.com>
2012-03-16 12:31:54 -0300

    Seeds should not destroy data, it's dangerous. Creating the admin the right way and using a different password.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # -*- encoding : utf-8 -*-
-[Admin, Profile, Question, Answer, Weight, SurveyAnswer].map(&:delete_all)
-
-superadmin = Role.create(:name => "superadministrator", :description => "Manages the whole system")
-Admin.create(:email => "admin@olook.com", :password =>"123456", :first_name => "administrator", :last_name => "olook", :role_id => superadmin.id)
+admin = Admin.new(:email => "admin@olook.com",
+                  :password =>"DifficultPassword123",
+                  :first_name => "administrator",
+                  :last_name => "olook")
+admin.role = superadmin
+admin.save!
 
 survey = Survey.new(SURVEY_DATA).build
