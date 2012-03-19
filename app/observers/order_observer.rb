9bb444f65b03153a92fbddd7515f948326c29a9a
Hugo Borges <agaelebe@gmail.com>
2012-03-19 19:32:37 -0300

    BUG: if a order is destroyed, the used credit must return to the user

--- app/observers/order_observer.rb
+++ app/observers/order_observer.rb
@@ -3,4 +3,10 @@ class OrderObserver < ActiveRecord::Observer
   def after_create(order)
     Resque.enqueue(Abacos::UpdateInventory)
   end
+
+  def before_destroy(order)
+    if order.credits > 0
+      Credit.add(order.credits, order.user, order)
+    end
+  end
 end
--- spec/observers/order_observer_spec.rb
+++ spec/observers/order_observer_spec.rb
@@ -8,4 +8,14 @@ describe OrderObserver do
      FactoryGirl.create(:order)
    end
   end
+
+  context "before destroy" do
+    it "returns credit to the user" do
+      user = FactoryGirl.create(:member)
+      order = FactoryGirl.create(:order, :credits => 5.3, :user => user)
+
+      Credit.should_receive(:add).with(5.3, user, order)
+      order.destroy
+    end
+  end
 end
