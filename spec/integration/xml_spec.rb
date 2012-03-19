c041880f7346e5872c98effd1dfe7245c202f29c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-03-19 15:29:45 -0300

    Updating xml click a porter

--- spec/integration/xml_spec.rb
+++ spec/integration/xml_spec.rb
@@ -31,5 +31,50 @@ feature "Show products on xml format" do
       </products>
       END
     end
+    context "in the mt_performance xml page" do
+    scenario "I want to see products of mt_perfomance" do
+      visit mt_performance_path
+      page.source.should == <<-END.gsub(/^ {6}/, '')
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <produtos>
+      <produto>
+      <nome>#{product.name}</nome>
+      <descricao>#{product.description}</descricao>
+      <url>http://www.olook.com.br/produto/#{product.id}?utm_campaign=remessaging&amp;utm_content=#{product.id}&amp;utm_medium=banner&amp;utm_source=criteo</url>
+      <imagem></imagem>
+      <marca>olook</marca>
+      <preco>#{product.price}</preco>
+      <preco_original>#{product.price}</preco_original>
+      <categoria>#{product.category}</categoria>
+      </produto>
+      </produtos>
+      END
+    end
+  end
+
+  context "in the click a porter xml page" do
+    scenario "I want to see products of click a porter" do
+      visit click_a_porter_path
+      page.source.should == <<-END.gsub(/^ {6}/, '')
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <produtos>
+      <produto>
+      <id_produto><![CDATA[#{product.id}]]></id_produto>
+      <link_produto><![CDATA[http://www.olook.com.br/produto/#{product.id}?utm_campaign=remessaging&amp;utm_content=#{product.id}&amp;utm_medium=banner&amp;utm_source=click_a_porter]]></link_produto>
+      <nome_produto><![CDATA[#{product.name}]]></nome_produto>
+      <marca><![CDATA[olook]]></marca>
+      <categoria><![CDATA[#{ product.category}]]></categoria>
+      <cores><cor><![CDATA[#{ product.color_name}]]></cor></cores>
+      <descricao><![CDATA[#{ product.description}]]></descricao>
+      <preco_de><![CDATA[#{ ActionController::Base.helpers.number_to_currency(product.price, :unit => "") }]]></preco_de>
+      <preco_por><![CDATA[#{ ActionController::Base.helpers.number_to_currency(product.price, :unit => "")}]]></preco_por>
+      <parcelamento><![CDATA[1]]></parcelamento>
+      <imagens>
+      </imagens>
+      </produto>
+      </produtos>
+      END
+    end
+  end
   end
 end
