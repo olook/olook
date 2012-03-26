62602d23f3a3f002b3e28ed536d3f33376e1b7dc
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-03-26 13:46:25 -0300

    removing code to give credit to user after order being deleted (BUG)

--- spec/observers/order_observer_spec.rb
+++ spec/observers/order_observer_spec.rb
@@ -3,19 +3,9 @@ require "spec_helper"
 
 describe OrderObserver do
   context "on create" do
-   it "should enqueue a job to update the inventory" do
-     Resque.should_receive(:enqueue).with(Abacos::UpdateInventory)
-     FactoryGirl.create(:order)
-   end
-  end
-
-  context "before destroy" do
-    it "returns credit to the user" do
-      user = FactoryGirl.create(:member)
-      order = FactoryGirl.create(:order, :credits => 5.3, :user => user)
-
-      Credit.should_receive(:add).with(5.3, user, order)
-      order.destroy
+    it "should enqueue a job to update the inventory" do
+      Resque.should_receive(:enqueue).with(Abacos::UpdateInventory)
+      FactoryGirl.create(:order)
     end
   end
 end
