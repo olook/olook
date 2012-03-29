f7d4d350416c13769ca23ec2df70e5a8a34f0754
Felipe JAPM <felipe.japm@gmail.com>
2012-03-29 17:28:14 -0300

    Fixed admin roles update

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,9 +1,10 @@
 # -*- encoding : utf-8 -*-
 class Admin < ActiveRecord::Base
 
-  attr_accessible :role_attributes
+  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me,
+                  :role_id
   belongs_to :role
-  accepts_nested_attributes_for :role
+
 
   has_paper_trail :on => [:update, :destroy]
 
@@ -13,16 +14,11 @@ class Admin < ActiveRecord::Base
 
 
   devise :database_authenticatable, :rememberable, :trackable, :validatable, :timeoutable, :lockable
-  attr_accessible :first_name, :last_name, :email, :password, :password_confirmation, :remember_me, :role_attributes
 
   validates :role_id, :presence => true
   validates :email, :format => {:with => EmailFormat}
   validates :first_name, :presence => true, :format => { :with => NameFormat }
   validates :last_name, :presence => true, :format => { :with => NameFormat }
-
-  def role_attributes=(attr)
-    self.role = Role.find_by_id(attr["id"])
-  end  
 
   def has_role?(role_name)
     self.role.name.to_sym == role_name if self.role
