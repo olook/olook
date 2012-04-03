7bef0920e3eaef4bd7fbcd3f4834a5fdfc540dea
joao paulo <jpcamargo+github@gmail.com>
2012-04-03 18:39:28 -0300

    changed whitespace in some spec files, and added a block of code in the occasions controller

--- app/controllers/gift/occasions_controller.rb
+++ app/controllers/gift/occasions_controller.rb
@@ -1,5 +1,22 @@
 class Gift::OccasionsController < ApplicationController
   def new
+    @occasion = GiftOccasion.new
+
+    # collections for selects
+    @occasion_types = GiftOccasionTypes.all
+    @recipient_relations = GiftRecipientRelations.all
+  end
+
+  def create
+    @occasion = GiftOccasion.new(params[:occasion])
+
+    if @goccasion.save
+      # saved
+
+    else
+      # errors
+
+    end
   end
 
 end
--- spec/models/gift_occasion_type_spec.rb
+++ spec/models/gift_occasion_type_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
 
 describe GiftOccasionType do
-  describe "validation" do
-    it { should validate_presence_of(:name) }
+  context "validation" do
+    it { should validate_presence_of :name }
   end
+
+  
 end
