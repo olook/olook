5e620c025daebec983bed6a6481e55b6783a8484
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-05 15:00:46 -0300

    adding association between survey and questions

--- spec/models/survey_spec.rb
+++ spec/models/survey_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Survey do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many :questions }
+  end
 end
