d110e464a1c5001ca6eee5c9a80803d696175b68
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-05 11:12:17 -0300

    xml fixes: removing space before '?', adding cdata, replacing preco_original and using new helpers

--- spec/integration/xml_spec.rb
+++ spec/integration/xml_spec.rb
@@ -7,14 +7,14 @@ feature "Show products on xml format" do
   let!(:product) { FactoryGirl.create :basic_shoe }
 
   background do
-    product.master_variant.update_attribute(:price, 1.0)
+    product.master_variant.update_attribute(:price, 99.90)
   end
 
   context "in the criteo xml page" do
     scenario "I want to see products of criteo" do
       visit criteo_path
       page.source.should == <<-END.gsub(/^ {6}/, '')
-      <?xml version="1.0" encoding="UTF-8" ?>
+      <?xml version="1.0" encoding="UTF-8"?>
       <products>
       <product id="#{product.id}">
       <name>#{product.name}</name>
@@ -37,17 +37,17 @@ feature "Show products on xml format" do
     scenario "I want to see products of mt_perfomance" do
       visit mt_performance_path
       page.source.should == <<-END.gsub(/^ {6}/, '')
-      <?xml version="1.0" encoding="UTF-8" ?>
+      <?xml version="1.0" encoding="UTF-8"?>
       <produtos>
       <produto>
-      <nome>#{product.name}</nome>
-      <descricao>#{product.description}</descricao>
+      <nome><![CDATA[#{product.name}]]></nome>
+      <descricao><![CDATA[#{product.description}]]></descricao>
       <url>http://www.olook.com.br/produto/#{product.id}?utm_campaign=produtos&amp;utm_content=#{product.id}&amp;utm_medium=vitrine&amp;utm_source=mt_performance</url>
       <imagem></imagem>
       <marca>olook</marca>
-      <preco>#{product.price}</preco>
-      <preco_original>#{product.retail_price}</preco_original>
-      <categoria>#{Category.t(product.category)} #{product.subcategory}</categoria>
+      <preco>#{ActionController::Base.helpers.number_with_precision(product.retail_price, :precision => 2)}</preco>
+      <preco_original>#{ActionController::Base.helpers.number_with_precision(product.price, :precision => 2)}</preco_original>
+      <categoria>#{Category.t(product.category)}</categoria>
       </produto>
       </produtos>
       END
@@ -58,19 +58,19 @@ feature "Show products on xml format" do
     scenario "I want to see products of click a porter" do
       visit click_a_porter_path
       page.source.should == <<-END.gsub(/^ {6}/, '')
-      <?xml version="1.0" encoding="UTF-8" ?>
+      <?xml version="1.0" encoding="UTF-8"?>
       <produtos>
       <produto>
       <id_produto><![CDATA[#{product.id}]]></id_produto>
       <link_produto><![CDATA[http://www.olook.com.br/produto/#{product.id}?utm_campaign=produtos&amp;utm_content=#{product.id}&amp;utm_medium=vitrine&amp;utm_source=click_a_porter]]></link_produto>
       <nome_produto><![CDATA[#{product.name}]]></nome_produto>
       <marca><![CDATA[olook]]></marca>
-      <categoria><![CDATA[#{Category.t(product.category)} #{product.subcategory}]]></categoria>
+      <categoria><![CDATA[#{Category.t(product.category)}]]></categoria>
       <cores><cor><![CDATA[#{ product.color_name}]]></cor></cores>
       <descricao><![CDATA[#{ product.description}]]></descricao>
-      <preco_de><![CDATA[#{ ActionController::Base.helpers.number_to_currency(product.price, :unit => "") }]]></preco_de>
-      <preco_por><![CDATA[#{ ActionController::Base.helpers.number_to_currency(product.price, :unit => "")}]]></preco_por>
-      <parcelamento><![CDATA[1]]></parcelamento>
+      <preco_de><![CDATA[#{ ActionController::Base.helpers.number_with_precision(product.price, :precision => 2) }]]></preco_de>
+      <preco_por><![CDATA[#{ ActionController::Base.helpers.number_with_precision(product.retail_price, :precision => 2)}]]></preco_por>
+      <parcelamento><![CDATA[3 x 33,30]]></parcelamento>
       <imagens>
       </imagens>
       </produto>
