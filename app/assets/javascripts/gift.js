c4818d538477dd8e7b309e2ef01f37fc988669ea
Raul Souza Lima <raulsouzalima@gmail.com>
2012-04-08 01:13:21 -0300

    Added preloader on calendar friends

--- app/assets/javascripts/gift.js
+++ app/assets/javascripts/gift.js
@@ -2,6 +2,14 @@ $(function () {
   $("div#calendar ul#months li a").live("click", function(event) {
     $("div#calendar ul#months li a").removeClass("selected");
     $(this).addClass("selected");
+    InitGift.friendsPreloader();
     event.preventDefault();
   });
 });
+
+InitGift = {
+  friendsPreloader : function() {
+    $("div#birthdays_list ul.friends_list").remove();
+    $("div#birthdays_list").html("<div class='preloader'></div>");
+  }
+}
