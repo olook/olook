f7e7103ee5cc15ac739f84e701d4b6903e9c2bf4
Fabiano Beselga <fabianobeselga@gmail.com>
2012-04-10 14:43:45 -0300

    added facebook permissions field to users

--- spec/controllers/user/users_controller_spec.rb
+++ spec/controllers/user/users_controller_spec.rb
@@ -13,7 +13,7 @@ describe User::UsersController do
 
   describe "DELETE destroy_facebook_account" do
     it "should destroy the facebook account removing the user uid and facebook_token" do
-      User.any_instance.should_receive(:update_attributes).with(:facebook_token => nil, :uid => nil, :has_facebook_extended_permission => false)
+      User.any_instance.should_receive(:update_attributes).with(:facebook_token => nil, :uid => nil, :facebook_permissions => [])
       delete :destroy_facebook_account
     end
   end
