76a1775775cf7c4eb51b6ad53c8357d2b1e6a360
joao paulo <jpcamargo+github@gmail.com>
2012-04-10 16:44:20 -0300

    added comments related to the errors people will get if the gift/occasions/new view is not fixed

--- app/controllers/gift/occasions_controller.rb
+++ app/controllers/gift/occasions_controller.rb
@@ -11,6 +11,8 @@ class Gift::OccasionsController < ApplicationController
   end
 
   def create
+    #FIXME: if you are getting an error saying the route is wrong, please make sure the form is posting to /gift/occasions
+    #FIXME: if you are getting an error related to string vs. integer association, please refer to the new/view, and add _id to the end of the gift_occasion_type and gift_recipient_relation fields.
     @recipient = GiftRecipient.new(params[:recipient].merge(:user_id => current_user ? current_user : nil))
     @occasion = GiftOccasion.new(params[:occasion].merge(:user_id => current_user ? current_user : nil, :gift_recipient_id => @recipient))
     
