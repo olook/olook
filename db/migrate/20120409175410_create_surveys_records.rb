481227b571df598070ac247ace507c42daa9c720
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-10 16:27:39 -0300

    the migration should create only one survey. Find or create is better than only creating.

--- db/migrate/20120409175410_create_surveys_records.rb
+++ db/migrate/20120409175410_create_surveys_records.rb
@@ -1,8 +1,7 @@
 class CreateSurveysRecords < ActiveRecord::Migration
   def up
-    survey = Survey.create(:name => "Registration Survey")
+    survey = Survey.find_or_create_by_name("Registration Survey")
     Question.update_all(:survey_id => survey.id) if survey
-    Survey.create!(:name => "Gift Survey")
   end
 
   def down
