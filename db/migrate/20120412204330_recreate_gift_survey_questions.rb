a366fcc9d0b0f1e8ad51ffb7a92093b54d45192d
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-12 18:00:22 -0300

    Recreating gift survey questions

---
+++ db/migrate/20120412204330_recreate_gift_survey_questions.rb
@@ -0,0 +1,11 @@
+class RecreateGiftSurveyQuestions < ActiveRecord::Migration
+  def up
+	  survey = Survey.find_by_name("Gift Survey")
+	  survey.questions.destroy_all
+    SurveyBuilder.new(GIFT_SURVEY_DATA, "Gift Survey").build
+  end
+
+  def down
+    Survey.find_by_name("Gift Survey").destroy
+  end
+end
