43456690d8c1ab0e073397acff8264dd0ca42b05
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-13 14:32:26 -0300

    update shoe_size and profile

--- app/controllers/gift/recipients_controller.rb
+++ app/controllers/gift/recipients_controller.rb
@@ -1,31 +1,28 @@
 # -*- encoding : utf-8 -*-
 class Gift::RecipientsController < Gift::BaseController
   # TO DO:
-  # - check if user is logged in
-  # - get shoe size
-  # - give the option to change the profile_id in new and edit
-  # - redirect to suggestion page on success
-  def new
+  # - check if current_user.id is equal to gift_recipient.user_id
+  before_filter :load_recipient
+
+  def edit
     profiles = session[:recipient_profiles]
     if profiles.present?
       @profiles = Profile.find(*profiles)
-      @gift_recipient = GiftRecipient.update_profile_and_shoe_size(session[:recipient_id], @profiles.first)
+      @gift_recipient.update_attributes!(:profile => @profiles.first) unless @gift_recipient.profile
     else
       redirect_to gift_root_path
     end
   end
 
-  def create
-    # profile = Profile.find(params[:gift_recipient][:profile])
-    # shoe_size = params[:gift_recipient][:shoe_size]
-    # GiftRecipient.update_profile_and_shoe_size(session[:recipient_id], profile)
-    #flash[:notice] = "As informações do seu presenteado foram atualizadas com sucesso"
+  def update
+    shoe_size_and_profile = params[:gift_recipient].slice(:shoe_size, :profile_id) if params[:gift_recipient].present?
+    @gift_recipient.update_attributes!(shoe_size_and_profile) if shoe_size_and_profile
     redirect_to gift_root_path
   end
 
-  def update
-  end
+  private
 
-  def edit
+  def load_recipient
+    @gift_recipient = GiftRecipient.find(params[:id])
   end
 end
