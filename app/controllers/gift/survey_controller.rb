2ddb9126262b5946e2b32dbf0399792490baf8cc
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-13 09:24:14 -0300

    refactoring survey and adding method to get recipient

--- app/controllers/gift/survey_controller.rb
+++ app/controllers/gift/survey_controller.rb
@@ -1,22 +1,31 @@
 # -*- encoding : utf-8 -*-
 class Gift::SurveyController < Gift::BaseController
+  before_filter :load_recipient
+  
   def new
-    @questions = Question.from_gift_survey
-    @presenter = GiftSurveyQuestions.new(@questions)
-    # get gift recipient relation (amiga, mãe, namorada, etc) to use on questions (and create a helper)
+    if @recipient
+      @questions = Question.from_gift_survey
+      @presenter = GiftSurveyQuestions.new(@questions)
+    else
+      redirect_to new_gift_survey_path
+    end
   end
 
   def create
     questions = params[:questions]
-    recipient_id = session[:recipient_id]
-    recipient = GiftRecipient.find(recipient_id) if recipient_id
-    if questions.present? && recipient.present?
+    if questions.present? && @recipient
       session[:questions] = questions
       session[:recipient_profiles] = ProfileBuilder.ordered_profiles(questions)
       redirect_to new_gift_recipient_path
     else
       redirect_to new_gift_survey_path
     end
+  end
+  
+  private
+  
+  def load_recipient
+    @recipient = GiftRecipient.find(session[:recipient_id]) if session[:recipient_id].present?
   end
 
 end
--- spec/controllers/gift/survey_controller_spec.rb
+++ spec/controllers/gift/survey_controller_spec.rb
@@ -1,35 +1,53 @@
 require 'spec_helper'
 
 describe Gift::SurveyController do
-
+  let!(:recipient) { FactoryGirl.create(:gift_recipient) }
+  
+  before do
+    session[:recipient_id] = recipient.id
+  end
+  
   describe "GET 'new'" do
     let(:questions) { [ FactoryGirl.create(:question) ]}
-
-    it "returns http success" do
-      get 'new'
-      response.should be_success
+    
+    context "when no gift recipient is found in the session" do
+      it "redirects to new_survey" do
+        session[:recipient_id] = nil
+        post 'new'
+        response.should redirect_to new_gift_survey_path
+      end
     end
-
-    it "gets all questions and answers from gift survey and assigns to question" do
-      Question.should_receive(:from_gift_survey).and_return(questions)
+    
+    it "find the recipient by id and assigns to @recipient" do
+      GiftRecipient.should_receive(:find).with(session[:recipient_id]).and_return(recipient)
       get 'new'
-      assigns(:questions).should eq questions
+      assigns(:recipient).should == recipient 
     end
+    
+    context "with the recipient found" do
+      before do
+        GiftRecipient.stub(:find).and_return(recipient)
+      end
+      
+      it "gets all questions and answers from gift survey and assigns to question" do
+        Question.should_receive(:from_gift_survey).and_return(questions)
+        get 'new'
+        assigns(:questions).should eq questions
+      end
 
-    it "instantiates @presenter with survey questions" do
-      presenter = double(:presenter)
-      Question.stub(:from_gift_survey).and_return(questions)
-      GiftSurveyQuestions.should_receive(:new).with(questions).and_return(presenter)
-      get 'new'
-      assigns(:presenter).should eq presenter
+      it "instantiates @presenter with survey questions" do
+        presenter = double(:presenter)
+        Question.stub(:from_gift_survey).and_return(questions)
+        GiftSurveyQuestions.should_receive(:new).with(questions).and_return(presenter)
+        get 'new'
+        assigns(:presenter).should eq presenter
+      end
     end
-
   end
 
   describe "POST 'create'" do
     let(:questions) {  [{"foo" => "bar"}, {"baz" => "bar"}]  }
     let(:ordered_profiles) { [3,2,1] }
-    let(:recipient) { FactoryGirl.create(:gift_recipient) }
 
     context "when no questions param is received" do
       it "redirects to new_survey" do
@@ -38,10 +56,15 @@ describe Gift::SurveyController do
       end
     end
 
-    context "when questions param is received" do
+    context "when questions param is received and the recipient is found" do
+      
+      before do
+        GiftRecipient.stub(:find).and_return(recipient)
+      end
 
       context "when no gift recipient is found in the session" do
         it "redirects to new_survey" do
+          session[:recipient_id] = nil
           post 'create', :questions => questions
           response.should redirect_to new_gift_survey_path
         end
@@ -51,6 +74,14 @@ describe Gift::SurveyController do
         before do
           session[:recipient_id] = recipient.id
         end
+        
+        it "find the recipient by id and assigns to @recipient" do
+          ProfileBuilder.stub(:ordered_profiles).and_return(ordered_profiles)
+          
+          GiftRecipient.should_receive(:find).with(session[:recipient_id]).and_return(recipient)
+          post 'create', :questions => questions
+          assigns(:recipient).should == recipient 
+        end
 
         it "keeps the questions and answers in the session" do
           ProfileBuilder.stub(:ordered_profiles).and_return(ordered_profiles)
@@ -59,7 +90,7 @@ describe Gift::SurveyController do
           session[:questions].should == questions
         end
 
-        it "keeps the recipient profile in the session" do
+        it "keeps the recipient profiles in the session" do
           ProfileBuilder.should_receive(:ordered_profiles).and_return(ordered_profiles)
 
           post 'create', :questions => questions
