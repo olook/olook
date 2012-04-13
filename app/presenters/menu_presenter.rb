87eee34f6dec836eddeb7fe56fe205bf2133b0b6
Fabiano Beselga <fabianobeselga@gmail.com>
2012-04-13 15:48:52 -0300

    refactoring menu

--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -1,7 +1,22 @@
 class MenuPresenter < BasePresenter
+  
   def render_item label, path, css_klass, hightlight_when
     h.content_tag(:li, h.link_to(label, path, :class => h.selected_if_current(hightlight_when)), :class => css_klass)
   end
+  
+  def render_menu
+    user.half_user ? render_half_user_menu : render_default_menu
+  end
+  
+  def render_default_menu
+    [showroom, lookbooks, stylist, my_friends, invite, gift, liquidation, blog, cart].join.html_safe
+  end
+  
+  def render_half_user_menu
+    [lookbooks, stylist, my_friends, invite, gift, liquidation, blog, cart].join.html_safe
+  end
+  
+  private
   
   def showroom
     render_item("Minha Vitrine", h.member_showroom_path, "showroom", ["members#showroom"]) if user.has_early_access?
@@ -23,6 +38,7 @@ class MenuPresenter < BasePresenter
     render_item("Convidar amigas", h.member_invite_path, "invite", ["members#invite"])
   end
   
+  #TODO: highlight gift
   def gift
     render_item("Presentes", h.gift_root_path, "gift", [])
   end
