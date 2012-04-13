f7970d57a68fcb82ab6a83799d513cf4b2fa2f7b
joao paulo <jpcamargo+github@gmail.com>
2012-04-13 16:35:53 -0300

    changed some whitespaces and parenthesis in the code

--- app/models/abacos/item.rb
+++ app/models/abacos/item.rb
@@ -4,20 +4,20 @@ module Abacos
     attr_reader :codigo, :quantidade, :preco_unitario, :preco_unitario_bruto, :embalagem_presente
 
     def initialize(line_item, gift=false)
-      @codigo                 = line_item.variant.number
-      @quantidade             = line_item.quantity
-      @preco_unitario         = "%.2f" % ( ( line_item.variant.liquidation? ) ? line_item.retail_price : line_item.price )
-      @preco_unitario_bruto   = "%.2f" % line_item.price
-      @embalagem_presente     = line_item.is_gift
+      @codigo               = line_item.variant.number
+      @quantidade           = line_item.quantity
+      @preco_unitario       = "%.2f" % (line_item.variant.liquidation? ? line_item.retail_price : line_item.price)
+      @preco_unitario_bruto = "%.2f" % line_item.price
+      @embalagem_presente   = line_item.is_gift
     end
-
+    
     def parsed_data
       {
-        'CodigoProduto'       => @codigo,
-        'QuantidadeProduto'   => @quantidade,
-        'PrecoUnitario'       => @preco_unitario,
-        'PrecoUnitarioBruto'  => @preco_unitario_bruto,
-        'EmbalagemPresente'   => @embalagem_presente
+        'CodigoProduto'      => @codigo,
+        'QuantidadeProduto'  => @quantidade,
+        'PrecoUnitario'      => @preco_unitario,
+        'PrecoUnitarioBruto' => @preco_unitario_bruto,
+        'EmbalagemPresente'  => @embalagem_presente
       }
     end
   end
