84ab5d804c16e0e46179e4d3251be9ad91f6bb20
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-13 10:45:38 -0300

    changing gift survey view

--- app/presenters/gift_survey_questions.rb
+++ app/presenters/gift_survey_questions.rb
@@ -7,22 +7,11 @@ class GiftSurveyQuestions
   end
 
   def common_questions
-    questions[0..3]
-  end
-
-  def heel_height_question
-    questions[4]
-  end
-
-  def shoe_size_question
-    questions[5]
+    questions[0..6]
   end
 
   def id_first_question
     questions.first.id if questions.size > 0
   end
 
-  def heel_height_question_number
-    22
-  end
 end
