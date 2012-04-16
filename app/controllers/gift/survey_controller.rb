d72b7c0701948fc3519e081bf2e40969d63d98cf
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-16 19:48:48 -0300

    survey controller now updates gift_recipient ranked_profile_ids

--- app/controllers/gift/survey_controller.rb
+++ app/controllers/gift/survey_controller.rb
@@ -3,7 +3,7 @@ class Gift::SurveyController < Gift::BaseController
   before_filter :load_recipient
   
   def new
-    if @recipient
+    if @gift_recipient
       @questions = Question.from_gift_survey
       @presenter = GiftSurveyQuestions.new(@questions)
     else
@@ -13,10 +13,9 @@ class Gift::SurveyController < Gift::BaseController
 
   def create
     questions = params[:questions]
-    if questions.present? && @recipient
-      session[:questions] = questions
-      session[:recipient_profiles] = ProfileBuilder.ordered_profiles(questions)
-      redirect_to edit_gift_recipient_path(@recipient)
+    if questions.present? && @gift_recipient
+      @gift_recipient.update_attributes!(:ranked_profile_ids => ProfileBuilder.ordered_profiles(questions))
+      redirect_to edit_gift_recipient_path(@gift_recipient)
     else
       redirect_to new_gift_survey_path
     end
@@ -25,7 +24,7 @@ class Gift::SurveyController < Gift::BaseController
   private
   
   def load_recipient
-    @recipient = GiftRecipient.find(session[:recipient_id]) if session[:recipient_id].present?
+    @gift_recipient = GiftRecipient.find(session[:recipient_id]) if session[:recipient_id].present?
   end
 
 end
