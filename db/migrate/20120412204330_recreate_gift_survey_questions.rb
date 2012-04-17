5b3bbdab71371ee228f8aae0fa739666a43e6460
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-16 22:00:49 -0300

    lets avoid nil

--- db/migrate/20120412204330_recreate_gift_survey_questions.rb
+++ db/migrate/20120412204330_recreate_gift_survey_questions.rb
@@ -1,8 +1,8 @@
 # -*- encoding : utf-8 -*-
 class RecreateGiftSurveyQuestions < ActiveRecord::Migration
   def up
-	  survey = Survey.find_by_name("Gift Survey")
-	  survey.questions.destroy_all
+    survey = Survey.find_by_name("Gift Survey")
+    survey.questions.destroy_all if survey
     SurveyBuilder.new(GIFT_SURVEY_DATA, "Gift Survey").build
   end
 
