318c06f43fe7c1f9d71950d92baf5b42bf835416
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-18 10:23:21 -0300

    finding gift recipient and occasion and adding real data to card view

--- app/controllers/gift/suggestions_controller.rb
+++ app/controllers/gift/suggestions_controller.rb
@@ -1,5 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Gift::SuggestionsController < Gift::BaseController
+  
   def index
+    @gift_recipient = GiftRecipient.find(params[:recipient_id])
+    @occasion = GiftOccasion.find(session[:occasion_id])
   end
 end
--- spec/controllers/gift/suggestions_controller_spec.rb
+++ spec/controllers/gift/suggestions_controller_spec.rb
@@ -1,12 +1,41 @@
 require 'spec_helper'
 
 describe Gift::SuggestionsController do
-
+  
+  let!(:recipient) { FactoryGirl.create(:gift_recipient) }
+  let!(:occasion) { FactoryGirl.create(:gift_occasion) }
+  
   describe "GET 'index'" do
-    it "returns http success" do
-      get 'index'
-      response.should be_success
+    before do
+      session[:occasion_id] = occasion.id
     end
+    
+    context "gift_recipient" do
+      it "finds gift recipient" do
+        GiftRecipient.should_receive(:find).with("123").and_return(recipient)
+        get 'index', :recipient_id => "123"
+      end
+
+      it "assigns @gift_recipient to the recipient found" do
+        get 'index', :recipient_id => recipient.id.to_s
+        assigns(:gift_recipient).should == recipient
+      end
+    end
+    
+    context "occasion" do
+      it "finds the occasion from the session" do
+        session[:occasion_id] = "456"
+        GiftOccasion.should_receive(:find).with("456").and_return(occasion)
+        get 'index', :recipient_id => recipient.id
+      end
+      
+      it "assigns @occasion to the found occasion" do
+        GiftOccasion.stub(:find).and_return(occasion)
+        get 'index', :recipient_id => recipient.id
+        assigns(:occasion).should == occasion
+      end
+    end
+
   end
 
 end
