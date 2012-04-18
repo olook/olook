d7aa78908480af70d5dfc889cd501b946bc7be22
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-18 10:22:17 -0300

    suggestion is now nested into recipient

--- app/controllers/gift/recipients_controller.rb
+++ app/controllers/gift/recipients_controller.rb
@@ -2,7 +2,6 @@
 class Gift::RecipientsController < Gift::BaseController
   # TO DO:
   # - hide recipient id (in session or via post)
-  # - make route to post to edit action
   before_filter :load_recipient
 
   def edit
@@ -14,13 +13,13 @@ class Gift::RecipientsController < Gift::BaseController
     profile_and_shoe = params[:gift_recipient].slice(:shoe_size, :profile_id) if params.include?(:gift_recipient)
     if profile_and_shoe && profile_and_shoe[:shoe_size].present?
       @gift_recipient.update_attributes!(profile_and_shoe) if @gift_recipient.belongs_to_user?(current_user)
-      redirect_to gift_suggestions_path
+      redirect_to gift_recipient_suggestions_path(@gift_recipient)
     else
       flash[:notice] = "Por favor, escolha o número de sapato da sua presenteada."
       redirect_to edit_gift_recipient_path(@gift_recipient)
     end
   end
-
+  
   private
 
   def load_recipient
