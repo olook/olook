19a691ec19968c944ac1fcda7efaa01b53c5f980
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-19 19:08:37 -0300

    test for select_gift xhr action

--- spec/controllers/gift/suggestions_controller_spec.rb
+++ spec/controllers/gift/suggestions_controller_spec.rb
@@ -4,6 +4,7 @@ describe Gift::SuggestionsController do
   
   let!(:recipient) { FactoryGirl.create(:gift_recipient) }
   let!(:occasion) { FactoryGirl.create(:gift_occasion) }
+  let!(:product) { FactoryGirl.create(:basic_bag) }
   
   describe "GET 'index'" do
     before do
@@ -35,7 +36,18 @@ describe Gift::SuggestionsController do
         assigns(:occasion).should == occasion
       end
     end
-
+  end
+  
+  context "GET 'select_gift'" do
+    it "finds product with product id" do
+      Product.should_receive(:find).with("123").and_return(product)
+      xhr :get, 'select_gift', :product_id => "123"
+    end
+    
+    it "assigns @product to the product found" do
+      xhr :get, 'select_gift', :product_id => product.id
+      assigns(:product).should == product
+    end
   end
 
 end
