b581ee23e0f0389fc40b818bd1b58807a04ab88d
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-04-19 20:12:07 -0300

    fixed tests

--- spec/controllers/gift/suggestions_controller_spec.rb
+++ spec/controllers/gift/suggestions_controller_spec.rb
@@ -4,6 +4,7 @@ describe Gift::SuggestionsController do
 
   let!(:recipient) { FactoryGirl.create(:gift_recipient) }
   let!(:occasion) { FactoryGirl.create(:gift_occasion) }
+  let!(:product) { FactoryGirl.create(:basic_bag) }
 
   describe "GET 'index'" do
     before do
@@ -37,6 +38,27 @@ describe Gift::SuggestionsController do
         assigns(:occasion).should == occasion
       end
     end
+
+    context "suggested products" do
+      let(:product_finder_service) { mock }
+
+      it "assigns @suggested_products as a list of suggested products" do
+        product_finder_service.stub(:showroom_products)
+        ProductFinderService.should_receive(:new).with(recipient).and_return(product_finder_service)
+        product_finder_service.should_receive(:suggested_products_for).
+          with(recipient.profile, recipient.shoe_size).and_return(:suggested_products)
+        get 'index', :recipient_id => recipient.id
+        assigns(:suggested_products).should == :suggested_products
+      end
+
+      it "assigns @products" do
+        product_finder_service.stub(:suggested_products_for)
+        ProductFinderService.should_receive(:new).with(recipient).and_return(product_finder_service)
+        product_finder_service.should_receive(:showroom_products).and_return(:showroom_products)
+        get 'index', :recipient_id => recipient.id
+        assigns(:products).should == :showroom_products
+      end
+    end
   end
 
   context "GET 'select_gift'" do
@@ -49,25 +71,5 @@ describe Gift::SuggestionsController do
       xhr :get, 'select_gift', :product_id => product.id
       assigns(:product).should == product
    end
-  end
-
-  context "suggested products" do
-    let(:product_finder_service) { mock }
-    it "assigns @suggested_products as a list of suggested products" do
-      product_finder_service.stub(:showroom_products)
-      ProductFinderService.should_receive(:new).with(recipient).and_return(product_finder_service)
-      product_finder_service.should_receive(:suggested_products_for).
-        with(recipient.profile, recipient.shoe_size).and_return(:suggested_products)
-      get 'index', :recipient_id => recipient.id
-      assigns(:suggested_products).should == :suggested_products
-    end
-
-    it "assigns @products" do
-      product_finder_service.stub(:suggested_products_for)
-      ProductFinderService.should_receive(:new).with(recipient).and_return(product_finder_service)
-      product_finder_service.should_receive(:showroom_products).and_return(:showroom_products)
-      get 'index', :recipient_id => recipient.id
-      assigns(:products).should == :showroom_products
-    end
   end
 end
