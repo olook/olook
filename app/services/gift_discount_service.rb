8e8e4cfc2e839ab7119841d39966998b00808349
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-19 12:48:08 -0300

    refactor to receive list of product_ids instead of products, renaming total_discount_price to total_price_for_products

--- app/services/gift_discount_service.rb
+++ app/services/gift_discount_service.rb
@@ -1,14 +1,16 @@
+
 class GiftDiscountService
-  def self.total_price_with_discount(products)
-    products.map(&:retail_price).zip(percents).inject(0) { |sum, price| sum += price.first * price.second }
+  def self.total_price_for_products(product_ids)
+    product_ids.collect.with_index { |product_id,index| price_for_product(product_id,index) }.sum
   end
 
   def self.price_for_product(product_id, position = 0)
     product = Product.find(product_id)
     (product.retail_price * percents[position]) if product
   end
-
+  
+  # Percent of the price to be considered according to the product position
   def self.percents
-    [1.0 , 0.8, 0.6]
+    [BigDecimal.new("1.0"), BigDecimal.new("0.8"), BigDecimal.new("0.6")]
   end
 end
