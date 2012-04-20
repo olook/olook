b9be6dccad445bbdbd58ec4a02715f1d9bed659f
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-20 15:02:24 -0300

    added some tests

--- spec/controllers/gift/home_controller_spec.rb
+++ spec/controllers/gift/home_controller_spec.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'spec_helper'
 
 describe Gift::HomeController do
@@ -5,11 +6,21 @@ describe Gift::HomeController do
   let(:user) { FactoryGirl.create :user }
 
   describe "GET 'index'" do
-    #it "returns http success" do
-      #sign_in user
-      #get 'index'
-      #response.should be_success
-    #end
+    it "returns http success" do
+      sign_in user
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'update_birthdays_by_month'" do
+    context "when facebook adapter is not available" do
+      it "assigns nil to @frinds" do
+        xhr :get, 'update_birthdays_by_month'
+        assigns(:friends).should be_nil
+        response.should be_success
+      end
+    end
   end
 
 end
