04bdde7a07ab01e6d0a1ddb8eddb2a4888a00801
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-04-24 11:42:43 -0300

    dont showing sold out products

--- app/controllers/gift/suggestions_controller.rb
+++ app/controllers/gift/suggestions_controller.rb
@@ -5,7 +5,7 @@ class Gift::SuggestionsController < Gift::BaseController
     @occasion = GiftOccasion.find(session[:occasion_id])
     product_finder_service = ProductFinderService.new(@gift_recipient)
     @suggested_products = product_finder_service.suggested_products_for(@gift_recipient.profile, @gift_recipient.shoe_size)
-    @products = product_finder_service.showroom_products
+    @products = product_finder_service.showroom_products(nil, @gift_recipient.shoe_size)
   end
 
   def select_gift
