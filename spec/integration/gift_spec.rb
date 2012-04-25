d5b530d9376cc396f06d91832c4f63ff9e4546f9
Fabiano Beselga <fabianobeselga@gmail.com>
2012-04-25 18:56:36 -0300

    change test on integration test to meet the new text

--- spec/integration/gift_spec.rb
+++ spec/integration/gift_spec.rb
@@ -27,7 +27,7 @@ feature "Buying Gifts", %q{
       end
 
       scenario "starting the process of creating a gift" do
-        page.should have_content("Você está criando um presente")
+        page.should have_content("Estou escolhendo um presente")
       end
 
       scenario "filling data and being redirect to quiz page" do
@@ -73,7 +73,7 @@ feature "Buying Gifts", %q{
     scenario "should start the gift creation" do
       visit gift_root_path
       click_link "new_occasion_link"
-      page.should have_content("Você está criando um presente")
+      page.should have_content("Estou escolhendo um presente")
     end
     
     scenario "should see the suggestions for a gift recipient" do
