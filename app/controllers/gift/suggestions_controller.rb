29960cd1210bfadac10c5f04e0fe64e861144b9b
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-25 10:55:16 -0300

    lets get the occasion from the association and not from the session

--- app/controllers/gift/suggestions_controller.rb
+++ app/controllers/gift/suggestions_controller.rb
@@ -3,7 +3,7 @@ class Gift::SuggestionsController < Gift::BaseController
   def index
     @gift_recipient = GiftRecipient.find(params[:recipient_id])
     @profile = @gift_recipient.profile
-    @occasion = GiftOccasion.find(session[:occasion_id])
+    @occasion = @gift_recipient.gift_occasions.last
     product_finder_service = ProductFinderService.new(@gift_recipient)
     @suggested_products = product_finder_service.suggested_products_for(@gift_recipient.profile, @gift_recipient.shoe_size)
     @products = product_finder_service.showroom_products(:description => @gift_recipient.shoe_size)
