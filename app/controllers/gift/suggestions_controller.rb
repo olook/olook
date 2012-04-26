e454449417519e0dedc2fc6c4e6e8199fff13d73
Rinaldi Fonseca <rinaldifonseca@gmail.com>
2012-04-26 19:25:04 -0300

    not sold out products

--- app/controllers/gift/suggestions_controller.rb
+++ app/controllers/gift/suggestions_controller.rb
@@ -7,7 +7,7 @@ class Gift::SuggestionsController < Gift::BaseController
     @occasion = @gift_recipient.gift_occasions.last
     product_finder_service = ProductFinderService.new(@gift_recipient)
     @suggested_products = product_finder_service.suggested_products_for(@gift_recipient.profile, @gift_recipient.shoe_size)
-    @products = product_finder_service.showroom_products(:description => @gift_recipient.shoe_size)
+    @products = product_finder_service.showroom_products(:description => @gift_recipient.shoe_size, :not_allow_sold_out_products => true)
   end
 
   def select_gift
