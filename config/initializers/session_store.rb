404c19e9202b2c0e8b4ea55e4bfc8e440bcfc0a3
eks <edu.fox@gmail.com>
2012-04-27 21:11:53 -0300

    removed memcached configurations from app files

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -6,5 +6,5 @@
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-require 'action_dispatch/middleware/session/dalli_store'
+#require 'action_dispatch/middleware/session/dalli_store'
 Olook::Application.config.session_store :active_record_store
