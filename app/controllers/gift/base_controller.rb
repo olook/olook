ebc7b06f51e2ba230271f75ac58b8bedce9deb44
Fabiano Beselga <fabianobeselga@gmail.com>
2012-04-28 17:40:38 -0300

    fix dirty session when accessing gift page

--- app/controllers/gift/base_controller.rb
+++ app/controllers/gift/base_controller.rb
@@ -1,22 +1,4 @@
 class Gift::BaseController < ApplicationController
   layout "gift"
-  before_filter :check_facebook_permissions, :load_facebook_adapter, :load_friends
-  
-  def current_month
-    Time.now.month
-  end
-  
-  def check_facebook_permissions
-    session[:facebook_scopes] = User::FACEBOOK_FRIENDS_BIRTHDAY
-  end  
 
-  def load_facebook_adapter
-    if current_user && current_user.has_facebook_friends_birthday?
-      @facebook_adapter = FacebookAdapter.new current_user.facebook_token 
-    end
-  end
-  
-  def load_friends 
-    @friends = @facebook_adapter.facebook_friends_with_birthday(current_month) if @facebook_adapter
-  end
 end
