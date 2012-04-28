90e6a321c66545eda943278040db10be996f92c1
Hugo Borges - agaelebe <agaelebe@gmail.com>
2012-04-28 17:26:52 -0300

    removing the default_scopes in favor of custom scopes (ordered_by_name). Don't worry, be happy.

--- app/models/gift_occasion_type.rb
+++ app/models/gift_occasion_type.rb
@@ -1,5 +1,5 @@
 class GiftOccasionType < ActiveRecord::Base
   has_many :gift_occasions
   validates :name, :presence => true, :length => {:minimum => 2}
-  default_scope :order => 'name ASC'
+  scope :ordered_by_name, :order => 'name ASC'
 end
