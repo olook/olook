789f30d13f146bf08b1dafbed9d457ec79578d62
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-04-30 14:29:20 -0300

    Fixed insert order issue for non-gift orders

--- app/models/abacos/pedido.rb
+++ app/models/abacos/pedido.rb
@@ -29,10 +29,6 @@ module Abacos
       @endereco         = parse_endereco(order.freight.address)
       @itens            = parse_itens(order.line_items)
       @pagamento        = parse_pagamento(order)
-
-      @nota_simbolica   = order.gift_wrap?
-      @valor_embalagem  = order.gift_wrap? ? YAML::load_file(Rails.root.to_s + '/config/gifts.yml')["values"][0] : false
-      @anotacao_pedido  = order.gift_wrap? ? order.gift_message : false
     end
 
     def parsed_data
@@ -46,18 +42,12 @@ module Abacos
             'DestNome'                 => @nome,
             'DestEmail'                => @email,
             'DestTelefone'             => @telefone,
-
             'ValorPedido'              => @valor_pedido,
             'ValorDesconto'            => @valor_desconto,
             'ValorFrete'               => @valor_frete,
             'Transportadora'           => @transportadora,
             'PrazoEntregaPosPagamento' => @tempo_entrega,
             'DataPrazoEntregaInicial'  => @data_entrega,
-
-            'EmitirNotaSimbolica'      => @nota_simbolica,
-            'ValorEmbalagemPresente'   => @valor_embalagem,
-            'Anotacao1'                => @anotacao_pedido,
-
             'Itens' => {
               'DadosPedidosItem'       => @itens.map {|item| item.parsed_data}
             },
