c4a4890cd45dd5010e3b41ddcebbf673ef40f1b4
Renato Elias <renato.elias@gmail.com>
2012-05-07 14:55:51 -0300

    Add Catalog Model

--- spec/models/moment_spec.rb
+++ spec/models/moment_spec.rb
@@ -2,15 +2,26 @@ require 'spec_helper'
 
 describe Moment do
   let(:moment) { Factory.create(:moment) }
+  let(:day_by_day) { Factory.build(:moment) }
   
   describe "validation" do
     it { should validate_presence_of(:name) }
     it { moment.should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:slug) }
     it { moment.should validate_uniqueness_of(:slug) }
+    it { should have_one(:catalog) }
   end
   
   describe "default" do
     it { Moment.new.active.should be_false }
+  end
+  
+  describe "after create" do
+    it "generate catalog" do
+      day_by_day.catalog.should be_blank
+      day_by_day.save!
+      day_by_day.catalog.should_not be_nil
+      day_by_day.catalog.should == Catalog::Moment.last
+    end
   end
 end
