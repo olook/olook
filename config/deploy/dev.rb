2cf2e0a883f67073c187ac6456b3ad88d5bc0662
eks <edu.fox@gmail.com>
2012-05-07 23:25:24 -0300

    modfications in capistrano's recipes

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -63,5 +63,20 @@ namespace :deploy do
     run "if [ -f /var/run/olook-unicorn.pid ]; then pid=`cat /var/run/olook-unicorn.pid` && kill -USR2 $pid; else cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.conf.rb -E #{env} -D; fi"
   end
 
-  after "deploy", "deploy:cleanup" # keep only the last 5 releases
+  # configuration = Capistrano::Configuration.respond_to?(:instance) ?
+  #   Capistrano::Configuration.instance(:must_exist) :
+  #   Capistrano.configuration(:must_exist)
+
+  # configuration.load do
+  #   namespace :deploy do    
+  #     namespace :rollback do
+  #       desc <<-DESC
+  #                         Rolls back the migration to the version found in schema.rb file of the previous release path.\\
+  #                               Uses sed command to read the version from schema.rb file.
+  #       DESC
+  #       task :migrations do
+  #         run "cd #{current_release};  rake db:migrate RAILS_ENV=#{rails_env} VERSION=`grep \\":version =>\\" #{previous_release}/db/schema.rb | sed -e 's/[a-z A-Z = \\> \\: \\. \\( \\)]//g'`"
+  #       end
+  #       after "deploy:rollback","deploy:rollback:migrations"
+        after "deploy", "deploy:cleanup" # keep only the last 5 releases
 end
