e76f055685face2190f02ce6c0b8df0826df682a
Fabiano Beselga <fabianobeselga@gmail.com>
2012-05-10 11:23:10 -0300

    facebook permission integration test

---
+++ spec/integration/facebook_permissions_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'integration/helpers'
+
+feature "Facebook Permissions", %q{
+  In order to be connected to my friends
+  As a user
+  I want to be able to connect to facebook
+  } do
+
+  describe "new user" do
+  end
+
+  describe "already user" do
+    scenario "friends" do
+    end
+
+    scenario "gift" do
+    end
+  end
+
+end
