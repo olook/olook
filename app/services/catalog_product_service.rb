ca08f5ff95ca57411603afa1f3b2b767d8d57a9e
Fabiano Beselga <fabianobeselga@gmail.com>
2012-05-11 18:15:48 -0300

    catalog moment strategy implemented

--- app/services/catalog_product_service.rb
+++ app/services/catalog_product_service.rb
@@ -1,5 +1,5 @@
 class CatalogProductService
-  
+
   def initialize catalog, product, options = {}
     @catalog = catalog
     @product = product
@@ -15,15 +15,19 @@ class CatalogProductService
       create_or_update
     end
   end
-  
+
   def retail_price
     (@product.price * (100 - @options[:discount_percentage].to_f)) / 100
+  end
+
+  def destroy
+    @catalog.products.where(:product_id => @product.id).delete_all
   end
 
   private
   def default_params
     last_variant = @product.variants.last
-    
+
     {
       :catalog_id => @catalog.id,
       :product_id => @product.id,
@@ -37,10 +41,10 @@ class CatalogProductService
       :inventory => (last_variant.inventory if last_variant)
     }
   end
-  
+
   def existing_product? options
     @catalog.products.where(
-      :product_id => @product.id, 
+      :product_id => @product.id,
       :variant_id => options[:variant_id]
     ).first
   end
@@ -54,18 +58,18 @@ class CatalogProductService
         params.delete(:retail_price)
         params.delete(:discount_percent)
       end
-      
+
       product.update_attributes!(params)
       product.reload
     else
       @catalog.products.create!(params)
     end
   end
-  
+
   def shoe_sizes
     @product.variants.map{|v| {:shoe_size => v.description.to_i, :inventory => v.inventory, :id => v.id } }
   end
-  
+
   def create_or_update_shoes
     shoe_sizes.inject([]) do |products, variant|
       products << create_or_update(
