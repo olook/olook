3f381c7333a9125878253f295deefaf15ca4acca
Raul Souza Lima <raulsouzalima@gmail.com>
2012-05-11 19:23:52 -0300

    Removed useless code

--- app/assets/javascripts/moments.js
+++ app/assets/javascripts/moments.js
@@ -5,24 +5,6 @@ jQuery(function() {
     }, 4000);
   }
 
-  $("div#carousel_container ul").carouFredSel({
-    width: 480,
-    items: 1,
-    auto : {
-      pauseDuration : 15000
-    },
-    prev : {
-      button : ".prev",
-      key : "left",
-      items : 1
-    },
-    next : {
-      button : ".next",
-      key : "right",
-      items : 1
-    }
-  });
-
   $('#moment_order_filter').change(function() {
     $("#moment_filter").submit();
   });
