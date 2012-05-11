4507800b0c65b6b8661a7666443f281ffb733bed
Fabiano Beselga <fabianobeselga@gmail.com>
2012-05-11 18:15:48 -0300

    catalog moment strategy implemented

--- app/services/catalog_service.rb
+++ app/services/catalog_service.rb
@@ -1,5 +1,5 @@
 class CatalogService
   def self.save_product product, options = {}
-    Catalog::Moment.save_product product, options
+    Catalogs::MomentStrategy.new(product, options).seek_and_destroy!
   end
 end
