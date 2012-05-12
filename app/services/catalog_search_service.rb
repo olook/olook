6e9bda5a158aa47d6f8836a1972dd53f256787dd
joao paulo <jpcamargo+github@gmail.com>
2012-05-11 21:30:53 -0300

    changed the query to exclude products that are in the liquidation products database from the results from the catalog products. added some tests to the catalog search service spec.

--- app/services/catalog_search_service.rb
+++ app/services/catalog_search_service.rb
@@ -4,8 +4,13 @@ class CatalogSearchService
 
   def initialize(params)
     @l_products = Catalog::Product.arel_table
-    @query_base = @l_products[:catalog_id].eq(params[:id]).and(@l_products[:inventory].gt(0)).and(Product.arel_table[:is_visible].eq(true))
-                  .and(LiquidationProduct.arel_table[:product_id].eq(l_products[:product_id]).not)
+    @query_base = @l_products[:catalog_id].eq(params[:id])
+                  .and(@l_products[:inventory].gt(0))
+                  .and(Product.arel_table[:is_visible].eq(true))
+                  .and(LiquidationProduct.arel_table[:product_id].eq(nil))
+                  # .and(LiquidationProduct.arel_table[:liquidation_id].eq(Liquidation.arel_table[:id]))
+                  # .and(Liquidation.arel_table[:active].eq(true))
+
     @params = params
   end
 
@@ -19,14 +24,14 @@ class CatalogSearchService
 
     query_bags_acessories = params[:bag_accessory_subcategories] ? l_products[:subcategory_name].in(params[:bag_accessory_subcategories]) : nil
 
-    @query_base = if query_bags_acessories
-      query_result ? @query_base.and(query_result.or(query_bags_acessories)) : @query_base.and(query_bags_acessories)
+    if query_bags_acessories
+      @query_base = query_result ? @query_base.and(query_result.or(query_bags_acessories)) : @query_base.and(query_bags_acessories)
     else
-      @query_base.and(query_result) if query_result
+      @query_base = @query_base.and(query_result) if query_result
     end
 
-    Catalog::Product.where(@query_base)
-                    .group("product_id")
+    Catalog::Product.joins('inner join products on products.id = catalog_products.product_id left outer join liquidation_products on liquidation_products.product_id = catalog_products.product_id').where(@query_base)
+                    .group("catalog_products.product_id")
                     .order("category_id asc, #{sort_filter}")
                     .paginate(:page => params[:page], :per_page => 12)
   end
