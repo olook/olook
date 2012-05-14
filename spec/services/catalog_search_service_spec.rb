dd3f1874658fefd8aa183b798250fdfddcb0a834
joao paulo <jpcamargo+github@gmail.com>
2012-05-14 11:53:46 -0300

    added more tests to the catalog search service spec

--- spec/services/catalog_search_service_spec.rb
+++ spec/services/catalog_search_service_spec.rb
@@ -29,36 +29,39 @@ describe CatalogSearchService do
 
   let(:basic_shoe_2) do
     product = (FactoryGirl.create :shoe_subcategory_name, description: "Melissa").product
+    FactoryGirl.create :shoe_heel, :product => product, :description => "Alto"
+    FactoryGirl.create :basic_shoe_size_40, :product => product, :inventory => 8
     product.master_variant.price = 100.00
     product.master_variant.save!
-    FactoryGirl.create :basic_shoe_size_40, :product => product, :inventory => 8
     product
   end
 
   let(:basic_shoe_3) do
     product = (FactoryGirl.create :shoe_subcategory_name, description: "Bota").product
+    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 5
     product.master_variant.price = 100.00
     product.master_variant.save!
-    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 5
     product
   end
   
-  let(:basic_sold_out_shoe) do
+  let(:sold_out_shoe) do
     product = (FactoryGirl.create :shoe_subcategory_name, description: "Galocha").product
+    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 0
     product.master_variant.price = 50.00
     product.master_variant.save!
-    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 0
     product
   end
 
-  let(:basic_hidden_shoe) do
+  let(:hidden_shoe) do
     product = (FactoryGirl.create :shoe_subcategory_name, description: "Invisivel").product
+    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 1
     product.master_variant.price = 50.00
     product.master_variant.save!
     product.update_attributes! is_visible: false
-    FactoryGirl.create :basic_shoe_size_37, :product => product, :inventory => 1
     product
   end
+
+  let(:liquidation) { FactoryGirl.create(:liquidation) }
 
   before :each do
     # LiquidationProduct.delete_all
@@ -106,15 +109,22 @@ describe CatalogSearchService do
       end
 
        it "returns 0 products if dont have inventory" do
-         cp1 = CatalogProductService.new(catalog, basic_sold_out_shoe).save!.first
-         params = {:id => catalog.id}
-         CatalogSearchService.new(params).search_products.should == []
+        cp1 = CatalogProductService.new(catalog, sold_out_shoe).save!.first
+        params = {:id => catalog.id}
+        CatalogSearchService.new(params).search_products.should == []
        end
 
        it "returns 0 products if the product is not visible" do
-        cp1 = CatalogProductService.new(catalog, basic_hidden_shoe).save!.first
-         params = {:id => catalog.id}
-         CatalogSearchService.new(params).search_products.should == []
+        cp1 = CatalogProductService.new(catalog, hidden_shoe).save!.first
+        params = {:id => catalog.id}
+        CatalogSearchService.new(params).search_products.should == []
+       end
+
+       it "returns 0 products if the product is in the liquidation" do
+        cp1 = CatalogProductService.new(catalog, basic_shoe).save!.first
+        lp1 = LiquidationProduct.create(:liquidation => liquidation, :product_id => basic_shoe_2.id, :subcategory_name => "rasteirinha", :inventory => 1)
+        params = {:id => catalog.id}
+        CatalogSearchService.new(params).search_products.should == []
        end
      end
 
@@ -133,7 +143,7 @@ describe CatalogSearchService do
         cp1 = CatalogProductService.new(catalog, basic_shoe).save!.first
         cp2 = CatalogProductService.new(catalog, basic_shoe_2).save!.first
         cp3 = CatalogProductService.new(catalog, basic_shoe_3).save!.first
-        params = {:id => catalog.id, :shoe_subcategories => ["Sandalia","Melissa"], :shoe_sizes => ["35","40"], :heels => ["0-5-cm"]}
+        params = {:id => catalog.id, :shoe_subcategories => ["Sandalia","Melissa"], :shoe_sizes => ["35","40"], :heels => ["0-5-cm","alto"]}
         CatalogSearchService.new(params).search_products.should == [cp1, cp2]
       end
 
