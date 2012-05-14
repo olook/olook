f95c90a6ca954d5f7599f6896df98879d451e114
joao paulo <jpcamargo+github@gmail.com>
2012-05-14 18:27:36 -0300

    fixed the initial pagination that called for a view that did not include a js format

--- app/models/catalog/product.rb
+++ app/models/catalog/product.rb
@@ -2,9 +2,8 @@ class Catalog::Product < ActiveRecord::Base
   self.table_name ='catalog_products'
   
   belongs_to :catalog
-  belongs_to :product
+  belongs_to :product, :class_name => "::Product"
   belongs_to :variant
   
   validates :product_id, :uniqueness => {:scope => [:catalog_id, :variant_id]}
-  
 end
