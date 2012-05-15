2ba7164beb53b2a86804847b185cc7cc54bd2abf
joao paulo <jpcamargo+github@gmail.com>
2012-05-15 18:14:26 -0300

    fixed moment model and model spec

--- spec/models/moment_spec.rb
+++ spec/models/moment_spec.rb
@@ -9,8 +9,9 @@ describe Moment do
     it { moment.should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:slug) }
     it { moment.should validate_uniqueness_of(:slug) }
+
     it { should validate_presence_of(:article) }
-    it { should validate_presence_of(:position) }
+    it { should validate_numericality_of(:position) }
     it { should have_one(:catalog) }
   end
   
