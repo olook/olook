8f37210fcd3693f2eb2929784db25ea2b29ec4a7
joao paulo <jpcamargo+github@gmail.com>
2012-05-15 17:52:50 -0300

    added validations to the moments model spec

--- spec/models/moment_spec.rb
+++ spec/models/moment_spec.rb
@@ -9,6 +9,8 @@ describe Moment do
     it { moment.should validate_uniqueness_of(:name) }
     it { should validate_presence_of(:slug) }
     it { moment.should validate_uniqueness_of(:slug) }
+    it { should validate_presence_of(:article) }
+    it { should validate_presence_of(:position) }
     it { should have_one(:catalog) }
   end
   
