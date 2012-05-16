9b4979f115a20635e8754c17d181d98bd9699945
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-05-16 10:36:55 -0300

    Added credit service and admin_credit_service specs

---
+++ spec/services/admin_credit_service_spec.rb
@@ -0,0 +1,33 @@
+# -*- encoding : utf-8 -*-
+describe AdminCreditService do
+
+  it "should have a defined transcation limit from credit.yml config" do
+    pending
+  end
+
+  it "should check if has exceeded transaction limit" do
+    pending
+  end
+
+
+  context "when a credit operation" do
+    it "should add credit to user account" do
+      pending
+    end
+
+    it "should raise an exception if transaction limit has exceeded" do
+      pending
+    end
+  end
+
+  context "when a debit operation" do
+    it "should remove credit from user account" do
+      pending
+    end
+  end
+
+  end
+
+
+
+end
---
+++ spec/services/credit_service_spec.rb
@@ -0,0 +1,16 @@
+# -*- encoding : utf-8 -*-
+describe CreditService do
+  
+  it "should load a credit config file" do
+    pending
+  end
+
+  it "should be initialized with a service specialization" do
+    pending
+  end
+
+  it "should create a credit transaction given operation, value, reason and user" do
+    pending
+  end
+
+end
