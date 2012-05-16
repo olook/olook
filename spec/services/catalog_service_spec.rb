dca78903dad1e9ef84ec23387c967330039f89ee
Renato Elias <renato.elias@gmail.com>
2012-05-16 15:02:00 -0300

    Fix for call catalog moment strategy only when has moments in options

--- spec/services/catalog_service_spec.rb
+++ spec/services/catalog_service_spec.rb
@@ -11,13 +11,31 @@ describe CatalogService do
     product
   end
 
+  let(:moment1) { FactoryGirl.create :moments }
+
   describe "save product" do
-    it "should execute moment catalogs insert strategy" do
+    it "should update product in catalogs" do
+      CatalogProductService.new(moment1.catalog, basic_bag).save!
+      
+      ct_product_service = double(CatalogProductService)
+      ct_product_service.should_receive(:save!)
+      CatalogProductService.should_receive(:new)
+                     .with(moment1.catalog, basic_bag, {:test_options => true}).and_return(ct_product_service)
+      
+      CatalogService.save_product basic_bag, :test_options => true
+    end
+
+    it "should  not execute moment catalogs without moments" do
+      Catalogs::MomentStrategy.should_not_receive(:new)
+      CatalogService.save_product basic_bag, :test_options => true
+    end
+        
+    it "should execute moment catalogs when has moments" do
       ct_moment_strategy = double(Catalogs::MomentStrategy)
       ct_moment_strategy.should_receive(:seek_and_destroy!)
       Catalogs::MomentStrategy.should_receive(:new)
-                     .with(basic_bag, {:test_options => true}).and_return(ct_moment_strategy)
-      CatalogService.save_product basic_bag, :test_options => true
+                     .with(basic_bag, {:test_options => true, :moments => []}).and_return(ct_moment_strategy)
+      CatalogService.save_product basic_bag, :test_options => true, :moments => []
     end
   end
 end
