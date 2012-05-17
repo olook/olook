1a2ba8dc1925fcfe57d7d6c0c535d67cff9758ef
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-05-17 01:03:14 -0300

    Refactored admin_credit_service and credit_service specs

--- spec/services/admin_credit_service_spec.rb
+++ spec/services/admin_credit_service_spec.rb
@@ -1,33 +1,54 @@
 # -*- encoding : utf-8 -*-
 describe AdminCreditService do
 
-  it "should have a defined transcation limit from credit.yml config" do
-    pending
+  let(:admin) {Factory :admin_superadministrator}
+  subject {described_class.new(admin)}
+
+    before(:each) do
+      @user = Factory :user
+    end
+
+
+  it "should have a defined transaction limit defined on credit.yml config" do
+    described_class::TRANSACTION_LIMIT.should be_an_instance_of(Float)
   end
 
-  it "should check if has exceeded transaction limit" do
-    pending
+
+  it "should allow operation if transaction limit has not exceeded" do
+    subject.has_not_exceeded_transaction_limit?(described_class::TRANSACTION_LIMIT + 1).should be_false
   end
 
-
-  context "when a credit operation" do
+  context "when performing a credit operation" do
+    
     it "should add credit to user account" do
-      pending
+      subject.add_credit(10, "Presente", @user)
+      @user.credits.last.total.should == 10
     end
 
-    it "should raise an exception if transaction limit has exceeded" do
-      pending
+    it "should not add credit to user account if credit limit has been reached" do
+      subject.add_credit(Credit::LIMIT_FOR_EACH_USER+1, "Presente", @user)
+      @user.credits.should be_empty
     end
+
   end
 
-  context "when a debit operation" do
+  context "when performing a debit operation" do
+    before(:each) do
+      credit = FactoryGirl.create(:credit)
+      @user.credits << credit
+    end
+
     it "should remove credit from user account" do
-      pending
+      subject.remove_credit(@user.current_credit, "Erro do Operador", @user)
+      @user.current_credit.should  == 0 
     end
-  end
+
+    it "should not remove credit if value is higher than the user current credit" do
+      subject.remove_credit(@user.current_credit+1, "Erro do Operador", @user)
+      @user.current_credit.should == @user.current_credit
+    end
 
   end
-
 
 
 end
--- spec/services/credit_service_spec.rb
+++ spec/services/credit_service_spec.rb
@@ -1,16 +1,40 @@
 # -*- encoding : utf-8 -*-
 describe CreditService do
+
+  let(:admin) {Factory :admin_superadministrator}
+  let(:user)  {Factory :user}
+  let(:service) {mock 'service'}
   
-  it "should load a credit config file" do
-    pending
+  subject{described_class.new(service)}
+
+
+  it "should load credit.yml file" do
+    File.exists?("#{Rails.root.to_s}/config/credit.yml").should be_true
   end
 
   it "should be initialized with a service specialization" do
-    pending
+    subject.service.should_not be_nil
   end
 
-  it "should create a credit transaction given operation, value, reason and user" do
-    pending
+  context "when creating a transaction" do
+      before(:each) do
+        subject.service = AdminCreditService.new(admin)
+      end
+    context "if service responds to an operation" do
+      it "should return true if transaction completes" do
+        subject.create_transaction("10", :add_credit, "Presente", user).should be_true
+      end
+
+      it "should return false if transaction aborts" do
+        subject.create_transaction("1000", :remove_credit, "Presente", user).should be_false
+      end
+
+    end
+    context "if service does not respond to an operation" do
+      it "should return false" do
+        subject.create_transaction("1000", :foo, "Presente", user).should be_false
+      end
+    end
   end
 
 end
