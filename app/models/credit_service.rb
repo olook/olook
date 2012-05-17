46763d7a7efe8783a1aad0e610c757d83647641c
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-05-17 01:01:29 -0300

    Refactored credit service classes

--- app/models/credit_service.rb
+++ app/models/credit_service.rb
@@ -2,14 +2,15 @@
 class CreditService
   
   CREDIT_CONFIG = YAML.load_file("#{Rails.root.to_s}/config/credit.yml")
-  OPERATION = [:add_credit, :remove_credit]
   
+  attr_accessor :service
+
   def initialize(service)
     @service = service
   end
 
   def create_transaction(value, operation, reason, user)
-    @service.send(operation, BigDecimal.new(value).abs, reason, user)
+    @service.respond_to?(operation) ? @service.send(operation, BigDecimal.new(value).abs, reason, user) : false
   end
 
 end
