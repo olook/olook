4a4a74167313d9a296148db6ff8c277bf1dd6d2e
Raul Souza Lima <raulsouzalima@gmail.com>
2012-05-18 12:55:18 -0300

    Reorder half user menu

--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -14,7 +14,7 @@ class MenuPresenter < BasePresenter
   end
 
   def render_half_user_menu
-    [lookbooks, moments, stylist, my_friends, invite, gift, liquidation, blog, cart].join.html_safe
+    [lookbooks, moments, gift, my_friends, stylist, blog, invite, liquidation, cart].join.html_safe
   end
 
   private
