0d796867981b61fec19eb2a10e70c7f7ceb7e351
Raul Souza Lima <raulsouzalima@gmail.com>
2012-05-18 16:55:24 -0300

    Updated valentine link

--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -1,44 +1,44 @@
 # -*- encoding : utf-8 -*
 class MenuPresenter < BasePresenter
-  
+
   def render_item label, path, css_klass, hightlight_when
     h.content_tag(:li, h.link_to(label, path, :class => h.selected_if_current(hightlight_when)), :class => css_klass)
   end
-  
+
   def render_menu
     user.half_user ? render_half_user_menu : render_default_menu
   end
-  
+
   def render_default_menu
     [showroom, lookbooks, stylist, my_friends, invite, gift, liquidation, valentine_invite, blog, cart].join.html_safe
   end
-  
+
   def render_half_user_menu
     [lookbooks, stylist, my_friends, invite, gift, liquidation, valentine_invite, blog, cart].join.html_safe
   end
-  
+
   private
-  
+
   def showroom
     render_item("Minha Vitrine", h.member_showroom_path, "showroom", ["members#showroom"]) if user.has_early_access?
   end
-  
+
   def lookbooks
     render_item("Tendências", h.lookbooks_path, "lookbooks", ["lookbooks#show"])
   end
-  
+
   def stylist
     render_item("Stylist", h.helena_linhares_path, "stylist", ['stylists#helena_linhares'])
   end
-  
+
   def my_friends
     render_item("Minhas amigas", h.facebook_connect_path, "my_friends", ['friends#facebook_connect','friends#home','friends#showroom'])
   end
-  
+
   def invite
     render_item("Convidar amigas", h.member_invite_path, "invite", ["members#invite"])
   end
-  
+
   def gift
     render_item("Presentes", h.gift_root_path, "gift",
      [
@@ -51,20 +51,20 @@ class MenuPresenter < BasePresenter
      ]
     )
   end
-  
+
   def liquidation
     render_item(h.current_liquidation.name, h.liquidations_path(h.current_liquidation.id), "liquidation", ["liquidations#show"]) if h.current_liquidation
   end
-  
+
   def cart
-    h.content_tag :li, (h.render 'shared/cart', :order => @order), :id => "cart", :class => "cart" if user.has_early_access?   
+    h.content_tag :li, (h.render 'shared/cart', :order => @order), :id => "cart", :class => "cart" if user.has_early_access?
   end
-  
+
   def blog
-    h.content_tag :li, h.link_to("Blog", "http://blog.olook.com.br", :target => "_blank"), :class => "blog" 
+    h.content_tag :li, h.link_to("Blog", "http://blog.olook.com.br", :target => "_blank"), :class => "blog"
   end
 
   def valentine_invite
-    render_item("Convidar namorado", h.member_valentine_invite_path, "valentine_invite", ["members#valentine_invite"])
+    render_item("Dia dos namorados", h.member_valentine_invite_path, "valentine_invite", ["members#valentine_invite"])
   end
 end
