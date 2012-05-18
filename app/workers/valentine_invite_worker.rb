33968d5cf37aa1e45c01a67900ff67573d9b420c
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-05-18 17:39:34 -0300

    Fixed reference to InviteMailer on ValentineInviteWorker

--- app/workers/valentine_invite_worker.rb
+++ app/workers/valentine_invite_worker.rb
@@ -3,8 +3,8 @@ class ValentineInviteWorker
   @queue = :mail_invite
 	
   def self.perform(user_name, to)
-	 mail = ValentineInviteMailer.invite_email(user_name, to)
+	 mail = InviteMailer.valentine_invite_email(user_name, to)
 	 mail.deliver
   end
-  
+
 end
