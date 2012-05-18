e00a9f7d7bd237a5f045f29646c758439aa21bc0
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-05-18 17:28:55 -0300

    Added valentine_invite_worker

---
+++ app/workers/valentine_invite_worker.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+class ValentineInviteWorker
+  @queue = :mail_invite
+	
+  def self.perform(user_name, to)
+	 mail = ValentineInviteMailer.invite_email(user_name, to)
+	 mail.deliver
+  end
+  
+end
