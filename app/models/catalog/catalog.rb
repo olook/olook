86639618b880c71cc8b43777154d5575c699abe6
joao paulo <jpcamargo+github@gmail.com>
2012-05-21 13:20:22 -0300

    added is visible to the filter query for moments

--- app/models/catalog/catalog.rb
+++ app/models/catalog/catalog.rb
@@ -2,27 +2,27 @@ class Catalog::Catalog < ActiveRecord::Base
   has_many :products, :class_name => "Catalog::Product", :foreign_key => "catalog_id"
   
   validates :type, :presence => true, :exclusion => ["Catalog::Catalog"]
-  
+
   def in_category(category_id)
-    products.where(category_id: category_id).where("inventory > 0")
+    products.joins(:product).where(category_id: category_id).where("inventory > 0").where('products.is_visible = 1')
   end
-  
+
   def subcategories(category_id)
     in_category(category_id).group(:subcategory_name).order("subcategory_name asc").map { |p| p.subcategory_name }.compact
   end
-  
+
   def shoes
     subcategories(Category::SHOE)
   end
-  
+
   def bags
     subcategories(Category::BAG)
   end
-  
+
   def accessories
     subcategories(Category::ACCESSORY)
   end
-  
+
   def shoe_sizes
     in_category(Category::SHOE).group(:shoe_size).order("shoe_size asc").map { |p| p.shoe_size }.compact
   end
