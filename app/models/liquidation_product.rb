7d3b6228fafe1aadbb001a424c0d4b815c121af0
joao paulo <jpcamargo+github@gmail.com>
2012-05-28 18:36:48 -0300

    changed showroom picture display size to 100x100

--- app/models/liquidation_product.rb
+++ app/models/liquidation_product.rb
@@ -6,6 +6,7 @@ class LiquidationProduct < ActiveRecord::Base
   delegate :name, :to => :product
   delegate :color_name, :to => :product
   delegate :thumb_picture, :to => :product
+  delegate :bag_picture, :to => :product
   delegate :showroom_picture, :to => :product
   delegate :color_sample, :to => :product
   
