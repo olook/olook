9799a058a1778f8b3cc44431081e1c5473ae5d94
Fabiano Beselga <fabianobeselga@gmail.com>
2012-05-30 12:17:14 -0300

    promotion being showed on the product while the user is not logged

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -6,15 +6,22 @@ module PromotionsHelper
       {:controller => "friends", :actions => ["showroom"]}
   ]
 
+  PROMOTION_BANNER_GUEST_WHITELIST = [
+    {:controller => "product", :actions => ["show"]}
+  ]
+
   def render_promotion_banner
-    if @promotion and current_user and page_included_in_whitelist?
+    if @promotion and current_user and page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)
+      render(:partial => "promotions/banners/#{@promotion.strategy}")
+    elsif !current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST)
+      @promotion = Promotion.find_by_strategy("purchases_amount_strategy")
       render(:partial => "promotions/banners/#{@promotion.strategy}")
     end
   end
 
-  def page_included_in_whitelist?
+  def page_included_in_whitelist? list
     result = false
-    PROMOTION_BANNER_WHITELIST.each do|whitelist|
+    list.each do|whitelist|
       if whitelist[:controller] == controller_name
         if whitelist[:actions]
           result = whitelist[:actions].include? action_name
