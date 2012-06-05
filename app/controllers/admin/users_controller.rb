32b22aee6e606f327310ddf31c3b03d944550190
Joao Moura <joaomdmoura@gmail.com>
2012-06-05 20:34:28 -0300

    removing survey info from user in admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,8 +12,8 @@ class Admin::UsersController < Admin::BaseController
 
   def show
     @user = User.find(params[:id])
-    survey_answers_parser = SurveyAnswerParser.new(@user.survey_answers)
-    @survey_answers = survey_answers_parser.build_survey_answers
+    # survey_answers_parser = SurveyAnswerParser.new(@user.survey_answers)
+    # @survey_answers = survey_answers_parser.build_survey_answers
     respond_with :admin, @user
   end
 
