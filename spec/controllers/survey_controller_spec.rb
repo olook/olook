0a51c5ff1fc66eb8fe55c0ba9d8f59ad1b34debd
Renato Elias <renato.elias@gmail.com>
2012-06-20 17:37:36 -0300

    Refactor Survey

--- spec/controllers/survey_controller_spec.rb
+++ spec/controllers/survey_controller_spec.rb
@@ -5,6 +5,9 @@ describe SurveyController do
 
   include Devise::TestHelpers
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:params) { {:questions => {:foo => :bar}, :year => '2012', :month => '12', :day => '12'} }
+
   describe "GET 'new'" do
     it "should be successful" do
       get 'new'
@@ -37,35 +40,114 @@ describe SurveyController do
     end
   end
 
-  describe "POST 'create'" do
-    context "answering quiz" do
-      it "should assign profile_points in the session" do
-        ProfileBuilder.stub(:profiles_given_questions).and_return("foo")
-        profile_points = ProfileBuilder.stub(:build_profiles_points).and_return("bar").call
-        post 'create', :questions => {:foo => :bar}, :year => '2012', :month => '12', :day => '12'
-        session[:profile_points].should == profile_points
+  describe "answering quiz" do
+    it "should redirect to survey_path when no has questions" do
+      post :create
+      response.should redirect_to new_survey_path
+    end
+    
+    context "without user" do
+      it "should save questions to session" do
+        post :create, params
+        session[:profile_questions].should == {"foo" => "bar"}
+        session[:profile_birthday].should == {:day => '12', :month => '12', :year => '2012'}
       end
 
-      it "should redirect to survey_path if params[:questions] is nil" do
-        post 'create'
-        response.should redirect_to new_survey_path
+      it "should redirect to registration" do
+        post :create, params
+        response.should redirect_to new_user_registration_path
       end
     end
-    context "retaking quiz" do
-      
-      it "should assign profile_points in the session" do
-        user = FactoryGirl.create(:user)
+    
+    context "with user" do
+    
+      before :each do
         sign_in user
-        ProfileBuilder.stub(:profiles_given_questions).and_return("foo")
-        ProfileBuilder.any_instance.stub(:create_user_points).and_return(true)
-        profile_points = ProfileBuilder.stub(:build_profiles_points).and_return("bar").call
-        post 'create', :questions => {:foo => :bar}, :year => '2012', :month => '12', :day => '12'
-        session[:profile_points].should == profile_points
+      end
+      
+      it "should not save questions to session" do
+        ProfileBuilder.stub(:factory)
+        post :create, params
+        session[:profile_questions].should be_nil
+        session[:profile_birthday].should be_nil
       end
 
-      it "should redirect to survey_path if params[:questions] is nil" do
-        post 'create'
-        response.should redirect_to new_survey_path
+      it "should upgrade to full user" do
+        user.should_receive(:upgrade_to_full_user!)
+        controller.stub(:current_user).and_return(user)
+        ProfileBuilder.stub(:factory)
+        post :create, params
+      end
+
+      it "should save questions to database" do
+        ProfileBuilder.should_receive(:factory).with(
+          {:day => '12', :month => '12', :year => '2012'},
+          {"foo" => 'bar'},
+          user
+        )
+        post :create, params
+      end
+
+      context "when has points in user" do
+        before :each do
+          ProfileBuilder.stub(:factory)
+          user.points.stub(:count).and_return(1)
+          controller.stub(:current_user).and_return(user)
+        end
+        
+        it "should delete questions of user" do
+          user.points.should_receive(:delete_all)
+          post :create, params
+        end
+
+        it "should set retake quiz in session to true" do
+          post :create, params
+          session[:profile_retake].should be_true
+        end
+
+        it "should redirect to showroom" do
+          post :create, params
+          response.should redirect_to member_showroom_path
+        end
+      end
+
+      context "when has answers in user" do
+        let(:answer) { SurveyAnswer.new }
+        
+        before :each do
+          ProfileBuilder.stub(:factory)
+          user.stub(:survey_answer).and_return(answer)
+          controller.stub(:current_user).and_return(user)
+        end
+        
+        it "should delete answers of user" do
+          answer.should_receive(:delete)
+          post :create, params
+        end
+        
+        it "should set retake quiz in session to true" do
+          post :create, params
+          session[:profile_retake].should be_true
+        end
+
+        it "should redirect to showroom" do
+          post :create, params
+          response.should redirect_to member_showroom_path
+        end
+      end
+      
+      context "when no has point in user" do
+        it "should set retake quiz in session to false" do
+          ProfileBuilder.stub(:factory)
+          post :create, params
+          session[:profile_retake].should be_false
+        end
+
+        it "should redirect to welcome" do
+          ProfileBuilder.stub(:factory)
+          post :create, params
+          response.should redirect_to member_welcome_path
+        end
       end
     end
   end
