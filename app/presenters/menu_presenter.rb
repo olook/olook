a6e6b3887ba754c3076175986b58dfc60092c0f2
regis <regis@regis.(none)>
2012-06-22 09:06:09 -0300

    Removing SPFW week button and adding OlookTV Link.

--- app/presenters/menu_presenter.rb
+++ app/presenters/menu_presenter.rb
@@ -17,11 +17,11 @@ class MenuPresenter < BasePresenter
   end
 
   def render_default_menu
-    [showroom, lookbooks, moments, gift, my_friends, stylist, liquidation, blog, spfw_blog, cart].join.html_safe
+    [showroom, lookbooks, moments, gift, my_friends, stylist, liquidation, blog, olook_tv, cart].join.html_safe
   end
 
   def render_half_user_menu
-    [lookbooks, moments, gift, my_friends, stylist, liquidation, blog, spfw_blog, cart].join.html_safe
+    [lookbooks, moments, gift, my_friends, stylist, liquidation, blog, olook_tv, cart].join.html_safe
   end
 
   private
@@ -71,8 +71,8 @@ class MenuPresenter < BasePresenter
     h.content_tag :li, (h.render 'shared/cart', :order => @order), :id => "cart", :class => "cart"
   end
 
-  def spfw_blog
-    render_item_with_label("Cobertura SPFW", "http://blog.olook.com.br/category/spfw-2/", "spfw", [])
+  def olook_tv
+    h.content_tag :li, h.link_to("olook TV", "http://blog.olook.com.br/category/spfw-2/", :target => "_blank"), :class => "olooktv"
   end
 
   def blog
