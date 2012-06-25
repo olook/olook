4b5abefde465914fb5340015db1e68267362a802
Renato Elias <renato.elias@gmail.com>
2012-06-25 15:58:00 -0300

    Registration Controller Tests

--- spec/controllers/users/registrations_controller_spec.rb
+++ spec/controllers/users/registrations_controller_spec.rb
@@ -5,10 +5,13 @@ describe Users::RegistrationsController do
 
   let(:user_attributes) { {"email" => "mail@mail.com", "password" => "123456", "password_confirmation" => "123456", "first_name" => "User Name", "last_name" => "Last Name" } }
   let(:user_attributes_invalid) { {"email" => "mail@mail.com", "password" => "12345", "password_confirmation" => "123456", "first_name" => "User Name", "last_name" => "Last Name" } }
+  let(:half_user_attributes) { user_attributes.merge({"half_user" => true , "gender" => "1"}) }
+  let(:woman_half_user_attributes) { user_attributes.merge({"half_user" => true , "gender" => "0"}) }
+  let(:half_user_attributes_invalid) { user_attributes_invalid.merge({"half_user" => true}) }
 
   let(:birthday) { {:day => "27", :month => "9", :year => "1987"} }
   let(:birthday_date) { Date.new(1987, 9, 27) }
-  let(:facebook_data) { {"extra" => {"raw_info" => "xyz"}, "credentials" => {"token" => "abc"}} }
+  let(:facebook_data) { {"extra" => {"raw_info" => {"first_name" => "XPTO"}}, "credentials" => {"token" => "abc"}} }
 
   render_views
 
@@ -25,6 +28,7 @@ describe Users::RegistrationsController do
   end
 
   after :each do
+    session[:gift_products] = nil
     session[:profile_questions] = nil
     session[:profile_birthday] = nil
     session[:tracking_params] = nil
@@ -34,46 +38,48 @@ describe Users::RegistrationsController do
 
   describe "registration form" do
     context "when is full user" do
-      it "should redirect if the user dont fill the Survey" do
+      it "should redirect if the user don't fill the Survey" do
         session[:profile_questions] = nil
         session[:profile_birthday] = :some_data
         get :new
         response.should redirect_to(new_survey_path)
       end
 
-      it "should redirect if the user dont fill the birthday" do
+      it "should redirect if the user don't fill the birthday" do
         session[:profile_questions] = :some_data
         session[:profile_birthday] = nil
         get :new
         response.should redirect_to(new_survey_path)
       end
 
-      xit "should render new when user fill the Survey" do
+      it "should render new when user fill the Survey" do
         session[:profile_questions] = :some_data
-        session[:profile_birthday] = :some_data
+        session[:profile_birthday] = birthday
         get :new
         response.should render_template ["layouts/site", "new"]
       end
 
-      xit "should not build the resource using session data" do
+      it "should not build the resource using session data" do
         session[:profile_questions] = :some_data
+        session[:profile_birthday] = birthday
         get :new
         controller.resource.email.should eq(nil)
       end
 
-      xit "should assigns @signup_with_facebook" do
+      it "should assigns @signup_with_facebook" do
         session[:profile_questions] = :some_data
+        session[:profile_birthday] = birthday
         session["devise.facebook_data"] = facebook_data
         get :new
         assigns(:signup_with_facebook).should eq(true)
       end
 
-      xit "should build the resource using session data" do
+      it "should build the resource using session data" do
         session[:profile_questions] = :some_data
-        data = {"email" => "mail@mail.com"}
-        controller.stub(:user_data_from_session).and_return(data)
+        session[:profile_birthday] = birthday
+        session["devise.facebook_data"] = facebook_data
         get :new
-        controller.resource.email.should eq("mail@mail.com")
+        controller.resource.first_name.should eq("XPTO")
       end
     end
     
@@ -82,38 +88,10 @@ describe Users::RegistrationsController do
         get :new_half
         response.should render_template ["layouts/site", "new_half"]
       end
-
-      xit "should not build the resource using session data" do
-        session[:profile_questions] = nil
-        get :new_half
-        controller.resource.email.should eq(nil)
-      end
-
-      xit "should assigns @signup_with_facebook" do
-        session[:profile_questions] = :some_data
-        session["devise.facebook_data"] = facebook_data
-        get :new_half
-        assigns(:signup_with_facebook).should eq(true)
-      end
-
-      xit "should build the resource using session data" do
-        session[:profile_questions] = :some_data
-        data = {"email" => "mail@mail.com"}
-        controller.stub(:user_data_from_session).and_return(data)
-        get :new_half
-        controller.resource.email.should eq("mail@mail.com")
-      end
     end
   end
   
   describe "sign up" do
-    it "should save tracking"
-    it "should clear tracking session"
-    it "should save invite"
-    it "should clear invite session"
-    it "should save facebook"
-    it "should clear facebook session"
-    
     context "when is full user" do
       it "should redirect if the user don't fill the Survey" do
         session[:profile_questions] = nil
@@ -185,46 +163,89 @@ describe Users::RegistrationsController do
         post :create, :user => user_attributes
         response.should redirect_to(member_welcome_path)
       end
+      
+      it "should set registered_via from quiz" do
+        session[:profile_questions] = :some_data
+        session[:profile_birthday] = birthday
+        ProfileBuilder.should_receive(:factory)
+        post :create, :user => user_attributes
+        controller.current_user.registered_via.should eq(User::RegisteredVia[:quiz])
+      end
     end
     
     context "when is half user" do
-      context "with gift product in session" do
-        xit "should save user" do
-          expect {
-            post :create_half, :user => user_attributes
-          }.to change(User, :count).by(1)
-        end
-        
-        it "should response error when invalid attributes" do
-          expect {
-            post :create_half, :user => user_attributes_invalid
-            response.should render_template ["layouts/site", "new_half"]
-          }.to_not change(User, :count)
-        end
-        
-        it "should invoke CartBuilder to add Products"
-        it "should redirect to cart page"
+      it "should save user" do
+        expect {
+          post :create_half, :user => half_user_attributes
+        }.to change(User, :count).by(1)
       end
 
-      context "with has offline product in session" do
-        xit "should save user" do
-          expect {
-            post :create_half, :user => user_attributes
-          }.to change(User, :count).by(1)
-        end
-        
-        it "should response error when invalid attributes" do
-          expect {
-            post :create_half, :user => user_attributes_invalid
-            response.should render_template ["layouts/site", "new_half"]
-          }.to_not change(User, :count)
-        end
-        
-        it "should invoke CartBuilder to add Products"
-        it "should redirect to cart page"
+      it "should response error when invalid attributes" do
+        expect {
+          post :create_half, :user => half_user_attributes_invalid
+          response.should render_template ["layouts/site", "half_user"]
+        }.to_not change(User, :count)
       end
-      it "should redirect to gift page when no has offline/gift products"
+
+      it "should redirect to gift page when session is empty" do
+        post :create_half, :user => half_user_attributes
+        response.should redirect_to(gift_root_path)
+      end
+
+      context "with gift product in session" do
+        before  :each do
+          session[:gift_products] = mock
+        end
+        
+        it "should set registered_via from gift" do
+          CartBuilder.stub(:gift).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+          controller.current_user.registered_via.should eq(User::RegisteredVia[:gift])
+        end
+        
+        it "should invoke CartBuilder to add Products" do
+          CartBuilder.should_receive(:gift).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+        end
+
+        it "should redirect to cart page" do
+          CartBuilder.stub(:gift).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+          response.should redirect_to(cart_path)
+        end
+      end
+
+      context "with offline product in session" do
+        before  :each do
+          session[:offline_variant] = mock
+        end
+        
+        it "should set registered_via from thin" do
+          CartBuilder.stub(:offline).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+          controller.current_user.registered_via.should eq(User::RegisteredVia[:thin])
+        end
+        
+        it "should invoke CartBuilder to add Products" do
+          CartBuilder.should_receive(:offline).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+        end
+
+        it "should redirect to cart page" do
+          CartBuilder.stub(:offline).and_return(cart_path)
+          post :create_half, :user => half_user_attributes
+          response.should redirect_to(cart_path)
+        end
+      end
     end
+    
+    it "should save tracking"
+    it "should clear tracking session"
+    it "should save invite"
+    it "should clear invite session"
+    it "should save facebook"
+    it "should clear facebook session"
+    
   end
 
   describe "PUT update" do
