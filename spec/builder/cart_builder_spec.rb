aa77a64476a6b839b4d11e4e9f33a09e3dfe913f
Renato Elias <renato.elias@gmail.com>
2012-06-26 16:51:14 -0300

    Add Offline test

--- spec/builder/cart_builder_spec.rb
+++ spec/builder/cart_builder_spec.rb
@@ -2,16 +2,37 @@
 require 'spec_helper'
 
 describe CartBuilder do
+  let(:variant) { FactoryGirl.create(:basic_shoe_size_35) }
 
   xit "should build gift session"
 
-  xit "should build offline session" do
-    session[:order] = order.id
-    session[:offline_variant] = { "id" => variant.id }
-    session[:offline_first_access] = true
-    get :showroom
-    order.line_items.count.should  be_eql(1)
-    session[:offline_first_access].should be_nil
-    session[:offline_variant].should be_nil
+  context "build offline session" do
+    it "should return cart_path" do
+      controller = double(Users::RegistrationsController)
+      path = mock
+      controller.stub(:session).and_return({})
+      controller.stub(:cart_path).and_return(path)
+      CartBuilder.offline(controller).should be(path)
+    end
+    
+    it "should add variant to order" do
+      controller = double(Users::RegistrationsController)
+
+      hash_session = {:offline_variant => { "id" => variant.id } }
+      
+      controller.stub(:session).and_return(hash_session)
+      controller.stub(:cart_path).and_return(nil)
+      
+      order = double(Order)
+      order.should_receive(:add_variant).with(variant)
+      
+      controller.stub(:current_order).and_return(order)
+      
+      expect {
+        CartBuilder.offline(controller)
+      }.to_not raise_error
+      
+      hash_session[:offline_variant].should be_nil
+    end
   end
 end
