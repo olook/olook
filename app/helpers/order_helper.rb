5acda87d412eb8e8fda32fc62ef60cf86036111c
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-06-26 17:15:10 -0300

    Fixed case wrong syntax

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -2,9 +2,9 @@
 module OrderHelper
   def link_to_tracking_code(order)
   	shipping_service = order.freight.shipping_service.name case
-    when "TEX":
+    when "TEX"
       "http://tracking.totalexpress.com.br/poupup_track.php?reid=1537&pedido=#{order.number}&nfiscal=#{order.invoice_number}"
-    when "PAC":
+    when "PAC"
       "http://websro.correios.com.br/sro_bin/txect01$.QueryList?P_LINGUA=001&P_TIPO=001&P_COD_UNI=#{order.tracking_code}"
     end
   end
