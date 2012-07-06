e2fdab4e39241be05bf6b661519425b889e9f212
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-06 11:23:50 -0300

    Added sac notification module

---
+++ app/models/sac/alert.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+module SAC
+  class Alert
+
+    attr_accessor :time, :header, :order_id, :message
+
+    def initialize(header, order_id=nil, message)
+      @header, @order_id, @message = set_header(header), order_id, message
+    end
+
+    
+    private
+
+    def set_header(header)
+      header = header + ". Recebido as " + set_time_format
+    end
+
+    def set_time_format
+      @time = Time.now.strftime("%d, %B %Y as %H:%M:%S")
+    end
+
+
+  end
+end
