86874ed38c7d65fd68ed4b70b077176830e07236
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-12 19:11:36 -0300

    Fixed reference to self attributes

--- app/models/ecommerce/order_state_transition.rb
+++ app/models/ecommerce/order_state_transition.rb
@@ -7,9 +7,9 @@ class OrderStateTransition < ActiveRecord::Base
   def snapshot
     order = self.order
     if order.state != "in_the_cart"
-      payment_response = order.payment.payment_response.status
-      payment_response_status = order.payment.payment_response.response_status
-      gateway_status_reason = order.payment.gateway_status_reason
+      self.payment_response = order.payment.payment_response.status
+      self.payment_response_status = order.payment.payment_response.response_status
+      self.gateway_status_reason = order.payment.gateway_status_reason
     end
   end
 
