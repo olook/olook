933fa6b046986ffe0e710dba688271c6ea3612bb
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-12 16:41:32 -0300

    Deleted old files and added new version

--- db/migrate/20120629172724_create_timeline_tracks.rb
+++
@@ -1,9 +0,0 @@
-class CreateTimelineTracks < ActiveRecord::Migration
-  def change
-    create_table :timeline_tracks do |t|
-      t.integer :order_id
-      t.text :timeline
-      t.timestamps
-    end
-  end
-end
