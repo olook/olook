724fd23df8184f55e64dcfbedb74a0f07f5517d9
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-12 16:41:32 -0300

    Deleted old files and added new version

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,5 +1,7 @@
 # -*- encoding : utf-8 -*-
 module OrderHelper
+include ::PurchaseTimeline::Helper
+
   def link_to_tracking_code(order)
     "http://tracking.totalexpress.com.br/poupup_track.php?reid=1537&pedido=#{order.number}&nfiscal=#{order.invoice_number}"
   end
