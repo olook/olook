917f2bcaff09f47db013ca295c0aba1df85356b9
joao paulo <jpcamargo+github@gmail.com>
2012-07-13 16:59:35 -0300

    removed commments and changed code identation

--- app/controllers/admin/lookbook_image_maps_controller.rb
+++ app/controllers/admin/lookbook_image_maps_controller.rb
@@ -1,25 +1,5 @@
 class Admin::LookbookImageMapsController < ApplicationController
   respond_to :js
-  
-  # def index
-  #   @lookbook_image_maps = LookbookImageMap.all
-  #   respond_with :admin, @lookbook_product_maps
-  # end
-
-  # def show
-  #   @lookbook_image_map = LookbookImageMap.find(params[:id])
-  #   respond_with :admin, @lookbook_image_map
-  # end
-
-  # def new
-  #   @lookbook_image_map = LookbookImageMap.new
-  #   respond_with :admin, @lookbook_image_map
-  # end
-
-  # def edit
-  #   @lookbook_image_map = LookbookImageMap.find(params[:id])
-  #   respond_with :admin, @lookbook_image_map
-  # end
 
   def create
     @lookbook_image_map = LookbookImageMap.new(params[:lookbook_image_map])
