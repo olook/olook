a0416f503d2a164b07e329b68a382314a38c7957
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-19 00:00:08 -0300

    Added migration for order_state_transitions

--- db/migrate/20120712150146_change_order_state_transitions.rb
+++ db/migrate/20120712150146_change_order_state_transitions.rb
@@ -1,11 +1,11 @@
 class ChangeOrderStateTransitions < ActiveRecord::Migration
   def up
   	add_column :order_state_transitions, :payment_response, :string
-  	add_column :order_state_transitions, :payment_response_status, :string
+  	add_column :order_state_transitions, :payment_transaction_status, :string
   end
 
   def down
   	remove_column :order_state_transitions, :payment_response, :string
-  	remove_column :order_state_transitions, :payment_response_status, :string
+  	remove_column :order_state_transitions, :payment_transaction_status, :string
   end
 end
