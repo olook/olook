16e31ec2d29ae45e13250c7fc59ee6a0df38f69f
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2012-07-19 00:03:41 -0300

    Refactored code

--- app/helpers/purchase_timeline/helper.rb
+++ app/helpers/purchase_timeline/helper.rb
@@ -4,33 +4,29 @@ module PurchaseTimeline
     ::BusinessTime::Config.beginning_of_workday = "8:00 am"
     ::BusinessTime::Config.end_of_workday = "6:00 pm"
 
-    def to_timeline_date_format(order_date)
+    def timeline_date_format(order_date)
      order_date.to_date.to_s.gsub!("-",",")
     end
 
-    def calculate_delivered_date(payment_date, delivery_estimation)
-      delivery_estimation.business_days.after(payment_date).strftime('%A, %e %B %Y')
-    end
-
-    def timeline_delivered_date(payment_date, delivery_estimation)
-      delivery_estimation.business_days.after(payment_date).to_date.to_s.gsub!("-",",")
-    end
-
-    def timeline_payment_expiration_date(order)
-    	order.payment.payment_expiration_date.to_date.to_s.gsub!("-",",")
-   	end
-    
-    def format_time(date)
+    def headline_time_format(date)
       date.to_datetime.strftime("%H:%M:%S")
     end
 
     def shipping_service_art(order)
-      case order.freight.shipping_service.erp_code
-        when "TEX"
-          "total-express.jpg"
-        when "PAC"
-          "correios.png"
-      end
+      shipping_service = order.freight.shipping_service.erp_code.downcase
+      image_tag "/assets/purchase_timeline/#{shipping_service}.png", :alt => shipping_service
+    end
+
+    def headline_delivered_date_format(payment_date, delivery_estimation)
+      calculate_delivered_date(payment_date, delivery_estimation).strftime('%A, %e %B %Y')
+    end
+
+    def timeline_delivered_date_format(payment_date, delivery_estimation)
+      calculate_delivered_date(payment_date, delivery_estimation).to_s.gsub!("-",",")
+    end
+    
+    def calculate_delivered_date(payment_date, delivery_estimation)
+      delivery_estimation.business_days.after(payment_date).to_date
     end
 
   end
