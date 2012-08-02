1ec715cbdd905be4d3aaef2acfaf4b0e2f6d4f39
Raul Souza Lima <raulsouzalima@gmail.com>
2012-08-02 09:44:30 -0300

    Updated helena products on stylist page

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,3 +1,7 @@
 class StylistsController < ApplicationController
   before_filter :load_order
+
+  def helena_linhares
+    @products_didi = Product.find(6391, 5591, 6463, 5894, 5627, 5777)
+  end
 end
