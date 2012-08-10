3a8babafd353f0db2c822d9e3877da3ba9507dcf
klebervirgilio <klebervirgilio@gmail.com>
2012-08-10 13:30:16 -0300

    Removing limit of credit for users

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -5,8 +5,6 @@ class Credit < ActiveRecord::Base
   belongs_to :order
   validates :value, :presence => true
 
-  LIMIT_FOR_EACH_USER = BigDecimal.new("400.00")
-
   INVITE_BONUS = BigDecimal.new("10.00")
 
   # TO DO: Refactor this class and move these methods to CreditService class
@@ -20,7 +18,7 @@ class Credit < ActiveRecord::Base
   def self.add_for_inviter(buyer, order)
     # TO DO: Double check to see if the credit was already gave for this order
     inviter = buyer.try(:inviter)
-    if inviter && buyer.first_buy? && inviter.has_not_exceeded_credit_limit?(INVITE_BONUS)
+    if inviter && buyer.first_buy?
       updated_total = inviter.current_credit + INVITE_BONUS
       inviter.credits.create!(:value => INVITE_BONUS, :total => updated_total, :order => order, :source => "invitee_bonus", :reason => "#{buyer.name} first buy")
     else
@@ -39,11 +37,9 @@ class Credit < ActiveRecord::Base
   end
 
   def self.add(amount, user, order)
-    if user.has_not_exceeded_credit_limit?(INVITE_BONUS)
-      updated_total = user.current_credit + amount
-      user.credits.create!(:value => amount, :total => updated_total, :order => order, :source => "order_credit",
-      :reason => "Order #{order.number} canceled")
-    end
+    updated_total = user.current_credit + amount
+    user.credits.create!(:value => amount, :total => updated_total, :order => order, :source => "order_credit",
+    :reason => "Order #{order.number} canceled")
   end
 
 end
