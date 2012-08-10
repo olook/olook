2d2fe1ccb0cb7e0862a416583afa812085000fac
Raul Souza Lima <raulsouzalima@gmail.com>
2012-08-10 19:19:38 -0300

    Added copy on share link

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -1,3 +1,9 @@
 $(function() {
-
+  $("section#friends_credits div.link_mail ul li a").zclip({
+    path: "/assets/ZeroClipboard.swf",
+    copy: function() { return $("section#friends_credits div.link_mail ul li input").val(); },
+    afterCopy: function(){
+      $("section#friends_credits div.link_mail div.box_copy").fadeIn().delay(2000).fadeOut();
+    }
+  });
 })
