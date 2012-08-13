9b89f1e9555466bbab5d1366141980022f5269fe
joao paulo <jpcamargo+github@gmail.com>
2012-08-13 11:57:46 -0300

    updated search queries, still missing a way to use the shoe size without breaking the rest of the queries

--- app/services/catalog_search_service.rb
+++ app/services/catalog_search_service.rb
@@ -8,7 +8,7 @@ class CatalogSearchService
                   .and(Product.arel_table[:is_visible].eq(true))
                   
     if @liquidation = LiquidationService.active
-      @query_base = @query_base.and(LiquidationProduct.arel_table[:product_id].eq(nil)) # .and(LiquidationProduct.arel_table[:liquidation_id].eq(@liquidation.id))
+      @query_base = @query_base.and(LiquidationProduct.arel_table[:product_id].eq(nil))
     end
 
     @params = params
@@ -22,28 +22,28 @@ class CatalogSearchService
 
     queries = [query_shoe_sizes, query_colors, query_heels, query_subcategories]
     query_shoes = queries.detect{|query| !query.nil?}
-    query_shoes = query_shoes.and(l_products[:category_id].in(Category::SHOE)) if query_shoes
+    # query_shoes = query_shoes.and(l_products[:category_id].in(Category::SHOE)) if query_shoes
 
-    query_bags = params[:bag_subcategories] ? l_products[:subcategory_name].in(params[:bag_subcategories]).and(l_products[:category_id].in(Category::BAG)) : nil
-    query_bags = build_sub_query(query_bags || query_base, Product.arel_table[:color_name].in(params[:bag_colors]).and(l_products[:category_id].in(Category::BAG))) if params[:bag_colors]
+    query_bags = params[:bag_subcategories] ? l_products[:subcategory_name].in(params[:bag_subcategories]) : nil
+    query_bags = build_sub_query(query_bags || query_base, Product.arel_table[:color_name].in(params[:bag_colors])) if params[:bag_colors]
+    query_bags = query_bags.and(l_products[:category_id].in(Category::BAG)) if query_bags
 
-    query_accessories = params[:accessory_subcategories] ? l_products[:subcategory_name].in(params[:accessory_subcategories]).and(l_products[:category_id].in(Category::ACCESSORY)) : nil
+    query_accessories = params[:accessory_subcategories] ? l_products[:subcategory_name].in(params[:accessory_subcategories]) : nil
+    query_accessories = query_accessories.and(l_products[:category_id].in(Category::ACCESSORY)) if query_accessories
 
     all_queries = [query_shoes, query_bags, query_accessories].compact
 
     @query_base = case all_queries.size
       when 1 then
-        puts "1 query"
         @query_base.and(all_queries[0])
       when 2 then
-        puts "2 queries"
         @query_base.and(all_queries[0].or(all_queries[1]))
       when 3 then
-        puts "3 queries"
         @query_base.and(all_queries[0].or(all_queries[1]).or(all_queries[2]))
+      else
+        @query_base
     end
 
-    #TODO: ADD in includes: master_variant, main_picture
     @query = Catalog::Product.joins(:product)
     if @liquidation
       @query = @query.joins('left outer join liquidation_products on liquidation_products.product_id = catalog_products.product_id')
