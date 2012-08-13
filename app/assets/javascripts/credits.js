eaf73a1fa3be659c2dddf7fae5a9dc26a519102a
Raul Souza Lima <raulsouzalima@gmail.com>
2012-08-13 11:18:26 -0300

    Added some js functions to credits

--- app/assets/javascripts/credits.js
+++ app/assets/javascripts/credits.js
@@ -1,4 +1,6 @@
 $(function() {
+  initCredits.createOrkutShareButton();
+
   $("section#friends_credits div.link_mail ul li a").zclip({
     path: "/assets/ZeroClipboard.swf",
     copy: function() { return $("section#friends_credits div.link_mail ul li input").val(); },
@@ -6,4 +8,14 @@ $(function() {
       $("section#friends_credits div.link_mail div.box_copy").fadeIn().delay(2000).fadeOut();
     }
   });
-})
+
+  orkutButton = $("#orkut_share").find("img").first();
+  $("div.box_invite.clone div.social ul li.orkut").append(orkutButton);
+});
+
+initCredits = {
+  createOrkutShareButton : function() {
+    orkutButton = $("#orkut_share").find("img").first();
+    $("section#friends_credits div.link_mail ul li.orkut").append(orkutButton);
+  }
+}
