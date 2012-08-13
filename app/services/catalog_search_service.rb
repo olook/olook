322a1884e8318697aedcbee2290f1ea4e283dfee
joao paulo <jpcamargo+github@gmail.com>
2012-08-13 11:17:07 -0300

    splitted bags and accessories

--- app/services/catalog_search_service.rb
+++ app/services/catalog_search_service.rb
@@ -18,22 +18,30 @@ class CatalogSearchService
     query_subcategories = params[:shoe_subcategories] ? l_products[:subcategory_name].in(params[:shoe_subcategories]) : nil
     query_heels =  params[:heels] ? build_sub_query((query_subcategories || query_base), l_products[:heel].in(params[:heels])) : nil
     query_colors = params[:shoe_colors] ? build_sub_query((query_heels || query_subcategories || query_base), Product.arel_table[:color_name].in(params[:shoe_colors])) : nil
-    query_shoe_sizes = (query_subcategories || query_heels || query_colors) && params[:shoe_sizes] ? build_sub_query((query_colors || query_heels || query_subcategories || query_base), l_products[:shoe_size].in(params[:shoe_sizes])) : nil
-    
+    query_shoe_sizes = (query_colors || query_heels || query_subcategories) && params[:shoe_sizes] ? build_sub_query((query_colors || query_heels || query_subcategories || query_base), l_products[:shoe_size].in(params[:shoe_sizes])) : nil
+
     queries = [query_shoe_sizes, query_colors, query_heels, query_subcategories]
-    query_result = queries.detect{|query| !query.nil?}
+    query_shoes = queries.detect{|query| !query.nil?}
+    query_shoes = query_shoes.and(l_products[:category_id].in(Category::SHOE)) if query_shoes
 
-    query_bags_acessories = params[:bag_accessory_subcategories] ? l_products[:subcategory_name].in(params[:bag_accessory_subcategories]) : nil
-    query_bags_acessories = build_sub_query(query_bags_acessories || query_base, Product.arel_table[:color_name].in(params[:bag_colors])) if params[:bag_colors]
+    query_bags = params[:bag_subcategories] ? l_products[:subcategory_name].in(params[:bag_subcategories]).and(l_products[:category_id].in(Category::BAG)) : nil
+    query_bags = build_sub_query(query_bags || query_base, Product.arel_table[:color_name].in(params[:bag_colors]).and(l_products[:category_id].in(Category::BAG))) if params[:bag_colors]
 
-    if query_bags_acessories
-      query_bags_acessories = build_sub_query(query_bags_acessories, l_products[:category_id].in(Category::BAG))
-      @query_base = query_result ? @query_base.and(query_result.or(query_bags_acessories)) : @query_base.and(query_bags_acessories)
-    else
-      @query_base = @query_base.and(query_result) if query_result
+    query_accessories = params[:accessory_subcategories] ? l_products[:subcategory_name].in(params[:accessory_subcategories]).and(l_products[:category_id].in(Category::ACCESSORY)) : nil
+
+    all_queries = [query_shoes, query_bags, query_accessories].compact
+
+    @query_base = case all_queries.size
+      when 1 then
+        puts "1 query"
+        @query_base.and(all_queries[0])
+      when 2 then
+        puts "2 queries"
+        @query_base.and(all_queries[0].or(all_queries[1]))
+      when 3 then
+        puts "3 queries"
+        @query_base.and(all_queries[0].or(all_queries[1]).or(all_queries[2]))
     end
-
-
 
     #TODO: ADD in includes: master_variant, main_picture
     @query = Catalog::Product.joins(:product)
