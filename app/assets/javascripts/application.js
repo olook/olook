36547e5b888193004d4d81c6115430f44941c4a0
eks <edu.fox@gmail.com>
2012-08-14 11:26:37 -0300

    fix for css and js wrong replacement

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,3 +6,9 @@
 //
 //= require jquery_ujs
 //= require_tree .
+//= require_tree ./plugins
+//= require_tree ./common
+//= require ./ui/jquery.ui.core.min
+//= require ./ui/jquery.ui.position.min
+//= require ./ui/jquery.ui.widget.min
+//= require ./ui/jquery.ui.dialog.min
