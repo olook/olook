d3cf06928d7fbc3d14abc6b7eb3f38cce3469afe
Luis Daher <luisotaviodaher@gmail.com>
2012-08-14 18:51:43 -0300

    loyalty credit started

--- app/models/loyalty_program_credit_type.rb
+++ app/models/loyalty_program_credit_type.rb
@@ -1,7 +1,33 @@
 class LoyaltyProgramCreditType < CreditType
-  
+
   def credit_sum(user_credit, date, is_debit)
   	user_credit.credits.where("activates_at <= ? AND expires_at >= ? AND is_debit = ?", date, date, is_debit).sum(:value)
   end
+
+  #TODO: create a mechanism that nullifies the available credits until the debit is paid
+  def remove(amount, user_credit, order)
+    if user_credit.total >= amount
+      true
+    else
+      false
+    end
+  end
+
+  def add(amount, user_credit, order)
+    updated_total = user_credit.total + amount
+    user_credit.credits.create!(:activates_at => period_start, :expires_at => period_end, :user => user_credit.user, :value => amount, :total => updated_total, :order => order, :source => "loyalty_program_credit",
+    :reason => "Fidelity credits for order #{order.number}")
+  end
+
+  private 
+    def period_start date = DateTime.now
+      date += 1.month
+      date.at_beginning_of_month
+    end
+
+    def period_end date = DateTime.now
+      date += 2.months
+      date.at_end_of_month
+    end
 
 end
--- spec/models/loyalty_program_credit_type_spec.rb
+++ spec/models/loyalty_program_credit_type_spec.rb
@@ -1,5 +1,43 @@
 require 'spec_helper'
 
 describe LoyaltyProgramCreditType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.create(:member) }
+
+  let(:loyalty_program_credit_type) { FactoryGirl.create(:loyalty_program_credit_type) }
+  let(:user_credit) { FactoryGirl.create(:user_credit, :user => user, :credit_type => loyalty_program_credit_type) }
+
+  describe "credit operations" do
+    let(:order) {FactoryGirl.create(:order, :user => user)}
+    let(:amount) { BigDecimal.new("33.33") }
+
+    describe "adding credits" do
+      context "when user creates a credit" do
+        it "should add credits" do
+          loyalty_program_credit_type.add(amount,user_credit,order)
+          loyalty_program_credit_type.total(user_credit, DateTime.now + 1.month).should == amount
+        end
+      end
+    end
+
+    describe "removing credits" do
+
+      context "when user has enough credits" do
+        xit "should remove credits" do
+          loyalty_program_credit_type.add(amount,user_credit,order)
+          loyalty_program_credit_type.remove(amount,user_credit,order)
+
+          loyalty_program_credit_type.total(user_credit, DateTime.now + 1.month).should == 0.0
+        end
+      end
+
+      context "when user hasn't got enough credits" do
+        xit "should not remove credits and should return false" do
+          loyalty_program_credit_type.remove(amount,user_credit,order).should == false
+
+          loyalty_program_credit_type.total(user_credit, DateTime.now + 1.month).should == 0.0
+        end
+      end      
+    end
+
+  end
 end
