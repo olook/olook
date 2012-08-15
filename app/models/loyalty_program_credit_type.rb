8117d2f705a058631ab39c696b5a52a5d062699c
Luis Daher <luisotaviodaher@gmail.com>
2012-08-15 11:30:28 -0300

    credit type rules described

--- app/models/loyalty_program_credit_type.rb
+++ app/models/loyalty_program_credit_type.rb
@@ -6,6 +6,13 @@ class LoyaltyProgramCreditType < CreditType
 
   #TODO: create a mechanism that nullifies the available credits until the debit is paid
   def remove(amount, user_credit, order)
+    
+    # busca os creditos mais proximos de expirar e que nao foram usados
+    #  cria um debito para anular cada um dos creditos. 
+    #  Caso todo o debito seja processado e ainda tenha sobrado creditos, anula todo o credito do ultimo
+    #  e cria um credito novo com o que sobrou.
+    #  Cada um dos creditos utilizados devem ter o debito designado, para ser marcado como usado.
+    #  NOTA: TODOS OS DEBITOS (E O CREDITO DE RESTO GERADO) DEVERAO TER AS MESMAS DATAS DE ATIVACAO E EXPIRACAO DOS CREDITOS CORRESPONDENTES. 
     if user_credit.total >= amount
       true
     else
