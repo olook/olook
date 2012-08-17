cef78d9426f728f7a8df7253048e7d5c26199f4d
Luis Daher <luisotaviodaher@gmail.com>
2012-08-17 17:45:10 -0300

    process user credits tests

--- app/models/user_credit.rb
+++ app/models/user_credit.rb
@@ -16,9 +16,26 @@ class UserCredit < ActiveRecord::Base
     self.credit_type.remove(amount, self, order)
   end
   
-  def self.add_for_inviter(order)
-    #CRIAR O CREDITO MGM PARA O INVITER
-    #CRIAR O CREDITO DE FIDELIDADE
+  def self.process!(order)
+    # TO DO: Double check to see if the credit was already given for this order
+
+    # creates MGM credits for inviter
+    UserCredit.add_invite_credits(order)
+    # creates loyalty program credits
+    UserCredit.add_loyalty_program_credits(order)
   end
+
+  private
+    def self.add_invite_credits(order)
+      buyer = order.user
+      inviter = buyer.try(:inviter)
+      inviter.user_credits_for(:invite).add(INVITE_BONUS, order) if inviter && buyer.first_buy?
+    end
+
+    def self.add_loyalty_program_credits(order)
+      user_credit = order.user.user_credits_for(:loyalty_program)
+      amount = order.amount_paid*0.20
+      user_credit.add(amount, order)
+    end
 
 end
