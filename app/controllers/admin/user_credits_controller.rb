5d5014ed388d3776f9ef53dc341d1edf69e2c5a8
klebervirgilio <klebervirgilio@gmail.com>
2012-08-20 14:32:50 -0300

    Admin credits dashboard: Orders vision, changed all the implementation to use meta_search gem.

--- app/controllers/admin/user_credits_controller.rb
+++ app/controllers/admin/user_credits_controller.rb
@@ -1,2 +1,7 @@
 class Admin::UserCreditsController < Admin::BaseController
+  #TODO: Implement it after payment refactoring...
+  def index
+    flash[:notice] = "Wating for the payment refactoring to be implemented..."
+    redirect_to admin_credits_path
+  end
 end
