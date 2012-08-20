d31a8515ff0032379e81568f5693fa247181d027
eks <edu.fox@gmail.com>
2012-08-20 11:25:58 -0300

    fix for precompile css needed

--- config/deploy/prodspare.rb
+++ config/deploy/prodspare.rb
@@ -1,6 +1,6 @@
-#role :app, 'app4.olook.com.br', 'app5.olook.com.br', 'app6.olook.com.br'
-role :app, 'app6.olook.com.br'
-role :web, 'app6.olook.com.br'
+role :app, 'app4.olook.com.br', 'app5.olook.com.br', 'app6.olook.com.br'
+#role :app, 'app6.olook.com.br'
+#role :web, 'app6.olook.com.br'
  
 # repo details
 set :branch, fetch(:branch, 'master')
@@ -39,8 +39,8 @@ namespace :deploy do
   desc 'Run migrations, clean assets'
   task :rake_tasks, :role => :app do
     run "cd #{path_app} && #{bundle} exec #{rake} db:migrate RAILS_ENV=#{rails_env}"
-    #run "cd #{path_app} && #{bundle} exec #{rake} assets:clean RAILS_ENV=#{rails_env}"
-    #run "cd #{path_app} && #{bundle} exec #{rake} assets:precompile RAILS_ENV=#{rails_env}"
+    run "cd #{path_app} && #{bundle} exec #{rake} assets:clean RAILS_ENV=#{rails_env}"
+    run "cd #{path_app} && #{bundle} exec #{rake} assets:precompile RAILS_ENV=#{rails_env}"
     run "cd #{path_app} && #{bundle} exec #{rake} olook:create_permissions RAILS_ENV=#{rails_env}"
   end
 
@@ -61,69 +61,6 @@ namespace :deploy do
     run "ln -nfs #{deploy_to}/shared/unicorn.conf.rb #{version_path}/config/unicorn.conf.rb"
   end
 
-
-
-
-
-
-#   namespace :assets do
-#     desc <<-DESC
-#       [internal] This task will set up a symlink to the shared directory \\\\\\\\
-#       for the assets directory. Assets are shared across deploys to avoid \\\\\\\\
-#       mid-deploy mismatches between old application html asking for assets \\\\\\\\
-#       and getting a 404 file not found error. The assets cache is shared \\\\\\\\
-#       for efficiency. If you customize the assets path prefix, override the \\\\\\\\
-#       :assets_prefix variable to match.
-#     DESC
-#     task :symlink, :roles => assets_role, :except => { :no_release => true } do
-#       run <<-CMD
-#         rm -rf #{latest_release}/public/#{assets_prefix} &&
-#         mkdir -p #{latest_release}/public &&
-#         mkdir -p #{shared_path}/assets &&
-#         ln -s #{shared_path}/assets #{latest_release}/public/#{assets_prefix}
-#       CMD
-#     end
-# 
-#     desc <<-DESC
-#       Run the asset precompilation rake task. You can specify the full path \\\\\\\\
-#       to the rake executable by setting the rake variable. You can also \\\\\\\\
-#       specify additional environment variables to pass to rake via the \\\\\\\\
-#       asset_env variable. The defaults are:
-# 
-#       set :rake,      "rake"
-#       set :rails_env, "production"
-#       set :asset_env, "RAILS_GROUPS=assets"
-#     DESC
-#     task :precompile, :roles => assets_role, :except => { :no_release => true } do
-#       run "cd #{latest_release} && #{bundle} exec #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
-#     end
-# 
-#     desc <<-DESC
-#       Run the asset clean rake task. Use with caution, this will delete \\\\\\\\
-#       all of your compiled assets. You can specify the full path \\\\\\\\
-#       to the rake executable by setting the rake variable. You can also \\\\\\\\
-#       specify additional environment variables to pass to rake via the \\\\\\\\
-#       asset_env variable. The defaults are:
-# 
-#       set :rake,      "rake"
-#       set :rails_env, "production"
-#       set :asset_env, "RAILS_GROUPS=assets"
-#     DESC
-#     task :clean, :roles => assets_role, :except => { :no_release => true } do
-#       run "cd #{latest_release} && #{bundle} exec #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:clean"
-#     end
-#   end
-
-
-
-
-
-
-
-
-
-
-
   desc 'Stop unicorn'
   task :stop_unicorn, :roles => :app do
     run "if [ -f /var/run/olook-unicorn.pid ]; then pid=`cat /var/run/olook-unicorn.pid` && kill -TERM $pid; fi"
@@ -138,14 +75,5 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "if [ -f /var/run/olook-unicorn.pid ]; then pid=`cat /var/run/olook-unicorn.pid` && kill -USR2 $pid; else cd #{current_path} && bundle exec unicorn_rails -c #{current_path}/config/unicorn.conf.rb -E #{rails_env} -D; fi"
   end
-
-
-
-  # after 'deploy:update', 'deploy:bundle_install' # keep only the last 5 releases
-  # before 'deploy:assets:precompile', 'deploy:bundle_install'
-
-  # before 'deploy:finalize_update', 'deploy:assets:symlink'
-  # after 'deploy:update_code', 'deploy:assets:precompile'
-
 
 end
