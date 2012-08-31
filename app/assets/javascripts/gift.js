892726b0d3109a142404f87cd332941a41758c69
Raul Souza Lima <raulsouzalima@gmail.com>
2012-08-31 18:28:51 -0300

    Updated recipient page

--- app/assets/javascripts/gift.js
+++ app/assets/javascripts/gift.js
@@ -6,17 +6,17 @@ $(function () {
     event.preventDefault();
   });
 
-  $("div#profile form ul.shoes li").live("click", function() {
-    $("div#profile form ul.shoes li").removeClass("selected");
-    $("div#profile form ul.shoes li label input[type='radio']").removeAttr("checked");
+  $("section#profile form ul.shoes li").live("click", function() {
+    $("section#profile form ul.shoes li").removeClass("selected");
+    $("section#profile form ul.shoes li label input[type='radio']").removeAttr("checked");
     $(this).find("label").find("input[type='radio']").attr("checked", "checked");
     if($(this).find("label").find("input[type='radio']").is(":checked")) {
       $(this).addClass("selected");
     }
   });
 
-  $("div#profile a.select_profile").live("click", function(event) {
-    $("div#profile div.profiles").slideDown('normal', function() {
+  $("section#profile a.select_profile").live("click", function(event) {
+    $("section#profile div.profiles").slideDown('normal', function() {
       container_position = $(this).position().top;
       position = container_position - 40;
       $('html, body').animate({
