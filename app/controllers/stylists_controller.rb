8f62e023f30832311e06b2f10cf778fe2fe4cc2f
Raul Souza Lima <raulsouzalima@gmail.com>
2012-09-01 03:13:30 -0300

    Updated didi products on stylist page

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,6 +1,6 @@
 class StylistsController < ApplicationController
   def helena_linhares
     @products_helena = Product.find(9408, 7425, 9276, 8536, 7787, 8964)
-    @products_didi = Product.find(9075, 7965)
+    @products_didi = Product.find(9075, 7965, 7679, 9004, 9303)
   end
 end
