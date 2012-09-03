124f35354d8cce66c6486fb073317733f2754949
klebervirgilio <klebervirgilio@gmail.com>
2012-09-03 19:05:58 -0300

    Change orders state.

--- app/controllers/admin/order_credits_controller.rb
+++ app/controllers/admin/order_credits_controller.rb
@@ -3,12 +3,5 @@ class Admin::OrderCreditsController < Admin::BaseController
   def index
     @search = Order.search(params[:search])
     @orders = @search.relation.uniq.payments_with_discount.order('amount_discount desc').page(params[:page]).per_page(15)
-    percentage = params[:percentage]
-
-    @orders = @orders.select do |order|
-      total = order.payments.sum(&:total_paid)
-      (order.amount_paid*100/percentage.to_f)-order.amount_paid >= total
-    end if percentage
-
   end
 end
