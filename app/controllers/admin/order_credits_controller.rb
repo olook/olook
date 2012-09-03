06c268d15cc1b66ac629e21b628a8283880ebcd0
klebervirgilio <klebervirgilio@gmail.com>
2012-09-03 14:52:50 -0300

    Payment aditional infos in admin

--- app/controllers/admin/order_credits_controller.rb
+++ app/controllers/admin/order_credits_controller.rb
@@ -2,6 +2,6 @@ class Admin::OrderCreditsController < Admin::BaseController
   
   def index
     @search = Order.search(params[:search])
-    @orders = @search.relation.paid.order('amount_discount desc').page(params[:page]).per_page(15)
+    @orders = @search.relation.includes(:payments).paid.order('amount_discount desc').page(params[:page]).per_page(15)
   end
 end
