84cc2c60f3e365222d3d4389822f769cc927db6f
klebervirgilio <klebervirgilio@gmail.com>
2012-09-03 18:03:02 -0300

    Rollbacking association between user and credit.

--- app/models/credit.rb
+++ app/models/credit.rb
@@ -4,6 +4,7 @@ class Credit < ActiveRecord::Base
   belongs_to :order
   has_many :debits, :class_name => "Credit", :foreign_key => "original_credit_id"
   validates :value, :presence => true
+  belongs_to :user
 
   INVITE_BONUS = BigDecimal.new("10.00")
 end
