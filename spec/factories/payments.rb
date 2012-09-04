075baf0cb6fe5fab5f8c1ac415cbd91ae412d0f4
Renato Elias <renato.elias@gmail.com>
2012-09-04 11:33:05 -0300

    Olooklet Payment / Gift Payment / Gross Amount / Percent in Payment

--- spec/factories/payments.rb
+++ spec/factories/payments.rb
@@ -11,17 +11,20 @@ FactoryGirl.define do
     telephone '(35)3456-6849'
     user_birthday '12/09/1976'
     receipt 'AVista'
+    total_paid 12.34
   end
 
   factory :debit do
     url 'www.payment.com'
     bank 'Visa'
     receipt 'AVista'
+    total_paid 12.34
   end
 
   factory :billet do
     url 'www.payment.com'
     receipt 'AVista'
+    total_paid 12.34
   end
 
   factory :payment do
