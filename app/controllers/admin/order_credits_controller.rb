dd8e82a0481e77a3710bf483c222f2201a74d26d
klebervirgilio <klebervirgilio@gmail.com>
2012-09-04 11:20:52 -0300

    Search percentage option.

--- app/controllers/admin/order_credits_controller.rb
+++ app/controllers/admin/order_credits_controller.rb
@@ -2,6 +2,11 @@ class Admin::OrderCreditsController < Admin::BaseController
   
   def index
     @search = Order.search(params[:search])
-    @orders = @search.relation.uniq.payments_with_discount.order('amount_discount desc').page(params[:page]).per_page(15)
+    @orders = @search.relation
+                      .uniq
+                      .payments_with_discount
+                      .order('amount_discount desc')
+                      .where('ifnull(payments.discount_percent,0) >= ?',params[:percentage].to_s.to_f)
+                      .page(params[:page]).per_page(15)
   end
 end
