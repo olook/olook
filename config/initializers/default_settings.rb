19e726295bc1ee7261a641c9d91bb5e9ad49a409
Luis Daher <luisotaviodaher@gmail.com>
2012-09-05 11:45:15 -0300

    settings ok

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -4,3 +4,4 @@ Setting.defaults[:invite_credits_available] = true
 Setting.defaults[:loyalty_program_credits_available] = true
 Setting.defaults[:invite_credits_bonus_for_inviter] = "10.00"
 Setting.defaults[:invite_credits_bonus_for_invitee] = "10.00"
+Setting.defaults[:percentage_on_order] = "0.20"
