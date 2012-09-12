94d3a85e2ac6a8c93d5e99602c7002acc808223c
Renato Elias <renato.elias@gmail.com>
2012-09-12 16:45:42 -0300

    Tela de credito

--- app/controllers/users/credits_controller.rb
+++ app/controllers/users/credits_controller.rb
@@ -6,7 +6,12 @@ class Users::CreditsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    
+    @loyalty_credits = @user.user_credits_for(:loyalty_program).credits.where(:is_debit => false).includes(:order).order("id")
+    @invite_credits = @user.user_credits_for(:invite).credits.where(:is_debit => false).includes(:order).order("id")
+    @redeem_credits = @user.user_credits_for(:redeem).credits.where(:is_debit => false).includes(:order).order("id")
+    @used_credits = Credit.joins(:user_credit).where(:user_credits => {:user_id => @user}, :is_debit => true).includes(:order).order("id")
+    @available_credits = @user.current_credit
+    @holding_credits = @user.current_credit(DateTime.now, :holding)
   end
   
 end
