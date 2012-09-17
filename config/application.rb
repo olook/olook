a33ed5bc57bdaaeeaec0f3cd73b9728270481d8a
eks <edu.fox@gmail.com>
2012-09-17 12:38:46 -0300

    precompiling adjustments

--- config/application.rb
+++ config/application.rb
@@ -53,7 +53,7 @@ module Olook
 
     config.action_mailer.default_url_options = { :host => 'www.olook.com.br' }
 
-    config.middleware.use "Graylog2Exceptions", { :hostname => '107.21.158.126', :port => '12201', :level => 0 }
+    #config.middleware.use "Graylog2Exceptions", { :hostname => '107.21.158.126', :port => '12201', :level => 0 }
 
     config.middleware.delete Rack::Cache
   end
