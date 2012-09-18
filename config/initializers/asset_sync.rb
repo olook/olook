ac3b5ffccc154a105529f8dc5a819e100392faa2
eks <edu.fox@gmail.com>
2012-09-18 16:55:20 -0300

    alterations in assets_sync initializer

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,17 +1,21 @@
 AssetSync.configure do |config|
   config.fog_provider = 'AWS'
-  config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  config.fog_directory = ENV['FOG_DIRECTORY']
+  #config.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
+  config.aws_access_key_id = 'AKIAJ2WH3XLYA24UTAJQ'
+  #config.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
+  config.aws_secret_access_key = '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'
+  #config.fog_directory = ENV['FOG_DIRECTORY']
+  bucket = Rails.env.production? ? 'cdn-app.olook.com.br' : 'cdn-app-staging.olook.com.br'
+  config.fog_directory = bucket 
   
   # Increase upload performance by configuring your region
-  # config.fog_region = 'eu-west-1'
+  config.fog_region = 'sa-east-1'
   #
   # Don't delete files from the store
-  # config.existing_remote_files = "keep"
+  config.existing_remote_files = "delete"
   #
   # Automatically replace files with their equivalent gzip compressed version
-  # config.gzip_compression = true
+  config.gzip_compression = true
   #
   # Use the Rails generated 'manifest.yml' file to produce the list of files to 
   # upload instead of searching the assets directory.
