51fa38c60c29bb676875f1afd2c31556e43caa71
joao paulo <jpcamargo+github@gmail.com>
2012-09-18 11:08:51 -0300

    file creation happens even if the .upload was not called for generate_userbase_with_auth_token

--- lib/marketing_reports/builder.rb
+++ lib/marketing_reports/builder.rb
@@ -44,14 +44,39 @@ module MarketingReports
 
     def generate_userbase_with_auth_token
       bounces = bounced_list
+      header = %w{ id email created_at sign_in_count current_sign_in_at last_sign_in_at invite_token first_name last_name facebook_token birthday has_purchases auth_token}
+
+      @file_name = "base_atualizada_purchases_auth_token_#{Time.now.strftime("%Y-%m-%d")}.csv"
+      
+      if Rails.env.development?
+        file = File.new(@file_name, "w+")
+        file.write(header.join(',') + "\n") if file.size == 0
+      end
+
       @csv = CSV.generate do |csv|
-        csv << %w{ id email created_at sign_in_count current_sign_in_at last_sign_in_at invite_token first_name last_name facebook_token birthday has_purchases auth_token}
+        csv << header
+        
         User.where("gender != #{User::Gender[:male]} or gender is null").each do |u|
           unless bounces.include?(u.email)
-            csv << [ u.id, u.email.chomp, u.created_at, u.sign_in_count, u.current_sign_in_at, u.last_sign_in_at, u.invite_token, u.first_name.chomp, u.last_name.chomp, u.facebook_token, u.birthday, u.has_purchases?, u.authentication_token ]
+            line = [ u.id, u.email.chomp, u.created_at, u.sign_in_count, u.current_sign_in_at, u.last_sign_in_at, u.invite_token, u.first_name.chomp, u.last_name.chomp, u.facebook_token, u.birthday, u.has_purchases?, u.authentication_token ]
+            csv << line
+            file.write(line.join(',') + "\n") if Rails.env.development?
           end
         end
-        emails_seed_list.each { |email| csv << [ nil, email, nil, nil, nil, nil, nil, 'seed list', nil, nil, nil, nil, nil ] }
+
+        emails_seed_list.each { |email|
+          line = [ nil, email, nil, nil, nil, nil, nil, 'seed list', nil, nil, nil, nil, nil ]
+          csv << line
+          file.write(line.join(',') + "\n") if Rails.env.development?
+        }
+      end
+
+      if Rails.env.development?
+        file.close
+
+        file = File.new("#{Time.now.strftime("%Y-%m-%d-%H-%M")}_base_atualizada_purchases_auth_token.csv", "w")
+        file.write(@csv)
+        file.close
       end
     end
 
