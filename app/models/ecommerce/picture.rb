98c4cd880bf8dc804caf40bf03b836de3c35e69b
joao paulo <jpcamargo+github@gmail.com>
2012-09-24 17:19:52 -0300

    sortable almost done.

--- app/models/ecommerce/picture.rb
+++ app/models/ecommerce/picture.rb
@@ -5,10 +5,15 @@ class Picture < ActiveRecord::Base
   has_enumeration_for :display_on, :with => DisplayPictureOn, :required => true
   mount_uploader :image, PictureUploader
   after_update :invalidate_cdn_image
+  before_save :set_position
 
   private
-
   def invalidate_cdn_image
     CloudfrontInvalidator.new.invalidate(self.image.url.slice(23..150)) unless self.image.url.nil?
+  end
+
+  protected
+  def set_position
+    self.position ||= 1 + (Picture.where(product_id: product_id).maximum(:position) || 0)
   end
 end
