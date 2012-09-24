9844ebdf2b3e4640643797fc7e97c7cc4a43daa2
Luis Daher <luisotaviodaher@gmail.com>
2012-09-24 13:58:57 -0300

    switch deleted

--- app/controllers/admin/toggle_controller.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Admin::ToggleController < Admin::BaseController
-  def switch_mode
-    session[:product_view_mode] = session[:product_view_mode] == "admin" || session[:product_view_mode].nil? ? "user" : "admin"
-    redirect_to :back
-  end
-end
