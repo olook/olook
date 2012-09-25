2c5edbc2602ebd23e3f436ab3105538955d348a5
Renato Elias <renato.elias@gmail.com>
2012-09-25 17:39:05 -0300

    Move Logic of Payments Controller to Set Payment Status Worker & Create Processed Flag

--- app/controllers/checkout/payments_controller.rb
+++ app/controllers/checkout/payments_controller.rb
@@ -5,23 +5,14 @@ class Checkout::PaymentsController < ActionController::Base
 
   def create
     payment = Payment.find_by_identification_code(params["id_transacao"])
-    order = payment.try(:order)
 
-    MoipCallback.create(:order_id => order.try(:id),
-                        :cod_moip => params["cod_moip"],
-                        :tipo_pagamento => params["tipo_pagamento"],
-                        :status_pagamento => params["status_pagamento"],
-                        :id_transacao => params["id_transacao"],
-                        :classificacao => Iconv.conv('UTF-8//IGNORE', "US-ASCII", params["classificacao"]),
-                        :payment_id => payment.try(:id))
-    if payment
-      payment.update_attributes!(:gateway_code   => params["cod_moip"],
-                              :gateway_type   => params["tipo_pagamento"],
-                              :gateway_status => params["status_pagamento"],
-                              :gateway_status_reason => Iconv.conv('UTF-8//IGNORE', "US-ASCII", params["classificacao"]))
+    MoipCallback.create!(:cod_moip => params["cod_moip"],
+                         :tipo_pagamento => params["tipo_pagamento"],
+                         :status_pagamento => params["status_pagamento"],
+                         :id_transacao => params["id_transacao"],
+                         :classificacao => Iconv.conv('UTF-8//IGNORE', "US-ASCII", params["classificacao"]),
+                         :payment_id => payment.try(:id))
 
-       Resque.enqueue_in(1.minute, SetPaymentStatusWorker, payment.id, params["status_pagamento"])
-    end
     render :nothing => true, :status => 200
   end
 end
