3471b63b7b1d48888e6eff199627ffe91c18701b
Matheus Augusto Bodo <mcbodo@gmail.com>
2012-09-27 13:30:44 -0300

    adding list of moip callbacks to admin

---
+++ app/controllers/admin/moip_callbacks_controller.rb
@@ -0,0 +1,11 @@
+class Admin::MoipCallbacksController < Admin::BaseController
+  respond_to :html
+
+  load_and_authorize_resource
+
+  def index
+  	@search = MoipCallback.search(params[:search])
+  	@moip_callbacks = @search.relation.page(params[:page]).per_page(15).order('processed asc, id asc')
+  end
+
+end
