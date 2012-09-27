caa5c7717f39b18c2ca5925c03026927bd95184e
klebervirgilio <klebervirgilio@gmail.com>
2012-09-27 15:40:20 -0300

    Retrieving all products given a collection.

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,6 +2,10 @@
 class ProductController < ApplicationController
   respond_to :html
 
+  rescue_from ActiveRecord::RecordNotFound do
+    render :template => "/errors/404.html.erb", :layout => 'error', :status => 404
+  end
+
   def show
     @facebook_app_id = FACEBOOK_CONFIG["app_id"]
     @url = request.protocol + request.host
