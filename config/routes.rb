3a54ae8a28b6265ecd452811fc461dd2ec461e41
Matheus Augusto Bodo <mcbodo@gmail.com>
2012-09-28 18:15:36 -0300

    adding action to mark moip callback as processed or not processed

--- config/routes.rb
+++ config/routes.rb
@@ -214,7 +214,13 @@ Olook::Application.routes.draw do
 
     resource :settings
 
-    resources :moip_callbacks, :only => [:index, :show]
+    resources :moip_callbacks do
+      member do
+        post 'change_to_processed'
+        post 'change_to_not_processed'
+      end
+    end
+    
     resources :payments, :only => [:index, :show]
 
   end
