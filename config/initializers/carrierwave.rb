da0a6d754136b4abf0132c1abb84f413a3a10e0b
eks <edu.fox@gmail.com>
2012-10-01 16:51:03 -0300

    adjustments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,20 +4,22 @@ CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider => 'AWS'
   }
-  #config.fog_directory = Rails.env.test? ? 'testcdn.olook.com.br' : 'cdn.olook.com.br'
-  #config.fog_host = "http://#{config.fog_directory}"
+  # config.fog_directory = Rails.env.test? ? 'testcdn.olook.com.br' : 'cdn.olook.com.br'
+  # config.fog_host = "http://#{config.fog_directory}"
 
   if Rails.env.test?
-    config.fog_host = 'http://testcdn.olook.com.br'
-  elsif Rails.env.production? || Rails.env.development?
-    config.fog_host = proc do |file|
-      "http://cdn-#{rand(3)}.olook.com.br"
+    config.fog_directory = 'testcdn.olook.com.br'
+  elsif Rails.env.staging?
+    config.fog_directory = proc do |file|
+      "cdn-staging-#{rand(3)}.olook.com.br"
     end
   else
-    config.fog_host = proc do |file|
-      "http://cdn-staging-#{rand(3)}.olook.com.br"
+    config.fog_directory = proc do |file|
+      "cdn-#{rand(3)}.olook.com.br"
     end
+    # config.fog_directory = "cdn.olook.com.br"
   end
+  config.fog_host = "http://#{config.fog_directory}"
   
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000',
                             'Expires' => 1.year.from_now.httpdate }
