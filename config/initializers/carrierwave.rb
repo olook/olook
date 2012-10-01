86bb5f50c90d084284b0790692523cc195ce4714
eks <edu.fox@gmail.com>
2012-10-01 20:46:05 -0300

    more adjustments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,17 +9,22 @@ CarrierWave.configure do |config|
 
   if Rails.env.test?
     config.fog_directory = 'testcdn.olook.com.br'
+    host = 'testcdn.olook.com.br'
+    config.fog_host = "http://#{host}"
   elsif Rails.env.staging?
-    config.fog_directory = proc do |file|
+    config.fog_directory = 'cdn-staging.olook.com.br'
+    host = proc do |file|
       "cdn-staging-#{rand(3)}.olook.com.br"
     end
+    config.fog_host = "http://#{host}"
   else
-    config.fog_directory = proc do |file|
+    config.fog_directory = 'cdn.olook.com.br'
+    host = proc do |file|
       "cdn-#{rand(3)}.olook.com.br"
     end
     # config.fog_directory = "cdn.olook.com.br"
+    config.fog_host = "http://#{host}"
   end
-  config.fog_host = "http://#{config.fog_directory}"
   
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000',
                             'Expires' => 1.year.from_now.httpdate }
