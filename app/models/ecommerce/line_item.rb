6cd8b846610cfea6ac350713c68f0bb726997d9b
Luis Daher <luisotaviodaher@gmail.com>
2012-10-01 20:07:58 -0300

    calculate_loyalty_credit_amount done for line_item. Please create the tests

--- app/models/ecommerce/line_item.rb
+++ app/models/ecommerce/line_item.rb
@@ -19,7 +19,18 @@ class LineItem < ActiveRecord::Base
 
   def calculate_loyalty_credit_amount
     # buscar amount_paid da order
-    # calcular retail_price/amount_paid para saber a porcentagem do produto na quantia paga.
+    # order_amount_paid = self.order.amount_paid
+
+    # buscar soma dos valores dos line_items
+    line_item_sum = self.order.line_items.sum(&:retail_price)
+
+    # calcular retail_price/line_item_sum para saber a porcentagem do produto na quantia paga.
+    percentage = retail_price/line_item_sum
+    
     # buscar crédito gerado pela order
+    total_credit_amount = Credit.where(source: "loyalty_program_credit", order_id: self.order.id).sum(&:value)
+    
+    # devolver quantia através da porcentagem calculada anteriormente
+    (total_credit_amount*percentage).round(2)
   end
 end
