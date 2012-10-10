8e6cf518707120507fe66407f0bcf54924a5906d
Luis Daher <luisotaviodaher@gmail.com>
2012-10-10 12:13:54 -0300

    facebook stuff now working :)

--- app/services/facebook_data_service.rb
+++ app/services/facebook_data_service.rb
@@ -2,6 +2,11 @@
 class FacebookDataService
   include Celluloid
 
+  DESTINATION_FOLDER = "/tmp/fb_csv/"
+
+  def initialize
+  end
+
   # def generate_facebook_friends_birthdays_csv_list(user)
   #   #fb_tokens = facebook_tokens
   #   # users.find_each do |user|
@@ -18,25 +23,24 @@ class FacebookDataService
   def friends_birthdays users
     birthdays = []
     users.each do |user|
-      puts "processing #{user.name}"
-      begin
-        friends = FacebookAdapter.new(user.facebook_token).facebook_friends_with_birthday(DateTime.now.month)
-        friends.each do |friend|
-          birthday_arr = friend.birthday.split("/")
-          friend.birthday = "#{birthday_arr[1]}/#{birthday_arr[0]}"
-          friend_hash = JSON.parse(friend.to_json)["table"]
-          friend_hash["friend_email"] = user.email
-          birthdays << friend_hash
-        end
-      rescue => e
-        puts "#{user.email} - #{e.message}"
+      # puts "processing #{user.name}"
+      adapter = FacebookAdapter.new(user.facebook_token)
+      friends = adapter.facebook_friends_with_birthday(DateTime.now.month)
+      friends.each do |friend|
+        birthday_arr = friend.birthday.split("/")
+        friend.birthday = "#{birthday_arr[1]}/#{birthday_arr[0]}"
+        friend_hash = JSON.parse(friend.to_json)["table"]
+        friend_hash["friend_email"] = user.email
+        birthdays << friend_hash
       end
+      puts "(#{user.id}) #{user.name} => success! #{friends.size} friends!"
     end
 
-    birthdays
+    puts "Saving file - #{users.first.id}-#{users.last.id}.csv"
+    save_file(format_to_csv(birthdays), "#{users.first.id}-#{users.last.id}.csv") unless birthdays.empty?
   end
 
-  def self.format_to_csv hashes
+  def format_to_csv hashes
     fields = hashes.first.keys
 
     # mounts header
@@ -49,6 +53,17 @@ class FacebookDataService
     end
 
     csv_header+"\n"+(csv_body.join("\n"))
+  end
+
+  def save_file(csv, name)
+    File.open("#{DESTINATION_FOLDER}#{name}.csv", 'w') do |file|
+      file.puts csv
+    end
+  end
+
+  def self.clean_destination_folder
+    FileUtils.rm_rf(DESTINATION_FOLDER)
+    FileUtils.mkdir_p(DESTINATION_FOLDER)
   end
 
 end
--- lib/tasks/facebook.rake
+++ lib/tasks/facebook.rake
@@ -2,27 +2,32 @@ namespace :facebook do
   desc 'Generates the csv file with the friends birthdays'
   task :generate_friends_birthdays_csv => :environment do
     t0 = DateTime.now.to_f
-    pool = FacebookDataService.pool(size: 128)
+    pool = FacebookDataService.pool(size: 2)
     puts "executing multithreaded tasks"
+    fbds = FacebookDataService.new
+    starting_point = ENV['STARTING_POINT']? ENV['STARTING_POINT']:0
 
+    FacebookDataService.clean_destination_folder
 
-    futures = FacebookDataService.facebook_users.limit(1000).find_in_batches(:batch_size => 50) do |users| 
-      pool.future(:friends_birthdays, users) 
+    batches = []
+    puts "finding users with facebook authentication token"
+    FacebookDataService.facebook_users.where("id > #{starting_point}").find_in_batches(:batch_size => 50){|users| batches << users}
+    # ok so far 
+
+    
+    batches.each do |users|
+      puts "processing" 
+      pool.friends_birthdays(users)
     end
-    csv_data = futures.map(&:value).inject(:|)
-    puts csv_data.size
 
-    doc = FacebookDataService.format_to_csv(csv_data)
-    local_filename = "#{Rails.root}/teste.csv"
-    File.open(local_filename, 'w') {|f| f.write(doc) }
-    puts "#{DateTime.now.to_f - t0} seconds"
+    # puts "creating csv file"
+    # csv_data = futures.map(&:value).inject(:|)
+    # puts csv_data.size
 
-    # t0 = DateTime.now.to_f    
-    # p "executing non-multithreaded tasks"
-    # fb = FacebookDataService.new
-    # futures = FacebookDataService.facebook_users.first(100).map {|user| fb.friends_birthdays(user) }
-    # csv_data = futures.inject(:|)
-    # p csv_data.size    
-    # p "#{DateTime.now.to_f - t0} seconds"
+    # doc = FacebookDataService.format_to_csv(csv_data)
+    # local_filename = "#{Rails.root}/teste.csv"
+    # File.open(local_filename, 'w') {|f| f.write(doc) }
+    # puts "#{DateTime.now.to_f - t0} seconds"
+
   end
 end
