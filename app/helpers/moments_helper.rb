5b464137f2096cdc88a9887d7cceece3980a06b0
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-11 16:40:43 -0300

    Remove params and use controller variable

--- app/helpers/moments_helper.rb
+++ app/helpers/moments_helper.rb
@@ -1,7 +1,8 @@
 # -*- encoding : utf-8 -*-
 module MomentsHelper
 
-  def msn_tags(id)
-    image_tag "http://view.atdmt.com/action/mmn_olook_colecoes#{id}", size: "1x1"
+  def msn_tags
+    image_tag "http://view.atdmt.com/action/mmn_olook_colecoes#{@moment.id}", size: "1x1"
   end
+
 end
