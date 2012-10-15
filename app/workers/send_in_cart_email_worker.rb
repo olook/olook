12dfb17fbc0f49bf15028bfdaba0aca07ef4898f
Luis Daher <luisotaviodaher@gmail.com>
2012-10-15 14:44:43 -0300

    related products/products

--- app/workers/send_in_cart_email_worker.rb
+++ app/workers/send_in_cart_email_worker.rb
@@ -5,8 +5,10 @@ class SendInCartEmailWorker
   def self.perform
     conditions = UserNotifier.get_carts( 1, 1, [ "notified = 0" ] )
     file_lines = UserNotifier.send_in_cart( conditions.join(" AND ") )
+
     File.open("#{Rails.root}/tmp/abandono.csv", 'w') do |file|
       file_lines.each{|fl| file.puts fl}  
     end  
+
   end
 end
