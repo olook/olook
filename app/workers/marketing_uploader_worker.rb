18da6fc62e144095b7ab91b83c9050e6e6872b85
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-15 13:49:42 -0300

    refactoring methods

--- app/workers/marketing_uploader_worker.rb
+++ app/workers/marketing_uploader_worker.rb
@@ -2,8 +2,8 @@
 class MarketingUploaderWorker
   @queue = :marketing_uploader
 
-  def self.perform(method_called, file, info_ftp)
-    MarketingReports::Builder.new(method_called).save_file(file, info_ftp)
+  def self.perform(method_called, filename, info_ftp)
+    MarketingReports::Builder.new(method_called).save_file(filename, info_ftp)
   end
 end
 
