5958921ef32729f9c0069acde09f2af21c1264d2
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-16 12:41:24 -0300

    setting 'info_ftp' default value as nil

--- app/workers/marketing_uploader_worker.rb
+++ app/workers/marketing_uploader_worker.rb
@@ -2,7 +2,7 @@
 class MarketingUploaderWorker
   @queue = :marketing_uploader
 
-  def self.perform(method_called, filename, info_ftp)
+  def self.perform(method_called, filename, info_ftp = nil)
     MarketingReports::Builder.new(method_called).save_file(filename, info_ftp)
   end
 end
