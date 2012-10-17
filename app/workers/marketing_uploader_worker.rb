4d5848baac3876117673508b9e2fb6ed4398279f
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-17 13:43:21 -0300

    convert all methods called to symbol

--- app/workers/marketing_uploader_worker.rb
+++ app/workers/marketing_uploader_worker.rb
@@ -3,7 +3,8 @@ class MarketingUploaderWorker
   @queue = :marketing_uploader
 
   def self.perform(method_called, filename, info_ftp = nil)
-    MarketingReports::Builder.new(method_called).save_file(filename, info_ftp)
+    type = method_called.to_sym
+    MarketingReports::Builder.new(type).save_file(filename, info_ftp)
   end
 end
 
