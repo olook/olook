6244ca09971d118a6faf37424e7a622c5f0f7c7d
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-19 13:43:16 -0300

    refactoring showroom button

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
       }, 'fast');
       $(this).addClass("open");
       $(word).text("Fechar");
-
          _gaq.push(['_trackEvent', 'product_show','open_showroom' ]);
+
     }
   });
 
