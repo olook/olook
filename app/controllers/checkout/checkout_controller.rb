5c2192e50acbcdb4224c65081ac0d8dd9f4f70ba
Tiago Almeida <tiago.almeida@olook.com.br>
2012-10-19 18:02:36 -0300

    Send MoipSenderStrategy class to payment_buider

--- app/controllers/checkout/checkout_controller.rb
+++ app/controllers/checkout/checkout_controller.rb
@@ -51,7 +51,8 @@ class Checkout::CheckoutController < Checkout::BaseController
     @payment.user_identification = @user.cpf
 
     if @payment.valid?
-      payment_builder = PaymentBuilder.new(@cart_service, @payment)
+      moip_sender_strategy = MoipSenderStrategy.new(@cart_service, @payment)
+      payment_builder = PaymentBuilder.new(@cart_service, @payment, moip_sender_strategy)
       response = payment_builder.process!
 
       if response.status == Payment::SUCCESSFUL_STATUS
@@ -74,7 +75,8 @@ class Checkout::CheckoutController < Checkout::BaseController
     @payment.user_identification = @user.cpf
 
     if @payment.valid?
-      payment_builder = PaymentBuilder.new(@cart_service, @payment)
+      moip_sender_strategy = MoipSenderStrategy.new(@cart_service, @payment)
+      payment_builder = PaymentBuilder.new(@cart_service, @payment, moip_sender_strategy)
       response = payment_builder.process!
 
       if response.status == Payment::SUCCESSFUL_STATUS
@@ -99,8 +101,10 @@ class Checkout::CheckoutController < Checkout::BaseController
     @payment.user_identification = @user.cpf
 
     if @payment.valid?
-      payment_builder = PaymentBuilder.new(@cart_service, @payment)
-      payment_builder.credit_card_number = params[:credit_card][:credit_card_number]
+      moip_sender_strategy = MoipSenderStrategy.new(@cart_service, @payment)
+      moip_sender_strategy.credit_card_number =  params[:credit_card][:credit_card_number]
+      payment_builder = PaymentBuilder.new(@cart_service, @payment, moip_sender_strategy)
+      #payment_builder.credit_card_number = params[:credit_card][:credit_card_number]
       response = payment_builder.process!
 
       if response.status == Payment::SUCCESSFUL_STATUS
