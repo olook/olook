d11b73d09acf7834c368e93350ad4dc9b9a576e3
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-22 14:43:48 -0200

    renaming payment controller class to moip

--- app/controllers/checkout/payments_controller.rb
+++ app/controllers/checkout/payments_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'iconv'
-class Checkout::PaymentsController < ActionController::Base
+class Checkout::MoipPaymentsController < ActionController::Base
   protect_from_forgery :except => :create
 
   def create
