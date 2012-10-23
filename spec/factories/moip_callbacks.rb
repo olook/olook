437953f9b16029ac0413a2b483919b1800e02415
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-10-23 11:19:57 -0200

    removing attrs

--- spec/factories/moip_callbacks.rb
+++ spec/factories/moip_callbacks.rb
@@ -10,12 +10,6 @@ FactoryGirl.define do
 
   factory :moip_callback, :parent => :clean_moip_callback do
     association :payment, :factory => :billet
-
-    status_pagamento "3"
-    cod_moip "3"
-    tipo_pagamento "Boleto"
-    classificacao "Tudo Certo"
-
     after_create do |moip_callback|
       moip_callback.update_attribute(:id_transacao, moip_callback.payment.identification_code)
     end
