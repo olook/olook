6d7a636ad19a401ec406c18a4b663ab343194e1b
darkseid <rafael.manoel@gmail.com>
2012-10-30 12:16:50 -0200

    add a avc banner for those who are not eligible for 20% bonus

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -21,6 +21,9 @@ module PromotionsHelper
       render(:partial => "promotions/banners/#{promotion.strategy}", :locals => {:promotion => promotion})
     elsif !current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST) && Promotion.purchases_amount
       render(:partial => "promotions/banners/#{Promotion.purchases_amount.strategy}", :locals => {:promotion => Promotion.purchases_amount})
+    else
+      # render AVC banner
+      render(:partial => "campaigns/avc_banner")
     end
   end
 
