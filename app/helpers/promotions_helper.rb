b1ff942f52496111c68f92fba87e2de6cffdf453
darkseid <rafael.manoel@gmail.com>
2012-10-30 20:12:17 -0200

    show the avc_banner only for whitelisted pages

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -21,7 +21,7 @@ module PromotionsHelper
       render(:partial => "promotions/banners/#{promotion.strategy}", :locals => {:promotion => promotion})
     elsif !current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST) && Promotion.purchases_amount
       render(:partial => "promotions/banners/#{Promotion.purchases_amount.strategy}", :locals => {:promotion => Promotion.purchases_amount})
-    else
+    elsif page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)
       # render AVC banner
       render(:partial => "campaigns/avc_banner")
     end
