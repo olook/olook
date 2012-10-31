a7d4bd2c601988bc7b50dd855625c4c9f7fecc4f
Matheus Bodo <mcbodo@gmail.com>
2012-10-31 15:40:27 -0200

    creating dummy braspag sender strategy

---
+++ app/models/ecommerce/strategies/braspag_sender_strategy.rb
@@ -0,0 +1,14 @@
+class BraspagSenderStrategy
+
+  attr_accessor :cart_service, :payment, :credit_card_number, :response
+
+  def initialize(cart_service, payment)
+    @cart_service, @payment = cart_service, payment
+  end
+
+  def send_to_gateway
+    ##TODO call braspag gem and set response
+    payment
+  end
+  
+end
