6250605228ca5b101459a30c8cfdc56b505a6dd0
Luis Daher <luisotaviodaher@gmail.com>
2012-10-31 20:26:38 -0200

    payment tests

--- app/services/payment_service.rb
+++ app/services/payment_service.rb
@@ -1,11 +1,17 @@
 class PaymentService
 
   def self.create_sender_strategy(cart_service, payment)
-    if Setting.braspag_whitelisted_only && cart_service.cart.user.email.match(/(olook\.com\.br$)/)
-      Payments::BraspagSenderStrategy.new(cart_service, payment)
-    else  
+
+    if ((Random.rand(100)+1) < Setting.braspag_percentage)
+      if Setting.braspag_whitelisted_only && !cart_service.cart.user.email.match(/(olook\.com\.br$)/)
+        Payments::MoipSenderStrategy.new(cart_service, payment)
+      else
+        Payments::BraspagSenderStrategy.new(cart_service, payment)        
+      end  
+    else
       Payments::MoipSenderStrategy.new(cart_service, payment)
     end
+
   end
 
 end
