493238fa9f967fdd6b5aee4de1a2722f4af0dfaa
darkseid <rafael.manoel@gmail.com>
2012-11-01 15:18:40 -0200

    remove binding.pry

--- app/controllers/checkout/addresses_controller.rb
+++ app/controllers/checkout/addresses_controller.rb
@@ -6,7 +6,6 @@ class Checkout::AddressesController < Checkout::BaseController
   before_filter :erase_freight
 
   def index
-    binding.pry
     redirect_to new_cart_checkout_address_path unless @user.addresses.any?
 
     @addresses = @user.addresses
@@ -51,9 +50,6 @@ class Checkout::AddressesController < Checkout::BaseController
   end
 
   def assign_address
-    puts "TESTEEEEEEEEEEEEEEE #{params}"
-    binding.pry
-
     @address = @user.addresses.find_by_id(params[:address_id])
     if @address
       set_telephone_user(@address.telephone)
