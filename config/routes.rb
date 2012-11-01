e8e2e780756c6602bbb9291a50d42b7e368b2f5c
darkseid <rafael.manoel@gmail.com>
2012-11-01 00:18:16 -0200

    remove didi lightboxe and add 1anomuito campaign

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Olook::Application.routes.draw do
   get "home/index"
   get "index/index"
 
+  match "/1anomuito" => redirect("/tendencias/time-olook")
+
   match '/404', :to => "application#render_public_exception"
   match '/500', :to => "application#render_public_exception"
   match "/home", :to => "home#index"
