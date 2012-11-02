fc25a2f299afd79d0a3cf229fc374faef724237e
Luis Daher <luisotaviodaher@gmail.com>
2012-11-02 12:58:55 -0200

    column_names method has been overridden in order for the sti to work properly

--- app/models/ecommerce/capture_response.rb
+++ app/models/ecommerce/capture_response.rb
@@ -1,2 +1,5 @@
 class CaptureResponse < BraspagResponse
+  def self.column_names
+    super | ["braspag_transaction_id", "acquirer_transaction_id", "amount", "authorization_code", "return_code", "return_message", "transaction_status"] 
+  end
 end
