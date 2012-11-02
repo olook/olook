822c375c63dbdf256e382a810e2bca649f8626f0
Matheus Augusto Bodo <mcbodo@gmail.com>
2012-11-02 11:47:18 -0200

    removing braspag callback model

--- spec/factories/braspag_callbacks.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :braspag_callback do
-    order_id "MyString"
-    status "MyString"
-    payment 1
-  end
-end
