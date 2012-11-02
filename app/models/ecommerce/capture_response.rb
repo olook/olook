6f7fa01a6f3121d28967212e4e8ce2075bcb77df
Luis Daher <luisotaviodaher@gmail.com>
2012-11-02 12:34:02 -0200

    sti attributes removed

--- app/models/ecommerce/capture_response.rb
+++ app/models/ecommerce/capture_response.rb
@@ -1,3 +1,2 @@
 class CaptureResponse < BraspagResponse
-  attr_accessor :braspag_transaction_id, :acquirer_transaction_id, :amount, :authorization_code, :return_code, :return_message, :transaction_status
 end
