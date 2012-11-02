f17af44191dade2ed37244d6a8cefa6d1763e672
Luis Daher <luisotaviodaher@gmail.com>
2012-11-02 12:33:02 -0200

    sti implemented

--- app/models/ecommerce/braspag_response.rb
+++ app/models/ecommerce/braspag_response.rb
@@ -1,4 +1,5 @@
 class BraspagResponse < ActiveRecord::Base
   scope :authorize_response, where("type = ?", "authorize_response")
   scope :capture_response, where("type = ?", "capture_response")
+  attr_accessor :correlation_id, :success, :error_code, :error_message, :processed
 end
