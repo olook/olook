d1752b81575cc315f6d2d4f4bcc5aa7bb5151183
Matheus Bodo <mcbodo@gmail.com>
2012-11-05 15:15:11 -0200

    changes columns for braspag response models

--- app/models/ecommerce/authorize_response.rb
+++ app/models/ecommerce/authorize_response.rb
@@ -1,5 +1,6 @@
 class AuthorizeResponse < BraspagResponse
   def self.column_names
-    super | ["order_id", "braspag_order_id", "braspag_transaction_id", "amount", "payment_method"]
+    super | ["order_id", "braspag_order_id", "braspag_transaction_id", "amount", "payment_method", "acquirer_transaction_id", 
+             "authorization_code", "return_code", "return_message", "transaction_status", "credit_card_token"]
   end
 end
