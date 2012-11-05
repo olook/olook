3af2feb9b7ffe4657f25d1d876fe5479c24bca0d
Matheus Bodo <mcbodo@gmail.com>
2012-11-05 15:15:11 -0200

    changes columns for braspag response models

--- app/models/ecommerce/braspag_response.rb
+++ app/models/ecommerce/braspag_response.rb
@@ -1,5 +1,5 @@
 class BraspagResponse < ActiveRecord::Base
   def self.column_names
-    ["correlation_id", "success", "error_code", "error_message", "processed", "created_at", "updated_at"]
+    ["correlation_id", "success", "error_message", "processed", "created_at", "updated_at"]
   end
 end
