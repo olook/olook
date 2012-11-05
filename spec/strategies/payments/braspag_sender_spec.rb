0af88cf1c412c5526a5f834daa3eefcb8a837172
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-05 10:24:55 -0200

    adding factories to test

--- spec/strategies/payments/braspag_sender_spec.rb
+++ spec/strategies/payments/braspag_sender_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe Payments::BraspagSenderStrategy do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:payment) {FactoryGirl.create(:payment)}
+  let(:shipping_service) { FactoryGirl.create :shipping_service }
+  let(:order) { FactoryGirl.create(:order, :user => user) }
+  let(:credit_card) { FactoryGirl.create(:credit_card, :order => order) }
+  let(:credit_card_with_response) { FactoryGirl.create(:credit_card_with_response) }
+  let(:billet) { FactoryGirl.create(:billet, :order => order) }
+  let(:debit) { FactoryGirl.create(:debit, :order => order) }
+  let(:address) { FactoryGirl.create(:address, :user => user) }
+  let(:freight) { { :price => 12.34, :cost => 2.34, :delivery_time => 2, :shipping_service_id => shipping_service.id, :address_id => address.id} }
+  let(:cart) { FactoryGirl.create(:cart_with_items, :user => user) }
+  let(:cart_service) { CartService.new({
+    :cart => cart,
+    :freight => freight,
+  }) }
+  let(:order_total) { 12.34 }
 
 end
