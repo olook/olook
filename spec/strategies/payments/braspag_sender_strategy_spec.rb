fbc0ad66d05132762fe9391f3c447b6299ff22f6
Matheus Bodo <mcbodo@gmail.com>
2012-11-06 09:52:36 -0200

    adding tests to braspag sender strategy

--- spec/strategies/payments/braspag_sender_strategy_spec.rb
+++ spec/strategies/payments/braspag_sender_strategy_spec.rb
@@ -44,7 +44,7 @@ describe Payments::BraspagSenderStrategy do
   end
 
   context "processing response" do
-    subject {Payments::BraspagSenderStrategy.new(cart_service, payment)}
+    subject {Payments::BraspagSenderStrategy.new(cart_service, credit_card)}
 
     it "should validate if the result is success" do
       subject.success_result?({:success => "true"}).should eq(true)
@@ -66,6 +66,44 @@ describe Payments::BraspagSenderStrategy do
       authorize_transaction_result = {:correlation_id => "1234567890", :error_report_data_collection => ["error_report_1", "error_report_2"]}
       expect {
         subject.create_failure_authorize_response(authorize_transaction_result)
+      }.to change(AuthorizeResponse, :count).by(1)
+    end
+
+    it "should create a correct success response" do
+      authorize_transaction_result =  {
+        :correlation_id => "1234567890",
+        :order_data => {:order_id => "123", :braspag_order_id => "456"},
+        :payment_data_collection => { :payment_data_response => {
+        :braspag_transaction_id => "555888", :amount => "500", :payment_method =>
+        "997", :acquirer_transaction_id => "999888", :authorization_code => "987",
+        :return_code => "1", :return_message => "Success", :status => "0",
+        :credit_card_token => "444555666"} }}   
+      authorization_response = subject.create_success_authorize_response(authorize_transaction_result)
+      authorization_response.correlation_id.should eq("1234567890")
+      authorization_response.order_id.should eq("123")
+      authorization_response.braspag_order_id.should eq("456")
+      authorization_response.braspag_transaction_id.should eq("555888")
+      authorization_response.amount.should eq("500")
+      authorization_response.payment_method.should eq(997)
+      authorization_response.acquirer_transaction_id.should eq("999888")
+      authorization_response.authorization_code.should eq("987")
+      authorization_response.return_code.should eq("1")
+      authorization_response.return_message.should eq("Success")
+      authorization_response.transaction_status.should eq(0)
+      authorization_response.credit_card_token.should eq("444555666") 
+    end
+
+    it "should create a failure AuthorizeResponse on database" do
+      authorize_transaction_result =  {
+        :correlation_id => "1234567890",
+        :order_data => {:order_id => "123", :braspag_order_id => "456"},
+        :payment_data_collection => { :payment_data_response => {
+        :braspag_transaction_id => "555888", :amount => "500", :payment_method =>
+        "997", :acquirer_transaction_id => "999888", :authorization_code => "987",
+        :return_code => "1", :return_message => "Success", :status => "0",
+        :credit_card_token => "444555666"} }}   
+      expect {
+        subject.create_success_authorize_response(authorize_transaction_result)
       }.to change(AuthorizeResponse, :count).by(1)
     end
 
