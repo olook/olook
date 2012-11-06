49564e3c46cbaf8d5bfc60d56278dfd812034c43
Matheus Bodo <mcbodo@gmail.com>
2012-11-06 09:22:25 -0200

    creating worker to process braspag callbacks

--- app/models/ecommerce/capture_response.rb
+++ app/models/ecommerce/capture_response.rb
@@ -1,4 +1,6 @@
 class CaptureResponse < BraspagResponse
+  scope :to_process, where(:processed => false).order(:id)
+  
   def self.column_names
     super | ["braspag_transaction_id", "acquirer_transaction_id", "amount", "authorization_code", "return_code", "return_message", "transaction_status"] 
   end
