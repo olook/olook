4a0b08fa8be189ccc3b8516e85bd7bf8a2e2d3a1
Matheus Bodo <mcbodo@gmail.com>
2012-11-06 11:10:16 -0200

    creating worker to process braspag responses

--- app/models/ecommerce/capture_response.rb
+++ app/models/ecommerce/capture_response.rb
@@ -4,4 +4,9 @@ class CaptureResponse < BraspagResponse
   def self.column_names
     super | ["braspag_transaction_id", "acquirer_transaction_id", "amount", "authorization_code", "return_code", "return_message", "transaction_status"] 
   end
+
+  def update_payment_status(payment)
+    
+  end
+  
 end
