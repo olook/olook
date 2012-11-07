a8c6b5a434290fc4b94a4b4ea63013aea64f43e7
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-07 13:21:50 -0200

    checking if table exists

--- db/migrate/20121105164439_add_credit_card_token_to_braspag_response.rb
+++ db/migrate/20121105164439_add_credit_card_token_to_braspag_response.rb
@@ -1,6 +1,5 @@
 class AddCreditCardTokenToBraspagResponse < ActiveRecord::Migration
-  def change
+  def self.table_exists?(braspag_responses)
     add_column :braspag_responses, :credit_card_token, :string
-
   end
 end
