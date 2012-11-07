4538fa9d303e632cef773eb9f04cfa3b93b1bd82
Matheus Bodo <mcbodo@gmail.com>
2012-11-07 18:43:20 -0200

    implementing the methods to process braspag responses

--- app/models/ecommerce/braspag_capture_response.rb
+++ app/models/ecommerce/braspag_capture_response.rb
@@ -1,7 +1,25 @@
 class BraspagCaptureResponse < ActiveRecord::Base
   scope :to_process, where(:processed => false).order(:id)
 
+  STATUS = {
+    0 => :authorize,
+    2 => :cancel
+  }
+
   def update_payment_status(payment)
-    
+    event = STATUS[status] || :cancel
+    binding.pry
+    if payment.set_state(event)
+      self.update_attribute(:processed, true)
+      if payment.order && payment.order.reload.canceled?
+        Resque.enqueue(Abacos::CancelOrder, payment.order.number)
+      end
+    else
+      self.update_attributes(
+        :retry => (self.retry + 1),
+        :error_message => payment.errors.full_messages.to_s
+      )
+    end
   end
+
 end
