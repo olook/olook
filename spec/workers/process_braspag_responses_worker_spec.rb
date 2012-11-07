46a1825b5e86c981bc188c5a520c5aef7c1ac19b
Matheus Bodo <mcbodo@gmail.com>
2012-11-07 10:56:18 -0200

    updating obraspag version

--- spec/workers/process_braspag_responses_worker_spec.rb
+++ spec/workers/process_braspag_responses_worker_spec.rb
@@ -4,8 +4,8 @@ require 'spec_helper'
 describe ProcessBraspagResponsesWorker do
 
   context "when payment does not exist" do
-    let!(:authorize_response) { FactoryGirl.create(:authorize_response, :order_id => "invalid") }
-    let!(:capture_response) { FactoryGirl.create(:capture_response, :order_id => "invalid") }
+    let!(:authorize_response) { FactoryGirl.create(:braspag_authorize_response, :order_id => "invalid") }
+    let!(:capture_response) { FactoryGirl.create(:braspag_capture_response, :order_id => "invalid") }
 
     it "should update authorize_response to processed with error" do
       ProcessBraspagResponsesWorker.perform
@@ -22,8 +22,8 @@ describe ProcessBraspagResponsesWorker do
 
   context "when payment exists" do
     let!(:payment) { FactoryGirl.create(:credit_card) }
-    let!(:authorize_response) { FactoryGirl.create(:authorize_response, :order_id => payment.identification_code) }
-    let!(:capture_response) { FactoryGirl.create(:capture_response, :order_id => payment.identification_code ) }
+    let!(:authorize_response) { FactoryGirl.create(:braspag_authorize_response, :order_id => payment.identification_code) }
+    let!(:capture_response) { FactoryGirl.create(:braspag_capture_response, :order_id => payment.identification_code ) }
 
     it "should call update_payment_status on authorize_response" do
       BraspagAuthorizeResponse.any_instance.should_receive(:update_payment_status).with(payment)
