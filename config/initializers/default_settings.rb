48baa2389c68340d39b577ee1298c2838b89f7df
darkseid <rafael.manoel@gmail.com>
2012-11-07 20:53:24 -0200

    use different set of product for each fashion profile

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -21,3 +21,18 @@ Setting.defaults[:checkout_suggested_product_id]= "12472"
 Setting.defaults[:in_cart_mail_range] = "1"
 Setting.defaults[:in_cart_mail_how_long] = "1"
 Setting.defaults[:in_cart_mail_condition] = "notified=0"
+
+# Gifts page
+# Setting.defaults[:profile_modern_ids] = "14"
+Setting.defaults[:profile_modern_product_ids] = "4999,5759,3515,4352,7759"
+# Setting.defaults[:profile_casual_ids] = "14"
+Setting.defaults[:profile_casual_product_ids] = "9950,7434,7425,7001,6023"
+# Setting.defaults[:profile_chic_ids] = "14"
+Setting.defaults[:profile_chic_product_ids] = "6727,4963,5008,9896,9081"
+# Setting.defaults[:profile_sexy_ids] = "14"
+Setting.defaults[:profile_sexy_product_ids] = "5044,5017,10022,4350,9406"
+
+
+
+
+
