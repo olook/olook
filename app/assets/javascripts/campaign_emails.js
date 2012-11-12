8148a1436fad253c52bb18a7823889cefb8e710d
Rafael Carvalho <rafa1402@gmail.com>
2012-11-12 19:42:02 -0200

    a/b test

--- app/assets/javascripts/campaign_emails.js
+++ app/assets/javascripts/campaign_emails.js
@@ -1,2 +1,23 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+function checkEmail() {
+	var email = $('#campaign_email_email');
+	var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+	if (!filter.test(email.value) || email.value == "seunome@email.com.br" ) {
+		$('.error').fadeIn();
+		email.focus;
+		return false;
+	}
+}
+
+$(function(){
+	$('#campaign_email_email').focus(function(){
+		if($(this).val() == 'seunome@email.com.br')
+			$(this).val('');
+	}).focusout(function(){
+		if($(this).val() == '')
+			$(this).val('seunome@email.com.br')
+	});
+	
+	$('.bt-enviar').click(function(){
+	//	checkEmail();	
+	})
+});
