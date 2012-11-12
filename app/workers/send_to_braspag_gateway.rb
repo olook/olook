809b3e665b44554c2f79c00f2019f18ed6bf3839
Luis Daher <luisotaviodaher@gmail.com>
2012-11-12 11:17:50 -0200

    extracting payment processing to resque

--- app/workers/send_to_braspag_gateway.rb
+++ app/workers/send_to_braspag_gateway.rb
@@ -4,9 +4,19 @@ module Braspag
     @queue = :order_status
 
     def self.perform(payment_id)
+
       payment = Payment.find(payment_id)
+
+      cart_service = CartService.new(
+        :cart => payment.cart,
+        :gift_wrap => session[:gift_wrap],
+        :coupon => coupon,
+        :promotion => @promotion,
+        :freight => session[:cart_freight],
+        :credits => session[:cart_use_credits]
+      )
       
-      strategy = Payments::BraspagSenderStrategy.new(nil, payment)
+      strategy = Payments::BraspagSenderStrategy.new(cart_service, payment)
 
       strategy.process_enqueued_request
     end
