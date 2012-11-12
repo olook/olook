febf17179e9e0941a4d5902617fa08d2da9a357e
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-12 11:54:23 -0200

    processing responses by views

--- app/controllers/admin/braspag_capture_responses_controller.rb
+++ app/controllers/admin/braspag_capture_responses_controller.rb
@@ -1,8 +1,6 @@
 class Admin::BraspagCaptureResponsesController < Admin::BaseController
   respond_to :html
 
-  load_and_authorize_resource
-
   def index
   	@search = BraspagCaptureResponse.search(params[:search])
   	@responses = @search.relation.page(params[:page]).per_page(100).order('processed asc, id asc')
@@ -13,5 +11,36 @@ class Admin::BraspagCaptureResponsesController < Admin::BaseController
   	@payment = Payment.find_by_identification_code(@response.identification_code)
   	respond_with :admin, @response, @payment
   end
+
+  def change_to_processed
+    @response = BraspagCaptureResponse.find(params[:id])
+    if (not @response.processed?)
+      @response.processed = true
+      if @response.save
+        flash[:notice] = 'A capture response foi marcada como processada com sucesso.'
+      else
+        flash[:error] = 'Nao foi possivel atualizar o capture response como processada.'
+      end
+    else
+      flash[:error] = 'A capture response ja foi processada.'
+    end
+    respond_with :admin, @response
+  end
+
+  def change_to_not_processed
+    @response = BraspagCaptureResponse.find(params[:id])
+    if (@response.processed?)
+      @response.processed = false
+      if @response.save
+        flash[:notice] = 'A capture response foi marcada como nao processada, e sera reprocessada em breve.'
+      else
+        flash[:error] = 'Nao foi possivel atualizar o capture response como nao processada.'
+      end
+    else
+      flash[:error] = 'A capture response ainda nao foi processada.'
+    end
+    respond_with :admin, @response
+  end
+
 
 end
