6920adf31756f70540c04f106cadef5247d7e4ad
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-13 19:18:28 -0200

    testing validations

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe Campaign do
   context "attributes validation" do
+    it { should validate_presence_of :title }
     it { should validate_presence_of :start_at }
     it { should validate_presence_of :end_at }
-    it { should validate_presence_of :lightbox }
-    it { should validate_presence_of :banner }
   end
 end
