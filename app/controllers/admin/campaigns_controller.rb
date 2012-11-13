a97d01c2a71313bea8b9c7532e42c8cd31ecfeb6
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-13 16:44:27 -0200

    doing index action

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -1,11 +1,17 @@
-class Admin::CampaignsController < ApplicationController
-  def index
-  end
+class Admin::CampaignsController < Admin::BaseController
+  respond_to :html
 
-  def new
+  def index
+    @search = Campaign.search(params[:search])
+    @campaigns = @search.relation.page(params[:page]).per_page(100)
   end
 
   def show
+  end
+
+  def new
+    @campaign = Campaign.new
+    respond_with :campaign, @campaign
   end
 
   def edit
