7c461baeeb107719b5864395f377ac88e1665c89
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-14 19:25:10 -0200

    making validations to campaigns

--- spec/factories/campaigns.rb
+++ spec/factories/campaigns.rb
@@ -2,18 +2,17 @@
 
 FactoryGirl.define do
   factory :campaign do
+    id "123"
     title "First Campaign"
-    start_at "2012-11-13"
-    end_at "2012-11-13"
-    lightbox "path_to_image"
+    start_at "2012-11-10"
+    end_at "2012-11-15"
     banner "path_to_image"
     background "#FFF"
     description "some description"
 
     factory :second_campaign do
+      id "456"
       title "Another Campaign"
-      start_at "2012-12-13"
-      end_at "2012-12-13"
     end
   end
 end
