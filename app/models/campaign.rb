bf50cac8b6ceab68b4a2e75198ef431b5647b6ef
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-14 19:25:10 -0200

    making validations to campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,6 +2,46 @@ class Campaign < ActiveRecord::Base
   validates :title, :presence => true
   validates :start_at, :presence => true
   validates :end_at, :presence => true
-  mount_uploader :lightbox, ImageUploader
+  validate :only_one_activated?
   mount_uploader :banner, ImageUploader
+
+  def is_active?
+    start_at <= Date.today && end_at >= Date.today
+  end
+
+  def only_one_activated?
+    errors.add(:is_active, ": sorry, but only one campaign can be active at the same time. Deactivate an existing one before you proceed") if Campaign.any_campaign_active_today?(self) && Campaign.invalid_range?(self)
+  end
+
+  def self.any_campaign_active_today?(campaign)
+    campaign_exists?(campaign) ? check_period_excluding_campaign(campaign) : check_period_campaign
+  end
+
+  def self.invalid_range?(campaign)
+    campaign_exists?(campaign) ? check_range_excluding_campaign(campaign) : check_period_campaign(campaign)
+  end
+
+  private
+
+  def self.campaign_exists?(campaign)
+    campaign.id
+  end
+
+  def self.check_period_excluding_campaign(campaign)
+    !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}' AND id != #{ campaign.id }").empty?
+  end
+
+  def self.check_period_campaign
+    !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}'").empty?
+  end
+
+  def self.check_range_excluding_campaign(campaign)
+    !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }' AND id != #{ campaign.id }").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }' AND id != #{ campaign.id }").empty?
+  end
+
+  def self.check_range_campaign
+    !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }'").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }'").empty?
+  end
+
 end
+
