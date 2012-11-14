d67240f373543e5f0e84f0232b0d23bf403a3857
Oliver Barnes <oli.azevedo.barnes@gmail.com>
2012-11-14 17:38:16 -0200

    fixing campaign_email mailer's smtp domain

--- spec/mailers/campaign_email_notification_mailer_spec.rb
+++ spec/mailers/campaign_email_notification_mailer_spec.rb
@@ -4,8 +4,15 @@ require 'spec_helper'
 describe CampaignEmailNotificationMailer do
   let(:campaign_email) { FactoryGirl.create(:campaign_email) }
 
-  it "should use sendgrid" do
-  	CampaignEmailNotificationMailer.smtp_settings[:address].should == "smtp.sendgrid.net"
+  context ".smtp_settings" do
+
+    it "should use sendgrid" do
+    	CampaignEmailNotificationMailer.smtp_settings[:address].should == "smtp.sendgrid.net"
+    end
+
+    it "should use the domain my.olookmail.com.br" do
+      CampaignEmailNotificationMailer.smtp_settings[:domain].should == "my.olookmail.com.br"
+    end
   end
 
   describe "#welcome_email" do
