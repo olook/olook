17c9cc92a8a52e86ae244f6a0b267df757d63291
Luis Daher <luisotaviodaher@gmail.com>
2012-11-14 20:28:21 -0200

    Settings for clearsale added

--- config/initializers/clearsale.rb
+++ config/initializers/clearsale.rb
@@ -1,3 +1,3 @@
 CLEARSALE_CONFIG = YAML.load_file("#{Rails.root}/config/clearsale.yml")[Rails.env]
-ENV["CLEARSALE_ENV"] = CLEARSALE_CONFIG[Rails.env]["environment"]
-ENV["CLEARSALE_ENTITYCODE"] = CLEARSALE_CONFIG[Rails.env]["entity_code"]
+ENV["CLEARSALE_ENV"] = CLEARSALE_CONFIG["environment"]
+ENV["CLEARSALE_ENTITYCODE"] = CLEARSALE_CONFIG["entity_code"]
