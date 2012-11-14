c60113b223ff673031faa608a2984058ac4c16a0
Oliver Barnes <oli.azevedo.barnes@gmail.com>
2012-11-14 15:13:14 -0200

    changing campaign email's lightbox iframe's src to /new

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,5 @@
 $(function(){
-	$("#modal-campaign").append('<iframe src="/campaign_emails/login" border="0" height="100%" width="100%"></iframe>');
+	$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" height="100%" width="100%"></iframe>');
 	$("#overlay-campaign, #modal-campaign").delay(150).fadeIn();
 	
 })
