0b989974500b38a1f6c06b8f5a20220bae2e877e
Luis Daher <luisotaviodaher@gmail.com>
2012-11-14 19:37:40 -0200

    clearsale configuration added

--- app/strategies/payments/braspag_sender_strategy.rb
+++ app/strategies/payments/braspag_sender_strategy.rb
@@ -34,13 +34,10 @@ module Payments
       begin
         gateway_response = web_service_data.checkout(authorize_transaction_data)
         process_response(gateway_response[:authorize_response], gateway_response[:capture_response])
-<<<<<<< HEAD
-=======
       rescue Exception => error
         ErrorNotifier.send_notifier("Braspag", error.message, payment)
         OpenStruct.new(:status => Payment::FAILURE_STATUS, :payment => payment)
         raise error
->>>>>>> 5fae44c1c91b3e6e4af662e251a757142108a078
       ensure
         payment.encrypt_credit_card
         payment.save!
