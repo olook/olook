2057df1160e39bd4674f6bb39e1f8e3a8cd4e148
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-11-15 19:43:34 -0200

    fixing enqueueing of campaign_emails to pass the email address instead of id

--- app/models/campaign_email.rb
+++ app/models/campaign_email.rb
@@ -2,7 +2,7 @@ class CampaignEmail < ActiveRecord::Base
 	after_create :enqueue_notification
 
 	def enqueue_notification
-		Resque.enqueue(CampaignEmailNotificationWorker, self.id)
+		Resque.enqueue(CampaignEmailNotificationWorker, self.email)
 	end
 
 end
