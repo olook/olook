e88cab82b258bc6e0a34344a00a0db9320987963
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-11-15 20:19:45 -0200

    changing the campaign_email's subject line

--- app/mailers/campaign_email_notification_mailer.rb
+++ app/mailers/campaign_email_notification_mailer.rb
@@ -1,8 +1,9 @@
+# -*- encoding : utf-8 -*-
 class CampaignEmailNotificationMailer < ActionMailer::Base
 	default :from => "olook <bemvinda@olook1.com.br>"
 
 	def welcome_email(email)
-		mail(:to => email, :subject => "Recebemos seu email com sucesso")
+		mail(:to => email, :subject => "Olá, use agora mesmo seus 20% de desconto!")
     
     headers["X-SMTPAPI"] = { 'category' => 'welcome_email' }.to_json
 	end
