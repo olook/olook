0a7ffa3a6e49c37764f513e9bc933cec1c7cce36
Raul Souza Lima <raulsouzalima@gmail.com>
2012-11-15 18:10:39 -0200

    Added shoe size filter

--- app/assets/javascripts/gift/suggestions.js
+++ app/assets/javascripts/gift/suggestions.js
@@ -1,4 +1,12 @@
 $(function() {
+  $("div#shoe_size_filter span").live("click", function(e) {
+    $(this).siblings(".sizes").toggle();
+    e.stopPropagation();
+  });
+
+  $("html").live("click", function() {
+    $("div#shoe_size_filter div.sizes").hide();
+  });
 });
 
 initSuggestion = {
