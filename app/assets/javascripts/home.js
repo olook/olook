fe76a7f6064b94f46ffa7151e79ef1a394358d86
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-11-15 19:50:02 -0200

    trying out Visual Web Optimizer with their js api

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,5 +1,4 @@
-$(function(){
-	$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
-	$("#overlay-campaign").delay(200).fadeIn();
-	
-})
+vwo_$(document).ready(function(){
+	vwo_$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
+	vwo_$("#overlay-campaign").delay(200).fadeIn();
+});
