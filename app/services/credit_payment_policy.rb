1edc308c9d2be24fea5ef057ac40dbc73b22c783
darkseid <rafael.manoel@gmail.com>
2012-11-19 09:39:27 -0200

    exclude suggested items from fullprice items

--- app/services/credit_payment_policy.rb
+++ app/services/credit_payment_policy.rb
@@ -5,7 +5,20 @@ class CreditPaymentPolicy
   end
 
   def allow?
-    full_price_items = @cart.items.select{ |item| item.price == item.retail_price}
-    ! full_price_items.empty?
+    fullprice_items_without_suggested_product.any?
   end
+
+  private
+    def fullprice_items_without_suggested_product
+      exclude_suggested_products_from fullprice_items
+    end
+
+    def fullprice_items
+      @cart.items.select{ |item| item.price == item.retail_price}
+    end
+
+    def exclude_suggested_products_from cart_items
+      cart_items.reject { |cart_item| cart_item.product.id == Setting.checkout_suggested_product_id.to_i}
+    end
+
 end
