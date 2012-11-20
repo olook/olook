73d0a86bfe67eb123389a389177ecc62582c6aa5
darkseid <rafael.manoel@gmail.com>
2012-11-20 16:33:05 -0200

    fix campaigns feature and remove olook tv from menu bar

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -6,12 +6,16 @@ class Campaign < ActiveRecord::Base
   validate :only_one_activated?
   mount_uploader :banner, ImageUploader
 
+
   def is_active?
     start_at <= Date.today && end_at >= Date.today
   end
 
   def only_one_activated?
-    errors.add(:is_active, ": sorry, but only one campaign can be active at the same time. Deactivate an existing one before you proceed") if Campaign.any_campaign_active_today?(self) && Campaign.invalid_range?(self)
+    conflictant_campaigns = Campaign.all.select { |campaign| campaign.overlaps?(self) && campaign.id != self.id }
+    if conflictant_campaigns.any? 
+      errors.add(:is_active, ": Foi detectado um conflito de datas com a campanha #{conflictant_campaigns[0].description}.")
+    end
   end
 
   def self.any_campaign_active_today?(campaign = nil)
@@ -19,39 +23,38 @@ class Campaign < ActiveRecord::Base
   end
 
   def self.invalid_range?(campaign)
-    campaign_exists?(campaign) ? check_range_excluding_campaign(campaign) : check_range_campaign(campaign)
+    campaign.start_at > campaign.end_at
   end
 
   def self.activated_campaign(date=Date.today)
     where("start_at <= '#{date}' AND end_at >= '#{date}'").first if any_campaign_active_today?
   end
 
+  def overlaps? another_campaign
+    start_at.between?(another_campaign.start_at, another_campaign.end_at) || end_at.between?(another_campaign.start_at, another_campaign.end_at)
+  end  
+
   private
 
-  def self.campaign_exists?(campaign)
-    campaign.try(:id)
-  end
+    def self.campaign_exists?(campaign)
+      campaign.try(:id)
+    end
 
-  def self.check_period_excluding_campaign(campaign)
-    !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}' AND id != #{ campaign.id }").empty?
-  end
+    def self.check_period_excluding_campaign(campaign)
+      !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}' AND id != #{ campaign.id }").empty?
+    end
 
-  def self.check_period_campaign
-    !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}'").empty?
-  end
+    def self.check_period_campaign
+      !where("start_at <= '#{Date.today}' AND end_at >= '#{Date.today}'").empty?
+    end
 
-  def self.check_range_excluding_campaign(campaign)
-    !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }' AND id != #{ campaign.id }").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }' AND id != #{ campaign.id }").empty?
-  end
+    def self.check_range_excluding_campaign(campaign)
+      !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }' AND id != #{ campaign.id }").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }' AND id != #{ campaign.id }").empty?
+    end
 
-  def self.check_range_campaign(campaign)
-    !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }'").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }'").empty?
-  end
-
-  def self.validate
-    campaigns = self.find :all, :order_by => :start_at
-
-  end
+    def self.check_range_campaign(campaign)
+      !where("start_at <= '#{ campaign.start_at }' AND end_at >= '#{ campaign.start_at }'").empty? || !where("start_at <= '#{ campaign.end_at }' AND end_at >= '#{ campaign.end_at }'").empty?
+    end
 
 end
 
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -10,8 +10,44 @@ describe Campaign do
     end
   end
 
+  describe "#ovelaps?" do
+    let(:campaign) {Campaign.new ({:start_at => "2012-01-01", :end_at => "2012-01-15"})}
+
+    context "same date range" do
+      it "should ovelap" do
+        campaign.overlaps?(campaign).should be_true
+      end
+    end
+
+    context "complete apart date ranges" do
+
+      let(:non_overlapping_campaign) {Campaign.new ({:start_at => "2012-01-16", :end_at => "2012-02-15"})}
+
+      it "should not ovelap" do
+        campaign.overlaps?(non_overlapping_campaign).should be_false
+      end
+    end
+
+    context "edge cases" do 
+
+      let(:ends_when_another_campaign_starts) {Campaign.new ({:start_at => "2011-12-15", :end_at => "2012-01-01"})}
+      let(:starts_when_another_campaign_ends) {Campaign.new ({:start_at => "2012-01-15", :end_at => "2012-01-31"})}
+
+      it "should overlap" do
+        campaign.overlaps?(ends_when_another_campaign_starts).should be_true
+      end
+
+      it "should overlap" do
+        campaign.overlaps?(starts_when_another_campaign_ends).should be_true
+      end
+
+    end
+
+  end
+
   describe "on #create" do
-    let(:valid_campaign) { FactoryGirl.create(:campaign) }
+    # valid_campaign must exist in the database before each spec
+    let!(:valid_campaign) { FactoryGirl.create(:campaign) }
     let(:invalid_campaign) { FactoryGirl.build(:second_campaign) }
 
     # before do
@@ -23,7 +59,7 @@ describe Campaign do
     context "should not create a campaign" do
       it "the date is in range of a valid campaign already created" do
         invalid_campaign.start_at = "2012-11-13"
-        invalid_campaign.end_at = "2012-11-13"
+        invalid_campaign.end_at = "2012-11-15"
         invalid_campaign.should be_invalid
       end
 
@@ -41,8 +77,8 @@ describe Campaign do
     end
     context "should create a campaign" do
       it "valid campaign" do
-        invalid_campaign.start_at = "2012-12-10"
-        invalid_campaign.end_at = "2012-12-10"
+        invalid_campaign.start_at = "2012-12-8"
+        invalid_campaign.end_at = "2012-12-9"
         invalid_campaign.should be_valid
       end
     end
@@ -54,16 +90,16 @@ describe Campaign do
     context "should return true" do
     
       before do
-        valid_campaign.start_at = 1.months.ago
-        valid_campaign.end_at = 10.days.since
+        valid_campaign.start_at = Date.today - 10
+        valid_campaign.end_at = Date.today - 5
         valid_campaign.save
       end
 
       it "for active campaign because already exists a campaign" do
-        new_campaign.start_at = Date.today
-        new_campaign.end_at = 1.days.since
+        new_campaign.start_at = Date.today - 1
+        new_campaign.end_at = Date.today + 10
 
-        Campaign.any_campaign_active_today?(new_campaign).should eq(true)
+        Campaign.any_campaign_active_today?(new_campaign).should be_false
       end
     end
 
@@ -75,22 +111,21 @@ describe Campaign do
   end
 
   describe ".invalid_range?" do
-    let!(:valid_campaign) { FactoryGirl.create(:campaign) }
     let(:new_campaign) { FactoryGirl.build(:second_campaign) }
 
     context "should return true" do
       it "because range of new campaign is not available" do
         new_campaign.start_at = "2012-11-10"
-        new_campaign.end_at = "2012-11-14"
-        Campaign.invalid_range?(new_campaign).should eq(true)
+        new_campaign.end_at = "2012-11-01"
+        Campaign.invalid_range?(new_campaign).should be_true
       end
     end
 
     context "should return false" do
       it "because range of new campaign is available" do
         new_campaign.start_at = "2012-12-10"
-        new_campaign.end_at = "2012-12-14"
-        Campaign.invalid_range?(new_campaign).should eq(false)
+        new_campaign.end_at = "2012-12-15"
+        Campaign.invalid_range?(new_campaign).should be_false
       end
     end
   end
