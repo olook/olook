9bcf0c8daeeb03f87ffa2a736db8aa211cf079e4
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-21 11:02:58 -0200

    removing some methods

--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -28,7 +28,7 @@ describe Campaign do
       end
     end
 
-    context "edge cases" do 
+    context "edge cases" do
 
       let(:ends_when_another_campaign_starts) {Campaign.new ({:start_at => "2011-12-15", :end_at => "2012-01-01"})}
       let(:starts_when_another_campaign_ends) {Campaign.new ({:start_at => "2012-01-15", :end_at => "2012-01-31"})}
@@ -80,52 +80,6 @@ describe Campaign do
         invalid_campaign.start_at = "2012-12-8"
         invalid_campaign.end_at = "2012-12-9"
         invalid_campaign.should be_valid
-      end
-    end
-  end
-
-  describe ".any_campaign_active_today?" do
-    let(:valid_campaign) { FactoryGirl.create(:campaign) }
-    let(:new_campaign) { FactoryGirl.build(:second_campaign) }
-    context "should return true" do
-    
-      before do
-        valid_campaign.start_at = Date.today - 10
-        valid_campaign.end_at = Date.today - 5
-        valid_campaign.save
-      end
-
-      it "for active campaign because already exists a campaign" do
-        new_campaign.start_at = Date.today - 1
-        new_campaign.end_at = Date.today + 10
-
-        Campaign.any_campaign_active_today?(new_campaign).should be_false
-      end
-    end
-
-    context "should return false" do
-      it "for active campaign because the search excludes the current campaign" do
-        Campaign.any_campaign_active_today?(valid_campaign).should eq(false)
-      end
-    end
-  end
-
-  describe ".invalid_range?" do
-    let(:new_campaign) { FactoryGirl.build(:second_campaign) }
-
-    context "should return true" do
-      it "because range of new campaign is not available" do
-        new_campaign.start_at = "2012-11-10"
-        new_campaign.end_at = "2012-11-01"
-        Campaign.invalid_range?(new_campaign).should be_true
-      end
-    end
-
-    context "should return false" do
-      it "because range of new campaign is available" do
-        new_campaign.start_at = "2012-12-10"
-        new_campaign.end_at = "2012-12-15"
-        Campaign.invalid_range?(new_campaign).should be_false
       end
     end
   end
