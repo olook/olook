faa3943a996df99c8d511805ba8d5f860bdfb53c
Tiago Almeida <tiago.almeida@olook.com.br>
2012-11-21 10:52:45 -0200

    Unify infinite scroll

--- app/assets/javascripts/moments.js
+++ app/assets/javascripts/moments.js
@@ -88,16 +88,7 @@ jQuery(function() {
   });
 
   if ($('.pagination').length) {
-    $(window).scroll(function() {
-      url = $('.pagination .next_page').attr('href');
-      var canPaginate =  url && ($(window).scrollTop() > ($(document).height() - $(window).height() - 1750)) && !$('.loading').is(':visible');
-      if (canPaginate) {
-        $('.loading').show();
-        $('.pagination .next_page').remove();
-        $('form#filter').find("input[type='checkbox']").attr("disabled", "true");
-        return $.getScript(url);
-      }
-    });
+    InfititeScroll(window, document)
   }
 });
 
