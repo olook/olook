9f468c8f06b93a779d0e497c0ce0a90fad60d071
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-11-22 16:27:51 -0200

    creating structure of gift boxes

---
+++ app/controllers/admin/gift_boxes_controller.rb
@@ -0,0 +1,19 @@
+class Admin::GiftBoxesController < Admin::BaseController
+  def index
+  end
+  
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
