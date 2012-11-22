6575a4bec83573b564a7ddc55036b0667d634b3d
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-11-22 17:23:00 -0200

    WIP gift boxes

--- spec/integration/admin/gift_box_management_spec.rb
+++ spec/integration/admin/gift_box_management_spec.rb
@@ -5,6 +5,7 @@ feature "Admin user with business 1 role manages gift boxes", %q{
   As a business 1 I can create, edit and delete gift boxes
 } do
 
+  let(:gift_boxes) { [FactoryGirl.create(:gift_box)] }
 
   before :each do
   	@admin = FactoryGirl.create(:admin_business1)
@@ -15,6 +16,11 @@ feature "Admin user with business 1 role manages gift boxes", %q{
   scenario "As a sac_operator I should not be allowed to destroy a collection" do
   	do_admin_login!(@admin)
     visit "/admin/gift_boxes"
+    page.should have_content "Listando Gift Boxes"
+    page.should have_content "Nome"  
+    page.should have_content "Top 5"
+    page.should have_content "Ativo"
+    page.should have_content "Sim"
   end 
 
 end
