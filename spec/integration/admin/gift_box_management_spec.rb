8ebbc0a9d6c9cea627d8ce669d499284989c7601
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-23 10:41:07 -0200

    creating scenario to update gift box type

--- spec/integration/admin/gift_box_management_spec.rb
+++ spec/integration/admin/gift_box_management_spec.rb
@@ -5,7 +5,7 @@ feature "Admin user with business 1 role manages gift boxes", %q{
   As a business 1 I can create, edit and delete gift boxes
 } do
 
-  let(:gift_boxes) { [FactoryGirl.create(:gift_box)] }
+  let(:gift_box) { FactoryGirl.create(:gift_box) }
 
   before :each do
   	@admin = FactoryGirl.create(:admin_business1)
@@ -20,5 +20,41 @@ feature "Admin user with business 1 role manages gift boxes", %q{
     page.should have_content "Nome"
     page.should have_content "Ativo"
   end
+
+  scenario "As a business1 user I should be denied to create a gift box if the form is not correct" do
+  	do_admin_login!(@admin)
+    visit "/admin/gift_boxes/new"
+    page.should have_content "Novo Gift Box Type"
+    page.should have_content "Nome"
+    page.should have_content "Ativo"
+    click_button('Criar Gift box')
+    page.should have_content "errors prohibited this landing page from being saved:"
+  end
+
+  scenario "As a business1 user I should be allowed to create a gift box if the form is correct" do
+  	do_admin_login!(@admin)
+    visit "/admin/gift_boxes/new"
+    page.should have_content "Novo Gift Box Type"
+    page.should have_content "Nome"
+    page.should have_content "Ativo"
+    fill_in 'gift_box[name]', :with => 'Gift Box Test'
+    check('gift_box[active]')
+    click_button('Criar Gift box')
+    page.should have_content "Gift Box Type criada com sucesso."
+  end
+
+  scenario "As a business1 user I should be allowed to update a gift box if the form is correct" do
+  	do_admin_login!(@admin)
+    visit "/admin/gift_boxes/#{ gift_box.id }/edit"
+    page.should have_content "Editar Gift Box Type"
+    page.should have_content "Nome"
+    page.should have_content "Ativo"
+    fill_in 'gift_box[name]', :with => 'Gift Box Test Updated'
+    check('gift_box[active]')
+    click_button('Atualizar Gift box')
+    page.should have_content "Gift Box Type atualizada com sucesso."
+    page.should have_content "Gift Box Test Updated"
+  end
+
 end
 
