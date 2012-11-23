7d7d5cd3c5f32f3c7774d956b8c85b7bfaf8563c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-23 14:45:40 -0200

    fixing relations

--- app/models/gift_box.rb
+++ app/models/gift_box.rb
@@ -1,4 +1,6 @@
 class GiftBox < ActiveRecord::Base
   validates :name, :presence => true
   mount_uploader :thumb_image, ImageUploader
+  has_many :gift_boxes_products, :dependent => :destroy
+  has_many :products, :through => :gift_boxes_products
 end
