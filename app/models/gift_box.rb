55780db67d0e90d7a23e7a2bd53c7275818e1658
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-23 12:11:17 -0200

    removing validation of presence of active

--- app/models/gift_box.rb
+++ app/models/gift_box.rb
@@ -1,5 +1,4 @@
 class GiftBox < ActiveRecord::Base
   validates :name, :presence => true
-  validates :active, :presence => true
   mount_uploader :thumb_image, ImageUploader
 end
--- spec/models/gift_box_spec.rb
+++ spec/models/gift_box_spec.rb
@@ -3,6 +3,5 @@ require 'spec_helper'
 describe GiftBox do
   describe "#validations" do
     it { should validate_presence_of :name }
-    it { should validate_presence_of :active }
   end
 end
