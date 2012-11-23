871174ef8f2335fde141f3e27b28f6c69ab7ab09
Rafael Carvalho <rafa1402@gmail.com>
2012-11-23 16:18:36 -0200

    add cookie function

--- app/assets/javascripts/campaign_emails_modal.js
+++ app/assets/javascripts/campaign_emails_modal.js
@@ -1,19 +1,22 @@
 function criaCookie(chave, value) { 
 	var expira = new Date(); 
 	expira.setTime(expira.getTime() + 87600000); //expira dentro de 24h 
-	document.cookie = chave + '=' + value + ';expira=' + expira.toUTCString(); 
-	console.log(document.cookie)
+	document.cookie = chave + '=' + value + ';expira=' + expira.toString(); 
 } 
 function lerCookie(chave) { 
 	var ChaveValor = document.cookie.match('(^|;) ?' + chave + '=([^;]*)(;|$)'); 
 	return ChaveValor ? ChaveValor[2] : null; 
 }
 $(function(){
-	criaCookie("modalShow","sim")
-	
+	if(lerCookie("modalShow") == null){
+		$("#overlay-campaign").delay(100).fadeIn();
+		$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
+		window.setTimeout(function(){
+				if($("#overlay-campaign").is(":visible"))
+					criaCookie("modalShow","sim");
+		},500)
 
-	$("#overlay-campaign").delay(100).fadeIn();
-	$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
-
+	}
 })
+
 
