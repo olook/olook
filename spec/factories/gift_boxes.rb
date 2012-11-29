66821587deb9e01d0ddc8a10e41bbfa1765e22f1
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-11-29 14:28:23 -0200

    perding tests

--- spec/factories/gift_boxes.rb
+++ spec/factories/gift_boxes.rb
@@ -1,6 +1,20 @@
 FactoryGirl.define do
-	factory :gift_box do
-		name "Top 5"
-		active true
-	end
+  factory :gift_box do
+    name "Top 5"
+    active true
+    thumb_image "image.jpg"
+
+    factory :gift_box_helena do
+      name "Dica da Helena"
+    end
+
+    factory :gift_box_top_five do
+      name "Top Five"
+    end
+
+
+    factory :gift_box_hot_fb do
+      name "Hot on Facebook"
+    end
+  end
 end
