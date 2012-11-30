e7cda4b9314f18554b5c9c26cf2978fb7fbf1103
Tiago Almeida <tiago.almeida@olook.com.br>
2012-11-30 15:52:33 -0200

    Change test for new way of do mailer

--- spec/mailers/loyalty_program_mailer_spec.rb
+++ spec/mailers/loyalty_program_mailer_spec.rb
@@ -5,7 +5,11 @@ describe LoyaltyProgramMailer do
   describe "loyalty program credits enabled e-mail" do
     let(:member) { FactoryGirl.create(:member) }
     let!(:loyalty_program_credit_type) { FactoryGirl.create(:loyalty_program_credit_type) }
-    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }  
+    let!(:redeem_credit_type) { FactoryGirl.create(:redeem_credit_type) }
+    let!(:invite_credit_type) {FactoryGirl.create(:invite_credit_type) }
+    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }
+    let(:user_credit2) { FactoryGirl.create(:user_credit, :credit_type => redeem_credit_type, :user => member , :amount  => "20") }
+    let(:user_credit3) { FactoryGirl.create(:user_credit, :credit_type => invite_credit_type, :user => member , :amount  => "20") }
 
 
     it "sets 'from' attribute to olook <avisos@olook.com.br>" do
@@ -38,7 +42,7 @@ describe LoyaltyProgramMailer do
   describe "loyalty program credits to be expired e-mail" do
     let(:member) { FactoryGirl.create(:member) }
     let!(:loyalty_program_credit_type) { FactoryGirl.create(:loyalty_program_credit_type) }
-    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }  
+    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }
 
 
     it "sets 'from' attribute to olook <avisos@olook.com.br>" do
@@ -68,7 +72,7 @@ describe LoyaltyProgramMailer do
       mail = LoyaltyProgramMailer.send_expiration_warning(member)
 
       # user_credit = loyalty_program_credit_type
-      @credit_amount = LoyaltyProgramCreditType.credit_amount_to_expire(user_credit)      
+      @credit_amount = LoyaltyProgramCreditType.credit_amount_to_expire(user_credit)
       mail.subject.should == "#{member.first_name}, seus R$ #{('%.2f' % @credit_amount).gsub('.',',')} em créditos vão expirar!"
 
       Delorean.back_to_the_present
@@ -79,11 +83,11 @@ describe LoyaltyProgramMailer do
 
     let(:member) { FactoryGirl.create(:member) }
     let!(:loyalty_program_credit_type) { FactoryGirl.create(:loyalty_program_credit_type) }
-    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }  
+    let(:user_credit) { FactoryGirl.create(:user_credit, :credit_type => loyalty_program_credit_type, :user => member) }
 
     it "sets 'from' attribute to olook <avisos@olook.com.br>" do
       user_credit.add({amount: 20})
-      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)      
+      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)
       mail = LoyaltyProgramMailer.send_expiration_warning(member, true)
       mail.from.should include("avisos@olook.com.br")
       Delorean.back_to_the_present
@@ -91,7 +95,7 @@ describe LoyaltyProgramMailer do
 
     it "sets 'to' attribute to passed member's email" do
       user_credit.add({amount: 20})
-      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)      
+      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)
       mail = LoyaltyProgramMailer.send_expiration_warning(member, true)
       mail.to.should include(member.email)
       Delorean.back_to_the_present
@@ -100,14 +104,14 @@ describe LoyaltyProgramMailer do
     it "sets 'title' attribute" do
 
       user_credit.add({amount: 20})
-      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)      
+      Delorean.time_travel_to(user_credit.credits.last.expires_at - 1.days)
       mail = LoyaltyProgramMailer.send_expiration_warning(member, true)
 
       # user_credit = loyalty_program_credit_type
-      @credit_amount = LoyaltyProgramCreditType.credit_amount_to_expire(user_credit)      
+      @credit_amount = LoyaltyProgramCreditType.credit_amount_to_expire(user_credit)
       mail.subject.should == "Corra #{member.first_name}, seus R$ #{('%.2f' % @credit_amount).gsub('.',',')} em créditos expiram amanhã!"
       Delorean.back_to_the_present
     end
-  end  
+  end
 
 end
