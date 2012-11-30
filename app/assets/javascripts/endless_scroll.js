a469196808c1e0456809926daeac0f7f37fa1551
Rafael Carvalho <rafa1402@gmail.com>
2012-11-30 14:26:42 -0200

    adjustment AMO! and filter

--- app/assets/javascripts/endless_scroll.js
+++ app/assets/javascripts/endless_scroll.js
@@ -31,15 +31,15 @@ jQuery(function() {
   }
 
   $("html").live("click", function() {
-    $("div#products_container div#shoe_size_filter div.sizes").hide();
+    $("div#shoe_size_filter div.sizes").hide();
   });
 
-  $("div#products_container div#shoe_size_filter span").live("click", function(e) {
+  $("div#shoe_size_filter span").live("click", function(e) {
     $(this).siblings(".sizes").toggle();
     e.stopPropagation();
   });
 
-  $("div#products_container div#shoe_size_filter div.sizes input").change(function(e) {
+  $("div#shoe_size_filter div.sizes input").change(function(e) {
     checked = $(this).is(":checked");
     val = $(this).val();
     filterSize = $("form#filter div.filter #"+val);
@@ -50,16 +50,18 @@ jQuery(function() {
       $(filterSize).attr("checked", false);
     }
     checkedSize = $("div#shoe_size_filter div.sizes input:checked").size();
+    console.log(checkedSize);
     $("div#shoe_size_filter div.sizes input:checked").each(function(index) {
       if(checkedSize == 1) {
-        $("div#products_container div#shoe_size_filter span").text($(this).val());
+        $("div#shoe_size_filter span").text($(this).val());
       } else {
         if(index == 0) {
           textSize = $(this).val();
         } else {
           textSize = textSize + ", " + $(this).val();
         }
-        $("div#products_container div#shoe_size_filter span").text(textSize);
+		console.info(checkedSize);
+        $("div#shoe_size_filter span").text(textSize);
       }
     });
     $(filterSize).click();
