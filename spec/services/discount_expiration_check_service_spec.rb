6c6f4f96798138e62fecbb7e39c99f9da68706c1
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-03 20:12:52 -0200

    WIP discount expiration: some test placeholders and comments where relevant code is

---
+++ spec/services/discount_expiration_check_service_spec.rb
@@ -0,0 +1,13 @@
+require "spec_helper"
+
+describe DiscountExpirationCheckService do
+
+	context "campaign email" {}
+
+	context "user created from campaign email" {}
+ 
+	context "user not created from a campaign email" {}
+
+	context "user has purchases" {}
+
+end
--- spec/strategies/promotions/purchases_amount_spec.rb
+++ spec/strategies/promotions/purchases_amount_spec.rb
@@ -1,13 +1,25 @@
 require 'spec_helper'
 
 describe Promotions::PurchasesAmountStrategy do
-  it "should be applied if the user matches the requirement" do
-    promo = FactoryGirl.create(:first_time_buyers)
-    Promotions::PurchasesAmountStrategy.new(promo.param, FactoryGirl.create(:user)).matches?.should be_true
-  end
+	context "#matches?" do
+	  it "should return true if user doesn't have any purchases" do
+	    promo = FactoryGirl.create(:first_time_buyers)
+	    Promotions::PurchasesAmountStrategy.new(promo.param, FactoryGirl.create(:user)).matches?.should be_true
+	  end
 
-  it "should not be applied if the user matches the requirement" do
-    promo = FactoryGirl.create(:first_time_buyers)
-    Promotions::PurchasesAmountStrategy.new(promo.param, FactoryGirl.create(:delivered_order).user).matches?.should be_false
-  end
+	  it "should return false if the user has any purchases" do
+	    promo = FactoryGirl.create(:first_time_buyers)
+	    Promotions::PurchasesAmountStrategy.new(promo.param, FactoryGirl.create(:delivered_order).user).matches?.should be_false
+	  end
+
+	  context "promotion expiration" do
+		  it "should return true if the promotion is still active" do
+		  	
+		  end
+
+		  it "should return false if promotion has expired" do
+
+		  end
+	  end
+	end
 end
