f306233ce15886d5daab278e5450e51a9a5b6cf8
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-04 19:18:03 -0200

    discount expiration: DiscountExpirationCheckService.discount_expired?

--- spec/services/discount_expiration_check_service_spec.rb
+++ spec/services/discount_expiration_check_service_spec.rb
@@ -2,13 +2,48 @@ require "spec_helper"
 
 describe DiscountExpirationCheckService do
 
-	context "#discount_expired?" do
+	# Used in purchases_amount_strategy, in cart_service
+	context ".discount_expired?" do 
 		
-		context "campaign email" 
-		
-		context "user created from campaign email"
- 
-		context "user not created from a campaign email"
-		
+		context "user converted from campaign email" do
+			let(:converted_user) { FactoryGirl.create(:user, converted_at: DateTime.now - 7.days) }
+
+			context "7 days after original campaign email's creation date" do
+				it "returns true" do
+					DiscountExpirationCheckService.discount_expired?(converted_user).should be_true
+				end
+			end
+
+			context "within 7 days of expiration date" do
+				let(:converted_user) { FactoryGirl.create(:user, converted_at: DateTime.now - 5.days) }
+
+				it "returns false" do
+					DiscountExpirationCheckService.discount_expired?(converted_user).should be_false
+				end
+			end
+		end
+
+		context "user not created from a campaign email" do
+			let(:user) { FactoryGirl.create(:user, created_at: DateTime.now - 7.days) }
+
+			context "7 days after original campaign email's creation date" do
+				it "returns true" do
+					DiscountExpirationCheckService.discount_expired?(user).should be_true
+				end
+			end
+
+			context "within 7 days of expiration date" do
+				let(:user) { FactoryGirl.create(:user, created_at: DateTime.now - 5.days) }
+
+				it "returns false" do
+					DiscountExpirationCheckService.discount_expired?(user).should be_false
+				end
+			end
+		end		
+
+	end
+
+	# Used in notification worker every day
+	context ".discount_expires_in_48_hours?" do
 	end
 end
