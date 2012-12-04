9b86a45d7f404980b770acd3ae263e0e3b7e04cc
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-04 19:18:03 -0200

    discount expiration: DiscountExpirationCheckService.discount_expired?

--- app/services/discount_expiration_check_service.rb
+++ app/services/discount_expiration_check_service.rb
@@ -1,2 +1,22 @@
 class DiscountExpirationCheckService
+
+	class << self 
+		def discount_expired?(user)
+			raise ArgumentError.new('a user is required!') unless user
+
+			sign_up_date(user).to_date <= 7.days.ago.to_date
+		end
+
+		def discount_expires_in_48_hours?
+
+		end
+
+		private
+
+			def sign_up_date(user)
+				user.converted_at? ? user.converted_at : user.created_at
+			end
+
+	end
+
 end
