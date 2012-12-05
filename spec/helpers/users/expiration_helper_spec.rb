b5fa2b0622d94bfde4ef4475fc7a6a2a16e685d2
Luis Daher <luisotaviodaher@gmail.com>
2012-12-05 18:19:29 -0200

    expiration helper implemented

--- spec/helpers/users/expiration_helper_spec.rb
+++ spec/helpers/users/expiration_helper_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe Users::ExpirationHelper do
-  context "#user_expiration_date" do
+  context "#user_expiration_month" do
     let(:now) { DateTime.now }
     let(:user) { FactoryGirl.create(:user, created_at: now) }
 
-    it "returns the expiration date for a given user" do
-      helper.user_expiration_date(user).should eq (now + 7.days).to_date
+    it "returns the expiration month for a given user formatted correctly" do
+      month_str = "%02d" % (now + 7.days).to_date.month.to_s
+      helper.user_expiration_month(user).should eq month_str
     end
+  end
 
+  context "#user_expiration_day" do
+    let(:now) { DateTime.now }
+    let(:user) { FactoryGirl.create(:user, created_at: now) }
+
+    it "returns the expiration day for a given user formatted correctly" do
+      day_str = "%02d" % (now + 7.days).to_date.day.to_s
+      helper.user_expiration_day(user).should eq day_str
+    end
   end
 end
