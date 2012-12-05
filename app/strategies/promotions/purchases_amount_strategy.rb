e16925cabcfdf32c4a27e9f4fbd3a77d3654c969
Rafael Carvalho <rafa1402@gmail.com>
2012-12-05 19:35:22 -0200

    setting to display the date on product page stating how long can use the discount

--- app/strategies/promotions/purchases_amount_strategy.rb
+++ app/strategies/promotions/purchases_amount_strategy.rb
@@ -9,6 +9,7 @@ module Promotions
     end
 
     def matches?
+      return false unless user && user.created_at
       return false if DiscountExpirationCheckService.discount_expired?(user)
 
       user ? user.orders.purchased.size == param.to_i : 0 == param.to_i
