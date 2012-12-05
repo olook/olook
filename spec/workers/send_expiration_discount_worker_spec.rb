ca6ecaf964e2f10272d9995395cc11635dfa73be
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-05 18:34:51 -0200

    updating worker test

--- spec/workers/send_expiration_discount_worker_spec.rb
+++ spec/workers/send_expiration_discount_worker_spec.rb
@@ -2,12 +2,20 @@ require "spec_helper"
 
 describe SendExpirationDiscountWorker do
   describe ".on perform" do
-    let(:user) { FacotyGirl.create(:user, email: "vinicius.monteiro@olook.com.br") }
-    let(:mock_mail) { double :mail }
     context "sending email" do
+      let!(:user) { FactoryGirl.create(:user, created_at: DateTime.now - 5.days ) }
+      let(:mock_mail) { double :mail }
       it "should send email" do
         mock_mail.should_receive(:deliver)
         ExpirationDiscountMailer.should_receive(:send_expiration_email).once.and_return(mock_mail)
+        described_class.perform
+      end
+
+      it "shouldn't send email" do
+        user.created_at = DateTime.now - 6.days
+        user.save
+        mock_mail.should_not_receive(:deliver)
+        ExpirationDiscountMailer.should_not_receive(:send_expiration_email)
         described_class.perform
       end
     end
