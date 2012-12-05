4230036e58ee16d24af10a39f889c8359b1e3ecc
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-05 13:43:48 -0200

    fixing syntax

--- app/mailers/expiration_discount_mailer.rb
+++ app/mailers/expiration_discount_mailer.rb
@@ -1,6 +1,6 @@
 # -*- encoding  utf-8 -*-
-class ExpirationDiscountMailer < ActionMailerBase
-  default_url_options[host] = "www.olook.com.br"
+class ExpirationDiscountMailer < ActionMailer::Base
+  default_url_options[:host] = "www.olook.com.br"
   default from: "olook <avisos@olook.com.br>"
 
   def self.smtp_settings
@@ -9,7 +9,7 @@ class ExpirationDiscountMailer < ActionMailerBase
       password: "AkYlOmgbIpISW33XVzQq8d9J4GnAgtQlEJuwgIxOFXmU",
       address: "email-smtp.us-east-1.amazonaws.com",
       port: 587,
-      authentication: plain,
+      authentication: :plain,
       enable_starttls_auto: true
     }
   end
