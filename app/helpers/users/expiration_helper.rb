b48b186cfb28d1506c0da58e99a504f34bf74c71
Luis Daher <luisotaviodaher@gmail.com>
2012-12-05 18:19:29 -0200

    expiration helper implemented

--- app/helpers/users/expiration_helper.rb
+++ app/helpers/users/expiration_helper.rb
@@ -1,5 +1,10 @@
 module Users::ExpirationHelper
-  def user_expiration_date user
-    DiscountExpirationDate.discount_expires_at(user)
+  def user_expiration_month(user)
+    "%02d" % ::DiscountExpirationCheckService.discount_expiration_date_for(user).month.to_s
   end
+
+  def user_expiration_day(user)
+    "%02d" % ::DiscountExpirationCheckService.discount_expiration_date_for(user).day.to_s
+  end
+
 end
