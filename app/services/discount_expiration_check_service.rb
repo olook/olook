51ff6d25e4c26fd36966ce68aeb49cd0360c9387
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-06 14:59:43 -0200

    fixing tests

--- app/services/discount_expiration_check_service.rb
+++ app/services/discount_expiration_check_service.rb
@@ -1,25 +1,31 @@
 class DiscountExpirationCheckService
 
-	class << self 
-		def discount_expired?(user)
-			raise ArgumentError.new('a user is required!') unless user
+  class << self
+    def discount_expired?(user)
+      raise ArgumentError.new('a user is required!') unless user
 
-			sign_up_date(user).to_date <= 7.days.ago.to_date
-		end
+      sign_up_date(user).to_date <= 7.days.ago.to_date
+    end
 
-		def discount_expires_in_48_hours?(user_or_campaign_email)
-			sign_up_date(user_or_campaign_email).to_date == 5.days.ago.to_date
-		end
+    def discount_expires_in_48_hours?(user_or_campaign_email)
+      sign_up_date(user_or_campaign_email).to_date == 5.days.ago.to_date
+    end
 
-		def discount_expiration_date_for(user_or_campaign_email)
-			(sign_up_date(user_or_campaign_email) + 7.days).to_date
-		end
+    def discount_expiration_date_for(user_or_campaign_email)
+      (sign_up_date(user_or_campaign_email) + 7.days).to_date
+    end
 
-		private
+    def discount_expiration_48_hours_emails_list
+      user_list = User.with_discount_about_to_expire_in_48_hours
+      user_list << CampaignEmail.with_discount_about_to_expire_in_48_hours
+      user_list.flatten
+    end
 
-			def sign_up_date(user_or_campaign_email)
-				(user_or_campaign_email.respond_to?(:campaign_email_created_at) && user_or_campaign_email.campaign_email_created_at) ? user_or_campaign_email.campaign_email_created_at : user_or_campaign_email.created_at
-			end
-	end
+    private
+
+    def sign_up_date(user_or_campaign_email)
+      (user_or_campaign_email.respond_to?(:campaign_email_created_at) && user_or_campaign_email.campaign_email_created_at) ? user_or_campaign_email.campaign_email_created_at : user_or_campaign_email.created_at
+    end
+  end
 
 end
