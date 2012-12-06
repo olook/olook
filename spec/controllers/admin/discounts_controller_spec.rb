e658aa52a0f5746b7d45af50cefbc9ff97c0a5ef
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-06 18:10:41 -0200

    admin sign_in before test

--- spec/controllers/admin/discounts_controller_spec.rb
+++ spec/controllers/admin/discounts_controller_spec.rb
@@ -2,6 +2,12 @@ require 'spec_helper'
 
 describe Admin::DiscountsController do
 
+  before :each do
+    request.env['devise.mapping'] = Devise.mappings[:admin]
+    @admin = FactoryGirl.create(:admin_superadministrator)
+    sign_in @admin
+  end
+
   describe "GET 'index'" do
     it "returns http success" do
       get 'index'
