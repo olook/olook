30c3b09112bef30c4e79a1572c870cd3b1b29c2e
darkseid <rafael.manoel@gmail.com>
2012-12-07 01:37:40 -0200

    small improvements on delivery date seach

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,11 +1,16 @@
 class ShippingsController < ApplicationController
   DEFAULT_VALUE = 50.0
-  respond_to :json
+  respond_to :json, :js
 
   def show
     zip_code = params[:id]
     freight = FreightCalculator.freight_for_zip zip_code, DEFAULT_VALUE
-    @days_to_deliver = freight[:delivery_time]
+
+    if freight.empty?
+      render :status => :not_found 
+    else
+      @days_to_deliver = freight[:delivery_time]
+    end
   end
 
 
