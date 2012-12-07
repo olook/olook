abe919da4b7633980c1054eeb658274a00809986
Tiago Almeida <tiago.almeida@olook.com.br>
2012-12-07 16:38:09 -0200

    Change method on PurchasesAmountStrategy

--- app/helpers/mailers/order_status_mailer_helper.rb
+++ app/helpers/mailers/order_status_mailer_helper.rb
@@ -2,7 +2,7 @@
 module Mailers::OrderStatusMailerHelper
 
   def used_20_percent_descount?
-    if Promotions::PurchasesAmountStrategy.new("1", @order.user).matches?
+    if Promotions::PurchasesAmountStrategy.new("1", @order.user, @order).matches_20_percent_promotion?
        content_tag :p, "Você utilizou seu desconto de 20% neste pedido."
     end
   end
