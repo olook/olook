ebf943a8f43282e69d27a420fba0fc22d1006352
Rafael Carvalho <rafa1402@gmail.com>
2012-12-07 18:31:27 -0200

    adjustment of spaces from result in shipping box and opacity IE8

--- app/assets/javascripts/campaign_emails_modal.js
+++ app/assets/javascripts/campaign_emails_modal.js
@@ -8,7 +8,7 @@ function lerCookie(chave) {
 
 $(function(){
 	if(lerCookie("modalShow") == null){
-		$("#overlay-campaign").delay(100).fadeIn();
+		$("#overlay-campaign").delay(100).show();
 		$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
 		window.setTimeout(function(){
 				if($("#overlay-campaign").is(":visible"))
