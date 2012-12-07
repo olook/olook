bec8fdfc018b8bd563cfc6ae87f044890f4797c2
Luis Daher <luisotaviodaher@gmail.com>
2012-12-07 11:51:11 -0200

    admin working now :D

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -2,17 +2,7 @@ class Admin::DiscountsController < Admin::BaseController
 
   respond_to :html
 
-  load_and_authorize_resource
-  
   def index
-
-    @user_discounts = nil
-    if params[:search]
-      @search = User.search(params[:search]) if params[:search]
-      @user_discounts = @search.order('created_at desc')
-    else
-      @user_discounts = DiscountExpirationCheckService.find_all_discounts
-    end
-    # @user_discounts = @search.order('created_at desc')
+      @user_discounts = DiscountExpirationCheckService.search(params[:search])
   end
 end
