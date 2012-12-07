fda81620f0c36a9c3ca67e10aba95d8cfcc3fc0a
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-07 18:52:05 -0200

    refactoring all context to show discounts

--- app/services/discount_expiration_check_service.rb
+++ app/services/discount_expiration_check_service.rb
@@ -19,23 +19,11 @@ class DiscountExpirationCheckService
       user_list = User.with_discount_about_to_expire_in_48_hours + CampaignEmail.with_discount_about_to_expire_in_48_hours
     end
 
-    def find_all_discounts
-      find_users_with_discount + find_campaign_emails_with_discount
-    end
-
     def search(email)
       format_user_list(User.where("email like ? " ,"%#{email}%")) + format_campaign_email_list(CampaignEmail.where("email like ? " ,"%#{email}%"))
     end
 
     private
-
-    def find_campaign_emails_with_discount
-      format_campaign_email_list(CampaignEmail.where({created_at: ((DateTime.now - 1.month).beginning_of_day..(DateTime.now).end_of_day)}).order('created_at desc'))
-    end
-
-    def find_users_with_discount
-      format_user_list(User.where({created_at: ((DateTime.now - 1.month).beginning_of_day..(DateTime.now).end_of_day)}).order('created_at desc').includes(:orders))
-    end
 
     def sign_up_date(user_or_campaign_email)
       (user_or_campaign_email.respond_to?(:campaign_email_created_at) && user_or_campaign_email.campaign_email_created_at) ? user_or_campaign_email.campaign_email_created_at : user_or_campaign_email.created_at
