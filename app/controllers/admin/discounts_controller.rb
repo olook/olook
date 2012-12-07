47ff3174ed742e9b2def4c127c97154192cdd230
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-07 15:40:42 -0200

    removing logic from specific method

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -3,6 +3,6 @@ class Admin::DiscountsController < Admin::BaseController
   respond_to :html
 
   def index
-      @user_discounts = DiscountExpirationCheckService.search(params[:email][:search])
+      @user_discounts = params[:email] && params[:email].size > 0 ? DiscountExpirationCheckService.search(params[:email][:search]) : DiscountExpirationCheckService.find_all_discounts.first(50)
   end
 end
