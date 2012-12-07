1feaa6c085df751babe3d02737f707ddaa492811
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-07 15:11:00 -0200

    refactoring params

--- app/controllers/admin/discounts_controller.rb
+++ app/controllers/admin/discounts_controller.rb
@@ -3,6 +3,6 @@ class Admin::DiscountsController < Admin::BaseController
   respond_to :html
 
   def index
-      @user_discounts = DiscountExpirationCheckService.search(params[:search])
+      @user_discounts = DiscountExpirationCheckService.search(params[:email][:search])
   end
 end
