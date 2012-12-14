52cbd96ea1f427a29b3859e274ebafa8734803b8
Matheus Bodo <mcbodo@gmail.com>
2012-12-14 17:38:35 -0200

    refactoring promotion strategy creation

--- spec/models/ecommerce/promotion_spec.rb
+++ spec/models/ecommerce/promotion_spec.rb
@@ -4,7 +4,7 @@ describe Promotion do
   describe "strategies" do
     it "should apply the appropriated strategy" do
       promo = FactoryGirl.create(:first_time_buyers)
-      promo.load_strategy.to_s.should  == "Promotions::PurchasesAmountStrategy"
+      promo.load_strategy.should  eq(Promotions::PurchasesAmountStrategy)
     end
   end
 end
