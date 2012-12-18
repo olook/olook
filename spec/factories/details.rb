542929612af2f27bb5a18b30aa4725f898d9ab89
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-18 18:58:14 -0200

    adding materials to google shopping xml and buscape xml; fixing olook title on all xml that uses it

--- spec/factories/details.rb
+++ spec/factories/details.rb
@@ -8,7 +8,7 @@ FactoryGirl.define do
       description "High heel"
       translation_token 'heel'
     end
-    
+
     factory :invisible_detail do
       display_on DisplayDetailOn::INVISIBLE
       description "Meta data used for data mining"
@@ -25,7 +25,7 @@ FactoryGirl.define do
       translation_token 'Categoria'
       description "Sandalia"
     end
-    
+
     factory :shoe_heel do
       display_on DisplayDetailOn::SPECIFICATION
       translation_token 'Salto/Tamanho'
