88a3e39680af931f8dd21fe782a6b240fa1a9af0
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2012-12-18 15:03:23 -0200

    creating logic for free item and creating tests

--- app/strategies/promotions/free_item_strategy.rb
+++ app/strategies/promotions/free_item_strategy.rb
@@ -11,8 +11,22 @@ module Promotions
       cart.items.count >= promotion.param.to_i if cart
     end
 
-    def calculate_value(item_price)
-      0
+    def calculate_value(cart_items, item)
+      number_of_items = cart_items.count / promotion.param.to_i
+      free_items = sort_cart_items(cart_items).first(number_of_items)
+      free_items.include?(item) ? 0 : item.price
+    end
+
+    def sort_cart_items(cart_items)
+      cart_items.sort { |a,b|
+        if a.price < b.price
+          -1
+        elsif a.price > b.price
+          1
+        else
+          a.id > b.id ? -1 : 1
+        end
+      }
     end
   end
 end
