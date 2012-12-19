d2263fdb522f3dbf20f307e007ef7b4f276a5e75
darkseid <rafael.manoel@gmail.com>
2012-12-19 12:15:39 -0200

    change campaign banner showing rule

--- app/helpers/promotions_helper.rb
+++ app/helpers/promotions_helper.rb
@@ -1,7 +1,5 @@
 module PromotionsHelper
   PROMOTION_BANNER_WHITELIST = [
-      {:controller => "members", :actions => ["showroom", "welcome"]},
-      {:controller => "lookbooks"},
       {:controller => "stylists"},
       {:controller => "friends", :actions => ["showroom"]},
       {:controller => "product", :actions => ["show"]},
@@ -10,7 +8,6 @@ module PromotionsHelper
   ]
 
   PROMOTION_BANNER_GUEST_WHITELIST = [
-    {:controller => "lookbooks"}, 
     {:controller => "product", :actions => ["show"]},
     {:controller => "home", :actions => ["index"]},
     {:controller => "moments"}
@@ -18,12 +15,19 @@ module PromotionsHelper
 
   def render_promotion_banner
 
-    promotion = PromotionService.new(@user).detect_current_promotion if @user
-    if promotion && @user && page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)
-      render(:partial => "promotions/banners/#{promotion.strategy}", :locals => {:promotion => promotion})
-    elsif !current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST) && Promotion.purchases_amount
-      render(:partial => "promotions/banners/#{Promotion.purchases_amount.strategy}", :locals => {:promotion => Promotion.purchases_amount})
-    elsif Campaign.activated_campaign && ((current_user && page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)) || (!current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST)))
+    # Please, do not delete these commented code, cause the product guys are ALWAYS asking to change it (delete, rollback, delete, rolback...)
+    # As son as we got time to think in a more persistent rule, we'll stop doing this.
+
+    # promotion = PromotionService.new(@user).detect_current_promotion(@cart) if @user
+    # if promotion && @user && page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)
+    #   render(:partial => "promotions/banners/#{promotion.strategy}", :locals => {:promotion => promotion})
+    # elsif !current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST) && Promotion.purchases_amount
+    #   render(:partial => "promotions/banners/#{Promotion.purchases_amount.strategy}", :locals => {:promotion => Promotion.purchases_amount})
+    # elsif Campaign.activated_campaign && ((current_user && page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)) || (!current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST)))
+    #   render(:partial => "campaigns/campaign_active")
+    # end
+
+    if Campaign.activated_campaign && ((current_user && page_included_in_whitelist?(PROMOTION_BANNER_WHITELIST)) || (!current_user && page_included_in_whitelist?(PROMOTION_BANNER_GUEST_WHITELIST)))    
       render(:partial => "campaigns/campaign_active")
     end
 
