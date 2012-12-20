dc9c2b65c39f3ee1aaeb47b9e2a9d47fc517c22c
Tiago Almeida <tiago.almeida@olook.com.br>
2012-12-20 14:19:41 -0200

    Organizing cheordic class

--- app/models/chaordic_info.rb
+++ app/models/chaordic_info.rb
@@ -22,30 +22,62 @@ class ChaordicInfo
 
   private
 
-  # USER
-  def self.user_pack user
-    if user.kind_of? User
-      chaordic_user_info = set_user user.id
-      chaordic_user_info.add_info('name', user.name)
-      chaordic_user_info.add_info('email', user.email)
-    else
-      chaordic_user_info = set_user "CS_ANONYMOUSUSER"
+    def self.user_pack user
+      if user.kind_of? User
+        chaordic_user_info = set_user user.id
+        chaordic_user_info.add_info('name', user.name)
+        chaordic_user_info.add_info('email', user.email)
+      else
+        chaordic_user_info = set_user "CS_ANONYMOUSUSER"
+      end
+      create_chaordic_object.pack(chaordic_user_info)
     end
-    create_chaordic_object.pack(chaordic_user_info)
-  end
 
+    def self.order_pack order
+      order_user = set_user order.user.id
+      order_cart = set_cart order.cart
+      order_cart.user = order_user
+      chaordic_order = set_order order.id, order_cart
+      create_chaordic_object.pack(chaordic_order)
+    end
 
-  # BUY_ORDER
-  def self.order_pack order
-    order_user = set_user order.user.id
-    order_cart = set_cart order.cart
-    order_cart.user = order_user
-    chaordic_order = set_order order.id, order_cart
-    create_chaordic_object.pack(chaordic_order)
-  end
+    def self.product_pack product
+      chaordic_product = Chaordic::Packr::Product.new
+      chaordic_product.pid = product.id
+      chaordic_product.category = product.category_humanize
+      chaordic_product.detail_url = product.id
+      chaordic_product.image_name = product.main_picture.image.to_s.split('pictures/')[1]
+      chaordic_product.name = product.name
+      chaordic_product.old_price = product.price
+      chaordic_product.price = product.retail_price
+      chaordic_product.description = product.description
+      chaordic_product.installment_count = 1
+      chaordic_product.installment_price = product.retail_price
+      if product.category == 1
+        product.variants.each do |v|
+          size = v.description
+          sku = product.inventory > 1 ? v.sku : ""
+          chaordic_product.add_variant size, sku
+        end
+      end
+      chaordic_product.status = product.inventory > 1 ? "AVAILABLE" : "UNAVAILABLE"
+      chaordic_product.sub_category = product.subcategory
+      create_chaordic_object.pack(chaordic_product)
+    end
 
+    def self.cart_pack cart, user
+      add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
+      add_to_card_cart = set_cart cart
+      add_to_card_cart.user = add_to_card_user
+      create_chaordic_object.pack(add_to_card_cart)
+    end
 
-
+    def self.add_to_cart_pack cart, user
+      add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
+      add_to_card_cart = set_last_item_cart cart
+      add_to_card_cart.user = add_to_card_user
+      create_chaordic_object.pack(add_to_card_cart)
+    end
 
   ########## Metodos auxiliares para buy_order
   def self.set_cart cart
@@ -72,47 +104,6 @@ class ChaordicInfo
     chaordic_user = Chaordic::Packr::User.new
     chaordic_user.uid = user_id
     chaordic_user
-  end
-
-
-  # PRODUCT
-  def self.product_pack product
-    chaordic_product = Chaordic::Packr::Product.new
-    chaordic_product.pid = product.id
-    chaordic_product.category = product.category_humanize
-    chaordic_product.detail_url = product.id
-    chaordic_product.image_name = product.main_picture.image.to_s.split('pictures/')[1]
-    chaordic_product.name = product.name
-    chaordic_product.old_price = product.price
-    chaordic_product.price = product.retail_price
-    chaordic_product.description = product.description
-    chaordic_product.installment_count = 1
-    chaordic_product.installment_price = product.retail_price
-    if product.category == 1
-      product.variants.each do |v|
-        size = v.description
-        sku = product.inventory > 1 ? v.sku : ""
-        chaordic_product.add_variant size, sku
-      end
-    end
-    chaordic_product.status = product.inventory > 1 ? "AVAILABLE" : "UNAVAILABLE"
-    chaordic_product.sub_category = product.subcategory
-    create_chaordic_object.pack(chaordic_product)
-  end
-
-
-  def self.cart_pack cart, user
-    add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
-    add_to_card_cart = set_cart cart
-    add_to_card_cart.user = add_to_card_user
-    create_chaordic_object.pack(add_to_card_cart)
-  end
-
-  def self.add_to_cart_pack cart, user
-    add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
-    add_to_card_cart = set_last_item_cart cart
-    add_to_card_cart.user = add_to_card_user
-    create_chaordic_object.pack(add_to_card_cart)
   end
 
   def self.create_chaordic_object
