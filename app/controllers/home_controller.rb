eb76a1acd3ef5c685d194bb263ce9f8797bdab65
Tiago Almeida <tiago.almeida@olook.com.br>
2012-12-20 19:37:02 -0200

    Add chaordic user on index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ class HomeController < ApplicationController
 
   def index
     @google_path_pixel_information = "Home"
+    @chaordic_user = ChaordicInfo.user current_user
+
     if params[:share]
       @user = User.find(params[:uid])
       @profile = @user.profile_scores.first.try(:profile).try(:first_visit_banner)
