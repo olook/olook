36c2078af7fd995eca73fc10c499afeb199e68c6
Tiago Almeida <tiago.almeida@olook.com.br>
2012-12-20 19:40:59 -0200

    Change set order logic

--- app/models/chaordic_info.rb
+++ app/models/chaordic_info.rb
@@ -23,21 +23,15 @@ class ChaordicInfo
   private
 
     def self.user_pack user
-      if user.kind_of? User
-        chaordic_user_info = set_user user.id
-        chaordic_user_info.add_info('name', user.name)
-        chaordic_user_info.add_info('email', user.email)
-      else
-        chaordic_user_info = set_user "CS_ANONYMOUSUSER"
-      end
+      chaordic_user_info = set_user user
       create_chaordic_object.pack(chaordic_user_info)
     end
 
     def self.order_pack order
-      order_user = set_user order.user.id
-      order_cart = set_cart order.cart
-      order_cart.user = order_user
-      chaordic_order = set_order order.id, order_cart
+      order_user = set_user order.user
+      #order_cart = set_cart order.cart
+      #order_cart.user = order_user
+      chaordic_order = set_order order, order_user
       create_chaordic_object.pack(chaordic_order)
     end
 
@@ -48,11 +42,11 @@ class ChaordicInfo
       chaordic_product.detail_url = product.id
       chaordic_product.image_name = product.main_picture.image.to_s.split('pictures/')[1]
       chaordic_product.name = product.name
-      chaordic_product.old_price = product.price
-      chaordic_product.price = product.retail_price
+      chaordic_product.old_price = product.price.round(2).to_s
+      chaordic_product.price = product.retail_price.round(2).to_s
       chaordic_product.description = product.description
       chaordic_product.installment_count = 1
-      chaordic_product.installment_price = product.retail_price
+      chaordic_product.installment_price = product.retail_price.round(2).to_s
       if product.category == 1
         product.variants.each do |v|
           size = v.description
@@ -66,48 +60,58 @@ class ChaordicInfo
     end
 
     def self.cart_pack cart, user
-      add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
+      add_to_card_user = set_user user
       add_to_card_cart = set_cart cart
       add_to_card_cart.user = add_to_card_user
       create_chaordic_object.pack(add_to_card_cart)
     end
 
     def self.add_to_cart_pack cart, user
-      add_to_card_user = set_user user.nil? ? "CS_ANONYMOUSUSER" : user.id
+      add_to_card_user = set_user user
       add_to_card_cart = set_last_item_cart cart
       add_to_card_cart.user = add_to_card_user
       create_chaordic_object.pack(add_to_card_cart)
     end
 
-  ########## Metodos auxiliares para buy_order
-  def self.set_cart cart
-    chaordic_cart = Chaordic::Packr::Cart.new
-    cart.items.each do |item|
-      chaordic_cart.add_product item.product.id, item.product.retail_price
+    def self.set_cart cart
+      chaordic_cart = Chaordic::Packr::Cart.new
+      cart.items.each do |item|
+        chaordic_cart.add_product item.product.id, item.product.retail_price.round(2).to_s
+      end
+      chaordic_cart
     end
-    chaordic_cart
-  end
 
-  def self.set_last_item_cart cart
-    chaordic_cart = Chaordic::Packr::Cart.new
-    chaordic_cart.add_product cart.items.last.product.id, cart.items.last.product.retail_price unless cart.items.empty?
-    chaordic_cart
-  end
+    def self.set_last_item_cart cart
+      chaordic_cart = Chaordic::Packr::Cart.new
+      chaordic_cart.add_product cart.items.last.product.id, cart.items.last.product.retail_price.round(2).to_s unless cart.items.empty?
+      chaordic_cart
+    end
 
-  def self.set_order id, cart
-    chaordic_order = Chaordic::Packr::BuyOrder.new(cart)
-    chaordic_order.oid = id
-    chaordic_order
-  end
+    def self.set_order order, user
+      chaordic_order = Chaordic::Packr::Cart.new
+      order.line_items.each do |item|
+        chaordic_order.add_product item.variant.product.id, item.retail_price.round(2).to_s
+      end
+      chaordic_order.user = user
+      chaordic_order = Chaordic::Packr::BuyOrder.new(chaordic_order)
+      chaordic_order.oid = order.id
+      chaordic_order
+    end
 
-  def self.set_user user_id
-    chaordic_user = Chaordic::Packr::User.new
-    chaordic_user.uid = user_id
-    chaordic_user
-  end
+    def self.set_user user
+      chaordic_user = Chaordic::Packr::User.new
+      unless user.nil?
+        chaordic_user.uid = user.id
+        chaordic_user.add_info('Name', user.name)
+        chaordic_user.add_info('Email', user.email)
+      else
+        chaordic_user.uid = "CS_ANONYMOUSUSER"
+      end
+      chaordic_user
+    end
 
-  def self.create_chaordic_object
-    chaordic_class = Chaordic::Packr::Chaordic.new('4NO8cgqKRJZenICPBL/YtA==')
-  end
+    def self.create_chaordic_object
+      chaordic_class = Chaordic::Packr::Chaordic.new('4NO8cgqKRJZenICPBL/YtA==')
+    end
 
 end
