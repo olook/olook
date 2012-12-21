136f5b36e4dab4c91b7d0b3289c4aa1ac1995415
Luis Daher <luisotaviodaher@gmail.com>
2012-12-21 18:18:48 -0200

    controller tests being created

--- spec/controllers/cart/items_controller_spec.rb
+++ spec/controllers/cart/items_controller_spec.rb
@@ -3,16 +3,27 @@ require 'spec_helper'
 
 describe Cart::ItemsController do
 
-  let(:cart_item) {FactoryGirl.create(:cart_item_that_belongs_to_a_cart)} #{mock_model(CartItem, :order => order)}
+  let(:cart) {mock_model(Cart, id: 1)}
+  let(:product) { FactoryGirl.create(:basic_shoe) }
   context "#create" do
-    it "adds a new item to the users cart" do
-      session[:cart_id] = cart_item.cart.id
+    context "adding a new item to the users cart" do
+      it "adds an item with quantity = 1" do
+        session[:cart_id] = cart.id
+        post :create, variant_id: 1
+      end
     end
+
+    context "adding an existing item to the users cart" do
+      it "increases the item quantity" do
+        pending
+      end
+    end
+
   end
 
   context "#destroy" do
     it "removes a given item from the users cart" do
-
+      pending
     end
   end
 
