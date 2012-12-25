2451100f77314b4f0afcb6401a24aabdfa1f533e
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-25 00:26:54 -0200

    WIP: cart::items controller

--- app/controllers/cart/items_controller.rb
+++ app/controllers/cart/items_controller.rb
@@ -1,12 +1,16 @@
 # -*- encoding : utf-8 -*-
 class Cart::ItemsController < ApplicationController
+	before_filter :load_cart
 
   def create
-    
+  	@cart.items.create(params)
+    render text: ''
   end
 
-  def destroy
-    
-  end
+  private
+
+  	def load_cart
+  		@cart ||= Cart.find(session[:cart_id])
+  	end
 
 end
