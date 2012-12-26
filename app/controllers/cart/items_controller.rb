b264583ff7943c775808ccb4d7f2ff45c75a227b
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-26 20:10:05 -0200

    WIP items controller, almost there in basic success scenario, stuck on a test bug

--- app/controllers/cart/items_controller.rb
+++ app/controllers/cart/items_controller.rb
@@ -1,15 +1,25 @@
 # -*- encoding : utf-8 -*-
 class Cart::ItemsController < ApplicationController
 	before_filter :load_cart
+	respond_to :js
 
   def create
-  	@cart.items.create(params)
-    render text: ''
+  	#TODO return an error if these params aren't present, shouldn't 
+  	# need to be checking them
+    variant_id = params[:variant][:id] if params[:variant]
+    variant_quantity = params[:variant][:quantity] if params[:variant]
+
+    @variant = Variant.find_by_id(variant_id)
+    @cart.add_item(@variant, variant_quantity)
+
+    respond_with { |format| format.js {} }
   end
 
   private
 
   	def load_cart
+			binding.pry
+
   		@cart ||= Cart.find(session[:cart_id])
   	end
 
