dfb74a9aec532b646700662d0217b0e4da30cba8
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-27 18:18:37 -0200

    cart items controller handling of errors. also refactored the controller for legibility

--- app/controllers/cart/items_controller.rb
+++ app/controllers/cart/items_controller.rb
@@ -1,17 +1,53 @@
 # -*- encoding : utf-8 -*-
 class Cart::ItemsController < ApplicationController
 	respond_to :js
+	before_filter :ensure_params!, :ensure_a_variant_is_found!
 
-  def create
-  	#TODO return an error if these params aren't present, shouldn't 
-  	# need to be checking them
-    variant_id = params[:variant][:id] if params[:variant]
-    variant_quantity = params[:variant][:quantity] if params[:variant]
-
-    @variant = Variant.find_by_id(variant_id)
-    @cart.add_item(@variant, variant_quantity)
-
-    respond_with { |format| format.js {} }
+  def create  
+    add_item_or_show_errors
+ 
+  	update_cart_summary_on_view
   end
+
+  private
+
+  	def update_cart_summary_on_view
+  		# renders create.js.erb
+  		respond_with { |format| format.js {} }
+  	end
+
+  	def add_item_or_show_errors
+  		unless @cart.add_item(@variant, variant_qty)
+	      respond_with(@cart) do |format|
+	        notice_response = @cart.has_gift_items? ? "Produtos de presente não podem ser comprados com produtos da vitrine" : "Produto esgotado"
+	        
+	        format.js { render :error, locals: { notice: notice_response } }
+	      end 
+	    end
+		end
+
+  	def ensure_a_variant_is_found!
+    	respond_with do |format|
+        format.js do 
+        	render :error, :locals => { :notice => "Por favor, selecione o tamanho do produto." }
+        end
+      end unless @variant = Variant.find_by_id(variant_id)
+  	end
+
+  	def ensure_params!
+  		respond_with do |format|
+        format.js do 
+        	render :error, :locals => { :notice => "Por favor, selecione o tamanho do produto." }
+        end	        
+      end unless params[:variant] && params[:variant][:id]
+  	end
+
+  	def variant_id
+  		params[:variant][:id]
+  	end
+
+  	def variant_qty
+  		params[:variant][:quantity]
+  	end
 
 end
