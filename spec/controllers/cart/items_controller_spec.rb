607e81403c4502ac88262fa181ea7d127156671b
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-27 15:56:13 -0200

    cart items controller specs passing

--- spec/controllers/cart/items_controller_spec.rb
+++ spec/controllers/cart/items_controller_spec.rb
@@ -6,35 +6,34 @@ describe Cart::ItemsController do
 	let!(:cart) { FactoryGirl.create(:clean_cart) }
 	
 	before do  
-		session[:cart_id] = cart.id
 		controller.stub!(:current_referer)
-		# binding.pry
+		Cart.stub(:find)
+        .and_return(cart)
 	end
 	
-	describe "#create" do 		
+	describe "#create", js: true do 		
 
 		context "with valid variant params" do 
 			let(:basic_bag) { FactoryGirl.create(:basic_bag_simple) }
 			let(:params) { {variant: { id: basic_bag.id }} }
 
 			it "loads the cart from the session" do
-				post :create, params, :format=> :js
+				post :create, params, :format => :js
 				assigns(:cart).should eq(cart)
 			end
 
 	    it "finds a variant" do
-	      post :create, params, :format=> :js
+	      post :create, params, :format => :js
 	      assigns(:variant).should eq(basic_bag)
 	    end
 
 			it "adds an item to the cart instance" do
-        post :create, params, :format=> :js
+        post :create, params, :format => :js
         cart.items.first.variant.should eq(basic_bag)
       end
 
       it "renders create when item added and respond for js" do
-        request.env['HTTP_ACCEPT'] = "text/javascript"
-        post :create, params, :format=> :js
+        post :create, params, :format => :js
         response.should render_template ["create"]
       end
 		end
