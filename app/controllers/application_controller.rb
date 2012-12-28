f53583ce2d7cf7ce608c7fd103f929634d22910d
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-28 16:58:05 -0200

    ApplicationController refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,76 +2,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout "site"
-  before_filter :load_user
-  before_filter :load_cart
-  before_filter :load_facebook_api
-  before_filter :load_referer
-  before_filter :load_tracking_parameters
+  
+  before_filter :load_user,
+                :load_cart,
+                :load_promotion,  
+                :load_coupon,
+                :load_cart_service,
+                :load_facebook_api, 
+                :load_referer, 
+                :load_tracking_parameters
+
+  helper_method :current_liquidation, 
+                :show_current_liquidation?, 
+                :show_current_liquidation_advertise?, 
+                :current_cart, 
+                :current_moment, 
+                :current_referer
 
   rescue_from CanCan::AccessDenied do  |exception|
     flash[:error] = "Access Denied! You don't have permission to execute this action.
     Contact the system administrator"
     redirect_to admin_url
-  end
-
-  helper_method :current_liquidation
-  def current_liquidation
-    LiquidationService.active
-  end
-
-  helper_method :show_current_liquidation?
-  def show_current_liquidation?
-    current_liquidation.try(:visible?)
-  end
-
-  helper_method :show_current_liquidation_advertise?
-  def show_current_liquidation_advertise?
-    current_liquidation.try(:show_advertise?)
-  end
-
-  #TODO: create CartBuilder
-  helper_method :current_cart
-  def current_cart
-    #ORDER_ID IN PARAMS BECAUSE HAVE EMAIL SEND IN PAST
-    cart_id_session = session[:cart_id]
-    cart_id_params = params[:cart_id]
-    cart_id_legacy = params[:order_id]
-
-    cart = @user.carts.find_by_id(cart_id_params)  if @user && cart_id_params
-    cart = @user.carts.find_by_legacy_id(cart_id_legacy)  if @user && cart_id_legacy
-
-    cart ||= Cart.find_by_id(cart_id_session)
-    cart ||= Cart.create(user: @user)
-
-    session[:cart_id] = cart.id
-    #not sending email in the case of a buy made from an admin
-    if current_admin
-      cart.update_attribute("notified", true)
-    end
-
-    if @user
-      cart.update_attribute("user_id", @user.id) if cart.user.nil?
-    end
-
-    @promotion = PromotionService.new(@user).detect_current_promotion
-
-    coupon = session[:cart_coupon]
-    coupon.reload if coupon
-
-    @cart_service = CartService.new(
-      :cart => cart,
-      :coupon => coupon,
-      :promotion => @promotion,
-      :freight => session[:cart_freight],
-      :credits => session[:cart_use_credits]
-    )
-
-    cart
-  end
-
-  helper_method :current_moment
-  def current_moment
-    Moment.active.first
   end
 
   def render_public_exception
@@ -84,67 +35,127 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  #TODO: please create RefererBuilder
-  helper_method :current_referer
-  def current_referer
-    session[:return_to] = case request.referer
-      when /produto|sacola/ then
-        session[:return_to] ? session[:return_to] : nil
-      when /colecoes/ then
-        { text: "Voltar para coleções", url: moments_path }
-      when /suggestions/ then
-        session[:recipient_id] ? { text: "Voltar para as sugestões", url: gift_recipient_suggestions_path(session[:recipient_id]) } : nil
-      when /gift/ then
-        { text: "Voltar para presentes", url: gift_root_path }
-      else
-        nil
-    end
-
-    if @cart.has_gift_items?
-      session[:return_to] ||= { text: "Voltar para as sugestões", url: gift_recipient_suggestions_path(session[:recipient_id]) }
-    elsif @user && !@user.half_user?
-      session[:return_to] ||= { text: "Voltar para a minha vitrine", url: member_showroom_path }
-    else
-      session[:return_to] ||= { text: "Voltar para tendências", url: lookbooks_path }
-    end
-  end
-
   private
-  def load_facebook_api
-    @facebook_app_id = FACEBOOK_CONFIG["app_id"]
-  end
 
-  def load_referer
-    @referer = current_referer
-  end
+    def current_liquidation
+      LiquidationService.active
+    end
 
-  def load_user
-    @user = current_user
-  end
+    def show_current_liquidation?
+      current_liquidation.try(:visible?)
+    end
 
-  def load_cart
-    @cart = current_cart
-  end
+    def show_current_liquidation_advertise?
+      current_liquidation.try(:show_advertise?)
+    end
 
-  def current_ability
-    @current_ability ||= ::Ability.new(current_admin)
-  end
-  
-  def logged_in?
-    current_user
-  end
+    def current_cart
+      #ORDER_ID IN PARAMS BECAUSE HAVE EMAIL SEND IN PAST
+      cart_id_session = session[:cart_id]
+      cart_id_params = params[:cart_id]
+      cart_id_legacy = params[:order_id]
 
-  def load_tracking_parameters
-    incoming_params = params.clone.delete_if {|key| ['controller', 'action'].include?(key) }
-    incoming_params[:referer] = request.referer unless request.referer.nil?
-    session[:tracking_params] = incoming_params if session[:tracking_params].nil? || session[:tracking_params].empty?
-    session[:order_tracking_params] = incoming_params if incoming_params.has_key?("utm_source") || external_referer?(request.referer)
-  end
+      cart = @user.carts.find_by_id(cart_id_params)  if @user && cart_id_params
+      cart = @user.carts.find_by_legacy_id(cart_id_legacy)  if @user && cart_id_legacy
 
-  def external_referer?(referer)
-    return false if referer.nil?
-    !(referer =~ /olook\.com\.br/)
-  end
+      cart ||= Cart.find_by_id(cart_id_session)
+      cart ||= Cart.create(user: @user)
+
+      session[:cart_id] = cart.id
+
+      #not sending email in the case of a buy made from an admin
+      if current_admin
+        cart.update_attribute("notified", true)
+      end
+
+      if @user
+        cart.update_attribute("user_id", @user.id) if cart.user.nil?
+      end
+
+      cart
+    end
+
+    def current_moment
+      Moment.active.first
+    end
+
+    def current_referer
+      session[:return_to] = case request.referer
+        when /produto|sacola/ then
+          session[:return_to] ? session[:return_to] : nil
+        when /colecoes/ then
+          { text: "Voltar para coleções", url: moments_path }
+        when /suggestions/ then
+          session[:recipient_id] ? { text: "Voltar para as sugestões", url: gift_recipient_suggestions_path(session[:recipient_id]) } : nil
+        when /gift/ then
+          { text: "Voltar para presentes", url: gift_root_path }
+        else
+          nil
+      end
+
+      if @cart.has_gift_items?
+        session[:return_to] ||= { text: "Voltar para as sugestões", url: gift_recipient_suggestions_path(session[:recipient_id]) }
+      elsif @user && !@user.half_user?
+        session[:return_to] ||= { text: "Voltar para a minha vitrine", url: member_showroom_path }
+      else
+        session[:return_to] ||= { text: "Voltar para tendências", url: lookbooks_path }
+      end
+    end
+
+    def load_coupon
+      @coupon = session[:cart_coupon]
+      @coupon.reload if coupon
+    end
+
+    def load_cart_service
+      @cart_service = CartService.new(
+        :cart => @cart,
+        :coupon => @coupon,
+        :promotion => @promotion,
+        :freight => session[:cart_freight],
+        :credits => session[:cart_use_credits]
+      )
+    end
+
+    def load_promotion
+      @promotion = PromotionService.new(@user).detect_current_promotion
+    end
+
+    def load_facebook_api
+      @facebook_app_id = FACEBOOK_CONFIG["app_id"]
+    end
+
+    def load_referer
+      @referer = current_referer
+    end
+
+    def load_user
+      @user = current_user
+    end
+
+    def load_cart
+      @cart = current_cart
+    end
+
+    def current_ability
+      @current_ability ||= ::Ability.new(current_admin)
+    end
+    
+    def logged_in?
+      current_user
+    end
+
+    def load_tracking_parameters
+      incoming_params = params.clone.delete_if {|key| ['controller', 'action'].include?(key) }
+      incoming_params[:referer] = request.referer unless request.referer.nil?
+      session[:tracking_params] = incoming_params if session[:tracking_params].nil? || session[:tracking_params].empty?
+      session[:order_tracking_params] = incoming_params if incoming_params.has_key?("utm_source") || external_referer?(request.referer)
+    end
+
+    def external_referer?(referer)
+      return false if referer.nil?
+      !(referer =~ /olook\.com\.br/)
+    end
 
 end
 
