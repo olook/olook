b0ecc7c822e60a754accb6b6155671a5e69b94e2
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-30 23:17:53 -0200

    cart::items_controller#destroy specs passing

--- spec/controllers/cart/items_controller_spec.rb
+++ spec/controllers/cart/items_controller_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 
 describe Cart::ItemsController do 
 	
-	describe "#create", js: true do 		
+	describe "POST #create", js: true do 		
 		context "with valid variant params" do 
 			let(:basic_bag) { FactoryGirl.create(:basic_bag_simple) }
 			let(:params) { {variant: { id: basic_bag.id }} }
@@ -24,10 +24,42 @@ describe Cart::ItemsController do
       end
 		end
 
+		context "with invalid params" do  
+      it "should render error in response for js" do
+        post :create, :format=> :js
+        response.should render_template ["error"]
+      end
+    end
+	end  
+
+	describe "DELETE #destroy", js: true do
+		context "with valid params" do
+			let(:cart) { FactoryGirl.create(:clean_cart) }
+			let(:item) { FactoryGirl.build(:cart_item) }
+			
+			before(:each) do 
+				Cart::ItemsController.any_instance.stub(:current_cart).and_return(cart) 
+				cart.items << item
+			end
+			
+			it "removes the cart item from the cart" do				
+				Cart.first.items.count.should == 1
+				expect {
+					delete :destroy, { :id => item.id.to_s }, :format=> :js
+				}.to change(Cart.first.items, :count).by(-1)
+			end
+
+			it "destroys the cart item itself" do
+				CartItem.count.should == 1
+				expect {
+					delete :destroy, { :id => item.id.to_s }, :format=> :js
+				}.to change(CartItem, :count).by(-1)
+			end
+		end
+
 		context "with invalid params" do
       it "should render error in response for js" do
-        request.env['HTTP_ACCEPT'] = "text/javascript"
-        post :create, :format=> :js
+        delete :destroy, { :id => '' }, :format=> :js
         response.should render_template ["error"]
       end
     end
