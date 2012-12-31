72919dd61055029d2fed391a949d9c3f42d3a501
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2012-12-30 23:42:56 -0200

    WIP cart items delete feature tesing. need to check what the desired flow will be

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -2,6 +2,12 @@
 FactoryGirl.define do
   factory :clean_cart, :class => Cart do
 
+    factory :cart_with_one_item do
+      after_create do |cart|
+        FactoryGirl.create(:cart_item, :quantity => 1, :cart => cart)
+      end
+    end
+
     factory :cart_with_items do
       after_create do |cart|
         FactoryGirl.create(:cart_item, :cart => cart)
