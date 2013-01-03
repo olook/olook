7f10e55b1b1b3e363f18537327df7af8c65ff696
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-03 14:54:10 -0200

    creating helper method to choose promotion

--- app/controllers/checkout/base_controller.rb
+++ app/controllers/checkout/base_controller.rb
@@ -2,41 +2,46 @@
 class Checkout::BaseController < ApplicationController
   layout "checkout"
   respond_to :html
-  
+
   private
   def erase_freight
     @cart_service.freight = nil
   end
-  
+
   def check_freight
     redirect_to cart_checkout_addresses_path, :notice => "Escolha seu endereço" if @cart_service.freight.nil?
   end
-  
+
   def check_cpf
     redirect_to new_cart_checkout_path, :notice => "Informe seu CPF" unless Cpf.new(@user.cpf).valido?
   end
-  
+
   def check_order
     return redirect_to cart_path, :notice => "Sua sacola está vazia" if @cart.nil?
 
     @cart.remove_unavailable_items
     @cart.reload
-    
+
     #TODO: Produtos com o baixa no estoque foram removidos de sua sacola
     return redirect_to cart_path, :notice => "Sua sacola está vazia" if @cart.items.empty?
-    
+
     coupon = @cart_service.coupon
     if coupon && (coupon.try(:expired?) || !coupon.try(:available?))
       session[:cart_coupon] = nil
       return redirect_to cart_path, :notice => "Cupom expirado. Informe outro por favor"
     end
   end
-  
+
   def clean_cart!
     session[:cart_id] = nil
     session[:gift_wrap] = nil
     session[:cart_coupon] = nil
     session[:cart_use_credits] = nil
     session[:cart_freight] = nil
+  end
+
+  def fire_event
+    #TODO call notification to verify what's the better promotion
+    #ActiveSupport::Notifications.instrument()
   end
 end
