6425cd748ff345c89067dcfeb8945df48b610772
darkseid <rafael.manoel@gmail.com>
2013-01-06 13:46:17 -0200

    finish Parameter module implementation

--- lib/parameters.rb
+++ lib/parameters.rb
@@ -1,30 +1,64 @@
 # -*- encoding : utf-8 -*-
 module Parameters
 
-  def params
-    raise "Override this method!"
+  attr_accessor :params
+
+  def self.included(base)
+    base.extend(ClassMethods)
   end
 
+  def convert_to value, type 
 
+    if type == :decimal
+      converted_value = BigDecimal.new(value) 
+    elsif type == :integer
+      converted_value = value.to_i
+    else
+      converted_value = value
+    end
+
+    converted_value
+  end
+
+  #
+  # The idea behind this module is to provide a simple way to serialize / deserialize
+  # field values (parameter) to store it in a database. To use it, include this module 
+  # and declare your parameters:
+  # 
+  # parameter :field_name, :field_type.
+  #
+  # For instance only :decimal, :integer and :string are supported. And the default is
+  # :string
+  #
   module ClassMethods
 
     PARAMETER_SUPPORTED_TYPES = [:decimal, :string, :integer]
 
     def parameter parameter_name, parameter_type
-      raise "invalid parameter type" unless PARAMETER_SUPPORTED_TYPES.include? type
-      type = parameter_type.to_sym
 
-      define_method parameter_name.to_sym do
-        value = params[parameter_name.to_sym][:value]
+      name, type = validate_parameter_name_and_type parameter_name, parameter_type
 
-        type == :decimal ? BigDecimal.new(value) : value
+      define_method name do
+        value = params[name][:value]
+        convert_to value, type
       end
 
       define_method "#{parameter_name}=".to_sym do |value|
-        params[parameter_name.to_sym][:value] = value.to_s
-        params[parameter_name.to_sym][:type] = type
+        params[name][:value] = value.to_s
+        params[name][:type] = type
       end
 
     end
+
+
+    private
+
+      def validate_parameter_name_and_type parameter_name, parameter_type
+        type = parameter_type.to_sym
+        raise "invalid parameter type" unless PARAMETER_SUPPORTED_TYPES.include? type
+        parameter = parameter_name.to_sym
+        [parameter, type]
+      end
+
   end
 end
