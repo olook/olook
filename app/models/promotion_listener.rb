51f200e171df9d51995c4ac5f4926b2c9e41a838
darkseid <rafael.manoel@gmail.com>
2013-01-07 18:03:35 -0200

    small refactor

--- app/models/promotion_listener.rb
+++ app/models/promotion_listener.rb
@@ -2,6 +2,11 @@
 class PromotionListener
 
   def self.update attributes
+    cart = attributes[:cart]
+
+    # TODO after merge with restful_checkout, we can user cart.coupon to check if the cart 
+    # has coupon and avoid giving promotion discount
+
     matched_promotions = []
 
     Promotion.active_and_not_expired(Date.current).each do |promotion|
@@ -16,9 +21,9 @@ class PromotionListener
     end
 
     # TODO choose the best promotion for the user, and apply it
-    matched_promotions.each { |promotion| promotion.apply(attributes[:cart]) }
+    matched_promotions.each { |promotion| promotion.apply(cart) }
 
-    Rails.logger.info "Applied promotions: #{matched_promotions} for cart [#{attributes[:cart].id}]"
+    Rails.logger.info "Applied promotions: #{matched_promotions} for cart [#{cart.id}]"
 
   end
 
