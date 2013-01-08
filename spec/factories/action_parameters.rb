f3ee2a807a5bb7b334a4d35888d78706d340ef18
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-08 15:25:28 -0200

    fixing model name

--- spec/factories/action_parameters.rb
+++ spec/factories/action_parameters.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :action_parameter, :class => 'ActionParameters' do
+  factory :action_parameter do
     promotion_id 1
     promotion_action_id 1
     param "MyString"
