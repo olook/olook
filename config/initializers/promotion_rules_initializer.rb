57783360c08674fdb93bc3457a22586508d777e8
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-08 17:16:33 -0200

    adding new folder in initializers

--- config/initializers/promotion_rules_initializer.rb
+++ config/initializers/promotion_rules_initializer.rb
@@ -3,7 +3,7 @@
 #
 # Force Load all rules definitions
 #
-rb_files = File.join( Rails.root, 'app', 'models', 'rules', '*.rb' )
+rb_files = File.join( Rails.root, 'app', 'models', 'promotions','rules', '*.rb' )
 Dir.glob( rb_files ).each do |file|
   require file.gsub('.rb', '')
 end
