d76e17af4db83cba8770822c2eff34ff958b3b58
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-08 18:02:35 -0200

    using as parameters real objects

--- app/listeners/promotion_listener.rb
+++ app/listeners/promotion_listener.rb
@@ -1,22 +1,20 @@
 # -*- encoding : utf-8 -*-
 class PromotionListener
 
-  def self.update attributes
-    cart = attributes[:cart]
+  def self.update cart
 
-    # TODO after merge with restful_checkout, we can user cart.coupon to check if the cart 
+    # TODO after merge with restful_checkout, we can user cart.coupon to check if the cart
     # has coupon and avoid giving promotion discount
 
     matched_promotions = []
 
-    Promotion.active_and_not_expired(Date.current).each do |promotion|
+    Promotion.active_and_not_expired(DateTime.now).each do |promotion|
 
       # TODO is the promotion valid and active ?
-
-      matched_all_rules = promotion.promotion_rules.inject(true) do | match_result, rule | 
-        match_result &&= rule.matches?(attributes)
+      matched_all_rules = promotion.promotion_rules.inject(true) do | match_result, rule |
+        match_result &&= rule.matches?(cart.user)
       end
-  
+
       matched_promotions << promotion if matched_all_rules
     end
 
