3d3f3fe122eac4e3f9459b1dd6648e8bc4e398dd
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-08 18:43:14 -0200

    calculating promotion

--- app/models/promotions/actions/create_adjustment.rb
+++ app/models/promotions/actions/create_adjustment.rb
@@ -2,11 +2,12 @@
 
 class CreateAdjustment < PromotionAction
   def self.apply(cart, promotion)
-    binding.pry
     cart.items.each do |cart_item|
       sub_total = cart_item.quantity * cart_item.price
-      adjust = sub_total * BigDecimal(promotion.param)
-      cart_item.adjustment.update_attributes(:value, adjust)
+      # TODO get params of rules params
+      #adjust = sub_total * BigDecimal(promotion.rules_parameters.first.params)
+      adjust = sub_total * BigDecimal("0.2")
+      cart_item.adjustment.update_attribute(:value, adjust)
     end
   end
 end
