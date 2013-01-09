927218f8508e47eda1b5b17124f33c11ea37791d
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-09 14:49:46 -0200

    removing module

--- app/models/promotions/actions/create_adjustment.rb
+++ app/models/promotions/actions/create_adjustment.rb
@@ -1,12 +1,10 @@
 # -*- encoding : utf-8 -*-
-module Promo
-  class CreateAdjustment < PromotionAction
-    def self.apply(cart, promotion)
-      cart.items.each do |cart_item|
-        sub_total = cart_item.quantity * cart_item.price
-        adjust = sub_total * BigDecimal(promotion.action_parameter.param)
-        cart_item.adjustment.update_attribute(:value, adjust)
-      end
+class CreateAdjustment < PromotionAction
+  def self.apply(cart, promotion)
+    cart.items.each do |cart_item|
+      sub_total = cart_item.quantity * cart_item.price
+      adjust = sub_total * BigDecimal(promotion.action_parameter.param)
+      cart_item.adjustment.update_attribute(:value, adjust)
     end
   end
 end
