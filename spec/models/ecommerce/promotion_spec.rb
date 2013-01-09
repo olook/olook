8c89ddf263f16c4491c9a0e28041560f466d8e46
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-09 16:14:00 -0200

    refactoring

--- spec/models/ecommerce/promotion_spec.rb
+++ spec/models/ecommerce/promotion_spec.rb
@@ -5,6 +5,7 @@ describe Promotion do
   let(:promo) { FactoryGirl.create(:first_time_buyers) }
   describe "#validations" do
 
+
     it { should have_many :rules_parameters }
     it { should have_many(:promotion_rules).through(:rules_parameters)}
 
@@ -20,8 +21,17 @@ describe Promotion do
 
   describe "#apply" do
     let(:cart) { FactoryGirl.create(:cart_with_items) }
-    it "returns true" do
-      promo.apply(cart).should be_true
+    let(:action_parameter) { FactoryGirl.build(:action_parameter) }
+
+    context "returns true" do
+      it "when promotion has no action parameter" do
+        promo.apply(cart).should be_true
+      end
+
+      it "when promotion has action parameter with param" do
+        promo.should_receive(:action_parameter).twice.and_return(action_parameter)
+        promo.apply(cart).should be_true
+      end
     end
   end
 end
