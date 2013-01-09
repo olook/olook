13130b252c029c3a5abbd115af24216b6a9cebaf
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-09 14:49:46 -0200

    removing module

--- app/models/promotions/rules/first_buy.rb
+++ app/models/promotions/rules/first_buy.rb
@@ -1,11 +1,9 @@
 # -*- encoding : utf-8 -*-
-module Promo
-  class FirstBuy < PromotionRule
+class FirstBuy < PromotionRule
 
-    def matches? user
-      return false if user.nil?
+  def matches? user
+    return false if user.nil?
 
-      return ! (user.has_purchased_orders? || DiscountExpirationCheckService.discount_expired?(user))
-    end
+    return ! (user.has_purchased_orders? || DiscountExpirationCheckService.discount_expired?(user))
   end
 end
