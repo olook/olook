afc56fd084a271fd3e560eba6b823450828b741c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-09 11:54:31 -0200

    testing promotion

--- spec/models/ecommerce/promotion_spec.rb
+++ spec/models/ecommerce/promotion_spec.rb
@@ -2,21 +2,26 @@ require 'spec_helper'
 
 describe Promotion do
 
-  describe "validations" do
+  let(:promo) { FactoryGirl.create(:first_time_buyers) }
+  describe "#validations" do
 
     it { should have_many :rules_parameters }
     it { should have_many(:promotion_rules).through(:rules_parameters)}
 
     it { should have_one :action_parameter }
     it { should have_one(:promotion_action).through(:action_parameter)}
-    # it { should have_many :promotion_actions }
-
   end
 
-  describe "strategies" do
+  describe "#load_strategy" do
     it "should apply the appropriated strategy" do
-      promo = FactoryGirl.create(:first_time_buyers)
       promo.load_strategy(nil, nil).class.should  eq(Promotions::PurchasesAmountStrategy)
+    end
+  end
+
+  describe "#apply" do
+    let(:cart) { FactoryGirl.create(:cart_with_items) }
+    it "returns true" do
+      promo.apply(cart).should be_true
     end
   end
 end
