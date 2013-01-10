97383e7640c15687d6979926e6aa7df4ceef7e95
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-10 17:06:27 -0200

    Add method for set utms

--- app/models/campaign_email.rb
+++ app/models/campaign_email.rb
@@ -1,8 +1,15 @@
 class CampaignEmail < ActiveRecord::Base
-	after_create :enqueue_notification
+  after_create :enqueue_notification
 
-	def enqueue_notification
-		Resque.enqueue(CampaignEmailNotificationWorker, self.email)
-	end
+  def enqueue_notification
+	Resque.enqueue(CampaignEmailNotificationWorker, self.email)
+  end
+
+  def set_utm_info utm_info
+      self.update_attributes({utm_source: utm_info.fetch(:utm_source, nil),
+                              utm_medium: utm_info.fetch(:utm_medium, nil),
+                              utm_campaign: utm_info.fetch(:utm_campaign, nil),
+                              utm_content: utm_info.fetch(:utm_content, nil)})
+  end
 
 end
