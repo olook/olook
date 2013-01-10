8641e72c38eee7ee91dad8766eb19ed728e5cf4d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-10 17:08:18 -0200

    Remove campaign name field

--- spec/models/campaign_email_spec.rb
+++ spec/models/campaign_email_spec.rb
@@ -4,6 +4,7 @@ describe CampaignEmail do
 
    let(:campaign_email) {FactoryGirl.create(:campaign_email, email: 'meu@email.com')}
    let(:user) {FactoryGirl.create(:user, email: 'meu@email.com')}
+   let(:utm_params) {{utm_source: "source", utm_medium: "medium", utm_content: "content", utm_campaign: "campaign"}}
 
   describe "after create" do
 	it "enqueues a CampaignEmailNotificationWorker in resque" do
@@ -11,17 +12,19 @@ describe CampaignEmail do
       campaign_email
     end
 
-    it "should set turned_user to true on creation" do
-      campaign_email.turned_user.should be_false
+    it "converted_user should be false on creation" do
+      campaign_email.converted_user.should be_false
     end
   end
 
-  describe "on user creation" do
-    it "should set turned_user to true" do
-      campaign_email
-      user
+  describe "on presence of utm params" do
+    it "#set_utm_info" do
+      campaign_email.set_utm_info utm_params
       campaign_email.reload
-      campaign_email.turned_user.should be_true
+      campaign_email.utm_medium.should eq("medium")
+      campaign_email.utm_source.should eq("source")
+      campaign_email.utm_campaign.should eq("campaign")
+      campaign_email.utm_content.should eq("content")
     end
   end
 end
