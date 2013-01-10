129afccd291e9529cfe00abef7b9afd5683bb824
Matheus Bodo <mcbodo@gmail.com>
2013-01-10 17:24:28 -0200

    refactoring create method

--- app/controllers/checkout/checkout_controller.rb
+++ app/controllers/checkout/checkout_controller.rb
@@ -29,17 +29,38 @@ class Checkout::CheckoutController < Checkout::BaseController
   end
 
   def create
-    params[:checkout][:address][:country] = 'BRA' if params[:checkout][:address]
+    address = shipping_address
+    return unless address && address.valid?
+
+    redirect_to :new_cart_checkout
+  end
+
+  private
+
+  def shipping_address
+    if params[:checkout][:address]
+      address = populate_shipping_address
+      display_form(address, CreditCard.new) unless address.save
+      address
+    else
+      address = Address.find(params[:address][:id]) if params[:address]
+      display_form(Address.new, CreditCard.new) unless address
+      address
+    end
+  end
+
+  def populate_shipping_address
+    params[:checkout][:address][:country] = 'BRA'
     address = params[:checkout][:address][:id].empty? ? @user.addresses.build() : @user.addresses.find(params[:checkout][:address][:id])
     params[:checkout][:address].delete(:id)
-    address.assign_attributes(params[:checkout][:address])      
+    address.assign_attributes(params[:checkout][:address])
+    address
+  end
 
-    if address.save
-      redirect_to :new_cart_checkout
-    else
-      @checkout = Checkout.new(address: address)
-      render :new
-    end
+  def display_form(address, payment)
+    @addresses = @user.addresses
+    @checkout = Checkout.new(address: address, payment: payment)
+    render :new
   end
 
 end
