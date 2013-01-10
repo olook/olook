576101d1bad306b1c1d029b0c24d0a014e022ff0
Luis Daher <luisotaviodaher@gmail.com>
2013-01-10 19:33:47 -0200

    WIP - freight price needs to be shown

--- app/controllers/zipcode_lookup_controller.rb
+++ app/controllers/zipcode_lookup_controller.rb
@@ -8,8 +8,13 @@ class ZipcodeLookupController < ApplicationController
   end
 
   def address_data
-    @result = ZipCodeAdapter.get_address(params[:zip_code])
-    @freight = FreightCalculator.freight_for_zip params[:zip_code], @cart_service.subtotal
+    result = ZipCodeAdapter.get_address(params[:zip_code])
+    freight = FreightCalculator.freight_for_zip params[:zip_code], @cart_service.subtotal
+    @address = @user.addresses.build(:first_name => @user.first_name, :last_name => @user.last_name)
+    @address.zip_code = params[:zip_code]
+    result.delete(:result_type)
+    @freight_price = freight[:price]
+    @address.assign_attributes result
   end
 
 end
