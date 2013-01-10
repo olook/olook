8fc12a668d13fe5230cd72a7a4a0b29e9d1e541d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-10 09:06:15 -0200

    Add initial test for new field on campaign email

--- spec/models/campaign_email_spec.rb
+++ spec/models/campaign_email_spec.rb
@@ -1,10 +1,17 @@
 require 'spec_helper'
 
 describe CampaignEmail do
-	describe "after create" do
-		it "enqueues a CampaignEmailNotificationWorker in resque" do
+
+   let(:campaign_email) {FactoryGirl.create(:campaign_email, email: 'meu@email.com')}
+
+  describe "after create" do
+	it "enqueues a CampaignEmailNotificationWorker in resque" do
       Resque.should_receive(:enqueue).with(CampaignEmailNotificationWorker, 'meu@email.com')
-      FactoryGirl.create(:campaign_email, email: 'meu@email.com')
+      campaign_email
+    end
+
+    it "should set turned_user to true" do
+      campaign_email.turned_user.should be_false
     end
   end
 end
