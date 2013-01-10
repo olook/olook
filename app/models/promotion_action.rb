b7a9272b91bc8b396462401389774a048db10b8d
darkseid <rafael.manoel@gmail.com>
2013-01-10 11:41:47 -0200

    small refactor

--- app/models/promotion_action.rb
+++ app/models/promotion_action.rb
@@ -1,5 +1,15 @@
 class PromotionAction < ActiveRecord::Base
+  include Parameters
+
   has_many :action_parameters
   has_many :promotions, through: :action_parameters
+
+  def params
+    promotion.action_parameter.param
+  end
+
+  def params= value
+    promotion.action_parameter.param = value
+  end
 
 end
