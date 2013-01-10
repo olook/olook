8293e979f93f2929a3de7579bdace9fae7893dd3
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-10 16:37:31 -0200

    fixing tests, relations and columns

--- app/models/ecommerce/promotion.rb
+++ app/models/ecommerce/promotion.rb
@@ -6,17 +6,19 @@ class Promotion < ActiveRecord::Base
 
   has_many :promotion_payments
 
-  has_many :rules_parameters
-  has_many :promotion_rules, :through => :rules_parameters
+  has_many :rule_parameters
+  has_many :promotion_rules, :through => :rule_parameters
 
   has_one :action_parameter
   has_one :promotion_action, through: :action_parameter
 
-  accepts_nested_attributes_for :rules_parameters, :action_parameter
+  accepts_nested_attributes_for :rule_parameters, :action_parameter
 
   def apply cart
+
     promotion_action.apply cart, self
     # CreateAdjustment.apply(cart, self)
   end
+
 
 end
