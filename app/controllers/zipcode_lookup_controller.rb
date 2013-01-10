31e3299a91cd012a2cc2375920fd8c1b0e5b498c
Luis Daher <luisotaviodaher@gmail.com>
2013-01-10 14:50:35 -0200

    WIP - zipcode lookup

--- app/controllers/zipcode_lookup_controller.rb
+++ app/controllers/zipcode_lookup_controller.rb
@@ -1,8 +1,15 @@
 class ZipcodeLookupController < ApplicationController
 
+  respond_to :json, :js
+
   def get_address_by_zipcode
     result = ZipCodeAdapter.get_address(params[:zipcode])
     render json: result
+  end
+
+  def address_data
+    @result = ZipCodeAdapter.get_address(params[:zip_code])
+    @freight = FreightCalculator.freight_for_zip params[:zip_code], @cart_service.subtotal
   end
 
 end
