da0775a3e662c720db1511634d00113625d48cfb
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-11 11:03:42 -0200

    inserting begin

--- app/models/promotion_action.rb
+++ app/models/promotion_action.rb
@@ -10,11 +10,14 @@ class PromotionAction < ActiveRecord::Base
 # end
 
   def self.inherited(base)
+    begin
     super base
     # register the inherited class (base) in the database if it is not there yet.
     # this is done in order to avoid manual insert into database whenever we create a
     # new promotion_rule
     Rails.logger.info "inserting a new PromotionAction #{base.name} into database"
     where(:type => base.name).first_or_create({:type => base.name})
+    rescue
+    end
   end
 end
--- app/models/promotion_rule.rb
+++ app/models/promotion_rule.rb
@@ -19,12 +19,15 @@ class PromotionRule < ActiveRecord::Base
   end
 
   def self.inherited(base)
+    begin
     super base
     # register the inherited class (base) in the database if it is not there yet.
     # this is done in order to avoid manual insert into database whenever we create a
     # new promotion_rule
     Rails.logger.info "inserting a new PromotionRule #{base.name} into database"
     where(:type => base.name).first_or_create({:name => base.name})
+    rescue
+    end
   end
 
 end
