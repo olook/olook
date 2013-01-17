98cec2f418912c2d1b346e3fcf1c301188575853
darkseid <rafael.manoel@gmail.com>
2013-01-17 14:38:07 -0200

    small refactor

--- app/listeners/promotion_listener.rb
+++ app/listeners/promotion_listener.rb
@@ -3,12 +3,21 @@ class PromotionListener
 
   def self.update cart
     reset_adjustments_for cart
+    # TODO[galeto] => these two methods could be merged into one (inside promotion class).
     promotions_to_apply = Promotion.matched_promotions_for cart
-    promotion = Promotion.best_promotion_for(cart, promotions_to_apply)
-    promotion.apply(cart) if promotion && promotion.should_apply_for?(cart)
+    best_promotion = Promotion.best_promotion_for(cart, promotions_to_apply)
+    apply best_promotion, cart
   end
 
   def self.reset_adjustments_for cart
     cart.items.each { |item| item.cart_item_adjustment.update_attributes(value: 0, source: nil) }
   end
+
+  private 
+
+    def self.apply promotion, cart
+      return if promotion.nil?
+      promotion.apply(cart) if promotion.should_apply_for?(cart)
+    end
+
 end
