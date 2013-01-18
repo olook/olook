a5889b2893aa9c58573cb48386de741a27a79e54
darkseid <rafael.manoel@gmail.com>
2013-01-18 14:18:08 -0200

    add back the 'add to cart' button

--- app/listeners/promotion_listener.rb
+++ app/listeners/promotion_listener.rb
@@ -3,25 +3,26 @@ class PromotionListener
 
   def self.update cart
     reset_adjustments_for cart
-    best_promotion = Promotion.select_promotion_for(cart)
-    apply best_promotion, cart
+    apply_best_promotion_for cart
   end
 
-  def self.reset_adjustments_for cart
-    cart.items.each { |item| item.cart_item_adjustment.update_attributes(value: 0, source: nil) }
-  end
-
+  # TODO[galeto] does this method really need to reset adjustments ?
   def self.should_apply_coupon?(cart, coupon)
     reset_adjustments_for cart
     best_promotion = Promotion.select_promotion_for(cart)
 
-  best_promotion ?  coupon.value > best_promotion.total_discount_for(cart) : true
+    best_promotion ?  coupon.value > best_promotion.total_discount_for(cart) : true
   end
+  
   private
 
-    def self.apply promotion, cart
-      return if promotion.nil?
-      promotion.apply(cart) if promotion.should_apply_for?(cart)
+    def self.reset_adjustments_for cart
+      cart.items.each { |item| item.cart_item_adjustment.update_attributes(value: 0, source: nil) }
+    end
+
+    def self.apply_best_promotion_for cart
+      best_promotion = Promotion.select_promotion_for(cart)
+      best_promotion.apply(cart) if best_promotion
     end
 
 end
