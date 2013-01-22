0484b7d53adc2c845b5b533f6a89aa3f349a2ee3
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-22 09:41:39 -0200

    Indenting code

--- spec/features/admin/operations_dashboard_spec.rb
+++ spec/features/admin/operations_dashboard_spec.rb
@@ -9,29 +9,29 @@ feature "Operations dashboard", %q{
 } do
 
 	scenario "Listing the orders by their dates and statuses" do
-		ApplicationController.any_instance.stub(:load_promotion)
+	  ApplicationController.any_instance.stub(:load_promotion)
 
-		@admin = FactoryGirl.create(:admin_superadministrator)
-	do_admin_login!(@admin)
+	  @admin = FactoryGirl.create(:admin_superadministrator)
+      do_admin_login!(@admin)
 
-	visit '/admin'
+      visit '/admin'
 
-	page.should have_content "Dashboard"
-	page.should have_content "Operações"
+      page.should have_content "Dashboard"
+      page.should have_content "Operações"
 
-	page.should have_content "Prazo para despacho"
-	page.should have_content "Pago"
-	page.should have_content "Aguardando Separação"
-	page.should have_content "Despachado"
-	page.should have_content "Entregue"
-	page.should have_content "Hoje"
-	page.should have_content "Ontem"
-	page.should have_content "2 dias atrás"
-	page.should have_content "3 dias atrás"
-	page.should have_content "4 dias atrás"
-	page.should have_content "5 dias atrás"
-	page.should have_content "6 ou mais dias atrás"
-	page.should have_content "TOTAL"
+      page.should have_content "Prazo para despacho"
+      page.should have_content "Pago"
+      page.should have_content "Aguardando Separação"
+      page.should have_content "Despachado"
+      page.should have_content "Entregue"
+      page.should have_content "Hoje"
+      page.should have_content "Ontem"
+      page.should have_content "2 dias atrás"
+      page.should have_content "3 dias atrás"
+      page.should have_content "4 dias atrás"
+      page.should have_content "5 dias atrás"
+      page.should have_content "6 ou mais dias atrás"
+      page.should have_content "TOTAL"
 
   end
 
