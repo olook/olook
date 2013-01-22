a7dd01456e0fbb0d3aa1a411491aaffdef880d0f
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-22 11:29:14 -0200

    verifying param of promotion and if promotion matches?

--- spec/factories/promotion_rules.rb
+++ spec/factories/promotion_rules.rb
@@ -1,8 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :promotion_rule do
-    name "MyString"
-    type ""
+  factory :promotion_free_item, class: FreeItem do
+    name "FreeItem"
+    type "FreeItem"
   end
 end
