d8dae1564360e4b435c6036508fbcdf9bffb2863
darkseid <rafael.manoel@gmail.com>
2013-01-22 11:34:38 -0200

    improve promotions admin page

--- app/models/promotion_rule.rb
+++ app/models/promotion_rule.rb
@@ -19,14 +19,15 @@ class PromotionRule < ActiveRecord::Base
   end
 
   def self.inherited(base)
-    begin
     super base
-    # register the inherited class (base) in the database if it is not there yet.
-    # this is done in order to avoid manual insert into database whenever we create a
-    # new promotion_rule
-    Rails.logger.info "inserting a new PromotionRule #{base.type} into database"
-    where(:type => base.type).first_or_create({:type => base.type})
-    rescue
+    begin
+      # register the inherited class (base) in the database if it is not there yet.
+      # this is done in order to avoid manual insert into database whenever we create a
+      # new promotion_rule
+      Rails.logger.info "inserting a new PromotionRule #{base.type} into database"
+      where(:type => base.name).first_or_create({:type => base.name})
+    rescue Exception => e
+      Rails.logger.error e
     end
   end
 
