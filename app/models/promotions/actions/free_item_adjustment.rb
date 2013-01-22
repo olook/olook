96c4715e2fbc3016a1a10122652599344dd7c835
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-22 14:26:05 -0200

    refactoring simulate and creating apply

--- app/models/promotions/actions/free_item_adjustment.rb
+++ app/models/promotions/actions/free_item_adjustment.rb
@@ -1,12 +1,19 @@
 # -*- encoding : utf-8 -*-
 class FreeItemAdjustment < PromotionAction
 
+  def apply(cart)
+    calculate(cart).each do |item|
+      item.cart_item_adjustment.update_attributes(value: item.retail_price)
+    end
+  end
+
   def simulate(cart)
-    cart.items.any? ? calculate(cart) : 0
+    cart.items.any? ? calculate(cart).sum(&:price) : 0
   end
 
   private
     def calculate(cart)
-      cart.items.sort_by { |item| item.price }.first.price
+      free_items = cart.items.size / 3 #TODO use promotion parameter
+      cart.items.sort_by { |item| item.price }.first(free_items)
     end
 end
