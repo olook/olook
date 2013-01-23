8ea4dffd487fe3a7602e695454ab75d8a4f3ed09
Matheus Augusto Bodo <mcbodo@gmail.com>
2013-01-23 13:56:49 -0200

    removing valid_association check because the algorithm is not guaranteed to be correct

--- app/validators/credit_card_number_validator.rb
+++ app/validators/credit_card_number_validator.rb
@@ -7,12 +7,11 @@ class CreditCardNumberValidator < ActiveModel::Validator
   private
   def valid_credit_card?(number)
 
+    return false unless number
     # Hypercard
     return true if number.length > 16
 
     number = number.to_s.gsub(/\D/, "")
-
-    return false unless valid_association?(number)
     number.reverse!
 
     relative_number = {'0' => 0, '1' => 2, '2' => 4, '3' => 6, '4' => 8, '5' => 1, '6' => 3, '7' => 5, '8' => 7, '9' => 9}
@@ -24,17 +23,6 @@ class CreditCardNumberValidator < ActiveModel::Validator
     end 
 
     sum % 10 == 0
-  end
-
-  def valid_association?(number)
-    number = number.to_s.gsub(/\D/, "") 
-
-    return :dinners  if number.length == 14 && number =~ /^3(0[0-5]|[68])/   # 300xxx-305xxx, 36xxxx, 38xxxx
-    return :amex     if number.length == 15 && number =~ /^3[47]/            # 34xxxx, 37xxxx
-    return :visa     if [13,16].include?(number.length) && number =~ /^4/    # 4xxxxx
-    return :master   if number.length == 16 && number =~ /^5[1-5]/           # 51xxxx-55xxxx
-    return :discover if number.length == 16 && number =~ /^6011/             # 6011xx
-    return nil
   end
 
 end
