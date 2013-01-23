a908a5d837fc57a778907b7939eac4ce702f77de
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-23 14:06:11 -0200

    Add show action for dashboard controller

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -9,4 +9,10 @@ class Admin::DashboardController < Admin::BaseController
   def show
 
   end
+
+  def show
+    day = params[:number]
+    @orders = Order.with_date((Time.now - day.to_i.days)).with_state(params[:state]).page(params[:page]).page(params[:page]).per_page(15).order('created_at desc')
+  end
 end
+
