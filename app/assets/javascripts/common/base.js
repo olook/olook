2359c397a20d7a22d214ff06543b788ddd4be488
Rafael Carvalho <rafa1402@gmail.com>
2013-01-23 17:28:12 -0200

    facebook popup

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -28,7 +28,25 @@ var LuhnCheck = (function()
 	}
 })();
 
+/* FACEBOOK POPUP */
+function popupCenter(url, width, height, name) {
+  var left = (screen.width/2)-(width/2), top = (screen.height/2)-(height/2), popupValue = "on";
+  return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
+}
+ 
+
 $(document).ready(function() {
+	
+	$("a.popup").click(function(e) {
+	  popupCenter($(this).attr("href"), $(this).attr("data-width"), $(this).attr("data-height"), "authPopup");
+	  e.stopPropagation(); return false;
+	});
+	if(window.opener && window.opener.popupValue === 'on') {
+	 delete window.opener.popupValue;
+	 window.opener.location.reload(true);
+	 window.close(); 
+	}
+	
   initBase.dialogLogin();
   initBase.loadJailImages();
   initBase.customSelect();
