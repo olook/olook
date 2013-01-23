93e8463067ee5c7af8ca8a1a68ceceb19d809350
darkseid <rafael.manoel@gmail.com>
2013-01-23 15:55:29 -0200

    fix spec

--- app/models/promotions/rules/cart_items_amount.rb
+++ app/models/promotions/rules/cart_items_amount.rb
@@ -2,6 +2,11 @@
 class CartItemsAmount < PromotionRule
 
   def matches?(promotion, cart)
-    cart.items.size % promotion.param_for(self) == 0
+    quantity_of_items(cart) % promotion.param_for(self) == 0
   end
+
+  private
+    def quantity_of_items cart
+      cart.items.inject(0) { |amount, item| amount += item.quantity }
+    end
 end
