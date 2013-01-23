cc0c5f9ef2d79903b26b34a01b38130129eb16a0
Rafael Carvalho <rafa1402@gmail.com>
2013-01-23 18:27:53 -0200

    facebook popup

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -33,7 +33,7 @@ function popupCenter(url, width, height, name) {
   var left = (screen.width/2)-(width/2), top = (screen.height/2)-(height/2), popupValue = "on";
   return window.open(url, name, "menubar=no,toolbar=no,status=no,width="+width+",height="+height+",toolbar=no,left="+left+",top="+top);
 }
- 
+  
 
 $(document).ready(function() {
 	
