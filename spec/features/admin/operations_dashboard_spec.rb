f0d8ffd27c171d3d3f9673d6d52fe05e1646f90d
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-23 20:50:11 -0200

    operation dashboard: details integration spec and tweaks to listing spec

--- spec/features/admin/operations_dashboard_spec.rb
+++ spec/features/admin/operations_dashboard_spec.rb
@@ -35,14 +35,41 @@ feature "Operations dashboard", %q{
     expect(page).to have_content("6 ou mais dias atrás")
     expect(page).to have_content("TOTAL")
 
+    expect(page.find('tr#0_dias td#total', text: '1'))
+
     expect(page).to have_css('#total_authorized', text: '1')
   end
 
   scenario "Viewing details for a list of orders" do
-    save_and_open_page
-    within('tr#0_dias') do
-      find('td.authorized a').first.click
-    end
+
+    page.find('tr#0_dias td#authorized a').click
+
+    expect(page).to have_content("Cadastro")
+    expect(page).to have_content("Pagamento")
+    expect(page).to have_content("Despacho Entrega")
+    expect(page).to have_content("Data prometida de Entrega")
+    expect(page).to have_content("Transportador")
+    expect(page).to have_content("Gateway de pagamento")
+    expect(page).to have_content("Cliente nome")
+    expect(page).to have_content("Cliente email")
+    expect(page).to have_content("Cidade")
+    expect(page).to have_content("Estado")
+    expect(page).to have_content("CEP")
+    expect(page).to have_content("Quantidade de itens")
+
+    expect(page).to have_content("Wednesday, 23 January 2013")
+    expect(page).to have_content("Wednesday, 23 January 2013")
+    #TODO: Despacho Entrega
+    #TODO: Data prometida de Entrega
+    expect(page).to have_content("TEX")
+    #TODO: Gateway de pagamento
+    expect(page).to have_content("José Ernesto")
+    expect(page).to have_content("jose.ernesto@dominio.com")
+    expect(page).to have_content("Rio de Janeiro")
+    expect(page).to have_content("RJ")
+    expect(page).to have_content("87656-908")
+    expect(page).to have_content("100.0")
+    expect(page).to have_content("0")
   end
 
 end
