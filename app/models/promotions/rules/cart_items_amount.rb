67d5f486f8a1c099f5d0a08e5e1f0dec67ca0d0c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-24 14:46:14 -0200

    using sum of quantity to match promotion

--- app/models/promotions/rules/cart_items_amount.rb
+++ app/models/promotions/rules/cart_items_amount.rb
@@ -2,6 +2,6 @@
 class CartItemsAmount < PromotionRule
 
   def matches?(cart, promotion)
-    cart.items.size % self.param_for(promotion) == 0
+    cart.items.sum(&:quantity) / self.param_for(promotion).to_i > 0
   end
 end
