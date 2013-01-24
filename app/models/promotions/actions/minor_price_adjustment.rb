3ef41cf4aa9bee4b06a0f641f5d07a3487fc870d
darkseid <rafael.manoel@gmail.com>
2013-01-24 19:24:11 -0200

    improve promotion actions, but it is not finished yet

--- app/models/promotions/actions/minor_price_adjustment.rb
+++ app/models/promotions/actions/minor_price_adjustment.rb
@@ -1,19 +1,30 @@
 # -*- encoding : utf-8 -*-
 class MinorPriceAdjustment < PromotionAction
 
-  def apply(cart, parameter)
-    calculate(cart, parameter).each do |item|
-      item.cart_item_adjustment.update_attributes(value: item.price)
+  def apply(cart, quantity)
+    calculate(cart, quantity).each do |hash|
+      item = hash[:item]
+      adjustment = hash[:adjustment]
+      item.cart_item_adjustment.update_attributes(value: adjustment)
     end
   end
 
-  def simulate(cart, parameter)
-    cart.items.any? ? calculate(cart).sum(&:price) : 0
+  def simulate(cart, quantity)
+    cart.items.any? ? calculate(cart, quantity).map{|hash| hash[:adjustment]}.reduce(:+) : 0
   end
 
   private
-    def calculate(cart, parameter)
-      free_items = cart.items.size / parameter
-      cart.items.sort_by { |item| item.price }.first(free_items)
+    def minor_price_items(cart, quantity)
+      sorted_cart_items = cart.items.sort_by { |cart_item| cart_item.price }
+      sorted_cart_items.first(quantity)
+    end
+
+
+    def calculate(cart, quantity)
+      calculated_adjustments = []
+      minor_price_items(cart, quantity).each do |item|
+        calculated_adjustments << {item: item, adjustment: item.price}
+      end
+      calculated_adjustments
     end
 end
