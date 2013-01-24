5bb92b166b19f5a97f7bb72140758e0d98fcb90e
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-24 10:45:56 -0200

    Add actions for two new reports page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
-    @report_days = [*0..6]
+
   end
 
   def show
@@ -12,6 +12,14 @@ class Admin::DashboardController < Admin::BaseController
   def show
     day = params[:number]
     @orders = Order.with_date((Time.now - day.to_i.days)).with_state(params[:state]).page(params[:page]).page(params[:page]).per_page(15).order('created_at desc')
+  end
+
+  def orders_life_cicle_report
+    @report_days = [*0..6]
+  end
+
+  def orders_time_report
+    @report_days = [*0..6]
   end
 end
 
