a79b1958bd6cbc86b7b16124e00e3414cc0d25de
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-24 10:00:19 -0200

    Remove unused instance variable

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,7 +3,6 @@ class Admin::DashboardController < Admin::BaseController
 
   def index
     @report_days = [*0..6]
-    @statuses = %w{authorized picking delivering delivered authorized,picking,delivering,delivered}.to_a
   end
 
   def show
