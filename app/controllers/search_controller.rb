4826cbf5fdfe91129c786d696152c831ab02726a
darkseid <rafael.manoel@gmail.com>
2013-01-26 16:45:19 -0200

    search engine: first version

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,10 +2,27 @@ class SearchController < ApplicationController
   respond_to :json
 
   def index
+<<<<<<< HEAD
   end
 
   def product_suggestions
     render json: ProductSearch.terms_for(params[:term]) if params[:term].length >= 3
   end
 
+=======
+
+  
+  end
+
+  def show
+  # url = "?q=#{params[:q]}&return-fields=categoria,name,description,text_relevance"
+    url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{params[:q]}&return-fields=categoria,name,description,text_relevance")
+    response = Net::HTTP.get_response(url)
+
+    @hits = JSON.parse(response.body)["hits"]
+
+  end
+
+
+>>>>>>> search engine: first version
 end
