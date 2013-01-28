1b0cf553bc7962e7d216af34db635e21eba9d233
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-28 17:27:52 -0200

    refactoring

--- app/models/promotions/rules/cart_items_total_value.rb
+++ app/models/promotions/rules/cart_items_total_value.rb
@@ -2,8 +2,7 @@
 class CartItemsTotalValue < PromotionRule
 
   def matches?(cart, parameter)
-    items_value = cart.items.inject(0) {|total, i| total += i.price * i.quantity}
-    items_value >= parameter
+    cart.total_price >= parameter
   end
 
 end
