eaff9b0474a40de6797622afb7cf6b01ba0267ab
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-28 14:18:17 -0200

    Add helper for deliver report

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -8,4 +8,15 @@ module Admin::DashboardHelper
     link_to(Order.with_date(number_day.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
   end
 
+  def report_deliver_link(number_day, options = {})
+    unless options[:state]
+      options[:state] = ["authorized","delivering","delivered"]
+    end
+    links_params = {number: number_day, state: options.fetch(:state)}
+    if number_day <= 3
+      link_to(Order.with_date(number_day.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    else
+      link_to(Order.with_date(number_day.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    end
+  end
 end
