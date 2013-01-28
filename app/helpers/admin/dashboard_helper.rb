2b9b80d65866b565061d0c3193da87a8b842ad87
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-28 14:41:19 -0200

    Add case on report_deliver_link

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -13,10 +13,21 @@ module Admin::DashboardHelper
       options[:state] = ["authorized","delivering","delivered"]
     end
     links_params = {number: number_day, state: options.fetch(:state)}
-    if number_day <= 3
-      link_to(Order.with_date(number_day.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
-    else
-      link_to(Order.with_date(number_day.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    case number_day
+    when number_day == 0
+      link_to(Order.with_date(3.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    when number_day == 1
+      link_to(Order.with_date(2.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    when number_day == 2
+      link_to(Order.with_date(1.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    when number_day == 3
+      link_to(Order.with_date(0.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    when number_day == 4
+      link_to(Order.with_date(1.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    when number_day == 5
+      link_to(Order.with_date(2.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    else number_day == 6
+      link_to(Order.with_date(3.business_days.after(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
     end
   end
 end
