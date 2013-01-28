104a3226daca0c697bffbda9bd9a44ad8d77560c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-28 16:47:24 -0200

    testing method matches?

--- app/models/promotions/rules/cart_items_total_value.rb
+++ app/models/promotions/rules/cart_items_total_value.rb
@@ -2,6 +2,8 @@
 class CartItemsTotalValue < PromotionRule
 
   def matches?(cart, parameter)
+    items_value = cart.items.inject(0) {|total, i| total += i.price * i.quantity}
+    items_value >= parameter
   end
 
 end
--- spec/models/promotions/rules/cart_items_total_value_spec.rb
+++ spec/models/promotions/rules/cart_items_total_value_spec.rb
@@ -2,5 +2,33 @@
 require 'spec_helper'
 
 describe CartItemsTotalValue do
+  describe "#matches" do
+    let(:cart) { FactoryGirl.create(:cart_with_items) }
 
+    it { should respond_to(:matches?).with(2).argument }
+
+    before :each do
+      cart.items.first.stub(:quantity).and_return(1)
+      cart.items.first.stub(:price).and_return(BigDecimal("100"))
+    end
+
+    context "when items total value is lower than parameter" do
+      it "returns false" do
+        subject.matches?(cart, BigDecimal("120")).should be_false
+      end
+    end
+
+    context "when items total value is greater than parameter" do
+      it "returns true" do
+        subject.matches?(cart, BigDecimal("50")).should be_true
+      end
+    end
+
+    context "when items total value is equal than parameter" do
+      it "returns true" do
+        subject.matches?(cart, BigDecimal("100")).should be_true
+      end
+    end
+
+  end
 end
