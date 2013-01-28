6793d804e5e77b1141b2e7a586a2c39042545e20
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-28 14:08:35 -0200

    Use business days on life cicle report

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -5,7 +5,7 @@ module Admin::DashboardHelper
       options[:state] = ["authorized","picking","delivering","delivered"]
     end
     links_params = {number: number_day, state: options.fetch(:state)}
-    link_to(Order.with_date(Time.now - number_day.days).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    link_to(Order.with_date(number_day.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
   end
 
 end
