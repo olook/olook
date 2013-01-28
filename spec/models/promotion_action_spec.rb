c7b48865213b0a04f0d0a8e907c16d1d080e8df1
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-28 17:57:27 -0200

    testing simulate

--- spec/models/promotion_action_spec.rb
+++ spec/models/promotion_action_spec.rb
@@ -10,4 +10,14 @@ describe PromotionAction do
     it { should respond_to(:calculate).with(2).arguments }
   end
 
+  describe "#simulate" do
+    let(:cart) { mock_model Cart, items: [] }
+
+    context "when cart has no items" do
+      it "returns zero" do
+        cart.items.should_receive(:any?).and_return(false)
+        subject.simulate(cart, 0).should eq(0)
+      end
+    end
+  end
 end
