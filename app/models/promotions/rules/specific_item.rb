a68813eb7adafea910142285914a49186cf2ef95
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-29 15:03:32 -0200

    creating method matches? for specific product rule

--- app/models/promotions/rules/specific_item.rb
+++ app/models/promotions/rules/specific_item.rb
@@ -1,8 +1,18 @@
 # -*- encoding : utf-8 -*-
 class SpecificItem < PromotionRule
 
-  def matches?(cart, parameter)
+  def matches?(cart_items, products_list)
+    (products_for(cart_items) & products_list).any?
   end
 
+  private
+
+    def products_for(cart_items)
+      products = []
+      cart_items.each do |item|
+        products << item.product.id
+      end
+      products
+    end
 end
 
