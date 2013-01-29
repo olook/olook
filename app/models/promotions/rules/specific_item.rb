b2e6d075c11244dce6a6a49d9756a5904a787fdd
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-29 14:19:03 -0200

    creating new rule

---
+++ app/models/promotions/rules/specific_item.rb
@@ -0,0 +1,8 @@
+# -*- encoding : utf-8 -*-
+class SpecificItem < PromotionRule
+
+  def matches?(cart, parameter)
+  end
+
+end
+
---
+++ spec/models/promotions/rules/specific_item_spec.rb
@@ -0,0 +1,10 @@
+# -*- encoding : utf-8 -*-
+require 'spec_helper'
+
+describe SpecificItem do
+
+  describe "#matches?" do
+    it { should respond_to(:matches?).with(2).arguments }
+  end
+end
+
