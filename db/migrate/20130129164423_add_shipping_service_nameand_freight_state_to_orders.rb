e2ac7020d26eda8bbcfc49cdd15a6b0d0cf5b37c
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-29 17:58:06 -0200

    operation dashboard: including new freight_state field in orders add_shipping_service_name migration

--- db/migrate/20130129164423_add_shipping_service_nameand_freight_state_to_orders.rb
+++ db/migrate/20130129164423_add_shipping_service_nameand_freight_state_to_orders.rb
@@ -2,5 +2,7 @@ class AddShippingServiceNameToOrders < ActiveRecord::Migration
   def change
     add_column :orders, :shipping_service_name, :string, :limit => 5
     add_index  :orders, :shipping_service_name
+    add_column :orders, :freight_state, :string, :limit => 3
+    add_index :orders, :freight_state
   end
 end
