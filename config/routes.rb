9fa4ab6655cf34f814807551a83d8f287ce4917d
darkseid <rafael.manoel@gmail.com>
2013-01-28 23:03:27 -0200

    add autocomplete and a suggestion words feature without cache yet

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Olook::Application.routes.draw do
 
   # Search Lab
   get "/search/show", :to => "search#show", :as => "search_show"
+  get "/search/q", :to => "search#q", :as => "search_query"
   get "/search", :to => "search#index", :as => "search_index"
   get "/search/product_suggestions", :to => "search#product_suggestions", :as => "search_index"
 
