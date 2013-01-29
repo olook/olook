f899efa917f1f132ece2358bff10ecda881cf6ff
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-29 11:50:09 -0200

    removing logic from child class and putting in base class

--- app/models/promotions/actions/percentage_adjustment.rb
+++ app/models/promotions/actions/percentage_adjustment.rb
@@ -1,15 +1,14 @@
 # -*- encoding : utf-8 -*-
 class PercentageAdjustment < PromotionAction
 
-  protected
+  private
 
     def calculate(cart_items, percent)
       calculated_values = []
       cart_items.each do |cart_item|
         sub_total = cart_item.quantity * cart_item.price
         adjustment = sub_total * BigDecimal("#{percent.to_i / 100.0}")
-        # TODO => put this small logic into super class (if should apply...)
-        calculated_values << { id: cart_item.id, adjustment: adjustment } if cart_item.should_apply?(adjustment)
+        calculated_values << { id: cart_item.id, adjustment: adjustment }
       end
       calculated_values
     end
