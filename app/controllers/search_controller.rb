4ba0d7c8a1912b379e7d233af84fab7ad22100f6
darkseid <rafael.manoel@gmail.com>
2013-01-28 23:53:11 -0200

    showing pictures

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,7 @@ class SearchController < ApplicationController
 
   def show
   # url = "?q=#{params[:q]}&return-fields=categoria,name,description,text_relevance"
-    url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{params[:q]}&return-fields=categoria,name,description,text_relevance")
+    url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{params[:q]}&return-fields=categoria,name,description,image,text_relevance")
     response = Net::HTTP.get_response(url)
 
     @hits = JSON.parse(response.body)["hits"]
