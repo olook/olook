7c40fd87663e68e5fa9b047591854c22ea072f48
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-01-29 13:37:21 -0200

    converting values

--- app/models/promotions/actions/value_adjustment.rb
+++ app/models/promotions/actions/value_adjustment.rb
@@ -3,7 +3,7 @@ class ValueAdjustment < PromotionAction
   private
     def calculate(cart_items, value)
       calculated_values = []
-      adjustment = value / cart_items.size
+      adjustment = BigDecimal(value) / cart_items.size
       cart_items.each do |item|
         calculated_values << { id: item.id, adjustment: adjustment }
       end
