5dcbb351ec2eaf4047c7dbd092cb0ae95bee4cba
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-29 16:09:36 -0200

    Adapt method to receive total number of orders

--- app/helpers/admin/dashboard_helper.rb
+++ app/helpers/admin/dashboard_helper.rb
@@ -1,11 +1,11 @@
 module Admin::DashboardHelper
 
-  def report_days_link(number_day, options = {})
+  def report_days_link(total,number_day, options = {})
     unless options[:state]
       options[:state] = ["authorized","picking","delivering","delivered"]
     end
     links_params = {number: number_day, state: options.fetch(:state)}
-    link_to(Order.with_date(number_day.business_days.before(Time.now)).with_state(options[:state]).size, admin_report_detail_path(links_params))
+    link_to(total, admin_report_detail_path(links_params))
   end
 
   def report_deliver_link(number_day, options = {})
