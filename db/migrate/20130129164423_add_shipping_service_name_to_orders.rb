764996629cd26aa4ebd8de62bb76f3d9776010b8
Tiago Almeida <tiago.almeida@olook.com.br>
2013-01-29 18:09:14 -0200

    Fix migration class name

--- db/migrate/20130129164423_add_shipping_service_name_to_orders.rb
+++
@@ -1,8 +0,0 @@
-class AddShippingServiceNameAndFreightStateToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :shipping_service_name, :string, :limit => 5
-    add_index  :orders, :shipping_service_name
-    add_column :orders, :freight_state, :string, :limit => 3
-    add_index :orders, :freight_state
-  end
-end
