345f09946b93e3b810828884dc87e1618c0fe571
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-29 20:01:55 -0200

    operation dashboard: state_totals and grand_total

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -15,20 +15,27 @@ class Admin::DashboardController < Admin::BaseController
   end
 
   def orders_life_cicle_report
-    @report_days = [*0..6]
-    @total = []
+    @report_days, @totals = [*0..6], []
+
     [:@authorized, :@picking, :@delivering, :@delivered].each do |name|
-      array = []
-      @all = []
+      state_counts = []
+
       @report_days.each do |report_day|
-        array << Order.with_date(report_day.business_days.before(Time.now)).with_state(name.to_s.delete('@')).count
+        state_counts << Order.with_date(report_day.business_days.before(Time.now)).with_state(name.to_s.delete('@')).count
       end
-      instance_variable_set(name, array)
-      @all << array.inject(0) { |total, value| total += value }
+
+      instance_variable_set(name, state_counts)
+
+      state_total = state_counts.inject(0) { |total, value| total += value }
+
+      instance_variable_set("#{name}_total", state_total)
     end
-    [*0..6].each do |index|
-      @total << (@authorized[index] + @picking[index] + @delivering[index] + @delivered[index])
+
+    @report_days.each do |index|
+      @totals << (@authorized[index] + @picking[index] + @delivering[index] + @delivered[index])
     end
+
+    @grand_total = @totals.inject(0) { |total, value| total += value }
   end
 
   def orders_time_report
