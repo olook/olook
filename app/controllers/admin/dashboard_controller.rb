55db010111666bb32735bc7359125bd72d15646b
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-30 14:54:04 -0200

    operation dashboard: WIP debug details page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -10,8 +10,12 @@ class Admin::DashboardController < Admin::BaseController
   end
 
   def show
-    day = params[:number]
-    @orders = Order.with_date((Time.now - day.to_i.days)).with_state(params[:state]).page(params[:page]).page(params[:page]).per_page(15).order('created_at desc')
+    number_of_days = params[:number].to_i
+    @orders = Order.with_date(number_of_days.business_days.before(Time.now)).
+                    with_state(params[:state]).
+                    page(params[:page]).
+                    per_page(15).
+                    order('created_at desc')
   end
 
   def orders_life_cicle_report
