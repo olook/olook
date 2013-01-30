8d3303521169737707c0ac2730871cac016d54b5
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-30 15:48:40 -0200

    operation dashboard: holidays configuration

---
+++ config/initializers/business_time.rb
@@ -0,0 +1,9 @@
+BusinessTime::Config.load("#{Rails.root}/config/business_time.yml")
+
+# or you can configure it manually:  look at me!  I'm Tim Ferris!
+BusinessTime::Config.beginning_of_workday = "10:00 am"
+BusinessTime::Config.end_of_workday = "11:30 am"
+
+Holiday.all.each do |holiday|
+  BusinessTime::Config.holidays << holiday.event_time
+end
