2bd6699aac682a3b27db8312f0e3452e9ae91fdd
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-01-31 17:23:29 -0200

    operations dashboard: data migration script

--- db/cockpit_update.rb
+++ db/cockpit_update.rb
@@ -1,7 +1,10 @@
 #batches of 1000
 Order.find_each do |o|
- if o.expected_delivery_on.nil? || o.shipping_service_name.nil?
+  if o.expected_delivery_on.nil? || o.shipping_service_name.nil?
+	updated_at = o.updated_at
     o.update_attribute(:expected_delivery_on, o.freight.delivery_time.days.from_now) if o.freight && o.freight.delivery_time
     o.update_attribute(:shipping_service_name, o.freight.shipping_service.name) if o.freight && o.freight.shipping_service
+    o.update_attribute(:updated_at, updated_at)
+    puts "updated #{o.inspect}"
  end
 end
