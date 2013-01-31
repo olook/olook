7f9e9d6e20ac90aeb34eeff0c91b080eda00d596
Rafael Carvalho <rafa1402@gmail.com>
2013-01-31 20:35:16 -0200

    new stylists page - AMO!

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,7 +1,7 @@
 class StylistsController < ApplicationController
   def helena_linhares
-    @products_helena = Product.find(12389, 13722, 12035, 13493, 9061, 13870)
+    @products_helena = Product.find(14367, 10400, 14846, 15084, 15162, 14814)
     @products_didi = Product.find(13830, 12003, 11069, 11606, 12043)
-    @products_paola = Product.find(9117, 13924, 9460, 11749, 11977, 13584)
+    @products_paola = Product.find(12425, 15074,14868, 13098, 13934, 15072)
   end
 end
