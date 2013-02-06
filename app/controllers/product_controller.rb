0ebc41bb6a14dd667d43d1ea5a1af48dfa904fa8
Tiago Almeida <tiago.almeida@olook.com.br>
2013-02-06 16:09:07 -0200

    simplifying class helpers for google tags

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -16,6 +16,7 @@ class ProductController < ApplicationController
     else
       Product.only_visible.find(params[:id])
     end
+    @google_pixel_information = @product
     @chaordic_user = ChaordicInfo.user current_user
     @chaordic_product = ChaordicInfo.product @product
     @chaordic_category = @product.category_humanize
