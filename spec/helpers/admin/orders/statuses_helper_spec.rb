52394ebb32fbeb7d491060e36b028b647f2a65b1
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-02-08 12:30:10 -0200

    admin orders delivery: testing individual filter params for order_status_link helper

--- spec/helpers/admin/orders/statuses_helper_spec.rb
+++ spec/helpers/admin/orders/statuses_helper_spec.rb
@@ -16,6 +16,24 @@ describe Admin::Orders::StatusesHelper do
       expect(helper.orders_status_link(options)).to eq(link)
     end
 
+    context "with freight_state param" do
+    	let(:options) { { total: 1, day_number: 1, state: 'delivered', freight_state: 'SP' } }
+    	
+    	it "returns the same link with this param included" do
+    		link = '<a href="statuses/show?total=1&amp;day_number=1&amp;state=delivered&amp;freight_state=SP">1</a>'
+    		expect(helper.orders_status_link(options)).to eq(link)
+    	end
+    end
+
+    context "with shipping_service params" do
+    	let(:options) { { total: 1, day_number: 1, state: 'delivered', shipping_service_name: 'PAC' } }
+    	
+    	it "returns the same link with this param included" do
+    		link = '<a href="statuses/show?total=1&amp;day_number=1&amp;state=delivered&amp;shipping_service_name=PAC">1</a>'
+    		expect(helper.orders_status_link(options)).to eq(link)
+    	end
+    end
+
     context "with freight_state and shipping_service params" do
     	let(:options) { { total: 1, day_number: 1, state: 'delivered', freight_state: 'SP', shipping_service_name: 'PAC' } }
     	
