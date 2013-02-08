3af8bc96be86a2c63bba4d1eb9a087defb3d0dea
Rafael Carvalho <rafa1402@gmail.com>
2013-02-08 10:39:44 -0200

    bugfix phone mask in Address page

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -259,23 +259,19 @@ $(document).ready(function() {
   });
 
   // For now both fone field will acept nine digits
+  function maskTel(tel){
+  	ddd  = $(tel).val().substring(1, 3);
+  	dig9 = $(tel).val().substring(4, 5);
+
+  	if(ddd == "11" && dig9 == "9")
+  		$(tel).setMask("(99)99999-9999");
+    else
+     	$(tel).setMask("(99)9999-9999");	  
+  }
   $("input:text.phone").keyup(function(){
-		ddd  = $(tel).val().substring(1, 3);
-		dig9 = $(tel).val().substring(5, 6);
-		if(ddd == "11" && dig9 == "9")
-			$(tel).setMask("(99) 99999-9999");
-	  else
-	    $(tel).setMask("(99) 9999-9999");	  
-	}).focus(function(){
-	    $(tel).unsetMask();
-	}).focusout(function() {
-			ddd  = $(tel).val().substring(1, 3);
-			dig9 = $(tel).val().substring(5, 6);
-	    if(ddd == "11" && dig9 == "9")
-				$(tel).setMask("(99) 99999-9999");
-		  else
-		    $(tel).setMask("(99) 9999-9999");
+		maskTel(this);
 	});
+
    
    
 
