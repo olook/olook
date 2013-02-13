61f3f7bb998a388b1ea3aac2d6f623cbc8ea709b
Tiago Almeida <tiago.almeida@olook.com.br>
2013-02-13 17:41:48 -0200

    Refactor showroom controller to use controller home index logic

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,19 +5,9 @@ class HomeController < ApplicationController
     @google_path_pixel_information = "Home"
     @chaordic_user = ChaordicInfo.user current_user
 
-    @top5 = Product.fetch_products :top5
-    @stylist = Product.fetch_products :selection
-
-    if params[:share]
-      @user = User.find(params[:uid])
-      @profile = @user.profile_scores.first.try(:profile).try(:first_visit_banner)
-      @qualities = Profile::DESCRIPTION["#{@profile}"]
-      @url = request.protocol + request.host
-    end
-    incoming_params = params.clone.delete_if {|key| ['controller', 'action'].include?(key) }
-    session[:tracking_params] ||= incoming_params
+    prepare_for_home
     if user_signed_in?
-      redirect_to member_showroom_path(incoming_params)
+      redirect_to member_showroom_path(@incoming_params)
       flash[:notice] = flash[:notice]
     end
   end
