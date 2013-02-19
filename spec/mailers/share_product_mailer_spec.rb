f47a84669aeb53af8ca73da720817007139be9b6
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-02-19 18:35:20 -0300

    updating product mailer information and updating and fixing tests

--- spec/mailers/share_product_mailer_spec.rb
+++ spec/mailers/share_product_mailer_spec.rb
@@ -4,12 +4,16 @@ require 'spec_helper'
 describe MemberMailer do
   let(:product) { FactoryGirl.create(:basic_shoe) }
 
-  describe "#welcome_email" do
+  describe "#send_share_message_for" do
     informations = { name_from: "User name", email_from: "user@email.com", emails_to_deliver: "user_friend@email.com" }
     let!(:mail) { ShareProductMailer.send_share_message_for(product, informations) }
 
-    it "sets 'from' attribute to olook user email" do
-      mail.from.should include(informations[:email_from])
+    it "sets 'from' attribute to olook email" do
+      mail.from.should include("vip@o.conviteolook.com.br")
+    end
+
+    it "sets 'reply_to' attribute to user email" do
+      mail.reply_to.should include(informations[:email_from])
     end
 
     it "sets 'to' attribute to passed member's email" do
@@ -19,5 +23,6 @@ describe MemberMailer do
     it "sets 'title' attribute welcoming the new member" do
       mail.subject.should eq product.id.to_s
     end
+
   end
 end
