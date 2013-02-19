19a9996bb26ae5c703afb01d27e4dbba825402b3
Rafael Carvalho <rafa1402@gmail.com>
2013-02-19 17:32:38 -0300

    bugfix modal

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
   });
   
   if($('#compartilhar_email').length == 1) {
-     var content = $('#compartilhar_email'), h = content.height() + 70, w = content.width() + 70;
+     var content = $('#compartilhar_email'), h = content.outerHeight(), w = content.outerWidth();
      $("ul.social li.email").click(function(e){
         e.preventDefault();
         e.stopPropagation();
