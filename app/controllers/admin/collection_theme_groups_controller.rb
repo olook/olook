62ea8bb9f76a1bc70555610582a93246bb6e917a
Luis Daher <luisotaviodaher@gmail.com>
2013-02-21 20:02:25 -0300

    WIP - CollectionThemeGroup development initiated

---
+++ app/controllers/admin/collection_theme_groups_controller.rb
@@ -0,0 +1,24 @@
+# -*- encoding : utf-8 -*-
+class Admin::CollectionThemeGroupsController < Admin::BaseController
+  respond_to :html, :js
+
+  load_and_authorize_resource
+
+  def index
+    @collection_theme_groups = CollectionThemeGroup.all
+    respond_with :admin, @collection_theme_groups
+  end
+
+  def show
+    @collection_theme_group = CollectionThemeGroup.find(params[:id])
+    respond_with :admin, @collection_theme_group
+  end
+
+  def create
+    @collection_theme_group = CollectionThemeGroup.new(params[:collection_theme_group])
+    if @collection_theme_group.save
+      flash[:notice] = 'Collection Theme Group was successfully created.'
+    end
+    respond_with :admin, @collection_theme_group
+  end  
+end
