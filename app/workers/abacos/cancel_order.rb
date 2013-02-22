4551527300003712c7aae3df68ec49a19fb91a9f
darkseid <rafael.manoel@gmail.com>
2013-02-22 16:51:11 -0300

    fix cancel order spec

--- app/workers/abacos/cancel_order.rb
+++ app/workers/abacos/cancel_order.rb
@@ -5,13 +5,19 @@ module Abacos
 
     def self.perform(order_number)
       order = Order.find_by_number order_number    
-      order.canceled if (order.can_be_canceled? && !order.payment_rollback?)
+      
+      order.canceled if should_cancel? order
 
       if order.canceled? && Abacos::OrderAPI.order_exists?(order_number)
         cancelar_pedido = Abacos::CancelarPedido.new order
         Abacos::OrderAPI.cancel_order cancelar_pedido
       end
     end
+
+    private
+      def self.should_cancel? order
+        (order.can_be_canceled? && !order.payment_rollback?)
+      end
   end
 
 
