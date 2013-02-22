370f0ef7ce400f01fc43dd48fe9656f96e7b58ba
Tiago Almeida <tiago.almeida@olook.com.br>
2013-02-22 14:49:40 -0300

    Change class name on catalog moment

--- app/models/catalog/moment.rb
+++ app/models/catalog/moment.rb
@@ -1,3 +1,3 @@
 class Catalog::Moment < Catalog::Catalog
-  belongs_to :moment, :class_name => "::Moment", :foreign_key => "association_id"
+  belongs_to :collection_theme, :class_name => "::CollectionTheme", :foreign_key => "association_id"
 end
