84b16b071af5309c5005e339596144891790263d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-02-22 11:00:01 -0300

    Add act as list on moments

--- app/models/collection_theme.rb
+++ app/models/collection_theme.rb
@@ -3,16 +3,18 @@ class CollectionTheme < ActiveRecord::Base
   validates :slug, presence: true, uniqueness: true
   validates :position, numericality: true
 
+  acts_as_list
+
   mount_uploader :header_image, ImageUploader
-  
+
   has_one :catalog, class_name: "Catalog::Moment", foreign_key: "association_id"
 
   after_create :generate_catalog
 
   scope :active, where(active: true)
-  
+
   private
-  
+
   def generate_catalog
     self.build_catalog.save!
   end
