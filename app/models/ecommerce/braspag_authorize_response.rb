6202c23920a77164679cad314549580a2a9704fb
darkseid <rafael.manoel@gmail.com>
2013-02-24 16:50:35 -0300

    turn the Braspag authorization process in synchronous

--- app/models/ecommerce/braspag_authorize_response.rb
+++ app/models/ecommerce/braspag_authorize_response.rb
@@ -18,8 +18,8 @@ class BraspagAuthorizeResponse < ActiveRecord::Base
       self.update_attributes(:processed => true, :error_message => "Invalid status")
     elsif payment.set_state(event)
       self.update_attribute(:processed, true)
-      if payment.order
-        Resque.enqueue_in(2.hours, Abacos::CancelOrder, payment.order.number)
+      if payment.order && payment.order.reload.canceled?
+        Resque.enqueue(Abacos::CancelOrder, payment.order.number)
       end
     else
       self.update_attributes(
