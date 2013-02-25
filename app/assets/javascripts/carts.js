4085b24feac0f793641d3281c65c33adb5cbdb57
Rafael Carvalho <rafa1402@gmail.com>
2013-02-25 18:01:43 -0300

    gift package modal in cart page

--- app/assets/javascripts/carts.js
+++ app/assets/javascripts/carts.js
@@ -57,7 +57,7 @@ $(function() {
       }
     });
   }
-
+  
   if($("div#carousel_lookbooks").size() > 0) {
     $("div#carousel_lookbooks ul").carouFredSel({
       auto: false,
@@ -77,7 +77,18 @@ $(function() {
       }
     });
   }
+  
+  showGiftPackage();
 });
+function showGiftPackage(){
+   content = $(".modal_gift");
+   $(".txt-conheca").bind("click", function(e){
+      initBase.newModal(content);
+      e.preventDefault();
+      e.stopPropagation();
+   })
+
+}
 
 function changeCartItemQty(cart_item_id) {
   $('form#change_amount_' + cart_item_id).submit();
