d5a5c1ba4397f67953b63980e1bcb5accaf3c540
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-02-25 12:01:26 -0300

    Fix Collection Theme acts_as_list

--- app/models/collection_theme.rb
+++ app/models/collection_theme.rb
@@ -3,12 +3,12 @@ class CollectionTheme < ActiveRecord::Base
   validates :slug, presence: true, uniqueness: true
   validates :position, numericality: true
 
-  acts_as_list
-
   mount_uploader :header_image, ImageUploader
 
   has_one :catalog, class_name: "Catalog::CollectionTheme", foreign_key: "association_id"
   belongs_to :collection_theme_group
+
+  acts_as_list scope: :collection_theme_group
 
   after_create :generate_catalog
 
