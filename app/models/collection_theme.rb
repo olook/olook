e1d25e91525d2b86d46effbd9e515978424af219
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-02-25 13:12:02 -0300

    Creating Group when saving Theme

--- app/models/collection_theme.rb
+++ app/models/collection_theme.rb
@@ -7,6 +7,7 @@ class CollectionTheme < ActiveRecord::Base
 
   has_one :catalog, class_name: "Catalog::CollectionTheme", foreign_key: "association_id"
   belongs_to :collection_theme_group
+  accepts_nested_attributes_for :collection_theme_group, reject_if: :all_blank
 
   acts_as_list scope: :collection_theme_group
 
@@ -14,7 +15,13 @@ class CollectionTheme < ActiveRecord::Base
 
   scope :active, where(active: true)
 
+  after_initialize :default_values
+
   private
+
+  def default_values
+    self["slug"] = self.name.parameterize unless self.name.nil?
+  end
 
   def generate_catalog
     self.build_catalog.save!
