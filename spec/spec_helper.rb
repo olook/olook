8a737bb157c33808182d6e3c0b265175d02a3a30
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-02-25 18:34:57 -0300

    Sort in index

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,14 +17,14 @@ Spork.prefork do
 
   # This file is copied to spec/ when you run 'rails generate rspec:install'
   ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'capybara/rspec'
+  require 'carrierwave/test/matchers'
 
   # Since we're using devise, the spork guys recommend us to reload the routes on this step
   # https://github.com/timcharper/spork/wiki/Spork.trap_method-Jujutsu
   Spork.trap_method(Rails::Application::RoutesReloader, :reload!) if defined?(Rails)
-
-  require 'rspec/rails'
-  require 'capybara/rspec'
-  require 'carrierwave/test/matchers'
 
   Capybara.javascript_driver = :webkit
 
@@ -95,7 +95,6 @@ Spork.prefork do
 end
 
 Spork.each_run do
-  require File.expand_path("../../config/environment", __FILE__)
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
