2ab3deeaae3c2f1bdc7208b9d240c0616e508e9e
Tiago Almeida <tiago.almeida@olook.com.br>
2013-02-27 14:54:20 -0300

    Add products to collection themes controller

--- app/controllers/collection_themes_controller.rb
+++ app/controllers/collection_themes_controller.rb
@@ -1,8 +1,33 @@
+# -*- encoding : utf-8 -*-
+
 class CollectionThemesController < ApplicationController
+
+  before_filter :load_catalog_products
+
   def index
+    @collection_theme_groups = CollectionThemeGroup.all
   end
-  
+
   def show
-  end    
+    @collection_theme_groups = CollectionThemeGroup.all
+    @chaordic_user = ChaordicInfo.user current_user
+  end
+
+  private
+    def load_catalog_products
+      @collection_themes = CollectionTheme.active.order(:position)
+      @collection_theme = params[:slug] ? CollectionTheme.find_by_slug(params[:slug]) : @collection_themes.last
+
+      if @collection_theme
+        @catalog_products = CatalogSearchService.new(params.merge({id: @collection_theme.catalog.id})).search_products
+        @products_id = @catalog_products.map{|item| item.product_id }.compact
+        # params[:id] is into array for pixel iterator
+        @categories_id = params[:id] ? [params[:id]] : @collection_themes.map(&:id).compact.uniq
+      else
+        redirect_to root_path
+        flash[:notice] = "No momento não existe nenhuma ocasião cadastrada."
+      end
+    end
+
 
 end
