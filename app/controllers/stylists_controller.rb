c9e940f2680a58ffa882107c927b014d8ee1e934
Rafael Carvalho <rafa1402@gmail.com>
2013-02-28 11:12:09 -0300

    new products in style page, and added rss link on footer

--- app/controllers/stylists_controller.rb
+++ app/controllers/stylists_controller.rb
@@ -1,6 +1,6 @@
 class StylistsController < ApplicationController
   def helena_linhares
-    @products_helena = Product.find(14367, 10400, 14846, 15084, 15162, 14814)
+    @products_helena = Product.find(8267, 14784, 15748, 15025, 14792, 15750)
     @products_didi = Product.find(13830, 12003, 11069, 11606, 12043)
     @products_paola = Product.find(12425, 15074,14868, 13098, 13934, 15072)
   end
