3b94d7b561598b0b9e6e4dd5d0ff6899f5370efa
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-05 14:24:53 -0300

    Removing app5 from deploy

--- config/deploy/prod_todas.rb
+++ config/deploy/prod_todas.rb
@@ -2,7 +2,7 @@ load 'deploy/assets'
 require 'airbrake/capistrano'
 require 'new_relic/recipes'
 
-role :app, 'app1.olook.com.br', 'app2.olook.com.br', 'app3.olook.com.br', 'app4.olook.com.br', 'app5.olook.com.br'
+role :app, 'app1.olook.com.br', 'app2.olook.com.br', 'app3.olook.com.br', 'app4.olook.com.br'
 role :web, 'app2.olook.com.br'
 role :db,  'app2.olook.com.br'
 
