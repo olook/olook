3484c21bc87398697c91c31c3f3e96a87d672f4d
Luis Daher <luisotaviodaher@gmail.com>
2013-03-05 16:16:00 -0300

    WIP - filters and product scroll @ collection_themes#show

--- app/controllers/collection_themes_controller.rb
+++ app/controllers/collection_themes_controller.rb
@@ -1,8 +1,9 @@
 # -*- encoding : utf-8 -*-
-
 class CollectionThemesController < ApplicationController
   respond_to :html, :js
 
+  before_filter :load_products_of_user_size, only: [:show]
+  before_filter :filter_products_by_category, :unless => lambda{ params[:category_id].nil? }
   before_filter :load_catalog_products
 
   def index
@@ -13,11 +14,23 @@ class CollectionThemesController < ApplicationController
     @chaordic_user = ChaordicInfo.user current_user
   end
 
-  def update
-    respond_with @catalog_products
-  end
-
   private
+
+    def load_products_of_user_size
+      # To show just the shoes of the user size at the
+      # first time that the liquidations page is rendered
+      params[:shoe_sizes] = [current_user.shoes_size.to_s] if current_user && current_user.shoes_size
+    end
+
+    def filter_products_by_category
+      if (params[:category_id].nil? || params[:category_id] == "")
+        params.delete :category_id
+      else
+        @category_id = params[:category_id].to_i
+      end
+      params.delete (:shoe_sizes) if @category_id != Category::SHOE
+    end
+
     def load_catalog_products
       @collection_theme_groups = CollectionThemeGroup.all
       @collection_themes = CollectionTheme.active.order(:position)
