d940fa33557a10526f0bc0205610a94ef27e2d6b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-07 18:28:13 -0300

    Fix ErrorNotifier

--- app/models/error_notifier.rb
+++ app/models/error_notifier.rb
@@ -2,7 +2,7 @@ class ErrorNotifier
 
   def self.send_notifier(gateway_name, error, payment)
     error_message = "#{gateway_name} Request #{error.message} - Order Number #{payment.try(:id)} - Payment Expiration #{payment.payment_expiration_date} - User ID #{payment.try(:user_id)}"
-      log("#{error.class}: #{error_message}\n#{error.backtrace.join("\n")}")
+      log("#{error.class}: #{error_message}\n#{error.backtrace.to_a.join("\n")}")
       NewRelic::Agent.add_custom_parameters({:error_msg => error_message})
       Airbrake.notify(error,
         :error_class   => "#{gateway_name} Request [#{error.class}]",
