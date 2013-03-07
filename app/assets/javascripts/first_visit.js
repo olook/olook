aeedd6f05f777ae397441de0962fd0175810aeb5
Rafael Carvalho <rafa1402@gmail.com>
2013-03-07 17:19:16 -0300

    adjust modal

--- app/assets/javascripts/first_visit.js
+++ app/assets/javascripts/first_visit.js
@@ -4,7 +4,7 @@ $(function() {
 
 initFirstVisit = {
   inviteLightbox : function() {
-    clone = $("div.box_invite")
+    clone = $("div.box_invite").addClass("clone");
     $(clone).find("li.facebook a").attr("id", "facebook_post_wall");
     $(clone).find("li.orkut #orkut_share").remove();
     //content = clone[0].outerHTML;
