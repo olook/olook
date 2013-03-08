4d2bd18364523083790c351bb3bb3c3589eb7921
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-08 15:44:58 -0300

    Adding pre on body

--- app/models/error_notifier.rb
+++ app/models/error_notifier.rb
@@ -3,7 +3,7 @@ class ErrorNotifier
   def self.send_notifier(gateway_name, error, payment)
     error_message = "#{gateway_name} Request #{error.message} - Order Number #{payment.try(:id)} - Payment Expiration #{payment.payment_expiration_date} - User ID #{payment.try(:user_id)}"
     log(_body = "#{error.class}: #{error_message}\n#{error.backtrace.to_a.join("\n")}")
-    ActionMailer::Base.mail(:from => "admin@olook.com.br", :to => "incidentes@olook.com.br", :subject => "ErrorNotifier: #{error_message}", :text => _body).deliver
+    ActionMailer::Base.mail(:from => "admin@olook.com.br", :to => "incidentes@olook.com.br", :subject => "ErrorNotifier: #{error_message}", :body => "<pre>#{_body}</pre>").deliver
     NewRelic::Agent.add_custom_parameters({:error_msg => error_message})
     Airbrake.notify(error,
       :error_class   => "#{gateway_name} Request [#{error.class}]",
