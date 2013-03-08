d90cdcbaf60c3ca0557f477a3d2f88a1e4d0ed39
Tiago Almeida <tiago.almeida@olook.com.br>
2013-03-08 17:22:04 -0300

    Add images on factories

--- spec/factories/collection_themes.rb
+++ spec/factories/collection_themes.rb
@@ -1,9 +1,11 @@
 # -*- encoding : utf-8 -*-
+include ActionDispatch::TestProcess
 FactoryGirl.define do
   factory :collection_theme do
     name "dia-a-dia"
     slug "dia-a-dia"
     article "Para a"
+    header_image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
     position 1
     active true
   end
@@ -16,6 +18,7 @@ FactoryGirl.define do
       "moment#{n}"
     end
     active true
+    header_image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
     article "Para a"
     position 2
   end
