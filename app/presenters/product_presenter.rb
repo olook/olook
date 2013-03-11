ba958f58efc0a75ae72b34db8d7360e1bf4b35c0
darkseid <rafael.manoel@gmail.com>
2013-03-10 23:42:33 -0300

    add a missing cloth size

--- app/presenters/product_presenter.rb
+++ app/presenters/product_presenter.rb
@@ -2,8 +2,8 @@
 class ProductPresenter < BasePresenter
 
   SIZES_TABLE = {"PP" => 1, "P" =>2, "M" => 3, "G" => 4,
-                 "34" => 5, "36" => 6, "38" => 7, "40" => 8, "42" => 9,
-                 "Único" => 10}
+                 "34" => 5, "36" => 6, "38" => 7, "40" => 8, "42" => 9, "44" => 10,
+                 "Único" => 11}
 
   def collection_name
    Collection.active.try(:name) || I18n.l(Date.today, :format => '%B')
