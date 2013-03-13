4ac2599f57319515178176acc04975799ef5c282
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-13 17:09:32 -0300

    Redirect to ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,6 @@
 # -*- encoding : utf-8 -*-
 Olook::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
-  config.force_ssl = true
 
   # Code is not reloaded between requests
   config.cache_classes = true
