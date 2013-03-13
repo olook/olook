30b83e34f32e50623edce1a444fdd16469a69172
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-13 18:45:20 -0300

    WIP bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,13 +2,25 @@
 
 bundle install
 
-for filename in config/*.sample 
-do
-  echo -n "$filename => ";
-  file_without_ext=`basename $filename .sample`;
-  cp -vn $filename config/$file_without_ext;
-  echo ""
-done
+[[ ! -a ~/.ec2 ]] && mkdir ~/.ec2
+cp ./config/deploy/gsg-keypair* ~/.ec2/
+chmod 0600 ~/.ec2/gsg-keypair*
+
+ruby <<SCRIPT
+require 'erb'
+  Dir["./config/deploy/*.yml.erb"].each do |location|
+    template = File.read(location)
+    /.*\/(?<file>.*)\.erb/ =~ location.to_s
+
+    config = ERB.new(template)
+
+    application = 'olook'
+    File.open('./config/database.yml', 'w') do |f|
+      f.puts config.result(binding)
+    end
+
+  end
+SCRIPT
 
 bundle exec rake db:create db:migrate olook:seed_admin db:seed olook:create_permissions
 bundle exec rake db:create db:migrate olook:seed_admin db:seed olook:create_permissions RAILS_ENV=test
