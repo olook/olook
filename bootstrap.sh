dc56ebc437607a7ff32ea320274a2a3dc08aa0f5
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-15 00:41:56 -0300

    improve bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,16 @@
 
 bundle install
 
+echo "Copying gsg keys for deploy and servers access"
 [[ ! -a ~/.ec2 ]] && mkdir ~/.ec2
 cp ./config/deploy/gsg-keypair* ~/.ec2/
 chmod 0600 ~/.ec2/gsg-keypair*
 
-ruby <<SCRIPT
-require 'erb'
-  Dir["./config/deploy/*.yml.erb"].each do |location|
-    template = File.read(location)
-    /.*\/(?<file>.*)\.erb/ =~ location.to_s
-
-    config = ERB.new(template)
-
-    application = 'olook'
-    File.open('./config/database.yml', 'w') do |f|
-      f.puts config.result(binding)
-    end
-
-  end
-SCRIPT
+for f in config/deploy/*.yml.erb; do
+  dest="config/$(echo $f | sed 's/.*\/\(.*\).erb/\1/')"
+  cat $f | sed 's/<%= "#{application}_\([^"]*\)" %>/olook_\1/' | sed 's/<%.*%>//' > $dest
+  echo "$f -> $dest"
+done
 
 bundle exec rake db:create db:migrate olook:seed_admin db:seed olook:create_permissions
 bundle exec rake db:create db:migrate olook:seed_admin db:seed olook:create_permissions RAILS_ENV=test
