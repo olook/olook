877d84f0e8c822d7cbdd525e9fe97218b95043c2
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-20 17:17:21 -0300

    back search show route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Olook::Application.routes.draw do
   get "index/index"
 
   # Search Lab
+  get "/search/show", :to => "search#show", :as => "search_show"
   get "/search", :to => "search#index", :as => "search_index"
   get "/search/product_suggestions", :to => "search#product_suggestions", :as => "search_index"
 
