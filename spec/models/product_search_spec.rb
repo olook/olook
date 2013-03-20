f7b6e2d65dea3277c9d14036ddf9152c0c0ba4bb
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-20 16:34:56 -0300

    using redis to save products search suggestion

--- spec/models/product_search_spec.rb
+++ spec/models/product_search_spec.rb
@@ -1,18 +1,10 @@
 describe ProductSearch do
   describe ".terms_for" do
     let!(:product) { FactoryGirl.create(:basic_shoe) }
-    context "when has terms for given parameter" do
-      let!(:another_product) { FactoryGirl.create(:basic_shoe, name: "Scarpin") }
-      it "returns only names like 'Chan'" do
-        expect(described_class.terms_for("Chan")).to include(product.name)
-        expect(described_class.terms_for("Chan")).to_not include(another_product.name)
-      end
-    end
 
-    context "when there's no terms for given parameter" do
-      it "returns an empty array" do
-        expect(described_class.terms_for("Something")).to eq([])
-      end
+    it "Redis receives zrevrange method " do
+      Redis.any_instance.should_receive(:zrevrange)
+      described_class.terms_for("Foo")
     end
   end
 end
