91c4cc60c2b10e78ae9d1caecad80c33707ecedc
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-20 16:34:56 -0300

    using redis to save products search suggestion

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,5 +1,14 @@
 class ProductSearch
+
   def self.terms_for(term)
-    Product.where("name like ?", "#{term}_%").limit(10).collect(&:name)
+    Resque.redis.zrevrange "product-search:#{term.downcase}", 0, 9
   end
+
+  def self.index_term(term)
+    1.upto(term.to_s.length - 1) do |n|
+      prefix = term[0, n]
+      Resque.redis.zincrby "product-search:#{prefix.downcase}", 1, term.downcase
+    end
+  end
+
 end
