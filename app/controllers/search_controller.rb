853fd412fa598773e49429575666a41bef6d76b8
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-20 16:33:00 -0300

    removing unnecessary actions from search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,35 +2,10 @@ class SearchController < ApplicationController
   respond_to :json
 
   def index
-    #render json: %w[foo bar]
-  end
-
-  def show
-  # url = "?q=#{params[:q]}&return-fields=categoria,name,description,text_relevance"
-    url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{params[:q]}&return-fields=categoria,name,description,image,text_relevance")
-    response = Net::HTTP.get_response(url)
-
-    @hits = JSON.parse(response.body)["hits"]
   end
 
   def product_suggestions
     render json: ProductSearch.terms_for(params[:term])
   end
-
-  def q
-    suggestions = []
-
-    url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{params[:term]}&return-fields=categoria,name,description,text_relevance")
-    response = Net::HTTP.get_response(url)
-
-    hits = JSON.parse(response.body)["hits"]
-    hits["hit"].each do |hit|
-      suggestions << hit["data"]["name"]
-    end
-
-    puts suggestions.flatten
-    render json: suggestions.flatten
-  end
-
 
 end
