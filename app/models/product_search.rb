6bbb8d6a4498695c46a01015f36dec2691490e11
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-20 17:20:41 -0300

    creating REDIS constant

--- app/models/product_search.rb
+++ app/models/product_search.rb
@@ -1,13 +1,13 @@
 class ProductSearch
 
   def self.terms_for(term)
-    Resque.redis.zrevrange "product-search:#{term.downcase}", 0, 9
+    REDIS.zrevrange "product-search:#{term.downcase}", 0, 9
   end
 
   def self.index_term(term)
     1.upto(term.to_s.length - 1) do |n|
       prefix = term[0, n]
-      Resque.redis.zincrby "product-search:#{prefix.downcase}", 1, term.downcase
+      REDIS.zincrby "product-search:#{prefix.downcase}", 1, term.downcase
     end
   end
 
