0eb312bcee3ab9b3ff8e2b41141305784cacf1ff
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-20 18:43:42 -0300

    Move function to catalog_search_service

--- app/helpers/collection_themes_helper.rb
+++ app/helpers/collection_themes_helper.rb
@@ -17,15 +17,6 @@ module CollectionThemesHelper
     {shoes_path => "sapatos", accessories_path => "acessórios", bags_path => "bolsas", clothes_path => "Roupas"}[request.fullpath]
   end
 
-  def available_categories_options_for(catalog_search_service=nil)
-    categories = Category.to_a
-    if catalog_search_service.respond_to?(:filter_without_paginate)
-      categories_in_query = catalog_search_service.filter_without_paginate.group(:category_id).count.keys
-      categories.select! { |opt| categories_in_query.include?(opt.last) }
-    end
-    options_for_select(categories.unshift(["Todas", nil]))
-  end
-
   private
     def number_of_installments_for price
       return 1 if price <= MIN_INSTALLMENT_VALUE
