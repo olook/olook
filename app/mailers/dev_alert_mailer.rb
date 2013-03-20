5b6fb6ecf1d95259258513743f4ef7cbc435bab1
Tiago Almeida <tiago.almeida@olook.com.br>
2013-03-20 17:54:45 -0300

    Remove smtp settings from dev mailer

--- app/mailers/dev_alert_mailer.rb
+++ app/mailers/dev_alert_mailer.rb
@@ -1,18 +1,6 @@
 # -*- encoding : utf-8 -*-
 class DevAlertMailer < BaseMailer
-  default_url_options[:host] = "www.olook.com.br"
-  default :from => "dev notifications <dev@olook.com.br>"
-
-  def self.smtp_settings
-    {
-      :user_name => "AKIAJJO4CTAEHYW34HGQ",
-      :password => "AkYlOmgbIpISW33XVzQq8d9J4GnAgtQlEJuwgIxOFXmU",
-      :address => "email-smtp.us-east-1.amazonaws.com",
-      :port => 587,
-      :authentication => :plain,
-      :enable_starttls_auto => true
-    }
-  end
+  default :from => "olook notification <avisos@olook.com.br>"
 
   def braspag_capture_warn(warn_payments)
     @warn_payments = warn_payments
