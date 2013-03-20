d4ecedaea776ad390d5d58b31f16bb0a2cc44c5b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-20 18:43:42 -0300

    Move function to catalog_search_service

--- app/services/catalog_search_service.rb
+++ app/services/catalog_search_service.rb
@@ -15,9 +15,13 @@ class CatalogSearchService
 
   end
 
-  def filter_without_paginate
-    base_process
-    @query
+  def categories_available_for_options
+    categories = Category.to_a
+    if catalog_search_service.respond_to?(:filter_without_paginate)
+      categories_in_query = self.filter_without_paginate.group(:category_id).count.keys
+      categories.select! { |opt| categories_in_query.include?(opt.last) }
+    end
+    categories.unshift(["Todas", nil])
   end
 
   def search_products
@@ -29,6 +33,11 @@ class CatalogSearchService
   end
 
   private
+
+  def filter_without_paginate
+    base_process
+    @query
+  end
 
   def base_process
     add_categories_filter_to_query_base
