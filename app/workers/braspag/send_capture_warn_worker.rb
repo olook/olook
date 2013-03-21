24a3396da86bbaab3d8487292462e545bf359056
Tiago Almeida <tiago.almeida@olook.com.br>
2013-03-21 11:58:42 -0300

    Generalize order status information

--- app/workers/braspag/send_capture_warn_worker.rb
+++
@@ -1,9 +0,0 @@
-# -*- encoding : utf-8 -*-
-class SendCaptureWarnWorker
-  @queue = :order_status
-
-  def self.perform
-    @warn_payments = BraspagAuthorizeResponse.find_by_sql("select p.id, a.identification_code, p.state, a.created_at from braspag_authorize_responses a left join braspag_capture_responses c  on a.identification_code = c.identification_code join payments p on a.identification_code = p.identification_code  where c.id is null and a.status = 1 and (a.created_at >= '#{(Time.zone.today - 15.days).strftime('%Y-%m-%d %H:%M:%S')}' and a.created_at <= '#{(Time.zone.today - 2.days).strftime('%Y-%m-%d %H:%M:%S')}') and p.state <> 'cancelled' order by a.created_at desc")
-    mail = DevAlertMailer.braspag_capture_warn.delivery unless @warn_payments.empty?
-  end
-end
