d33e9570a9644b876e0d176b9db1bd0e2471a1d2
Tiago Almeida <tiago.almeida@olook.com.br>
2013-03-21 12:00:33 -0300

    Add warn order to dev alert mailer

--- app/mailers/dev_alert_mailer.rb
+++ app/mailers/dev_alert_mailer.rb
@@ -2,8 +2,9 @@
 class DevAlertMailer < BaseMailer
   default :from => "olook notification <avisos@olook.com.br>"
 
-  def braspag_capture_warn(warn_payments)
+  def braspag_capture_warn(warn_payments, warn_order)
     @warn_payments = warn_payments
+    @warn_orders = warn_order
     mail(:to => "incidentes@olook.com.br", :subject => "Pedidos que deveriam ter sido capturados pela braspag")
   end
 end
