d497b9ecd1a6154127ea33f0ce9b5b1569ab8e30
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2013-03-21 16:02:43 -0300

    refactor of moip strategy spec

--- spec/strategies/payments/moip_sender_strategy_spec.rb
+++ spec/strategies/payments/moip_sender_strategy_spec.rb
@@ -18,43 +18,62 @@ describe Payments::MoipSenderStrategy do
   }) }
   let(:order_total) { 12.34 }
 
+  subject { Payments::MoipSenderStrategy.new(cart_service, payment) }
 
-  it "should be initialize with success" do
-    Payments::MoipSenderStrategy.new(cart_service, payment).should be_true
+  describe "#initialize" do
+    context "with no arguments" do
+      it "raises an ArgumentError" do
+        expect { Payments::MoipSenderStrategy.new() }.to raise_error(ArgumentError)
+      end
+    end
   end
 
-  context "with a valid class" do
-
-    subject {Payments::MoipSenderStrategy.new(cart_service, payment)}
-
-    it "should set url on payment" do
+  describe  "#save_payment_url!" do
+    it "sets url on payment" do
       subject.stub(:payment_url).and_return('www.fake.com')
       subject.save_payment_url!
-      payment.url.should == 'www.fake.com'
+      expect(payment.url).to eql('www.fake.com')
     end
+  end
 
-    it "should send payments" do
-      CartService.any_instance.should_receive(:freight).twice.and_return(freight)
-      MoIP::Client.should_receive(:checkout).with(subject.payment_data)
+  describe "#send_to_gateway" do
+    before(:each) do
       payment.stub(:build_response)
       payment.stub(:gateway_response_status).and_return(Payment::SUCCESSFUL_STATUS)
       payment.stub(:gateway_transaction_status).and_return(:success)
       subject.stub(:payment_url).and_return('www.fake.com')
+    end
+
+    #weird
+    it "gets freight from CartService" do
+      CartService.any_instance.should_receive(:freight).and_return(freight)
       subject.send_to_gateway
     end
 
-    it "should set payment gateway" do
-      subject.set_payment_gateway
-      subject.payment.gateway.should eq(1)
+    it "passes payment data to Moip::Client" do
+      CartService.any_instance.stub(:freight).and_return(freight)
+      MoIP::Client.should_receive(:checkout).with(subject.payment_data)
+      subject.send_to_gateway
     end
+  end
 
-    it "should get the payment_url" do
+  describe "#set_payment_gateway" do
+    it "sets payment.gateway" do
+      subject.set_payment_gateway
+      expect(subject.payment.gateway).to eq(1)
+    end
+  end
+
+  describe "#payment_url" do
+    it "passes the response token to the MoIP::Client" do
       subject.response = {"Token" => "XCV"}
       MoIP::Client.should_receive(:moip_page).with(subject.response["Token"])
       subject.payment_url
     end
+  end
 
-    it "should return the payer" do
+  describe "#payer" do
+    it "returns a hash with the customer's data" do
       CartService.any_instance.should_receive(:freight).and_return(freight)
       delivery_address = order.freight.address
       expected = {
@@ -75,73 +94,81 @@ describe Payments::MoipSenderStrategy do
 
       subject.payer.should == expected
     end
+  end
 
+  describe "#payment_data" do  
+    before(:each) { CartService.any_instance.should_receive(:freight).twice.and_return(freight) }
+    
+    context "for billet" do
+      it "returns a hash with its payment data" do
+        subject.payment = billet
+        expected_expiration_date = billet.payment_expiration_date.strftime("%Y-%m-%dT15:00:00.0-03:00")
+        expected = { :valor => order_total, :id_proprio => billet.identification_code,
+                    :forma => billet.to_s, :recebimento => billet.receipt, :pagador => subject.payer,
+                    :razao=> Payment::REASON, :data_vencimento => expected_expiration_date }
 
-    it "should return payment data for billet" do
-      CartService.any_instance.should_receive(:freight).twice.and_return(freight)
-      subject.payment = billet
-      expected_expiration_date = billet.payment_expiration_date.strftime("%Y-%m-%dT15:00:00.0-03:00")
-      expected = { :valor => order_total, :id_proprio => billet.identification_code,
-                  :forma => billet.to_s, :recebimento => billet.receipt, :pagador => subject.payer,
-                  :razao=> Payment::REASON, :data_vencimento => expected_expiration_date }
-
-      subject.payment_data.should == expected
+        subject.payment_data.should == expected
+      end
     end
 
-    it "should return payment data for debit" do
-      CartService.any_instance.should_receive(:freight).twice.and_return(freight)
-      subject.payment = debit
-      expected = { :valor => order_total, :id_proprio => debit.identification_code, :forma => subject.payment.to_s,
-                 :instituicao => debit.bank, :recebimento => debit.receipt, :pagador => subject.payer,
-                 :razao => Payment::REASON }
+    context "for debit" do
+      it "returns a hash with its payment data" do
+        subject.payment = debit
+        expected = { :valor => order_total, :id_proprio => debit.identification_code, :forma => subject.payment.to_s,
+                   :instituicao => debit.bank, :recebimento => debit.receipt, :pagador => subject.payer,
+                   :razao => Payment::REASON }
 
-      subject.payment_data.should == expected
+        subject.payment_data.should == expected
+      end
     end
 
-    it "should return payment data for credit card" do
-      CartService.any_instance.should_receive(:freight).twice.and_return(freight)
-      subject.payment = credit_card
-      subject.credit_card_number = credit_card.credit_card_number
-      payer = subject.payer
-      expected = { :valor => order_total, :id_proprio => credit_card.identification_code, :forma => subject.payment.to_s,
-                :instituicao => credit_card.bank, :numero => credit_card.credit_card_number,
-                :expiracao => credit_card.expiration_date, :codigo_seguranca => credit_card.security_code,
-                :nome => credit_card.user_name, :identidade => credit_card.user_identification,
-                :telefone => credit_card.telephone, :data_nascimento => credit_card.user_birthday,
-                :parcelas => credit_card.payments, :recebimento => credit_card.receipt,
-                :pagador => payer, :razao => Payment::REASON }
+    context "for credit card" do
+      it "returns a hash with its payment data" do
+        subject.payment = credit_card
+        subject.credit_card_number = credit_card.credit_card_number
+        payer = subject.payer
+        expected = { :valor => order_total, :id_proprio => credit_card.identification_code, :forma => subject.payment.to_s,
+                  :instituicao => credit_card.bank, :numero => credit_card.credit_card_number,
+                  :expiracao => credit_card.expiration_date, :codigo_seguranca => credit_card.security_code,
+                  :nome => credit_card.user_name, :identidade => credit_card.user_identification,
+                  :telefone => credit_card.telephone, :data_nascimento => credit_card.user_birthday,
+                  :parcelas => credit_card.payments, :recebimento => credit_card.receipt,
+                  :pagador => payer, :razao => Payment::REASON }
 
-      subject.payment_data.should == expected
+        subject.payment_data.should == expected
+      end
     end
   end
 
-  context "#payment_successful" do
-    subject {Payments::MoipSenderStrategy.new(cart_service, payment)}
-
-    it "should return false when response status is not successful" do
-      credit_card.gateway_response_status = Payment::CANCELED_STATUS
-      subject.payment = credit_card
-      subject.payment_successful?.should eq(false)
+  describe "#payment_successful" do
+    context "response is cancelled" do
+      it "returns false" do
+        credit_card.gateway_response_status = Payment::CANCELED_STATUS
+        subject.payment = credit_card
+        subject.payment_successful?.should eq(false)
+      end
     end
 
-    it "should return false when transaction status is canceled" do
-      credit_card.gateway_response_status = Payment::SUCCESSFUL_STATUS
-      credit_card.gateway_transaction_status = Payment::CANCELED_STATUS
-      subject.payment = credit_card
-      subject.payment_successful?.should eq(false)
+    context "response is successful but transaction is cancelled" do
+      it "returns false" do
+        credit_card.gateway_response_status = Payment::SUCCESSFUL_STATUS
+        credit_card.gateway_transaction_status = Payment::CANCELED_STATUS
+        subject.payment = credit_card
+        subject.payment_successful?.should eq(false)
+      end
     end
 
-    it "should return false when transaction status is canceled" do
-      credit_card.gateway_response_status = Payment::SUCCESSFUL_STATUS
-      credit_card.gateway_transaction_status = Payment::SUCCESSFUL_STATUS
-      subject.payment = credit_card
-      subject.payment_successful?.should eq(true)
+    context "response is successful and transaction is successful" do
+      it "returns true" do
+        credit_card.gateway_response_status = Payment::SUCCESSFUL_STATUS
+        credit_card.gateway_transaction_status = Payment::SUCCESSFUL_STATUS
+        subject.payment = credit_card
+        subject.payment_successful?.should eq(true)
+      end
     end
   end
 
-  context "formatting the telephone number" do
-    subject {Payments::MoipSenderStrategy.new(cart_service, payment)}
-
+  describe "#format_telephone" do
     it "should remove the ninth digit of the telephone" do
       subject.format_telephone("(11)99123-4567").should eq("(11)9123-4567")
     end
