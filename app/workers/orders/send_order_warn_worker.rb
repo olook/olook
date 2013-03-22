698e0b92eac0f93be7a3a309ff6cb6a8567e271e
Tiago Almeida <tiago.almeida@olook.com.br>
2013-03-22 13:58:19 -0300

    Fix deliver method name

--- app/workers/orders/send_order_warn_worker.rb
+++ app/workers/orders/send_order_warn_worker.rb
@@ -5,6 +5,6 @@ class SendOrderWarnWorker
   def self.perform
     warn_payments = BraspagAuthorizeResponse.find_by_sql("select p.id, a.identification_code, p.state, a.created_at from braspag_authorize_responses a left join braspag_capture_responses c  on a.identification_code = c.identification_code join payments p on a.identification_code = p.identification_code  where c.id is null and a.status = 1 and (a.created_at >= '#{(Time.zone.today - 15.days).strftime('%Y-%m-%d %H:%M:%S')}' and a.created_at <= '#{(Time.zone.today - 2.days).strftime('%Y-%m-%d %H:%M:%S')}') and p.state <> 'cancelled' order by a.created_at desc")
     warn_orders = Order.select("number, created_at").where(state: "Authorized").where(erp_payment_at: nil).order("created_at DESC")
-    DevAlertMailer.braspag_capture_warn(warn_payments, warn_orders).delivery unless warn_payments.empty? && warn_orders.empty?
+    DevAlertMailer.braspag_capture_warn(warn_payments, warn_orders).deliver unless warn_payments.empty? && warn_orders.empty?
   end
 end
