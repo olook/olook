035ebb08c6fe855cf302c6fe464ec0de4d2a35f2
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-22 11:10:58 -0300

    adding picture image as a required field and fixing pictures factories to upload an existing image

--- spec/factories/picture.rb
+++ spec/factories/picture.rb
@@ -1,10 +1,11 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :picture do
-    image {"PIC_#{Random.rand 1000}"}
+    image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
     association :product, :factory => [:shoe, :casual]
 
     factory :main_picture do
+      image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
       display_on DisplayPictureOn::GALLERY_1
     end
 
--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -4,6 +4,7 @@ require 'spec_helper'
 describe Picture do
   context "validation" do
     it { should validate_presence_of(:product) }
+    it { should validate_presence_of(:image) }
     it { should belong_to(:product) }
   end
 
