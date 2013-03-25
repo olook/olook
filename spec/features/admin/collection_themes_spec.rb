9825c3c094b2c7a64f98dfeb029791c5bdd49259
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-25 13:09:10 -0300

    Adding integration test to admin

--- spec/features/admin/collection_themes_spec.rb
+++ spec/features/admin/collection_themes_spec.rb
@@ -10,18 +10,21 @@ feature "Admin user manages collection_themes", %q{
 
   before :each do
     @admin = FactoryGirl.create(:admin_business1)
+    [:index, :new, :show, :edit, :update, :create, :destroy].each { |a| p = Permission.create(model_name: 'CollectionTheme', action_name: a); p.roles << @admin.role }
   end
 
   scenario "As a business1 user I should be able to create a CollectionTheme" do
     do_admin_login!(@admin)
     visit '/admin'
-    click 'Coleções Temáticas'
-    click 'Nova Coleção Temática'
+    click_link 'Coleções temáticas'
+    click_link 'Nova Coleção Temática'
     fill_in 'collection_theme_collection_theme_group_attributes_name', with: 'Grupo TESTE'
     fill_in 'collection_theme_name', with: 'Coleção Teste'
-    click 'collection_theme_active'
-
-    raise 'Finish this test!'
+    fill_in 'collection_theme_slug', with: 'colecao-teste'
+    check 'collection_theme_active'
+    attach_file 'collection_theme_header_image', File.join(fixture_path, 'collection_theme_header_image.jpg')
+    click_button 'Criar Coleção temática'
+    page.should have_content('Tema: Coleção Teste')
   end
 
 end
