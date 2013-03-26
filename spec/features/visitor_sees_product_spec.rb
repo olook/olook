023f7075012bc3bbcc35d97f5bc93c07b9df6e44
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-03-26 17:01:16 -0300

    testing user showroom with basic products info (accessory, shoe, bag and cloth)

--- spec/features/visitor_sees_product_spec.rb
+++ spec/features/visitor_sees_product_spec.rb
@@ -1,4 +1,7 @@
+ # -*- encoding : utf-8 -*-
+
 require 'spec_helper'
+require 'features/helpers'
 
 feature "Visitor sees Products", %q{
   In order to ensure that all product partials are visible to visitor
@@ -6,7 +9,50 @@ feature "Visitor sees Products", %q{
   To see if a product is present
 } do
 
+  let!(:casual_profile) { FactoryGirl.create(:casual_profile, :with_points, :with_user) }
+  let!(:user) { casual_profile.users.first }
+  let!(:collection) { FactoryGirl.create(:collection) }
+  let!(:shoe) { FactoryGirl.create(:shoe, :in_stock, collection: collection) }
+  let!(:bag)  { FactoryGirl.create(:bag, :in_stock, collection: collection) }
+  let!(:accessory) { FactoryGirl.create(:basic_accessory, :in_stock, collection: collection) }
+  let!(:cloth) { FactoryGirl.create(:simple_garment, :in_stock, collection: collection) }
+
   scenario "Member Showroom" do
+    user.update_attribute(:password, '123456')
+    user.update_attribute(:password_confirmation, '123456')
+
+    casual_profile.products.push(shoe, bag, accessory, cloth)
+    do_login!(user)
+
+    visit "/"
+
+    within("#clothes_container") do
+      expect(page).to have_content('Suas roupas')
+      expect(page).to have_content(cloth.formatted_name)
+      expect(page).to have_content(cloth.brand)
+      expect(page).to have_content(number_to_currency(cloth.price))
+    end
+
+    within("#shoes_container") do
+      expect(page).to have_content('Seus sapatos')
+      expect(page).to have_content(shoe.formatted_name)
+      expect(page).to have_content(shoe.brand)
+      expect(page).to have_content(number_to_currency(shoe.price))
+    end
+
+    within("#purse_container") do
+      expect(page).to have_content('Suas bolsas')
+      expect(page).to have_content(bag.formatted_name)
+      expect(page).to have_content(bag.brand)
+      expect(page).to have_content(number_to_currency(bag.price))
+    end
+
+    within("#accessories_container") do
+      expect(page).to have_content('Seus acessórios')
+      expect(page).to have_content(accessory.formatted_name)
+      expect(page).to have_content(accessory.brand)
+      expect(page).to have_content(number_to_currency(accessory.price))
+    end
   end
 
   scenario "Lookbooks" do
