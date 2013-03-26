77a7f595a6cc9763b2b0d3952f487004d041535d
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-03-26 15:37:09 -0300

    Adding new feature spec

---
+++ spec/features/visitor_sees_product_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+feature "Visitor sees Products", %q{
+  In order to ensure that all product partials are visible to visitor
+  I want to access all 7 sections of the site
+  To see if a product is present
+} do
+
+  scenario "Member Showroom" do
+  end
+
+  scenario "Lookbooks" do
+  end
+
+  scenario "Catalog Clothes" do
+  end
+
+  scenario "Catalog Shoes" do
+  end
+
+  scenario "Catalog Bags" do
+  end
+
+  scenario "Catalog Accessories" do
+  end
+
+  scenario "Gift index" do
+  end
+
+  scenario "Gift profiles" do
+  end
+
+  scenario "Gift quiz" do
+  end
+end
