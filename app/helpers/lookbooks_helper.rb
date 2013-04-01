99dc244c1716671cc7cb1add6125ab816ef2e0f4
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-04-01 19:17:34 -0300

    using product of liquidation product instead liquidation product in olooklet pages

--- app/helpers/lookbooks_helper.rb
+++ app/helpers/lookbooks_helper.rb
@@ -1,7 +1,7 @@
 module LookbooksHelper
   def get_partial(product)
     if product.liquidation?
-      render "shared/product_item", :product => LiquidationProductService.liquidation_product(product)
+      render "shared/product_item", :product => LiquidationProductService.liquidation_product(product).product
     else
       render "shared/product_item", :product => product
     end
