7264df54e84b4fa1f7a16b8f933aae4285e4c77e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-04-03 11:40:31 -0300

    Expiration of cache when inventory increments

--- app/observers/product_observer.rb
+++ app/observers/product_observer.rb
@@ -2,6 +2,12 @@ class ProductObserver < ActiveRecord::Observer
   observe :variant
 
   def after_update(variant)
-    variant.product.delete_cache if variant.inventory.to_i == 0
+    inventory_changes = variant.changes['inventory']
+    if inventory_changes && (
+      (inventory_changes.first.to_i.zero? && inventory_changes.last.to_i > 0) ||
+      (inventory_changes.last.to_i.zero? && inventory_changes.first.to_i > 0)
+    )
+      variant.product.delete_cache
+    end
   end
 end
