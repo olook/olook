d663afcf068c8ef74d0b6c2715ec4cb2c1d1b5c1
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-04-11 14:08:59 -0300

    Adding strip on all uploaders

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 class ImageUploader < CarrierWave::Uploader::Base
-
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -22,9 +21,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process :scale => [200, 300]
   #
-  def scale(width, height)
-    # do something
-  end
+  process :strip
+
+  #def scale(width, height)
+  #  # do something
+  #end
 
   # Create different versions of your uploaded files:
   version :thumb do
--- app/uploaders/temp_file_uploader.rb
+++ app/uploaders/temp_file_uploader.rb
@@ -1,7 +1,11 @@
 # -*- encoding : utf-8 -*-
 class TempFileUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :fog
-  
+
+  process :strip
+
   def filename
     @name ||= "#{SecureRandom.uuid}.#{file.extension}" if original_filename.present?
   end
