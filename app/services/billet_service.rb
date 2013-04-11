835b7d5531f39e41704d9dc47b1a68c3f34976ac
Tiago Almeida <tiago.almeida@olook.com.br>
2013-04-11 17:47:50 -0300

    Rollback billet new logic

--- app/services/billet_service.rb
+++ app/services/billet_service.rb
@@ -1,46 +1,41 @@
 # -*- encoding : utf-8 -*-
 class BilletService
 
-  def initialize(file_name)
-    @successful_array, @unsuccessful_array = [],[]
-    @file_name = file_name
-    @billet_numbers_array = parse_file
-  end
-
   def self.process_billets(file_name)
-    new(file_name)
-    @billet_numbers_array.each do |line_billet|
-      billet_id = line_billet[27..38].to_i.to_s
+    successful_array, unsuccessful_array = [],[]
+    billet_numbers_array = parse_file(file_name)
+    billet_numbers_array.each do |line_billet|
+    billet_id = line_billet[27..38].to_i.to_s
 
       billet = Billet.find_by_id billet_id
       if billet.blank?
-        @unsuccessful_array << {id: billet_id, message: "Pedido não encontrado"}
+        unsuccessful_array << {id: billet_id, message: "Pedido não encontrado"}
       elsif billet.state != "waiting_payment"
-        @unsuccessful_array << {id: billet_id, message: "Estado '#{billet.state}' não elegível para autorização de pagamento"}
+        unsuccessful_array << {id: billet_id, message: "Estado '#{billet.state}' não elegível para autorização de pagamento"}
       elsif billet && billet.try(:total_paid).to_s != line_billet[58..69].gsub(' ','').gsub(',','.')
-        @unsuccessful_array << {id: billet_id, message: "Valor pago não confere com o valor do boleto"}
+        unsuccessful_array << {id: billet_id, message: "Valor pago não confere com o valor do boleto"}
       elsif line_billet[58..69].gsub(' ','') != line_billet[98..107].gsub(' ','')
-        @unsuccessful_array << {id: billet_id, message: "Valor pago não confere com o valor do título"}
+        unsuccessful_array << {id: billet_id, message: "Valor pago não confere com o valor do título"}
       else
         begin
-          #billet.authorize
-          @successful_array << {id: billet_id, message: "OK"}
+          billet.authorize
+          successful_array << {id: billet_id, message: "OK"}
         rescue => e
           Airbrake.notify(
             :error_class   => "Admin::ProcessBilletFileWorker",
             :error_message => "process_billets: the following error occurred: #{e.message}"
           )
-          @unsuccessful_array << {id: billet_id, message: "Problema na transição de status do boleto"}
+          unsuccessful_array << {id: billet_id, message: "Problema na transição de status do boleto"}
         end
       end
     end
-    { successful: @successful_array, unsuccessful: @unsuccessful_array }
+    { successful: successful_array, unsuccessful: unsuccessful_array }
   end
 
 
   def self.save_file(file_content, file_name)
     MarketingReports::FileUploader.new(file_name, file_content).save_local_file
-    MarketingReports::FileUploader.copy_file(file_name, "/tmp") if Rails.env.production?
+    MarketingReports::FileUploader.copy_file(file_name, "/home/allinmail/retorno_santander") if Rails.env.production?
   end
 
   def self.file_name
@@ -48,8 +43,8 @@ class BilletService
   end
 
   private
-    def parse_file
-      file_content = read_file
+    def self.parse_file(billet_file_name)
+      file_content = read_file(billet_file_name)
       file_content.gsub!(/\r\n?/, "\n");
       line_array = file_content.split("\n")
       start_line = 0
@@ -62,11 +57,11 @@ class BilletService
       line_array[start_line..end_line]
     end
 
-    def read_file
-      File.open("#{file_path}#{@file_name}", "r").read
+    def self.read_file(billet_file_name)
+      File.open("#{file_path}#{billet_file_name}", "r").read
     end
 
-    def file_path
-      Rails.env.production? ? "to be defined" : "#{Rails.root}/tmp/"
+    def self.file_path
+      Rails.env.production? ? "/home/allinmail/retorno_santander/" : "#{Rails.root}/tmp/"
     end
 end
