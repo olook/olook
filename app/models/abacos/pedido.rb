d5d93a1d87435bcc029cafcf6ab5d75cfef0fc87
darkseid <rafael.manoel@gmail.com>
2013-04-19 11:38:30 -0300

    change RepresentanteVendas' field value

--- app/models/abacos/pedido.rb
+++ app/models/abacos/pedido.rb
@@ -38,7 +38,7 @@ module Abacos
           'DadosPedidos' => {
             'NumeroDoPedido'           => @numero,
             'CodigoCliente'            => @codigo_cliente,
-            'RepresentanteVendas'      => '1',
+            'RepresentanteVendas'      => 'SITE',
             'CPFouCNPJ'                => @cpf,
             'DataVenda'                => @data_venda,
             'DestNome'                 => @nome,
