a276ca117c7f46d3a2608c4c9aab983377839fc9
Tiago Almeida <tiago.almeida@olook.com.br>
2013-04-23 15:53:59 -0300

    Fix shoes colors find

--- app/services/catalog_search_service.rb
+++ app/services/catalog_search_service.rb
@@ -92,7 +92,7 @@ class CatalogSearchService
   end
 
   def query_colors
-    build_sub_query((query_heels || query_subcategories_for(@params[:shoe_subcategories]) || query_base), Detail.arel_table[:translation_token].eq("Cor filtro").and(Detail.arel_table[:description]).in(@params[:shoe_colors])) if @params[:shoe_colors]
+    build_sub_query((query_heels || query_subcategories_for(@params[:shoe_subcategories]) || query_base), Detail.arel_table[:translation_token].eq("Cor filtro").and(Detail.arel_table[:description].in(@params[:shoe_colors]))) if @params[:shoe_colors]
   end
 
   def query_heels
