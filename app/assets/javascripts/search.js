f52afc35b6401e658aa1b95ecb35b5d8bc96792e
darkseid <rafael.manoel@gmail.com>
2013-04-24 19:33:26 -0300

    index the formatted name of the product

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,6 +2,14 @@
 // All this logic will automatically be available in application.js.
 $(function() {
 
+    $("#search_product").keypress(function(e) {
+      if (e.which == 13) {
+        $("form#search_form").submit();
+        e.preventDefault();
+        return false;
+      }
+    });
+
     $("#search_product").autocomplete({
         source: "/search/product_suggestions"
     });
