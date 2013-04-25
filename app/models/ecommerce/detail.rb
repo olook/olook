f2f8842e584d75f427cc01bf1f27702350ba818d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-04-25 11:52:25 -0300

    Add scope to detail class to find filter colors from one product category

--- app/models/ecommerce/detail.rb
+++ app/models/ecommerce/detail.rb
@@ -1,15 +1,16 @@
 # -*- encoding : utf-8 -*-
 class Detail < ActiveRecord::Base
   belongs_to :product
-  
+
   validates :product, :presence => true
   validates :translation_token, :presence => true
   validates :description, :presence => true
 
   has_enumeration_for :display_on, :with => DisplayDetailOn, :required => true
-  
+
   scope :only_invisible     , where(:display_on => DisplayDetailOn::INVISIBLE)
   scope :only_specification , where(:display_on => DisplayDetailOn::SPECIFICATION).where("translation_token not like 'Cor%'")
   scope :with_valid_values  , where("description <> '_'")
   scope :only_how_to        , where(:display_on => DisplayDetailOn::HOW_TO)
+  scope :colors, lambda {|product_category| Detail.joins(:product).where("details.translation_token = 'Cor filtro'").where('products.is_visible = true').where("products.category = #{product_category}")}
 end
