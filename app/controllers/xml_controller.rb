264db652ed48d5ac3165970d39cdf52e7c2ad268
Tiago Almeida <tiago.almeida@olook.com.br>
2013-04-26 15:00:28 -0300

    Add method to fetch liquidations products ids

--- app/controllers/xml_controller.rb
+++ app/controllers/xml_controller.rb
@@ -2,6 +2,7 @@ class XmlController < ApplicationController
 
   respond_to :xml
   before_filter :load_products, except: [:criteo]
+  before_filter :liquidation_products, except: [:criteo]
 
   def sociomantic
     respond_with(@products)
@@ -34,6 +35,7 @@ class XmlController < ApplicationController
   end
 
   def shopping_uol
+    remove_liquidation_products
     respond_with(@products)
   end
 
@@ -53,6 +55,16 @@ class XmlController < ApplicationController
 
   def load_products
     @products = Product.valid_for_xml(Product.xml_blacklist("products_blacklist"), Product.xml_blacklist("collections_blacklist"))
+  end
+
+  def liquidation_products
+    @liquidation_products = []
+    active_liquidation = LiquidationService.active
+    @liquidation_products = active_liquidation.resume[:products_ids] if active_liquidation
+  end
+
+  def remove_liquidation_products
+    @products.delete_if{|product| @liquidation_products.include?(product.id)}
   end
 
 end
