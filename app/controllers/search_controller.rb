7200d243b614855b3fc53f85e167182c1b2ef700
darkseid <rafael.manoel@gmail.com>
2013-04-30 15:49:09 -0300

    use a new class for simulating product

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,6 @@ class SearchController < ApplicationController
   end
 
   def show
-    #url = URI.parse("http://search-olook-products-m7cmewyo4k3ih7na2nzixh5jae.us-east-1.cloudsearch.amazonaws.com/2011-02-01/search?q=#{CGI.escape params[:q]}&return-fields=categoria,name,description,image,price,text_relevance")
     url = URI.parse("http://busca.olook.com.br/2011-02-01/search?q=#{CGI.escape params[:q]}&return-fields=categoria,name,brand,description,image,price,backside_image,category,text_relevance")
     response = Net::HTTP.get_response(url)
 
@@ -20,7 +19,6 @@ class SearchController < ApplicationController
     @brands = {}
 
     @products = @hits["hit"].map do |hit| 
-
       data = hit["data"]
       brand = data["brand"][0].downcase.strip
       category = data["categoria"][0].downcase.strip
@@ -36,17 +34,9 @@ class SearchController < ApplicationController
       @brands[brand] = @brands[brand].to_i + 1
       @categories[category] = @categories[category].to_i + 1
 
-      OpenStruct.new(
-        id: hit["id"].to_i, 
-        formatted_name: data["name"][0], 
-        model_name: data["categoria"][0],
-        category: data["category"][0],
-        catalog_image: data["image"][0], 
-        backside_picture: data["backside_image"][0],
-        price: BigDecimal.new(data["price"][0]), 
-        promotion?: false
-      )
+      SearchedProduct.new(hit["id"].to_i, data)
     end
+
 
     @show_filters = wanted_category.nil? && wanted_brand.nil?
     @products.compact!
@@ -57,6 +47,29 @@ class SearchController < ApplicationController
 
   def product_suggestions
     render json: ProductSearch.terms_for(params[:term].downcase)
+  end
+
+  class SearchedProduct
+
+    attr_accessor :id, :formatted_name, :model_name, :category, :catalog_image, :backside_picture, :price, :brand
+    attr_writer :promotion
+
+    def initialize id, data
+      self.id = id
+      self.formatted_name = data["name"][0] 
+      self.model_name = data["categoria"][0]
+      self.category = data["category"][0]
+      self.catalog_image = data["image"][0]
+      self.backside_picture = data["backside_image"][0]
+      self.brand = data["brand"][0]
+      self.price = BigDecimal.new(data["price"][0])
+      self.promotion = false
+    end
+
+    def promotion?
+      @promotion
+    end
+
   end
 
 end
