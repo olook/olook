d66b5f981431af6e3ffe20f3f7f9e628d0e519ad
darkseid <rafael.manoel@gmail.com>
2013-04-30 18:08:53 -0300

    parameterize shoe-size for cache key

--- app/models/ecommerce/product.rb
+++ app/models/ecommerce/product.rb
@@ -436,7 +436,7 @@ class Product < ActiveRecord::Base
   end
 
   def item_view_cache_key_for(shoe_size=nil)
-    shoe? ? CACHE_KEYS[:product_item_partial_shoe][:key] % [id, shoe_size] : CACHE_KEYS[:product_item_partial][:key] % id
+    shoe? ? CACHE_KEYS[:product_item_partial_shoe][:key] % [id, shoe_size.to_s.parameterize] : CACHE_KEYS[:product_item_partial][:key] % id
   end
 
   def brand
