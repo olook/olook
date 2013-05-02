a1debd514c0674b897e2dcee734557583b14da80
darkseid <rafael.manoel@gmail.com>
2013-05-02 16:30:31 -0300

    improve search controller readability and upload base.sdf on indexProductsWorker

--- app/workers/index_products_worker.rb
+++ app/workers/index_products_worker.rb
@@ -1,9 +1,30 @@
 # -*- encoding : utf-8 -*-
 class IndexProductsWorker
+
+  SEARCH_CONFIG = YAML.load_file("#{Rails.root}/config/cloud_search.yml")[Rails.env]
+
   @queue = :product
 
   def self.perform
     all_products=products_to_index.map do |product|
+      create_sdf_entry_for product
+    end
+
+    flush_to_sdf_file all_products
+    upload_sdf_file
+  
+  end
+
+  private
+
+    def self.flush_to_sdf_file all_products
+      File.open("base.sdf", "w:UTF-8") do |file| 
+        file << all_products.to_json
+      end
+    end
+
+
+    def self.create_sdf_entry_for product
 
       values = {
         'type' => "add",
@@ -18,7 +39,7 @@ class IndexProductsWorker
 
       fields['name'] = product.name
       fields['description'] = product.description
-      fields['image'] = main_pic.image_url(:catalog)
+      fields['image'] = product.main_picture.image_url(:catalog)
       fields['backside_image'] = product.backside_picture
       fields['brand'] = product.brand
       fields['price'] = product.retail_price
@@ -28,27 +49,20 @@ class IndexProductsWorker
       details = product.details.delete_if { |d| d.translation_token.downcase == 'salto/tamanho'}
 
       details.each do |detail|
-        # fields[detail.translation_token.to_sym] = detail.description
         fields[detail.translation_token.downcase.gsub(" ","_")] = detail.description
       end
 
       values['fields'] = fields
-      values
+      values      
     end
 
-    File.open("base.sdf", "w:UTF-8") do |file| 
-      file << all_products.to_json
+    def self.upload_sdf_file
+      docs_domain =  SEARCH_CONFIG["docs_domain"]
+      `curl -X POST --upload-file base.sdf "#{docs_domain}"/2011-02-01/documents/batch --header "Content-Type:application/json"`
     end
-
-    puts "Finished"
-    # TODO: upload base.sdf to amazon 
-  
-  end
-
-  private
 
     def self.products_to_index
-      Product.where("collection_id = 20 and is_visible = 1").select{|p| p.inventory > 0 && p.price > 0 && p.main_picture.try(:image_url)}
+      Product.where("collection_id = 20 and is_visible = 1").limit(10).select{|p| p.inventory > 0 && p.price > 0 && p.main_picture.try(:image_url)}
     end
 
 end
