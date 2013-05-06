ec05dccc5da8d7034837d7fc6d705ce335a60b06
Rafael Carvalho <rafa1402@gmail.com>
2013-05-06 16:33:47 -0300

    bugfix jquery cookie

--- app/assets/javascripts/lite_application.js
+++ app/assets/javascripts/lite_application.js
@@ -8,6 +8,7 @@
 //= require ./plugins/jquery.jqzoom-core
 //= require ./plugins/jquery.carouFredSel-6.2.1-packed
 //= require ./common/product_view
+//= require ./common/jquery.cookie
 //= require ./common/lite_base
 //= require ./ui_v1.10/jquery-ui-1.10.2.custom.min
 //= require ./search
