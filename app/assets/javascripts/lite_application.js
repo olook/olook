4ed727d397e9fccb0a073be9fe860c65d700a404
Rafael Carvalho <rafa1402@gmail.com>
2013-05-07 15:38:15 -0300

    changed file place

--- app/assets/javascripts/lite_application.js
+++ app/assets/javascripts/lite_application.js
@@ -9,6 +9,6 @@
 //= require ./plugins/jquery.carouFredSel-6.2.1-packed
 //= require ./common/product_view
 //= require ./common/jquery.cookie
-//= require ./common/lite_base
+//= require ./lite_base
 //= require ./ui_v1.10/jquery-ui-1.10.2.custom.min
 //= require ./search
