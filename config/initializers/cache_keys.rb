d0d42d9935df87167a89348d813d5ff24872e3c7
darkseid <rafael.manoel@gmail.com>
2013-05-09 15:58:18 -0300

    add a missing comma

--- config/initializers/cache_keys.rb
+++ config/initializers/cache_keys.rb
@@ -7,6 +7,6 @@ CACHE_KEYS = {
   product_colors: { key: "p:colors:%s:%s", expire: 30*60 },
   product_clothes_for_profile: { key: "SR:%s", expire: 10*60 },
   product_fetch_all_featured_products_of: { key: "featured_products_%s", expire: 10*60 },
-  product_search: { key: "ps:%s", expire: 10*60*60 }
+  product_search: { key: "ps:%s", expire: 10*60*60 },
   detail_color: { key: "detail:colors:%s", expire: 30*60 }
 }
