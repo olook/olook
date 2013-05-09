4c4fed85d1a4be7c4bcf0a53cbc12434e2ad99af
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-09 15:28:03 -0300

    removing moments_helper

--- app/helpers/moments_helper.rb
+++
@@ -1,6 +0,0 @@
-module MomentsHelper
-  def some_filter_selected_for? category
-    basic_request = {"category_id"=> category, "id"=>1, "controller"=>"moments", "action"=>"show"}
-    basic_request != params
-  end
-end
