4e0fb85defc36a5ed8d5f3480b3856568b545f93
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-05-10 13:21:41 -0300

    Updating schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506224333) do
+ActiveRecord::Schema.define(:version => 20130510150313) do
 
   create_table "action_parameters", :force => true do |t|
     t.integer  "promotion_id"
@@ -180,8 +180,8 @@ ActiveRecord::Schema.define(:version => 20130506224333) do
   create_table "carts", :force => true do |t|
     t.integer  "user_id"
     t.boolean  "notified",                :default => false, :null => false
-    t.datetime "created_at",                                 :null => false
-    t.datetime "updated_at",                                 :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "legacy_id"
     t.boolean  "gift_wrap",               :default => false
     t.boolean  "use_credits",             :default => false
@@ -911,16 +911,6 @@ ActiveRecord::Schema.define(:version => 20130506224333) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "settings", :force => true do |t|
     t.string   "var",                      :null => false
