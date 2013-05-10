10e5c0c7ebc66cd792f5cd7c4e019f74d4997fcd
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-05-10 14:48:21 -0300

    Rollbacking to active_record_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,11 +1,7 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.production?
-  Olook::Application.config.session_store :active_record_store
-else
-  Olook::Application.config.session_store :cookie_store, key: '_olook_session'
-end
+Olook::Application.config.session_store :active_record_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
