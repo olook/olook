47a90f4a764272c38f7a3106637325365702cbfc
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-05-10 14:31:04 -0300

    Readd session table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130510150313) do
+ActiveRecord::Schema.define(:version => 20130506224333) do
 
   create_table "action_parameters", :force => true do |t|
     t.integer  "promotion_id"
@@ -911,6 +911,16 @@ ActiveRecord::Schema.define(:version => 20130510150313) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "settings", :force => true do |t|
     t.string   "var",                      :null => false
