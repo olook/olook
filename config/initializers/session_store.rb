4fa127e950a4c4bc13385ed10c73d78a6ae087ea
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-05-10 13:36:23 -0300

    Set session_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,10 +1,13 @@
 # -*- encoding : utf-8 -*-
 # Be sure to restart your server when you modify this file.
 
-#Olook::Application.config.session_store :cookie_store, key: '_olook_session'
+if Rails.env.production?
+  Olook::Application.config.session_store :dalli_store, 'appcache.o2ltwu.0001.use1.cache.amazonaws.com',{ :namespace => 'olook', :expires_in => 1.day , :compress => true }
+else
+  Olook::Application.config.session_store :cookie_store, key: '_olook_session'
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
 
-Olook::Application.config.session_store :cache_store
