558525f46713bb37748c417f4d5db1d0138b67a0
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-20 19:13:48 -0300

    removing method Product.remove_color_variations and extending ProductFinder module

--- app/models/ecommerce/product.rb
+++ app/models/ecommerce/product.rb
@@ -8,7 +8,7 @@ class Product < ActiveRecord::Base
   QUANTITY_OPTIONS = {1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5}
   MINIMUM_INVENTORY_FOR_XML = 3
 
-  include ProductFinder
+  extend ProductFinder
 
   has_enumeration_for :category, :with => Category, :required => true
 
@@ -288,30 +288,6 @@ class Product < ActiveRecord::Base
       :utm_content => id
     }
     Rails.application.routes.url_helpers.product_url(self, params.merge!(options))
-  end
-
-  def self.remove_color_variations(products)
-    result = []
-    already_displayed = []
-    displayed_and_sold_out = {}
-
-    products.each do |product|
-      # Only add to the list the products that aren't already shown
-      unless already_displayed.include?(product.name)
-        result << product
-        already_displayed << product.name
-        displayed_and_sold_out[product.name] = result.length - 1 if product.sold_out?
-      else
-        # If a product of the same color was already displayed but was sold out
-        # and the algorithm find another color that isn't, replace the sold out one
-        # by the one that's not sold out
-        if displayed_and_sold_out[product.name] && !product.sold_out?
-          result[displayed_and_sold_out[product.name]] = product
-          displayed_and_sold_out.delete product.name
-        end
-      end
-    end
-    result
   end
 
   def subcategory
