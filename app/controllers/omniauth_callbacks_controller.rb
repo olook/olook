5ae0558dbe9c4c67009e687557bf588ff19b4652
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-20 16:41:38 -0300

    redirecting to gift path in ominiauth callback controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -40,7 +40,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     #
     # Clearly this isn't the best place to put this code, but the problem is that putting this
-    # logic in ApplicationController#current_referer doesn't work because the user is not 
+    # logic in ApplicationController#current_referer doesn't work because the user is not
     # loaded yet on that point
     #
     def redirect user
@@ -49,7 +49,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       elsif !user.half_user?
         redirect_to member_showroom_path
       else
-        redirect_to lookbooks_path
+        redirect_to gift_root_path
       end
     end
 
