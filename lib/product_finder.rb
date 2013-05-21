c9dc35a7157a32fb7a7099141820141410b03a0c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-21 10:47:14 -0300

    more refactor

--- lib/product_finder.rb
+++ lib/product_finder.rb
@@ -6,7 +6,7 @@ module ProductFinder
     already_displayed = []
     displayed_and_sold_out = {}
 
-    products.each do |product|
+    products.each_with_index do |product, index|
       # Only add to the list the products that aren't already shown
       if already_displayed.include?(product.producer_code)
         # If a product of the same color was already displayed but was sold out
@@ -19,7 +19,7 @@ module ProductFinder
       else
         result << product
         already_displayed << product.producer_code
-        displayed_and_sold_out[product.producer_code] = result.length - 1 if product.sold_out?
+        displayed_and_sold_out[product.producer_code] = index if product.sold_out?
       end
     end
     result.sort! {|product, product2| product2.inventory <=> product.inventory}
