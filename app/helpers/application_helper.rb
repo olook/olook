db567ef31cae08859c9fba518159cae93de7dc4f
Luis Daher <luisotaviodaher@gmail.com>
2013-05-22 11:29:16 -0300

    analytics tags for valentines day

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,6 +58,10 @@ module ApplicationHelper
     track_event("Shopping", "AddToCart#{ga_event_referer}", product_id)
   end
 
+  def track_add_to_cart_valentines_day_event(product_id = '')
+    track_event("Shopping", "AddToCartValentinesDay#{ga_event_referer}", product_id)
+  end
+
   def user_avatar(user, type = "large")
     "https://graph.facebook.com/#{user.uid}/picture?type=#{type}"
   end
