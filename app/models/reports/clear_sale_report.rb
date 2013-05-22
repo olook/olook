9e0797a38c47e1c33f8b3e12319efbea240dbc87
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-22 17:39:13 -0300

    creating structure to send email with clear sale reports in backgroud

--- app/models/reports/clear_sale_report.rb
+++ app/models/reports/clear_sale_report.rb
@@ -3,6 +3,6 @@ class ClearSaleReport < Report
   def schedule_generation
     start_date = Date.parse(@start_date)
     end_date = Date.parse(@end_date)
-    Resque.enqueue_at(10.seconds, ClearSaleReportWorker, start_date, end_date)
+    Resque.enqueue_at(10.seconds, ClearSaleReportWorker, start_date, end_date, @admin)
   end
 end
--- app/models/reports/report.rb
+++ app/models/reports/report.rb
@@ -1,6 +1,12 @@
 class Report
-  def initialize(start_date, end_date)
-    @start_date = start_date
-    @end_date = end_date
+  def initialize(start_date, end_date, admin)
+    @start_date = parse start_date
+    @end_date = parse end_date
+    @admin = admin
   end
+
+  private
+    def parse date
+      date.to_a.sort.collect{|c| c[1]}.join("-")
+    end
 end
