f14b9feb22bc3df312e499b5a71ea13e870ca812
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-05-23 13:45:26 -0300

    removing class 'Report'

--- app/models/reports/clear_sale_report.rb
+++ app/models/reports/clear_sale_report.rb
@@ -1,6 +1,18 @@
-class ClearSaleReport < Report
+class ClearSaleReport
+
+  def initialize(start_date, end_date, admin)
+    @start_date = parse start_date
+    @end_date = parse end_date
+    @admin = admin
+  end
 
   def schedule_generation
     Resque.enqueue(ClearSaleReportWorker, @start_date, @end_date, @admin.email)
   end
+
+  private
+
+    def parse date
+      date.to_a.sort.collect{|c| c[1]}.join("-")
+    end
 end
