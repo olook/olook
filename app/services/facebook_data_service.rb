f6863053f87ccb4b5cb89f3868c1b60f1607ff7c
Luis Daher <luisotaviodaher@gmail.com>
2013-05-27 14:20:26 -0300

    handling old facebook sessions

--- app/services/facebook_data_service.rb
+++ app/services/facebook_data_service.rb
@@ -6,9 +6,12 @@ class FacebookDataService
 
   def generate_csv_lines(start_date)
       csv_lines = ["email,first_name,auth_token,friend_data"]
-      FacebookDataService.facebook_users.each do |user|
-        user_hash = format_user_data(user, start_date) 
-        csv_lines << user_hash unless user_hash.blank?
+      FacebookDataService.facebook_users.last(10).each do |user|
+        begin
+          user_hash = format_user_data(user, start_date) 
+          csv_lines << user_hash unless user_hash.blank?
+        rescue Koala::Facebook::APIError => e
+        end
       end
       csv_lines    
   end
@@ -27,14 +30,21 @@ class FacebookDataService
   end
 
   def format_fb_friend_data(friend, start_date)
+    return nil unless friend.birthday
     birthday_arr = friend.birthday.split("/")
     friend.birthday = "#{birthday_arr[1]}/#{birthday_arr[0]}"
     friend_hash = JSON.parse(friend.to_json)["table"]
     friend_hash["picture"] = "https://graph.facebook.com/#{friend_hash['uid']}/picture"
-    add_friend_to_list?(friend_start_date) ? "#{friend_hash['first_name']}|#{friend_hash['picture']}|#{friend_hash['birthday']}" : nil
+    add_friend_to_list?(friend, start_date) ? "#{friend_hash['first_name']}|#{friend_hash['picture']}|#{friend_hash['birthday']}" : nil
   end
 
   def add_friend_to_list?(friend, start_date)
-    (start_date + 15.days) >= Date.strptime(friend.birthday, "%m/%d")
+    
+    friend_birthday_date = begin
+      Date.strptime("#{friend.birthday}", "%d/%m")
+    rescue
+      Date.strptime("1/03", "%d/%m") if friend.birthday.include?("29/02")
+    end
+    friend.birthday && (friend_birthday_date <= (start_date + 15.days)) && (friend_birthday_date >= start_date)
   end
 end
