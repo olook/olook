12c745aae0eccdf1f2d8af387ebf661b0ec4b45a
darkseid <rafael.manoel@gmail.com>
2013-05-28 19:01:34 -0300

    add new filters to BRANDs page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,50 +7,64 @@ class SearchController < ApplicationController
   end
 
   def show
+    @q = params[:q]
     @brand = params[:brand].humanize if params[:brand]
+    @color = params[:color]
+    @category = params[:category].parameterize if params[:category]
 
     url_builder = SearchUrlBuilder.new 
 
+    # Gambiarra temporária
+    if @brand == "All"
+      @q, @brand = "a*", nil
+      @dont_show_message = true
+    end
+
     url = url_builder
-      .for_term(params[:q])
-      .with_category(params[:category])
+      .for_term(@q)
+      .with_category(@category)
       .with_brand(@brand)
+      .with_color(@color)
+      .grouping_by
       .build_url
 
     # .build_url_with({category: params[:category], brand: params[:brand], rank: "cor_e_marca"})
 
     response = Net::HTTP.get_response(url)
     @hits = JSON.parse(response.body)["hits"]
+    @facets = JSON.parse(response.body)["facets"]
 
-
-    @model_names = {}
-    @brands = {}
+    @brands = group "brand_facet"
+    @model_names = group "categoria"
+    @colors = group "cor_filtro"
 
     @products = @hits["hit"].map do |hit|
-      data = hit["data"]
-      brand = data["brand"][0].nil? ? "" : data["brand"][0].strip.capitalize
-      model = data["categoria"][0].nil? ? "" : data["categoria"][0].strip.capitalize
-      category = data["category"][0].nil? ? "" : data["category"][0].strip.capitalize
-
-      @brands[brand] = @brands[brand].to_i + 1
-      @model_names[category] ||= {}
-      @model_names[category][model] = @model_names[category][model].to_i + 1
-
-      SearchedProduct.new(hit["id"].to_i, data)
+      SearchedProduct.new(hit["id"].to_i, hit["data"])
     end
 
-    # @show_filters = wanted_category.nil? && wanted_brand.nil?
     @show_filters = true
     @products.compact!
-    @q=params[:q]
-    #####END
 
-    ### to render home partials ###
     @stylist = Product.fetch_products :selection
   end
 
   def product_suggestions
     render json: ProductSearch.terms_for(params[:term].downcase)
   end
+
+  private
+    def group facet_name
+      facet_list = {}
+      return {} if @facets[facet_name].nil?
+
+      @facets[facet_name]["constraints"].map do |facet|
+        facet_name = facet["value"].humanize
+        facet_count = facet["count"]
+        facet_list[facet_name] = facet_count
+      end
+      facet_list
+    end
+
+  
 
 end
--- app/models/search/search_url_builder.rb
+++ app/models/search/search_url_builder.rb
@@ -7,6 +7,7 @@ class SearchUrlBuilder
   def initialize base_url=SEARCH_CONFIG["search_domain"] + "/2011-02-01/search"
     @base_url = base_url
     @expressions = []
+    @facets = []
   end
 
   def for_term term
@@ -24,11 +25,26 @@ class SearchUrlBuilder
     self
   end
 
+  def with_color color
+    @expressions << "cor_filtro:'#{CGI.escape color}'" unless color.nil? || color.empty?
+    self
+  end
+
+  def grouping_by
+    @facets << "brand_facet"
+    @facets << "categoria"
+    @facets << "cor_filtro"
+    self
+  end
+
   def build_url
     bq = build_boolean_expression
+    bq += "facet=#{@facets.join(',')}&" if @facets.any?
     q = @query ? "?#{@query}&" : "?"
     URI.parse("http://#{@base_url}#{q}#{bq}return-fields=categoria,name,brand,description,image,price,backside_image,category,text_relevance&size=100&rank=-cor_e_marca")
   end
+
+
 
   private
 
