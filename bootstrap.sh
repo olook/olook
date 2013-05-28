f020d63721a3e33c5c9cdd15ee8e53efcafb02f1
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-05-28 18:46:37 -0300

    Fix bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Copying gsg keys for deploy and servers access"
-[[ ! -a ~/.ec2 ]] && mkdir ~/.ec2
-cp ./config/deploy/gsg-keypair* ~/.ec2/
-chmod 0600 ~/.ec2/gsg-keypair*
+if [[ $1 == 'development' ]]; then
+  echo "Copying gsg keys for deploy and servers access"
+  mkdir -p ~/.ec2
+  cp ./config/deploy/gsg-keypair* ~/.ec2/
+  chmod 0600 ~/.ec2/gsg-keypair*
+fi
 
 for f in config/deploy/*.yml.erb; do
   dest="config/$(echo $f | sed 's/.*\/\(.*\).erb/\1/')"
-  cat $f | sed 's/<%= "#{application}_\([^"]*\)" %>/olook_\1/' | sed 's/<%.*%>//' > $dest
-  echo "$f -> $dest"
+  if [[ ! -e $dest ]]; then
+    cat $f | sed  's/<%= "#{Rubber.env}" %>/development/g' | sed 's/<%= "#{application}_\([^"]*\)" %>/olook_\1/' | sed 's/<%.*%>//' > $dest
+    echo "$f -> $dest"
+  fi
 done
