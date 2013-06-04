8dd5d1854bfcec7f373ba8a19ef734727f8f7402
darkseid <rafael.manoel@gmail.com>
2013-06-04 19:40:11 -0300

    refactor to improve code quality

---
+++ app/models/search/search_result.rb
@@ -0,0 +1,46 @@
+# -*- encoding : utf-8 -*-
+class SearchResult
+
+  attr_reader :products
+
+  def initialize response
+   @hits = JSON.parse(response.body)["hits"]
+   @facets = JSON.parse(response.body)["facets"]
+   parse_facets
+   parse_products
+  end
+
+  def found_products
+    @hits["found"]
+  end
+
+  def grouped_products key
+    @groups[key]
+  end
+
+  # TEMP
+  def hits
+    @hits
+  end
+
+  private
+
+    def parse_facets 
+      @groups = {}
+      @facets.map do |group_name, constraints|
+        @groups[group_name] = {}
+        constraints["constraints"].each do |c|
+          @groups[group_name][c["value"]] = c['count']
+        end
+      end
+    end  
+
+    def parse_products
+      @products = @hits["hit"].map do |hit|
+        SearchedProduct.new(hit["id"].to_i, hit["data"])
+      end
+
+      @products.compact!
+    end
+
+end
