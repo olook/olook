b462dd15415937516f9105d94014fa0ecaadc29c
Rafael <rafael.manoel@gmail.com>
2013-06-13 14:21:19 -0300

    add specs for highlight

--- spec/models/highlight_spec.rb
+++ spec/models/highlight_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Highlight do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "validations" do
+    it { should validate_presence_of :image }
+    it { should validate_presence_of :link }
+    it { should validate_presence_of :position }
+  end
+
+  describe ".highlights_to_show" do
+    
+    it "should sort the Highlights by position" do
+      Highlight.should_receive(:all).with(order: :position)
+      sorted_by_position = Highlight.highlights_to_show
+    end
+
+  end
+
 end
