3d2172366475775be45de920aa06d354dcf58590
Rafael <rafael.manoel@gmail.com>
2013-06-14 12:32:25 -0300

    fix specs

--- spec/factories/highlights.rb
+++ spec/factories/highlights.rb
@@ -1,7 +1,20 @@
+# -*- encoding : utf-8 -*-
 # Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :highlight do
-    image ""
+    image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
+    link "http://www.olook.com.br"
+
+    trait :at_position_1 do
+      position 1
+    end
+
+    trait :at_position_2 do
+      position 2
+    end
+
+    trait :at_position_3 do
+      position 3
+    end
   end
 end
