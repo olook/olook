6ad227151bed750ba4434e31bc7c654f5877d164
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-06-14 12:18:25 -0300

    adding temporary method (gambiarra) SearchedProduct#product

--- app/models/search/searched_product.rb
+++ app/models/search/searched_product.rb
@@ -19,4 +19,53 @@ class SearchedProduct
     @promotion
   end
 
+  def item_view_cache_key_for shoe_size
+    "_product_item:#{id}"
+  end
+
+  def sold_out?
+    product.sold_out?
+  end
+
+  def shoe?
+    product.shoe?
+  end
+
+  def name
+    product.name?
+  end
+
+  def catalog_picture
+    catalog_image
+  end
+
+  def showroom_picture
+    product.showroom_picture
+  end
+
+  def wearing_picture
+    product.wearing_picture
+  end
+
+  def color_name
+    product.color_name
+  end
+
+  def colors
+    product.colors
+  end
+
+  def color_sample
+    product.color_sample
+  end
+
+  # gambiarra temporaria
+  def product
+    Product.find id
+  end
+
+  def self.model_name
+
+  end
+
 end
