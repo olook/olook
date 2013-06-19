33ef8c071e82ccea159badbd7e9450cd765e3e69
Rafael Carvalho <rafa1402@gmail.com>
2013-06-19 15:05:43 -0300

    frontend

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,8 +1,11 @@
 module CatalogsHelper
   def filter_link_to(path, field, text, amount=nil)
     filter_link = create_query_string_catalog(field => text.parameterize)
+    span_class = text.downcase
     text += " (#{amount})" if amount
-    link_to text, path + "?#{filter_link}"
+    link_to path + "?#{filter_link}" do
+      content_tag(:span, text, class:"txt-#{span_class}")
+    end
   end
 
   def build_link_path
