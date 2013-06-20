33515227e5e7d67c9aeb45a72cac4fedc8730849
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-06-20 15:30:43 -0300

    removing binding.pry

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -14,7 +14,6 @@ class CatalogsController < SearchController
     @filters = SearchEngine.new(category: @category).filters
     @filters.grouped_products('categoria').delete_if{|c| Product::CARE_PRODUCTS.include?(c) }
     @search = SearchEngine.new(category: @category, subcategories: @subcategories, color: @color, heel: @heel, care: @care, brand: @brand, price: params[:preco]).for_page(params[:page]).with_limit(100)
-    binding.pry
     @catalog_products = @search.products
 
     # TODO => Mover para outro lugar
