dd7773ebf320ba7f64cafe564b30510703c1449e
Luis Daher <luisotaviodaher@gmail.com>
2013-06-21 16:04:45 -0300

    search changes

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,8 +1,10 @@
 module CatalogsHelper
   def filter_link_to(link, text, selected=false, amount=nil)
     span_class = text.downcase
+    search_param = params[:q].blank? ? "" : "?q=#{params[:q]}"
     text += " (#{amount})" if amount
     class_hash = selected ? {class: "selected"} : {}
+    link+=search_param
     link_to(link, class_hash) do
       content_tag(:span, text, class:"txt-#{span_class}")
     end
