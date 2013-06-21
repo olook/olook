6af1618640fe497422e8dbce96d2bb66dd7aefcb
Rafael <rafael.manoel@gmail.com>
2013-06-21 17:27:34 -0300

    add type for highlights

--- app/models/highlight.rb
+++ app/models/highlight.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 class Highlight < ActiveRecord::Base
   after_save :clean_cache
   after_destroy :clean_cache
@@ -7,16 +8,28 @@ class Highlight < ActiveRecord::Base
   validates :image, presence: true
   validates :link, presence: true
   validates :position, presence: true
+  validates :title, presence: true, :if => lambda{|type| type == HighlightType::WEEKLY}
+  validates :subtitle, presence: true, :if => lambda{|type| type == HighlightType::WEEKLY}
 
-  def self.highlights_to_show
-    Rails.cache.fetch("highlights", :expires_in => 15.minutes) do 
-      all(order: :position)
+  has_enumeration_for :highlight_type, :with => HighlightType, :required => true
+
+  def self.highlights_to_show type
+    Rails.cache.fetch("highlights-#{type}", :expires_in => 30.minutes) do 
+      where(highlight_type: type).order(:position)
     end
+  end
+
+  def self.grouped_by_type
+    grouped_highlights = {}
+    HighlightType.list.each do |type_id|
+      grouped_highlights[type_id] = Highlight.where(highlight_type: type_id).order(:position)
+    end    
+    grouped_highlights
   end
 
   private
     def clean_cache
-      Rails.cache.delete("highlights")
+      HighlightType.list.each{|id| Rails.cache.delete("highlights-#{id}")}
     end
 
 end
--- spec/factories/highlights.rb
+++ spec/factories/highlights.rb
@@ -4,6 +4,8 @@ FactoryGirl.define do
   factory :highlight do
     image { fixture_file_upload("#{Rails.root}/spec/fixtures/files/shoe02.jpg", "image/jpeg") }
     link "http://www.olook.com.br"
+    highlight_type 1
+    
 
     trait :at_position_1 do
       position 1
