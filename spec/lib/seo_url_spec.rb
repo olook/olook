82c09b58c8332a28eb564a9558726ee079847cba
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-06-25 18:41:35 -0300

    keep price params at catalog filters

--- spec/lib/seo_url_spec.rb
+++ spec/lib/seo_url_spec.rb
@@ -143,5 +143,10 @@ describe SeoUrl do
       it { expect(SeoUrl.build(category: ['Acessório'], subcategory: ['rasteira'], size: ['36', 'p'], color: ['azul', 'Onça'])).
            to eq({ parameters: "acessorio/rasteira/tamanho-36-p_cor-azul-onca" }) }
     end
+
+    context "when paramter price order was passed" do
+      it { expect(SeoUrl.build({ category: ['sapato'], subcategory: ['Sandália'], size: ['36', 'p'], color: ['azul', 'vermelho']}, por: 'menor-preco')).
+          to eq({ parameters: "sapato/sandalia/tamanho-36-p_cor-azul-vermelho", por: 'menor-preco'} ) }
+    end
   end
 end
