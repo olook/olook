2b678f79b4056662b59540b8d9ad092f3299c862
Rafael <rafael.manoel@gmail.com>
2013-06-25 14:36:07 -0300

    fix size and brands sorting

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -1,4 +1,11 @@
+# encoding: utf-8
 module CatalogsHelper
+  CLOTH_SIZES_TABLE = {"PP" => 1, "P" =>2, "M" => 3, "G" => 4, "GG" => 5,
+                 "34" => 6, "36" => 7, "38" => 8, "40" => 9, "42" => 10, "44" => 11, "46" => 12,
+                 "Único" => 13}
+
+  HIGHLIGHT_BRANDS = {"olook" => 1, "olook concept" => 2}
+
   def filter_link_to(link, text, selected=false, amount=nil)
     span_class = text.downcase
     search_param = params[:q].blank? ? "" : "?q=#{params[:q]}"
@@ -17,7 +24,24 @@ module CatalogsHelper
   end
 
   def filters_by filter
-    @filters.grouped_products(filter).sort
+    facets = @filters.grouped_products(filter)
+
+    if filter == 'size'
+      facets.keys.sort{|a,b| CLOTH_SIZES_TABLE[a.to_s].to_i <=> CLOTH_SIZES_TABLE[b.to_s].to_i}
+    elsif filter == 'brand_facet'
+      # Olook and Olook Concept must be shown at the top
+      facets.keys.sort do |a,b|
+        if !HIGHLIGHT_BRANDS[a.downcase].nil?
+          -1
+        elsif !HIGHLIGHT_BRANDS[b.downcase].nil?
+          1
+        else
+          a <=> b
+        end
+      end
+    else
+      facets.sort
+    end
   end
 
 end
