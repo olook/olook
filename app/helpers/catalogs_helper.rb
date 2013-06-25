b9e7590a014612dd11ba4f69a8e5253c58d226ee
Rafael Carvalho <rafa1402@gmail.com>
2013-06-25 15:18:04 -0300

    adjust layout

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -20,7 +20,7 @@ module CatalogsHelper
   def current_section_link_to(link, selected=false)
     search_param = params[:q].blank? ? "" : "?q=#{params[:q]}"
     link+=search_param
-    link_to("X", link)
+    link_to("( x )", link)
   end
 
   def filters_by filter
