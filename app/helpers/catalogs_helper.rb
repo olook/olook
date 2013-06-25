057864642ac54d9d3fd5655d22e5fa158b207b18
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-06-25 10:42:11 -0300

    adding new helper 'current_section_link_to'

--- app/helpers/catalogs_helper.rb
+++ app/helpers/catalogs_helper.rb
@@ -10,6 +10,16 @@ module CatalogsHelper
     end
   end
 
+  def current_seaction_link_to(link, text, selected=false, content_tag=nil)
+    span_class = text.downcase
+    search_param = params[:q].blank? ? "" : "?q=#{params[:q]}"
+    class_hash = selected ? {class: "selected"} : {}
+    link+=search_param
+    link_to(link, class_hash) do
+      content_tag(:span, "#{text}(X)", class:"txt-#{span_class}")
+    end
+  end
+
   def filters_by filter
     @filters.grouped_products(filter)
   end
