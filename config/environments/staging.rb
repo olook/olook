ab92303cec6ae105cb21525d2446a89bafdc9b56
Rafael <rafael.manoel@gmail.com>
2013-06-26 11:44:04 -0300

    change dalli expiration time to 1 day

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -41,7 +41,7 @@ Olook::Application.configure do
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
-  config.cache_store = :dalli_store, 'localhost',{ :namespace => 'olook', :expires_in => 5.minutes , :compress => true }
+  config.cache_store = :dalli_store, 'localhost',{ :namespace => 'olook', :expires_in => 1.day , :compress => true }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   #config.action_controller.asset_host = "http://cdn-app-staging.olook.com.br.s3.amazonaws.com"
