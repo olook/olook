633012ce96966cd052582bbd927212c771291046
Luis Daher <luisotaviodaher@gmail.com>
2013-06-27 15:37:11 -0300

    fixing tests

--- spec/models/search/search_url_builder_spec.rb
+++ spec/models/search/search_url_builder_spec.rb
@@ -11,7 +11,7 @@ describe SearchUrlBuilder do
     it {expect(subject.build_url_for().to_s).to match(/\?q=term/)}
 
     context "and a color" do
-      it {expect(subject.with_brand('brand').build_url_for().to_s).to match(/\?q=term&bq=%28field%20brand%20%27brand%27%29/)}
+      it {expect(subject.with_brand('brand').build_url_for().to_s).to match(/\?q=term&bq=(.*)%28field%20brand%20%27brand%27%29/)}
     end
   end
 
@@ -22,7 +22,7 @@ describe SearchUrlBuilder do
     it {expect(subject.build_url_for().to_s).to_not match(/\?&/)}
 
     context "but with brand" do
-      it {expect(subject.with_brand('brand').build_url_for().to_s).to match(/\?bq=%28field%20brand%20%27brand%27%29/)}
+      it {expect(subject.with_brand('brand').build_url_for().to_s).to match(/\?bq=(.*)%28field%20brand%20%27brand%27%29/)}
     end
 
 
