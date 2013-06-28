6ac0937529dc6e8496f0f33bee81df905e00eaf5
Tiago Almeida <tiago.almeida@olook.com.br>
2013-06-28 10:25:53 -0300

    Add controller and routes to highlight campaign

---
+++ app/controllers/admin/highlight_campaigns_controller.rb
@@ -0,0 +1,46 @@
+class Admin::HighlightCampaignsController < Admin::BaseController
+
+  respond_to :html
+
+  def index
+    @highlight_campaigns = HighlightCampaign.grouped_by_type
+  end
+
+  def show
+    @highlight_campaign = HighlightCampaign.find(params[:id])
+  end
+
+  def new
+    @highlight_campaign = HighlightCampaign.new
+  end
+
+  def edit
+    @highlight_campaign = HighlightCampaign.find(params[:id])
+  end
+
+  def create
+    @highlight_campaign = HighlightCampaign.new(params[:highlight_campaign])
+    if @highlight_campaign.save
+      redirect_to [:admin, @highlight_campaign], notice: ''
+    else
+      render action: "new"
+    end
+
+  end
+
+  def update
+    @highlight_campaign = HighlightCampaign.find(params[:id])
+    if @highlight_campaign.update_attributes(params[:highlight_campaign])
+      redirect_to [:admin, @highlight_campaign], notice: ''
+    else
+      render action: "edit"
+    end
+
+  end
+
+  def destroy
+    @highlight_campaign = HighlightCampaign.find(params[:id])
+    @highlight_campaign.destroy
+    redirect_to admin_highlight_campaigns_url
+  end
+end
