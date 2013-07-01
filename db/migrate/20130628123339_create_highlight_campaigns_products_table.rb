50cc32d1d96214de5ab259f690994fe796f029f1
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 13:46:14 -0300

    Change migrate to remove id from join table

--- db/migrate/20130628123339_create_highlight_campaigns_products_table.rb
+++ db/migrate/20130628123339_create_highlight_campaigns_products_table.rb
@@ -1,6 +1,6 @@
 class CreateHighlightCampaignsProductsTable < ActiveRecord::Migration
   def change
-    create_table :highlight_campaigns_products do |t|
+    create_table :highlight_campaigns_products, id: false do |t|
       t.integer :highlight_campaign_id
       t.integer :product_id
     end
