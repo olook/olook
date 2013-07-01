966591ade9b6fa7154f251dd94c08f3461bc45b0
galeto <vsmont.monteiro@gmail.com>
2013-07-01 10:59:05 -0300

    changing received parameters at brands controller and creatig logic of amazon cloud search in brands controller too

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,5 +4,21 @@ class BrandsController < ApplicationController
   end
 
   def show
+    params.merge!(SeoUrl.parse_brands(params[:parameters], params))
+    Rails.logger.debug("New params: #{params.inspect}")
+
+    @search = SearchEngine.new(category: params[:category],
+                               care: params[:care],
+                               subcategory: params[:subcategory],
+                               color: params[:color],
+                               heel: params[:heel],
+                               care: params[:care],
+                               price: params[:price],
+                               size: params[:size],
+                               brand: params[:brand],
+                               sort: params[:sort]).for_page(params[:page]).with_limit(48)
+    @search.for_admin if current_admin
+    @catalog_products = @search.products
+    @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
   end
 end
