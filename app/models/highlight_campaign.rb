2873ce50135b3746437a0f8ff417dfbbd7bf102d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 09:41:19 -0300

    WIP - Add initial add_product method on highlight_campaign

--- app/models/highlight_campaign.rb
+++ app/models/highlight_campaign.rb
@@ -1,5 +1,10 @@
 class HighlightCampaign < ActiveRecord::Base
-  attr_accessible :label
+  attr_accessible :label, :product_ids
   validates :label, presence: true
   has_and_belongs_to_many :products
+
+  def add_products string
+    return {code: "2",fail_product_ids: []} if string.blank?
+    product_ids = string.split(',')
+  end
 end
--- spec/models/highlight_campaign_spec.rb
+++ spec/models/highlight_campaign_spec.rb
@@ -7,4 +7,10 @@ describe HighlightCampaign do
   describe "Acossiations" do
     it { should have_and_belong_to_many(:products) }
   end
+  describe "#add_products" do
+    context "When receive empty || nil string"
+    it "return 2" do
+      expect(subject.add_products("")).to include(code: "2",fail_product_ids: [])
+    end
+  end
 end
