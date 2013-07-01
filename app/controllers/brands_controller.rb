6ad541e33e2d87e248caa58e9139fa9cac95494d
galeto <vsmont.monteiro@gmail.com>
2013-07-01 15:31:53 -0300

    creating select box to filter by size

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -7,6 +7,7 @@ class BrandsController < ApplicationController
     params.merge!(SeoUrl.parse_brands(params[:parameters], params))
     Rails.logger.debug("New params: #{params.inspect}")
 
+    @filters = create_filters
     @search = SearchEngine.new(category: params[:category],
                                care: params[:care],
                                subcategory: params[:subcategory],
@@ -21,4 +22,17 @@ class BrandsController < ApplicationController
     @catalog_products = @search.products
     @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
   end
+
+  private
+    def create_filters
+      filters = SearchEngine.new(category: params[:category]).filters
+      remove_care_products_from(filters)
+      filters
+    end
+
+    def remove_care_products_from(filters)
+      if filters.grouped_products('subcategory')
+        filters.grouped_products('subcategory').delete_if{|c| Product::CARE_PRODUCTS.map(&:parameterize).include?(c.parameterize) }
+      end
+    end
 end
