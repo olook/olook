f702b92c2bb8de1ad975f5b3ebed5983acf527b7
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 10:45:35 -0300

    WIP - Complete highlight_campaign add_products funntiofunctionality

--- app/models/highlight_campaign.rb
+++ app/models/highlight_campaign.rb
@@ -5,6 +5,17 @@ class HighlightCampaign < ActiveRecord::Base
 
   def add_products string
     return {code: "2",fail_product_ids: []} if string.blank?
+    fail_product_ids = []
     product_ids = string.split(',')
+    product_ids.each do |product|
+      _product = Product.find_by_id(product.to_i)
+      if _product
+        products << _product
+      else
+        fail_product_ids << product
+      end
+    end
+    code = fail_product_ids.empty? ? "0" : "1"
+    {code: code,fail_product_ids: fail_product_ids}
   end
 end
