a0f18be234a0c2128f157f5367ef33c50ca04980
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 10:45:35 -0300

    WIP - Complete highlight_campaign add_products funntiofunctionality

--- spec/models/highlight_campaign_spec.rb
+++ spec/models/highlight_campaign_spec.rb
@@ -8,9 +8,26 @@ describe HighlightCampaign do
     it { should have_and_belong_to_many(:products) }
   end
   describe "#add_products" do
-    context "When receive empty || nil string"
-    it "return 2" do
-      expect(subject.add_products("")).to include(code: "2",fail_product_ids: [])
+    context "When receive empty || nil string" do
+      it "return 2" do
+        expect(subject.add_products("")).to include(code: "2",fail_product_ids: [])
+      end
+    end
+    context "When have products" do
+      before do
+        @product1 = FactoryGirl.create(:shoe)
+        @product2 = FactoryGirl.create(:shoe)
+      end
+      context "and find all products" do
+        it "return 0" do
+          expect(subject.add_products("#{@product1.id},#{@product2.id}")).to include(code: "0",fail_product_ids: [])
+        end
+      end
+      context "and dont find all products" do
+        it "return 1" do
+          expect(subject.add_products("#{@product1.id},#{@product2.id},123")).to include(code: "1",fail_product_ids: ["123"])
+        end
+      end
     end
   end
 end
