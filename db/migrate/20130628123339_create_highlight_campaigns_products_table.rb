8b70ceae606c821a2db033f32ab5ec9f3ee75c0d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 13:34:20 -0300

    Fix join migration to add highlights and products

--- db/migrate/20130628123339_create_highlight_campaigns_products_table.rb
+++ db/migrate/20130628123339_create_highlight_campaigns_products_table.rb
@@ -1,8 +1,8 @@
 class CreateHighlightCampaignsProductsTable < ActiveRecord::Migration
   def change
     create_table :highlight_campaigns_products do |t|
-      t.belongs_to :highlight_campaigns
-      t.belongs_to :products
+      t.integer :highlight_campaign_id
+      t.integer :product_id
     end
   end
 end
