0c6b8340e9b64bd7455cd83819b9e82a82a0a36c
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-07-01 18:24:09 -0300

    Remove highlights products join table

--- db/migrate/20130628123339_create_highlight_campaigns_products_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateHighlightCampaignsProductsTable < ActiveRecord::Migration
-  def change
-    create_table :highlight_campaigns_products, id: false do |t|
-      t.integer :highlight_campaign_id
-      t.integer :product_id
-    end
-  end
-end
