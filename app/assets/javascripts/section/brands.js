483854693293eeb365f6f76a97aa996af573cf26
Rafael Carvalho <rafa1402@gmail.com>
2013-07-03 14:21:41 -0300

    adjust js

--- app/assets/javascripts/section/brands.js
+++ app/assets/javascripts/section/brands.js
@@ -4,7 +4,7 @@ brands = b = {
   init: function(){
     b.changeText();
     b.selectedFilter();
-    //b.hideFilterByBody();
+    b.hideFilterByBody();
   },
   
   changeText: function(){
@@ -19,10 +19,11 @@ brands = b = {
   selectedFilter: function(){
     var txt = $("span.txt-filter");
     txt.each(function(){ 
-      $(this).on("click", function(){
+      $(this).on("click", function(event){
         $(this).parent().siblings().find("ul, .tab_bg").hide();
         $(this).parent().siblings().find("span.txt-filter.clicked").removeClass("clicked");
         $(this).toggleClass('clicked').siblings().toggle();
+        event.stopPropagation();
       })
     })
     
@@ -32,7 +33,7 @@ brands = b = {
     $("html").on("click", function(){
       if($(".filter ul").is(":visible")){
         $(".filter ul:visible, .filter .tab_bg:visible").hide();
-        $(".fiilter span.txt-filter.clicked").removeClass("clicked");
+        $(".filter span.txt-filter.clicked").removeClass("clicked");
       }
     });
   }
