c9ca09049a8ece3c8c33e7b8c15e1cb9577099cf
Rafael Carvalho <rafa1402@gmail.com>
2013-07-03 13:26:44 -0300

    adjust js

--- app/assets/javascripts/section/brands.js
+++ app/assets/javascripts/section/brands.js
@@ -4,7 +4,7 @@ brands = b = {
   init: function(){
     b.changeText();
     b.selectedFilter();
-    //b.hideFilterByBody();
+    b.hideFilterByBody();
   },
   
   changeText: function(){
@@ -19,22 +19,23 @@ brands = b = {
   selectedFilter: function(){
     var txt = $("span.txt-filter");
     txt.each(function(){ 
-      $(this).on("click", function(){
+      $(this).on("click", function(e){
         $(this).parent().siblings().find("ul, .tab_bg").hide();
         $(this).parent().siblings().find("span.txt-filter.clicked").removeClass("clicked");
         $(this).toggleClass('clicked').siblings().toggle();
+        //e.stopPropagation();
       })
     })
+    
   },
   
   hideFilterByBody: function(){
-    $("body").on("click", function(){
+    $("html").on("click", function(){
       if($(".filter ul").is(":visible")){
         $(".filter ul:visible, .filter .tab_bg:visible").hide();
         $(".fiilter span.txt-filter.clicked").removeClass("clicked");
       }
     });
-    
   }
 }
 
