f206606823b0df2a0e76b2f06fbe7fff808d69b4
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-07-05 14:15:32 -0300

    removing validation of header image

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,7 +2,6 @@
 class Brand < ActiveRecord::Base
   attr_accessible :header_image, :header_image_alt, :name
   validates :name, presence: true
-  validates :header_image, presence: true
   mount_uploader :header_image, BannerUploader
   before_save :format_name
 
