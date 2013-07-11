baae203a065f6e0bbcc5258e0c8aed2aad79e98a
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-07-11 11:54:10 -0300

    not displaying modal show if client is a device

--- app/assets/javascripts/campaign_emails_modal.js
+++ app/assets/javascripts/campaign_emails_modal.js
@@ -17,35 +17,46 @@ function dontShow(){
 }
 
 $(function(){
+    if ( navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
 
-	if(show_modal == "1"){
-		$("#overlay-campaign").delay(100).show();
-		$("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
-		window.setTimeout(function(){
-				if($("#overlay-campaign").is(":visible"))
-					criaCookieAB("ms","1", 1);
-		},500)
+       ){
+           return false
+       }
+           else{
+           if(show_modal == "1"){
+               $("#overlay-campaign").delay(100).show();
+               $("#modal-campaign").append('<iframe src="/campaign_emails/new" border="0" frameborder="0" height="100%" width="100%"></iframe>');
+               window.setTimeout(function(){
+                   if($("#overlay-campaign").is(":visible"))
+                   criaCookieAB("ms","1", 1);
+               },500)
 
-	}
-	$(document).one({
-		click: function(e){
-		//	$("#modal-campaign,#overlay-campaign").fadeOut();
-			if($("#modal-campaign iframe").contents().find(".dont_show").is(":checked")){
-				dontShow();
-			}
-			_gaq.push(['_trackEvent', 'Modal', 'Close', '', , true]);
-			stopProp(e);
-		},
-		keyup: function(e) {
-			if (e.keyCode == 27) { //ESC
-	   		$("#modal-campaign,#overlay-campaign").fadeOut();
-			}
-			if($("#modal-campaign iframe").contents().find(".dont_show").is(":checked")){
-				dontShow();
-			}
-			_gaq.push(['_trackEvent', 'Modal', 'Close', '', , true]);
-			stopProp(e);
-		}
-	})
-
-})
+           }
+           $(document).one({
+               click: function(e){
+                   //	$("#modal-campaign,#overlay-campaign").fadeOut();
+                   if($("#modal-campaign iframe").contents().find(".dont_show").is(":checked")){
+                       dontShow();
+                   }
+                   _gaq.push(['_trackEvent', 'Modal', 'Close', '', , true]);
+                   stopProp(e);
+               },
+           keyup: function(e) {
+               if (e.keyCode == 27) { //ESC
+                   $("#modal-campaign,#overlay-campaign").fadeOut();
+               }
+               if($("#modal-campaign iframe").contents().find(".dont_show").is(":checked")){
+                   dontShow();
+               }
+               _gaq.push(['_trackEvent', 'Modal', 'Close', '', , true]);
+               stopProp(e);
+           }
+           })
+       }
+});
