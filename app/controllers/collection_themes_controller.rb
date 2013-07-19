ae93cf33c13b8a736f258fb8ae103047fcb38a12
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-07-19 15:10:25 -0300

    using new SeoUrl.parse at collection themes controller

--- app/controllers/collection_themes_controller.rb
+++ app/controllers/collection_themes_controller.rb
@@ -8,22 +8,13 @@ class CollectionThemesController < SearchController
   end
 
   def show
-    @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
+    search_params = SeoUrl.parse(params)
     params.merge!(SeoUrl.parse(params[:parameters], params))
     Rails.logger.debug("New params: #{params.inspect}")
 
     @filters = create_filters #TODO use SearchEngine#filters instead create new filters
 
-    @search = SearchEngine.new(category: params[:category],
-                               care: params[:care],
-                               subcategory: params[:subcategory],
-                               color: params[:color],
-                               heel: params[:heel],
-                               care: params[:care],
-                               price: params[:price],
-                               size: params[:size],
-                               collection_themes: params[:collection_theme],
-                               sort: params[:sort]).for_page(params[:page]).with_limit(48)
+    @search = SearchEngine.new(search_params).for_page(params[:page]).with_limit(48)
     @collection_theme = CollectionTheme.find_by_slug(params[:collection_theme])
     @collection_theme_groups = CollectionThemeGroup.order(:position).includes(:collection_themes)
   end
