499a1efc6cfb432dbb60876c2db03be1b9fda148
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-07-19 15:09:08 -0300

    refactoring SeoUrl.parse

--- lib/seo_url.rb
+++ lib/seo_url.rb
@@ -36,42 +36,30 @@ class SeoUrl
     parsed_values[:category] = @params[:category] || ((@params[:parameters].to_s.split('/').first.to_s.split('-') & categories).join('-'))
     parsed_values[:subcategory] = extract_subcategories
     parsed_values.merge!(parse_filters)
+    parsed_values.merge!(parse_order)
     parsed_values
   end
 
-  def self.parse parameters, other_parameters={}
-    parsed_values = HashWithIndifferentAccess.new
-
-    parsed_values[:category] = extract_category(parameters, other_parameters)
-    parsed_values[:brand] = extract_brand(parameters, other_parameters)
-    parsed_values[:subcategory] = extract_subcategories(parameters, other_parameters)
-
-    parsed_values.merge!(parse_filters(parameters))
-    parsed_values.merge!(translate_other_parameters(other_parameters))
-
-    parsed_values
+  def self.parse parameters
+    self.new(parameters).new_parse
   end
 
   def self.build_for current_key, params, other_params={  }
     return_hash = build(params, other_params)
-    return_hash.delete(current_key.to_s)
+    return_hash.delete(current_key.to_sym)
     path = [ return_hash[:category], return_hash[:brand], return_hash[:subcategory] ].flatten.select {|p| p.present? }.uniq.map{ |p| ActiveSupport::Inflector.transliterate(p).downcase }.join('-')
     { parameters: [path, return_hash[:filter_params]].reject { |p| p.blank? }.join('/') }.merge(return_hash[:order_params])
   end
 
   private
 
-    def self.all_brands
-      YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_brands.yml' ) ) ) )
-    end
-
     def all_brands
       YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_brands.yml' ) ) ) )
     end
 
-    def self.translate_other_parameters(other_parameters)
+    def parse_order
       parsed_values = {}
-      other_parameters.each do |k, v|
+      @params.each do |k, v|
         if VALUES[k]
           parsed_values[VALUES[k]] = VALUES[v].present? ? VALUES[v] : v
         end
@@ -79,30 +67,9 @@ class SeoUrl
       parsed_values
     end
 
-    def self.extract_category(parameters, other_parameters={})
-      return other_parameters[:category] if other_parameters[:category]
-      (parameters.to_s.split('/').first.to_s.split('-') & categories).join('-')
-    end
-
-    def self.extract_brand(parameters, other_parameters)
-      return other_parameters[:brand] if other_parameters[:brand]
-      brands = (all_brands & parameters.to_s.split('/').first.to_s.split('-').map { |s| ActiveSupport::Inflector.transliterate(s).titleize })
-      brands.join("-") if brands.any?
-    end
-
     def extract_brand
       brands = (all_brands & @params[:parameters].to_s.split('/').first.to_s.split('-').map { |s| ActiveSupport::Inflector.transliterate(s).titleize })
       brands.join("-") if brands.any?
-    end
-
-    def self.extract_subcategories(parameters, other_parameters)
-      subcategories_and_brands = parameters.to_s.split("/").first.split("-") rescue []
-      _all_subcategories = self.all_subcategories || []
-      unless other_parameters[:search]
-        _all_subcategories -= CARE_PRODUCTS.map { |s| ActiveSupport::Inflector.transliterate(s) }
-      end
-      subcategories = (subcategories_and_brands.map { |s| ActiveSupport::Inflector.transliterate(s) } & _all_subcategories)
-      subcategories.join("-") if subcategories.any?
     end
 
     def extract_subcategories
@@ -115,15 +82,6 @@ class SeoUrl
       subcategories.join("-") if subcategories.any?
     end
 
-    def self.categories
-      cat = YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_categories.yml' ) ) ) )
-      cat = cat.keys
-      cat.concat( cat.map{ |s| s.downcase } )
-      cat.concat( cat.map{ |s| ActiveSupport::Inflector.transliterate(s) } )
-      cat.concat( cat.map{ |s| s.parameterize } )
-      cat
-    end
-
     def categories
       cat = YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_categories.yml' ) ) ) )
       cat = cat.keys
@@ -131,18 +89,6 @@ class SeoUrl
       cat.concat( cat.map{ |s| ActiveSupport::Inflector.transliterate(s) } )
       cat.concat( cat.map{ |s| s.parameterize } )
       cat
-    end
-
-    def self.parse_filters(parameters)
-      filter_params = parameters.to_s.split("/").last
-      parsed_values = {}
-      filter_params.to_s.split('_').each do |item|
-        auxs = item.split('-')
-        key = auxs.shift
-        vals = auxs.join('-')
-        parsed_values[VALUES[key]] = vals
-      end
-      parsed_values
     end
 
     def parse_filters
@@ -155,6 +101,13 @@ class SeoUrl
         parsed_values[VALUES[key]] = vals
       end
       parsed_values
+    end
+
+    def all_subcategories
+      subs = YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_subcategories.yml' ) ) ) )
+      subs.concat( subs.map{ |s| s.downcase } )
+      subs.concat( subs.map{ |s| ActiveSupport::Inflector.transliterate(s) } )
+      subs.concat( subs.map{ |s| s.parameterize } )
     end
 
     def self.build params, other_params = {  }
@@ -184,19 +137,5 @@ class SeoUrl
       return_hash[:order_params] = post_parameters
 
       return_hash
-    end
-
-    def self.all_subcategories
-      subs = YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_subcategories.yml' ) ) ) )
-      subs.concat( subs.map{ |s| s.downcase } )
-      subs.concat( subs.map{ |s| ActiveSupport::Inflector.transliterate(s) } )
-      subs.concat( subs.map{ |s| s.parameterize } )
-    end
-
-    def all_subcategories
-      subs = YAML.load( File.read( File.expand_path( File.join( File.dirname(__FILE__), '../config/seo_url_subcategories.yml' ) ) ) )
-      subs.concat( subs.map{ |s| s.downcase } )
-      subs.concat( subs.map{ |s| ActiveSupport::Inflector.transliterate(s) } )
-      subs.concat( subs.map{ |s| s.parameterize } )
     end
 end
