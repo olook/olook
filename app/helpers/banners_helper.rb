4e8201da83c3970e171a1e20cd24e99e48d2544d
Tiago Almeida <tiago.almeida@olook.com.br>
2013-07-25 14:58:08 -0300

    Add new helper to banner links with analytics

---
+++ app/helpers/banners_helper.rb
@@ -0,0 +1,23 @@
+# -*- encoding : utf-8 -*-
+module BannersHelper
+  def top_banner(catalog, category_name=nil)
+    category_id = Category.with_name category_name if category_name
+    action_event = category_id.blank? ? "TopBannerFromShowroom" : "TopBannerFrom#{Category.key_for(category_id).to_s.camelize}"
+    link_to('Roupas', "/catalogo/#{catalog}?utf8=%E2%9C%93&por=maior-desconto&cmp=liquida_#{catalog}", class: Category.for_class_name(catalog), onclick: track_event('Catalog', action_event, catalog))
+  end
+
+  def banner_for(category_name, options={})
+    category_id = Category.with_name category_name
+    if options[:position] == :sidetop
+      path = '/colecoes/olook?utf8=%E2%9C%93&slug=olook&category_id=4&sort_filter=1&price=0-600&shoe_sizes[]='
+      link_to(image_tag('moments/banner_marca_olook.jpg'), path, onclick: track_event('Catalog', "SideBanner1From#{Category.key_for(category_id).to_s.camelize}", path))
+    elsif options[:position] == :sidebotton
+      path = '/roupa/eclectic-leeloo-mandi?utf8=✓&por=menor-preco'
+      link_to(image_tag('catalog/marcas_160X600.gif'), '/roupa/eclectic-leeloo-mandi?utf8=✓&por=menor-preco', onclick: track_event('Catalog', "SideBanner2From#{Category.key_for(category_id).to_s.camelize}", path))
+    elsif options[:position] == :botton
+      link_to(image_tag('catalog/banner_quiz.jpg'), new_survey_path , onclick: track_event('Catalog', "BottomBannerFrom#{Category.key_for(category_id).to_s.camelize}", new_survey_path))
+    else
+      ""
+    end
+  end
+end
