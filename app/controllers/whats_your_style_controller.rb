f70e7527aa7240cd6fc8a7643373bf939246b70f
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-07-30 14:21:17 -0300

    displaying results from whats your style app at olook's app view

--- app/controllers/whats_your_style_controller.rb
+++ app/controllers/whats_your_style_controller.rb
@@ -1,4 +1,7 @@
 class WhatsYourStyleController < ApplicationController
   def new
+    url = URI.parse "http://whatsyourstyle.olook.com.br:9000/v1/quizzes/1?api_token=wmy47jY9_5ZwRWJKUHJ1ng" #TODO please move me to some constant into some class
+    response = Net::HTTP.get_response(url)
+    @quiz = HashWithIndifferentAccess.new(JSON.parse response.body)
   end
 end
