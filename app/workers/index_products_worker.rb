5c195b568207514989a71b5c930f9c8787774963
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-08-01 12:42:13 -0300

    Worng var name

--- app/workers/index_products_worker.rb
+++ app/workers/index_products_worker.rb
@@ -59,7 +59,7 @@ class IndexProductsWorker
         fields['brand_facet'] = ActiveSupport::Inflector.transliterate(product.brand).gsub(/[\.\/\?]/, ' ').gsub('  ', ' ').strip.titleize
         fields['price'] = (product.price.to_d * 100).round
         fields['retail_price'] = (product.retail_price.to_d * 100).round
-        fields['discount'] = (fields['retail_price'].to_i * 100) / price['price'].to_i
+        fields['discount'] = (fields['retail_price'].to_i * 100) / fields['price'].to_i
         fields['in_promotion'] = product.liquidation? ? 1 : 0
         fields['category'] = product.category_humanize.downcase
         fields['size'] = product.variants.select{|v| v.inventory > 0}.map{|b| b.description}
