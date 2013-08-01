4947c130f58847145ca97567016332753ae7b86a
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-01 15:29:10 -0300

    creating questions with answers at Quiz class

--- spec/lib/whats_your_style/quiz_spec.rb
+++ spec/lib/whats_your_style/quiz_spec.rb
@@ -3,7 +3,14 @@ require 'spec_helper'
 describe WhatsYourStyle::Quiz do
   describe "#questions" do
     use_vcr_cassette 'whats_your_style'
-    it { expect(subject.questions).to be_a(Array) }
-    it { expect(subject.questions.first[:answers]).to_not be_nil }
+    subject { described_class.new.questions }
+    it { should be_a(Array) }
+    it { expect(subject.first.answers).to_not be_nil }
+
+    it "creates questions" do
+      WhatsYourStyle::Question.should_receive(:new).exactly(12).times
+      described_class.new.questions
+    end
+
   end
 end
