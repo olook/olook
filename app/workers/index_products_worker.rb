e579790bc3d66503dd2c523eaf2549eb9617d5f3
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-08-01 11:47:31 -0300

    Use liquidation to set in_promotion

--- app/workers/index_products_worker.rb
+++ app/workers/index_products_worker.rb
@@ -60,7 +60,7 @@ class IndexProductsWorker
         fields['price'] = (product.price.to_d * 100).round
         fields['retail_price'] = (product.retail_price.to_d * 100).round
         fields['discount'] = (fields['retail_price'].to_i * 100) / price['price'].to_i
-        fields['in_promotion'] = product.promotion? ? 1 : 0
+        fields['in_promotion'] = product.liquidation? ? 1 : 0
         fields['category'] = product.category_humanize.downcase
         fields['size'] = product.variants.select{|v| v.inventory > 0}.map{|b| b.description}
         fields['care'] = product.subcategory.titleize if Product::CARE_PRODUCTS.include?(product.subcategory)
