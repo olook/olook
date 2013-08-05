d52ba40504caba244349df22a1576d43c73426d7
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-05 17:19:21 -0300

    creating js of quiz

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,2 +1,14 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+quiz = {} || null;
+quiz = {
+    respond_question: function(el) {
+        el = $(el);
+          el.on("click", function(){
+                $("img.selected").removeClass("selected").prev().find("input").removeAttr("checked");
+                $(this).addClass("selected").prev().find("input").attr("checked", "checked");
+                $(this).prev().parent().parent().parent().animate({ "margin-left": "-850px" }, "slow" );
+            })
+    }
+}
+$(function(){
+    quiz.respond_question("img");
+});
