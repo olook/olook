25e9d0f243a7b43a892ccde90d76842b135f6b24
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-08-05 14:43:44 -0300

    Quiz Responder next step and validate

--- app/services/quiz_responder.rb
+++ app/services/quiz_responder.rb
@@ -1,5 +1,6 @@
 class QuizResponder
-  attr_accessor :uuid, :profile, :quiz
+  ROUTES = Rails.application.routes.url_helpers
+  attr_accessor :uuid, :profile, :quiz, :user, :next_step
 
   def self.find(uuid)
     json = redis.get(redis_key(uuid))
@@ -12,6 +13,8 @@ class QuizResponder
     @profile = attrs[:profile] || attrs['profile']
     @questions = attrs[:questions] || attrs['questions']
     @name = attrs[:name] || attrs['name']
+    @user = attrs[:user] || attrs['user']
+    @next_step = ROUTES.wysquiz_path
   end
 
   def save
@@ -26,24 +29,73 @@ class QuizResponder
     redis.del(self.class.redis_key(uuid))
   end
 
-  def update_profile(user)
-    user.update_attributes(profile: @profile)
+  def update_profile
+    @user.update_attributes(profile: @profile)
   end
 
   def retrieve_profile
-    api = WhatsYourStyle::Quiz.new
     result = api.profile_from(name: @name, answers: @questions)
 
     @uuid = result[:uuid]
     @profile = result[:profile]
+    self
+  end
+
+  def validate!
+    if !has_profile?
+      if can_ask_for_profile?
+        retrieve_profile
+      else
+        @next_step = ROUTES.wysquiz_path
+        return self
+      end
+    end
+
+    if @user.blank?
+      save
+      add_session(:uuid, @uuid)
+      @next_step = ROUTES.join_path
+      return self
+    end
+
+    update_profile
+    destroy
+
+    @next_step = ROUTES.profile_path
+    self
+  end
+
+  def session_data
+    @session_data
+  end
+
+  def save_session?
+    @session_data.present?
   end
 
   private
 
+  def can_ask_for_profile?
+    @name.present? && @questions.present?
+  end
+
+  def has_profile?
+    @profile.present? && @uuid.present?
+  end
+
+  def add_session(key, val)
+    @session_data ||= {}
+    @session_data[key] = val
+  end
+
+  def api
+    WhatsYourStyle::Quiz.new
+  end
+
   def self.redis
     aux = ( ENV['QUIZ_RESPONDER_REDIS'] || '127.0.0.1:6379/0' ).split(':')
     host = aux.shift
-    port, db = aux.split('/')
+    port, db = aux.shift.to_s.split('/')
     Redis.new(host: host, port: port, db: db)
   end
 
