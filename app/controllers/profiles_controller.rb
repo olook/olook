eb78ac42cadeb34d94560448066df46a4299ccc6
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-05 14:14:07 -0300

    creating action and views of profiles#show

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,19 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_user!
   def show
-    @user = User.find(params[:user_id])
+    @recommended = RecomendationService.new(profiles: current_user.profiles)
+
+    admin = current_admin.present?
+    # This is needed becase when we turn the month collection we never have cloth
+    @cloth = Product.where("id in (?)", Setting.cloth_showroom_casual.split(","))
+    @cloth += @recommended.products( category: Category::CLOTH, collection: @collection, limit: 10, admin: admin)
+    @cloth = @cloth.first(10)
+
+
+    @shoes = @recommended.products( category: Category::SHOE, collection: @collection, admin: admin)
+    @bags = @recommended.products( category: Category::BAG, collection: @collection, admin: admin)
+    @accessories = @recommended.products( category: Category::ACCESSORY, collection: @collection, admin: admin)
+
+    render layout: 'lite_application'
   end
 end
