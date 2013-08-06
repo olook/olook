15ed2f31b1ddf39d490a28c69426056fbb7a2b44
Tiago Almeida <tiago.almeida@olook.com.br>
2013-08-06 13:35:16 -0300

    Add login action and refactor set_user_profile method

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -9,9 +9,19 @@ class JoinController < ApplicationController
     @user = User.new
   end
 
-  def create
+  def register
     @user = User.new(params[:user])
     if @user.save
+      sign_in(:user, @user)
+      redirect_to set_user_profile(@user).next_step, notice: I18n.t("join_controller.create.success")
+    else
+      render :new
+    end
+  end
+
+  def login
+    @user = User.find_for_authentication(:email => params[:email])
+    if @user && @user.valid_password?(params[:password])
       sign_in(:user, @user)
       redirect_to set_user_profile(@user).next_step, notice: I18n.t("join_controller.create.success")
     else
@@ -25,10 +35,13 @@ class JoinController < ApplicationController
     end
 
     def set_user_profile(user)
-      load_quiz_responder
-      @qr = QuizResponder.find(@quiz_responder[:uuid])
-      @qr.user = user
-      @qr.validate!
-      @qr
+      if load_quiz_responder
+        @qr = QuizResponder.find(@quiz_responder[:uuid])
+        @qr.user = user
+        @qr.validate!
+        @qr
+      else
+         QuizResponder.new(user)
+      end
     end
 end
