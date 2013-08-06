99aee7fbaef3755523e22d69c5fb3729d0e22d6f
Tiago Almeida <tiago.almeida@olook.com.br>
2013-08-06 11:45:32 -0300

    Use QuizResponder to manage user profile

--- app/controllers/join_controller.rb
+++ app/controllers/join_controller.rb
@@ -13,10 +13,22 @@ class JoinController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in(:user, @user)
-      redirect_to profile_path, notice: I18n.t("join_controller.create.success")
+      redirect_to set_user_profile(@user).next_step, notice: I18n.t("join_controller.create.success")
     else
       render :new
     end
   end
 
+  private
+    def load_quiz_responder
+      @quiz_responder = session[:qr]
+    end
+
+    def set_user_profile(user)
+      load_quiz_responder
+      @qr = QuizResponder.find(@quiz_responder[:uuid])
+      @qr.user = user
+      @qr.validate!
+      @qr
+    end
 end
