eed60c5a8713f4c600dbc0ebee4c8b78f9b7c5ec
Tiago Almeida <tiago.almeida@olook.com.br>
2013-08-07 09:37:17 -0300

    Add new quiz path on facebook_redirect_paths

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -59,7 +59,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
 
     def facebook_redirect_paths
-      {:friends => friends_home_path, :gift => gift_root_path, :showroom => member_showroom_path, :checkout => new_checkout_path}
+      {:friends => friends_home_path, :gift => gift_root_path, :showroom => member_showroom_path, :checkout => new_checkout_path, :quiz => profile_path}
     end
 
     def already_exist_a_facebook_account(omniauth)
