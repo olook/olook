0db4b31397c95b48d21a66ed207c2d6953f49a58
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-08-08 15:48:30 -0300

    Adding javascript to deal with facebook connect

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Olook::Application.routes.draw do
   get "/join", to: 'join#new', as: 'join'
   post "/join", to: 'join#register', as: 'join'
   put '/join', to: 'join#login', as: 'join'
-  post '/join/facebook_login', "join#facebook_login", as: 'facebook_login', :defaults => { :format => 'json' }
+  post '/join/facebook_login' => "join#facebook_login", as: 'facebook_login'
   get "/voce", to: "profiles#show", as: 'profile'
 
   mount Resque::Server => "/admin/resque"
