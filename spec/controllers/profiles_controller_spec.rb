16899ef3a7d7d131d67762761223ede083a5d269
Tiago Almeida <tiago.almeida@olook.com.br>
2013-08-12 17:05:57 -0300

    Fix profiles controller test

--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe ProfilesController do
 
   describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+    with_a_logged_user do
+      it "returns http success" do
+        get 'show'
+        response.should be_success
+      end
     end
   end
-
 end
