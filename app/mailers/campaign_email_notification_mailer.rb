341fcf0fce675504bd518c08014cc11f38b29379
Luis Daher <luisotaviodaher@gmail.com>
2013-08-12 15:57:46 -0300

    new welcome email changes

--- app/mailers/campaign_email_notification_mailer.rb
+++ app/mailers/campaign_email_notification_mailer.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class CampaignEmailNotificationMailer < ActionMailer::Base
-	default :from => "olook <bemvinda@olook1.com.br>"
+	default :from => "olook <bemvinda@my.olookmail.com.br>"
 
 	def welcome_email(email)
 		mail(:to => email, :subject => "Recebemos seu e-mail. Agora é hora de descobrir o seu estilo.")
