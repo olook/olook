b97846aaa73ff92a2c8b3b34d65f5765b23b3367
Luis Daher <luisotaviodaher@gmail.com>
2013-08-12 11:46:20 -0300

    covering minor use cases

--- spec/models/antibounce_box_spec.rb
+++ spec/models/antibounce_box_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe AntibounceBox do
   describe "#need_antibounce_box" do
-    context "when the antibounce box is needed" do
+    context "" do
     end
-    context "when the antibounce box isn't needed" do
-    end    
   end
 end
