08729c70e283be5efbe349314a74ad2dc19016a2
galeto <vsmont.monteiro@gmail.com>
2013-08-14 17:23:15 -0300

    displaying profile image at share profile resukt

--- app/assets/javascripts/profiles.js
+++ app/assets/javascripts/profiles.js
@@ -1,4 +1,17 @@
 //= require facebook_sdk
 $("#facebook_share_profile").on("click", function(){
-    share_profile_result();
+    switch(user_profile) {
+      case "sexy":
+        profile = "sexy";
+        break;
+      case "chic":
+        profile = "elegante";
+        break;
+      case "moderna":
+        profile = "fashion"
+        break;
+      default:
+        profile = "casual"
+    };
+    share_profile_result(profile);
 });
