60b2abdf7718aafe337778e1598801a2965b05ea
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-19 18:39:19 -0300

    decrementing products to integrate count when current product is integrating

--- spec/workers/abacos/confirm_product_spec.rb
+++ spec/workers/abacos/confirm_product_spec.rb
@@ -4,8 +4,10 @@ require "spec_helper"
 describe Abacos::ConfirmProduct do
   describe "#perform" do
     let(:fake_protocol) { 'PROT321' }
+
     it "should call process_product, process_inventory, process_price" do
       Abacos::ProductAPI.should_receive(:confirm_product).with(fake_protocol)
+      REDIS.should_receive(:decrby).with("products_to_integrate", 1)
       described_class.perform fake_protocol
     end
   end
