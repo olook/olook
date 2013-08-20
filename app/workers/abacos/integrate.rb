5267b15538168237e3f9a31ba14e6e6af937d71c
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-20 16:47:24 -0300

    ensure that product will be decremented if integration process failure

--- app/workers/abacos/integrate.rb
+++ app/workers/abacos/integrate.rb
@@ -4,8 +4,16 @@ module Abacos
     @queue = :product
 
     def self.perform(klass, parsed_data)
+      begin
       entity = klass.constantize.new parsed_data
       entity.integrate
+      rescue Exception => e
+        Abacos::IntegrateProductsObserver.decrement_products_to_be_integrated!
+        Airbrake.notify(
+          :error_class   => "Abacos Confirm Product product",
+          :error_message => e.message
+        )
+      end
     end
   end
 end
