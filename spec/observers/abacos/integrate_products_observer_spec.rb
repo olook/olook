8b4423287e622cd85edcf71747ce6c303d35d1f3
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-20 11:01:28 -0300

    adding integration observer into abacos module

--- spec/observers/abacos/integrate_products_observer_spec.rb
+++ spec/observers/abacos/integrate_products_observer_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe IntegrateProductsObserver do
+describe Abacos::IntegrateProductsObserver do
   describe '.start_with' do
     it "sets how many products will be integrated" do
       REDIS.should_receive(:incrby).with("products_to_integrate",10)
