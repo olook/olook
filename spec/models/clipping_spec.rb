cc10698980347d29d98eb4e2fdfc2716a11d2c7b
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-27 14:32:40 -0300

    format clipping tests

--- spec/models/clipping_spec.rb
+++ spec/models/clipping_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
 describe Clipping do
-  it { should validate_presence_of(:logo) }
-  it { should validate_presence_of(:clipping_text) }
-  it { should validate_presence_of(:link) }
-  it { should validate_presence_of(:published_at) }
-  it { should validate_presence_of(:source) }
-  it { should validate_presence_of(:title) }
+  describe '#validations' do
+    it { should validate_presence_of(:logo) }
+    it { should validate_presence_of(:clipping_text) }
+    it { should validate_presence_of(:link) }
+    it { should validate_presence_of(:published_at) }
+    it { should validate_presence_of(:source) }
+    it { should validate_presence_of(:title) }
+  end
 end
