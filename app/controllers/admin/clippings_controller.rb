096753c42cf8988d26cc698ec4582ed582a3b828
Tiago Almeida <tiago.almeida@olook.com.br>
2013-08-27 16:01:21 -0300

    Add clippings actions

--- app/controllers/admin/clippings_controller.rb
+++ app/controllers/admin/clippings_controller.rb
@@ -1,2 +1,32 @@
-class Admin::ClippingsController < ApplicationController
+class Admin::ClippingsController < Admin::BaseController
+  def index
+    @clippings = Clipping.all
+  end
+
+  def show
+    @clipping = Clipping.find(params[:id])
+  end
+
+  def new
+    @clipping = Clipping.new
+  end
+
+  def edit
+    @clipping = Clipping.find(params[:id])
+  end
+
+  def create
+    @clipping = Clipping.new(params[:clipping])
+    if @clipping.save
+      redirect_to [:admin, @clipping], notice: I18n.t('activerecord.models.clipping.messages.successful.create')
+    else
+      render action: "new"
+    end
+  end
+
+  def destroy
+    @clipping = Clipping.find(params[:id])
+    @clipping.destroy
+    redirect_to admin_clippings_url
+  end
 end
