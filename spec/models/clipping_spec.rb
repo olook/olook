d0e06d5939087e0a725c26856a4fe9df57017be9
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-27 15:00:25 -0300

    ordering clippings by published at desc

--- spec/models/clipping_spec.rb
+++ spec/models/clipping_spec.rb
@@ -9,4 +9,13 @@ describe Clipping do
     it { should validate_presence_of(:source) }
     it { should validate_presence_of(:title) }
   end
+
+  describe 'default scope' do
+    let!(:old_clipping) { FactoryGirl.create(:clipping, published_at: (Time.zone.now - 2.day)) }
+    let!(:recent_clipping) { FactoryGirl.create(:clipping, published_at: (Time.zone.now - 1.day)) }
+
+    subject { described_class.all }
+
+    it { should eq([recent_clipping, old_clipping]) }
+  end
 end
