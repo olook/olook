3e57c3b7d30e8fab58948eab3b60b2e940e16a47
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-28 14:04:30 -0300

    removing link validation

--- spec/models/clipping_spec.rb
+++ spec/models/clipping_spec.rb
@@ -4,7 +4,6 @@ describe Clipping do
   describe '#validations' do
     it { should validate_presence_of(:logo) }
     it { should validate_presence_of(:clipping_text) }
-    it { should validate_presence_of(:link) }
     it { should validate_presence_of(:published_at) }
     it { should validate_presence_of(:source) }
     it { should validate_presence_of(:title) }
