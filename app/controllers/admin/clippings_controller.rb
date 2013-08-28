ed9b450af6dee3b0b6dda4589ec43ea7cca16748
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-08-28 13:32:07 -0300

    adding Clipping#pdf_file

--- app/controllers/admin/clippings_controller.rb
+++ app/controllers/admin/clippings_controller.rb
@@ -14,7 +14,7 @@ class Admin::ClippingsController < Admin::BaseController
   def create
     @clipping = Clipping.new(params[:clipping])
     if @clipping.save
-      redirect_to [:admin, @clipping], notice: I18n.t('activerecord.models.clipping.messages.successful.create')
+      redirect_to admin_clippings_path, notice: I18n.t('activerecord.models.clipping.messages.successful.create')
     else
       render action: "new"
     end
--- app/models/clipping.rb
+++ app/models/clipping.rb
@@ -1,8 +1,9 @@
 class Clipping < ActiveRecord::Base
-  attr_accessible :clipping_text, :link, :logo, :published_at, :source, :title
+  attr_accessible :clipping_text, :link, :logo, :published_at, :source, :title, :pdf_file, :alt
   mount_uploader :logo, ImageUploader
+  mount_uploader :pdf_file, FileUploader
 
-  validates :clipping_text, :link, :logo, :published_at, :source, :title, presence: true
+  validates :clipping_text, :link, :logo, :published_at, :source, :title, :pdf_file, presence: true
 
   default_scope { order('published_at desc') }
 end
