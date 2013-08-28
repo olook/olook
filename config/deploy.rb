d4f6c45bf3a60a6a7131df613392a7059b64dd69
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-08-28 14:22:44 -0300

    correctly avoid assets precompilation if theres no changes on assets

--- config/deploy.rb
+++ config/deploy.rb
@@ -111,19 +111,6 @@ namespace :deploy do
   end
 end
 
-namespace :deploy do
-  namespace :assets do
-    task :precompile, :roles => :web, :except => { :no_release => true } do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
-        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
-      else
-        logger.info "Skipping asset pre-compilation because there were no asset changes"
-      end
-    end
-  end
-end
-
 # load asset pipeline tasks, and reorder them to run after
 # rubber:config so that database.yml/etc has been generated
 load 'deploy/assets'
@@ -132,3 +119,36 @@ callbacks[:before].delete_if {|c| c.source == "deploy:assets:symlink"}
 before "deploy:assets:precompile", "deploy:public:symlink"
 before "deploy:assets:precompile", "deploy:assets:symlink"
 after "rubber:config", "deploy:assets:precompile"
+
+
+namespace :deploy do
+  namespace :assets do
+    task :precompile, :roles => lambda { assets_role }, :except => { :no_release => true } do
+      from = source.next_revision(current_revision)
+      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run <<-CMD.compact
+          cd -- #{latest_release} && 
+          RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} #{rake} assets:precompile
+        CMD
+
+        if capture("ls -1 #{shared_path.shellescape}/#{shared_assets_prefix}/manifest* | wc -l").to_i > 1
+          raise "More than one asset manifest file was found in '#{shared_path.shellescape}/#{shared_assets_prefix}'.  If you are upgrading a Rails 3 application to Rails 4, follow these instructions: http://github.com/capistrano/capistrano/wiki/Upgrading-to-Rails-4#asset-pipeline"
+        end
+
+        # Sync manifest filenames across servers if our manifest has a random filename
+        if shared_manifest_path =~ /manifest-.+\./
+          run <<-CMD.compact
+          [ -e #{shared_manifest_path.shellescape} ] || mv -- #{shared_path.shellescape}/#{shared_assets_prefix}/manifest* #{shared_manifest_path.shellescape}
+          CMD
+        end
+
+        # Copy manifest to release root (for clean_expired task)
+        run <<-CMD.compact
+          cp -- #{shared_manifest_path.shellescape} #{current_release.to_s.shellescape}/assets_manifest#{File.extname(shared_manifest_path)}
+        CMD
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+  end
+end
