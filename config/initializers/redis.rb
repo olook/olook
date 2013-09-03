80d9191350997abb8a24183ae290b25b1be6e5b0
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-09-03 11:38:42 -0300

    moving redis config to application

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,3 @@
 host, port = YAML.load_file(Rails.root + 'config/resque.yml')[Rails.env].split(":")
 REDIS = Redis.new(:host => host, :port => port, :db => 1)
 ENV['QUIZ_RESPONDER_REDIS'] ||= "#{host}:#{port}/0"
-if Rails.env.production?
-  ENV['REDIS_CACHE_STORE'] ||= "redis://#{host}:#{port}/0/cache"
-else
-  ENV['REDIS_CACHE_STORE'] ||= "redis://localhost:6379/0/cache"
-end
-
