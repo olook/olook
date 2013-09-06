08b5924c58a1c747c1814aee28bb0229ce364879
Tiago Almeida <tiago.almeida@olook.com.br>
2013-09-06 19:14:10 -0300

    Create method to pick every email infos of date range

--- app/models/simple_email_service_info.rb
+++ app/models/simple_email_service_info.rb
@@ -1,3 +1,14 @@
 class SimpleEmailServiceInfo < ActiveRecord::Base
   attr_accessible :bounces, :complaints, :delivery_attempts, :rejects
+  scope :between_date, ->(initial_date,final_date) {where(sent: (initial_date.beginning_of_day)..final_date.end_of_day) }
+
+  def self.info(initial_date,final_date)
+    email_info_range = between_date(initial_date,final_date)
+    collect_email_info(email_info_range)
+  end
+
+  private
+  def self.collect_email_info(email_infos)
+    Hash[ [:bounces,:complaints,:delivery_attempts,:rejects].map{ |k| [k, email_infos.inject(0){|sum, info| sum + info.send(k)}] } ]
+  end
 end
