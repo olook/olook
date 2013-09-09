8aa43196c89fd78bfcc2ed5629f9c7702ec430ce
Tiago Almeida <tiago.almeida@olook.com.br>
2013-09-09 14:35:12 -0300

    Fix vcr configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -111,7 +111,6 @@ Spork.prefork do
     config.use_transactional_fixtures = false
 
     config.include Devise::TestHelpers, :type => :controller
-    config.extend VCR::RSpec::Macros
   end
 end
 
