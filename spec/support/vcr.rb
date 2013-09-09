1d815b0dc2ed19f3b941ab50818bcbe065f59167
Tiago Almeida <tiago.almeida@olook.com.br>
2013-09-09 14:15:27 -0300

    Set webmock to VCR

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -3,6 +3,5 @@ require 'vcr'
 
 VCR.configure do |c|
   c.cassette_library_dir = Rails.root.join 'spec', 'vcr_tapes'
-  c.hook_into :fakeweb
-  c.ignore_localhost = true
+  c.hook_into :webmock
 end
