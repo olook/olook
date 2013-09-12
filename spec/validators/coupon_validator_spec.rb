f9d1e724568eaa85143703647b4150bfc0b66051
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-12 18:45:40 -0300

    refactor coupon validation

--- spec/validators/coupon_validator_spec.rb
+++ spec/validators/coupon_validator_spec.rb
@@ -22,6 +22,35 @@ describe CouponValidator do
       end
     end
 
+    context "when coupon has brand" do
+
+      context "and cart has any product with brand eq coupon's brand" do
+      end
+
+      context "but cart has no any product with brand eq coupon's brand" do
+        let(:coupon) { FactoryGirl.build(:standard_coupon) }
+
+        before do
+          coupon.stub(:can_be_applied_to_any_product_in_the_cart?).and_return(false)
+        end
+
+        it "adds an error" do
+          cart.coupon_code = coupon.code
+          Coupon.stub(:find_by_code).with(coupon.code).and_return(coupon)
+          subject.validate(cart)
+          cart.errors.size.should eq(1)
+        end
+
+        it "associates an error to coupon_code" do
+          cart.coupon_code = coupon.code
+          Coupon.stub(:find_by_code).with(coupon.code).and_return(coupon)
+          subject.validate(cart)
+          cart.errors[:coupon_code].first.should eq("Não pode ser aplicado")
+        end
+      end
+
+    end
+
     context "coupon_code exists" do
 
       context "and coupon is expired" do
@@ -39,6 +68,28 @@ describe CouponValidator do
           Coupon.stub(:find_by_code).with(expired_coupon.code).and_return(expired_coupon)
           subject.validate(cart)
           cart.errors[:coupon_code].first.should eq("Cupom expirado. Informe outro por favor")
+        end
+      end
+
+      context "but is not more advantageous than any promotion" do
+        let(:coupon) { FactoryGirl.build(:standard_coupon) }
+
+        before do
+          coupon.stub(:is_more_advantageous_than_any_promotion?).and_return(false)
+        end
+
+        it "adds an error" do
+          cart.coupon_code = coupon.code
+          Coupon.stub(:find_by_code).with(coupon.code).and_return(coupon)
+          subject.validate(cart)
+          cart.errors.size.should eq(1)
+        end
+
+        it "associates an error to coupon_code" do
+          cart.coupon_code = coupon.code
+          Coupon.stub(:find_by_code).with(coupon.code).and_return(coupon)
+          subject.validate(cart)
+          cart.errors[:coupon_code].first.should eq("A promoção é mais vantajosa que o cupom")
         end
       end
 
@@ -60,7 +111,7 @@ describe CouponValidator do
 
       context "and coupon is not available" do
         let(:unavailable_coupon) { FactoryGirl.build(:unavailable_coupon) }
-        
+
         it "adds an error" do
           cart.coupon_code = unavailable_coupon.code
           Coupon.stub(:find_by_code).with(cart.coupon_code).and_return(unavailable_coupon)
@@ -78,7 +129,7 @@ describe CouponValidator do
 
       context "and coupon is available" do
         let(:available_coupon) { FactoryGirl.build(:coupon) }
-        
+
         it "doesnt add an error to cart" do
           cart.coupon_code = available_coupon.code
           Coupon.stub(:find_by_code).with(cart.coupon_code).and_return(available_coupon)
