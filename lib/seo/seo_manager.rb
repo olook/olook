cb170b49c2c9dfef0cf10e9d44e04d9cb8a0799f
Tiago Almeida <tiago.almeida@olook.com.br>
2013-09-13 11:02:50 -0300

    make sep manager initialize

--- lib/seo/seo_manager.rb
+++ lib/seo/seo_manager.rb
@@ -1,5 +1,11 @@
 # -*- encoding : utf-8 -*-
 module Seo
   class SeoManager
+    attr_accessor :url
+
+    def initialize url_params
+      @url = url_params
+    end
+
   end
 end
--- spec/lib/seo/seo_manager_spec.rb
+++ spec/lib/seo/seo_manager_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
-describe Seo::SesManager do
+describe Seo::SeoManager do
+  before do
+    @seo_class = Seo::SeoManager.new("/sapato")
+  end
+  describe "#initialize" do
+    it "set url" do
+      expect(@seo_class.url).to_not be_nil
+    end
+  end
 
 end
