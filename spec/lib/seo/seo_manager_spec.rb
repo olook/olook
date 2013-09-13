a2f9b4e547e605a7d2d205e40b3e94faf4c5c6ef
Tiago Almeida <tiago.almeida@olook.com.br>
2013-09-13 18:06:26 -0300

    WIP - seo choose meta tag logic

--- spec/lib/seo/seo_manager_spec.rb
+++ spec/lib/seo/seo_manager_spec.rb
@@ -10,13 +10,28 @@ describe Seo::SeoManager do
     end
   end
 
-  context "When find on meta tags constant file" do
-    describe "#meta_tag" do
-      it "always return value" do
-        expect(@seo_class.meta_tag).to_not be_nil
+  describe "#meta_tag" do
+    it "always return value" do
+      expect(@seo_class.select_meta_tag).to_not be_nil
+    end
+    context "When dont match url dont match to file items " do
+      it "return default" do
+        seo_class_fail = Seo::SeoManager.new("/asd")
+        expect(seo_class_fail.select_meta_tag).to eql('Sapatos Femininos e Roupas Femininas | Olook')
       end
-
+    end
+    context "When find meta tags on file" do
       it "return map meta tag" do
+        expect(@seo_class.select_meta_tag).to eql('Sapatos Femininos | Olook')
+      end
+    end
+    context "When find meta tags on model" do
+      before do
+        @product = FactoryGirl.create(:shoe, name: 'Vestido Estampado Manga Longa Ecletic')
+        @seo_model = Seo::SeoManager.new("/asd", model: @product)
+      end
+      it "return map meta tag" do
+        expect(@seo_class.select_meta_tag).to eql('Vestido Estampado Manga Longa Ecletic - Roupas e Sapatos Femininos | Olook')
       end
     end
   end
