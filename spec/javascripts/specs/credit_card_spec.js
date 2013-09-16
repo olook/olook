b9e38459ba1cd14cd9a996409edb0e266a13f471
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-16 15:04:04 -0300

    create tests for credit card js validation and refactor it

--- spec/javascripts/specs/credit_card_spec.js
+++ spec/javascripts/specs/credit_card_spec.js
@@ -1,12 +1,12 @@
 describe("CreditCard", function() {
   describe(".validateNumber", function() {
-    it("return true for '4111.1111.1111.1111'", function() {  
+    it("return true for '4111.1111.1111.1111'", function() {
       expect(CreditCard.validateNumber("4111.1111.1111.1111")).toBeTruthy();
     });
 
-    it("return true for '4111111111111111'", function() {  
+    it("return true for '4111111111111111'", function() {
       expect(CreditCard.validateNumber("4111.1111.1111.1111")).toBeTruthy();
-    });   
+    });
 
     it("return false for '0000000000000001'", function() {
       expect(CreditCard.validateNumber("0000000000000001")).toBeFalsy();
@@ -14,6 +14,22 @@ describe("CreditCard", function() {
 
     it("return false for '0000.0000.0000.0001'", function() {
       expect(CreditCard.validateNumber("0000000000000001")).toBeFalsy();
+    });
+
+    it("displays message error for invalid credit card number", function() {
+      loadFixtures("checkout_form.html");
+      $("#checkout_payment_credit_card_number").validateCreditCardNumber();
+      $("#checkout_payment_credit_card_number").val('123')
+      $("#checkout_payment_credit_card_number").blur();
+      expect($("#credit_card_error.span_error")).toHaveText('O número do cartão parece estranho. Pode conferir?');
+    });
+
+    it("removes message error for invalid credit card number", function() {
+      loadFixtures("checkout_form.html");
+      $("#checkout_payment_credit_card_number").validateCreditCardNumber();
+      $("#checkout_payment_credit_card_number").val('4111.1111.1111.1111')
+      $("#checkout_payment_credit_card_number").blur();
+      expect($("#credit_card_error.span_error")).toHaveText('');
     });
   });
 });
