897615d81a62467a1116bf2a0758f10e9589544d
Rafael <rafael.manoel@gmail.com>
2013-09-17 15:47:08 -0300

    move Formatter object to a new file.

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -825,17 +825,3 @@ olook = o = {
   }
 }
 /*** END EMAIL BAR FUNCTIONS ***/
-
-/**
-  * Formatter. This object may be used by the whole applicaton
-  */
-
-var Formatter = Formatter || {
-  /**
-    * Converts number to currency (Real with ',' for decimal)
-    *
-    */
-  toCurrency: function(value) {
-    return "R$ " + value.toFixed(2).toString().replace('.',',');
-  }
-};
