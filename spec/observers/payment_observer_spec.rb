223631b0561bb90782e1ba68930d1d98f5911e6d
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-18 16:33:15 -0300

    fixing payment observer tests

--- spec/observers/payment_observer_spec.rb
+++ spec/observers/payment_observer_spec.rb
@@ -8,8 +8,10 @@ describe PaymentObserver do
     end
     context "when is a billet" do
       let(:payment) { FactoryGirl.build(:billet, :waiting_payment) }
+      let(:mock_mail) { double :mail }
       it "enqueues an email to notify about authorization" do
-        DevAlertMailer.should_receive(:notify).with(to: 'rafael.manoel@olook.com.br', subject: "Ordem de numero #{ payment.order.number } foi autorizada")
+        DevAlertMailer.should_receive(:notify).with(to: 'rafael.manoel@olook.com.br', subject: "Ordem de numero #{ payment.order.number } foi autorizada").and_return(mock_mail)
+        mock_mail.should_receive(:deliver)
         described_class.notify_about_authorization(payment)
       end
     end
