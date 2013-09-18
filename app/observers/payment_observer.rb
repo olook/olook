96dc4e682e0917d3f00873c140fe5bfc64d8fdf1
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-18 19:17:05 -0300

    remove payment observer and extracting logic

--- app/observers/payment_observer.rb
+++
@@ -1,7 +0,0 @@
-class PaymentObserver < ActiveRecord::Observer
-  observe :payment
-
-  def self.notify_about_authorization payment
-    DevAlertMailer.notify(to: "rafael.manoel@olook.com.br", subject: "Ordem de numero #{ payment.order.number } foi autorizada").deliver
-  end
-end
