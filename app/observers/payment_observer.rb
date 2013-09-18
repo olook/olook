4a368e693a3dcba058d12df1b4a3c7741ffbe8a8
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-18 15:12:26 -0300

    delivering email

--- app/observers/payment_observer.rb
+++ app/observers/payment_observer.rb
@@ -2,6 +2,6 @@ class PaymentObserver < ActiveRecord::Observer
   observe :payment
 
   def self.notify_about_authorization payment
-      DevAlertMailer.notify(to: 'rafael.manoel@olook.com.br', subject: "Ordem de numero #{ payment.order.number } foi autorizada")
+    DevAlertMailer.notify(to: "vinicius.monteiro@olook.com.br", subject: "Ordem de numero #{ payment.order.number } foi autorizada").deliver
   end
 end
