010050af6153d24c515226a218a03c6a9e7f62a6
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-09-20 17:57:12 -0300

    Adding email template

--- app/mailers/campaign_email_notification_mailer.rb
+++ app/mailers/campaign_email_notification_mailer.rb
@@ -1,11 +1,15 @@
 # -*- encoding : utf-8 -*-
 class CampaignEmailNotificationMailer < ActionMailer::Base
-	default :from => "olook <bemvinda@my.olookmail.com.br>"
+  default :from => "olook <bemvinda@my.olookmail.com.br>"
 
-	def welcome_email(email)
-		mail(:to => email, :subject => "Recebemos seu e-mail. Agora é hora de descobrir o seu estilo.")
-    
+  def welcome_email(email)
+    mail(:to => email, :subject => "Recebemos seu e-mail. Agora é hora de descobrir o seu estilo.")
     headers["X-SMTPAPI"] = { 'category' => 'welcome_email' }.to_json
-	end
+  end
+
+  def olookmovel_welcome_email(email)
+    mail(:to => email, :subject => "Recebemos seu e-mail. Agora é hora de descobrir o seu estilo.")
+    headers["X-SMTPAPI"] = { 'category' => 'welcome_email' }.to_json
+  end
 
 end
