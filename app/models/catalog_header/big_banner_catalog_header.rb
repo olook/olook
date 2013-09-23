a90eb07caaf4452eefe8279a3c2cf7e8f8216e65
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-09-23 18:12:44 -0300

    Apply structure of catalog header

---
+++ app/models/catalog_header/big_banner_catalog_header.rb
@@ -0,0 +1,3 @@
+class CatalogHeader::BigBannerCatalogHeader < CatalogHeader::CatalogHeader
+  attr_accessible :big_banner, :link_big_banner, :alt_big_banner
+end
---
+++ app/models/catalog_header/small_banner_catalog_header.rb
@@ -0,0 +1,5 @@
+class CatalogHeader::SmallBannerCatalogHeader < CatalogHeader::CatalogHeader
+  attr_accessible :medium_banner, :link_medium_banner,:alt_medium_banner,
+                  :small_banner1, :link_small_banner1,:alt_small_banner1,
+                  :small_banner2, :link_small_banner2,:alt_small_banner2
+end
