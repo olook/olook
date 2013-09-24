9dc04318a2a70cc510f5f49f21392e9c802b59e2
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-09-24 16:50:14 -0300

    Correctly send welcome email to olookmovel user

--- app/workers/campaign_email_notification_worker.rb
+++ app/workers/campaign_email_notification_worker.rb
@@ -2,7 +2,11 @@ class CampaignEmailNotificationWorker
 	@queue = :mailer
 
 	def self.perform(email)
-    mail = CampaignEmailNotificationMailer.welcome_email(email)
+    if CampaignEmail.where(email: email).first.profile == 'olookmovel'
+      mail = CampaignEmailNotificationMailer.olookmovel_welcome_email(email)
+    else
+      mail = CampaignEmailNotificationMailer.welcome_email(email)
+    end
     mail.deliver
   end
 end
