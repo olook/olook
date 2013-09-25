6b5f30a6de05ee5b2236feeeb043f8634e9be772
Vinícius Monteiro <vsmont.monteiro@gmail.com>
2013-09-25 11:36:13 -0300

    save old price instead new price on product observer when price has changed

--- spec/observers/product_observer_spec.rb
+++ spec/observers/product_observer_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 describe ProductObserver do
 
   let(:product) { FactoryGirl.create(:shoe) }
-  let(:old_price) { product.price }
-  let(:old_retail_price) { product.retail_price }
+  let!(:old_price) { product.price }
+  let!(:old_retail_price) { product.retail_price }
   let(:new_value) { BigDecimal "99" }
   context "when changing product price" do
     it { expect{product.update_attributes(price: new_value)}.to change{product.price_logs.count}.from(0).to(1) }
@@ -19,6 +19,7 @@ describe ProductObserver do
     it { expect{product.update_attributes(price: new_value)}.to change{product.price_logs.count}.from(0).to(1) }
     context "price logs data" do
       before do
+        product.master_variant.stub(:retail_price_was).and_return(old_retail_price)
         product.update_attributes(retail_price: new_value)
       end
       it { expect(product.price_logs.first.retail_price).to eq(old_retail_price) }
