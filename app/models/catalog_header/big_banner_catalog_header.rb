86a6efe007e915d984b9bc2e70e47701245f739f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-09-25 13:34:51 -0300

    Apply new uploader on SmallBannerCatalogHeader and BigBannerCatalogHeader

--- app/models/catalog_header/big_banner_catalog_header.rb
+++ app/models/catalog_header/big_banner_catalog_header.rb
@@ -1,3 +1,5 @@
 class CatalogHeader::BigBannerCatalogHeader < CatalogHeader::CatalogHeader
   attr_accessible :big_banner, :link_big_banner, :alt_big_banner
+
+  mount_uploader :big_banner, CatalogHeaderUploader
 end
