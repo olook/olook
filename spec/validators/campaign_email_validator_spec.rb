00a8aca9f7dcb262255cc030a8fcf304904e92eb
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-09-25 17:50:57 -0300

    Fix tests

--- spec/validators/campaign_email_validator_spec.rb
+++ spec/validators/campaign_email_validator_spec.rb
@@ -10,7 +10,7 @@ describe CampaignEmailValidator do
     context "when there's any user with given email" do
       it "returns message Email já cadastrado" do
         campaign_email.email = user.email
-        campaign_email.errors.should_receive(:add).with(:email, 'já está cadastrado')
+        campaign_email.errors.should_receive(:add).with(:email, :exists, email: user.email)
         subject.validate(campaign_email)
       end
     end
@@ -18,7 +18,7 @@ describe CampaignEmailValidator do
     context "when there's no user with given email" do
       it "doesn't return message Email já cadastrado" do
         campaign_email.email = "foo@bar.com"
-        campaign_email.errors.should_not_receive(:add).with(:email, 'já está cadastrado')
+        campaign_email.errors.should_not_receive(:add).with(:email, :exists, email: "foo@bar.com")
         subject.validate(campaign_email)
       end
     end
