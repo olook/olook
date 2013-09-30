db05970365865555e5e6738d50323f1d3864176f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-09-30 14:44:54 -0300

    Finish countdown implementation

--- app/assets/javascripts/admin/countdown.js
+++ app/assets/javascripts/admin/countdown.js
@@ -4,22 +4,31 @@ function Countdown(domEl, charCount){
   if(this._input.next('.countdown').length == 1){
     this._placer = this._input.next('.countdown');
   };
+  return this;
 };
 
 Countdown.prototype.placer = function() {
   if(!this._placer) {
-    this._input.after('<div class="countdown"></div>')
+    this._input.after('<div class="countdown"></div>');
   }
+  this._placer = this._input.next('.countdown');
+  this._placer.html(this._count);
+  return this;
 }
 
 Countdown.prototype.attach = function (){
-  if(input.next('.countdown').length == 1){
-    input.parent().find('.countdown').html(count);
-  };
-  input.change(function(){
-    updateCountdown($(this), count);
+  this.updateCount();
+  var cd = this;
+  this._input.keyup(function(){
+    cd.updateCount();
   });
-  input.keyup(function(){
-    updateCountdown($(this), count);
+  this._input.change(function(){
+    cd.updateCount();
   });
 };
+
+Countdown.prototype.updateCount = function (){
+  this._placer.text(this._count - this._input.val().length);
+};
+
+
