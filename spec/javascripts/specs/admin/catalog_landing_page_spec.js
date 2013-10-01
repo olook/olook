6f06fff296851bc3db3691a3563c0be699d371ab
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-01 10:41:11 -0300

    Finish show hide action of catalog landing page implementation

--- spec/javascripts/specs/admin/catalog_landing_page_spec.js
+++ spec/javascripts/specs/admin/catalog_landing_page_spec.js
@@ -5,13 +5,37 @@ describe("Show and Hide according CatalogLandingPage#type", function(){
 
   it("show #small_banner_catalog_header when type is CatalogHeader::SmallBannerCatalogHeader", function(){
     var type = $('#catalog_base_type');
+    changeCatalogBaseType();
+    type.val('CatalogHeader::SmallBannerCatalogHeader').change();
     var small_fieldset = $('fieldset#small_banner_catalog_header');
     var big_fieldset = $('fieldset#big_banner_catalog_header');
     var text_fieldset = $('fieldset#text_catalog_header');
-    changeCatalogBaseType();
-    type.val('CatalogHeader::SmallBannerCatalogHeader').change();
     expect(small_fieldset).toBeVisible();
     expect(big_fieldset).toBeHidden();
     expect(text_fieldset).toBeHidden();
+  });
+
+  it("show #big_banner_catalog_header when type is CatalogHeader::BigBannerCatalogHeader", function(){
+    var type = $('#catalog_base_type');
+    changeCatalogBaseType();
+    type.val('CatalogHeader::BigBannerCatalogHeader').change();
+    var small_fieldset = $('fieldset#small_banner_catalog_header');
+    var big_fieldset = $('fieldset#big_banner_catalog_header');
+    var text_fieldset = $('fieldset#text_catalog_header');
+    expect(small_fieldset).toBeHidden();
+    expect(big_fieldset).toBeVisible();
+    expect(text_fieldset).toBeHidden();
+  });
+
+  it("show #text_catalog_header when type is CatalogHeader::TextCatalogHeader", function(){
+    var type = $('#catalog_base_type');
+    changeCatalogBaseType();
+    type.val('CatalogHeader::TextCatalogHeader').change();
+    var small_fieldset = $('fieldset#small_banner_catalog_header');
+    var big_fieldset = $('fieldset#big_banner_catalog_header');
+    var text_fieldset = $('fieldset#text_catalog_header');
+    expect(text_fieldset).toBeVisible();
+    expect(small_fieldset).toBeHidden();
+    expect(big_fieldset).toBeHidden();
   });
 });
