c9a59accba1461fd570f2920bc7141217d31346e
Rafael <rafael.manoel@gmail.com>
2013-10-01 14:25:26 -0300

    improve test readability

--- spec/javascripts/specs/admin/catalog_landing_page_spec.js
+++ spec/javascripts/specs/admin/catalog_landing_page_spec.js
@@ -1,39 +1,35 @@
 describe("Show and Hide according CatalogLandingPage#type", function(){
   beforeEach(function(){
     loadFixtures('catalog_landing_page_form.html');
+    type = $('#catalog_base_type');
+
+    small_fieldset = $('fieldset#small_banner_catalog_header');
+    big_fieldset = $('fieldset#big_banner_catalog_header');
+    text_fieldset = $('fieldset#text_catalog_header');    
+    
+    changeCatalogBaseType();
+  });
+
+  it("should attach a function to type's change event ", function() {
+    expect(type).toHandle('change');
   });
 
   it("show #small_banner_catalog_header when type is CatalogHeader::SmallBannerCatalogHeader", function(){
-    var type = $('#catalog_base_type');
-    changeCatalogBaseType();
     type.val('CatalogHeader::SmallBannerCatalogHeader').change();
-    var small_fieldset = $('fieldset#small_banner_catalog_header');
-    var big_fieldset = $('fieldset#big_banner_catalog_header');
-    var text_fieldset = $('fieldset#text_catalog_header');
     expect(small_fieldset).toBeVisible();
     expect(big_fieldset).toBeHidden();
     expect(text_fieldset).toBeHidden();
   });
 
   it("show #big_banner_catalog_header when type is CatalogHeader::BigBannerCatalogHeader", function(){
-    var type = $('#catalog_base_type');
-    changeCatalogBaseType();
     type.val('CatalogHeader::BigBannerCatalogHeader').change();
-    var small_fieldset = $('fieldset#small_banner_catalog_header');
-    var big_fieldset = $('fieldset#big_banner_catalog_header');
-    var text_fieldset = $('fieldset#text_catalog_header');
     expect(small_fieldset).toBeHidden();
     expect(big_fieldset).toBeVisible();
     expect(text_fieldset).toBeHidden();
   });
 
   it("show #text_catalog_header when type is CatalogHeader::TextCatalogHeader", function(){
-    var type = $('#catalog_base_type');
-    changeCatalogBaseType();
     type.val('CatalogHeader::TextCatalogHeader').change();
-    var small_fieldset = $('fieldset#small_banner_catalog_header');
-    var big_fieldset = $('fieldset#big_banner_catalog_header');
-    var text_fieldset = $('fieldset#text_catalog_header');
     expect(text_fieldset).toBeVisible();
     expect(small_fieldset).toBeHidden();
     expect(big_fieldset).toBeHidden();
