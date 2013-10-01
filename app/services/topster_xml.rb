121e39d855aed2838510579a50b7cb768698ecd6
Rafael <rafael.manoel@gmail.com>
2013-10-01 10:31:35 -0300

    refactor to improve readability

--- app/services/topster_xml.rb
+++ app/services/topster_xml.rb
@@ -10,28 +10,7 @@ class TopsterXml
     @products = load_products
 
     templates.each do |template_name|
-      # TODO REFACTOR ALL THIS LOGIC
-      #header
-      xml = '<?xml version="1.0" encoding="UTF-8"?>'
-      if (template_name == :nextperformance)
-        xml += '<products>'
-      elsif (template_name == :topster)
-        xml += '<produtos>'
-      end
-
-
-      @products.each do |product|
-        xml += generate_for(template_name, product)
-      end
-
-      # footer
-      if (template_name == :nextperformance)
-        xml += '</products>'
-      elsif (template_name == :topster)
-        xml += '</produtos>'
-      end
-
-      xmls[template_name] = xml
+      xmls[template_name] = generate_for(template_name)
     end
 
     xmls
@@ -51,11 +30,13 @@ class TopsterXml
   end
 
   private
-    def self.generate_for(template_name, product)
+    def self.generate_for(template_name)
       if template_name == :nextperformance
         renderer = ERB.new(File.read('app/views/xml/nextperformance_template.xml.erb'))
       elsif template_name == :topster
         renderer = ERB.new(File.read('app/views/xml/topster_template.xml.erb'))
+      else
+        renderer = OpenStruct.new({result: ""})
       end
 
       renderer.result(binding)
@@ -63,7 +44,7 @@ class TopsterXml
 
     def self.load_products
       # This method was copied from XmlController
-      products = Product.valid_for_xml(Product.xml_blacklist("products_blacklist").join(','))     
+      products = Product.where(collection_id: 24).valid_for_xml(Product.xml_blacklist("products_blacklist").join(','))     
       liquidation_products = []
       active_liquidation = LiquidationService.active
       liquidation_products = active_liquidation.resume[:products_ids] if active_liquidation
