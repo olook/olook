66aedfd849b069591bae9a4a925ae1565dd10ac1
Luis Daher <luisotaviodaher@gmail.com>
2013-10-01 13:53:42 -0300

    javascript refactoring

--- spec/javascripts/specs/string_utils_spec.js
+++ spec/javascripts/specs/string_utils_spec.js
@@ -1,19 +1,19 @@
-describe("initProduct", function() {
+describe("StringUtils", function() {
     describe("#isEmpty", function(){
         it("returns true for an empty value", function() {
-            expect(initProduct.isEmpty("")).toBeTruthy();
+            expect(StringUtils.isEmpty("")).toBeTruthy();
         });
 
         it("returns true for a null value", function() {
-            expect(initProduct.isEmpty(null)).toBeTruthy();
+            expect(StringUtils.isEmpty(null)).toBeTruthy();
         });
 
         it("returns true for an undefined value", function() {
-            expect(initProduct.isEmpty(undefined)).toBeTruthy();
+            expect(StringUtils.isEmpty(undefined)).toBeTruthy();
         });
 
         it("returns false for a non-empty value", function() {
-            expect(initProduct.isEmpty("test")).toBeFalsy();
+            expect(StringUtils.isEmpty("test")).toBeFalsy();
         });
     })
 });
