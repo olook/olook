0d55e24ef848fc4ccc9eb6d95a6ff6237b028876
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-01 10:41:11 -0300

    Finish show hide action of catalog landing page implementation

--- app/assets/javascripts/admin/catalog_landing_page.js
+++ app/assets/javascripts/admin/catalog_landing_page.js
@@ -3,14 +3,27 @@ function changeCatalogBaseType(){
   var small_fieldset = $('fieldset#small_banner_catalog_header');
   var big_fieldset = $('fieldset#big_banner_catalog_header');
   var text_fieldset = $('fieldset#text_catalog_header');
+  if(type.val() == 'CatalogHeader::SmallBannerCatalogHeader') {
+    small_fieldset.show();
+  }else if (type.val() == 'CatalogHeader::BigBannerCatalogHeader'){
+    big_fieldset.show();
+  } else if(type.val() == 'CatalogHeader::TextCatalogHeader'){
+    text_fieldset.show();
+  }
   type.change(function(){
     small_fieldset.hide();
     big_fieldset.hide();
     text_fieldset.hide();
     if($(this).val() == 'CatalogHeader::SmallBannerCatalogHeader') {
       small_fieldset.show();
+    }else if ($(this).val() == 'CatalogHeader::BigBannerCatalogHeader'){
+      big_fieldset.show();
+    } else if($(this).val() == 'CatalogHeader::TextCatalogHeader'){
+      text_fieldset.show();
     }
   });
 };
 
-$(changeCatalogBaseType);
+$(function(){
+  $(changeCatalogBaseType);
+});
