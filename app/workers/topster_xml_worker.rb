3e76458741ba42bd3db7ffcae06ede61193ecfd5
Rafael <rafael.manoel@gmail.com>
2013-10-01 17:19:23 -0300

    add criteo, sociomantic and triggit partners

--- app/workers/topster_xml_worker.rb
+++ app/workers/topster_xml_worker.rb
@@ -2,9 +2,11 @@ class TopsterXmlWorker
   @queue = :topster_xml
 
   def self.perform
-    xmls = TopsterXml.create_xml([:topster, :nextperformance])
-    TopsterXml.send_to_amazon(xmls[:topster], 'topster_data.xml')
-    TopsterXml.send_to_amazon(xmls[:nextperformance], 'nextperformance_data.xml')
+    xmls = TopsterXml.create_xmls
+    TopsterXml.upload(xmls)
+    # TopsterXml.send_to_amazon(xmls[:topster], 'topster_data.xml')
+    # TopsterXml.send_to_amazon(xmls[:nextperformance], 'nextperformance_data.xml')
+    # TopsterXml.send_to_amazon(xmls[:criteo], 'criteo_data.xml')
   end
 
 end
