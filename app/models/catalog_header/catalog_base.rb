915b24a5226f20fe5f1b0163928f3286714f5421
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-03 15:57:43 -0300

    Change model to use new fields structure

--- app/models/catalog_header/catalog_base.rb
+++ app/models/catalog_header/catalog_base.rb
@@ -1,5 +1,5 @@
 class CatalogHeader::CatalogBase < ActiveRecord::Base
-  attr_accessible :h1, :h2, :type, :url
+  attr_accessible :seo_text, :type, :url
 
   validates :type, :url, :presence => true, :exclusion => ["CatalogHeader::CatalogBase"]
 
