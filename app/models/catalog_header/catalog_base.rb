4c6af60a13421fb9e0ea65f443a9a18725a56cba
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-04 16:47:33 -0300

    Adding methods to check type of catalog header

--- app/models/catalog_header/catalog_base.rb
+++ app/models/catalog_header/catalog_base.rb
@@ -6,12 +6,28 @@ class CatalogHeader::CatalogBase < ActiveRecord::Base
   scope :with_type, ->(type) {where(type: type)} 
 
   def self.factory params
-   if params[:type] == 'CatalogHeader::BigBannerCatalogHeader'
-     CatalogHeader::BigBannerCatalogHeader.new(params)
-   elsif params[:type] == 'CatalogHeader::SmallBannerCatalogHeader'
-     CatalogHeader::SmallBannerCatalogHeader.new(params)
-   else
-     CatalogHeader::TextCatalogHeader.new(params)
-   end
+    if params[:type] == 'CatalogHeader::BigBannerCatalogHeader'
+      CatalogHeader::BigBannerCatalogHeader.new(params)
+    elsif params[:type] == 'CatalogHeader::SmallBannerCatalogHeader'
+      CatalogHeader::SmallBannerCatalogHeader.new(params)
+    else
+      CatalogHeader::TextCatalogHeader.new(params)
+    end
+  end
+
+  def self.for_url(url)
+    where(url: url)
+  end
+
+  def text?
+    false
+  end
+
+  def big_banner?
+    false
+  end
+
+  def small_banner?
+    false
   end
 end
--- spec/models/catalog_header/catalog_base_spec.rb
+++ spec/models/catalog_header/catalog_base_spec.rb
@@ -9,15 +9,26 @@ describe CatalogHeader::CatalogBase do
   describe ".scopes" do
 
     before do
-    FactoryGirl.create(:catalog_header, :text)
-    FactoryGirl.create(:catalog_header, :big_banner)
-    FactoryGirl.create(:catalog_header, :small_banner)
+      FactoryGirl.create(:catalog_header, :text)
+      FactoryGirl.create(:catalog_header, :big_banner)
+      FactoryGirl.create(:catalog_header, :small_banner)
     end
     it "return only text type" do
       expect(described_class.with_type("CatalogHeader::TextCatalogHeader").count).to eql(1)
     end
     it "return without text type" do
       expect(described_class.with_type(["CatalogHeader::SmallBannerCatalogHeader", "CatalogHeader::BigBannerCatalogHeader"]).count).to eql(2)
+    end
+  end
+
+  describe '.for_url' do
+    it "should return Arel" do
+      expect(CatalogHeader::CatalogBase.for_url('/sapato')).to respond_to(:all)
+    end
+
+    it "should find exact url" do
+      bota = FactoryGirl.create(:catalog_header, :text, :url => '/sapato/bota')
+      expect(CatalogHeader::CatalogBase.for_url('/sapato/bota').first).to eq(bota)
     end
   end
 end
