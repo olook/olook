e71667fc059ea18f1f86b96bffd131bb739426c8
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-10 14:57:27 -0300

    Removing specs

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -3,8 +3,6 @@ class LandingPagesController < ApplicationController
   layout 'application'
 
   def show
-    @landing_page = LandingPage.find_by_page_url!(params[:page_url])
-    redirect_to root_path unless @landing_page.enabled?
   end
 
   def olookmovel
