a66fcedc8de4f32efe8a5f120f7ec3a2869e416f
Luis Daher <luisotaviodaher@gmail.com>
2013-10-10 11:40:25 -0300

    wip - breadcrumbs implemented for catalog

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -6,4 +6,13 @@ module BreadcrumbHelper
       end.concat(content_tag(:li,"#{product.formatted_name(500)}")).html_safe
     end
   end
+
+  def first_level_breadcrumb(title, style_class="breadcrumb")
+    content_tag(:ul, class: style_class) do
+      [BreadcrumbService.home_url].inject("") do |whole, breadcrumb_hash|
+        whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
+      end.concat(content_tag(:li,title)).html_safe
+    end    
+  end
+
 end
