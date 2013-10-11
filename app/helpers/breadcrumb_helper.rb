42e63d0f60d2e9962583ca82146692250614cc10
Luis Daher <luisotaviodaher@gmail.com>
2013-10-11 18:57:30 -0300

    removing useless code

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -2,7 +2,7 @@
 module BreadcrumbHelper
   def product_breadcrumbs_for(product, style_class="breadcrumb")
     content_tag(:ul, class: style_class) do
-      BreadcrumbService.product_breadcrumbs_for(product).inject("") do |whole, breadcrumb_hash|
+      [home_url,{name: product.category_humanize.titleize.pluralize, url: "/#{product.category_humanize.downcase}"}].inject("") do |whole, breadcrumb_hash|
         whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
       end.concat(content_tag(:li,"#{product.formatted_name(500)}")).html_safe
     end
@@ -10,7 +10,7 @@ module BreadcrumbHelper
 
   def first_level_breadcrumb(title, style_class="breadcrumb")
     content_tag(:ul, class: style_class) do
-      [BreadcrumbService.home_url].inject("") do |whole, breadcrumb_hash|
+      [home_url].inject("") do |whole, breadcrumb_hash|
         whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
       end.concat(content_tag(:li,title)).html_safe
     end    
@@ -18,7 +18,7 @@ module BreadcrumbHelper
 
   def brand_breadcrumbs_for(brand, style_class="breadcrumb")
     content_tag(:ul, class: style_class) do
-      BreadcrumbService.brand_breadcrumbs.inject("") do |whole, breadcrumb_hash|
+      [home_url,{name: "Marcas", url: "/marcas"}].inject("") do |whole, breadcrumb_hash|
         whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
       end.concat(content_tag(:li,"#{brand.name}")).html_safe
     end
@@ -26,7 +26,7 @@ module BreadcrumbHelper
 
   def collection_theme_breadcrumbs_for(collection_theme, style_class="breadcrumb")
     content_tag(:ul, class: style_class) do
-      BreadcrumbService.collection_theme_breadcrumbs.inject("") do |whole, breadcrumb_hash|
+      [home_url,{name: "Coleções", url: "/colecoes"}].inject("") do |whole, breadcrumb_hash|
         whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
       end.concat(content_tag(:li,"#{collection_theme.name}")).html_safe
     end
@@ -34,10 +34,16 @@ module BreadcrumbHelper
 
   def gift_breadcrumbs_for(style, style_class="breadcrumb")
     content_tag(:ul, class: style_class) do
-      BreadcrumbService.gift_breadcrumbs.inject("") do |whole, breadcrumb_hash|
+      [home_url,{name: "Presentes", url: "/presentes"}].inject("") do |whole, breadcrumb_hash|
         whole.concat content_tag(:li,link_to("#{breadcrumb_hash[:name]}",breadcrumb_hash[:url]))
       end.concat(content_tag(:li,style)).html_safe
     end
-  end    
+  end
+
+  private
+
+  def home_url
+    {name: "Home", url: "/"}
+  end
 
 end
