b6b352bafdaa3cbe17c0efbad2f62b532e8b8df8
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-14 16:37:59 -0300

    dropping custom_url

--- app/models/catalog_header/custom_url_catalog_header.rb
+++
@@ -1,9 +0,0 @@
-class CatalogHeader::CustomUrlCatalogHeader < CatalogHeader::CatalogBase
-  attr_accessible :organic_url, :product_list, :custom_url, :big_banner, :link_big_banner, :alt_big_banner,
-                  :resume_title, :text_complement, :title,
-                  :medium_banner, :link_medium_banner,:alt_medium_banner,
-                  :small_banner1, :link_small_banner1,:alt_small_banner1,
-                  :small_banner2, :link_small_banner2,:alt_small_banner2
-
-  validates :organic_url, :product_list
-end
