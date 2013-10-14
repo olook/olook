482cc9be2fb1afa7d2588ee7f3ccd17befc4d34f
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-14 16:10:49 -0300

    Apply all banners format to custom url type

--- spec/models/catalog_header/custom_url_catalog_header_spec.rb
+++ spec/models/catalog_header/custom_url_catalog_header_spec.rb
@@ -2,11 +2,7 @@
 require 'spec_helper'
 
 describe CatalogHeader::CustomUrlCatalogHeader do
-  it { should validate_presence_of(:big_banner) }
-  it { should validate_presence_of(:link_big_banner) }
-  it { should validate_presence_of(:alt_big_banner) }
   it { should validate_presence_of(:organic_url) }
   it { should validate_presence_of(:product_list) }
-  it { should validate_presence_of(:url) }
 end
 
