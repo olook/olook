15b96068239721b4fd544891533876d572306b63
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-14 14:59:28 -0300

    Fill products instance variables for view use

--- app/controllers/custom_url_controller.rb
+++ app/controllers/custom_url_controller.rb
@@ -1,6 +1,15 @@
 class CustomUrlController < ApplicationController
   layout "lite_application"
+  DEFAULT_PAGE_SIZE = 48
   def show
-    
+    @custom_url = CatalogHeader::CustomUrlCatalogHeader.find params[:id]
+    if @custom_url
+      @custom_url_products = SearchEngine.new(product_id: @custom_url.product_list)
+      page_size = params[:page_size] || DEFAULT_PAGE_SIZE
+      search_params = SeoUrl.parse(@custom_url.organic_url)
+      @search = SearchEngine.new(search_params, true).for_page(params[:page]).with_limit(page_size)
+    else
+      redirect_to root_url
+    end
   end
 end
