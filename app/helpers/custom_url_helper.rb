cd747cffda153baeb27367cc5c08add4bcabff59
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-15 09:55:21 -0300

    Add helper to choose correct filter

---
+++ app/helpers/custom_url_helper.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+module CustomUrlHelper
+
+  def retreive_filter path
+    case
+    when /colecoes/ =~ path
+      "collection_themes/menu"
+    when /marcas/ =~ path
+      "brands/side_filters"
+    else
+      category = [/bolsa/, /sapato/, /acessorio/, /roupa/].map{|a| path if a =~ path}.compact.first.gsub("/","")
+      ["catalog/filters", category]
+    end
+  end
+end
