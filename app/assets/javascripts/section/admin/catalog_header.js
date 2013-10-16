b4dfb98edf695a3084ff526ff84cb7eb4d51a0e9
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-16 10:50:20 -0300

    Add js function to show hide custom url attributes

--- app/assets/javascripts/section/admin/catalog_header.js
+++ app/assets/javascripts/section/admin/catalog_header.js
@@ -1,9 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //= require plugins/admin/countdown
+//= require plugins/admin/custom_url_attributes
 new Countdown('#catalog_base_url', 50).placer().attach();
-new Countdown('#catalog_base_h1', 70).placer().attach();
-new Countdown('#catalog_base_h2', 200).placer().attach();
 new Countdown('#catalog_base_alt_small_banner1', 40).placer().attach();
 new Countdown('#catalog_base_alt_small_banner2', 40).placer().attach();
 new Countdown('#catalog_base_alt_medium_banner', 40).placer().attach();
@@ -11,4 +10,8 @@ new Countdown('#catalog_base_alt_big_banner', 40).placer().attach();
 new Countdown('#catalog_base_title', 40).placer().attach();
 new Countdown('#catalog_base_resume_title', 200).placer().attach();
 new Countdown('#catalog_base_text_complement', 600).placer().attach();
+$(document).ready( function() {
+  new CustomUrlAttributes('#catalog_base_new_url').show_hide();
+});
+
 
