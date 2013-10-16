f0ae38434ceb6abad18df1d2b274683bb8a7f95b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-16 18:54:23 -0300

    Fix landing page for collection brand

--- app/helpers/custom_url_helper.rb
+++ app/helpers/custom_url_helper.rb
@@ -1,6 +1,5 @@
 # encoding: utf-8
 module CustomUrlHelper
-
   def retrieve_filter custom_url
     case
     when /^\/?colecoes/ =~ custom_url.organic_url
@@ -9,6 +8,17 @@ module CustomUrlHelper
       "brands/side_filters"
     else
       "catalogs/filters"
+    end
+  end
+
+  def load_stylesheet custom_url
+    case
+    when /^\/?colecoes/ =~ custom_url.organic_url
+      stylesheet_link_tag "new_structure/section/collection_themes"
+    when /^\/?marcas/ =~ custom_url.organic_url
+      stylesheet_link_tag "new_structure/section/brands"
+    else
+      stylesheet_link_tag "new_structure/section/lite_catalog"
     end
   end
 end
