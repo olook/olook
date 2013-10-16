3c3ea3b55eab9a1c9374f71c023da75007ed754e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-16 18:54:23 -0300

    Fix landing page for collection brand

--- app/controllers/custom_url_controller.rb
+++ app/controllers/custom_url_controller.rb
@@ -1,6 +1,8 @@
 class CustomUrlController < ApplicationController
   layout "lite_application"
   DEFAULT_PAGE_SIZE = 48
+  helper_method :check_organic_url_section
+
   def show
     @custom_url = CatalogHeader::CatalogBase.for_url(request.path).first
     if @custom_url
@@ -8,15 +10,43 @@ class CustomUrlController < ApplicationController
       page_size = params[:page_size] || DEFAULT_PAGE_SIZE
       search_params = SeoUrl.parse(@custom_url.organic_url)
       @search = SearchEngine.new(search_params, true).for_page(params[:page]).with_limit(page_size)
-      @url_builder = SeoUrl.new(search_params, "category", @search)
+      @url_builder = check_organic_url_section(
+        catalog: SeoUrl.new(search_params, "category", @search),
+        brand: SeoUrl.new(search_params, "brand", @search),
+        collection: SeoUrl.new(search_params, "collection_theme", @search)
+      )
       /\/?(?<category>roupa|bolsa|sapato|acessorio)[\/$]/ =~ @custom_url.organic_url.to_s
       @category = category
+      @collection_theme_groups = CollectionThemeGroup.order(:position).includes(:collection_themes)
     else
       redirect_to root_url
     end
   end
 
   private
+
+  def check_organic_url_section(hash)
+    case
+    when /^\/?colecoes/ =~ @custom_url.organic_url
+      if hash[:collection].respond_to?(:call)
+        hash[:collection].call
+      else
+        hash[:collection]
+      end
+    when /^\/?marcas/ =~ @custom_url.organic_url
+      if hash[:brand].respond_to?(:call)
+        hash[:brand].call
+      else
+        hash[:brand]
+      end
+    else
+      if hash[:catalog].respond_to?(:call)
+        hash[:catalog].call
+      else
+        hash[:catalog]
+      end
+    end
+  end
 
     def title_text
       Seo::SeoManager.new(request.path, model: @custom_url).select_meta_tag
