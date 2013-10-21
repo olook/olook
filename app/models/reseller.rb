cca6e82078e812baaa9da734a9b6d391e7bc098d
Tiago Almeida <tig.luiz.almeida@gmail.com.br>
2013-10-21 17:52:07 -0200

    Add boolean field on reseller

--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -1,5 +1,5 @@
 class Reseller < User
-  attr_accessible :cnpj, :active, :reseller, :corporate_name
-  attr_accessor :cnpj, :active, :reseller, :corporate_name
+  attr_accessible :cnpj, :active, :reseller, :corporate_name, :has_corporate
+  attr_accessor :cnpj, :active, :reseller, :corporate_name, :has_corporate
   scope :all_reseller, where(reseller: true)
 end
