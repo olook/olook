f08e254ff823b41606e70886dcacc2f43211899b
Tiago Almeida <tig.luiz.almeida@gmail.com.br>
2013-10-22 17:30:49 -0200

    Initial validation of reseller

--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -2,4 +2,6 @@ class Reseller < User
   attr_accessible :cnpj, :active, :reseller, :corporate_name, :has_corporate
   attr_accessor :cnpj, :active, :reseller, :corporate_name, :has_corporate
   scope :all_reseller, where(reseller: true)
+  validates :gender, :has_corporate, :birthday, presence: true
+  validates_presence_of :cnpj, :corporate_name, if: Proc.new { |reseller| reseller.has_corporate }
 end
