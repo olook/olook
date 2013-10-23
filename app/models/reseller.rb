29aa3c52b9f9a39479fcb6d9bdc0f1d561b4642d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-23 17:38:21 -0200

    Add accepts_nested_attributes_for addresses on users and reseller

--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -1,8 +1,8 @@
 class Reseller < User
-  attr_accessible :cnpj, :active, :reseller, :corporate_name, :has_corporate
-  attr_accessor :cnpj, :active, :reseller, :corporate_name, :has_corporate
+  attr_accessible :cnpj, :active, :reseller, :corporate_name, :has_corporate, :birthday, :gender, :addresses_attributes
   scope :all_reseller, where(reseller: true)
   validates :gender, :has_corporate, :birthday, presence: true
-  validates_presence_of :cnpj, :corporate_name, if: Proc.new { |reseller| reseller.has_corporate }
+  validates_presence_of :cnpj, :corporate_name, if: Proc.new { |reseller| reseller.has_corporate == true }
+  accepts_nested_attributes_for :addresses
   usar_como_cnpj :cnpj
 end
