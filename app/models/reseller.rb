bcad0e4fdc9f0689902c7a6586304052822a34e3
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-24 14:49:02 -0200

    Fix boolean validates of has_corporate

--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -1,7 +1,8 @@
 class Reseller < User
   attr_accessible :cnpj, :active, :reseller, :corporate_name, :has_corporate, :birthday, :gender, :addresses_attributes
   scope :all_reseller, where(reseller: true)
-  validates :gender, :has_corporate, :birthday, presence: true
+  validates :gender, :birthday, presence: true
+  validates :has_corporate, :inclusion => { :in => [true, false] }
   validates_presence_of :cnpj, :corporate_name, if: Proc.new { |reseller| reseller.has_corporate == true }
   accepts_nested_attributes_for :addresses
   usar_como_cnpj :cnpj
