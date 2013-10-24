ee74f1bbde3cc8f44115dd23948ad447fda48a43
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-24 17:10:37 -0200

    Add telephone validation only if we have the field

--- app/models/address.rb
+++ app/models/address.rb
@@ -10,7 +10,8 @@ class Address < ActiveRecord::Base
   MobileFormat = /^(?:\(11\)9\d{4}-\d{3,4}|\(\d{2}\)\d{4}-\d{4})$/
   StateFormat = /^[A-Z]{2}$/
 
-  validates_presence_of :country, :state, :street, :city, :number, :zip_code, :neighborhood, :telephone
+  validates_presence_of :country, :state, :street, :city, :number, :zip_code, :neighborhood
+  validate :telephone, :presence_of => true, :if => 'telephone.present?'
 
   validates :number, :numericality => true
   validates :zip_code, :format => {:with => ZipCodeFormat}
