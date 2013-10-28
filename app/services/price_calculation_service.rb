979ffa9c3df36382977726e6f44f7dfc75da1eb7
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-10-28 16:31:34 -0200

    price calculation related tests and adjustments

--- app/services/price_calculation_service.rb
+++ app/services/price_calculation_service.rb
@@ -2,28 +2,41 @@ class PriceCalculationService
   def initialize(product, options={})
     @product = product
     @coupon = options[:coupon]
+    @promotion = options[:promotion]
   end
 
   def calculate
-    return price_with_coupon if @coupon && @coupon.eligible_for_product?(@product)
-    if @product.retail_price < @product.price
-      @product.retail_price
-    else
-      @product.price
-    end
+    return price_with_promotion if eligible_promotion?
+    return price_with_coupon if eligible_coupon?
+    return @product.retail_price if eligible_markdown?
+    return @product.price
   end
 
   def final_price
     @final_price ||= calculate
   end
 
+
   private
   def price_with_coupon
-    @product.price * (1 - (@coupon.value * 0.01)) 
+    return @product.price * (1 - (@coupon.value * 0.01)) if @coupon.is_percentage?
+    return @product.price - @coupon.value
   end
 
   def eligible_coupon?
-    
+    @coupon && @coupon.eligible_for_product?(@product)
+  end
+
+  def eligible_markdown?
+    @product.retail_price < @product.price
+  end
+
+  def eligible_promotion? 
+    @promotion && @promotion.eligible_for_product?(@product)
+  end
+
+  def price_with_promotion
+    return @promotion.simulate_for_product(@product) if @promotion
   end
 end
 
