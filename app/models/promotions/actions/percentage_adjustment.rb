5ef14c5ef31ae61bf15ebd5b1356015f2f234fdf
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-10-30 17:00:08 -0200

    Convert haml show

--- app/models/promotions/actions/percentage_adjustment.rb
+++ app/models/promotions/actions/percentage_adjustment.rb
@@ -2,15 +2,18 @@
 class PercentageAdjustment < PromotionAction
 
   private
-
-    def calculate(cart_items, percent)
-      calculated_values = []
-      cart_items.each do |cart_item|
-        sub_total = cart_item.quantity * cart_item.price
-        adjustment = sub_total * BigDecimal("#{percent.to_i / 100.0}")
-        calculated_values << { id: cart_item.id, adjustment: adjustment }
-      end
-      calculated_values
+  def calculate(cart_items, percent)
+    calculated_values = []
+    cart_items.each do |cart_item|
+      sub_total = cart_item.quantity * cart_item.price
+      adjustment = sub_total * BigDecimal("#{percent.to_i / 100.0}")
+      calculated_values << { id: cart_item.id, adjustment: adjustment }
     end
+    calculated_values
+  end
+
+  def calculate_product(product, percent)
+    product.retail_price * BigDecimal("#{( 100.0 - percent.to_f ) / 100.0}")
+  end
 end
 
