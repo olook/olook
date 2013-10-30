e49c331329050854a3e376d01d0d170e3cc2ded7
Luis Daher <luisotaviodaher@gmail.com>
2013-10-30 11:05:06 -0200

    logic extracted to cart discount service

--- app/services/cart_discount_service.rb
+++ app/services/cart_discount_service.rb
@@ -1,28 +1,41 @@
 class CartDiscountService
   def initialize(cart, options={})
     @cart = cart
-    @coupon = options[:coupon]
     @promotion = options[:promotion]
   end
 
   def calculate
     return price_with_promotion if eligible_promotion?
     return price_with_coupon if eligible_coupon?
-    return @cart.total_price
+    return @cart.sub_total
+  end
+
+  def base_price
+    @cart.total_price
   end
 
   def final_price
     @final_price ||= calculate
   end
 
+  def calculate_coupon_discount 
+    coupon_value = @cart.coupon.value if @cart.coupon && !@cart.coupon.is_percentage?
+    coupon_value = 0.0 if @cart.coupon && !should_override_promotion_discount?
+    coupon_value ||= 0.0
+
+    if coupon_value >= @cart.sub_total
+      coupon_value = @cart.sub_total
+    end      
+    coupon_value
+  end
 
   private
   def price_with_coupon
-    @coupon.calculate_for_cart(@cart)
+    @cart.coupon.calculate_for_cart(@cart)
   end
 
   def eligible_coupon?
-    @coupon && @coupon.eligible_for_cart?(@cart)
+    @cart.coupon && @cart.coupon.eligible_for_cart?(@cart)
   end
 
   def eligible_promotion? 
@@ -32,4 +45,8 @@ class CartDiscountService
   def price_with_promotion
     @promotion.calculate_for_cart(@cart) if @promotion
   end
+
+  def should_override_promotion_discount?
+    @cart.total_coupon_discount > @cart.total_promotion_discount
+  end  
 end
