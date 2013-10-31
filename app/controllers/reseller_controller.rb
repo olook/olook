c9cc76c79356f258a2e258e4006bc191fe4bccd0
Thaiane Gazzi <thaianegazzi@gmail.com>
2013-10-31 18:54:05 -0200

    p.o adjusts 2

--- app/controllers/reseller_controller.rb
+++ app/controllers/reseller_controller.rb
@@ -10,7 +10,7 @@ class ResellerController < ApplicationController
     @reseller = Reseller.new(params[:reseller])
     if @reseller.save
       SACAlertMailer.reseller_notification(@reseller, "diogo.silva@olook.com.br,rafael.manoel@look.com.br,tiago.almeida@olook.com.br").deliver
-      redirect_to reseller_show_path(anchor: "success")
+      redirect_to reseller_show_path(anchor: "error_anchor")
     else
       custom_error_messages @reseller
       render "new"
