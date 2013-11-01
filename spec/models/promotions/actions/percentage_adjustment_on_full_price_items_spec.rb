b72109790b5de424e47781458945969bb97568f3
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-01 10:37:10 -0200

    Calculate and calculate_cart_items spec

--- spec/models/promotions/actions/percentage_adjustment_on_full_price_items_spec.rb
+++ spec/models/promotions/actions/percentage_adjustment_on_full_price_items_spec.rb
@@ -2,24 +2,28 @@
 require 'spec_helper'
 
 describe PercentageAdjustmentOnFullPriceItems do
-  let(:subject) { FactoryGirl.create(:percentage_adjustment) }
   let(:cart) { FactoryGirl.create(:cart_with_1_full_and_1_discount) }
-  describe "#apply" do
-    let(:promo) { mock_model(Promotion) }
-    let(:action_parameter) { mock_model(ActionParameter, param: "0.2") }
-    it "cart item returns price without markdown" do
-      promo.should_receive(:action_parameter).at_least(1).times.and_return(action_parameter)
-      subject.apply(cart, promo.action_parameter.action_params, promo)
-      cart.items.last.adjustment_value.should eq(cart.items.first.price * promo.action_parameter.param.to_d)
+  describe "#calculate_cart_item" do
+    before do
+      @cart_item = cart.items.last
     end
-    it "cart item returns price with markdown" do
-      promo.should_receive(:action_parameter).at_least(1).times.and_return(action_parameter)
-      cart.items.first.product.should_receive(:price).and_return(100.0)
-      cart.items.first.product.should_receive(:retail_price).and_return(100.0)
-      cart.items.last.product.should_receive(:price).and_return(100.0)
-      cart.items.last.product.should_receive(:retail_price).and_return(70.0)
-      subject.apply(cart, promo.action_parameter.action_params, promo)
-      cart.items.first.adjustment_value.should eq(cart.items.first.price * promo.action_parameter.param.to_d)
+    it "returns hash with adjust for markdown product" do
+      @cart_item.should_receive(:price).and_return(BigDecimal("100.0"))
+      expect(subject.calculate_cart_item(@cart_item, '30')).to include({id: @cart_item.id, adjustment: BigDecimal("30.0")})
     end
-  end  
+  end
+  describe "#calculate" do
+    it "return empty array if dont have item with full price" do
+      cart.items.each do |item|
+        item.should_receive(:promotion?).and_return(true)
+      end
+      expect(subject.calculate(cart.items,"30")).to be_empty
+    end
+    it "return array with adjustment if we have item with full price" do
+      cart.items.each do |item|
+        item.should_receive(:promotion?).and_return(false)
+      end
+      expect(subject.calculate(cart.items,"30").size).to eql(2)
+    end
+  end
 end
