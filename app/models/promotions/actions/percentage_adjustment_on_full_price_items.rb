3013546f28a7cdba442c44d3dc13e30b404a011d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-01 10:35:15 -0200

    Remove private method and create another method to better class responsability

--- app/models/promotions/actions/percentage_adjustment_on_full_price_items.rb
+++ app/models/promotions/actions/percentage_adjustment_on_full_price_items.rb
@@ -1,16 +1,18 @@
 # -*- encoding : utf-8 -*-
 class PercentageAdjustmentOnFullPriceItems < PromotionAction
 
-  private
-
     def calculate(cart_items, percent)
       calculated_values = []
       cart_items.each do |cart_item|
-        next if cart_item.product.promotion? 
-        sub_total = cart_item.quantity * cart_item.price
-        adjustment = sub_total * BigDecimal("#{percent.to_i / 100.0}")
-        calculated_values << { id: cart_item.id, adjustment: adjustment }
+        next if cart_item.promotion? 
+        calculated_values << calculate_cart_item(cart_item,percent)
       end
       calculated_values
+    end
+
+    def calculate_cart_item(cart_item,percent)
+      sub_total = cart_item.quantity * cart_item.price
+      adjustment = sub_total * BigDecimal("#{percent.to_i / 100.0}")
+      { id: cart_item.id, adjustment: adjustment }
     end
 end
