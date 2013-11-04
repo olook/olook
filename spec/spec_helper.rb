b934c44c5aff206938ed18c52d32a294956d1a63
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-11-04 14:09:09 -0200

    Start code climate only with env

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 # -*- encoding : utf-8 -*-
 require 'rubygems'
 require 'spork'
-#require "codeclimate-test-reporter"
+require "codeclimate-test-reporter"
 #CODECLIMATE_REPO_TOKEN=f8b9bc0159baef99717bde98ff461e5ecd83f2fb4ce41df305e38709f99aceac
-#CodeClimate::TestReporter.start
+CodeClimate::TestReporter.start if ENV['CODECLIMATE_REPO_TOKEN']
 
 
 module Resque
