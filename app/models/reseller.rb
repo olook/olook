63e845f9dd1ece0001f7d6b4de84a26144b81211
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-05 12:57:50 -0200

    Remove params try on custom validator

--- app/models/reseller.rb
+++ app/models/reseller.rb
@@ -4,7 +4,7 @@ class Reseller < User
   validates :gender, :birthday, presence: true
   validates :has_corporate, :inclusion => { :in => [true, false] }
   validates_presence_of :cnpj, :corporate_name, if: Proc.new { |reseller| reseller.has_corporate == true }
-  validates_with CpfValidator, :attributes => [:cpf,invalid_message: 'bla',duplicate_message: 'po'], :if => Proc.new { |reseller| reseller.has_corporate == false }
+  validates_with CpfValidator, :attributes => [:cpf], :if => Proc.new { |reseller| reseller.has_corporate == false }
   accepts_nested_attributes_for :addresses
   usar_como_cnpj :cnpj
   
