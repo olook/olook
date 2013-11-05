61e1839adbc47ba0d643603b8ec4f385c984c12e
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-05 12:58:11 -0200

    Use locale on custom validator

--- app/validators/cpf_validator.rb
+++ app/validators/cpf_validator.rb
@@ -2,13 +2,13 @@
 class CpfValidator < ActiveModel::Validator
   def validate(record)
     current_cpf = Cpf.new(record.cpf)
-    record.errors.add(:cpf, "é inválido") unless current_cpf.valido?
+    record.errors.add(:cpf, I18n.t("activerecord.errors.models.#{record.class.name.downcase}.attributes.cpf.invalid")) unless current_cpf.valido?
     record.cpf.gsub(/\D/,"") unless record.cpf.blank?
     duplicate_condition = User.where(:cpf => record.cpf)
     duplicate_condition = duplicate_condition.where('id <> :id', :id => record.id) unless record.id.nil?
 
     if duplicate_condition.any?
-      record.errors.add(:cpf, "já está cadastrado")
+      record.errors.add(:cpf, "activerecord.errors.models.#{record.class.name.downcase}.attributes.cpf.blank")
     end
   end
 end
