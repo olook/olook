d28b5771828e94a75c4d75a6144e3b188a3cf586
Rafael <rafael.manoel@gmail.com>
2013-11-06 17:20:23 -0200

    add log for mercadopago notifications

--- app/controllers/mercado_pago_controller.rb
+++ app/controllers/mercado_pago_controller.rb
@@ -2,20 +2,30 @@ class MercadoPagoController < ApplicationController
 
   def create
     begin
+      log("IPN received [id=#{id}]")
+
       response = MP.get_payment_info params[:id]
       mp_transaction = response['response']['collection']
-
+      order_number = mp_transaction['external_reference']
       status = mp_transaction['status']
+
+      log("order_number=#{order_number}, status=#{status}")
+
       if status == 'approved'
-        order_number = mp_transaction['external_reference']
         order = Order.find_by_number order_number
         order.erp_payment.authorize
       end
 
+      log("IPN handled successfully")
       render json: {status: :ok}.to_json
     rescue
+      log("Error handling IPN id=#{id}")
       render json: {status: :bad_request}.to_json
     end
   end
 
+  private
+    def log msg
+      Rails.logger.info("[MERCADOPAGO] #{msg}")
+    end
 end
