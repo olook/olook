6d078c2fc12da42b7a20fb52cb03b5f2d7526d70
darkseid <rafael.manoel@gmail.com>
2013-11-07 22:57:34 -0200

    extract cmp logic to model class

--- app/models/highlight_campaign.rb
+++ app/models/highlight_campaign.rb
@@ -1,4 +1,13 @@
 class HighlightCampaign < ActiveRecord::Base
   attr_accessible :label, :product_ids
   validates :label, presence: true
+
+  def campaign_products
+    if campaign_name.present? && campaign = HighlightCampaign.find_by_label(campaign_name)
+      SearchEngine.new(product_id: campaign.product_ids).with_limit(1000)
+    else
+      []      
+    end
+  end
+
 end
