9c8acbd4f984b8235dbe352e4147a771fba50c89
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-13 18:10:13 -0200

    Wip - liquidations preview info

--- app/models/liquidation_preview.rb
+++ app/models/liquidation_preview.rb
@@ -1,4 +1,24 @@
 class LiquidationPreview < ActiveRecord::Base
   belongs_to :product
   attr_accessible :category, :color, :discount_percentage, :inventory, :name, :picture_url, :price, :retail_price, :subcategory, :visibility, :visible
+
+  def self.import_csv csv_file
+      LiquidationProduct.destroy_all
+      visibility_hash = {}
+      CSV.foreach(csv.path, headers: false){|row| visibility_hash[row.first] = row.last}
+      Product.where(id: visibility_hash.keys).include(:variant).each do |p|
+        LiquidationProduct.create(product: p,
+                                  category: p.category_humanize,
+                                  color: p.product_color,
+                                  discount_percentage: p.discount_percent,
+                                  inventory: p.inventory,
+                                  name: p.name,
+                                  picture_url: p.bag_picture,
+                                  price: p.price,
+                                  retail_price: p.retail_price,
+                                  subcategory: p.subcategory_name,
+                                  visibility: visibility_hash[p.id.to_s],
+                                  visible: p.is_visible)
+      end
+  end
 end
