faf7ce3a05dd9a0d9908d3841b65bab045118799
Luis Daher <luisotaviodaher@gmail.com>
2013-11-13 14:14:09 -0200

    fine tuning

--- app/controllers/olooklet_controller.rb
+++ app/controllers/olooklet_controller.rb
@@ -28,7 +28,7 @@ class OlookletController < ApplicationController
 
   def header
     @header ||= CatalogHeader::CatalogBase.for_url(request.path).first
-    @header ||= CatalogHeader::CatalogBase.for_url("/olooklet").first
+    @header ||= CatalogHeader::CatalogBase.for_url("/olooklet-teste").first
   end
 
   def title_text
