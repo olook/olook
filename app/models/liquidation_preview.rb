f02855d7d02acf012207aa26f71fb774e7c9d209
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-11-13 19:07:53 -0200

    csv imported to liquidation_previews

--- app/models/liquidation_preview.rb
+++ app/models/liquidation_preview.rb
@@ -3,22 +3,26 @@ class LiquidationPreview < ActiveRecord::Base
   attr_accessible :category, :color, :discount_percentage, :inventory, :name, :picture_url, :price, :retail_price, :subcategory, :visibility, :visible
 
   def self.import_csv csv_file
-      LiquidationProduct.destroy_all
-      visibility_hash = {}
-      CSV.foreach(csv.path, headers: false){|row| visibility_hash[row.first] = row.last}
-      Product.where(id: visibility_hash.keys).include(:variant).each do |p|
-        LiquidationProduct.create(product: p,
-                                  category: p.category_humanize,
-                                  color: p.product_color,
-                                  discount_percentage: p.discount_percent,
-                                  inventory: p.inventory,
-                                  name: p.name,
-                                  picture_url: p.bag_picture,
-                                  price: p.price,
-                                  retail_price: p.retail_price,
-                                  subcategory: p.subcategory_name,
-                                  visibility: visibility_hash[p.id.to_s],
-                                  visible: p.is_visible)
-      end
+    LiquidationPreview.destroy_all
+    visibility_hash = {}
+    CSV.foreach(csv_file.path, headers: false){|row| visibility_hash[row.first] = row.last}
+    Product.where(id: visibility_hash.keys).includes(:variants).each do |p|
+      LiquidationPreview.create(product: p,
+                                category: p.category_humanize,
+                                color: p.product_color,
+                                discount_percentage: p.discount_percent,
+                                inventory: p.inventory,
+                                name: p.name,
+                                picture_url: p.bag_picture,
+                                price: p.price,
+                                retail_price: p.retail_price,
+                                subcategory: p.subcategory_name,
+                                visibility: visibility_hash[p.id.to_s],
+                                visible: p.is_visible)
+    end
+  end
+
+  def self.update_visibility_in_products
+    
   end
 end
