545bec4808e668b6622bc13802431914a28ad77d
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-11-22 14:25:14 -0200

    Removing javascript

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -2,7 +2,7 @@ class PricesController < ApplicationController
   def index
     ids = params[:product_ids].is_a?(Array) ? params[:product_ids] : params[:product_ids].to_s.split(/\D/).select {|i| i.present? }
     @products = Product.only_visible.where(id: ids).all
-    @promotion = Promotion.select_promotion_for(@cart)
+    # @promotion = Promotion.select_promotion_for(@cart)
     Product.set_master_variants(@products)
     prices = @products.map do |p|
       if p.discount_price(cart: @cart, coupon: @cart.coupon, promotion: @promotion) != p.price
