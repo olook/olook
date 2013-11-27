292c4103939ee1b8e833a8fd65963ef9d3217a09
Luis Daher <luisotaviodaher@gmail.com>
2013-11-27 14:51:39 -0200

    loyalty program credits working for products

--- app/assets/javascripts/lite_base.js
+++ app/assets/javascripts/lite_base.js
@@ -243,3 +243,50 @@ olook.showEmailBar = function(){
 		o.registerEmail();
 	}
 }
+
+
+olook.newModal = function(content){
+  var $modal = $("div#modal.promo-olook"),
+  h = $(content).outerHeight(),
+  w = $(content).outerWidth(),
+  ml = -parseInt((w/2)),
+  mt = -parseInt((h/2)),
+  heightDoc = $(document).height(),
+  _top = Math.max(0, (($(window).height() - h) / 2) + $(window).scrollTop()),
+  _left=Math.max(0, (($(window).width() - w) / 2) + $(window).scrollLeft());
+
+  $("#overlay-campaign").css({"background-color": "#000", 'height' : heightDoc}).fadeIn().bind("click", function(){
+   _iframe = $modal.contents().find("iframe") || null;
+
+    if (_iframe.length > 0){
+      $(_iframe).remove();
+    }
+
+     $("#modal").hide();
+     $(this).fadeOut();
+  });
+
+  $modal.html(content)
+  .css({
+     'height'      : h,
+     'width'       : w,
+     'top'         : _top,
+     'left'        : _left,
+     /*'margin-left' : ml,
+     'margin-top'  : mt*/
+  })
+ .append('<button type="button" class="close" role="button">close</button>')
+ .delay(500).fadeIn().children().fadeIn();
+
+ $("#modal button.close, #modal a.me").click(function(){
+   _iframe = $modal.contents().find("iframe") || null;
+
+   if (_iframe.length > 0){
+     $(_iframe).remove();
+   }
+
+    $("#modal").hide();
+    $("#overlay-campaign").fadeOut();
+ })
+
+}
--- app/assets/javascripts/partials/_credits_info.js
+++ app/assets/javascripts/partials/_credits_info.js
@@ -2,7 +2,7 @@ var showInfoCredits = function() {
   $("a.open_loyalty_lightbox").live('click', function(e) {
     _gaq.push(['_trackEvent', 'product_show', 'show_loyalty_info', '']);
     content = $("div.credits_description");
-    initBase.newModal(content);
+    olook.newModal(content);
     e.preventDefault();
   });
 };
