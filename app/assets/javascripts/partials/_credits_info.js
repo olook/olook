fbca56f5bf1b0577135be7327fa7a45019035a2a
Luis Daher <luisotaviodaher@gmail.com>
2013-12-02 16:15:11 -0200

    product js working using lite-base

--- app/assets/javascripts/partials/_credits_info.js
+++ app/assets/javascripts/partials/_credits_info.js
@@ -2,7 +2,7 @@ var showInfoCredits = function() {
   $("a.open_loyalty_lightbox").live('click', function(e) {
     _gaq.push(['_trackEvent', 'product_show', 'show_loyalty_info', '']);
     content = $("div.credits_description");
-    olook.newModal(content);
+    modal.show(content);
     e.preventDefault();
   });
 };
