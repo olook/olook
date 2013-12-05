f7961038402c2ab5aa3a2260d84913ef6e192a9b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2013-12-05 19:19:46 -0200

    Adding some methods to use on views

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 class Freebie
   def initialize(attrs={})
     @subtotal = attrs[:subtotal]
@@ -7,4 +8,14 @@ class Freebie
     @subtotal.to_f > 200
   end
 
+  def what_can_i_do_to_receive_freebie
+    miss = 200.0 - @subtotal.to_f
+    "Falta R$ #{miss.to_i},#{( (miss - miss.to_i) * 100 ).round} para conseguir o brinde"
+  end
+
+  class << self
+    def product_is_freebie?(product_id)
+      product_id.to_i == 23035
+    end
+  end
 end
