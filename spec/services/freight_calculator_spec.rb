06e4df6d09686c86915318d9a889427febb89cb9
Tiago Almeida <tig.luiz.almeida@gmail.com.br>
2013-12-05 14:28:16 -0200

    Fix old tests and add new one to new way of return freight

--- spec/services/freight_calculator_spec.rb
+++ spec/services/freight_calculator_spec.rb
@@ -7,8 +7,8 @@ describe FreightCalculator do
       let(:zip_code) { '05379020' }
 
       context "for which there isn't a freight price" do
-        it "should return a hash with price, delivery time and cost equal to zero" do
-          freight = described_class.freight_for_zip(zip_code, 0)
+        it "returns a array of hashs with price, delivery time and cost equal to zero" do
+          freight = described_class.freight_for_zip(zip_code, 0).first
           expect(freight[:price]).to  eql(FreightCalculator::DEFAULT_FREIGHT_PRICE)
           expect(freight[:cost]).to eql(FreightCalculator::DEFAULT_FREIGHT_COST)
           expect(freight[:delivery_time]).to eql(FreightCalculator::DEFAULT_INVENTORY_TIME)
@@ -19,32 +19,52 @@ describe FreightCalculator do
       context 'for which there is a freight price' do
         let(:order_value) { 70.1 }
 
-        let(:freight_details) { {:price => 10.0, :cost => 5.0, :delivery_time => 2 + FreightCalculator::DEFAULT_INVENTORY_TIME, :shipping_service_id => shipping_service.id} }
-        let(:wrong_shipping_service) { FactoryGirl.create :shipping_service, :priority => 1 }
-        let(:shipping_service) { FactoryGirl.create :shipping_service, :priority => 99 }
-        
+        let(:freight_details1) { {:price => BigDecimal.new("10.0"), :cost => BigDecimal.new("5.0"), :delivery_time => 2 + FreightCalculator::DEFAULT_INVENTORY_TIME, :shipping_service_id => shipping_service1.id, shipping_service_priority: shipping_service1.priority } }
+        let(:freight_details2) { {:price => BigDecimal.new("5.0"), :cost => BigDecimal.new("3.0"), :delivery_time => 30 + FreightCalculator::DEFAULT_INVENTORY_TIME, :shipping_service_id => shipping_service2.id, shipping_service_priority: shipping_service2.priority } }
+        let(:shipping_service2) { FactoryGirl.create :shipping_service, :priority => 1 }
+        let(:shipping_service1) { FactoryGirl.create :shipping_service, :priority => 99 }
+
         before :each do
-          FactoryGirl.create  :freight_price, :shipping_service => wrong_shipping_service,
+          FactoryGirl.create  :freight_price, :shipping_service => shipping_service2,
                               :zip_start => 5379000, :zip_end => 5379014,
                               :order_value_start => 2.5, :order_value_end => 4.5,
                               :price => 310.0, :cost => 35.0, :delivery_time => 30
 
-          FactoryGirl.create  :freight_price, :shipping_service => shipping_service,
+          FactoryGirl.create  :freight_price, :shipping_service => shipping_service2,
+                              :zip_start => 5379016, :zip_end => 5379100,
+                              :order_value_start => 69.00, :order_value_end => 128.00,
+                              :price => 5.0, :cost => 3.0, :delivery_time => 30
+
+          FactoryGirl.create  :freight_price, :shipping_service => shipping_service1,
                               :zip_start => 5379016, :zip_end => 5379100,
                               :order_value_start => 0.0, :order_value_end => 68.99,
                               :price => 7.0, :cost => 3.0, :delivery_time => 1
-          FactoryGirl.create  :freight_price, :shipping_service => shipping_service,
+          FactoryGirl.create  :freight_price, :shipping_service => shipping_service1,
                               :zip_start => 5379016, :zip_end => 5379100,
                               :order_value_start => 69.00, :order_value_end => 128.99,
                               :price => 10.0, :cost => 5.0, :delivery_time => 2
-          FactoryGirl.create  :freight_price, :shipping_service => shipping_service,
+          FactoryGirl.create  :freight_price, :shipping_service => shipping_service1,
                               :zip_start => 5379016, :zip_end => 5379100,
                               :order_value_start => 129.0, :order_value_end => 1000.0,
                               :price => 20.0, :cost => 15.0, :delivery_time => 3
         end
-        
-        it "should return a hash with price, delivery time and cost" do
-          expect(described_class.freight_for_zip(zip_code, order_value)).to eql(freight_details)
+        context "with two shipping services" do 
+          it "should return a hash with price, delivery time and cost" do
+            expect(described_class.freight_for_zip(zip_code, order_value)).to eql([freight_details2,freight_details1])
+          end
+        end
+        context "with tree shipping services" do 
+          let(:shipping_service3) { FactoryGirl.create :shipping_service, :priority => 10 }
+          let(:freight_details3) { {:price => BigDecimal.new("5.0"), :cost => BigDecimal.new("3.0"), :delivery_time => 30 + FreightCalculator::DEFAULT_INVENTORY_TIME, :shipping_service_id => shipping_service3.id, shipping_service_priority: shipping_service3.priority } }
+          before do
+            FactoryGirl.create  :freight_price, :shipping_service => shipping_service3,
+                                :zip_start => 5379016, :zip_end => 5379100,
+                                :order_value_start => 69.00, :order_value_end => 128.00,
+                                :price => 5.0, :cost => 3.0, :delivery_time => 30
+          end
+          it "should return a hash with price, delivery time and cost" do
+            expect(described_class.freight_for_zip(zip_code, order_value)).to eql([freight_details2,freight_details3])
+          end
         end
       end
     end
