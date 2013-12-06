0e082afa3b03cf09fe2e1114f218c1aa07b24c1d
Tiago Almeida <tig.luiz.almeida@gmail.com.br>
2013-12-06 15:56:51 -0200

    Ensure to use first shipping service

--- app/helpers/checkout/checkout_helper.rb
+++ app/helpers/checkout/checkout_helper.rb
@@ -15,7 +15,7 @@ module Checkout::CheckoutHelper
   end
 
   def freight_for(address)
-    FreightCalculator.freight_for_zip(address.zip_code, @cart_service.subtotal)
+    FreightCalculator.freight_for_zip(address.zip_code, @cart_service.subtotal).first
   end
 
   def total_with_freight(freight_value, payment=nil)
