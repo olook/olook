f083614794d9e41071524928f0242a6a2a44fddc
Tiago Almeida <tig.luiz.almeida@gmail.com.br>
2013-12-11 14:48:30 -0200

    Apply freight count to get free

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -7,7 +7,6 @@ class ShippingsController < ApplicationController
   def show
 
     zip_code = params[:id]
-
     freights =  FreightCalculator.freight_for_zip(
       zip_code,
       @cart_service.subtotal > 0 ? @cart_service.subtotal : DEFAULT_VALUE,
@@ -20,7 +19,7 @@ class ShippingsController < ApplicationController
     @has_two_shipping_services = freights.count > 1
     @days_to_deliver = freights.first[:delivery_time]
     @freight_price = freights.first[:price]
-    #@first_free_freight_price = freight[:first_free_freight_price]  if freight[:first_free_freight_price]
+    @first_free_freight_price = freights.first[:cost_for_free]  if freights.first[:cost_for_free]
   end
 
 end
