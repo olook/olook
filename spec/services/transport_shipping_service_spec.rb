406289f5518df36cfde099bc68d69f61d9180923
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-12-16 09:27:01 -0200

    Add test for ensure same transport initialize choose_better_transport_shipping method resut

--- spec/services/transport_shipping_service_spec.rb
+++ spec/services/transport_shipping_service_spec.rb
@@ -1,7 +1,7 @@
 describe TransportShippingService do
 
   before do
-    defaut_transport_service = {
+    @defaut_transport_service = {
       :price => 0.0,
       :cost => 0.0,
       :delivery_time => 2,
@@ -9,10 +9,16 @@ describe TransportShippingService do
       :shipping_service_priority => "1",
       :cost_for_free => ''
     }
-    @transport_service = TransportShippingService.new([defaut_transport_service])
+    @transport_service = TransportShippingService.new([@defaut_transport_service])
   end
 
-  it "return one shipping transport if array have one transport service" do
-    expect(@transport_service.choose_better_transport_shipping.count).to eql 1
+  context "when have one transport shipping" do
+    it "return one shipping transport" do
+      expect(@transport_service.choose_better_transport_shipping.count).to eql 1
+    end
+
+    it "return same transport shipping info that was initialize" do
+      expect(@transport_service.choose_better_transport_shipping).to eql [@defaut_transport_service]
+    end
   end
 end
