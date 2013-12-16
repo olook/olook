b29c3cebc4b63924ad1fdc02d8cce8786b3334b1
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-12-16 10:40:14 -0200

    Add class to choose better transport shipping

--- app/services/transport_shipping_service.rb
+++ app/services/transport_shipping_service.rb
@@ -1,9 +1,33 @@
 class TransportShippingService
+  DEFAULT_DELIVERY_TIME_FACTOR = 0.2
+  DEFAULT_PRICE_FACTOR = 0.3
+  attr_reader :transport_shippins
+  attr_accessor :return_shippings
+
   def initialize transport_shippins
     @transport_shippins = transport_shippins
+    @return_shippings = []
   end
 
   def choose_better_transport_shipping
-    @transport_shippins
+    return transport_shippins if transport_shippins.count == 1
+    sort_shipping_services = order_by_cost
+    better_shipping = sort_shipping_services.delete_at(0)
+    return_shippings << better_shipping
+    sort_shipping_services.each do |shipping|
+      return_shippings << shipping if delivery_time_calculation(shipping[:delivery_time],better_shipping[:delivery_time]) && price_calculation(shipping[:price],better_shipping[:price])
+    end
+    return_shippings.first(2)
   end
+
+  private
+    def order_by_cost
+      transport_shippins.sort{|x,y| x[:cost] <=> y[:cost]}
+    end
+    def delivery_time_calculation delivery_time, control_delivery_time
+      delivery_time <= (control_delivery_time - (control_delivery_time * DEFAULT_DELIVERY_TIME_FACTOR))
+    end
+    def price_calculation price, price_control
+      price >= (price_control + (price_control * DEFAULT_PRICE_FACTOR))
+    end
 end
--- spec/services/transport_shipping_service_spec.rb
+++ spec/services/transport_shipping_service_spec.rb
@@ -9,16 +9,55 @@ describe TransportShippingService do
       :shipping_service_priority => "1",
       :cost_for_free => ''
     }
-    @transport_service = TransportShippingService.new([@defaut_transport_service])
   end
 
   context "when have one transport shipping" do
+    before do
+      @transport_service = TransportShippingService.new([@defaut_transport_service])
+    end
     it "return one shipping transport" do
       expect(@transport_service.choose_better_transport_shipping.count).to eql 1
     end
 
     it "return same transport shipping info that was initialize" do
       expect(@transport_service.choose_better_transport_shipping).to eql [@defaut_transport_service]
+    end
+  end
+
+  context "when have more than one transport shipping" do
+    before do
+      @transport_service_1 = {
+        :price => 5.0,
+        :cost => 1.0,
+        :delivery_time => 1,
+        :shipping_service_id => 4,
+        :shipping_service_priority => "2",
+        :cost_for_free => ''
+      }
+    end
+    context "and have two possible choices" do
+      before do
+        @transport_service = TransportShippingService.new([@defaut_transport_service, @transport_service_1])
+      end
+      it "return two transport shipping ordering with cost" do
+        expect(@transport_service.choose_better_transport_shipping).to eql [@defaut_transport_service, @transport_service_1]
+      end
+    end
+    context "and have tree possible choices" do
+      before do
+        @transport_service_2 = {
+          :price => 5.0,
+          :cost => 4.0,
+          :delivery_time => 1,
+          :shipping_service_id => 4,
+          :shipping_service_priority => "3",
+          :cost_for_free => ''
+        }
+        @transport_service = TransportShippingService.new([@defaut_transport_service, @transport_service_1,@transport_service_2])
+      end
+      it "return two transport shipping ordering with cost" do
+        expect(@transport_service.choose_better_transport_shipping).to eql [@defaut_transport_service, @transport_service_1]
+      end
     end
   end
 end
