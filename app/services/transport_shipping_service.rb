0fd7d0d5b2b462edfef9163f2bcf0da92d44c74c
Tiago Almeida <tig.luiz.almeida@gmail.com>
2013-12-16 09:23:01 -0200

    Add inicial test for one transport shipping

--- app/services/transport_shipping_service.rb
+++ app/services/transport_shipping_service.rb
@@ -2,4 +2,8 @@ class TransportShippingService
   def initialize transport_shippins
     @transport_shippins = transport_shippins
   end
+
+  def choose_better_transport_shipping
+    @transport_shippins
+  end
 end
