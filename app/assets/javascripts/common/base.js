7938f301ee3c2e617a7b0a4a6848f2033c79d423
Luis Daher <luisotaviodaher@gmail.com>
2013-12-19 16:00:36 -0200

    fixing telephone masks

--- app/assets/javascripts/common/base.js
+++ app/assets/javascripts/common/base.js
@@ -251,13 +251,16 @@ $(document).ready(function() {
 
   // For now both fone field will acept nine digits
   function maskTel(tel){
-  	dig9 = $(tel).val().substring(4, 5);
+    dig9 = $(tel).val().substring(4, 5);
+    ddd  = $(tel).val().substring(1, 3);
 
-  	if(dig9 == "9")
-  		$(tel).setMask("(99)99999-9999");
-    else
-     	$(tel).setMask("(99)9999-9999");
+    if(dig9 == "9" && ddd.match(/11|12|13|14|15|16|17|18|19|21|22|24|27|28/)){
+      $(tel).setMask("(99)99999-9999");
+    } else {
+      $(tel).setMask("(99)9999-9999");
+    }
   }
+  
   $("input:text.phone").keyup(function(){
 		maskTel(this);
 	});
