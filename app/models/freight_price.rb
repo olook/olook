cbc4338108ed7a4ce212d597fc3a0711db5cf7ed
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-08 16:37:54 -0200

    Create scope to freight price

--- app/models/freight_price.rb
+++ app/models/freight_price.rb
@@ -1,6 +1,8 @@
 # -*- encoding : utf-8 -*-
 class FreightPrice < ActiveRecord::Base
   belongs_to :shipping_service
+
+  scope :with_zip_and_value, ->(zip,value) {where('(:zip >= zip_start) AND (:zip <= zip_end) AND (:order_value >= order_value_start) AND (:order_value <= order_value_end)', :zip => zip,:order_value => value).group("shipping_service_id")}
   
   ZIP_VALIDATION      = { :presence => true,
                           :numericality => { :only_integer => true , :greater_than_or_equal_to => 0, :less_than_or_equal_to => 99999999 }
