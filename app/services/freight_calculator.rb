6b2d3d6b1a45600a6867f993f48a1d7680cd3659
Rafael <rafael.manoel@gmail.com>
2014-01-09 17:53:26 -0200

    remove unecessary query

--- app/services/freight_calculator.rb
+++ app/services/freight_calculator.rb
@@ -3,11 +3,19 @@ module FreightCalculator
   VALID_ZIP_FORMAT = /\A(\d{8})\z/
   VALID_SHIPPING_SERVICES_ID_LIST = /^(\d+,?)*$/
 
-
   DEFAULT_FREIGHT_PRICE   = 0.0
   DEFAULT_FREIGHT_COST    = 0.0
   DEFAULT_INVENTORY_TIME  = 10
   DEFAULT_FREIGHT_SERVICE = 2 # CORREIOS
+
+  DEFAULT_FREIGHT = {
+    default_shipping: {
+      price: DEFAULT_FREIGHT_PRICE, 
+      cost: DEFAULT_FREIGHT_COST,
+      delivery_time: DEFAULT_INVENTORY_TIME,
+      shipping_service_id: DEFAULT_FREIGHT_SERVICE
+    }
+  }
 
 
   def self.freight_for_zip(zip_code, order_value, shipping_service_ids = nil, use_message = false)
@@ -16,7 +24,7 @@ module FreightCalculator
     return_array = []
     shipping_service_ids = ShippingService.all.map(&:id) if shipping_service_ids.blank?
     freight_prices = FreightPrice.with_zip_and_value(clean_zip_code,order_value, shipping_service_ids)
-    return {default_shipping: {price: DEFAULT_FREIGHT_PRICE, cost: DEFAULT_FREIGHT_COST,delivery_time: DEFAULT_INVENTORY_TIME,shipping_service_id: DEFAULT_FREIGHT_SERVICE}} if freight_prices.blank?
+    return DEFAULT_FREIGHT if freight_prices.blank?
     freight_prices.compact.each do |freight|
       return_array << {
       :price => freight.try(:price) || DEFAULT_FREIGHT_PRICE,
