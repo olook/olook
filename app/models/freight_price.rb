e03f21ea61eca3585e472fe7881fb731365aac23
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-09 11:40:41 -0200

    Add freigh price scopes

--- app/models/freight_price.rb
+++ app/models/freight_price.rb
@@ -2,7 +2,8 @@
 class FreightPrice < ActiveRecord::Base
   belongs_to :shipping_service
 
-  scope :with_zip_and_value, ->(zip,value) {where('(:zip >= zip_start) AND (:zip <= zip_end) AND (:order_value >= order_value_start) AND (:order_value <= order_value_end)', :zip => zip,:order_value => value).group("shipping_service_id")}
+  scope :with_zip_and_value, ->(zip,value,shipping_ids) {where('(:zip >= zip_start) AND (:zip <= zip_end) AND (:order_value >= order_value_start) AND (:order_value <= order_value_end)', :zip => zip,:order_value => value).where(shipping_service_id: shipping_ids)}
+  scope :free_cost, ->(zip,value,shipping_id) {where(':zip >= zip_start AND :zip <= zip_end AND :order_value < order_value_start', :zip => zip,:order_value => value).where(price: "0").where(shipping_service_id: shipping_id)}
   
   ZIP_VALIDATION      = { :presence => true,
                           :numericality => { :only_integer => true , :greater_than_or_equal_to => 0, :less_than_or_equal_to => 99999999 }
