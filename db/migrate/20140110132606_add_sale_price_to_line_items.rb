745a0f339087c2148730ac5913bdd7ec12af1642
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-10 13:58:23 -0200

    fix decimal

--- db/migrate/20140110132606_add_sale_price_to_line_items.rb
+++ db/migrate/20140110132606_add_sale_price_to_line_items.rb
@@ -1,5 +1,5 @@
 class AddSalePriceToLineItems < ActiveRecord::Migration
   def change
-    add_column :line_items, :sale_price, :decimal, :default => 0.0
+    add_column :line_items, :sale_price, :decimal, precision: 10, scale: 2, default: 0
   end
 end
