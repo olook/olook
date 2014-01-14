27211289ea1498c9a4ad8b88ad2dea2fa28afbb5
Luis Daher <luisotaviodaher@gmail.com>
2014-01-14 18:48:48 -0200

    major refactoring and unit tests

--- app/assets/javascripts/application_core/olook_app.js
+++ app/assets/javascripts/application_core/olook_app.js
@@ -2,12 +2,30 @@
  * Alternativamente, podemos tambem criar metodos que tem como objetivo desacoplar o codigo de bibliotecas de manipulacao de
  * DOM e chamadas AJAX (ex.: jQuery)
  */
-function OlookApp(mediator) {
-  this.mediator = mediator;
+function OlookApp(_mediator) {
+  var mediator = _mediator;
+
+  shift = function(list) {
+    return Array.prototype.slice.call(list, 1);
+  }
+
+  return{
+    mediator: mediator,
+    publish: function(){
+      if (arguments.length == 0)
+        throw "topic name is required";
+      args = shift(arguments);
+      mediator.publish(arguments[0], args);
+    },
+    subscribe: function(channel){
+      mediator.subscribe(channel.name, channel.facade);
+    }
+
+  }
 };
 
 var olookApp = null;
 
 $(function(){
-  olookApp = new OlookApp(new Mediator());
+  olookApp = olookApp || new OlookApp(new Mediator());
 });
--- app/assets/javascripts/modules/complete_look/minicart_fade_out_manager.js
+++ app/assets/javascripts/modules/complete_look/minicart_fade_out_manager.js
@@ -1,26 +1,29 @@
-function MinicartFadeOutManager(){
-  
-  
-  function hasToFadeOut(variantNumber){
-    return ((StringUtils.isEmpty($("#total_price").val()) || ($( ".cart_related ul li").length -1 == 0)) && !StringUtils.isEmpty(variantNumber));
-  }
+MinicartFadeOutManager = function(){
 
-  function fadeOut(){
+
+  var hasToFadeOut = function(variantNumber){
+    return ((StringUtils.isEmpty($("#total_price").val()) || ($( ".cart_related ul li").length -1 == 0)) && !StringUtils.isEmpty(variantNumber));
+  };
+
+  var fadeOut = function(){
     $("#total_price").val('0.0');
 
     $('.cart_related').fadeOut("fast",function(){
       $('.cart_related').addClass('product_added');
       $('.cart_related div.empty_minicart').hide();    
     });
-  }
+  };
 
-  this.facade = function(variantNumber){
-    if(hasToFadeOut(variantNumber)){
-      fadeOut();
+  return {
+    name: "FADE_OUT_MINICART",
+    facade: function(variantNumber){
+      if(hasToFadeOut(variantNumber)){
+        fadeOut();
+      }
     }
-  }
-}
+  };
+}();
 
 $(function(){
-  olookApp.mediator.subscribe("fadeOutMinicart", new MinicartFadeOutManager().facade); 
+  olookApp.subscribe(MinicartFadeOutManager); 
 });
