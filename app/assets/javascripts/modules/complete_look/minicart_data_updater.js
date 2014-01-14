ab0e4d79f738fe45e39a78613e13dfd35f5a20ec
Luis Daher <luisotaviodaher@gmail.com>
2014-01-14 18:48:48 -0200

    major refactoring and unit tests

--- app/assets/javascripts/modules/complete_look/minicart_data_updater.js
+++ app/assets/javascripts/modules/complete_look/minicart_data_updater.js
@@ -1,5 +1,5 @@
-function MinicartDataUpdater(){
-  function writePrice(){
+MinicartDataUpdater = function(){
+  var writePrice = function(){
     var installments = CreditCard.installmentsNumberFor($("#total_price").val());
 
     if ($("li.product").length == ($(".cart_related ul li").length - 1)){
@@ -7,48 +7,52 @@ function MinicartDataUpdater(){
     } else if(($(".cart_related ul li").length - 1) > 0){
       $(".cart_related .minicart_price").html("Por<span class='first_price'>" + installments + "x de " + Formatter.toCurrency( $("#total_price").val() / installments ) + " sem juros </span>");
     }
-  }
+  };
 
-  function productNameInMinicart(productId){
+  var productNameInMinicart = function(productId){
     return($( ".cart_related ul li:contains("+$('li[data-id='+productId+'] img').attr("alt")+")" ));
-  }
+  };
 
-  function addItem(productId, productPrice){
+  var addItem = function(productId, productPrice){
     $('.cart_related ul').append("<li>"+$('li[data-id='+productId+'] img').attr("alt")+"</li>");
     $("#total_price").val( parseFloat($("#total_price").val()) + parseFloat(productPrice));
-  }
+  };
 
-  function removeItem(productId, productPrice){
+  var removeItem = function(productId, productPrice){
     productNameInMinicart(productId).remove();
     if(isCartEmpty()){
       $("#total_price").val('');
     } else {
       $("#total_price").val( parseFloat($("#total_price").val()) - parseFloat(productPrice));
     }    
-  }
+  };
 
-  function isAddition(productId, variantNumber){
+  var isAddition = function(productId, variantNumber){
     return (productNameInMinicart(productId).length == 0 && variantNumber.length > 0);
-  }
+  };
 
-  function isRemoval(productId, variantNumber){
+  var isRemoval = function(productId, variantNumber){
     return (productNameInMinicart(productId).length > 0 && variantNumber.length == 0);
-  }
+  };
 
-  function isCartEmpty(){
+  var isCartEmpty = function(){
     return ($( ".cart_related ul li").length - 1 == 0);
-  }
+  };
 
-  this.facade = function(productId, productPrice, variantNumber){
-    if (isAddition(productId, variantNumber)){
-      addItem(productId, productPrice);
-    } else if (isRemoval(productId, variantNumber)){
-      removeItem(productId, productPrice);
+  return{
+    name: "UPDATE_MINICART_DATA",
+    facade: function(productId, productPrice, variantNumber){
+      if (isAddition(productId, variantNumber)){
+        addItem(productId, productPrice);
+      } else if (isRemoval(productId, variantNumber)){
+        removeItem(productId, productPrice);
+      }
+      writePrice();
     }
-    writePrice();
-  }
-}
+  };
+
+}();
 
 $(function(){
-  olookApp.mediator.subscribe("updateMinicartData", new MinicartDataUpdater().facade);
+  olookApp.subscribe(MinicartDataUpdater);
 });
