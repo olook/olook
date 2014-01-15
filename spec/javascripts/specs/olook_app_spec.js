0cde6239d2ab4afc6abcdb9d555845c62a31b7bd
Luis Daher <luisotaviodaher@gmail.com>
2014-01-15 14:37:39 -0200

    jasmine tests created for OlookApp

--- spec/javascripts/specs/olook_app_spec.js
+++ spec/javascripts/specs/olook_app_spec.js
@@ -1,47 +1,54 @@
 describe("Olook APP", function() {
-  
-  function DummyMediator() {
-    var _topicName;
-    var _parameters;
-    return {
-      publish: function(topicName, parameters) {
-        _topicName = topicName
-        _parameters = parameters
-      },
-      topicName: function() {return _topicName},
-      parameters: function() {return _parameters}
-    }
-  }
 
-  app = new OlookApp(new DummyMediator());
+  var app;
+  var mockMediator;
+
+  beforeEach(function() {
+    mockMediator = jasmine.createSpyObj('Mediator', ['publish', 'subscribe']);
+    app = new OlookApp(mockMediator);
+  });
 
 
   describe("#publish", function() {
 
     it("fails if there is no arguments", function() {
-      expect(app.publish).toThrow();
+      expect(app.publish).toThrow("channel name is required");
     });
 
     it("accept one argument", function() {
       app.publish('topic name');
-      expect(app.mediator.topicName()).toEqual('topic name');
+      expect(mockMediator.publish).toHaveBeenCalledWith('topic name', []);
     });
 
     it("accept two or more arguments", function() {
       app.publish('topic name', 'param1', 'param2');
-
-      expect(app.mediator.topicName()).toEqual('topic name');
-      expect(app.mediator.parameters()).toEqual(['param1', 'param2']);
+      expect(mockMediator.publish).toHaveBeenCalledWith('topic name', ['param1', 'param2']);
     });
         
   });
 
   describe("#subscribe", function() {
-    it("Tarefa pro luis", function() {
-      expect(false).toBeTruthy();
+    var nullChannel = null;
+    var nullNameChannel = {name: null};
+    var nullFacadeMethodChannel = {name: "a", facade: null};
+    var channel = {name: "channelName", facade: "channelFacade"};
+
+    it("returns an exception when channel is null", function() {
+      expect(app.subscribe.bind(null, nullChannel)).toThrow("channel is required");
     });
+
+    it("returns an exception when channel name is null", function() {
+      expect(app.subscribe.bind(null, nullNameChannel)).toThrow("channel name is required");
+    });
+
+    it("returns an exception when channel facade method is null", function() {
+      expect(app.subscribe.bind(null, nullFacadeMethodChannel)).toThrow("channel facade method is required");
+    });
+
+    it("calls the mediator subscribe method when the channel is being passed properly", function() {
+      app.subscribe(channel);
+      expect(mockMediator.subscribe).toHaveBeenCalledWith(channel.name, channel.facade);
+    });      
   });
-
-
 
 });
