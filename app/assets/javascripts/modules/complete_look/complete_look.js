822ff97877e881cec308e8e397fa32478cf3cce2
Rafael <rafael.manoel@gmail.com>
2014-01-15 10:59:26 -0200

    remove create.js.erb and move the logic to complete_look.js

---
+++ app/assets/javascripts/modules/complete_look/complete_look.js
@@ -0,0 +1,45 @@
+var completeLook = function(){
+
+  addLookItemToCart = function(actionUrl, values) {
+    $.post(actionUrl, values, function( data ) {
+
+      var variantNumber = data['variant_number'];
+      var productPrice = data['product_price'];
+      var productId = data['product_id'];
+
+      var variantNumbers = data['variant_numbers'];
+
+      olookApp.mediator.publish(MinicartFadeOutManager.name, variantNumber);
+      olookApp.mediator.publish(MinicartInputsUpdater.name,variantNumbers, variantNumber, "<input type='hidden' name='variant_numbers[]' value='"+variantNumber+"'>");
+
+      setTimeout(function() {
+        olookApp.mediator.publish(MinicartDataUpdater.name, productId, productPrice, variantNumber);  
+        olookApp.mediator.publish(MinicartBoxDisplayUpdater.name);
+        olookApp.publish(MinicartFadeInManager.name);  
+      },300);    
+    });
+  }
+
+  bindEvents = function(){
+    $('li.product form #variant_number').change(function(e){
+      var element = $(this);
+      var actionUrl = element.parent().attr('action');
+
+      values = {
+        'product_id': element.data('product-id'),
+        'variant_number': element.val()
+      }
+      
+      addLookItemToCart(actionUrl, values);
+    });
+  }
+
+  return {
+    bindEvents: bindEvents
+  }
+  
+}();
+
+$(function() {
+  completeLook.bindEvents();
+})
