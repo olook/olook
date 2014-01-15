19d05369f237dd5a4dc51b311726dfcea3417a9b
Rafael <rafael.manoel@gmail.com>
2014-01-15 10:59:26 -0200

    remove create.js.erb and move the logic to complete_look.js

--- app/controllers/cart/look_items_controller.rb
+++ app/controllers/cart/look_items_controller.rb
@@ -1,6 +1,14 @@
 class Cart::LookItemsController < ApplicationController
-  respond_to :js
+  respond_to :json
   def create
-    @variant_numbers = Product.find(params[:product_id]).variants.map{|v| v.number}
+    product = Product.find(params[:product_id])
+    @variant_numbers = product.variants.map{|v| v.number}
+
+    render json: {
+      product_id: product.id,
+      product_price: product.retail_price,
+      variant_number: params[:variant_number],
+      variant_numbers: @variant_numbers,
+    }
   end
 end
