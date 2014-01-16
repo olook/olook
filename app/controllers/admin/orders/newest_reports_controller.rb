1109f15f3b838f19970697842fcd0ac8dc53d784
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-16 18:06:39 -0200

    Refactor scope

--- app/controllers/admin/orders/newest_reports_controller.rb
+++ app/controllers/admin/orders/newest_reports_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Admin::Orders::NewestReportsController < Admin::BaseController
   def index
-    @orders = Order.with_first_buyers(Time.zone.now)
+    @orders = Order.with_first_buyers
   end
 end
