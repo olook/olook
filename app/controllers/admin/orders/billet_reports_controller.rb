ea3f191bcc1f9cb641673e867298af13f940d21e
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-16 15:16:06 -0200

    Fix search time on billets

--- app/controllers/admin/orders/billet_reports_controller.rb
+++ app/controllers/admin/orders/billet_reports_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Admin::Orders::BilletReportsController < Admin::BaseController
   def index
-    @billets = Billet.expire_at(Time.zone.now - 1.days, Time.zone.now)
+    @billets = Billet.expire_at( Time.zone.now, 1.business_day.from_now)
   end
 end
