ce2dca1cf1e85bc3607d7d655b6e53e0afc6aaab
Luis Daher <luisotaviodaher@gmail.com>
2014-01-16 16:54:05 -0200

    MinicartBoxDisplayUpdater tests done

--- spec/javascripts/specs/modules/minicart_box_display_updater_spec.js
+++ spec/javascripts/specs/modules/minicart_box_display_updater_spec.js
@@ -8,25 +8,40 @@ describe("MinicartBoxDisplayUpdater", function() {
 
   describe("#facade", function() {
     describe("when the cart is empty", function() {
-      it("must show the empty cart box",function(){});
-      it("must disable the submit button",function(){});
+      it("must show the empty cart box",function(){
+        setFixtures("<div class='empty_minicart' style='display:none;'></div>");
+        MinicartBoxDisplayUpdater.facade();
+        JasmineAjaxHelper.expectCartDisplayProperty($('.empty_minicart'),"display","block", "the empty minicart didn't fade in");
+      });
+
+      it("must not indicate that there are products inside the cart",function(){
+        setFixtures("<div class='cart_related product_added'></div>");
+        MinicartBoxDisplayUpdater.facade();
+        expect($('.product_added').length).toEqual(0);
+      });
+
+      it("erases the minicart price",function(){
+        setFixtures("<div class='minicart_price'>fsaddfsaafdsdfsa</div>");
+        MinicartBoxDisplayUpdater.facade();
+        expect($('.minicart_price').html()).toEqual("");        
+      });
+
+      it("must disable the submit button",function(){
+        setFixtures("<div class='js-minicartItem'></div><div class='js-addToCartButton disabled' disabled='disabled'></div>");
+        MinicartBoxDisplayUpdater.facade();
+        expect($('.js-addToCartButton').attr("disabled")).toEqual(undefined);
+        expect($('.disabled').length).toEqual(0);        
+      });
     });
 
     describe("when the cart isn't empty", function() {
-      it("must enable the submit button",function(){});
-    });    
-
-    it("fades out when it has to fade out", function(){
-      setFixtures("<input id='total_price' name='total_price' /><div class='cart_related' style='display:block;'><ul><li></li></ul></div>");
-      MinicartFadeOutManager.facade(123);
-       // Wait for fadeOut event finished
-      waitsFor(function() {
-      return ($('.cart_related').css("display") == "none");
-      }, "The element won't ever be hidden", 3000);
-      runs(function(){
-        expect($('.cart_related').css("display")).toEqual("none");
+      it("enables the submit button",function(){
+        setFixtures("<div class='js-minicartItem'></div><div class='js-addToCartButton disabled' disabled='disabled'></div>");
+        MinicartBoxDisplayUpdater.facade();
+        expect($('.js-addToCartButton').attr("disabled")).toEqual(undefined);
+        expect($('.disabled').length).toEqual(0);
       });
-    });
+    });    
 
   });
 });
