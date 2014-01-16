b22492a23ff0e5112fcdea4f92c83bfacf6212c1
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-16 18:04:06 -0200

    Change for use scope

--- app/controllers/admin/orders/newest_reports_controller.rb
+++ app/controllers/admin/orders/newest_reports_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class Admin::Orders::NewestReportsController < Admin::BaseController
   def index
-    @orders = Order.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).joins(:user).group(:user_id).having("count(orders.id) = 1")
+    @orders = Order.with_first_buyers(Time.zone.now)
   end
 end
