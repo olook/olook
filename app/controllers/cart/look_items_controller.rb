3ab75378ff729691c917f7ab84aa65759500f75b
Rafael <rafael.manoel@gmail.com>
2014-01-16 17:01:54 -0200

    new spec and a little refactor to improve readability

--- app/controllers/cart/look_items_controller.rb
+++ app/controllers/cart/look_items_controller.rb
@@ -2,13 +2,10 @@ class Cart::LookItemsController < ApplicationController
   respond_to :json
   def create
     product = Product.find(params[:product_id])
-    @variant_numbers = product.variants.map{|v| v.number}
 
     render json: {
       product_id: product.id,
-      product_price: product.retail_price,
-      variant_number: params[:variant_number],
-      variant_numbers: @variant_numbers,
+      product_price: product.retail_price
     }
   end
 end
