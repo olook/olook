55767be3b267549128254ca90a3d8cb90017acf6
Rafael <rafael.manoel@gmail.com>
2014-01-17 19:52:06 -0200

    add wishlist model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140117210120) do
+ActiveRecord::Schema.define(:version => 20140117214951) do
 
   create_table "action_parameters", :force => true do |t|
     t.integer  "matchable_id"
@@ -1280,6 +1280,12 @@ ActiveRecord::Schema.define(:version => 20140117210120) do
     t.decimal  "retail_price", :precision => 10, :scale => 0
     t.datetime "created_at",                                  :null => false
     t.datetime "updated_at",                                  :null => false
+  end
+
+  create_table "wishlists", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
