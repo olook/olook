12a2099ca97e9bbc427b39d4490033e0bc2b2ebb
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-17 15:34:31 -0200

    Fix tests

--- app/validators/coupon_validator.rb
+++ app/validators/coupon_validator.rb
@@ -3,17 +3,13 @@ class CouponValidator < ActiveModel::Validator
   def validate(cart)
     if cart.coupon_code.present?
       coupon = Coupon.find_by_code(cart.coupon_code)
-      case
-      when coupon.nil?
+      if coupon.nil?
         cart.errors.add(:coupon_code, "Cupom inexistente. Tente digitar novamente.")
-      when coupon.try(:expired?)
+        return
+      end
+
+      if coupon.try(:expired?) || !coupon.try(:available?)
         cart.errors.add(:coupon_code, "Este cupom não é mais válido :/")
-      else
-        best_promotion = Promotion.select_promotion_for(cart)
-        pd = ProductDiscountService.new(item.product, cart: cart, coupon: coupon, promotion: best_promotion)
-        if pd.best_discount != coupon
-          cart.errors.add(:coupon_code, "Os descontos não são cumulativos")
-        end
       end
     end
   end
