314383159c6d791c87c260ececbd5ce2a3e41a99
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-17 14:41:26 -0200

    Messages when adding coupon

--- app/validators/coupon_validator.rb
+++ app/validators/coupon_validator.rb
@@ -4,12 +4,16 @@ class CouponValidator < ActiveModel::Validator
     if cart.coupon_code.present?
       coupon = Coupon.find_by_code(cart.coupon_code)
       case
-        when coupon.try(:expired?)
-          cart.errors.add(:coupon_code, "O cupom informado já está expirado. Se você possui outro, informe abaixo.")
-        when !coupon.try(:available?)
-          cart.errors.add(:coupon_code, "O cupom informado não é válido. Por favor, verifique o código informado.")
-        when cart.items.any? && !coupon.try(:can_be_applied_to_any_product_in_the_cart?, cart)
-          cart.errors.add(:coupon_code, "O cupom informado é válido apenas para produtos da marca #{ coupon.try(:brand)}. Navegue em nosso site e escolha outras peças desta marca. ;)")
+      when coupon.nil?
+        cart.errors.add(:coupon_code, "Cupom inexistente. Tente digitar novamente.")
+      when coupon.try(:expired?)
+        cart.errors.add(:coupon_code, "Este cupom não é mais válido :/")
+      else
+        best_promotion = Promotion.select_promotion_for(cart)
+        pd = ProductDiscountService.new(item.product, cart: cart, coupon: coupon, promotion: best_promotion)
+        if pd.best_discount != coupon
+          cart.errors.add(:coupon_code, "Os descontos não são cumulativos")
+        end
       end
     end
   end
