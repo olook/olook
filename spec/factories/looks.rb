cf0590bf993d0c1c06496afc63ce0e28a53b4149
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-20 19:16:14 -0200

    Adding model Look

---
+++ spec/factories/looks.rb
@@ -0,0 +1,10 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :look do
+    product_id 1
+    picture "MyString"
+    launched_at "2014-01-20 19:14:43"
+    profile_id 1
+  end
+end
