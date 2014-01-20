91b7cb509100f39f3c27a46160a272e49e174c41
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-20 13:34:01 -0200

    Add default value on migration

--- db/migrate/20140117120353_add_orders_count_to_users.rb
+++ db/migrate/20140117120353_add_orders_count_to_users.rb
@@ -1,5 +1,5 @@
 class AddOrdersCountToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :orders_count, :integer
+    add_column :users, :orders_count, :integer, default: 0
   end
 end
