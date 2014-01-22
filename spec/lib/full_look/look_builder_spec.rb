e752914cdcaa910b814a753537521e36fa55633c
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-22 13:53:19 -0200

    finish olook builder tests

--- spec/lib/full_look/look_builder_spec.rb
+++ spec/lib/full_look/look_builder_spec.rb
@@ -14,27 +14,50 @@ describe FullLook::LookBuilder do
     expect( described_class.instance_variable_get('@queue') ).to_not be_nil
   end
 
-
   describe '#perform' do
     def product(id)
       mock("Product##{id}",
-           gallery_5_pictures: [mock(image_url: 'image')],
-           launch_date: Time.now)
+           gallery_5_pictures: [mock(image_url: "image#{id}")],
+           launch_date: "2013-12-25")
     end
 
     before do
       subject.stub!(:set_category_weight_factor).and_return(Hash.new(1))
+    end
+    it "should create 2 Looks" do
+      FullLook::LookProfileCalculator.stub(:calculate).and_return(1)
       relateds = []
       relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(2))
       relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(3))
       relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(4))
       relateds << mock('RelatedProduct', product_a_id: 5, product_a: product(5), product_b: product(6))
       relateds << mock('RelatedProduct', product_a_id: 5, product_a: product(5), product_b: product(7))
-      subject.should_receive(:related_products).and_return(relateds)
+      subject.stub(:related_products).and_return(relateds)
+      Look.should_receive(:build_and_create).exactly(2).times
+      subject.perform
     end
 
-    it "should create 2 Looks" do
-      Look.should_receive(:build_and_create).exactly(2).times
+    it "create with correct params" do
+      FullLook::LookProfileCalculator.stub(:calculate).and_return(1)
+      relateds = []
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(2))
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(3))
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product(1), product_b: product(4))
+      subject.stub(:related_products).and_return(relateds)
+      Look.should_receive(:build_and_create).with({product_id: 1, picture: "image1", launched_at: "2013-12-25", profile_id: 1})
+      subject.perform
+    end
+    it "send related products to look profile calculator" do
+      product_1 = product(1)
+      product_2 = product(2)
+      product_3 = product(3)
+      product_4 = product(4)
+      FullLook::LookProfileCalculator.should_receive(:calculate).with([product_1,product_2,product_3,product_4],category_weight: subject.category_weight).and_return(1)
+      relateds = []
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product_1, product_b: product_2)
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product_1, product_b: product_3)
+      relateds << mock('RelatedProduct', product_a_id: 1, product_a: product_1, product_b: product_4)
+      subject.stub(:related_products).and_return(relateds)
       subject.perform
     end
   end
