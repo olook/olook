9546d9005acf623037199c79a34a52d0d72fb7e7
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-23 16:35:17 -0200

    Adding carrierwave

--- app/models/look.rb
+++ app/models/look.rb
@@ -3,11 +3,14 @@ class Look < ActiveRecord::Base
   belongs_to :product
   belongs_to :profile
 
+  mount_uploader :full_look_picture, LookImageUploader
+  mount_uploader :front_picture, LookImageUploader
+
   def self.build_and_create(attr)
     look = self.new
     look.product_id = attr[:product_id]
-    look.full_look_picture = attr[:full_look_picture]
-    look.front_picture = attr[:front_picture]
+    look.remote_full_look_picture_url = attr[:full_look_picture]
+    look.remote_front_picture_url = attr[:front_picture]
     look.launched_at = attr[:launched_at]
     look.profile_id = attr[:profile_id]
     look.save
