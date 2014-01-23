c214dee82599f8ab8fd4f4c4b263000a09af1784
Dommit <dommit@gmail.com>
2014-01-23 19:28:23 -0200

    added class soldOut to wishlist

--- app/helpers/wishlist_helper.rb
+++ app/helpers/wishlist_helper.rb
@@ -5,11 +5,21 @@ module WishlistHelper
   def get_label_and_class_name(cart, wished_product)
 
     variant_number = wished_product.variant.number
-    
+    sold_out_class = sold_out?(wished_product) ? 'soldOut' : ''
+    binding.pry
     if cart.items.map{|item| item.variant.number}.include?(variant_number)
-      { class_name: 'added_product', label: 'Adicionado' }
+      label = sold_out?(wished_product) ? 'Esgotado' : 'Adicionado'
+      { class_name: 'added_product', label: label, sold_out: sold_out_class }
     else
-      { class_name: 'add_product', label: 'Adicionar' }
+      label = sold_out?(wished_product) ? 'Esgotado' : 'Adicionar'
+      { class_name: 'add_product', label: label, sold_out: sold_out_class }
     end
   end
+
+  private 
+    def sold_out?(wished_product)
+      wished_product.variant.inventory = 0
+    end
+
+
 end
