05eb93da71f00b3898a35aaa547d7bdf1d60aadc
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-23 11:54:18 -0200

    Refactor and prevent to show same products on look

--- spec/factories/looks.rb
+++ spec/factories/looks.rb
@@ -1,9 +1,9 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
 FactoryGirl.define do
   factory :look do
     product_id 1
-    picture "MyString"
+    sequence :picture do |n|
+      "image#{n}"
+    end
     launched_at "2014-01-20 19:14:43"
     profile_id 1
   end
