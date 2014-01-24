b634d6bec1fb47e51ef353ca1a31e01aae5cbec2
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-24 13:38:43 -0200

    Add looks and rollback lite application

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
 
-  layout "lite_application"
   def index
     @google_path_pixel_information = "Home"
     @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
-
+    @looks = RecomendationService.new(profiles: current_user.profiles).full_looks(limit: 4)
     prepare_for_home
   end
 
