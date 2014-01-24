9cd670de0245c4813007519bd634a31b50b5cabd
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-24 11:31:32 -0200

    Add lite application on home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 class HomeController < ApplicationController
 
+  layout "lite_application"
   def index
     @google_path_pixel_information = "Home"
     @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
