72a928c2ebda6342e35b3a6e59cc0f5931b5b4d0
Luis Daher <luisotaviodaher@gmail.com>
2014-01-24 15:07:22 -0200

    wishlist feedback working without the need of refreshing the browser

--- app/assets/javascripts/modules/product/remove_from_wishlist_success_message.js
+++ app/assets/javascripts/modules/product/remove_from_wishlist_success_message.js
@@ -7,6 +7,13 @@ RemoveFromWishlistSuccessMessage = function(){
 
     // wishlist page
     $('.js-product-' + productId).fadeOut();
+
+    if($(".js-full-wishlist-box").size() == 1 && parseInt($(".js-product-count").text()) == 1){
+      $('.js-full-wishlist-box').addClass('wishlist').removeClass('wishlistHasProduct').removeClass('js-full-wishlist-box').addClass('js-empty-wishlist-box');
+      $('.js-sub-text').html("Você ainda não adicionou nenhum<br />produto a sua lista de favoritos.");
+    }else if($(".js-full-wishlist-box").size() == 1 && parseInt($(".js-product-count").text()) > 1){      
+      $(".js-product-count").text(parseInt($(".js-product-count").text()) - 1); 
+    }    
   }
 
   return {
