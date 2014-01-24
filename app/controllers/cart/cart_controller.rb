3c835692d99a28d576cacbfdb7c7d2f2fca553ca
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-24 18:39:04 -0200

    add coupon value for percentage coupon too

--- app/controllers/cart/cart_controller.rb
+++ app/controllers/cart/cart_controller.rb
@@ -49,11 +49,8 @@ class Cart::CartController < ApplicationController
     @cart.reload
     @cart_calculator = CartProfit::CartCalculator.new(@cart)
 
-    #
-    # Isto é feio, muito feio. Juro que volto aqui para refatorar
-    #
-    if @cart.coupon && @cart.coupon.promotion_action.is_a?(ValueAdjustment)
-      @coupon_value = -1 * @cart.coupon.action_parameter.action_params[:param].to_f
+    if @cart.coupon
+      @coupon_value = -1 * (@cart.items.inject(0.0){|sum,i| sum+i.adjustment_value.to_f})
     end
 
     @freebie = Freebie.new(subtotal: @cart.sub_total, cart_id: @cart.id)
