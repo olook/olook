5abc1a7888c7d11df2a644da4a79028ff3c9655f
Luis Daher <luisotaviodaher@gmail.com>
2014-01-27 17:51:29 -0200

    WIP - admin changes

---
+++ app/uploaders/side_highlight_banner_uploader.rb
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+class SideHighlightBannerUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  include Piet::CarrierWaveExtension
+
+  storage :fog
+
+  def store_dir
+    "#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  version :thumb do
+    process :resize_to_fit => [200, 100]
+    process optimize: [{quality: 90}]
+  end
+
+  version :site do
+    process :resize_to_fit => [239, 360]
+    process optimize: [{quality: 90}]
+  end  
+
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+end
