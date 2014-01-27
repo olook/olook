8033f8bb0d904389a6553b1e6100e8b42c68b7a5
Dommit <dommit@gmail.com>
2014-01-27 19:14:26 -0200

    wip new home

--- app/assets/javascripts/section/lite_home.js
+++ app/assets/javascripts/section/lite_home.js
@@ -1,2 +1,13 @@
-//= require jquery.elastislide
+//= require jquery
 //= require modernizr.custom.17475
+//= require jquerypp.custom
+//= require jquery.elastislide
+
+
+$(function(){
+  olook.carousel();
+});
+
+olook.carousel = function() {
+  $('#carousel').elastislide();
+}
