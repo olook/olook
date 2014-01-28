22ffe222cbf020d009c372a05ee1e49ed8189b86
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-01-28 16:39:23 -0200

    Apply image only if exists

--- app/controllers/admin/highlights_controller.rb
+++ app/controllers/admin/highlights_controller.rb
@@ -5,9 +5,9 @@ class Admin::HighlightsController <  Admin::BaseController
   respond_to :haml
 
   def index
-    @left_image = Highlight.find_by_position 2
-    @center_image = Highlight.find_by_position 1
-    @right_image = Highlight.find_by_position 3
+    @left_hightlight = Highlight.find_by_position 2
+    @center_hightlight = Highlight.find_by_position 1
+    @right_hightlight = Highlight.find_by_position 3
   end
 
   def show
