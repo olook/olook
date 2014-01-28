9cae76a9e70d07a299a51e941dd459ebc1547b18
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-28 16:51:44 -0200

    Dynamic unlogged page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,27 +5,25 @@ class HomeController < ApplicationController
   def index
     @google_path_pixel_information = "Home"
     @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
-    @recommendation = RecommendationService.new(profiles: current_user.try(:profiles_with_fallback))
+    @recommendation = RecommendationService.new(profiles: current_user.try(:profiles_with_fallback) || [Profile.default])
     @looks = @recommendation.full_looks(limit: 4)
 
     @products = []
-    if current_user
-      categories = [
-        Category::CLOTH,
-        Category::SHOE,
-        Category::BAG,
-        Category::ACCESSORY
-      ]
-      products = categories.map do |category_id|
-        @recommendation.products(category: category_id, limit: 18)
-      end.flatten
-      categories.cycle do |category_id|
-        break if products.empty? || @products.size >= 18
-        p = products.find { |_p| _p.category == category_id }
-        if p
-          @products.push(p)
-          products.delete(p)
-        end
+    categories = [
+      Category::CLOTH,
+      Category::SHOE,
+      Category::BAG,
+      Category::ACCESSORY
+    ]
+    products = categories.map do |category_id|
+      @recommendation.products(category: category_id, limit: 18)
+    end.flatten
+    categories.cycle do |category_id|
+      break if products.empty? || @products.size >= 18
+      p = products.find { |_p| _p.category == category_id }
+      if p
+        @products.push(p)
+        products.delete(p)
       end
     end
 
