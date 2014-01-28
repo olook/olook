0d9716dcbba69393113b0bfb5c9dd7ef578f4723
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-28 20:14:48 -0200

    Refactoring home

---
+++ app/presenters/showroom_presenter.rb
@@ -0,0 +1,53 @@
+class ShowroomPresenter
+  CATEGORIES_FOR_SHOWROOM = [
+    Category::CLOTH,
+    Category::SHOE,
+    Category::BAG,
+    Category::ACCESSORY
+  ]
+
+  def initialize(args={})
+    @recommendation = args[:recommendation]
+    @products_limit = args[:products_limit] || 18
+    @looks_limit = args[:looks_limit] || 4
+    @products = []
+  end
+
+  def products
+    fetched_products = fetch_products_in_each_category
+    organize_fetched_products_in_category_sequence(fetched_products)
+    @products
+  end
+
+  def looks(opts = {})
+    @looks_limit = opts[:limit] if opts[:limit]
+    @recommendation.full_looks(limit: @looks_limit)
+  end
+
+  def look
+    @look ||= looks(limit:1).first
+  end
+
+  private
+
+  def fetch_products_in_each_category
+    categories_for_showroom.map do |category_id|
+      @recommendation.products(category: category_id, limit: @limit)
+    end.flatten
+  end
+
+  def organize_fetched_products_in_category_sequence(fetched_products)
+    categories_for_showroom.cycle do |category_id|
+      break if fetched_products.empty? || @products.size >= @products_limit
+      p = fetched_products.find { |_p| _p.category == category_id }
+      if p
+        @products.push(p)
+        fetched_products.delete(p)
+      end
+    end
+  end
+
+  def categories_for_showroom
+    CATEGORIES_FOR_SHOWROOM
+  end
+end
