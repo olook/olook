53637bc3058fcdaafc200e740b378501601a1ae2
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-01-29 12:16:35 -0200

    Change change_picture_onhover to work also in home

--- app/assets/javascripts/plugins/change_picture_onhover.js
+++ app/assets/javascripts/plugins/change_picture_onhover.js
@@ -1,12 +1,29 @@
 if(!visualization_mode) var visualization_mode = "product";
 if(!olook) var olook = {};
-olook.changePictureOnhover = function(selector) {
+olook.changePictureOnhover = function(selector, hover_picture) {
+  if(!hover_picture){
+    hover_picture = 'backside-picture';
+  } else {
+    hover_picture = 'hover';
+  }
   $(selector).mouseenter(function() {
-    // debugger;
-    var it = $(this).children().first();
-    it.attr('src', it.data('backside-picture'));
+    var it;
+    if($(this).is('img')){
+      it = $(this);
+    }else{
+      it = $(this).children('img:first');
+    }
+    if(!it.data('original')){
+      it.data('original', it.attr('src'));
+    }
+    it.attr('src', it.data(hover_picture));
   }).mouseleave(function() {
-    var it = $(this).children().first();
-    it.attr('src', it.data(visualization_mode));
+    var it;
+    if($(this).is('img')){
+      it = $(this);
+    }else{
+      it = $(this).children('img:first');
+    }
+    it.attr('src', it.data('original'));
   });
 };
--- app/assets/javascripts/section/lite_home.js
+++ app/assets/javascripts/section/lite_home.js
@@ -1,10 +1,12 @@
 //= require modernizr.custom.17475
 //= require jquerypp.custom
 //= require jquery.elastislide
+//= require plugins/change_picture_onhover
 
 
 $(function(){
   olook.carousel();
+  olook.changePictureOnhover('a:has(img[data-hover])', 'hover')
 });
 
 olook.carousel = function() {
