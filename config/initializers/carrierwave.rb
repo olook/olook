781827ff432bb70a7271859895d75edf16651544
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-04 16:55:55 -0200

    Using edgecast as cdn endpoint

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -17,7 +17,13 @@ CarrierWave.configure do |config|
   #   end
   else
     config.fog_directory = 'cdn.olook.com.br'
-    config.fog_host = "//d1m0eb15reawkf.cloudfront.net"
+    config.fog_host = Proc.new { |source, request| 
+      if request.ssl?
+        "https://gp1.wac.edgecastcdn.net/80BFF9/uploads/"
+      else
+        "http://wac.bff9.edgecastcdn.net/80BFF9/uploads/"
+      end
+    }
   end
   
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000',
