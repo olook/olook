8c1596c691b586affc775481dccd88236c90b418
Luis Daher <luisotaviodaher@gmail.com>
2014-02-06 18:43:52 -0200

    fixing carrierwave config file to work with the test environment correctly

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
 
   if Rails.env.test?
     config.fog_directory = 'testcdn.olook.com.br'
-    config.fog_host = 'http://testcdn.olook.com.br'
+    config.asset_host = 'http://testcdn.olook.com.br'
   # elsif Rails.env.staging? || Rails.env.development?
   #   config.fog_directory = 'cdn-staging.olook.com.br'
   #   config.fog_host = proc do |file|
