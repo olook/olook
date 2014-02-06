26cedfc1b252ec7f6494df9c7b3a3582e094a539
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-02-06 17:17:01 -0200

    Rollback test

--- app/helpers/checkout/checkout_helper.rb
+++ app/helpers/checkout/checkout_helper.rb
@@ -12,7 +12,7 @@ module Checkout::CheckoutHelper
 
   def show_motoboy_freight?
     current_time = Time.now
-    @shipping_service_fast.shipping_service_id == MOTOBOY_FREIGHT_SERVICE_ID #&& Time.workday?(current_time) && !Time.before_business_hours?(current_time)# && !Time.after_business_hours?(current_time)
+    @shipping_service_fast.shipping_service_id == MOTOBOY_FREIGHT_SERVICE_ID && Time.workday?(current_time) && !Time.before_business_hours?(current_time) && !Time.after_business_hours?(current_time)
   end
 
   def error_class_if_needed(object, field)
