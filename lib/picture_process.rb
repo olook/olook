d58d06713ad72f0b180323c4b0453d108308c90e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-10 18:02:28 -0200

    Check if job is pending

--- lib/picture_process.rb
+++ lib/picture_process.rb
@@ -14,12 +14,35 @@ class PictureProcess
     self.new(options).perform
   end
 
+  def self.is_pending?(*args)
+    is_working?(*args) || is_in_queue?(*args) 
+  end
+
   def initialize(options={})
-    @key = options[:key]
+    @key = options['key']
     raise ArgumentError.new("Directory to process on S3 Bucket is necessary (key is nil)") if @key.blank?
   end
 
   def perform
     puts "Fazendo a porra toda"
+    sleep 600
+  end
+
+  private
+
+  def self.is_in_queue?(*args)
+    key = Resque.encode(class: self.to_s, args: args)
+    queue = "queue:#{@queue}"
+    jobs = Resque.redis.lrange(queue, 0, -1)
+    jobs.any? do |job|
+      job == key
+    end
+  end
+
+  def self.is_working?(*args)
+    key = Resque.decode(Resque.encode( class: self.to_s, args: args ))
+    Resque.working.any? do |worker|
+      worker.job['payload'] == key
+    end
   end
 end
