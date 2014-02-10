23b3403f57cc97e996e7e7881e54c74e12a77805
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-10 18:31:12 -0200

    fix flash notice

--- app/controllers/admin/pictures_process_controller.rb
+++ app/controllers/admin/pictures_process_controller.rb
@@ -7,6 +7,6 @@ class Admin::PicturesProcessController < Admin::BaseController
   def create
     authorize! :create_multiple_pictures, Picture
     Resque.enqueue(PictureProcess, key: params[:key])
-    redirect_to admin_pictures_process_path notice: 'Processamento enfileirado. Aguarde o email daqui alguns minutos'
+    redirect_to admin_pictures_process_path, notice: 'Processamento enfileirado. Aguarde o email daqui alguns minutos'
   end
 end
