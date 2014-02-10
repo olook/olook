de81a8db1a45d4bdcdded87615e58d754d7f23d7
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-10 16:12:45 -0200

    Send job

--- app/controllers/admin/pictures_process_controller.rb
+++ app/controllers/admin/pictures_process_controller.rb
@@ -1,10 +1,12 @@
 class Admin::PicturesProcessController < Admin::BaseController
   def index
-    @keys = PictureProcess.new(prefix: params[:prefix]).list
     authorize! :new_multiple_pictures, Picture
+    @keys = PictureProcess.list(prefix: params[:prefix])
   end
 
   def create
     authorize! :create_multiple_pictures, Picture
+    Resque.enqueue(PictureProcess, key: params[:key])
+    redirect_to admin_pictures_process_path notice: 'Processamento enfileirado. Aguarde o email daqui alguns minutos'
   end
 end
