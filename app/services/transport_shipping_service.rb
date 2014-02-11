bf6352a87bcbf76883f8641e4911826dda031688
Rafael <rafael.manoel@gmail.com>
2014-02-11 13:00:55 -0200

    sort the faster shipping by price instead of cost

--- app/services/transport_shipping_service.rb
+++ app/services/transport_shipping_service.rb
@@ -24,7 +24,7 @@ class TransportShippingService
 
   private
     def better_cost_shipping
-      transport_shippings.sort{|x,y| x[:cost] <=> y[:cost]}.first
+      transport_shippings.sort{|x,y| x[:price] <=> y[:price]}.first
     end
     def better_time_shipping
       transport_shippings.sort{|x,y| x[:delivery_time] <=> y[:delivery_time]}.first
