f36451ee0c6ceb38bb855b07997c94127fdcd66e
Dommit <dommit@gmail.com>
2014-02-12 11:12:10 -0200

    fixed thumbs async

--- app/assets/javascripts/section/search.js
+++ app/assets/javascripts/section/search.js
@@ -1,6 +1,6 @@
 //= require plugins/change_picture_onhover
 
 $(function(){
-  olook.changePictureOnhover('.look_thumbnail');
   new ImageLoader().load('look_thumbnail');
+  olook.changePictureOnhover('.async');
 });
