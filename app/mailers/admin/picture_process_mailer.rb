cc527b5f5d44783c1ba5aa5ecc19ee7786015d77
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-02-14 16:55:39 -0200

    Passing admin to mailer

--- app/mailers/admin/picture_process_mailer.rb
+++ app/mailers/admin/picture_process_mailer.rb
@@ -1,9 +1,11 @@
 # -*- encoding : utf-8 -*-
-class PictureProcessMailer < ActionMailer::Base
+class Admin::PictureProcessMailer < ActionMailer::Base
   default_url_options[:host] = "www.olook.com.br"
   default :from => "olook notification <dev.notifications@olook.com.br>"
 
-  def notify_picture_process(process_info)
-    
+  def notify_picture_process( user_email, process_info )
+    @user_email = user_email
+    @process_info = process_info
+    mail(to: "#{user_email},tiago.almeida@olook.com.br", :subject => "Informações sobre processamento de images")
   end
 end
