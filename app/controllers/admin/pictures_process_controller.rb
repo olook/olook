687b2274f698f221b12143a91fb65b5dbe5c2389
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-02-14 16:55:14 -0200

    Add admin to enqueue

--- app/controllers/admin/pictures_process_controller.rb
+++ app/controllers/admin/pictures_process_controller.rb
@@ -6,7 +6,7 @@ class Admin::PicturesProcessController < Admin::BaseController
 
   def create
     authorize! :create_multiple_pictures, Picture
-    Resque.enqueue(PictureProcess, key: params[:key])
-    redirect_to admin_pictures_process_path, notice: 'Processamento enfileirado. Aguarde o email daqui alguns minutos'
+    Resque.enqueue(PictureProcess, {"key" => params[:key], "user_email" => current_admin.email})
+    redirect_to admin_pictures_process_path, notice: 'Processamento está sendo realizado. Aguarde o email com as informações'
   end
 end
