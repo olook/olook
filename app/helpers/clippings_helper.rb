56f25f2199074934d65deae44ca6ee0a6eae4aac
Rafael <rafael.manoel@gmail.com>
2014-02-19 16:49:42 -0300

    fix url bug on 'olook na imprensa' page

--- app/helpers/clippings_helper.rb
+++ app/helpers/clippings_helper.rb
@@ -2,7 +2,13 @@
 module ClippingsHelper
   def show_link clipping
     if clipping.link.blank?
-      "http:#{clipping.pdf_file_url}"
+
+      if clipping.pdf_file_url =~ /http/
+        clipping.pdf_file_url
+      else
+        "http:#{clipping.pdf_file_url}"
+      end
+
     else
       clipping.link
     end
