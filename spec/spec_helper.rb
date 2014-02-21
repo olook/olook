bb2d49020245da8d8b0af867cd184f4eac03010e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-21 18:10:00 -0300

    Adding config to use focus true to run only the example using guard

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -69,6 +69,9 @@ Spork.each_run do
 
   HTTPI.log = false
   RSpec.configure do |config|
+    config.filter_run :focus => true
+    config.run_all_when_everything_filtered = true
+
     config.treat_symbols_as_metadata_keys_with_true_values = true
 
     # Trying: http://devblog.avdi.org/2012/08/31/configuring-database_cleaner-with-rails-rspec-capybara-and-selenium/
