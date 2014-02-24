e112358461b8f3742d92ec7896cda7074f3c63b9
Luis Daher <luisotaviodaher@gmail.com>
2014-02-24 19:28:54 -0300

    changes for the cart upload method

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -145,7 +145,7 @@ class Cart < ActiveRecord::Base
         self.coupon_code = self.coupon.code
       elsif self.coupon_code
         _coupon = Coupon.find_by_code(self.coupon_code)
-        self.coupon = _coupon if _coupon
+        self.coupon = _coupon if _coupon && !coupon_used_by_user?(_coupon)
         self.gift_wrap = true if free_gift_wrap?
       end
     end
