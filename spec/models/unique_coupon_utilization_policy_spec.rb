b8a918fee14c279e55a21805a5d3d5d15014d33f
Rafael <rafael.manoel@gmail.com>
2014-02-25 14:49:29 -0300

    refactor to improve testability and fix some specs

---
+++ spec/models/unique_coupon_utilization_policy_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe UniqueCouponUtilizationPolicy do
+  describe ".apply?" do
+    context 'coupon is unique per user' do
+      let(:coupon) {double(Coupon, one_per_user?: true, id: 100)}
+      subject {
+        UniqueCouponUtilizationPolicy.apply?(
+          coupon: coupon, 
+          user_coupon: user_coupon)
+      }
+
+      context 'user haven\'t used the coupon' do
+        let(:user_coupon) { double(UserCoupon, include?: false)}
+
+        it { should be_true }
+      end
+
+      context 'user alredy used the coupon' do
+        let(:user_coupon) { double(UserCoupon, include?: true)}
+        it { should be_false }
+
+      end
+
+    end
+
+    context 'when there is no user_coupon' do
+      subject {UniqueCouponUtilizationPolicy.apply?(coupon: double(Coupon))}
+      it { should be_true }
+    end
+
+  end
+end
