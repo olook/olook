2929150ccba1374e5407ab580bc712e4afbe088e
darkseid <rafael.manoel@gmail.com>
2014-02-26 17:15:49 -0300

    fix creating element problem

--- app/assets/javascripts/plugins/async_image_loader.js
+++ app/assets/javascripts/plugins/async_image_loader.js
@@ -13,11 +13,9 @@ function ImageLoader() {
       var img_container;
       var attributes;
       if (container.tagName != 'IMG') {
-        img_container = new Image();
+        img_container = document.createElement('img');
         img_container.className = 'async'
-        console.log('img container created');
         container.appendChild(img_container);
-        console.log('img container added to ' + container.tagName );
         attributes = {
           'data-backside-picture': container.getAttribute('data-backside-picture'),
           'data-product': container.getAttribute('data-product')
