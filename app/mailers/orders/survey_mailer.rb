3cf8e757eb0cb867fc8c90e72124f0e50eaa0e48
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-02-27 11:05:37 -0300

    Fix mailer name

--- app/mailers/orders/survey_mailer.rb
+++ app/mailers/orders/survey_mailer.rb
@@ -1,10 +1,11 @@
 # -*- encoding : utf-8 -*-
-class OrderStatusMailer < ActionMailer::Base
+class Orders::SurveyMailer < ActionMailer::Base
   default_url_options[:host] = "www.olook.com.br"
   default :from => "Equipe Olook <avisos@olook.com.br>"
 
   def order_survey order
     @order = order
+    @link = Survey::Order.generate @order.line_items
     mail(:to => @order.user_email, :subject => "Pesquisa de satisfação")
   end
 end
