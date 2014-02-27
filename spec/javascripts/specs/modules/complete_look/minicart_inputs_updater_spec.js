f09ed0e565ef1c563b09a19e60cea93cccb0909e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-27 16:36:51 -0300

    All tests pass

--- spec/javascripts/specs/modules/complete_look/minicart_inputs_updater_spec.js
+++ spec/javascripts/specs/modules/complete_look/minicart_inputs_updater_spec.js
@@ -1,8 +1,23 @@
 describe("MinicartInputsUpdater", function() {
-  
-  describe("name", function() {
-    expect(MinicartInputsUpdater.name).toEqual('UPDATE_MINICART_INPUTS');
+
+  describe("#config", function() {
+    beforeEach(function(){
+      olookApp = jasmine.createSpyObj('olookApp', ['subscribe']);
+    });
+    it("should call subscribe in channel minicart:update:inputs", function(){
+      var obj = new MinicartInputsUpdater();
+      obj.config();
+      expect(olookApp.subscribe).toHaveBeenCalledWith("minicart:update:inputs", obj.facade, {}, obj);
+    });
   });
+
+  var expectHiddenFieldWithValue = function(value) {
+    var hiddenField = $('#minicart input');
+    expect(hiddenField.size()).toEqual(1);
+    expect(hiddenField.attr('id')).toEqual('variant_numbers_');
+    expect(hiddenField.attr('name')).toEqual('variant_numbers[]');
+    expect(hiddenField.val()).toEqual(value);
+  }
 
   describe("#facade", function() {
 
@@ -12,7 +27,7 @@ describe("MinicartInputsUpdater", function() {
 
     describe("when the cart is empty", function() {
       it("should have only 1 hidden field", function() {
-        MinicartInputsUpdater.facade(1000, '3');
+        new MinicartInputsUpdater().facade({productId: 1000, variantNumber: '3'});
         expectHiddenFieldWithValue('3')
       });
     });
@@ -24,35 +39,27 @@ describe("MinicartInputsUpdater", function() {
       });
 
       it("should exchange the previous one for the new one", function() {
-        MinicartInputsUpdater.facade(1001, "2");
+        new MinicartInputsUpdater().facade({productId: 1001, variantNumber: "2"});
         expectHiddenFieldWithValue('2')
       });
-      
-    });   
+
+    });
 
     describe("when there is 2 different products in the cart", function() {
-      
+
       beforeEach(function() {
         $('#minicart').append("<input type='hidden' id='variant_numbers_' name='variant_numbers[]' class='js-1002' value='1'>");
-        $('#minicart').append("<input type='hidden' id='variant_numbers_' name='variant_numbers[]' class='js-1003' value='3'>");     
+        $('#minicart').append("<input type='hidden' id='variant_numbers_' name='variant_numbers[]' class='js-1003' value='3'>");
       });
 
       describe("and only the second is changed", function() {
         it("should update only the variant number of the last product", function() {
           expect($('.js-1003').val()).toEqual('3');
-          MinicartInputsUpdater.facade(1003, "2");   
+          new MinicartInputsUpdater().facade({productId: 1003, variantNumber: "2"});
           expect($('.js-1003').val()).toEqual('2');
         });
       });
     });
   });
-
-  expectHiddenFieldWithValue = function(value) {
-    var hiddenField = $('#minicart input');
-    expect(hiddenField.size()).toEqual(1);
-    expect(hiddenField.attr('id')).toEqual('variant_numbers_');
-    expect(hiddenField.attr('name')).toEqual('variant_numbers[]');
-    expect(hiddenField.val()).toEqual(value);    
-  }
 
 });
