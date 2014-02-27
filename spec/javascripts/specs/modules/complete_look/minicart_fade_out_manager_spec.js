b8608e50d2e5994dd0af2e0d41fbbce7b344bcea
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-02-27 16:36:51 -0300

    All tests pass

--- spec/javascripts/specs/modules/complete_look/minicart_fade_out_manager_spec.js
+++ spec/javascripts/specs/modules/complete_look/minicart_fade_out_manager_spec.js
@@ -1,8 +1,13 @@
 describe("MinicartFadeOutManager", function() {
 
-  describe("#name", function() {
-    it("should be called FADE_OUT_MINICART", function(){
-      expect(MinicartFadeOutManager.name).toEqual("FADE_OUT_MINICART");
+  describe("#config", function() {
+    beforeEach(function(){
+      olookApp = jasmine.createSpyObj('olookApp', ['subscribe']);
+    });
+    it("should call subscribe in channel minicart:update:fadeout", function(){
+      var obj = new MinicartFadeOutManager();
+      obj.config();
+      expect(olookApp.subscribe).toHaveBeenCalledWith("minicart:update:fadeout", obj.facade, {}, obj);
     });
   });
 
@@ -11,22 +16,22 @@ describe("MinicartFadeOutManager", function() {
     beforeEach(function() {
       html = setFixtures("<input id='total_price' name='total_price' /><div class='cart_related' style='display:block;'></div>");
     });
-  
+
     it("fades out when there is no element on the cart and variantNumber is provided", function(){
-      MinicartFadeOutManager.facade(123);
-      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'none', "The element won't ever be shown");      
+      new MinicartFadeOutManager().facade({variantNumber: 123});
+      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'none', "The element won't ever be shown");
     });
-    
+
     it("doesn't fade out when there is no element in the list but variantNumber is not provided", function(){
-      MinicartFadeOutManager.facade();
-      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'block', "The element won't ever be shown");      
+      new MinicartFadeOutManager().facade({variantNumber:''});
+      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'block', "The element won't ever be shown");
     });
 
     it("doesn't fade out when there's an element in the list", function(){
       html.append(sandbox({class: 'js-minicartItem'}))
 
-      MinicartFadeOutManager.facade(123);
-      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'block', "The element won't ever be shown");      
+      new MinicartFadeOutManager().facade({variantNumber:123});
+      JasmineAjaxHelper.expectCartDisplayProperty($('.cart_related'),"display",'block', "The element won't ever be shown");
 
     });
   });
