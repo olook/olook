80ed6c2a5910ba15aae70a916e573575cee3264e
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-07 14:47:22 -0300

    Add has purchase on csv

--- lib/multi_workers_process_slave.rb
+++ lib/multi_workers_process_slave.rb
@@ -24,13 +24,14 @@ class MultiWorkersProcessSlave
   end
 
   def self.map data
-    User.find_by_sql("select uuid, first_name, created_at, email, birthday, authentication_token , total from (select u.id as uuid, (
+    User.find_by_sql("select uuid, first_name, users.created_at, email, birthday, authentication_token, tem_pedido, po,total from (select u.id as uuid, (
     IFNULL((select sum(c.value) from credits c where c.user_credit_id = uc.id and c.is_debit = 0 and c.activates_at <= date(now()) and c.expires_at >= date(now())),0)  -
     IFNULL((select sum(c.value) from credits c where c.user_credit_id = uc.id and c.is_debit = 1 and c.activates_at <= date(now()) and c.expires_at >= date(now())), 0)
-  ) total from users u left join user_credits uc on u.id = uc.user_id and uc.credit_type_id = 1
-      where u.id >= #{data['first']} and u.id < #{data['last']}
+  ) total, ( select IF(count(o.id) > 0, 'SIM', 'NAO' )) tem_pedido, (select MAX(o.created_at)) po from users u left join user_credits uc on u.id = uc.user_id and uc.credit_type_id = 1 left join orders o on u.id = o.user_id and o.state in ('authorized', 'delivery', 'picking', 'delivering')
+      where u.id >= 0 and u.id < 100
       group by u.id
-) as tmp join users on tmp.uuid = users.id") 
+      limit 100
+  ) as tmp join users on tmp.uuid = users.id") 
   end
 
 end
