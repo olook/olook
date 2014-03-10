682743a185f212f6b4b8c68ff3e9ded6ea472277
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-10 15:51:48 -0300

    Front end connect working

--- app/assets/javascripts/lite_application.js
+++ app/assets/javascripts/lite_application.js
@@ -24,6 +24,7 @@
 //= require ./partials/_credits_info
 //= require plugins/footer_popup
 //= require modules/facebook/events
+//= require_tree ./modules/facebook/auth
 //= require modules/facebook/auth
 
 new FacebookEvents().config();
