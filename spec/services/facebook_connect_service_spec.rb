d2bd8c43559c1c8f999e844842242ee900160c3d
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-11 15:06:58 -0300

    Logic ready, need to implement create and update user

--- spec/services/facebook_connect_service_spec.rb
+++ spec/services/facebook_connect_service_spec.rb
@@ -1,25 +1,35 @@
 require File.expand_path(File.join(__dir__, '../../app/services/facebook_connect_service'))
-# require File.expand_path(File.join(File.dirname(__FILE__), '../../app/services/cart_discount_service'))
 
 describe FacebookConnectService do
   describe "#get_facebook_data" do
-
+    before(:each) do
+      @access_token = 'HEHEHE'
+    end
+    subject { described_class.new({'accessToken' => @access_token}) }
+    it "should call fb_api with '/me' and @access_token" do
+      subject.should_receive(:fb_api).with('/me', @access_token)
+      subject.get_facebook_data
+    end
   end
 
   describe "#connect!" do
     before(:each) do
-      subject.stub(:valid_facebook_data?).and_return(true)
+      subject.stub(:get_facebook_data).and_return({"id" => '123', 'email' => 'teste@teste.com'})
     end
-    context "when there's not enough data to connect" do
-      subject { described_class.new({}) }
+
+    subject { described_class.new({ 'accessToken' => 'HEHEHE' }) }
+
+    context "when facebook data has error" do
+      before(:each) do
+        subject.stub(:get_facebook_data).and_return({"error"=>{"message"=>"The access token could not be decrypted", "type"=>"OAuthException", "code"=>190}})
+      end
       it { expect(subject.connect!).to be_false }
     end
 
     context "new user" do
       before(:each) do
-        subject.stub!(:existing_user).and_return(nil)
+        subject.stub!(:existing_user).and_return(false)
       end
-      subject { described_class.new({ 'accessToken' => 'HEHEHE' }) }
 
       it "should create an User" do
         subject.should_receive(:create_user).once
@@ -27,9 +37,10 @@ describe FacebookConnectService do
       end
 
       it "should set user" do
-        user = double('user')
-        subject.stub(:create_user).and_return(user)
-        expect(subject.user).to be_eql(user)
+        @user = double('user')
+        subject.stub!(:create_user).and_return(@user)
+        subject.connect!
+        expect(subject.user).to eq(@user)
       end
     end
 
@@ -38,17 +49,17 @@ describe FacebookConnectService do
         @user = double('user')
         subject.stub!(:existing_user).and_return(@user)
       end
-      subject { described_class.new({ 'email' => 'teste@teste.com', 'id' => 123 }, { 'accessToken' => 'HEHEHE' }) }
 
-      it "should create an User" do
+      it "should update an User" do
         subject.should_not_receive(:create_user)
         subject.should_receive(:update_user)
         subject.connect!
       end
 
       it "should set user" do
-        subject.stub(:create_user).and_return(user)
-        expect(subject.user).to be_eql(user)
+        subject.stub(:update_user).and_return(@user)
+        subject.connect!
+        expect(subject.user).to eq(@user)
       end
     end
   end
