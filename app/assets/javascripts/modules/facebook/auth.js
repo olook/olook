6be0347b032b53b8b4196ec4935c0abcd5587a65
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-11 15:06:58 -0300

    Logic ready, need to implement create and update user

--- app/assets/javascripts/modules/facebook/auth.js
+++ app/assets/javascripts/modules/facebook/auth.js
@@ -1,10 +1,6 @@
 var FacebookAuth = (function(){
   function FacebookAuth() { };
 
-  var getUserData = function(callback) {
-    FB.api('/me', callback);
-  };
-
   FacebookAuth.prototype.config = function(){
     olookApp.subscribe('fb:auth:statusChange', this.facade, {}, this);
     new FacebookAuthSuccess().config();
@@ -14,27 +10,20 @@ var FacebookAuth = (function(){
     this.authResponse = response.authResponse;
     this.status = response.status;
     olookApp.publish('fb:auth:statusChange.before');
-    getUserData(function(userData){
-      console.log(userData);
-      $.ajax({
-        url: '/facebook_connect',
-        type: 'POST',
-        contentType: 'application/json',
-        dataType: 'json',
-        data: JSON.stringify({
-          facebookData: userData,
-          authResponse: this.authResponse })
-      }).complete(function(){
-        olookApp.publish('fb:auth:statusChange.complete');
-      }).success(function(result){
-        olookApp.publish('fb:auth:success', result);
-      }).error(function(){
-        olookApp.publish('fb:auth:error');
-      });
+    $.ajax({
+      url: '/facebook_connect',
+      type: 'POST',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ authResponse: this.authResponse })
+    }).complete(function(){
+      olookApp.publish('fb:auth:statusChange.complete');
+    }).success(function(result){
+      olookApp.publish('fb:auth:success', result);
+    }).error(function(){
+      olookApp.publish('fb:auth:error');
     });
   };
-
-
 
   return FacebookAuth;
 })();
--- app/services/facebook_connect_service.rb
+++ app/services/facebook_connect_service.rb
@@ -1,8 +1,76 @@
+require 'net/https'
+require 'json'
+
 class FacebookConnectService
-  def initialize(facebook_data_hash, auth_response_hash)
-    
+  attr_reader :user
+
+  def initialize(auth_response_hash)
+    @access_token = auth_response_hash['accessToken']
+    @user_id = auth_response_hash['userId']
+  end
+
+  def get_facebook_data
+    fb_api('/me', @access_token)
   end
 
   def connect!
+    @facebook_data = get_facebook_data
+    if(@facebook_data.respond_to?(:[]) && @facebook_data['error'])
+      return false
+    end
+
+    if existing_user
+      @user = update_user
+    else
+      @user = create_user
+    end
+    return true
+  end
+
+  private
+
+  def existing_user
+    @user = User.find_by_email(@facebook_data['email'])
+    !!@user
+  end
+
+  def create_user
+  end
+
+  def update_user
+  end
+
+  # Allows to make GET or POST requests to the facebook API graph
+  # the url parameter is the last part of the fb url excluding the https:/graph.facebook.com/
+  # It returns a dict from a JSON data structure.
+  #
+  # Note: You can also use open-uri for simple GET requests like this:
+  # obj = JSON.parse(open("https://graph.facebook.com/#{uid}?fields=id&access_token=#{access_token}").read)
+  # The problem is that in case
+  # of errors it raises a 400 HTTPBadRequest and it doesn't return any content of the body.
+  # Therefore, since the errors of a facebook request are inside the body of the response
+  # you couldn't get any fb errors from the simple function open.
+  def fb_api(url, access_token, attach = {}, request_type="GET")
+    uri = URI("https://graph.facebook.com/#{url}")
+
+    if request_type == "GET"
+      uri.query = URI.encode_www_form(attach.merge('access_token' => access_token))
+      req = Net::HTTP::Get.new uri.request_uri
+
+    elsif request_type == "POST"
+      req = Net::HTTP::Post.new(uri.path)
+      req.set_form_data(attach.merge('access_token' => access_token))
+    end
+
+    res = Net::HTTP.new(uri.host, uri.port)
+    res.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    res.use_ssl = true
+
+    response = nil
+    res.start do |http|
+      response = http.request(req)
+    end
+
+    return JSON.parse(response.read_body)
   end
 end
