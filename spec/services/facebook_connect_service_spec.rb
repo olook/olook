0515a8195bdb8fafc7b95d093d53ae4ddd5dc376
Luis Daher <luisotaviodaher@gmail.com>
2014-03-11 13:29:02 -0300

    WIP - facebook login service created

--- spec/services/facebook_connect_service_spec.rb
+++ spec/services/facebook_connect_service_spec.rb
@@ -1,4 +1,5 @@
-require File.expand_path(File.join(__dir__, 'app/service/facebook_connect_service'))
+require File.expand_path(File.join(__dir__, '../../app/services/facebook_connect_service'))
+# require File.expand_path(File.join(File.dirname(__FILE__), '../../app/services/cart_discount_service'))
 
 describe FacebookConnectService do
   describe "#connect!" do
