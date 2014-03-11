0aee900a53a88d5cfe7c3f37e0e96e27e10b1e8e
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-11 18:46:48 -0300

    Working login

--- app/controllers/users/facebook_connect_controller.rb
+++ app/controllers/users/facebook_connect_controller.rb
@@ -1,6 +1,12 @@
 class Users::FacebookConnectController < ApplicationController
+  include Devise::Controllers::InternalHelpers
   def create
-    @facebook_connect = FacebookConnectService.new(params[:facebookData], params[:authResponse])
-    render json: { redirectTo: request.referer }
+    @facebook_connect = FacebookConnectService.new(params[:authResponse])
+    if @facebook_connect.connect!
+      sign_in(@facebook_connect.user)
+      render json: { redirectTo: request.referer }
+    else
+      render json: { error: 'Failed to connect' }, status: :unprocessable_entity
+    end
   end
 end
