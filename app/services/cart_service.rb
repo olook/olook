3fa3a0dd343bc5b6b534ca805ab2f159bbb4b021
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-13 14:45:18 -0300

    Fix abacos integration bug

--- app/services/cart_service.rb
+++ app/services/cart_service.rb
@@ -153,7 +153,7 @@ class CartService
       :gift_wrap => cart.gift_wrap,
       :amount_discount => total_discount(payment),
       :amount_increase => total_increase,
-      :amount_paid => total(payment),
+      :amount_paid => total(payment: payment),
       :subtotal => subtotal,
       :user_first_name => user.first_name,
       :user_last_name => user.last_name,
