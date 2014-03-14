0376b6c3f939e7b63d58318a55a564ba8c04139b
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-14 16:53:55 -0300

    Change controller place for permissions works

--- app/controllers/admin/newest_reports_controller.rb
+++ app/controllers/admin/newest_reports_controller.rb
@@ -1,5 +1,5 @@
 # -*- encoding : utf-8 -*-
-class Admin::Orders::NewestReportsController < Admin::BaseController
+class Admin::NewestReportsController < Admin::BaseController
   def index
     @orders = Order.between.with_first_buyers
   end
