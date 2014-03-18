f82dffae58a4ad6d9103dd3ef132156a01a3f28c
Luis Daher <luisotaviodaher@gmail.com>
2014-03-18 18:40:01 -0300

    events created for carousel arrows on home

--- app/assets/javascripts/section/lite_home.js
+++ app/assets/javascripts/section/lite_home.js
@@ -2,12 +2,18 @@
 //= require jquerypp.custom
 //= require jquery.elastislide
 //= require plugins/change_picture_onhover
+//= require_tree ../modules/home
 
 
 initHome = function(){
   olook.carousel();
   olook.changePictureOnhover('.look_thumbnail');
   new ImageLoader().load('look_thumbnail');
+  new HomeEvents().config();
+  setTimeout(function(){
+    olookApp.publish('home:load');
+  }, 1000);
+
 };
 
 olook.carousel = function() {
@@ -15,3 +21,4 @@ olook.carousel = function() {
 }
 
 window.addEventListener('load', initHome);
+
