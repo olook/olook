f1f7db7d3cbdd3abc08c8e0d9f9d6e0fa9c5c790
Luis Daher <luisotaviodaher@gmail.com>
2014-03-25 18:45:03 -0300

    analytics event corrected

--- app/assets/javascripts/modules/facebook/auth/success.js
+++ app/assets/javascripts/modules/facebook/auth/success.js
@@ -7,7 +7,7 @@ var FacebookAuthSuccess = (function(){
 
   FacebookAuthSuccess.prototype.facade = function (result) {
     if(result.redirectTo){
-      _gaq.push(['_trackEvent', 'FacebookLogin', 'ClickSubmit', term]);
+      _gaq.push(['_trackEvent', 'FacebookLogin', 'ClickSubmit', true]);
       window.location = result.redirectTo;
     }
   };
