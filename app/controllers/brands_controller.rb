55a6d8a69b64e5cdf8dca4cad51552628a3b5a66
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-03-27 20:14:39 -0300

    Fix brand (maybe WIP)

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -4,21 +4,16 @@ class BrandsController < ApplicationController
   end
 
   def show
-    search_params = SeoUrl.parse(path: request.fullpath, path_positions: '/marcas/:brand:/:category:-:subcategory:/:care:_:color:_:size:_:heel:')
+    @url_builder = SeoUrl.new(path: request.fullpath, path_positions: '/marcas/:brand:/-:category::subcategory:-/-:care::color::size::heel:_')
     Rails.logger.debug("New params: #{params.inspect}")
 
-    @search = SearchEngine.new(search_params).for_page(params[:page]).with_limit(48)
+    @search = SearchEngine.new(@url_builder.parse_params).for_page(params[:page]).with_limit(48)
     @search.for_admin if current_admin
-
-    params.merge!(search_params)
+    @url_builder.set_search @search
 
     @brand = Brand.where(name:  params[:brand].to_s.split("-").map{|brand| ActiveSupport::Inflector.transliterate(brand).downcase.titleize})
     @antibounce_box = AntibounceBox.new(params) if @brand.any? && AntibounceBox.need_antibounce_box?(@search, @brand.map{|b| b.name.downcase}, params)
     @chaordic_user = ChaordicInfo.user(current_user,cookies[:ceid])
-    @url_builder = SeoUrl.new(path: request.fullpath, search: @search, path_positions: '/:brand:/:category:-:subcategory:/:care:_:color:_:size:_:heel:')
-    @url_builder.set_link_builder do |_path|
-      brand_path(_path)
-    end
   end
   private
     def title_text
