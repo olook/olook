51a54b66d155d224add90000940212283a30a4c3
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-27 11:17:03 -0300

    Fix admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,23 +4,15 @@ class Ability
     if admin.has_role? :superadministrator
       can :manage, :all
     else
-      can :manage, :visibility_batch
       admin.role.permissions.each do |permission|
         begin
-          if controllers_without_model.include?(permission.model_name)
-            can permission.action_name.to_sym, permission.model_name.gsub!(/(.)([A-Z])/,'\1_\2').downcase
-          else
-            can permission.action_name.to_sym, permission.model_name.constantize
-          end
+          can :manage, :visibility_batch
+          can permission.action_name.to_sym, permission.model_name.constantize
         rescue NameError => e
           Rails.logger.error("#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}")
           next
         end
       end
     end
-  end
-
-  def controllers_without_model
-    ["VisibilityBatch"]
   end
 end
