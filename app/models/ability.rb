70491a8ae654735bc0dfd80605d3ea77c6e86970
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-27 10:51:54 -0300

    Change ability for non restifull controllers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,14 +4,23 @@ class Ability
     if admin.has_role? :superadministrator
       can :manage, :all
     else
+      can :manage, :visibility_batch
       admin.role.permissions.each do |permission|
         begin
-          can permission.action_name.to_sym, permission.model_name.constantize
+          if controllers_without_model.include?(permission.model_name)
+            can permission.action_name.to_sym, permission.model_name.gsub!(/(.)([A-Z])/,'\1_\2').downcase
+          else
+            can permission.action_name.to_sym, permission.model_name.constantize
+          end
         rescue NameError => e
           Rails.logger.error("#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}")
           next
         end
       end
     end
+  end
+
+  def controllers_without_model
+    ["VisibilityBatch"]
   end
 end
