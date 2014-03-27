c0177c0404546e6ff5fbfb43c9c2a6480461c1ab
Rafael <rafael.manoel@gmail.com>
2014-03-27 17:42:57 -0300

    use a presenter and partials to improve code readability

---
+++ app/presenters/header_presenter.rb
@@ -0,0 +1,17 @@
+# -*- encoding : utf-8 -*-
+class HeaderPresenter < BasePresenter
+
+  #
+  # We use the type name to link to the template, if you need to create a new
+  # header, make sure to use a compatible name for your partial template, like:
+  #
+  # => NoBanner -> _no_banner.html.haml
+  # => BigBanner -> _big_banner.html.haml
+  #
+  def show_header 
+    _header = header || OpenStruct.new({type: "NoBanner"})
+    template_name =  _header.type.tableize.singularize.gsub("_catalog_header", "")
+    h.render partial: "headers/#{template_name}", locals: {header: header}
+  end
+
+end
