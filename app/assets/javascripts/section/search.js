500b6c2cc66ee23603a3ffc928d5f5906ace333c
George Domit <dommit@gmail.com>
2014-03-28 13:59:39 -0300

    wip spy on search

--- app/assets/javascripts/section/search.js
+++ app/assets/javascripts/section/search.js
@@ -1,6 +1,15 @@
 //= require plugins/change_picture_onhover
+//= require plugins/spy
+
+$(function(){
+  olook.init();
+  olook.spy('.spy');
+});
+
+o = olook;
 
 $(function(){
   new ImageLoader().load('look_thumbnail');
   olook.changePictureOnhover('.async');
+
 });
