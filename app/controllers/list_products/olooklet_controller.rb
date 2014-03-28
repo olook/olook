0889ce421b74b6aee70d8d3df8019b7eefec7b39
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-03-28 15:50:54 -0300

    Fix path position

--- app/controllers/list_products/olooklet_controller.rb
+++ app/controllers/list_products/olooklet_controller.rb
@@ -3,9 +3,8 @@ class ListProducts::OlookletController < ListProductsController
   @url_prefix = '/olooklet'
 
   def index
-    @path_positions = '/olooklet/:category:-:brand:-:subcategory:/:care:_:color:_:size:_:heel:'
+    @path_positions = '/olooklet/-:category::brand::subcategory:-/-:care::color::size::heel:_'
     visibility = params[:visibility] || "#{Product::PRODUCT_VISIBILITY[:olooklet]}-#{Product::PRODUCT_VISIBILITY[:all]}"
-    request_path = params[:path] || request.fullpath
     search_params = SeoUrl.parse(path: request.fullpath, path_positions: @path_positions).merge({visibility: visibility})
     default_params(search_params,"olooklet")
     @url_builder.set_link_builder { |_param| olooklet_path(_param) }
