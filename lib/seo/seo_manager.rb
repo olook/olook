bc7f1d53e1df1f0aebf9dfde3a2eb86355e32de7
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-03 10:36:59 -0300

    Prepare url to prevent space between words errors

--- lib/seo/seo_manager.rb
+++ lib/seo/seo_manager.rb
@@ -7,7 +7,7 @@ module Seo
     attr_accessor :url, :page_title, :page_description
 
     def initialize url, options={}
-      @url = url
+      @url = URI.decode(url).gsub(" ", "-").downcase
     end
 
     def select_meta_tag
@@ -20,29 +20,6 @@ module Seo
         title = search_meta_tag[:title] || DEFAULT_PAGE_TITLE
         description = search_meta_tag[:description] || DEFAULT_PAGE_DESCRIPTION
         {title: "#{title} | Olook" , description: description}
-      end
-
-      def extract_meta_tag_text
-      end
-
-      def category_meta_tag
-        subcategory_name = extract_subcategories unless subcategory.blank?
-        category_name = category.first unless category.blank?
-        return "#{subcategory_name} #{color_formatted}" if !subcategory.blank? && !color.blank?
-        return "#{subcategory_name}" unless subcategory.blank?
-        if category && category_name
-          return "#{CATEGORY_TEXT[category_name.downcase.to_sym]} #{color_formatted}" unless color.blank?
-          return "#{CATEGORY_TEXT[category_name.downcase.to_sym]}" 
-        end
-        ""
-      end
-
-      def extract_subcategories
-       if subcategory.size > 3
-         "#{subcategory.map(&:capitalize).first(3).join(" - ")} e outros"
-       else
-         subcategory.map(&:capitalize).join(" - ")
-       end
       end
 
       def search_meta_tag
