ae8682c0e10c7db8940d4521797a8b46e98af283
darkseid <rafael.manoel@gmail.com>
2014-04-09 13:35:15 -0300

    create a mkt_settings

---
+++ app/models/mkt_setting.rb
@@ -0,0 +1,5 @@
+class MktSetting < RailsSettings::CachedSettings
+  self.table_name = 'mkt_settings'
+
+	attr_accessible :var
+end
---
+++ db/migrate/20140409134110_create_settings.rb
@@ -0,0 +1,17 @@
+class CreateSettings < ActiveRecord::Migration
+  def self.up
+    create_table :mkt_settings do |t|
+      t.string :var, :null => false
+      t.text   :value, :null => true
+      t.integer :thing_id, :null => true
+      t.string :thing_type, :limit => 30, :null => true
+      t.timestamps
+    end
+    
+    add_index :mkt_settings, [ :thing_type, :thing_id, :var ], :unique => true
+  end
+
+  def self.down
+    drop_table :mkt_settings
+  end
+end
