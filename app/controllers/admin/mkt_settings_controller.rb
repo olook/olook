0334ee1f679d543bebfc22aa59bb98730629baca
darkseid <rafael.manoel@gmail.com>
2014-04-09 13:35:15 -0300

    create a mkt_settings

---
+++ app/controllers/admin/mkt_settings_controller.rb
@@ -0,0 +1,19 @@
+#encoding: utf-8
+class Admin::MktSettingsController < Admin::BaseController
+  load_and_authorize_resource
+
+  def show
+    @settings = MktSetting.all
+  end
+
+  def create
+    settings = params[:settings]
+    settings.each do |key, value|
+
+      # value = convert_to_appropriate_object(value)
+
+      MktSetting.send("#{key}=", value)
+    end
+    redirect_to :admin_settings, :notice => "Configurações atualizadas com successo"
+  end
+end
