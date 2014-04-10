39aed415c74b70fc494e9d20abf0366c7b5b9c73
Rafael <rafael.manoel@gmail.com>
2014-04-10 17:11:54 -0300

    add stats tracking

--- app/models/facebook_highlight.rb
+++ app/models/facebook_highlight.rb
@@ -1,9 +1,9 @@
 class FacebookHighlight
+	MAX_SIZE = 2
 
 	def self.products
 		product_ids = MktSetting.facebook_products
-		search = SearchEngine.new(product_id: product_ids).with_limit(1000)
-    search.products
+		search = SearchEngine.new(product_id: product_ids).with_limit(MAX_SIZE).products
 	end
 
 end
