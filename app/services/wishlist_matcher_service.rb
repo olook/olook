76afdc1f59f6bca599789c1628e357e43039af49
Luis Daher <luisotaviodaher@gmail.com>
2014-04-10 15:31:30 -0300

    fixes for modal showing

--- app/services/wishlist_matcher_service.rb
+++ app/services/wishlist_matcher_service.rb
@@ -1,9 +1,13 @@
 class WishlistMatcherService
-  def matches? wishlist
-    (product_id_array_for(wishlist) & matching_product_array).size == 3
+  def matches?(wishlist, variant_id)
+    ((product_id_array_for(wishlist) & matching_product_array).size == 3) && matching_product_array.include?(product_id_for(variant_id))
   end
 
   private
+
+  def product_id_for variant_id
+    Variant.find(variant_id).product_id.to_s
+  end
 
   def matching_product_array
     Setting.bunny_products.split(",")
