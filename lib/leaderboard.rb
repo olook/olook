aef64244fa6f81463a3d97f55a7a9f73d9f792f6
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-10 17:51:00 -0300

    Adding worker

--- lib/leaderboard.rb
+++ lib/leaderboard.rb
@@ -1,4 +1,11 @@
 class Leaderboard
+  PREFIX = 'leaderboard/'
+  def self.clear
+    redis = Redis.new(url: ENV['REDIS_LEADERBOARD'])
+    keys = redis.keys("#{PREFIX}*")
+    redis.del(*keys)
+  end
+
   def initialize(options={})
     @redis = options.delete(:redis) || Redis.new(url: ENV['REDIS_LEADERBOARD'])
     @key = options.delete(:key)
@@ -13,11 +20,11 @@ class Leaderboard
 
   def score(id)
     @keys.each do |key|
-      @redis.zincrby(key, 1, id)
+      @redis.zincrby("#{PREFIX}#{key}", 1, id)
     end
   end
 
   def rank(limit)
-    @redis.zrevrange(@key, 0, limit - 1)
+    @redis.zrevrange("#{PREFIX}#{@key}", 0, limit - 1)
   end
 end
