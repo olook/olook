4879308b9140f138efb10e42cc0f54de48eca7d2
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-15 08:22:40 -0300

    Add sitemap view, controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ require 'resque/server'
 # -*- encoding : utf-8 -*-
 Olook::Application.routes.draw do
 
+  get "sitemap/index"
+
   resources :wished_products, only: [:create, :destroy]
 
   get "/wishlist_matcher", to: 'wishlist_matcher#index', as: 'wishlist_matcher'
