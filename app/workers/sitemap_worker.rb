bc7d9de6343579f9d83e19d45f8f1d02dc37fd5d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-16 19:30:42 -0300

    on brand search, use products with inventory

--- app/workers/sitemap_worker.rb
+++ app/workers/sitemap_worker.rb
@@ -3,7 +3,7 @@ class SitemapWorker
 
   def self.perform
     sitemap = {
-      brands: Product.only_visible.pluck(:brand).uniq.compact.reject! { |c| c.empty? },
+      brands: Product.only_visible.joins(:variants).where("variants.inventory >= 1").pluck(:brand).uniq.compact.reject! { |c| c.empty? },
       collection_themes: CollectionTheme.active.all.map{|a| [a.name, a.slug]},
       shoes: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 1, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq,
       accessories: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 3, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq,
