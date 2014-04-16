5ef68aefcbe6805fa5a2a80c451ad0446f31179a
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-16 13:08:46 -0300

    Add worker to fetch sections info

---
+++ app/workers/sitemap_worker.rb
@@ -0,0 +1,20 @@
+class SitemapWorker
+  @queue = 'low'
+
+  def self.perform
+    sitemap = {
+      brands: Product.only_visible.pluck(:brand).uniq.compact.reject! { |c| c.empty? },
+      collection_themes: CollectionTheme.active.all.map{|a| [a.name, a.slug]},
+      shoes: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 1, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq,
+      accessories: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 3, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq,
+      bags: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 2, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq,
+      cloths: Detail.where(translation_token: 'Categoria').joins(:product).where(products: {category: 4, is_visible: true}).pluck(:description).map{|b| b.parameterize}.uniq
+    }
+    redis.set("sitemap", sitemap.to_json)
+  end
+
+  def self.redis
+    Redis.new
+  end
+end
+
