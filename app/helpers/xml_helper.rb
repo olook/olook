eedc95e9e822a9325495a9a271afdead7d45f344
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-04-24 17:03:52 -0300

    Adding new categorization on ilove_categories

--- app/helpers/xml_helper.rb
+++ app/helpers/xml_helper.rb
@@ -32,7 +32,7 @@ module XmlHelper
   end
 
   def get_ilove_category_for product
-    subcategory_in_downcase = product.subcategory_name.nil? ? nil : product.subcategory_name.downcase
+    subcategory_in_downcase = product.subcategory_name.nil? ? nil : product.subcategory_name.parameterize
     ilove_categories[product.category][subcategory_in_downcase]
   end
 
@@ -58,28 +58,74 @@ module XmlHelper
     def ilove_categories
 
       shoe_subcategories = {
-        'sandália' => 14,
-        'bota' => 13
+        'sandalia' => 14,
+        'bota' => 13,
+        'flats' => 164,
+        'tenis' => 15,
       }
       shoe_subcategories.default=12
 
       bag_subcategories={}
       bag_subcategories.default=18
 
-      accessory_subcategories={}
+      accessory_subcategories={
+        'anel' => 82,
+        'pulseiras' => 60,
+        'colar' => 80,
+        'brinco' => 81,
+        'cachecol' => 123,
+        'chapeu' => 122,
+        'cinto' => 121,
+        'relogio' => 124
+      }
       accessory_subcategories.default=17
 
-      cloth_subcategories={
+      cloth_subcategories = {
         'saia' => 9,
-        'shorts' => 9,
-        'calça' => 8,
+        'saias-midi' => 102,
+        'saias-mini' => 101,
+        'saias-longas' => 103,
+        'shorts' => 104,
         'vestido' => 5,
+        'vestidos-curto' => 86,
+        'vestidos-longos' => 85,
+        'vestidos-de-festa' => 87,
+        'blusa' => 6,
+        'regatas' => 88,
+        'camisas' => 89,
+        't-shirts' => 90,
+        'tops' => 91,
         'casaco' => 7,
-        'jaqueta' => 7,
-        'blusa' => 6
+        'blazer' => 92,
+        'cardiga' => 93,
+        'jaquetas' => 94,
+        'coletes' => 95,
+        'calça' => 8,
+        'cropped' => 96,
+        'flare' => 97,
+        'jeans' => 98,
+        'legging' => 99,
+        'skinny' => 100
       }
 
-      cloth_subcategories.default=4
+      cloth_subcategories.default = 4
+
+      lingerie_subcategories = {
+        'basica'=>107,
+        'sutia'=>105,
+        'calcinha'=>106,
+        'renda'=>108
+      }
+
+      lingerie_subcategories.default = 10
+
+      beachwear_subcategories = {
+        'biquini' => 109,
+        'maio' => 110,
+        'workout' => 111,
+        'saida-de-praia' => 112
+      }
+      beachwear_subcategories.default = 11
 
       @ilove_categories ||= {
         Category::SHOE       => shoe_subcategories,
@@ -87,7 +133,7 @@ module XmlHelper
         Category::ACCESSORY  => accessory_subcategories,
         Category::CLOTH  => cloth_subcategories,
         Category::LINGERIE  => cloth_subcategories,
-        Category::BEACHWEAR => cloth_subcategories,
+        Category::BEACHWEAR => beachwear_subcategories,
         Category::CURVES => cloth_subcategories
       }
     end
