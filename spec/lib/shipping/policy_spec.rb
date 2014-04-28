3166021e9821f6403f08706e61dab730f3c1e0cc
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-28 13:59:52 -0300

    Refactor of shipping policy

--- spec/lib/shipping/policy_spec.rb
+++ spec/lib/shipping/policy_spec.rb
@@ -4,23 +4,26 @@ describe Shipping::Policy, focus: true do
     @default_amount = "89.9"
   end
   context "#free_shipping?" do
-    context "when dont receive params" do
-      it "return false" do
-        expect(subject.free_shipping?).to be_false
-      end
+    context "when it doesn`t receive the params" do
+      it{expect(subject.free_shipping?).to be_false}
     end
-    context "when dont find model" do
-      it "return false" do
-        expect(described_class.new(@default_zip_code, @default_amount).free_shipping?).to be_false
-      end
+    context "when it doesn`t find a policy" do
+      it{ expect(described_class.new(@default_zip_code, @default_amount).free_shipping?).to be_false}
     end
-    context "When found policy for cep" do
-      before do
-        @policy = described_class.new(@default_zip_code, @default_amount)
-        @policy.should_receive(:seek_policy).and_return([mock(:shipping_policy)])
+    context "When a policy is found" do
+      context "but the amount is less than minimum for free shipping" do
+        before do
+          @policy = described_class.new(@default_zip_code, @default_amount)
+          @policy.should_receive(:has_free_shipping?).and_return(false)
+        end
+        it{  expect(@policy.free_shipping?).to be_false }
       end
-      it "return true" do
-        expect(@policy.free_shipping?).to be_true
+      context "for a given zip_code" do
+        before do
+          @policy = described_class.new(@default_zip_code, @default_amount)
+          @policy.should_receive(:has_free_shipping?).and_return(true)
+        end
+        it{ expect(@policy.free_shipping?).to be_true }
       end
     end
   end
