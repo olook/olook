1055378b879d4b6d04364301fa9161ca75c76407
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-28 11:21:11 -0300

    Add scopes to shipping policy

--- app/models/shipping_policy.rb
+++ app/models/shipping_policy.rb
@@ -1,3 +1,5 @@
 class ShippingPolicy < ActiveRecord::Base
   attr_accessible :value_end, :value_start, :zip_end, :zip_start
+  scope :zip_code_between, ->(zip) {where('(:zip >= zip_start) AND (:zip <= zip_end)', zip: zip)}
+  scope :with_amount, ->(amount) {where('(:amount >= value_start) AND (:amount <= value_end)', amount: amount)}
 end
