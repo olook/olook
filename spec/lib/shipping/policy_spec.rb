417a66e5f28683db3670f40c8fad5e784a0d6ee3
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-28 13:10:11 -0300

    Change method name for a better one

--- spec/lib/shipping/policy_spec.rb
+++ spec/lib/shipping/policy_spec.rb
@@ -2,17 +2,16 @@ describe Shipping::Policy, focus: true do
   before do
     @default_zip_code = "05302030"
     @default_amount = "89.9"
-    # ShippingPolicy.should_receive(:find).with()
   end
-  context "#perform" do
+  context "#free_shipping?" do
     context "when dont receive params" do
       it "return false" do
-        expect(subject.perform).to be_false
+        expect(subject.free_shipping?).to be_false
       end
     end
     context "when dont find model" do
       it "return false" do
-        expect(described_class.new(@default_zip_code, @default_amount).perform).to be_false
+        expect(described_class.new(@default_zip_code, @default_amount).free_shipping?).to be_false
       end
     end
     context "When found policy for cep" do
@@ -21,7 +20,7 @@ describe Shipping::Policy, focus: true do
         @policy.should_receive(:seek_policy).and_return([mock(:shipping_policy)])
       end
       it "return true" do
-        expect(@policy.perform).to be_true
+        expect(@policy.free_shipping?).to be_true
       end
     end
   end
