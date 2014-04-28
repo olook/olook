e13e18ec9ae0330fa6d143a50b8467efe2bfa7f8
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-28 13:59:52 -0300

    Refactor of shipping policy

--- lib/shipping/policy.rb
+++ lib/shipping/policy.rb
@@ -6,12 +6,21 @@ class Shipping::Policy
   end
 
   def free_shipping?
-    return false unless zip || amount
-    return false if seek_policy.blank?
-    true
+    return true if has_params? && has_free_shipping?
   end
 
-  def seek_policy
-    ShippingPolicy.zip_code_between(zip).with_amount(amount)
+  private
+
+  def has_free_shipping?
+    ship = ShippingPolicy.zip_code_between(zip).first
+    ship && amount_bigger_than_current_shipping?(ship)
+  end
+
+  def amount_bigger_than_current_shipping? shipping_policy
+    amount.to_d >= shipping_policy.free_shipping.to_d
+  end
+
+  def has_params?
+    zip && amount
   end
 end
