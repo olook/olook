7456bb962de469401e3db3fc58219ec81130bf9a
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-04-29 09:58:28 -0300

    Remove spaces

--- app/services/shipping_importer_service.rb
+++ app/services/shipping_importer_service.rb
@@ -24,18 +24,18 @@ class ShippingImporterService
     while values.size > 0 do
       Shipping.connection.insert(insert_query_prefix + values.join(','))
       values = query_entries.shift(1000)
-    end        
+    end
   end
 
   def insert_query_prefix
-    "INSERT INTO `#{Shipping.table_name}` (`created_at`, `updated_at`, `zip_start`, `zip_end`, `delivery_time`, `income`, `cost`, `shipping_service_id`) VALUES "    
+    "INSERT INTO `#{Shipping.table_name}` (`created_at`, `updated_at`, `zip_start`, `zip_end`, `delivery_time`, `income`, `cost`, `shipping_service_id`) VALUES "
   end
 
   def transform_array_into_query_entries csv_array
     now = Time.zone.now.utc.iso8601.gsub(/T/, ' ').gsub('Z', '')
     csv_array.each.map do |row| 
       ( "('%s', '%s', '%s', '%s', %d, %0.2f, %0.2f, %d )" % [now, now, row[1],row[2],row[5], parse_float(row[6]),parse_float(row[7]), @shipping_service.id] ) rescue nil
-    end    
+    end
   end
 
   def delete_previous_shippings
