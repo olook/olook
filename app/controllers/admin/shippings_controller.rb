7f12dc21e5c3d42fb2373aed1b522666ecd73f66
Luis Daher <luisotaviodaher@gmail.com>
2014-04-29 11:02:43 -0300

    generalizing the csv parsing service

--- app/controllers/admin/shippings_controller.rb
+++ app/controllers/admin/shippings_controller.rb
@@ -4,7 +4,7 @@ class Admin::ShippingsController < Admin::BaseController
     if params[:shipping_file]
       temp_file_uploader = TempFileUploader.new
       temp_file_uploader.store!(params[:shipping_file])
-      ShippingImporterService.new(temp_file_uploader.filename).process_file
+      ShippingImporterService.new(temp_file_uploader.filename, ShippingParserService.new).process_file
     end
     redirect_to :back
   end
--- app/services/shipping_importer_service.rb
+++ app/services/shipping_importer_service.rb
@@ -1,7 +1,9 @@
 class ShippingImporterService
 
-  def initialize filename
+
+  def initialize(filename, parser)
     @filename = filename
+    @parser = parser
   end
 
   def process_file
@@ -12,39 +14,19 @@ class ShippingImporterService
 
   def import_csv
     csv_array = load_csv
-    @shipping_service = find_shipping_service(csv_array.first[0])
-    delete_previous_shippings
+    @parser.prepare csv_array
+    ActiveRecord::Base.connection.delete @parser.delete_query
     save_all csv_array
   end
 
   def save_all csv_array
-    query_entries = transform_array_into_query_entries(csv_array)
+    query_entries = @parser.transform_array_into_query_entries(csv_array)
     values = query_entries.shift(1000)
 
     while values.size > 0 do
-      Shipping.connection.insert(insert_query_prefix + values.join(','))
+      ActiveRecord::Base.connection.insert(@parser.insert_prefix + values.join(','))
       values = query_entries.shift(1000)
     end
-  end
-
-  def insert_query_prefix
-    "INSERT INTO `#{Shipping.table_name}` (`created_at`, `updated_at`, `zip_start`, `zip_end`, `delivery_time`, `income`, `cost`, `shipping_service_id`) VALUES "
-  end
-
-  def transform_array_into_query_entries csv_array
-    now = Time.zone.now.utc.iso8601.gsub(/T/, ' ').gsub('Z', '')
-    csv_array.each.map do |row| 
-      ( "('%s', '%s', '%s', '%s', %d, %0.2f, %0.2f, %d )" % [now, now, row[1],row[2],row[5], parse_float(row[6]),parse_float(row[7]), @shipping_service.id] ) rescue nil
-    end
-  end
-
-  def delete_previous_shippings
-    Shipping.connection.delete("DELETE FROM `#{Shipping.table_name}` WHERE shipping_service_id = #{@shipping_service.id}")
-  end
-
-  def find_shipping_service name
-    shipping_service_name = name.upcase
-    ShippingService.find_or_create_by_name(shipping_service_name)
   end
 
   def load_csv
@@ -54,7 +36,4 @@ class ShippingImporterService
     CSV.read(temp_file_uploader.file.path, {:col_sep => ';'})[1 .. -1]
   end
 
-  def parse_float float_value
-    BigDecimal.new(float_value.gsub(/[^\d,\.]/, '').gsub(',','.'), 10)
-  end  
 end
