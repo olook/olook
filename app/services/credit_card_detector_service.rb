9b6cb23e661c764d6d95b568ecb3fda8533b2fea
Luis Daher <luisotaviodaher@gmail.com>
2014-05-01 15:59:57 -0300

    added fallback behavior for credit card type

--- app/services/credit_card_detector_service.rb
+++ app/services/credit_card_detector_service.rb
@@ -3,6 +3,6 @@ class CreditCardDetectorService
   def self.detect credit_card_number
     return "Visa" if credit_card_number =~ /^4/
     return "Mastercard" if credit_card_number =~ /^5[1-5]/
-    return "Amex" if credit_card_number =~ /^3[47]/
+    return "Diners" if credit_card_number =~ /^3(?:0[0-5]|[68][0-9])/
   end
 end
