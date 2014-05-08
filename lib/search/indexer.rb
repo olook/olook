7432a48abddf08891ae95375513fb03bdce1bfb3
Luis Daher <luisotaviodaher@gmail.com>
2014-05-08 18:15:54 -0300

    major refactoring

---
+++ lib/search/indexer.rb
@@ -0,0 +1,78 @@
+module Search
+  class Indexer
+    extend Fixes
+    SEARCH_CONFIG = YAML.load_file("#{Rails.root}/config/cloud_search.yml")[Rails.env]    
+
+    attr_reader :entities, :adapter
+
+    def initialize(entities, adapter)
+      @entities = entities
+      @adapter = adapter
+    end
+
+    def index
+      add_documents
+      remove_documents  
+    end
+
+    private
+
+      def flush_to_sdf_file file_name, all_documents
+        File.open("#{file_name}", "w:UTF-8") do |file|
+          file << all_documents.to_json
+        end
+      end
+
+      def entities_to_index(entity_ids)
+        _products = Product.where(id: entity_ids).where('price > 0').includes(:pictures, :variants, :details, :collection, :collection_themes).all
+        _products.select! {|p| p.main_picture && p.main_picture[:image] }
+        _products
+      end
+
+      def entities_to_remove(entity_ids)
+        _products = Product.where(price: 0).where(id: entity_ids).includes(:pictures).all
+        _products.select! {|p| p.main_picture.nil? || p.main_picture[:image].nil? }
+        _products
+      end
+
+      def create_sdf_entry_for(entity, type)
+        document = @adapter.adapt(entity,type)
+        document.to_document    
+      end
+
+      def upload_sdf_file file_name
+        docs_domain =  SEARCH_CONFIG["docs_domain"]
+        `curl -s -X POST --upload-file "#{file_name}" "#{docs_domain}"/2011-02-01/documents/batch --header "Content-Type:application/json"`
+      end
+
+      def add_documents
+        entities.each_slice(500).with_index do |slice, index|
+          run slice, index
+        end
+      end
+
+      def remove_documents
+        documents = entities_to_remove(entities).map { |entity| create_sdf_entry_for(entity, 'delete') }.compact
+        flush_to_sdf_file "/tmp/base-remove.sdf", documents
+        upload_sdf_file "/tmp/base-remove.sdf"
+      end
+
+      def run(slice, index)
+        begin
+          entries = entities_to_index(slice).map do |entities|
+            create_sdf_entry_for(entities, 'add')
+          end
+          entries.compact!
+          file_name = "/tmp/base-add#{'%03d' % index}.sdf"
+          flush_to_sdf_file file_name, entries
+          upload_sdf_file file_name
+        rescue => e
+          opts = {
+            body: "Falha ao gerar o arquivo para indexacao: #{index}-add<br> #{e} <br> #{e.backtrace}",
+            to: "tech@olook.com.br",
+            subject: "Falha ao rodar a indexacao de produtos"}
+          DevAlertMailer.notify(opts).deliver
+        end
+      end
+  end
+end
