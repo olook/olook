1ab106d115db403ae0cea0829f9e7fe324fada7c
Luis Daher <luisotaviodaher@gmail.com>
2014-05-09 15:37:02 -0300

    refactoring ranking calculator

--- lib/search/ranking_calculator.rb
+++ lib/search/ranking_calculator.rb
@@ -10,6 +10,7 @@ class RankingCalculator
     @age_weight ||= Setting[:age_weight].to_i
     @max_age_rating ||= ( @age_weight * RankingCalculator::RANKING_POWER )
     @inventory_weight ||= Setting[:inventory_weight].to_i
+    initialize_count_by_category
   end
 
   def newest
@@ -26,37 +27,60 @@ class RankingCalculator
 
   def save_temporary_table_vars
     create_temporary_products_with_inventory_table do
-      count = Product.connection.select_all("SELECT count(0) qty FROM products_with_more_than_one_inventory WHERE age < #{DAYS_TO_CONSIDER_OLD}").first['qty']
-      third_quartile = ( count * 0.75 ).round
-
-      @newest = Product.connection.select("SELECT age FROM products_with_more_than_one_inventory WHERE age < #{DAYS_TO_CONSIDER_OLD} ORDER BY age desc LIMIT 1 OFFSET #{third_quartile}").first['age']
-
-      count_by_category ||= Product.connection.
-        select_all("SELECT category, count(0) `count` from products_with_more_than_one_inventory GROUP BY category").
-        inject({}) {|h,r| h[r['category']] = r['count']; h }
-
-      @third_quartile_inventory ||= count_by_category.inject({}) do |hash, aux|
-        category, count = aux
-        third_quartile = ( count*0.75 ).round
-        hash[category] = Product.connection.
-          select("SELECT sum_inventory FROM products_with_more_than_one_inventory
-                   WHERE category = #{category} order by sum_inventory limit 1 offset #{third_quartile}").
-          first['sum_inventory']
-        hash
-      end
+      initialize_newest
+      initialize_third_quartile_inventory
     end
   end
 
   def create_temporary_products_with_inventory_table
     begin
+      drop_existent_temporary_table
+      create_temporary_table
+      yield
+    ensure
+      drop_existent_temporary_table
+    end
+  end
+
+  private
+
+    def initialize_third_quartile_inventory
+      count = valid_products_count 
+
+      third_quartile = ( count * 0.75 ).round
+      @third_quartile_inventory ||= count_by_category.inject({}) do |hash, aux|
+        category, count = aux
+        hash[category] = Product.connection.
+          select("SELECT sum_inventory FROM products_with_more_than_one_inventory
+                   WHERE category = #{category} order by sum_inventory limit 1 offset #{third_quartile}").
+          first['sum_inventory']
+        hash
+      end        
+    end
+
+    def initialize_count_by_category
+      @count_by_category ||= Product.connection.
+        select_all("SELECT category, count(0) `count` from products_with_more_than_one_inventory GROUP BY category").
+        inject({}) {|h,r| h[r['category']] = r['count']; h }
+    end
+
+    def valid_products_count
+      Product.connection.select_all("SELECT count(0) qty FROM products_with_more_than_one_inventory WHERE age < #{DAYS_TO_CONSIDER_OLD}").first['qty']      
+    end
+
+    def initialize_newest
+      @newest = Product.connection.select("SELECT age FROM products_with_more_than_one_inventory WHERE age < #{DAYS_TO_CONSIDER_OLD} ORDER BY age desc LIMIT 1 OFFSET #{third_quartile}").first['age']      
+    end
+
+    def drop_existent_temporary_table
+      Product.connection.execute('DROP TEMPORARY TABLE IF EXISTS products_with_more_than_one_inventory')
+    end
+
+    def create_temporary_table
       sql = Product.only_visible.joins(:variants).group('products.id').having('sum(inventory) > 0').
         select('products.id, IF(products.launch_date = NULL, 365, CURDATE() - products.launch_date) age,
                products.category, sum(variants.inventory) sum_inventory').to_sql
-        Product.connection.execute('DROP TEMPORARY TABLE IF EXISTS products_with_more_than_one_inventory')
-        Product.connection.execute("CREATE TEMPORARY TABLE products_with_more_than_one_inventory AS #{sql}")
-        yield
-    ensure
-      Product.connection.execute('DROP TEMPORARY TABLE IF EXISTS products_with_more_than_one_inventory')
+
+      Product.connection.execute("CREATE TEMPORARY TABLE products_with_more_than_one_inventory AS #{sql}")      
     end
-  end   
 end
