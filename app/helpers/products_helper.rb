c8fc885bf3e579b43a5135940ea0d2aa7f07e069
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-12 12:56:59 -0300

    refactor for modify_meta_description method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -84,8 +84,8 @@ module ProductsHelper
 
   def modify_meta_description description, color
     _description = description || ""
-    _description.concat(" Cor #{color}") unless color == "Não informado" || color.blank?
-    _description.concat(". #{sort_words}")
+    _description = "#{_description} Cor #{color}" unless color == "Não informado" || color.blank?
+    "#{_description}. #{sort_words}"
   end
 
   def sort_words
