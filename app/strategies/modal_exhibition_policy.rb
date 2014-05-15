3b0b33d305debf3225a4dc61fb472ee25bf5a3b1
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-15 17:34:36 -0300

    Refactor of modal_exhibition_policy

--- app/strategies/modal_exhibition_policy.rb
+++ app/strategies/modal_exhibition_policy.rb
@@ -1,7 +1,17 @@
 class ModalExhibitionPolicy
   BLACK_LIST = /(sacola|pagamento)/
 
-  def self.apply?(path,cookie)
-    path !~ BLACK_LIST && cookie.blank?
+  def self.apply?(opts={})
+     avaliable_path?(opts[:path]) && without_cookie?(opts[:cookie]) && opts[:user].blank?
+  end
+
+  private
+
+  def self.avaliable_path?(path)
+    path !~ BLACK_LIST
+  end
+
+  def self.without_cookie?(cookie)
+    cookie.blank?
   end
 end
