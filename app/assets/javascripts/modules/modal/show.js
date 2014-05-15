d252362aeaabf16c5ba46d85801378b93054fe80
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-15 17:31:52 -0300

    Remove publish from config

--- app/assets/javascripts/modules/modal/show.js
+++ app/assets/javascripts/modules/modal/show.js
@@ -2,14 +2,13 @@ var ModalShow = (function(){
   function ModalShow() {};
   ModalShow.prototype.config = function(){
     olookApp.subscribe('modal:show', this.facade);
-    olookApp.publish('modal:show', document.URL);
   };
 
   ModalShow.prototype.facade = function(path){
     $.get("/modal", {path: path}).done(function(data){
-      olook.newModal(data, 504, 610, '#fff');
+      olook.newModal(data.html, data.width, date.height, data.color);
     }).fail(function() {
-      console.log("falhou")
+      olookApp.publish("modal:error");
     });
   };
   return ModalShow
