dcf335e8ebad0300fe23cae7978bd5c7b9edb61d
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-15 13:41:17 -0300

    Add modal ab test

--- app/controllers/modal_controller.rb
+++ app/controllers/modal_controller.rb
@@ -3,7 +3,9 @@ class ModalController < ApplicationController
   def show
     if ModalExhibitionPolicy.apply?(params["path"], cookies[:sm])
       set_modal_cookie if cookies[:sm].blank?
-      render "modal/show"
+      variation = ab_test('modal', 'modal1', 'modal2') 
+      render "modal/show1" if variation == 'modal1'
+      render "modal/show2" if variation == 'modal2'
     else
       render status: 401
     end
