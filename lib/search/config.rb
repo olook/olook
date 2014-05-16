ac4b20887fec9771e2736c817a3ca7956383e5df
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-16 18:05:44 -0300

    Adding config

---
+++ lib/search/config.rb
@@ -0,0 +1,30 @@
+module Search
+  def self.config &block
+    if block
+      block.call(Search::Config)
+    else
+      Search::Config
+    end
+  end
+end
+
+module Search
+  class Config
+    @@config = {}
+    def self.[]=(key, val)
+      @@config[key.to_s] = val
+    end
+
+    def self.[](key)
+      @@config[key.to_s]
+    end
+
+    def self.method_missing(m, *args, &block)
+      if /(?<met_name>\w+)=/ =~ m.to_s
+        @@config[met_name] = args.first
+      else
+        @@config[m.to_s]
+      end
+    end
+  end
+end
