23eac61a309ad2f31d539c93dd06e0eedaea6439
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-16 15:45:12 -0300

    Tests on modal

--- app/assets/javascripts/modules/modal/show.js
+++ app/assets/javascripts/modules/modal/show.js
@@ -1,15 +1,14 @@
 var ModalShow = (function(){
   function ModalShow() {};
   ModalShow.prototype.config = function(){
-    olookApp.subscribe('modal:show', this.facade);
+    olookApp.subscribe('modal:show', this.facade, {}, this);
   };
 
-  ModalShow.prototype.facade = function(path){
-    $.get("/modal", {path: path}).done(function(data){
-      olook.newModal(data.html, data.width, data.height, data.color);
-    }).fail(function() {
-      olookApp.publish("modal:error");
+  ModalShow.prototype.facade = function(data){
+    var start = new Date();
+    olook.newModal(data.html, data.width, data.height, data.color, function(){
+      _gaq.push(['_trackEvent', 'Modal', 'Close', data.name, ( new Date() - start )], true);
     });
   };
-  return ModalShow
+  return ModalShow;
 })();
--- app/controllers/modal_controller.rb
+++ app/controllers/modal_controller.rb
@@ -6,9 +6,9 @@ class ModalController < ApplicationController
       set_modal_cookie if cookies[:sm].blank?
       variation = ab_test('acquisition_popup_test', 'facebook', 'email')
       if variation == 'facebook'
-        render json: {html: render_to_string(partial: 'show1.html.erb'), width: "504",height: "610", color: "#fff" }
+        render json: {html: render_to_string(partial: 'show1.html.erb'), name: variation, width: "504",height: "610", color: "#fff" }
       else
-        render json: {html: render_to_string(partial: 'show2.html.erb'), width: "504",height: "610", color: "#fff" }
+        render json: {html: render_to_string(partial: 'show2.html.erb'), name: variation, width: "504",height: "610", color: "#fff" }
       end
     else
       render text: "",status: 401
