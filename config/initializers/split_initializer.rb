0097adf896d37ddaec8a8cbb764065b265405a97
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-19 12:52:17 -0300

    Remove multiple ab tests

--- config/initializers/split_initializer.rb
+++ config/initializers/split_initializer.rb
@@ -3,7 +3,3 @@ Split.redis = ENV['REDIS_CACHE_STORE']
 Split::Dashboard.use Rack::Auth::Basic do |username, password|
   username == 'admin' && password == 'olook123abc'
 end
-
-Split.configure do |config|
-  config.allow_multiple_experiments = true
-end
