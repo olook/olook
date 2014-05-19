a6a522a82d75cb74ee3cbedd681beb1741957b24
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-19 15:46:11 -0300

    Replicating 2013 files

---
+++ lib/search/query/v20130101/facets.rb
@@ -0,0 +1,29 @@
+module Search
+  module Query
+    module V20130101
+      class Facets
+        def initialize(fields=[])
+          @facets = fields
+          @top = {}
+        end
+
+        def set_top_for(field, top)
+          @top[field] = top
+        end
+
+        def <<(facet)
+          @facets << facet
+        end
+
+        def query_url
+          return if @facets.size == 0
+          param = ["facet=#{@facets.join(',')}"]
+          @top.each do |field, top|
+            param << "facet-#{field}-top-n=#{top}"
+          end
+          param
+        end
+      end
+    end
+  end
+end
