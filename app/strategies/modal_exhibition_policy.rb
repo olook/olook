19ab24fcc3246aae544162a92e33810ef65e2e30
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-19 15:56:37 -0300

    Add try to FB method

--- app/strategies/modal_exhibition_policy.rb
+++ app/strategies/modal_exhibition_policy.rb
@@ -2,7 +2,7 @@ class ModalExhibitionPolicy
   BLACK_LIST = /(sacola|pagamento)/
 
   def self.apply?(opts={})
-     avaliable_path?(opts[:path]) && without_cookie?(opts[:cookie]) && opts[:user].blank?
+     avaliable_path?(opts[:path]) && without_cookie?(opts[:cookie]) && opts[:user].blank? && opts[:mobile] == false
   end
 
   private
@@ -14,4 +14,5 @@ class ModalExhibitionPolicy
   def self.without_cookie?(cookie)
     cookie.blank?
   end
+
 end
