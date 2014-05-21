63a02e39503ebda15e7b87acf25c7137d30909d7
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-20 21:09:24 -0300

    Fix duplication of returnfields

--- lib/search/query/return_fields.rb
+++ lib/search/query/return_fields.rb
@@ -4,6 +4,20 @@ module Search
       def self.factory
         eval("#{Search::Config.api_version_module_name}::ReturnFields")
       end
+
+      def initialize(*fields)
+        @fields = []
+        set_fields(fields)
+      end
+
+      def set_fields(*fields)
+        @fields.concat(fields)
+        @fields.flatten!
+      end
+
+      def query_url
+        raise NotImplementedError.new("You should implement query_url in #{self.class}")
+      end
     end
   end
 end
--- lib/search/query/v20130101/return_fields.rb
+++ lib/search/query/v20130101/return_fields.rb
@@ -1,17 +1,7 @@
 module Search
   module Query
     module V20130101
-      class ReturnFields
-        def initialize(*fields)
-          @fields = []
-          set_fields(fields)
-        end
-
-        def set_fields(*fields)
-          @fields.concat(fields)
-          @fields.flatten!
-        end
-
+      class ReturnFields < Search::Query::ReturnFields
         def query_url
           "return=#{@fields.join(',')}" if @fields.size > 0
         end
