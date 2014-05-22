d56d1acdf51d7bdd714d63977023d9613c570b11
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-22 13:49:23 -0300

    Add correct elements for already taken email

--- app/assets/javascripts/modules/modal/post.js
+++ app/assets/javascripts/modules/modal/post.js
@@ -4,13 +4,14 @@ var ModalPost  = (function(){
     olookApp.subscribe('modal:post', this.facade);
   };
 
-  ModalPost.prototype.facade = function(email, element){
+  ModalPost.prototype.facade = function(email, close_element, error_element){
     $.post("/campaign_email_subscribe", {email: email})
     .done(function(data){
       element.click();
     })
     .fail(function(data) {
-      console.log("FALHOOOOO" + data);
+      data = JSON.parse(data.responseText);
+      error_element.html(data.message);
   });
   };
   return ModalPost;
