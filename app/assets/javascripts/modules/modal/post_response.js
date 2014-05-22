c410879bf57711fb720c389f41a296af0445e1cf
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-22 16:29:00 -0300

    Refactor to send error div to show

--- app/assets/javascripts/modules/modal/post_response.js
+++ app/assets/javascripts/modules/modal/post_response.js
@@ -4,12 +4,12 @@ var ModalPostResponse  = (function(){
     olookApp.subscribe('modal:post_response', this.facade);
   };
 
-  ModalPostResponse.prototype.facade = function(email, close_element, error_element){
+  ModalPostResponse.prototype.facade = function(error_element){
     $("#campaign_email_form").on("ajax:success", function(e, data, status, xhr) {
       return $('#modal button.close').click();
     }).on("ajax:error", function(e, xhr, status, error) {
       _data = JSON.parse(xhr.responseText);
-      return $('.modal_error').html(_data.message).css({'display':'block'});
+      return error_element.html(_data.message).css({'display':'block'});
     });
   };
   return ModalPostResponse;
