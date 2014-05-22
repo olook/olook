2a25250f489d0a298a90bb0fb06fb0a60b056d74
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-22 15:17:24 -0300

    Close modal on success

--- app/assets/javascripts/modules/modal/post_response.js
+++ app/assets/javascripts/modules/modal/post_response.js
@@ -6,7 +6,7 @@ var ModalPostResponse  = (function(){
 
   ModalPostResponse.prototype.facade = function(email, close_element, error_element){
     $("#campaign_email_form").on("ajax:success", function(e, data, status, xhr) {
-      return $('#modal button.close');
+      return $('#modal button.close').click();
     }).on("ajax:error", function(e, xhr, status, error) {
       _data = JSON.parse(xhr.responseText);
       return $('.modal_error').html(_data.message);
