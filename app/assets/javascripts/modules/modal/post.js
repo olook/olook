ed93807aa3811a59dc55b3b22eaab143cfff7691
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-22 10:27:32 -0300

    Inicial modal post post

--- app/assets/javascripts/modules/modal/post.js
+++ app/assets/javascripts/modules/modal/post.js
@@ -1,4 +1,13 @@
 var ModalPost  = (function(){
+  function ModalPost() {};
+  ModalPost.prototype.config = function(){
+    olookApp.subscribe('modal:post', this.facade);
+  };
 
+  ModalPost.prototype.facade = function(email){
+    $.post("/campaign_email_subscribe", function(){
+
+    });
+  };
   return ModalPost;
 })();
