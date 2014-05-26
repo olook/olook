e74f2332355d16d2e304984722b9cb935f779868
Rafael <rafael.manoel@gmail.com>
2014-05-26 11:14:52 -0300

    refactor to remove remote:true from form

--- app/assets/javascripts/modules/modal/load.js
+++ app/assets/javascripts/modules/modal/load.js
@@ -1,7 +1,7 @@
 //= require modules/modal/request
 //= require modules/modal/show
-//= require modules/modal/post_response
+//= require modules/modal/post
 
 new ModalShow().config();
 new ModalRequest().config();
-new ModalPostResponse().config();
+new ModalPost().config();
---
+++ app/assets/javascripts/modules/modal/post.js
@@ -0,0 +1,24 @@
+var ModalPost  = (function(){
+  function ModalPost() {};
+  ModalPost.prototype.config = function(){
+    olookApp.subscribe('modal:post_email', this.facade);
+  };
+
+  ModalPost.prototype.facade = function(error_element){
+
+    log_event('click','newsletter-popup',{value: 'email'});
+    emailToBeSubmited = $('#js-emailValue').val();
+
+    $.post('/new_campaign_email_subscribe', {email: emailToBeSubmited})
+      .done(function(e) {
+        log_event('action','newsletter-popup',{value: 'email'});
+        $('#modal button.close').click();
+      }).fail(function(data) {
+      _data = JSON.parse(data.responseText);
+      error_element.html(_data.message).css({'display':'block'});
+    });
+
+  };
+
+  return ModalPost;
+})();
