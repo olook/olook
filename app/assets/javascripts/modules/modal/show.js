3eab4ccb77e300d9aae500f9282fe774d196b66b
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-05-28 09:34:55 -0300

    Remove view log_event from modal show

--- app/assets/javascripts/modules/modal/show.js
+++ app/assets/javascripts/modules/modal/show.js
@@ -9,7 +9,6 @@ var ModalShow = (function(){
     olook.newModal(data.html, data.width, data.height, data.color, function(){
       _gaq.push(['_trackEvent', 'Modal', 'Close', data.name, ( new Date() - start )], true);
     });
-    log_event('view', 'newsletter-popup');
   };
   return ModalShow;
 })();
