89fb846819d36c7e60aefe2d346f44c2ffe4897b
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-05-30 16:41:13 -0300

    Recommendation Service pull looks from Products

--- app/services/recommendation_service.rb
+++ app/services/recommendation_service.rb
@@ -3,6 +3,19 @@ class RecommendationService
   DAYS_AGO_TO_CONSIDER_NEW = 140
   DAYS_AGO_TO_CONSIDER_NEW_PRODUCTS = 30
   DATE_WHEN_PICTURES_CHANGED = "2013-07-01"
+  WHITELISTED_SUBCATEGORIES = [
+    'blazer',
+    'blusa',
+    'camisa',
+    'camiseta',
+    'casaco',
+    'casaco e jaqueta',
+    'colete',
+    'macacao',
+    'regata',
+    'top cropped',
+    'vestido'
+  ]
 
   def profile_name
     @profiles.first.try(:alternative_name) || "casual"
@@ -34,37 +47,52 @@ class RecommendationService
 
   private
 
-    def filtered_looks_for_profile(opts={})
-      Look.where(profile_id: @profiles).where(Look.arel_table[:launched_at].gt(DAYS_AGO_TO_CONSIDER_NEW.days.ago)).order('RAND()').first(opts[:limit])
-    end
+  def filtered_looks_for_profile(opts={})
+    #Look.where(profile_id: @profiles).where(Look.arel_table[:launched_at].gt(DAYS_AGO_TO_CONSIDER_NEW.days.ago)).order('RAND()').first(opts[:limit])
+    _pAt = Product.arel_table
+    _vAt = Variant.arel_table
+    _dAt = Detail.arel_table
 
-    def filtered_list_for_profile(profile, opts={})
-      return [] unless profile
-      _pAt = Product.arel_table
-      _vAt = Variant.arel_table
+    is_admin = opts.fetch(:admin, false)
 
-      is_admin = opts.fetch(:admin, false)
-      category = opts[:category]
-      collection = opts[:collection]
+    result = Product.where(_pAt[:launch_date].gt(DAYS_AGO_TO_CONSIDER_NEW.days.ago))
+    .where(_pAt[:created_at].gt(DATE_WHEN_PICTURES_CHANGED))
+    .includes(:variants, :pictures)
 
-      result = profile.products.where(_pAt[:launch_date].gt(DAYS_AGO_TO_CONSIDER_NEW_PRODUCTS.days.ago))
-                               .where(_pAt[:created_at].gt(DATE_WHEN_PICTURES_CHANGED))
-                               .includes(:variants, :pictures)
-                               .order('RAND()')
+    result = result.joins(:details).where(_dAt[:translation_token].eq('categoria').and(_dAt[:description].in(WHITELISTED_SUBCATEGORIES)))
 
-      result = result.only_visible.where(_vAt[:inventory].gt(0).and(_vAt[:price].gt(0))) unless is_admin
+    result = result.only_visible.where(_vAt[:inventory].gt(0).and(_vAt[:price].gt(0))) unless is_admin
 
-      result = result.where(_pAt[:collection_id].eq(collection.id)) if collection
+    result
+  end
 
-      result = result.joins(:variants).
-        where(_pAt[:category].not_eq(Category::SHOE).
-            or(_pAt[:category].eq(Category::SHOE).
-                and(_vAt[:description].eq(@shoe_size))
-              )) if @shoe_size.present?
+  def filtered_list_for_profile(profile, opts={})
+    return [] unless profile
+    _pAt = Product.arel_table
+    _vAt = Variant.arel_table
 
-      result = result.where(category: category) if category.present?
+    is_admin = opts.fetch(:admin, false)
+    category = opts[:category]
 
-      result
-    end
+    result = profile.products.where(_pAt[:launch_date].gt(DAYS_AGO_TO_CONSIDER_NEW_PRODUCTS.days.ago))
+    .where(_pAt[:created_at].gt(DATE_WHEN_PICTURES_CHANGED))
+    .includes(:variants, :pictures)
+    .order('RAND()')
+
+    result = result.only_visible.where(_vAt[:inventory].gt(0).and(_vAt[:price].gt(0))) unless is_admin
+
+    result = result.joins(:variants).
+      where(
+        _pAt[:category].not_eq(Category::SHOE).
+        or(
+          _pAt[:category].eq(Category::SHOE).
+          and(_vAt[:description].eq(@shoe_size))
+        )
+    ) if @shoe_size.present?
+
+    result = result.where(category: category) if category.present?
+
+    result
+  end
 
 end
