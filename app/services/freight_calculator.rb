7f76e179f7b04f5a6e5c1a46f8ebb7e150e9d5a6
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-06-02 13:45:20 -0300

    Prepare shipping query if there is specific shipping service

--- app/services/freight_calculator.rb
+++ app/services/freight_calculator.rb
@@ -19,10 +19,11 @@ module FreightCalculator
   def self.freight_for_zip(zip_code, order_value, shipping_service_ids = nil, use_message = false)
     _zip_code = ZipCode::SanitizeService.clean(zip_code)
     return {} unless ZipCode::ValidService.apply?(_zip_code)
-    freights = Shipping.with_zip(_zip_code)
+    freights = prepare_shipping_query(_zip_code,shipping_service_ids)
     return DEFAULT_FREIGHT if freights.blank?
     result = Freight::TransportShippingChooserService.new(freights).perform
-    check_free_freight_policy(result, _zip_code, order_value)
+    result = check_free_freight_policy(result, _zip_code, order_value) unless shipping_service_ids
+    result
   end
 
    private
@@ -31,5 +32,11 @@ module FreightCalculator
        result[:default_shipping][:price] = '0.0'.to_d
      end
      result
+   end
+
+   def self.prepare_shipping_query(zip_code, shipping_ids)
+    shipping_query = Shipping.with_zip(zip_code)
+    shipping_query = shipping_query.with_shipping_service(shipping_ids) if shipping_ids
+    shipping_query
    end
 end
