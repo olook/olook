90748b6068f166e4181f5170372cd7f5f35ed0af
Rafael <rafael.manoel@gmail.com>
2014-06-06 19:36:34 -0300

    fix erros and add authorization

--- app/models/product_interest.rb
+++ app/models/product_interest.rb
@@ -1,23 +1,20 @@
 class ProductInterest < ActiveRecord::Base
   attr_accessible :campaign_email_id, :product_id
 
-
-
   belongs_to :campaign_email
-
 
   def self.creates_for email, product_id
     user = User.find_by_email email   
     newsletter_user = CampaignEmail.find_or_create_by_email(email) do |n|
       n.profile = "produto_esgotado"
+      n.converted_user = user.present? 
     end
 
-    if user
-      newsletter_user.update_attribute(:converted_user, true)
-    end
 
-    ProductInterest.create({campaign_email_id: newsletter_user.id, product_id: product_id})
-
+    # TODO => Nao gostei disso.
+    interest = ProductInterest.create({campaign_email_id: newsletter_user.id, product_id: product_id})   
+    interest.errors.add(:email, "Email invalido") if !newsletter_user.valid?
+    interest
   end
 
 end
