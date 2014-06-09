902059079dbfba14af1a40b0355c343cf345ffc7
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-06-09 15:16:42 -0300

    Add export csv funcionality

--- app/controllers/admin/product_interests_controller.rb
+++ app/controllers/admin/product_interests_controller.rb
@@ -1,8 +1,13 @@
 # encoding: utf-8
 class Admin::ProductInterestsController < Admin::BaseController
   load_and_authorize_resource
-  respond_to :html
+  respond_to :html, :csv
   def index
-    @product_interests = ProductInterest.all
+    @search = ProductInterest.search(params[:search])
+    @product_interests = @search.relation.page(params[:page]).per_page(50)
+    respond_to do |format|
+      format.html
+      format.csv { send_data @product_interests.as_csv }
+    end
   end
 end
--- app/models/product_interest.rb
+++ app/models/product_interest.rb
@@ -18,6 +18,15 @@ class ProductInterest < ActiveRecord::Base
     interest
   end
 
+  def self.as_csv
+    CSV.generate do |csv|
+      csv << column_names
+      all.each do |item|
+        csv << item.attributes.values_at(*column_names)
+      end
+    end
+  end
+
   private
 
     def self.newsletter_user_for email
