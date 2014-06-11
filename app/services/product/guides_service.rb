0e8d246c7d1e1441e7d3441b19bcab96b45ad008
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-06-11 11:07:22 -0300

    Refactor to match new style intead old one

--- app/services/product/guides_service.rb
+++ app/services/product/guides_service.rb
@@ -1,20 +1,20 @@
 class Product::GuidesService
   attr_accessor :table_lines
-  OLD_DETAILS = /<p/
+  NEW_DETAILS = /#/
 
   def initialize(details)
     @details = details
     @return_hash = {}
   end
 
-  def process
-    return @details if old_style?
+  def parse
+    return @details if !new_style?
     model_size, @table_lines =  @details.split('#').first, @details.split('#').last.split(";")
     {header: header_table_info, content: line_table_info, model_size: model_size}
   end
 
-  def old_style?
-    !!(@details =~ OLD_DETAILS)
+  def new_style?
+    !!(@details.match(NEW_DETAILS))
   end
 
   private
@@ -24,7 +24,7 @@ class Product::GuidesService
   end
 
   def line_table_info
-    @table_lines.map{|a| @return_hash[a.split('=>').first] = a.split('=>').last.split(',').map{|b| b.split(':').last}}
+    @table_lines.map{|a| @return_hash[a.split('=>').first] = a.split('=>').last.split(',').map{|b| b.split(':').last.to_i}}
     @return_hash
   end
 
