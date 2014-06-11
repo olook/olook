6a6a936dfa3b534907b50b4503bc57917cb007b9
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-06-11 11:04:11 -0300

    Refactor class

--- spec/services/product/guides_service_spec.rb
+++ spec/services/product/guides_service_spec.rb
@@ -3,28 +3,28 @@ describe Product::GuidesService do
   let(:new_detail) {"P#P=>ombro:12cm,busto:12cm,quadril:12cm,comp.:12cm;M=>ombro:12cm,busto:12cm,quadril:12cm,comp.:12cm;G=>ombro:12cm,busto:12cm,quadril:12cm,comp.:12cm"}
   context "In different style content" do
     context "When old" do
-      it {expect(described_class.new(old_detail).old_style?).to be_true}
+      it {expect(described_class.new(old_detail).new_style?).to be_false}
     end
     context "When new" do
-      it {expect(described_class.new(new_detail).old_style?).to be_false}
+      it {expect(described_class.new(new_detail).new_style?).to be_true}
     end
   end
   context "On old style" do
     it "return same string" do
-      expect(described_class.new(old_detail).process).to eql(old_detail)
+      expect(described_class.new(old_detail).parse).to eql(old_detail)
     end
 
   end
   context "On new style" do
     before do
       @detail = described_class.new(new_detail)
-      @detail_result = @detail.process
+      @detail_result = @detail.parse
     end
     it "return table header" do
       expect(@detail_result.fetch(:header)).to eql(['ombro','busto','quadril','comp.'])
     end
     it "return table keys content" do
-      expect(@detail_result.fetch(:content)).to include("P" => ['12cm','12cm', '12cm', '12cm'],"M" => ['12cm','12cm', '12cm', '12cm'], "G" => ['12cm','12cm', '12cm', '12cm'])
+      expect(@detail_result.fetch(:content)).to include("P" => [12,12,12,12],"M" => [12,12,12,12], "G" => [12,12,12,12])
     end
     it "return model size" do
       expect(@detail_result.fetch(:model_size)).to include("P")
