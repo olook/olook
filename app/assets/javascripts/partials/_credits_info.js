85dc75262f5ec5a195ff3c45493f16672299f671
Thaiane Gazzi <thaianegazzi@gmail.com>
2014-06-13 17:53:57 -0300

    fixed rules modal

--- app/assets/javascripts/partials/_credits_info.js
+++ app/assets/javascripts/partials/_credits_info.js
@@ -2,7 +2,7 @@ var showInfoCredits = function() {
   $("a.open_loyalty_lightbox").live('click', function(e) {
     _gaq.push(['_trackEvent', 'product_show', 'show_loyalty_info', '']);
     var content = $("div.credits_description");
-    olook.newModal(content, 402, 692, '#fff');
+    olook.newModal(content, 450, 800, '#fff');
     e.preventDefault();
   });
 };
