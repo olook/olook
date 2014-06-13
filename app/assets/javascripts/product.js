070cd71de47037a49ba5de9528ba9faf478f54bf
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-06-13 18:12:34 -0300

    Fix wishlist feedback

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -20,15 +20,22 @@ initProduct = {
         console.log(data);
       });
 
-
       e.preventDefault();
     });
   },
   checkRelatedProducts : function() {
     return $("div#related ul.carousel").size() > 0 ? true : false;
   },
-  showAlert : function(){
-    $('p.alert_size, p.js-alert').show().html("Selecione seu tamanho").delay(3000).fadeOut();
+  showAlert : function(el){
+    if(el) {
+      var it = $(el);
+      var alert_p = $('<div class="alert_div"><p class="alert">Selecione seu tamanho</p></div>');
+      it.before(alert_p);
+      $('.alert_div .alert').show();
+      $('.alert_div').delay(3000).fadeOut();
+    } else {
+      $('p.alert_size, p.js-alert').show().html("Selecione seu tamanho").delay(3000).fadeOut();
+    }
   },
   // for reasons unknown, this carousel is awkwardly inverted. I had to re-invert the names in order for it to work properly :P
   showCarousel : function() {
@@ -55,16 +62,16 @@ initProduct = {
     initProduct.changeQuantity(-1);
   },
   changeQuantity: function(by) {
-    var maxVal = initProduct.selectedVariantMaxVal(),
+    var maxVal = initProduct.selectedVariantMaxVal(this),
       newVal = parseInt($("#variant_quantity").val()) + by;
     if(maxVal && newVal <= maxVal && newVal >= 1 ){
       $("#variant_quantity").val(newVal);
     }
   },
-  selectedVariantMaxVal: function(){
+  selectedVariantMaxVal: function(el){
     var variant = $('[name="variant[id]"]:checked');
     if (variant.length == 0) {
-      initProduct.showAlert();
+      initProduct.showAlert(el);
       return false;
     }
     var inventory = $('[name=inventory_' + variant.val() + ']');
@@ -86,11 +93,11 @@ initProduct = {
     $("a.open_loyalty_lightbox").show();
 
     $("form#product_add_to_cart").submit(function() {
-      return !!(initProduct.selectedVariantMaxVal());
+      return !!(initProduct.selectedVariantMaxVal(this));
     });
 
     $(".js-add-product").click(function(e) {
-      if(!initProduct.selectedVariantMaxVal()){
+      if(!initProduct.selectedVariantMaxVal(this)){
         e.stopPropagation();
       }
     });
@@ -110,7 +117,7 @@ initProduct = {
       } else {
         var variant = $('[name="variant[id]"]:checked');
         var inventory = $('#inventory_' + variant.val());
-        if(initProduct.selectedVariantMaxVal() && it.val() > inventory.val()) {
+        if(initProduct.selectedVariantMaxVal(this) && it.val() > inventory.val()) {
           it.val(inventory.val());
         }
       }
