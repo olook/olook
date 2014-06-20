be7cae7b1e4dd0e25df04d9800465e192d77d1fb
Luis Daher <luisotaviodaher@gmail.com>
2014-06-20 15:41:02 -0300

    mail changes

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -95,8 +95,8 @@ module ProductsHelper
     product.pictures.order(:display_on).first(limit)
   end
 
-  def indefinite_article_for product
-    MALE_CATEGORY.include?(product.category) ? "um" : "uma"
+  def indefinite_article_for category
+    MALE_CATEGORY.include?(category) ? "um" : "uma"
   end
 
 end
--- app/mailers/share_product_mailer.rb
+++ app/mailers/share_product_mailer.rb
@@ -1,11 +1,13 @@
 # -*- encoding : utf-8 -*-
 class ShareProductMailer < ActionMailer::Base
+  helper :products
   default_url_options[:host] = "www.olook.com.br"
   default :from => "avisos@olook.com.br"
 
   def send_share_message_for(product, informations, email_receiver)
     @user_name = informations[:name_from]
     @user_email = informations[:email_from]
+    @email_body = informations[:email_body]
     @product = Product.find(product)
     mail(to: email_receiver, reply_to: @user_email, subject: "#{ @user_name.upcase } viu #{ @product.category_humanize.last } #{ @product.category_humanize } #{ @product.name } no site da olook e lembrou de você").deliver
   end
