e76b454ef95e561bad39d94fce7ee18cca382a8c
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-06-20 12:51:55 -0300

    Fix Spy JS

--- app/assets/javascripts/modules/product/show_cart_modal_manager.js
+++ app/assets/javascripts/modules/product/show_cart_modal_manager.js
@@ -5,7 +5,8 @@ var ShowCartModalManager  = (function(){
   };
 
   ShowCartModalManager.prototype.facade = function(){
-    olook.addToCartModal('<div class="product_added"><p>Produto adicionado à sacola</p><button type="button" class="js-close_modal close_modal" role="button">Continuar Comprando</button><button type="button" class="js-go_to_cart go_to_cart" role="button">Ir Para Minha Sacola</button></div>', 200, '#000', function(){console.log("closed")});
+    var content = '<div class="product_added"><p>Produto adicionado à sacola</p><button type="button" class="js-close_modal close_modal" role="button">Continuar Comprando</button><button type="button" class="js-go_to_cart go_to_cart" role="button">Ir Para Minha Sacola</button></div>';
+    olook.addToCartModal(content, 200, '#FFF');
   };
 
   return ShowCartModalManager;
