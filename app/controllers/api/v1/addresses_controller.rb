d8d104d68c2e0c8df43e87865dbf147e5f98b91d
Luis Daher <luisotaviodaher@gmail.com>
2014-06-24 15:44:23 -0300

    WIP - creating tests

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,44 +1,48 @@
-class Api::V1::AddressesController < ApiBasicController
-  respond_to :json
+module Api
+  module V1
+    class AddressesController < ApiBasicController
+      respond_to :json
 
-  before_filter :authenticate_user!
+      before_filter :authenticate_user!
 
-  def index
-    render json: current_user.addresses.to_json
-  end
+      def index
+        render json: current_user.addresses.to_json
+      end
 
-  def destroy
-    selected_address(params[:id]).destroy
-    render json: address.to_json, status: :ok
-  end
+      def destroy
+        selected_address(params[:id]).destroy
+        render json: address.to_json, status: :ok
+      end
 
-  def create
-    params[:address][:country] = 'BRA' if params[:address]    
-    address = current_user.addresses.create(params[:address])
-    if address.valid?
-      render json: address.to_json, status: :created
-    else  
-      render json: address.errors.to_json, status: :unprocessable_entity
+      def create
+        params[:address][:country] = 'BRA' if params[:address]    
+        address = current_user.addresses.create(params[:address])
+        if address.valid?
+          render json: address.to_json, status: :created
+        else  
+          render json: address.errors.to_json, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        address = selected_address(params[:id])
+        if address.update_attributes(params[:address])
+          render json: address.to_json, status: :ok
+        else  
+          render json: address.errors.to_json, status: :unprocessable_entity
+        end
+      end  
+
+      def show
+        render json: selected_address(params[:id]).to_json, status: :ok    
+      end
+
+      private
+
+      def selected_address id
+        current_user.addresses.active.find(params[:id])
+      end
+
     end
   end
-
-  def update
-    address = selected_address(params[:id])
-    if address.update_attributes(params[:address])
-      render json: address.to_json, status: :ok
-    else  
-      render json: address.errors.to_json, status: :unprocessable_entity
-    end
-  end  
-
-  def show
-    render json: selected_address(params[:id]).to_json, status: :ok    
-  end
-
-  private
-
-  def selected_address id
-    current_user.addresses.active.find(params[:id])
-  end
-
 end
--- spec/controllers/api/v1/addresses_controller_spec.rb
+++ spec/controllers/api/v1/addresses_controller_spec.rb
@@ -2,4 +2,104 @@ require 'spec_helper'
 
 describe Api::V1::AddressesController do
 
+  let(:user) { FactoryGirl.create :user }
+ 
+  before :each do
+    sign_in user
+  end
+
+  describe "#create" do
+ 
+    let(:address) { FactoryGirl.build(:address, :user => user) }
+
+    context "when the address is valid" do
+      it "returns success status (201)" do
+        post :create, address: address.attributes
+        response.should be_success
+      end
+
+      it "returns the address JSON" do
+        post :create, address: address.attributes
+        address.attributes.reject{|k,v| k == "id"}.should eq(JSON.parse(response.body).reject{|k,v| k == "id"})
+      end
+    end
+
+    context "when the address isn't valid" do
+
+      before do
+        address.city = nil
+      end
+
+      it "returns unprocessable entity" do
+        post :create, address: address.attributes
+        response.status.should eq(422)
+      end
+
+      it "returns the errors JSON" do
+        post :create, address: address.attributes
+        JSON.parse(response.body).should_not be_empty 
+      end
+    end    
+  end
+
+  describe "#update" do
+    let(:address) { FactoryGirl.create(:address, :user => user) }
+
+    before do
+      address.city = "Teresópolis"
+    end
+
+    context "when the address is valid" do
+      it "returns success status (201)" do
+        put :update, address: address.attributes, id: address.id
+        response.should be_success
+      end
+
+      it "returns the updated address JSON" do
+        post :update, address: address.attributes, id: address.id
+        address.attributes.reject{|k,v| k == "id"}.should eq(JSON.parse(response.body).reject{|k,v| k == "id"})
+      end
+    end
+
+    context "when the address isn't valid" do
+      before do
+        address.city = nil
+      end
+
+      it "returns unprocessable entity" do
+        put :update, address: address.attributes, id: address.id
+        response.status.should eq(422)
+      end
+
+      it "returns the errors JSON" do
+        put :update, address: address.attributes, id: address.id
+        JSON.parse(response.body).should_not be_empty
+      end
+    end        
+
+  end
+
+  describe "#destroy" do
+
+    context "when the address is valid" do
+      # let(:address) { FactoryGirl.create(:address, :user => user) }
+
+      # it "returns success status (201)" do
+      #   delete :destroy, id: address.id
+      #   response.should be_success
+      # end
+
+      # it "returns the updated address JSON" do
+      #   delete :destroy, id: address.id
+      #   address.attributes.reject{|k,v| k == "id"}.should eq(JSON.parse(response.body).reject{|k,v| k == "id"})
+      # end
+    end
+  end
+
+  describe "#index" do
+  end
+
+  describe "#show" do
+  end
+
 end
