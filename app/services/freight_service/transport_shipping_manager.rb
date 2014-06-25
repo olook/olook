7b5e3bc1bb6948c8bc69c6fd51290d326ca7d304
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-06-25 19:25:26 -0300

    Fix test refactor

--- app/services/freight_service/transport_shipping_manager.rb
+++ app/services/freight_service/transport_shipping_manager.rb
@@ -1,28 +1,24 @@
 class FreightService::TransportShippingManager
-  def default_freight
-    {
-      default_shipping: {
-        price: @freight_calculator::DEFAULT_FREIGHT_PRICE,
-        cost: @freight_calculator::DEFAULT_FREIGHT_COST,
-        delivery_time: @freight_calculator::DEFAULT_INVENTORY_TIME_WITH_EXTRA_TIME,
-        shipping_service_id: @freight_calculator::DEFAULT_FREIGHT_SERVICE
-      }
-    }
+  def default_shipping
+    Struct.new('DefaultShipping').new(
+      income: @freight_calculator::DEFAULT_FREIGHT_PRICE,
+      cost: @freight_calculator::DEFAULT_FREIGHT_COST,
+      delivery_time: @freight_calculator::DEFAULT_INVENTORY_TIME_WITH_EXTRA_TIME,
+      shipping_service_id: @freight_calculator::DEFAULT_FREIGHT_SERVICE
+    )
   end
 
   def initialize(zip_code, amount_value, transport_shippings, opts={})
     @zip_code = zip_code
     @amount_value = amount_value
-    @transport_shippings = transport_shippings
+    @transport_shippings = transport_shippings.to_a.empty? ? default_shipping : transport_shippings
     @freight_calculator = opts[:freight_calculator] || FreightCalculator
   end
 
   def default
-    return default_freight if @transport_shippings.empty?
-    return parse_info(@transport_shippings.first) if @transport_shippings.size == 1
     chosen = choose_by_cost
     chosen = check_free_freight_policy(chosen)
-    parse_info(chosen)
+    chosen
   end
 
   def fast
@@ -46,7 +42,7 @@ class FreightService::TransportShippingManager
   end
 
   def choose_by_cost
-    @transport_shippings.sort {|x,y| x.cost <=> y.cost }.first
+    @transport_shippings.map { |s| parse_info(s) }.sort {|a,b| a[:cost] <=> b[:cost] }.first
   end
 
   def is_free_cost?
@@ -55,7 +51,7 @@ class FreightService::TransportShippingManager
 
   def check_free_freight_policy(chosen)
     if is_free_cost?
-      chosen[:default_shipping][:price] = '0.0'.to_d
+      chosen[:price] = '0.0'.to_d
     end
     chosen
   end
--- spec/services/freight_service/transport_shipping_manager_spec.rb
+++ spec/services/freight_service/transport_shipping_manager_spec.rb
@@ -1,27 +1,26 @@
 require File.expand_path(File.join(File.dirname(__FILE__), '../../../app/services/freight_service/transport_shipping_manager'))
 describe FreightService::TransportShippingManager do
+  subject { described_class.new("08730810", "79.9", @shippings, freight_calculator: FR) }
+  before do
+    stub_const('FR::DEFAULT_INVENTORY_TIME', 2)
+  end
   context "When there isn't shippings" do
-    subject { described_class.new("08730810", "79.9", []) }
     it "return default info" do
+      @shippings = []
       subject.should_receive(:default_freight).and_return(:default_freight)
       expect(subject.default).to eql(:default_freight)
     end
   end
   context "When there shipping" do
-    subject { described_class.new("08730810", "79.9", @shippings, freight_calculator: FR) }
-    before do
-      @shippings = [double('Shipping', cost: 10, income: 15, delivery_time: 2, shipping_service_id: 1)]
-      @ship = { price: 15, cost: 10, :delivery_time => 4, :shipping_service_id => 1 }
-      stub_const('FR::DEFAULT_INVENTORY_TIME', 2)
-    end
     context "with one" do
       it "return shipping" do
-        expect(subject.default).to eql(@ship)
+        @shippings = [double('Shipping', cost: 10, income: 15, delivery_time: 2, shipping_service_id: 1)]
+        ship = { price: 15, cost: 10, :delivery_time => 4, :shipping_service_id => 1 }
+        expect(subject.default).to eql(ship)
       end
     end
     context "with more than one" do
       it "return shipping" do
-        subject
         cost_less = double('Shipping', cost: 5, income: 15, delivery_time: 5, shipping_service_id: 1)
         @shippings.push cost_less
         ship = { price: cost_less.income, cost: cost_less.cost, :delivery_time => 7, :shipping_service_id=>1}
@@ -31,7 +30,7 @@ describe FreightService::TransportShippingManager do
     context "When there there is free freight policy" do
       it "return shipping" do
         subject.should_receive(:is_free_cost?).and_return(true)
-        expect(subject.default[:price]).to eql('0.0')
+        expect(subject.default[:price]).to eql(0)
       end
     end
   end
