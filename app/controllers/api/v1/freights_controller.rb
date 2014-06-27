3d28781afdc84a39ef2218b2a8957a656022c711
Rafael <rafael.manoel@gmail.com>
2014-06-27 16:15:43 -0300

    refactor to improve readability

--- app/controllers/api/v1/freights_controller.rb
+++ app/controllers/api/v1/freights_controller.rb
@@ -2,14 +2,36 @@
 module Api
   module V1
     class FreightsController < ActionController::Base
-      def show
-        return render(json: "Missing params", status: :unprocessable_entity) if !params[:zip_code].present? || !params[:amount_value].present?
-        zip_code = ZipCode::SanitizeService.clean(params[:zip_code])
-        return render(json: "wrong zipcode", status: :unprocessable_entity) if !ZipCode::ValidService.apply?(zip_code)
 
-        transport_shippings = Freight::TransportShippingManager.new(params[:zip_code],params[:amount_value], Shipping.with_zip(zip_code))
-        render json: transport_shippings.to_json, status: :ok
+      rescue_from ArgumentError do |ex|
+        render(json: ex.message, status: :unprocessable_entity) 
       end
+
+      def show
+
+        transport_shippings = FreightService::TransportShippingManager.new(
+          params[:zip_code],
+          params[:amount_value], 
+          Shipping.with_zip(get_zip_code))
+
+        render json: transport_shippings.to_json, status: :ok
+
+      end
+
+      private
+        def get_zip_code
+          if !params[:zip_code].present? || !params[:amount_value].present?          
+            raise(ArgumentError, "Missing params")
+          else
+            zip_code = ZipCode::SanitizeService.clean(params[:zip_code])
+
+            if ZipCode::ValidService.apply?(zip_code)
+              zip_code
+            else
+              raise(ArgumentError, "wrong zipcode")
+            end
+          end
+        end
     end
   end
 end
