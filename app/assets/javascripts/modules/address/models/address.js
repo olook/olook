79f660923c83aa3febaf9ca634721fe7678f7352
Luis Daher <luisotaviodaher@gmail.com>
2014-07-02 17:16:20 -0300

    validations created

--- app/assets/javascripts/modules/address/models/address.js
+++ app/assets/javascripts/modules/address/models/address.js
@@ -23,12 +23,33 @@ app.models.Address = Backbone.Model.extend({
     errors = [];
 
     if (StringUtils.isEmpty(attr.city)) {
-      errors.push({name: 'city', message: 'Cidade é obrigatória'});
+      errors.push({name: 'city', message: 'Qual é o nome da cidade, mesmo?'});
     }
     
     if (StringUtils.isEmpty(attr.zip_code)) {
-      errors.push({name: 'zip_code', message: 'CEP é obrigatório'});
+      errors.push({name: 'zip_code', message: 'Precisamos do seu CEP'});
     }
+
+    if (StringUtils.isEmpty(attr.street)) {
+      errors.push({name: 'street', message: 'Onde devemos entregar? Selecione um endereço'});
+    }
+
+    if (StringUtils.isEmpty(attr.number)) {
+      errors.push({name: 'number', message: 'Também precisamos do número'});
+    }
+
+    if (StringUtils.isEmpty(attr.state)) {
+      errors.push({name: 'state', message: 'Também precisamos da sigla do estado (UF)'});
+    }
+
+    if (StringUtils.isEmpty(attr.neighborhood)) {
+      errors.push({name: 'neighborhood', message: 'Qual é o seu bairro, mesmo?'});
+    }
+
+    if (StringUtils.isEmpty(attr.telephone)) {
+      errors.push({name: 'telephone', message: 'É bom termos o seu telefone. Qualquer coisa, ligamos'});
+    }
+
 
     return errors.length > 0 ? errors : false;
   }
