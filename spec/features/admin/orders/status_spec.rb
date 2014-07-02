f71c10c13678f46ebd9dad32651074975891c026
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-07-02 15:48:46 -0300

    Removing pending tests

--- spec/features/admin/orders/status_spec.rb
+++ spec/features/admin/orders/status_spec.rb
@@ -52,40 +52,6 @@ feature "Orders statuses dashboard", %q{
     expect(page).to have_css('#total_authorized', text: '2')
   end
 
-  scenario "Viewing details for a list of authorized orders" do
-    pending
-    click_link 'Status dos pedidos'
-
-    page.find('tr#0_dias td#authorized a').click
-
-    expect(page).to have_content("Cadastro")
-    expect(page).to have_content("Pagamento")
-    expect(page).to have_content("Despacho Entrega")
-    expect(page).to have_content("Data prometida de Entrega")
-    expect(page).to have_content("Transportador")
-    expect(page).to have_content("Gateway de pagamento")
-    expect(page).to have_content("Cliente nome")
-    expect(page).to have_content("Cliente email")
-    expect(page).to have_content("Cidade")
-    expect(page).to have_content("Estado")
-    expect(page).to have_content("CEP")
-    expect(page).to have_content("Quantidade de itens")
-
-    expect(page).to have_content(order.created_at.strftime('%A, %e %B %Y'))
-    expect(page).to have_content(order.payments.for_erp.first.created_at.strftime('%A, %e %B %Y'))
-    #TODO: Despacho Entrega
-    #TODO: Data prometida de Entrega
-    expect(page).to have_content("TEX")
-    #TODO: Gateway de pagamento
-    expect(page).to have_content("José Ernesto")
-    expect(page).to have_content("jose.ernesto@dominio.com")
-    expect(page).to have_content("Rio de Janeiro")
-    expect(page).to have_content("RJ")
-    expect(page).to have_content("87656-908")
-    expect(page).to have_content("100.0")
-    expect(page).to have_content("0")
-  end
-
   scenario 'Transportation filter' do
 
     click_link 'Status dos pedidos'
