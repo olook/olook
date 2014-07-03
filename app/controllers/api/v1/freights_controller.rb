61f36f7462bcfc2be9c7e598a3e0d20c991f317c
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-07-03 14:47:10 -0300

    Freights Controller REST

--- app/controllers/api/v1/freights_controller.rb
+++ app/controllers/api/v1/freights_controller.rb
@@ -4,10 +4,14 @@ module Api
     class FreightsController < ApiBasicController
       skip_before_filter :restrict_access
 
-      def show
+      rescue_from ArgumentError do |ex|
+        render(json: ex.message, status: :unprocessable_entity)
+      end
+
+      def index
         transport_shippings = FreightService::TransportShippingManager.new(
           params[:zip_code],
-          params[:amount_value], 
+          params[:amount_value],
           Shipping.with_zip(get_zip_code))
         render json: transport_shippings.to_json, status: :ok
       end
