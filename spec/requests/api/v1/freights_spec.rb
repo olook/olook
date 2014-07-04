5b4b46c16e0d124e472b0387753a02475ef7014f
Nelson Haraguchi <nelsonmhjr@gmail.com>
2014-07-04 10:32:37 -0300

    Fix tests

--- spec/requests/api/v1/freights_spec.rb
+++ spec/requests/api/v1/freights_spec.rb
@@ -3,12 +3,12 @@ require 'spec_helper'
 describe "Freights" do
   context "Missing params" do
     it "return falure response" do
-      get '/api/v1/freights/123'
+      get '/api/v1/freights'
       expect(response.status).to eql 422
     end
     it "return falure response" do
-      get '/api/v1/freights/123'
-      expect(response.body).to eql 422
+      get '/api/v1/freights', zip_code: '123'
+      expect(response.body).to eql "Missing params"
     end
   end
 end
