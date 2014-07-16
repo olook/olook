3afd3e0a77e719ccd7d09776606cf8932ae003ad
Rafael <rafael.manoel@gmail.com>
2014-07-16 19:00:19 -0300

    improve error handling

--- app/controllers/admin/b2b_orders_controller.rb
+++ app/controllers/admin/b2b_orders_controller.rb
@@ -7,15 +7,25 @@ class Admin::B2bOrdersController < Admin::BaseController
 
   def create
     csv = params[:b2b_order].delete(:order_items_file)
-    result = ShowroomOrderGenerator.new.run(params[:b2b_order])   
-    
-    if result[:error].present?
-      flash[:error] = "#{result[:error]}"
+
+    if csv.nil? || any_missing_parameter?
+      flash[:error] = "Por favor, preencha todos os campos."
     else
-      flash[:notice] = "Pedido #{result[:order]} criado com sucesso. Confira no abacos"
+      result = ShowroomOrderGenerator.new.run(params[:b2b_order])
+      if result[:error].present?
+        flash[:error] = "#{result[:error]}"
+      else
+        flash[:notice] = "Pedido #{result[:order]} criado com sucesso. Confira no abacos"
+      end
     end
-    # redirect_to admin_b2b_orders
+
     render :index
   end
+
+  private
+    def any_missing_parameter?
+      blanks = [:document, :customer, :name, :email].select{|key| params[:b2b_order][key].blank?}
+      blanks.any?
+    end
 
 end
