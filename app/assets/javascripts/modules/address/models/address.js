94003dc97b3d277737422055d4ad6efba6c40f86
Luis Daher <luisotaviodaher@gmail.com>
2014-08-04 17:32:06 -0300

    error messages fixed in front-end code

--- app/assets/javascripts/modules/address/models/address.js
+++ app/assets/javascripts/modules/address/models/address.js
@@ -25,24 +25,24 @@ app.models.Address = Backbone.Model.extend({
   validate: function(attr) {
     errors = [];
 
-    if (StringUtils.isEmpty(attr.city)) {
-      errors.push({name: 'city', message: 'Qual é o nome da cidade, mesmo?'});
-    }
-
     if (StringUtils.isEmpty(attr.zip_code) || !attr.zip_code.match(/\d{5}-\d{3}/g)) {
       errors.push({name: 'zip_code', message: 'Precisamos do seu CEP'});
     }
 
+    if (StringUtils.isEmpty(attr.state)) {
+      errors.push({name: 'state', message: 'Precisamos da sigla do estado (UF)'});
+    }
+
+    if (StringUtils.isEmpty(attr.city)) {
+      errors.push({name: 'city', message: 'Qual é o nome da cidade, mesmo?'});
+    }
+
     if (StringUtils.isEmpty(attr.street)) {
-      errors.push({name: 'street', message: 'Onde devemos entregar? Selecione um endereço'});
+      errors.push({name: 'street', message: 'Precisamos do seu endereço completo'});
     }
 
     if (StringUtils.isEmpty(attr.number)) {
-      errors.push({name: 'number', message: 'Também precisamos do número'});
-    }
-
-    if (StringUtils.isEmpty(attr.state)) {
-      errors.push({name: 'state', message: 'Também precisamos da sigla do estado (UF)'});
+      errors.push({name: 'number', message: 'Precisamos do seu endereço completo'});
     }
 
     if (StringUtils.isEmpty(attr.neighborhood)) {
