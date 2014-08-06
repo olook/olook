ca1c5d42af5f6de0476101f781cf7088a12ca5f8
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-06 13:41:16 -0300

    Remove double delete on address

--- app/assets/javascripts/modules/address/collections/addresses.js
+++ app/assets/javascripts/modules/address/collections/addresses.js
@@ -1,10 +1,4 @@
 app.collections.Addresses = Backbone.Collection.extend({
   model: app.models.Address,
   url: app.server_api_prefix + '/addresses',
-  initialize: function() {
-    this.on('remove', this.onModelRemoved, this);
-  },
-  onModelRemoved: function (model, collection, options) {
-    model.destroy();
-  }
 });
