7a9a88665377e9e3aeaf6735ce6d83d3394998aa
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-08 13:39:31 -0300

    Fix async bug on payment types cart total

--- app/assets/javascripts/modules/checkout/controller.js
+++ app/assets/javascripts/modules/checkout/controller.js
@@ -27,7 +27,7 @@ var CheckoutController = (function() {
   CheckoutController.prototype.paymentTypeSelected = function(model){
     console.log(model.attributes);
     console.log("o desconto para "+model.attributes.name+" é de "+model.attributes.percentage+"%");
-    this.cart.save({ payment_method: model.get('type') });
+    this.cart.save({ payment_method: model.get('type')}, {wait: true});
   };
 
   CheckoutController.prototype.freightSelected = function(model) {
