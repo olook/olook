bd64a8a1bd744c8786665cd268ad5d6945de5c9a
Lucas <lucas.santanadesouza@gmail.com>
2014-08-13 16:04:10 -0300

    wholesale page added

--- app/models/wholesale.rb
+++ app/models/wholesale.rb
@@ -1,6 +1,6 @@
 class Wholesale
 
-  attr_accessor :first_name, :last_name, :email, :password, :birthday, :gender, :addresses_attributes
+  attr_accessor :first_name, :last_name, :email, :password, :birthday, :gender, :addresses_attributes, :addresses, :address
   extend ActiveModel::Naming
   include ActiveModel::Conversion
   def persisted?
