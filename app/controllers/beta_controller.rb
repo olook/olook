6d4e9b0587b6117ba5a7fd45239f2e4a1f1c27e9
Rafael <rafael.manoel@gmail.com>
2014-08-19 13:43:19 -0300

    add dynamic content for conclusion checkout page

--- app/controllers/beta_controller.rb
+++ app/controllers/beta_controller.rb
@@ -1,12 +1,28 @@
+# -*- encoding : utf-8 -*-
 class BetaController < ApplicationController
+
+  PAYMENTS = {
+    CreditCard: 'Cartão de Crédito',
+    Billet: 'Boleto Bancário',
+    Debit: 'Débito em Conta',
+    MercadoPagoPayment: 'MercadoPago'
+  }
+
+
   def index
     render layout: 'lite_checkout'
   end
 
   def confirmation
     @order = Order.find_by_number(params[:number])
+
+    @payment_method = @order.erp_payment.type.downcase
+    @payment_description = PAYMENTS[@order.erp_payment.type.to_sym]
+
     @has_long_cart = !!params[:lc]
     render layout: 'lite_checkout'
   end
+
+
 
 end
