224d0fff4e0c4b3dc890d28b6331b6561789a1f0
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-25 17:59:40 -0300

    Add wholesale controller

---
+++ app/controllers/wholesale_controller.rb
@@ -0,0 +1,24 @@
+
+class WholesaleController < ApplicationController
+
+  def new
+    @wholesale = Wholesale.new
+  end
+
+  def create
+    @wholesale = Wholesale.new(params[:wholesale])
+    if @wholesale.valid?
+      Resque.enqueue(NewWholesaleNotification, @wholesale)
+      redirect_to wholesale_show_path
+    else
+      render action: 'new'
+    end
+  end
+
+  def show
+  end
+
+  def meta_description
+    "Revenda na Olook as principais tendências da moda. Vantagens especiais e descontos progressivos. Seja uma de nossas afiliadas e ganhe com a gente."
+  end
+end
