f2cc24c25e4ce76fdc043ddcd008c33c8ba0e057
Lucas <lucas.santanadesouza@gmail.com>
2014-08-26 15:34:24 -0300

    Name Validation added

--- app/models/wholesale.rb
+++ app/models/wholesale.rb
@@ -15,6 +15,7 @@ class Wholesale
     errors.add(:cnpj, I18n.t("activerecord.errors.models.wholesale.attributes.cnpj")) if cnpj.blank?
     errors.add(:corporate_name, I18n.t("activerecord.errors.models.wholesale.attributes.corporate_name")) if corporate_name.blank?
     errors.add(:fantasy_name, I18n.t("activerecord.errors.models.wholesale.attributes.fantasy_name")) if fantasy_name.blank?
+    errors.add(:first_name, I18n.t("activerecord.errors.models.wholesale.attributes.first_name")) if first_name.blank?
     errors.add(:email, I18n.t("activerecord.errors.models.wholesale.attributes.email")) if email.blank?
     errors.add(:cellphone, I18n.t("activerecord.errors.models.wholesale.attributes.cellphone")) if state.blank?
     errors.add(:telephone, I18n.t("activerecord.errors.models.wholesale.attributes.telephone")) if telephone.blank?
