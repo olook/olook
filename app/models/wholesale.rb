90de1a6adeaa614f1da706b91ea0b22d69ab8042
Tiago Almeida <tig.luiz.almeida@gmail.com>
2014-08-26 13:36:28 -0300

    Add only required fields

--- app/models/wholesale.rb
+++ app/models/wholesale.rb
@@ -15,19 +15,13 @@ class Wholesale
     errors.add(:cnpj, I18n.t("activerecord.errors.models.wholesale.attributes.cnpj")) if cnpj.blank?
     errors.add(:corporate_name, I18n.t("activerecord.errors.models.wholesale.attributes.corporate_name")) if corporate_name.blank?
     errors.add(:fantasy_name, I18n.t("activerecord.errors.models.wholesale.attributes.fantasy_name")) if fantasy_name.blank?
-    errors.add(:first_name, I18n.t("activerecord.errors.models.wholesale.attributes.first_name")) if first_name.blank?
-    errors.add(:last_name, I18n.t("activerecord.errors.models.wholesale.attributes.last_name")) if last_name.blank?
     errors.add(:email, I18n.t("activerecord.errors.models.wholesale.attributes.email")) if email.blank?
-    errors.add(:address, I18n.t("activerecord.errors.models.wholesale.attributes.address")) if address.blank?
-    errors.add(:neighborhood, I18n.t("activerecord.errors.models.wholesale.attributes.neighborhood")) if neighborhood.blank?
-    errors.add(:city, I18n.t("activerecord.errors.models.wholesale.attributes.city")) if city.blank?
-    errors.add(:state, I18n.t("activerecord.errors.models.wholesale.attributes.state")) if state.blank?
-    errors.add(:zip_code, I18n.t("activerecord.errors.models.wholesale.attributes.zip_code")) if zip_code.blank?
+    errors.add(:cellphone, I18n.t("activerecord.errors.models.wholesale.attributes.cellphone")) if state.blank?
     errors.add(:telephone, I18n.t("activerecord.errors.models.wholesale.attributes.telephone")) if telephone.blank?
   end
 
   def persisted?
-  	false
+    false
   end
 
 end
