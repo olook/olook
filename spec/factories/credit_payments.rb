c581d89f376ca4d0611cdfba3440d59cdbc5e95d
Luis Daher <luisotaviodaher@gmail.com>
2014-10-31 13:10:11 -0200

    removing useless Guardfile configuration

--- spec/factories/credit_payments.rb
+++ spec/factories/credit_payments.rb
@@ -1,9 +1,14 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :credit_payment do
+  factory :redeem_credit_payment, class: CreditPayment do
     total_paid 12.34
     association :credit_type, :factory => :redeem_credit_type
   end
+
+  factory :invite_credit_payment, class: CreditPayment do
+    total_paid 12.34
+    association :credit_type, :factory => :invite_credit_type
+  end  
 
 end
