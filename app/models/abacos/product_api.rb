0a44f7990032f7b77b89e1d49ca2134419657961
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-02-07 00:09:58 -0200

    Adding method on product_api to bulk consumption

--- app/models/abacos/product_api.rb
+++ app/models/abacos/product_api.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 module Abacos
   class ProductAPI
+    class ProductAPIError < StandardError; end
     extend Helpers
 
     def self.wsdl
@@ -19,6 +20,28 @@ module Abacos
       download_xml :precos_disponiveis, :dados_preco
     end
 
+    def self.download_online_inventory(product_number_list=[])
+      result = {}
+      path = ABACOS_CONFIG["wsdl_product_api"].dup.gsub('?wsdl', '')
+      path += "/EstoqueOnLine?ChaveIdentificacao=#{ABACOS_CONFIG['key']}"
+
+      product_list = product_number_list.map { |number| "ListaDeCodigosProdutos=#{number}" }
+
+      get_path = path.dup
+      results = {}
+      product_list.each do |product_path|
+        get_path += "&#{product_path}"
+        if get_path.size > 1800
+          results.merge(get_online_inventory(get_path))
+          get_path = path.dup
+        end
+      end
+      if get_path.size > path.size
+        results.merge(get_online_inventory(get_path))
+      end
+      results
+    end
+
     def self.confirm_product(protocol)
       confirm_integration :produto, protocol
     end
@@ -30,7 +53,25 @@ module Abacos
     def self.confirm_price(protocol)
       confirm_integration :preco, protocol
     end
-  private
+
+    private
+
+    def self.get_online_inventory(path)
+      Rails.logger.info("GET #{path}")
+      uri = URI.parse(path)
+      response = Net::HTTP.get_response(uri)
+      Rails.logger.debug(response.body)
+      xml_doc  = Nokogiri::XML(response.body)
+      raise ProductAPIError.new("Error on GET #{path}\nresponse body:\n#{response.body}") if xml_doc.css('ResultadoOperacao Tipo').text != 'tdreSucesso'
+      inventory_result = xml_doc.css('DadosEstoqueResultado')
+      raise ProductAPIError.new("NotFound Results, maybe products don't exist on Abacos? GET #{path}\nresponse body:\n#{response.body}") if inventory_result.size == 0
+      results = inventory_result.inject({}) do |h, node|
+        h[node.css('CodigoProduto').text] = node.css('SaldoDisponivel').text
+        h
+      end
+      results
+    end
+
     def self.confirm_integration(method, protocol)
       parsed_method = "confirmar_recebimento_#{method}".to_sym
       response = call_webservice(wsdl, parsed_method, {"Protocolo#{method.to_s.capitalize}" => protocol})
