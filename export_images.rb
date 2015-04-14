c617cc64be221678c07b6d6e4d13add3bb13558a
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-04-14 02:28:45 -0300

    Fix image export

--- export_images.rb
+++ export_images.rb
@@ -1,7 +1,11 @@
-CSV.open('pictures.csv', 'wb') do |csv|
-  csv << ['sku', 'picture_id', 'image_url', 'image_zoom_url', 'display_on']
-  Picture.includes(:product).order(:product_id).all.each do |picture|
-    csv << [picture.product.model_number, picture.id, "http:#{picture.image.url}", picture.display_on_humanize]
+Rails.logger = Logger.new STDOUT
+CSV.open('pictures.csv', 'wb', encoding: 'iso-8859-1') do |csv|
+  csv << ['ean', 'sku', 'picture_id', 'image_url', 'image_zoom_url']
+  product_ids = Variant.where('inventory > 0').pluck(:product_id)
+  Picture.where(product_id: product_ids).includes(:product).order(:product_id).all.each do |picture|
+    if picture.image.file.exists?
+      csv << ['', picture.product.model_number, picture.id, "http:#{picture.image.url}", "http:#{picture.image.zoom_out.url}"]
+    end
   end
 end
 
