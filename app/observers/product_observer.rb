04d7a3976131a66cc8f2d202083f261375cc03a1
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-06-16 02:45:39 -0300

    Fix if product doesnt have variant

--- app/observers/product_observer.rb
+++ app/observers/product_observer.rb
@@ -3,6 +3,7 @@ class ProductObserver < ActiveRecord::Observer
 
   def before_update product
     variant = product.master_variant
+    return unless variant
     if variant.price_changed? || variant.retail_price_changed?
       product.price_logs.create(price: variant.price_was, retail_price: variant.retail_price_was)
     end
