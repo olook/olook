cac655fa63f79dda9aaf94cefd226ad923c9c533
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-06-16 22:34:52 -0300

    Importing using index

--- import_pics.rb
+++ import_pics.rb
@@ -6,17 +6,16 @@ ps = tsv.inject({}) do |hash, _row|
   cod_pai = row[0].to_s.gsub(/-/, '')
   pic_url = row[2]
   hash[cod_pai] ||= []
-  hash[cod_pai] << pic_url
+  hash[cod_pai] << {url: pic_url, display_on: row[3].to_i}
   hash
 end
 ps.each do |cod_pai, pics|
   begin
     product = Product.find_by_model_number cod_pai
     puts("Not Found #{cod_pai}") || next if product.blank?
-    product.pictures.destroy_all
-    pics.each_with_index do |pic_url, ind|
-      i = ind + 1
-      i = 7 if i > 7
+    pics.each do |hash|
+      pic_url = hash[:url]
+      i = hash[:display_on]
       data = { remote_image_url: pic_url, display_on: i, product_id: product.id }
       Resque.enqueue(PictureImporterWorker, data)
       #PictureImporterWorker.perform data
