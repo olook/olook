20c8e638aa962f136d3efd65785e25e117f10009
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-06-20 03:15:51 -0300

    Fix

--- app/workers/picture_importer_worker.rb
+++ app/workers/picture_importer_worker.rb
@@ -1,7 +1,7 @@
 class PictureImporterWorker
   @queue = 'low'
   def self.perform(data, options)
-    if options['delete_ids'] && options.delete_ids.to_a.size > 0
+    if options['delete_ids'] && options['delete_ids'].to_a.size > 0
       Picture.where(id: options['delete_ids']).destroy_all
     end
     Picture.new(data).save!
