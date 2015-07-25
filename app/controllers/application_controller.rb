78470e480c609f256abee33da5eefefaabdd1075
Nelson Haraguchi <nelsonmhjr@gmail.com>
2015-07-25 01:24:31 -0300

    Set auth basic for prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: "orlok", password: "orlok" if Rails.env.staging?
+  http_basic_authenticate_with name: "olook", password: "koolook"
   protect_from_forgery
   layout "lite_application"
 
